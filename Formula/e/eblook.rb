870e32b46d553ab6065fb4d605364fe0a85ebcb7
Takahiro SATOH <zalt50cc@gmail.com>
2024-05-31 10:27:10 +0900

    eblook: update 1.6.1+media-20220426
    Signed-off-by: Takahiro SATOH <zalt50cc@gmail.com>

--- Formula/e/eblook.rb
+++ Formula/e/eblook.rb
@@ -1,11 +1,11 @@
 require "formula"
 
 class Eblook < Formula
-  url "http://green.ribbon.to/~ikazuhiro/lookup/files/eblook-1.6.1+media-20200414.tar.gz"
+  url "http://green.ribbon.to/~ikazuhiro/lookup/files/eblook-1.6.1+media-20220426.tar.gz"
   homepage "http://green.ribbon.to/~ikazuhiro/lookup/lookup.html#EBLOOK"
-  sha256 "ebd5886176c2d52aea9077ef441af06b9181bda2ac4395def55ff305c288094e"
+  sha256 "0a26ac65567bc2854245289040f5689952ba23925dff9faeae62471c475c3569"
   license "GPL-2.0"
-  version "1.6.1+media-20200414"
+  version "1.6.1+media-20220426"
 
   depends_on "zalt50/core/ebu"
 
