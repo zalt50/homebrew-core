0bd145738e451942b1807c954d93adb7a985f670
Takahiro SATOH <zalt50cc@gmail.com>
2024-06-01 02:06:33 +0900

    change sha256sum

--- Formula/e/eblook.rb
+++ Formula/e/eblook.rb
@@ -3,7 +3,7 @@ require "formula"
 class Eblook < Formula
   url "http://green.ribbon.to/~ikazuhiro/lookup/files/eblook-1.6.1+media-20220426.tar.gz"
   homepage "http://green.ribbon.to/~ikazuhiro/lookup/lookup.html#EBLOOK"
-  sha256 "0a26ac65567bc2854245289040f5689952ba23925dff9faeae62471c475c3569"
+  sha256 "e83c9521239ee046d37efa7b4cca356c1a3eac4e35a3153c90d6dd24c1cb0c0f"
   license "GPL-2.0"
   version "1.6.1+media-20220426"
 
--- Formula/e/ebu.rb
+++ Formula/e/ebu.rb
@@ -3,7 +3,7 @@ require "formula"
 class Ebu < Formula
   url "http://green.ribbon.to/~ikazuhiro/dic/files/ebu-4.5-20220808.tar.gz"
   homepage "http://green.ribbon.to/~ikazuhiro/dic/ebu.html"
-  sha256 "df72d09a937dcc91586eff5b9e4d18b9d5d80bc78dd74e541b052f3ebe92a2a5"
+  sha256 "374e90f8738d0ffc7a1e2f3d1c2be70626135f89aeb0656bc7ab0a0eb66f5b89"
   license "BSD-3-Clause"
   version "4.5-20220808"
 
