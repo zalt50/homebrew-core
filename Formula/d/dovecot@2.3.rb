639d5795563e3f51beb023418031f627c2340ce2
Takahiro SATOH <zalt50cc@gmail.com>
2025-02-01 21:02:04 +0900

    add dovecot@2.3
    Signed-off-by: Takahiro SATOH <zalt50cc@gmail.com>

---
+++ Formula/d/dovecot@2.3.rb
@@ -0,0 +1,111 @@
+class DovecotAT23 < Formula
+  desc "IMAP/POP3 server"
+  homepage "https://dovecot.org/"
+  url "https://dovecot.org/releases/2.3/dovecot-2.3.21.1.tar.gz"
+  sha256 "2d90a178c4297611088bf7daae5492a3bc3d5ab6328c3a032eb425d2c249097e"
+  license all_of: ["BSD-3-Clause", "LGPL-2.1-or-later", "MIT", "Unicode-DFS-2016", :public_domain]
+
+  livecheck do
+    url "https://www.dovecot.org/download/"
+    regex(/href=.*?dovecot[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    sha256 arm64_sequoia: "6e3f7b7e6aae562c02ddb68a1d7fc39da82f10bad221803a9c60fc60d027ebfc"
+    sha256 arm64_sonoma:  "97e1c6443d5546139a4584cc87c80b20d090eb8d228d97774356cdda65c5f278"
+    sha256 arm64_ventura: "2152eb14bd601eab60ac6fe646b59665dc73632ae8a8be07fcc6f4c3a8119da2"
+    sha256 sonoma:        "81efa1c3b5985fa1ca367731c0229125a798f88c94ff5c3de491f7e832287f9b"
+    sha256 ventura:       "cdd2b83bc98d4fe5081cfa0cf1018aa9c329189462786f324946ada25f44497d"
+    sha256 x86_64_linux:  "f4f03b65d28a0345273fcee911937a6894980e476c7d6af6bf19d4af77352ae5"
+  end
+
+  depends_on "openssl@3"
+
+  uses_from_macos "bzip2"
+  uses_from_macos "libxcrypt"
+  uses_from_macos "sqlite"
+  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "linux-pam"
+    depends_on "lz4"
+    depends_on "xz"
+    depends_on "zstd"
+  end
+
+  resource "pigeonhole" do
+    url "https://pigeonhole.dovecot.org/releases/2.3/dovecot-2.3-pigeonhole-0.5.21.tar.gz"
+    sha256 "1ca71d2659076712058a72030288f150b2b076b0306453471c5261498d3ded27"
+  end
+
+  # dbox-storage.c:296:32: error: no member named 'st_atim' in 'struct stat'
+  # dbox-storage.c:297:24: error: no member named 'st_ctim' in 'struct stat'
+  # Following two patches submitted upstream at https://github.com/dovecot/core/pull/211
+  patch do
+    url "https://github.com/dovecot/core/commit/6b2eb995da62b8eca9d8f713bd5858d3d9be8062.patch?full_index=1"
+    sha256 "3e3f74b95f95a1587a804e9484467b1ed77396376b0a18be548e91e1b904ae1b"
+  end
+
+  patch do
+    url "https://github.com/dovecot/core/commit/eca7b6b9984dd1cb5fcd28f7ebccaa5301aead1e.patch?full_index=1"
+    sha256 "cedfeadd1cd43df3eebfcf3f465314fad4f6785c33000cbbd1349e3e0eb8c0ee"
+  end
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --disable-dependency-tracking
+      --libexecdir=#{libexec}
+      --sysconfdir=#{etc}
+      --localstatedir=#{var}
+      --with-bzlib
+      --with-pam
+      --with-sqlite
+      --with-ssl=openssl
+      --with-zlib
+      --without-icu
+      --with-solr
+    ]
+
+    system "./configure", *args
+    system "make", "install"
+
+    resource("pigeonhole").stage do
+      args = %W[
+        --disable-dependency-tracking
+        --with-dovecot=#{lib}/dovecot
+        --prefix=#{prefix}
+      ]
+
+      system "./configure", *args
+      system "make"
+      system "make", "install"
+    end
+  end
+
+  def caveats
+    <<~EOS
+      For Dovecot to work, you may need to create a dovecot user
+      and group depending on your configuration file options.
+    EOS
+  end
+
+  service do
+    run [opt_sbin/"dovecot", "-F"]
+    require_root true
+    environment_variables PATH: std_service_path_env
+    error_log_path var/"log/dovecot/dovecot.log"
+    log_path var/"log/dovecot/dovecot.log"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{sbin}/dovecot --version")
+
+    cp_r share/"doc/dovecot/example-config", testpath/"example"
+    inreplace testpath/"example/conf.d/10-master.conf" do |s|
+      s.gsub! "#default_login_user = dovenull", "default_login_user = #{ENV["USER"]}"
+      s.gsub! "#default_internal_user = dovecot", "default_internal_user = #{ENV["USER"]}"
+    end
+    system bin/"doveconf", "-c", testpath/"example/dovecot.conf"
+  end
+end
