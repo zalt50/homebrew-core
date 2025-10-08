4ed12d0269711a033b719bddf693ec8c9b6c66a3
Roberto Aloi <robertoaloi@meta.com>
2025-10-08 11:20:34 +0000

    erlang-language-platform 2025-10-21 (new formula)

---
+++ Formula/e/erlang-language-platform.rb
@@ -0,0 +1,78 @@
+class ErlangLanguagePlatform < Formula
+  desc "LSP server and CLI for the Erlang programming language"
+  homepage "https://whatsapp.github.io/erlang-language-platform/"
+  # We require the submodules, so we fetch via git
+  url "https://github.com/WhatsApp/erlang-language-platform.git",
+      tag:      "2025-10-21",
+      revision: "d6f2636f015069ed32137a42f99b9b2ecbaa35a9"
+  license any_of: ["Apache-2.0", "MIT"]
+  head "github.com/WhatsApp/erlang-language-platform.git", branch: "main"
+
+  depends_on "rust" => :build
+  depends_on "sbt" => :build
+  depends_on "scala" => :build
+  depends_on "erlang"
+  depends_on "openjdk"
+  depends_on "rebar3"
+
+  def install
+    # Build eqwalizer and copy the relevant artifacts to the buildpath
+    cd "eqwalizer/eqwalizer" do
+      system "sbt", "assembly"
+      cp Dir["target/scala-*/eqwalizer.jar"].first, buildpath/"eqwalizer.jar"
+    end
+    # Build ELP, using the generated artifacts
+    ENV["ELP_EQWALIZER_PATH"] = buildpath/"eqwalizer.jar"
+    ENV["EQWALIZER_DIR"] = "eqwalizer/eqwalizer_support"
+    # The manifest is a workspace manifest, there's nothing "installable"
+    build_args = ["build", "--release"]
+    system "cargo", *build_args, *std_cargo_args.reject { |arg| arg["--root"] || arg["--path"] }
+    bin.install "target/release/elp"
+    generate_completions_from_executable(bin/"elp", "generate-completions", shells: [:bash, :fish, :zsh])
+  end
+
+  test do
+    # Test version
+    assert_match version.to_s, shell_output("#{bin}/elp version")
+
+    # Test ELP diagnostic detection
+    (testpath/"my_module.erl").write <<~ERL
+      -module(my_module).
+      -moduledoc """
+      This is a test module.
+      """.
+      -export([test_function/0]).
+
+      -doc """
+      This is a test function
+      """.
+      test_function() ->
+          X = 42,
+          ok.
+    ERL
+
+    # Run ELP lint to detect diagnostics
+    output = shell_output("#{bin}/elp lint my_module.erl", 101)
+
+    # Verify that ELP detected the unused variable diagnostic
+    assert_match("variable 'X' is unused", output)
+
+    # Test Eqwalizer integration
+    ENV["JAVA_HOME"] = Language::Java.java_home
+
+    (testpath/"my_typed_module.erl").write <<~ERL
+      -module(my_typed_module).
+      -export([test_function/0]).
+
+      -spec test_function() -> string().
+      test_function() ->
+          42.
+    ERL
+
+    # Run ELP lint to detect diagnostics
+    output = shell_output("#{bin}/elp eqwalize my_typed_module")
+
+    # Verify that ELP detected the type mismatch
+    assert_match("incompatible_types", output)
+  end
+end
