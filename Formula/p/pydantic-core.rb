0c4962645492c570955dd717117cabb902fafabe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-14 11:13:15 +0000

    pydantic-core 2.41.4

--- Formula/p/pydantic-core.rb
+++ Formula/p/pydantic-core.rb
@@ -3,8 +3,8 @@ class PydanticCore < Formula
 
   desc "Core functionality for Pydantic validation and serialization"
   homepage "https://github.com/pydantic/pydantic-core"
-  url "https://files.pythonhosted.org/packages/7d/14/12b4a0d2b0b10d8e1d9a24ad94e7bbb43335eaf29c0c4e57860e8a30734a/pydantic_core-2.41.1.tar.gz"
-  sha256 "1ad375859a6d8c356b7704ec0f547a58e82ee80bb41baa811ad710e124bc8f2f"
+  url "https://files.pythonhosted.org/packages/df/18/d0944e8eaaa3efd0a91b0f1fc537d3be55ad35091b6a87638211ba691964/pydantic_core-2.41.4.tar.gz"
+  sha256 "70e47929a9d4a1905a67e4b687d5946026390568a8e952b92824118063cee4d5"
   license "MIT"
 
   bottle do
@@ -26,23 +26,24 @@ class PydanticCore < Formula
         .map { |f| f.opt_libexec/"bin/python" }
   end
 
+  resource "typing-extensions" do
+    url "https://files.pythonhosted.org/packages/72/94/1a15dd82efb362ac84269196e94cf00f187f7ed21c242792a923cdb1c61f/typing_extensions-4.15.0.tar.gz"
+    sha256 "0cea48d173cc12fa28ecabc3b837ea3cf6f38c6d1136f85cbaaf598984861466"
+  end
+
   def install
     pythons.each do |python3|
+      resource("typing-extensions").stage do
+        system python3, "-m", "pip", "install", *std_pip_args(build_isolation: true), "."
+      end
+
       system python3, "-m", "pip", "install", *std_pip_args, "."
     end
   end
 
   test do
-    resource "typing-extensions" do
-      url "https://files.pythonhosted.org/packages/72/94/1a15dd82efb362ac84269196e94cf00f187f7ed21c242792a923cdb1c61f/typing_extensions-4.15.0.tar.gz"
-      sha256 "0cea48d173cc12fa28ecabc3b837ea3cf6f38c6d1136f85cbaaf598984861466"
-    end
-
     pythons.each do |python3|
-      venv = virtualenv_create(testpath/"venv", python3)
-      venv.pip_install resource("typing-extensions")
-      python = venv.root/"bin/python"
-      system python, "-c", "import pydantic_core;"
+      system python3, "-c", "import pydantic_core;"
     end
   end
 end
