d5025f231eb201ab0cf3dabbeb916a967492c4e8
Aditya Garg <gargaditya08@live.com>
2025-10-15 17:57:44 +0530

    pyqt: migrate to `python@3.14`
    Signed-off-by: Aditya Garg <gargaditya08@live.com>

--- Formula/p/pyqt.rb
+++ Formula/p/pyqt.rb
@@ -4,6 +4,7 @@ class Pyqt < Formula
   url "https://files.pythonhosted.org/packages/32/1b/567f46eb43ca961efd38d7a0b73efb70d7342854f075fd919179fdb2a571/pyqt6-6.9.1.tar.gz"
   sha256 "50642be03fb40f1c2111a09a1f5a0f79813e039c15e78267e6faaf8a96c1c3a6"
   license "GPL-3.0-only"
+  revision 1
 
   bottle do
     rebuild 1
@@ -15,7 +16,7 @@ class Pyqt < Formula
   end
 
   depends_on "pyqt-builder" => :build
-  depends_on "python@3.13"
+  depends_on "python@3.14"
   depends_on "qt3d"
   depends_on "qtbase"
   depends_on "qtcharts"
@@ -76,7 +77,7 @@ class Pyqt < Formula
   end
 
   def python3
-    "python3.13"
+    "python3.14"
   end
 
   def install
