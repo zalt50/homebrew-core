239bf2ca9d52652c811488724a5dac7f25705bf6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-17 21:58:41 +0000

    mlx: update 0.29.3 bottle.

--- Formula/m/mlx.rb
+++ Formula/m/mlx.rb
@@ -12,10 +12,10 @@ class Mlx < Formula
   head "https://github.com/ml-explore/mlx.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "fde68fd98567b869988dd6fb740be85cd6b792a887651d7dbf943b7b7633c4a5"
-    sha256 cellar: :any, arm64_sequoia: "bdfea8207d407ad77147f6839ddd097bcb7ea39e5963d3919aff15a39177092a"
-    sha256 cellar: :any, arm64_sonoma:  "7bd4d3e3563981462b7bdbd8b2f7fc26601ae133c9750c899c7c44b9dcc28f35"
-    sha256 cellar: :any, sonoma:        "26967d4a96e9d6af0e2be38d49a69cf6f01fd28bb4996ae0870de4a7a6c5143d"
+    sha256 cellar: :any, arm64_tahoe:   "307b6050e32cd4a06e69a1c2fdc7002b45027de60fca2c2baadeb86f2319b230"
+    sha256 cellar: :any, arm64_sequoia: "6d3c496087147a9d5b584ee0b2d07fc8713c482a8daeef6a98b69d82ffaa1bea"
+    sha256 cellar: :any, arm64_sonoma:  "f07446ac7dc92098fe2b9f577118bb44f653f893f2821e1ea92c5cfe2ffe8bee"
+    sha256 cellar: :any, sonoma:        "b5ae824f6ee2d797f9c95778fd57c6e22295b3fa5c2421a2bb5f5c33c15785c5"
   end
 
   depends_on "cmake" => :build
