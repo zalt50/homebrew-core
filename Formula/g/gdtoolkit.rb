bb38799f1a95b22d650672a328dd898da2879314
Branch Vincent <branchevincent@gmail.com>
2025-10-18 16:03:11 -0400

    gdtoolkit: migrate to `python@3.14`

--- Formula/g/gdtoolkit.rb
+++ Formula/g/gdtoolkit.rb
@@ -18,7 +18,7 @@ class Gdtoolkit < Formula
   end
 
   depends_on "libyaml"
-  depends_on "python@3.13"
+  depends_on "python@3.14"
 
   resource "colorama" do
     url "https://files.pythonhosted.org/packages/d8/53/6f443c9a4a8358a93a6792e2acffb9d9d5cb0a5cfd8802644b7b1c9a02e4/colorama-0.4.6.tar.gz"
