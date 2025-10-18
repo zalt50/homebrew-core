24aae1d285e366036e6458866d16d0a61d75846c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-18 06:01:18 +0000

    moor: update 2.6.0 bottle.

--- Formula/m/moor.rb
+++ Formula/m/moor.rb
@@ -9,12 +9,12 @@ class Moor < Formula
   no_autobump! because: :bumped_by_upstream
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2b82c06f771c38a1b3f39d9f31eca15a2187d51c8682915611b82024a7502fa4"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2b82c06f771c38a1b3f39d9f31eca15a2187d51c8682915611b82024a7502fa4"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2b82c06f771c38a1b3f39d9f31eca15a2187d51c8682915611b82024a7502fa4"
-    sha256 cellar: :any_skip_relocation, sonoma:        "f3078bf5d5add1fd6f30ac6004f67aafe5d86cdc9a2b54478dc2b9e9aefa6b09"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b9ea35c6f99de3ff3c913b76b71be9a51df607055bd8451b76641b5144fb6fde"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f2abf24387d33debd945fb68733f9e3ab353ed7730ab30f6e2aee9d1eaa4af3d"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9d37476a0b6b0cff1cff3ad26cc6edddd402e7606f12f0041d0cee4dd807b521"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9d37476a0b6b0cff1cff3ad26cc6edddd402e7606f12f0041d0cee4dd807b521"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9d37476a0b6b0cff1cff3ad26cc6edddd402e7606f12f0041d0cee4dd807b521"
+    sha256 cellar: :any_skip_relocation, sonoma:        "6cdecca86cd958d445ffa707b9af33d1b3802a7828b487cd6e8a42457428d21b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "44202aa681675085279d61a41d2707dd4e7b553eb8de8000ba665d595e1d9e47"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "900c44b62c7f2b49590cded2068fe3f81c73312f639c79e3f8ec3215dc1356c0"
   end
 
   depends_on "go" => :build
