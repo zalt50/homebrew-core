91c7572d074d558734a76e9b243dfc318ec98dcd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-18 15:52:15 +0000

    libpostal: update 1.1.4 bottle.

--- Formula/lib/libpostal.rb
+++ Formula/lib/libpostal.rb
@@ -9,12 +9,12 @@ class Libpostal < Formula
   no_autobump! because: :requires_manual_review
 
   bottle do
-    sha256 arm64_tahoe:   "0a6f889d4b41b5cbfd4a64dbdfb2c7e61a58eafcffe57335ff88edf9419f2838"
-    sha256 arm64_sequoia: "fbf12aa77b62468c76d5ac57d38f81c0e5cd379a9af827723cded2e09f45db40"
-    sha256 arm64_sonoma:  "9c6f9de07f3285847e3b7831c0f2bf8fc163ef95654574d8b47736392158b709"
-    sha256 sonoma:        "80fa9ed5f262fb37122bff7c246a211ed48a2de3de9e2a64b543e34cbd20dca4"
-    sha256 arm64_linux:   "f462facf77dfb1a6231b76f42201ee1ca5e26de5bfcc8a19b6d57d52218a463e"
-    sha256 x86_64_linux:  "1f054e6c0e4f0a12845e343c5e406a0683faf9ba71dd3880e30bcb23ad5199c1"
+    sha256 arm64_tahoe:   "cf3a9637bea332e8d28cde170dc85c7c5dbb4b5731886edd9f30bed504742e5b"
+    sha256 arm64_sequoia: "954b38f9b41bb3fe6702b91e2589baa8023b459162dc3bfa3a3dd6218bf33312"
+    sha256 arm64_sonoma:  "19638446df502bd5a2fa13bf21d89eca018e70965595940a03f89ca14b213609"
+    sha256 sonoma:        "3f4ee3c0636f7fc19ddc1fc60a2273912f64a539dc314df0488d2eab8f856308"
+    sha256 arm64_linux:   "32461394f17714bbf96a4a090b4e89240a2dd2a97637006ed0997d114d0831be"
+    sha256 x86_64_linux:  "79bf7b3b79c1e80a9d61a7ad19599b9d18440390bec4babbf85a17ad5219db34"
   end
 
   depends_on "autoconf" => :build
