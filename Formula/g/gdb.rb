13d7ec1638cb3baac7e75adc53a65645ea08d135
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-10-19 21:20:35 +0900

    gdb: migrate to `python@3.14`

--- Formula/g/gdb.rb
+++ Formula/g/gdb.rb
@@ -25,7 +25,7 @@ class Gdb < Formula
   depends_on "gmp"
   depends_on "mpfr"
   depends_on "ncurses" # https://github.com/Homebrew/homebrew-core/issues/224294
-  depends_on "python@3.13"
+  depends_on "python@3.14"
   depends_on "readline"
   depends_on "xz" # required for lzma support
   depends_on "zstd"
@@ -61,7 +61,7 @@ class Gdb < Formula
       --with-curses
       --with-expat
       --with-lzma
-      --with-python=#{which("python3.13")}
+      --with-python=#{which("python3.14")}
       --with-system-readline
       --with-system-zlib
       --with-zstd
