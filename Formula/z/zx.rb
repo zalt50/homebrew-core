7d0df727b974884fa5096b6e0e432df701af49e8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-19 14:46:11 +0000

    zx: update 8.8.5 bottle.

--- Formula/z/zx.rb
+++ Formula/z/zx.rb
@@ -6,7 +6,7 @@ class Zx < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "85be4dbfde67def6cbdbf913de4922a95e0c2df95ea61bceaad44613f885e626"
+    sha256 cellar: :any_skip_relocation, all: "7b9906d0d69c0dcf726b2e8287fc62518b2f4dc08be0cc72cbfd1b92aa79b55e"
   end
 
   depends_on "node"
