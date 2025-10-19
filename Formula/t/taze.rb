5715e94f42ede2b2a2279db3eafa867a15fd28c9
Bevan Kay <email@bevankay.me>
2025-10-19 17:05:56 +1100

    taze 19.8.0 (new formula)

---
+++ Formula/t/taze.rb
@@ -0,0 +1,32 @@
+class Taze < Formula
+  desc "Modern cli tool that keeps your deps fresh"
+  homepage "https://github.com/antfu-collective/taze"
+  url "https://registry.npmjs.org/taze/-/taze-19.8.0.tgz"
+  sha256 "5325952c53f7bbf2a1881581401a522b5808ed9edb2977676430c6c963931622"
+  license "MIT"
+  head "https://github.com/antfu-collective/taze.git", branch: "main"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/taze --version")
+
+    (testpath/"package.json").write <<~JSON
+      {
+        "name": "brewtest",
+        "version": "1.0.0",
+        "dependencies": {
+          "homebrew-nonexistent": "1.1.0"
+        }
+      }
+    JSON
+
+    output = shell_output("#{bin}/taze 2>&1")
+    assert_match "Failed to fetch package \"homebrew-nonexistent\"", output
+  end
+end
