11e70b5a011b82ca22f91578162fa6181b8bc836
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-19 11:00:28 +0000

    zsv: add 1.0.0 bottle.

--- Formula/z/zsv.rb
+++ Formula/z/zsv.rb
@@ -6,6 +6,14 @@ class Zsv < Formula
   license "MIT"
   head "https://github.com/liquidaty/zsv.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3729ee8c4eb1dce36b70fb206912248f357146485d0a39df840e376c712c576a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e649d91755a4572aaab24b5e17a6d1fe4853b7c86bad15f8ba572886e4d40a0d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ebea84879a8b0fbe6498ca58dbf7c66afe6660ded5dc708cbba30ed8c2107c43"
+    sha256 cellar: :any_skip_relocation, sonoma:        "0ec7b16bd2e324745b9c437d1ab9147d01afcd6921e47fba190aa6008f494cbc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "967bcebc244bf695843cada533bbcaa4daf071ea484a47364ed78590a8fd758b"
+  end
+
   depends_on "jq"
 
   uses_from_macos "ncurses"
