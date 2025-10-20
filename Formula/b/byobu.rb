9ec0963da459f03abd5cc38cb6fd598665c0b48c
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-10-20 15:33:45 +0900

    byobu: revision by `new` python migration

--- Formula/b/byobu.rb
+++ Formula/b/byobu.rb
@@ -4,6 +4,7 @@ class Byobu < Formula
   url "https://github.com/dustinkirkland/byobu/archive/refs/tags/6.13.tar.gz"
   sha256 "9690c629588e8f95d16b2461950d39934faaf8005dd2a283886d4e3bd6c86df6"
   license "GPL-3.0-only"
+  revision 1
 
   livecheck do
     url :stable
