6e7ebbe86d24174407c09d4248f71139594fb7de
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-20 22:56:12 +0000

    code-server 4.105.1

--- Formula/c/code-server.rb
+++ Formula/c/code-server.rb
@@ -1,8 +1,8 @@
 class CodeServer < Formula
   desc "Access VS Code through the browser"
   homepage "https://github.com/coder/code-server"
-  url "https://registry.npmjs.org/code-server/-/code-server-4.105.0.tgz"
-  sha256 "73fc1873bc15936eb6bb7a133b6201e6bcf60a5a410190168626381126079722"
+  url "https://registry.npmjs.org/code-server/-/code-server-4.105.1.tgz"
+  sha256 "4a716e1f6294eecde5ffdbfa2af146df91991ce53115d4d3e8e86d101b2101c9"
   license "MIT"
 
   bottle do
