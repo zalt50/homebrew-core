581b7370d524d232f308352b75539ee6ac8e6f42
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-21 03:42:20 +0000

    gotests: update 1.7.3 bottle.

--- Formula/g/gotests.rb
+++ Formula/g/gotests.rb
@@ -9,19 +9,12 @@ class Gotests < Formula
   no_autobump! because: :requires_manual_review
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:    "e757db1d0bd49c3857d96d0722e2287549621c5c2c664a5ae315652b23103327"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "0a4ead9137295c0e945537a9deb41f5ff067502aff113bf316a5248efb187fdf"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "c0b134507e83f35bd6ea48afde71bd95ffe174d98b9dd64fbd1f7afe7fda75b5"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "cb95a89300241d532d54e78bf10b6190a2172c6e83a8e32e00805624dfa9cd69"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a2daf6aeff322ba4de342bb2d21b485732ae4851575387860ff91462e14e8d31"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ca761cc187d08b5d211562b383610d614c67a4533de9c552f6e2b57a07b7ed5b"
-    sha256 cellar: :any_skip_relocation, sonoma:         "f6c46068d4c9adb7ce035b5eedad92647e2a2397a5040981765615c522a26108"
-    sha256 cellar: :any_skip_relocation, ventura:        "14616cbad502d944d8462efa61b0da7a0f74cd4f1fd01fb1b3e68dcd391d1ddb"
-    sha256 cellar: :any_skip_relocation, monterey:       "cbd11e7ba3400d2a6896974899257d37105759cabebe94bc4a599c739e3ade92"
-    sha256 cellar: :any_skip_relocation, big_sur:        "222e2a0280ae3d085bbf76080b9bd30e4fe1e9942a0427dc2840d74337621f76"
-    sha256 cellar: :any_skip_relocation, catalina:       "717ea92ad6172ab6ca8987b618683c85f4a576161e78bc75c02000966ea3f9ac"
-    sha256 cellar: :any_skip_relocation, arm64_linux:    "8b6170f5549b26603900f77372b9c19979e37bd363e6a9f3e9cabef4f3cb1f2f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d2f0b6e37e88deaf7713ebf6a2e34baf767e2a7b6168244f31e909ef4b8f8100"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "10a259b5cb1684f57af33bb5dd0a2ccef63808f2531d8304ea7b530e9fb5373e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "10a259b5cb1684f57af33bb5dd0a2ccef63808f2531d8304ea7b530e9fb5373e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "10a259b5cb1684f57af33bb5dd0a2ccef63808f2531d8304ea7b530e9fb5373e"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a4f223b149bd684c8b26e2b5fa6c108a510e96a541a01b189be4dd934a14cb80"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ac4b3bc5b4fa3ad6ef70b1d9abc26139c441b42b79e0157e961546ba103510af"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "889ef45d6601cb599bd65dcadf910e9b16aeeebfe2939cf3dc911d74720fdc1f"
   end
 
   depends_on "go" => [:build, :test]
