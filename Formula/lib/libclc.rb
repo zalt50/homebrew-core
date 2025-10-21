6b707067b740041896ca195d3d8e3935375bbc9e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-21 11:09:18 +0000

    libclc 21.1.4

--- Formula/lib/libclc.rb
+++ Formula/lib/libclc.rb
@@ -1,8 +1,8 @@
 class Libclc < Formula
   desc "Implementation of the library requirements of the OpenCL C programming language"
   homepage "https://libclc.llvm.org/"
-  url "https://github.com/llvm/llvm-project/releases/download/llvmorg-21.1.3/libclc-21.1.3.src.tar.xz"
-  sha256 "7ec2e1207739d617580a3585549c55cd73e44a6565e39e074344df41e5253868"
+  url "https://github.com/llvm/llvm-project/releases/download/llvmorg-21.1.4/libclc-21.1.4.src.tar.xz"
+  sha256 "2311128b86ae95b4319341671a8ec93950928d0bc4fa13d0e6a97d7a65507aaa"
   license "Apache-2.0" => { with: "LLVM-exception" }
 
   livecheck do
