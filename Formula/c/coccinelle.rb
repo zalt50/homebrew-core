52f03876cceb2e74977ff8495ea642ed107853b1
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-10-21 12:25:26 +0900

    coccinelle: migrate to `python@3.14`

--- Formula/c/coccinelle.rb
+++ Formula/c/coccinelle.rb
@@ -30,7 +30,7 @@ class Coccinelle < Formula
   depends_on "ocaml-findlib" => :build
   depends_on "opam" => :build
   depends_on "pkgconf" => :build
-  depends_on "python@3.13" => :build
+  depends_on "python@3.14" => :build
   depends_on "ocaml"
   depends_on "pcre"
 
