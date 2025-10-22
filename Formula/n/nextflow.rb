ead34e88fc3a4c19a9538cb556318191df1da3e4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-22 22:07:33 +0000

    nextflow: update 25.10.0 bottle.

--- Formula/n/nextflow.rb
+++ Formula/n/nextflow.rb
@@ -11,12 +11,12 @@ class Nextflow < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "11b7da95ac1c29791aefc6bfcf78080e2ad22e163acae7b47aa801b370a627bb"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7c579fc43e3228347df9fc19fdc0d14f2032d9aaa4b2362b0875d123e7357508"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3040aec77bc02247e282c6334825e25a8131d6b6320dcef2b67c94b694b5d906"
-    sha256 cellar: :any_skip_relocation, sonoma:        "8713354c645af039d11e468bdad6043f7fff048e73d0ba839fdf052e5a169a2a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "960825f509660273808f677856fc59d862851ac7e3d40c3fc349935a2956296e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6ad10034c0721e0ea2bee3b62ca5f69a0d93e9d43f2ed586a39b413e8f23458e"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "87eeaa6fb715b56bb62d7e930d2096d260780c1d7148b15368f44177cbea359a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1fc4f3d1978f211b9a6e28f689b75a98fa97ff975a9569837b19a06d8e921b25"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d7ab5a73bfd8359baa33bb3153a1bc2ad270b5b14d84ea8c24f1d3057cdc90f5"
+    sha256 cellar: :any_skip_relocation, sonoma:        "72d88010882e4eec672c3c7ee4bd0c476a8e09b41535d7a948c853f6f01bc991"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "76422087bc55bcf7fd6ce5275e0ff455b5ba42b1a67fd3a140029f680d4aafc5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c42f136e27541fcda577c76e56f521ccf708a601b4768664af40ae93bb079988"
   end
 
   depends_on "gradle" => :build
