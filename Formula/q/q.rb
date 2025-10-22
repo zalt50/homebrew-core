4a8ec5138c7ae4dcb04f94562b05eabc476944ca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-22 01:09:32 +0000

    q: add 0.19.9 bottle.

--- Formula/q/q.rb
+++ Formula/q/q.rb
@@ -5,6 +5,14 @@ class Q < Formula
   sha256 "18a68f067daccce97f2a7e4ce4c72e206eba03a1c57040f8c7488f100040d2fc"
   license "GPL-3.0-only"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6d601d03915b15c3729f1b1ce5d1f1e5a2d59cb25953cae53d1d2e8354899115"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6d601d03915b15c3729f1b1ce5d1f1e5a2d59cb25953cae53d1d2e8354899115"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6d601d03915b15c3729f1b1ce5d1f1e5a2d59cb25953cae53d1d2e8354899115"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e4dcaa7ed1d6a1bf6d852a5b98feb09d613452685f2e35c6536e23b1aab881b8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d99cb88155b05dfea7798eacf2aba53dbb764aa2ca87f3fe249fab5e163482be"
+  end
+
   depends_on "go" => :build
 
   def install
