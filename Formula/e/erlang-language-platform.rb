68b456fe983d9ec5052993ecc85e3844d71ad0a9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-22 12:31:23 +0000

    erlang-language-platform: add 2025-10-21 bottle.

--- Formula/e/erlang-language-platform.rb
+++ Formula/e/erlang-language-platform.rb
@@ -8,6 +8,14 @@ class ErlangLanguagePlatform < Formula
   license any_of: ["Apache-2.0", "MIT"]
   head "github.com/WhatsApp/erlang-language-platform.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "74b858f39da070d67f5606088a502bfe5eceefe97f75a179eb7d53005f615517"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ec1cdb61be4218490856f31cc21913fbe45574bd216e720b9a2bfb6aacff9f81"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b377631223b13b28a3704de569f2ac75718a842b7cb54776c2fa85002a0eeb15"
+    sha256 cellar: :any_skip_relocation, sonoma:        "dcb4f04040b91c7b1f96ac6e0231f4f3048cffa976d82abdd4b3dc1514a8c6d4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "89df1cb575a38666c08a9d17f5ac51e20dcffa9d9745483bffa27b7c5e90705a"
+  end
+
   depends_on "rust" => :build
   depends_on "sbt" => :build
   depends_on "scala" => :build
