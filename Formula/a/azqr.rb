3b11e337b039dec836a7ac83d957cf8735c13f5c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-22 17:32:01 +0000

    azqr: update 2.11.1 bottle.

--- Formula/a/azqr.rb
+++ Formula/a/azqr.rb
@@ -9,12 +9,12 @@ class Azqr < Formula
   head "https://github.com/Azure/azqr.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5aa6d7d1f25c18d322e74a2a07fb14f0cba46afea08f983b60a93c0cede601ff"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5aa6d7d1f25c18d322e74a2a07fb14f0cba46afea08f983b60a93c0cede601ff"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5aa6d7d1f25c18d322e74a2a07fb14f0cba46afea08f983b60a93c0cede601ff"
-    sha256 cellar: :any_skip_relocation, sonoma:        "af3cd72453321a52fde32f32049c6986fdc2699772d3559981713eae2bbfeffa"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "95a2921f706bc92265a6f4ceb4dcc29bef083f569fd9d2f92e621b353dc810c6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e87b02c0c0afc133f4b4634b658b7327d3532e46c2c9794059e94001599e502f"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0a71eaf129cf1b56cb82bc495aaa31c385cd2c4545506bc743e1c8572bed730c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0a71eaf129cf1b56cb82bc495aaa31c385cd2c4545506bc743e1c8572bed730c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0a71eaf129cf1b56cb82bc495aaa31c385cd2c4545506bc743e1c8572bed730c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "6ebfcc1eb5bb7e0065702e99cb2a3ea6f38ec9829718757137cf194d1cdfc38b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f2f41802e614d501ae858592f8f9b897e2f5f46e72a5eb02e4730548e7db487e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "50d550ad6f87692129a100308d14234085eb633952dc234b4f77f8829be438c6"
   end
 
   depends_on "go" => :build
