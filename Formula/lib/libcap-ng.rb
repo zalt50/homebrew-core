03e719a45a95625d45114e07ec4b75dbdb9bedeb
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-10-23 17:02:52 +0900

    libcap-ng: migrate to `python@3.14`

--- Formula/lib/libcap-ng.rb
+++ Formula/lib/libcap-ng.rb
@@ -26,12 +26,12 @@ class LibcapNg < Formula
   end
 
   depends_on "python-setuptools" => :build
-  depends_on "python@3.13" => [:build, :test]
+  depends_on "python@3.14" => [:build, :test]
   depends_on "swig" => :build
   depends_on :linux
 
   def python3
-    "python3.13"
+    "python3.14"
   end
 
   def install
