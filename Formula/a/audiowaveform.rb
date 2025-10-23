ce6ecd017a4b69475205b76241d918e59ed02839
Grant Shangreaux <grant@unabridgedsoftware.com>
2025-10-22 23:53:07 -0500

    audiowaveform: update homepage and source location
    The previous homepage URL goes to a site which has an SSL issue, and
    if bypassed ends with a 503 error about no server existing.
    Additionally, active development has moved to Codeberg.
    
    - update the homepage to the Codeberg repository page
    - update the source URL to the Codeberg release archive
    - update the sha256 for the tarball

--- Formula/a/audiowaveform.rb
+++ Formula/a/audiowaveform.rb
@@ -1,8 +1,8 @@
 class Audiowaveform < Formula
   desc "Generate waveform data and render waveform images from audio files"
-  homepage "https://waveform.prototyping.bbc.co.uk"
-  url "https://github.com/bbc/audiowaveform/archive/refs/tags/1.10.3.tar.gz"
-  sha256 "191b7d46964de9080b6321411d79c6a2746c6da40bda283bb0d46cc7e718c90b"
+  homepage "https://codeberg.org/chrisn/audiowaveform"
+  url "https://codeberg.org/chrisn/audiowaveform/archive/1.10.3.tar.gz"
+  sha256 "f33680a4c74a718648ee0567511537ad80e015fb3bf5c8440a8176414afb415a"
   license "GPL-3.0-only"
 
   bottle do
