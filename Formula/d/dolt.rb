23b51080fa8699ce89cf87b86673f4c700b47fe0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-23 23:21:00 +0000

    dolt: update 1.76.2 bottle.

--- Formula/d/dolt.rb
+++ Formula/d/dolt.rb
@@ -13,12 +13,12 @@ class Dolt < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "5f720e048b1630fe43747b80eea7c4e853590ee5c216ad6caeb576b27871ff88"
-    sha256 cellar: :any,                 arm64_sequoia: "0ad21bf03e0769341fd987436afbcc7d077839b7e2be7a5063879bacc2bdb31f"
-    sha256 cellar: :any,                 arm64_sonoma:  "69fffd0a9c1de79ceadfa38012e51e323f654eb55f395687d5fc5f6a11f44eba"
-    sha256 cellar: :any,                 sonoma:        "3a007b9069ff32f0ff558bea0e0e574edee74dd8fd9486a644b9d3060a720860"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "1c52a6d8350325328026ca13777196f572a20a13106aacaa29a3e2c5ae3cf931"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a60b1fefa51551544150ec0e5540b83b10cd67a8a8de8683492a0f2ca2d6fc3b"
+    sha256 cellar: :any,                 arm64_tahoe:   "0e516821d59b1346cdd37a2b14132fb345adc0d54e9657804437f5574fd69cb5"
+    sha256 cellar: :any,                 arm64_sequoia: "bab75331a52d73651fbac3db5474e53ab242985a7a5f2d37089894298abe4b20"
+    sha256 cellar: :any,                 arm64_sonoma:  "d573ddd2aa3afda0ac749059d432dc6c2b5aca05b14cb98b1bc7ea794d524ace"
+    sha256 cellar: :any,                 sonoma:        "2fcd495e9629fc8900171cda3274506b735d6944cff5cf506fdd21f52259731e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "746d5e23bd40abc5968651c27e0d0ff096dc3d2482b6b65a20613a298d89fba2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "692e5b252ca27d20d027d3a03b56ebabca53b225cac3e8ce2db86314bc5b841e"
   end
 
   depends_on "go" => :build
