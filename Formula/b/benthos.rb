b51e80a80e4058c8f8bd265ee50769194e1d2d89
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-23 17:20:00 +0000

    benthos: update 4.58.0 bottle.

--- Formula/b/benthos.rb
+++ Formula/b/benthos.rb
@@ -7,12 +7,12 @@ class Benthos < Formula
   head "https://github.com/redpanda-data/benthos.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2d3871c4ba7a7e9577eeafe34628ee9649740162976f2be28f23ae0baf406062"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2d3871c4ba7a7e9577eeafe34628ee9649740162976f2be28f23ae0baf406062"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2d3871c4ba7a7e9577eeafe34628ee9649740162976f2be28f23ae0baf406062"
-    sha256 cellar: :any_skip_relocation, sonoma:        "e3155ae3e5c731f215e0e6c26879378190b6712ae32d4cb1ef4bfa76b8a8605a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3ac8824967f5c85fc30823a1b46679a9023cc044b20f14748f0705b523809cba"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e99ebeb8a7b9ba26790b1579adfd99e3376381c9944a7854574e68a34774e16f"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ba32755590003ab105798ad288592e0e76b01c30a8199ba0939d5d786911362d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ba32755590003ab105798ad288592e0e76b01c30a8199ba0939d5d786911362d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ba32755590003ab105798ad288592e0e76b01c30a8199ba0939d5d786911362d"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e152c4a4a99609ef6681a53270b162c6bb98e1246a82f4ee52abf8eb38155522"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "5b8f839d5481265c01c5c94ca62a11218542b2ce4d3f2e71bd71e01c7717469f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4dfbaf1112a1f27fcb085a4437ac3221d8cbf60716439ebd57e534a483938c46"
   end
 
   depends_on "go" => :build
