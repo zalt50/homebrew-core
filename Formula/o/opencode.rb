a1a724393a3f22995aee5d81c4ca0d013787fd79
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-24 05:36:26 +0000

    opencode: update 0.15.16 bottle.

--- Formula/o/opencode.rb
+++ Formula/o/opencode.rb
@@ -6,12 +6,12 @@ class Opencode < Formula
   license "MIT"
 
   bottle do
-    sha256                               arm64_tahoe:   "ba1f6474b5541ae66fa254e70169dc2faad4c6f83221d4b9e1981d04d64bb7a5"
-    sha256                               arm64_sequoia: "ba1f6474b5541ae66fa254e70169dc2faad4c6f83221d4b9e1981d04d64bb7a5"
-    sha256                               arm64_sonoma:  "ba1f6474b5541ae66fa254e70169dc2faad4c6f83221d4b9e1981d04d64bb7a5"
-    sha256 cellar: :any_skip_relocation, sonoma:        "795004ea2f733dbfd1740cd50653aa6d2f65adc8481f903b76e209f382190cf1"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "2f22bc3668ed782744ac6d9a8c5aef58f08a68b9447d600e2ea73675a9469365"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d742dd2a82e736efb75643c2e01bf5f20526c79611895508379cc5862ecd0a9d"
+    sha256                               arm64_tahoe:   "375364c49e39eb0e0b4db84575818db6ef93e031422ffcb65945363d5514d3de"
+    sha256                               arm64_sequoia: "375364c49e39eb0e0b4db84575818db6ef93e031422ffcb65945363d5514d3de"
+    sha256                               arm64_sonoma:  "375364c49e39eb0e0b4db84575818db6ef93e031422ffcb65945363d5514d3de"
+    sha256 cellar: :any_skip_relocation, sonoma:        "56b49a6eec0287713c1ede910ba04ca15b76cc1cb15d91006ec9adca7f1f75b5"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "cb3ede84d70713a7bae59dbebf9322040d63fe6410d25d63753f88c1f6fcdfeb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "449b646550870d95edfdabc9736031513d84427165f291373d7dad1027905990"
   end
 
   depends_on "node"
