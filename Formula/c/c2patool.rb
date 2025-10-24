60662a7cef9cb811e9e1a280d71abc82f3b0878b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-24 23:12:24 +0000

    c2patool: update 0.24.0 bottle.

--- Formula/c/c2patool.rb
+++ Formula/c/c2patool.rb
@@ -12,12 +12,12 @@ class C2patool < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "8722bad5997f234d11f5c37a0a9e508857b6cb9b0b41e94e2b94c3eaafeb3cd7"
-    sha256 cellar: :any,                 arm64_sequoia: "bfa0c8d2bfea87cac092c3ba1192cbd61da2bf307d302849e85e39646fdaf8f3"
-    sha256 cellar: :any,                 arm64_sonoma:  "a8cf8886be73579b9036d73b7194b036a0d3bc18c76ce3b7e3ada12b0660183f"
-    sha256 cellar: :any,                 sonoma:        "1dd0969b1899cf6155c4e3702bc21fc262c89bf0728e2502bf1052d00d99a9c8"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a1709bdb01e7204aad9bf71a0b1bfebe4f9d1c811d541b71da28215941272b04"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4cd751d40da5f0e4dd7a5f9299934b15f03cdd34f9576a34087d96a30417e270"
+    sha256 cellar: :any,                 arm64_tahoe:   "083fb4728682fc44354c6526cfd30ca1e8f2f43c74a640186d0ededc2f3ead6f"
+    sha256 cellar: :any,                 arm64_sequoia: "740a22ec2d358bbfcbff4cd9988d33cf806623c0cc5c9d2b09f5fa3dd7ed4b27"
+    sha256 cellar: :any,                 arm64_sonoma:  "f5b2008492c275aa784187a2517fcb328547722789f53092d2aef39ef1aca505"
+    sha256 cellar: :any,                 sonoma:        "13c744d5e010afb3fc20f37e17575836d33e22ab3d14a2d6c3d8dc0940492f47"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "7ac6df8ab6220c938498c1cc083a1b9a6268a9677c14c9829193aa53c821c55b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "42bcabe3893755127464e157ab1103c862ec8874b0647c50c2de03a861a5ff53"
   end
 
   depends_on "pkgconf" => :build
