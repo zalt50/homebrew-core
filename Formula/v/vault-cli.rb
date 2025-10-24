a76dc5b5c9ebcfd4b8324f24963a28713277a3e6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-24 11:43:49 +0000

    vault-cli 4.1.4

--- Formula/v/vault-cli.rb
+++ Formula/v/vault-cli.rb
@@ -1,8 +1,8 @@
 class VaultCli < Formula
   desc "Subversion-like utility to work with Jackrabbit FileVault"
   homepage "https://jackrabbit.apache.org/filevault/index.html"
-  url "https://search.maven.org/remotecontent?filepath=org/apache/jackrabbit/vault/vault-cli/4.0.0/vault-cli-4.0.0-bin.tar.gz"
-  sha256 "d63b105df96888dc2fdf06b52a2241388c0751e5030111de8a963f6b8bc4233d"
+  url "https://search.maven.org/remotecontent?filepath=org/apache/jackrabbit/vault/vault-cli/4.1.4/vault-cli-4.1.4-bin.tar.gz"
+  sha256 "f832ab743b3b0947565fbd2cad652ad4a314156e63fd95db4ab619f4cf6aa38c"
   license "Apache-2.0"
   head "https://github.com/apache/jackrabbit-filevault.git", branch: "master"
 
