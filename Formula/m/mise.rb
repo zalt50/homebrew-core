ce911a920aa32c8bf234525d2fa418317a807a4e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-24 20:15:33 +0000

    mise 2025.10.17

--- Formula/m/mise.rb
+++ Formula/m/mise.rb
@@ -1,8 +1,8 @@
 class Mise < Formula
   desc "Polyglot runtime manager (asdf rust clone)"
   homepage "https://mise.jdx.dev/"
-  url "https://github.com/jdx/mise/archive/refs/tags/v2025.10.16.tar.gz"
-  sha256 "c05d929ac6f352f92d65c24febfc852253cd8504d4afdb38bcf3df5c242934f7"
+  url "https://github.com/jdx/mise/archive/refs/tags/v2025.10.17.tar.gz"
+  sha256 "5b72cb4d8eff9a6b8e56b18ecbf8c3cdd220e1bb6dcad6c7b552f09448c30077"
   license "MIT"
   head "https://github.com/jdx/mise.git", branch: "main"
 
