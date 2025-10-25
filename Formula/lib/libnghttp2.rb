bb136c67fcfac92c7f33af6beb0733777abf7569
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-25 11:02:10 +0000

    libnghttp2 1.68.0

--- Formula/lib/libnghttp2.rb
+++ Formula/lib/libnghttp2.rb
@@ -1,12 +1,12 @@
 class Libnghttp2 < Formula
   desc "HTTP/2 C Library"
   homepage "https://nghttp2.org/"
-  url "https://github.com/nghttp2/nghttp2/releases/download/v1.67.1/nghttp2-1.67.1.tar.gz"
-  mirror "http://fresh-center.net/linux/www/nghttp2-1.67.1.tar.gz"
-  mirror "http://fresh-center.net/linux/www/legacy/nghttp2-1.67.1.tar.gz"
+  url "https://github.com/nghttp2/nghttp2/releases/download/v1.68.0/nghttp2-1.68.0.tar.gz"
+  mirror "http://fresh-center.net/linux/www/nghttp2-1.68.0.tar.gz"
+  mirror "http://fresh-center.net/linux/www/legacy/nghttp2-1.68.0.tar.gz"
   # this legacy mirror is for user to install from the source when https not working for them
   # see discussions in here, https://github.com/Homebrew/homebrew-core/pull/133078#discussion_r1221941917
-  sha256 "da8d640f55036b1f5c9cd950083248ec956256959dc74584e12c43550d6ec0ef"
+  sha256 "2c16ffc588ad3f9e2613c3fad72db48ecb5ce15bc362fcc85b342e48daf51013"
   license "MIT"
 
   livecheck do
