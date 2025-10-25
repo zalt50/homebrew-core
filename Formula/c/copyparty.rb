40160cdf322e48ddc2798274de925882faff80f6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-25 16:55:05 +0000

    copyparty 1.19.18

--- Formula/c/copyparty.rb
+++ Formula/c/copyparty.rb
@@ -3,8 +3,8 @@ class Copyparty < Formula
 
   desc "Portable file server"
   homepage "https://github.com/9001/copyparty"
-  url "https://files.pythonhosted.org/packages/f7/1f/6cfc4c4b37f0fdc30927761364fc83eea8b3ad2873ec9a3802384cecd123/copyparty-1.19.17.tar.gz"
-  sha256 "099b2e59108ba7bed35c143ac438861e0b4313dfa7dd4ae9d9d4edaad9cc2777"
+  url "https://files.pythonhosted.org/packages/39/92/b23f162a789b8367b102c92bbce3790c6d54d02b6d02b7f23fa18e89e3de/copyparty-1.19.18.tar.gz"
+  sha256 "debfaeefee55a9d79a3de26cb45522c9b7a23983383b79583ecfc9f1a97a52d7"
   license "MIT"
 
   bottle do
