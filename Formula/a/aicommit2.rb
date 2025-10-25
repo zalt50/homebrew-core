77833c0dda7bc141ac5dd2689db34f02b3c841bc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-25 04:48:59 +0000

    aicommit2 2.4.13

--- Formula/a/aicommit2.rb
+++ Formula/a/aicommit2.rb
@@ -1,8 +1,8 @@
 class Aicommit2 < Formula
   desc "Reactive CLI that generates commit messages for Git and Jujutsu with AI"
   homepage "https://github.com/tak-bro/aicommit2"
-  url "https://registry.npmjs.org/aicommit2/-/aicommit2-2.4.12.tgz"
-  sha256 "3ca087a41c266948c2013ea3fbbc2492745d93799556a3074bb1b79d6916322c"
+  url "https://registry.npmjs.org/aicommit2/-/aicommit2-2.4.13.tgz"
+  sha256 "4442c27366f5c76bcf5be679f18964fdb9d2f7a2433871565c07a849fcebca49"
   license "MIT"
 
   bottle do
