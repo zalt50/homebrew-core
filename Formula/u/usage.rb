8706674701dac34308bf8edc857126feb9d66588
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-26 15:06:48 +0000

    usage: update 2.5.1 bottle.

--- Formula/u/usage.rb
+++ Formula/u/usage.rb
@@ -7,13 +7,12 @@ class Usage < Formula
   head "https://github.com/jdx/usage.git", branch: "main"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7647703cfaae159361e00005d2b4f560a33822aa58663409e75c9abcf80b62b6"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7a8dd31ff5e62e0d37e645016e2ed6af8a7daaa03b451a57898fba2c777a67d6"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2d262faed157c1ef8bc2a4862770a30a0889fd9ac420e71e294b13e8c81920c1"
-    sha256 cellar: :any_skip_relocation, sonoma:        "608684fe0c13071a5343921d3689e4f3467cbcce4d0f936403e4bc9c62a7e4fb"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "fb489d829cbb86c4ad8e75d88cb1d961d06f9e642baa58e5c8fb7ef0cbc6c275"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8ecef288b31572339c32247c0bcfe4f8d0efd277b4ca3dc5ef8b20ab5ba2dc89"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1c7aed70d5e3e99ce958d355ed9b6b4800125e875e43fedc472519574e3956ff"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "603a8118b421ae32a8faafc60aac97d0f6af9bb4d0cc62a31c8e857cf7b52d22"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e5bcbc763b3a91f60be417a647bd43a3646bbb02a1a4527f25c958fd60010d32"
+    sha256 cellar: :any_skip_relocation, sonoma:        "34a85f4297b0de61d9c904a2ae15837c087de50dd9d29d3448da0576c518f401"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e35dc4d85bca253738bd219616e96421aecc3b697e80137f6c9df634dd4154ea"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d73eba30c8418302d9ddb6400ba09c68827838aefa8f0001d4b604675889a129"
   end
 
   depends_on "rust" => :build
