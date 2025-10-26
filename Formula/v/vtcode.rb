4bcc30b2e6310864d7897b553b1f6e4534e523c5
Rui Chen <rui@chenrui.dev>
2025-10-25 21:47:35 -0400

    vtcode 0.31.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/v/vtcode.rb
@@ -0,0 +1,26 @@
+class Vtcode < Formula
+  desc "CLI Semantic Coding Agent"
+  homepage "https://github.com/vinhnx/vtcode"
+  url "https://static.crates.io/crates/vtcode/vtcode-0.31.0.crate"
+  sha256 "140f93c1b7f9beeaae51c06cd66e8dfc7989cefe7558cc716cb7d9a8ac803547"
+  license "MIT"
+  head "https://github.com/vinhnx/vtcode.git", branch: "main"
+
+  depends_on "pkgconf" => :build
+  depends_on "rust" => :build
+
+  on_linux do
+    depends_on "openssl@3"
+  end
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/vtcode --version")
+
+    output = shell_output("#{bin}/vtcode init 2>&1", 1)
+    assert_match "No API key found for OpenAI provider", output
+  end
+end
