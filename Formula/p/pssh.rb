9979fec29b712d66bbf8e5f1fe09675e33b2d2ba
Branch Vincent <branchevincent@gmail.com>
2025-10-26 16:42:26 -0700

    pssh 2.3.5

--- Formula/p/pssh.rb
+++ Formula/p/pssh.rb
@@ -2,11 +2,11 @@ class Pssh < Formula
   include Language::Python::Virtualenv
 
   desc "Parallel versions of OpenSSH and related tools"
-  homepage "https://code.google.com/archive/p/parallel-ssh/"
-  url "https://files.pythonhosted.org/packages/60/9a/8035af3a7d3d1617ae2c7c174efa4f154e5bf9c24b36b623413b38be8e4a/pssh-2.3.1.tar.gz"
-  sha256 "539f8d8363b722712310f3296f189d1ae8c690898eca93627fc89a9cb311f6b4"
+  homepage "https://github.com/lilydjwg/pssh"
+  url "https://github.com/lilydjwg/pssh/archive/refs/tags/v2.3.5.tar.gz"
+  sha256 "97277f9d08b512c6a1b6dc5eac9677f34038096bae24484452d326137ba0d080"
   license "BSD-3-Clause"
-  revision 6
+  head "https://github.com/lilydjwg/pssh.git", branch: "master"
 
   bottle do
     rebuild 7
@@ -17,17 +17,9 @@ class Pssh < Formula
 
   conflicts_with "putty", because: "both install `pscp` binaries"
 
-  # Fix for Python 3 compatibility
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/homebrew-core/1cf441a0/Patches/pssh/python3.patch"
-    sha256 "aba524c201cdc1be79ecd1896d2b04b758f173cdebd53acf606c32321a7e8c33"
-  end
-
   def install
-    # fix man folder location issue
-    inreplace "setup.py", "'man/man1'", "'share/man/man1'"
-
     virtualenv_install_with_resources
+    man1.install buildpath.glob("man/man1/*.1")
   end
 
   test do
