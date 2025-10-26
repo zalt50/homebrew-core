c71f5fa3d3e170e41c1e813b0d6f0f597a07bb7a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-26 15:29:34 +0000

    vgo: add 0.2.0 bottle.

--- Formula/v/vgo.rb
+++ Formula/v/vgo.rb
@@ -6,6 +6,14 @@ class Vgo < Formula
   license "MIT"
   head "https://github.com/vg006/vgo.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "625060653e73c0e996276bc83f4f6dfc586c10739248ea167e296ef4d1212d64"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "625060653e73c0e996276bc83f4f6dfc586c10739248ea167e296ef4d1212d64"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "625060653e73c0e996276bc83f4f6dfc586c10739248ea167e296ef4d1212d64"
+    sha256 cellar: :any_skip_relocation, sonoma:        "ed465eb32ba71c56fbb1f2430e89f8db84230a825ea361a7332d622065be2ee0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "00919354e85aa7eabe2a62e05f80e9704ec285193c5caf5a5a752d26af38084f"
+  end
+
   depends_on "go" => :build
 
   def install
