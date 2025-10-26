76153a34a61bf79c595435e81bc40d0aa62d6c20
Anton Melnikov <antonsm21@gmail.com>
2025-10-26 00:50:15 +0000

    shortest: update 0.4.9 bottle.

--- Formula/s/shortest.rb
+++ Formula/s/shortest.rb
@@ -10,6 +10,7 @@ class Shortest < Formula
     sha256 cellar: :any_skip_relocation, arm64_sequoia: "cb3317b16f32964930b0dc0800926f935f8b4b8e43587b6c307218000ff8bdb0"
     sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cb3317b16f32964930b0dc0800926f935f8b4b8e43587b6c307218000ff8bdb0"
     sha256 cellar: :any_skip_relocation, sonoma:        "6b1d99746d550614d0d529edf8a185e30f5af518e1c1013d1f89e50dce1a2ccf"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "91ca37e8ada7364de9a79f531762d0ef3abbfca5117c5de66efa702816c18bde"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "dab31d829a0e0de264d83636e041d73e5c8e01c9f7e6081921a26905509a75f0"
   end
 
