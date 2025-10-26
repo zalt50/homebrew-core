7e09c6bc42b5e0ae8975a7700859ec0913949d93
Michael Cho <michael@michaelcho.dev>
2025-10-26 09:44:30 -0400

    povray: `--disable-optimiz` and fix HEAD on Tahoe

--- Formula/p/povray.rb
+++ Formula/p/povray.rb
@@ -3,11 +3,12 @@ class Povray < Formula
   homepage "https://www.povray.org/"
   license "AGPL-3.0-or-later"
   revision 14
-  head "https://github.com/POV-Ray/povray.git", branch: "master"
 
   stable do
     url "https://github.com/POV-Ray/povray/archive/refs/tags/v3.7.0.10.tar.gz"
     sha256 "7bee83d9296b98b7956eb94210cf30aa5c1bbeada8ef6b93bb52228bbc83abff"
+
+    depends_on "boost"
 
     on_sequoia :or_newer do
       # Apply FreeBSD patches for libc++ >= 19 needed in Xcode 16.3
@@ -19,9 +20,6 @@ class Povray < Formula
         url "https://raw.githubusercontent.com/freebsd/freebsd-ports/6133473e4227abbfcf023bea6ab5eeed9c17e55b/graphics/povray37/files/patch-vfe_vfeconf.h"
         sha256 "8e2246c5ded770b0fe835ae062aca44e98fc220314e39ba6c068ed7f270b71b2"
       end
-
-      # Workaround for Xcode 16.3+, issue ref: https://github.com/POV-Ray/povray/issues/479
-      patch :DATA
     end
   end
 
@@ -43,9 +41,14 @@ class Povray < Formula
     sha256 x86_64_linux:  "56f9af299864280e57cfa641a177d27b056f8c0dfa4034aaa4ace72e5ebf50f5"
   end
 
+  head do
+    url "https://github.com/POV-Ray/povray.git", branch: "master"
+    depends_on "boost" => :build
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
-  depends_on "boost"
+  depends_on "pkgconf" => :build
   depends_on "imath"
   depends_on "jpeg-turbo"
   depends_on "libpng"
@@ -57,15 +60,24 @@ class Povray < Formula
   def install
     ENV.cxx11
     # See https://github.com/freebsd/freebsd-ports/commit/6133473e4227abbfcf023bea6ab5eeed9c17e55b
-    if OS.mac? && MacOS.version >= :sequoia
+    if build.stable? && OS.mac? && MacOS.version >= :sequoia
       ENV.append "CPPFLAGS", "-DPOVMSUCS2=char16_t -DUCS2=char16_t -DUCS4=char32_t"
     end
 
+    # Workaround for Xcode 16.3+, issue ref: https://github.com/POV-Ray/povray/issues/479
+    inreplace "source/#{build.stable? ? "backend" : "core"}/shape/truetype.cpp",
+              "#if !defined(TARGET_OS_MAC)",
+              "#if !defined(__MACTYPES__)"
+
+    # Remove bundled libraries
+    rm_r("libraries")
+
+    # Disable optimizations similar to Debian/Fedora. This mainly removes `-ffast-math`
+    # as `povray` has open bugs like https://github.com/POV-Ray/povray/issues/460.
+    # Other optimizations like `-O3` and `-march=native` were always removed by brew.
     args = %W[
-      COMPILED_BY=homebrew
-      --disable-debug
-      --disable-dependency-tracking
-      --prefix=#{prefix}
+      COMPILED_BY=#{tap&.user || "Homebrew"}
+      --disable-optimiz
       --mandir=#{man}
       --with-boost=#{Formula["boost"].opt_prefix}
       --with-openexr=#{Formula["openexr"].opt_prefix}
@@ -75,39 +87,28 @@ class Povray < Formula
 
     # Adjust some scripts to search for `etc` in HOMEBREW_PREFIX.
     %w[allanim allscene portfolio].each do |script|
-      inreplace "unix/scripts/#{script}.sh",
-                /^DEFAULT_DIR=.*$/, "DEFAULT_DIR=#{HOMEBREW_PREFIX}"
+      inreplace "unix/scripts/#{script}.sh", /^DEFAULT_DIR=.*$/, "DEFAULT_DIR=#{HOMEBREW_PREFIX}"
     end
 
     cd "unix" do
       system "./prebuild.sh"
     end
 
-    system "./configure", *args
+    system "./configure", *args, *std_configure_args
     system "make", "install"
   end
 
   test do
-    # Condensed version of `share/povray-3.7/scripts/allscene.sh` that only
-    # renders variants of the famous Utah teapot as a quick smoke test.
-    scenes = share.glob("povray-3.7/scenes/advanced/teapot/*.pov")
+    # Render variants of the famous Utah teapot as a quick smoke test
+    sampledir = share/"povray-#{version.major_minor}"
+    scenes = sampledir.glob("scenes/advanced/teapot/*.pov")
     refute_empty scenes, "Failed to find test scenes."
+
+    # Also render a sample without viewing angle set
+    scenes << (sampledir/"scenes/advanced/chess2.pov")
+
     scenes.each do |scene|
-      system share/"povray-3.7/scripts/render_scene.sh", ".", scene
+      system sampledir/"scripts/render_scene.sh", ".", scene
     end
   end
 end
-
-__END__
-diff --git a/source/backend/shape/truetype.cpp b/source/backend/shape/truetype.cpp
-index 7e27ccc3..80ab047c 100644
---- a/source/backend/shape/truetype.cpp
-+++ b/source/backend/shape/truetype.cpp
-@@ -117,7 +117,7 @@ typedef unsigned int ULONG;
- typedef short FWord;
- typedef unsigned short uFWord;
- 
--#if !defined(TARGET_OS_MAC)
-+#if !defined(__MACTYPES__)
- typedef int Fixed;
- #endif
