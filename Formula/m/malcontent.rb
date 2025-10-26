39f8ca645b0419d17aa1b6d4789984dc3528379c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-26 20:11:41 +0000

    malcontent 1.16.4

--- Formula/m/malcontent.rb
+++ Formula/m/malcontent.rb
@@ -1,8 +1,8 @@
 class Malcontent < Formula
   desc "Supply Chain Attack Detection, via context differential analysis and YARA"
   homepage "https://github.com/chainguard-dev/malcontent"
-  url "https://github.com/chainguard-dev/malcontent/archive/refs/tags/v1.16.3.tar.gz"
-  sha256 "8db8b509ef8277f3879154df14dffe04c5ba882df4a34ef4e037eebd9f9403c8"
+  url "https://github.com/chainguard-dev/malcontent/archive/refs/tags/v1.16.4.tar.gz"
+  sha256 "e3900136d0bf6b6947d521141eb7baa06259aa3b4a0e747493ca819470ae3783"
   license "Apache-2.0"
   head "https://github.com/chainguard-dev/malcontent.git", branch: "main"
 
