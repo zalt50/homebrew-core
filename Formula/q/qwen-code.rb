3b58a0309fb76dd9364af29fa3996f159259714a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-27 13:41:38 +0000

    qwen-code: update 0.1.0 bottle.

--- Formula/q/qwen-code.rb
+++ Formula/q/qwen-code.rb
@@ -6,12 +6,12 @@ class QwenCode < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256                               arm64_tahoe:   "a9f29cc07fbcfb267e802df01fd0a0b28f55c13df53ebc54eacda04665ddfd70"
-    sha256                               arm64_sequoia: "2b865bff0f651169e7f4b765bd082303b50e6fea05158d396b55886cbda47f9f"
-    sha256                               arm64_sonoma:  "ee6c61b92c8c10455445b4717ed3a5eff477cbd43325fb2436eb234cb3b2aba1"
-    sha256                               sonoma:        "3341ada0c797840b802bcf60ecffba0def9956d546005de261ebf38ed3ee4ff0"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "2e4b1c9bce93a5d8d0c78f355cee0aa0b5c0b62d212d8278db86efd35597a67a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "550f7984b2a6549ca22cbc5c03ad42a9c68d05f753bdb21e221933f4ebffb801"
+    sha256                               arm64_tahoe:   "199c35dad015819a4b0f7dd54d2052550a61c2297e19f222be57eeba57470803"
+    sha256                               arm64_sequoia: "dcea1507a0ff500d333ff8f8ae7b5f3c3367494d2bb5d110e13a7d4b2633053d"
+    sha256                               arm64_sonoma:  "b7823340f00fcb2169bb30b97490547894a266e8881f66b839cccf9a529d8b85"
+    sha256                               sonoma:        "2e32a325c95e783b2db0f203c4c4c6dd4efab209e143cede2501703b946fb614"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a7091c1cda35d98cc77d84de6a063a3437fafb8a4bd3dec8d373bfbfe0cae484"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5eeec4cc2aeb85b28ea7856c9cfb808ccc1ee05f02d9e8f6a9ff0f610af04c28"
   end
 
   depends_on "node"
