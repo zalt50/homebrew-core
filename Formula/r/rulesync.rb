ca92f72ecc1fc10fd34a5c97b7277f68f55323c1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-27 08:24:37 +0000

    rulesync 3.11.3

--- Formula/r/rulesync.rb
+++ Formula/r/rulesync.rb
@@ -1,8 +1,8 @@
 class Rulesync < Formula
   desc "Unified AI rules management CLI tool"
   homepage "https://github.com/dyoshikawa/rulesync"
-  url "https://registry.npmjs.org/rulesync/-/rulesync-3.11.2.tgz"
-  sha256 "53807a6771813174132e1ba3f3f6390725defc4e76e0a2ca581815e5b14bb409"
+  url "https://registry.npmjs.org/rulesync/-/rulesync-3.11.3.tgz"
+  sha256 "22232f686992708071d1a389d8445b778aa72f987a5ca2abe82c0a3d83d66bab"
   license "MIT"
 
   bottle do
