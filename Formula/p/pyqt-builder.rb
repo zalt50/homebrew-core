637fbfe04bee02ce5694259bb6aa3a36a7ef12c4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-27 21:19:29 +0000

    pyqt-builder: update 1.19.0 bottle.

--- Formula/p/pyqt-builder.rb
+++ Formula/p/pyqt-builder.rb
@@ -9,13 +9,13 @@ class PyqtBuilder < Formula
   head "https://github.com/Python-PyQt/PyQt-builder.git", branch: "main"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b267576d42c4dbe0ad3cb4406b6ad880a036123c12edcdd5a08675cb4f6f1971"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b267576d42c4dbe0ad3cb4406b6ad880a036123c12edcdd5a08675cb4f6f1971"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b267576d42c4dbe0ad3cb4406b6ad880a036123c12edcdd5a08675cb4f6f1971"
-    sha256 cellar: :any_skip_relocation, sonoma:        "dc4e2f984f70d3d7b61a0525ea625ca9adc2308b3e4c91a7c177784018be14c9"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "dc4e2f984f70d3d7b61a0525ea625ca9adc2308b3e4c91a7c177784018be14c9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dc4e2f984f70d3d7b61a0525ea625ca9adc2308b3e4c91a7c177784018be14c9"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ecfcd5b57f733f4ed32dea12d7826bea613bb2da1109ebcded9c15e27a16599b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ecfcd5b57f733f4ed32dea12d7826bea613bb2da1109ebcded9c15e27a16599b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ecfcd5b57f733f4ed32dea12d7826bea613bb2da1109ebcded9c15e27a16599b"
+    sha256 cellar: :any_skip_relocation, sonoma:        "30112f1fa107c03b9b474b41fee3ae63a1932905e7c9c355281e4068455b6f8a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "30112f1fa107c03b9b474b41fee3ae63a1932905e7c9c355281e4068455b6f8a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "30112f1fa107c03b9b474b41fee3ae63a1932905e7c9c355281e4068455b6f8a"
   end
 
   depends_on "python@3.14"
