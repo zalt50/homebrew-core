daf265a56c75e2c74d1aee6d42ca4c787f4a8c0a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-27 20:49:18 +0000

    pyrefly: update 0.39.1 bottle.

--- Formula/p/pyrefly.rb
+++ Formula/p/pyrefly.rb
@@ -7,12 +7,12 @@ class Pyrefly < Formula
   head "https://github.com/facebook/pyrefly.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e654015959cf85ec9febf58636cfe291687c4417fbc44a521cd8d7be0a50b004"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4e4c955f45d0a57433b928e970b2f66f59b80007f59884465dcac8b1caa7feb0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "33f9d29cdf6a7a1c3d98baad7a25613c3fdd655fb101f61c8c5e97054a0f24a3"
-    sha256 cellar: :any_skip_relocation, sonoma:        "122279d15a48a6a4ce4e30a6025c55346a3072dee11902c040030632194e7068"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d7b70749d09ba548b5314d1e348966b7ff9bc60e686534031174b73e4b067b4c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c01f623cf2a62609a406a2b5808f0bcd73322b8532387d17f2b8f2ebe3dee7e3"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "37a92a749a578ccbb41549c11ff93493a10ce4ce3787aee46424c3e7f7bb3541"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a9634398cac218cff997ce1d4350fddff2f232311fc365da965f54161cad6dbc"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4a92eab54e5a0bba6643c4aa66464d66def0d69cdb277503575d29ae24f56c44"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e7446d858c5118ffcd69c34c5205d04f752ca6f17b870953d41dafbbfcceed3b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "badac94a7069e5c060d86b4dca2161a8aa8e142185c64c44d3dbfd2dc8e7c301"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0de013bcdde5562333d984fe353c5e36a7104f98c4577b258d0eb90754f27ee3"
   end
 
   depends_on "rust" => :build
