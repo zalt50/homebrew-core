8f0b9851331a6bec9ec88444ef3b5549e0c475fe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-27 22:52:28 +0000

    cargo-outdated 0.18.0

--- Formula/c/cargo-outdated.rb
+++ Formula/c/cargo-outdated.rb
@@ -1,10 +1,9 @@
 class CargoOutdated < Formula
   desc "Cargo subcommand for displaying when Rust dependencies are out of date"
   homepage "https://github.com/kbknapp/cargo-outdated"
-  url "https://github.com/kbknapp/cargo-outdated/archive/refs/tags/v0.17.0.tar.gz"
-  sha256 "6c1c6914f34d3c0d9ebf26b74224fa6744a374e876b35f9836193c2b03858fa4"
+  url "https://github.com/kbknapp/cargo-outdated/archive/refs/tags/v0.18.0.tar.gz"
+  sha256 "a0acb73eca2cc65915db426e49a0b834d20ef2cb302bd2ce21c1d59f3cb1894b"
   license "MIT"
-  revision 1
   head "https://github.com/kbknapp/cargo-outdated.git", branch: "master"
 
   bottle do
@@ -25,17 +24,6 @@ class CargoOutdated < Formula
   depends_on "openssl@3"
 
   uses_from_macos "zlib"
-
-  # libgit2 1.9 patch, upstream pr ref, https://github.com/kbknapp/cargo-outdated/pull/417
-  patch do
-    url "https://github.com/kbknapp/cargo-outdated/commit/67213eb08b60f402d543d4b2aeb79f813f1ade5e.patch?full_index=1"
-    sha256 "712df30c8293327848e5156df8524f60fb425c9d397f954d88c5d31c36189a79"
-  end
-  # cargo 0.87 update
-  patch do
-    url "https://github.com/kbknapp/cargo-outdated/commit/9c766bf49d37fc2d3fc19ee6b06c4b022c7138a1.patch?full_index=1"
-    sha256 "5d3d1361804eb64272eb8d88110eeafbf998eff4262687989164b0d32e0c2225"
-  end
 
   def install
     ENV["LIBGIT2_NO_VENDOR"] = "1"
