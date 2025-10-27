64140f653c65dc8a7c27585b44316a423c8ef57c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-27 02:09:40 +0000

    fennel-ls: add 0.2.2 bottle.

--- Formula/f/fennel-ls.rb
+++ Formula/f/fennel-ls.rb
@@ -5,6 +5,14 @@ class FennelLs < Formula
   sha256 "38b5e45098f3f317d2c45a2898902795b18c6801577016df9151e54721bb667a"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d1662af687fab82378d2a7a6defa17eaecdd45f5d2fe1fdb96ad3d54b4b0f915"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d1662af687fab82378d2a7a6defa17eaecdd45f5d2fe1fdb96ad3d54b4b0f915"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d1662af687fab82378d2a7a6defa17eaecdd45f5d2fe1fdb96ad3d54b4b0f915"
+    sha256 cellar: :any_skip_relocation, sonoma:        "d1662af687fab82378d2a7a6defa17eaecdd45f5d2fe1fdb96ad3d54b4b0f915"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5cc1f870f47958e1b37446c8d34fd59598562b068e398ad006150b1519e3728f"
+  end
+
   depends_on "pandoc" => :build
   depends_on "lua"
 
