5e3bd9be421b2b8ef49dbcad8586bbed20422565
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-28 17:07:18 +0000

    gotests 1.9.0

--- Formula/g/gotests.rb
+++ Formula/g/gotests.rb
@@ -1,8 +1,8 @@
 class Gotests < Formula
   desc "Automatically generate Go test boilerplate from your source code"
   homepage "https://github.com/cweill/gotests"
-  url "https://github.com/cweill/gotests/archive/refs/tags/v1.8.0.tar.gz"
-  sha256 "6d7ce2754290c6247d3a4ee3a9ad521c84f4334e3905274c68ae232613f37953"
+  url "https://github.com/cweill/gotests/archive/refs/tags/v1.9.0.tar.gz"
+  sha256 "1a36874dd5beec211e9b9aaf7d72be8839e76b5ad0a002cb4e83b80ad948697b"
   license "Apache-2.0"
   head "https://github.com/cweill/gotests.git", branch: "develop"
 
