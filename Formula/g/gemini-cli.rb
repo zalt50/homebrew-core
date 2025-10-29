6c8aa608bb1ea6ae5eadd7c877c81f7850a36dfa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-29 05:42:27 +0000

    gemini-cli: update 0.11.0 bottle.

--- Formula/g/gemini-cli.rb
+++ Formula/g/gemini-cli.rb
@@ -6,12 +6,12 @@ class GeminiCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256                               arm64_tahoe:   "7633d3776c834c9e3ef0bb32ae1f90afa3d944895847b02ce87f3f7c4db991c4"
-    sha256                               arm64_sequoia: "e73ccc3f8c40780fc49bd02c31bca0af1da6377d183c0c251ab1d48cf477d580"
-    sha256                               arm64_sonoma:  "002ecb26b2e61d6b915e6b0fdb3d591255d6401b09a04a3c30ddcaaa59513f81"
-    sha256                               sonoma:        "d87057dd872558778d6ba16f1235f5cf449b66fe77f9b5c6acac55134f45a6df"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3604d62e7b2dfcd4a3bfd5eaa817d0c306aba527673a3de2442c39df97272a8a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ec2d1dcebb2db2b3dc52faed000cdf849aa56a9f670580f2396f536a72dc13c3"
+    sha256                               arm64_tahoe:   "d5b199074d8002d0a9a5a59c6d8c3ef9d8492d102efb0f6bdd915d7f63792cb1"
+    sha256                               arm64_sequoia: "bae3a8e80ebb4f00e475f5c2b99c64a65999dbe8655c2415214b8709b7b713f5"
+    sha256                               arm64_sonoma:  "db5e58cbecfb5c3da109ec8e348b794a4b7915d742bbbd2d294e296f78bcf61b"
+    sha256                               sonoma:        "aab05cc8b5ea7811f51b07840ba2d0a5a4190f99138f3054d56e9c10bb88fcbb"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "02f237da98c81b66813f197678b0a0a7fbbdaa73d5a346b5b07a3d1c13b0d800"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "021eddd258375a873e37cb54dcbebacdbeea7f7e8ad16ab390fa7183507abdbe"
   end
 
   depends_on "node"
