89d66aef1e9cd487e413f176902326b5c2e831de
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-29 11:13:29 +0000

    limine 10.2.1

--- Formula/l/limine.rb
+++ Formula/l/limine.rb
@@ -1,8 +1,8 @@
 class Limine < Formula
   desc "Modern, advanced, portable, multiprotocol bootloader and boot manager"
   homepage "https://codeberg.org/Limine/Limine"
-  url "https://codeberg.org/Limine/Limine/releases/download/v10.2.0/limine-10.2.0.tar.gz"
-  sha256 "78aa0a8d127e25b6b0b9c61736f62b44cb647726e7c41dc6d7193feca4786122"
+  url "https://codeberg.org/Limine/Limine/releases/download/v10.2.1/limine-10.2.1.tar.gz"
+  sha256 "b7db8d29b24a53daafc7d2c9442c8301e2de2260cc07197a9b5dd542df04e816"
   license "BSD-2-Clause"
 
   livecheck do
