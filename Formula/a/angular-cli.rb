c606934e8bf4f6a491c224fca5c4fb5b5866ae44
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-29 19:48:58 +0000

    angular-cli 20.3.8

--- Formula/a/angular-cli.rb
+++ Formula/a/angular-cli.rb
@@ -1,8 +1,8 @@
 class AngularCli < Formula
   desc "CLI tool for Angular"
   homepage "https://angular.dev/cli/"
-  url "https://registry.npmjs.org/@angular/cli/-/cli-20.3.7.tgz"
-  sha256 "6ae8946affaa36f5cc5e36c8b96cae782ae4beefec9f7320a034651e3fe7680c"
+  url "https://registry.npmjs.org/@angular/cli/-/cli-20.3.8.tgz"
+  sha256 "cc542c8c10e7e28b0c631eb7c2ae78cf419aba6e2fe522fd0b2bdb21c620dea8"
   license "MIT"
 
   bottle do
