e46a72068613aee0b45526a80a49d36ac084e61e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-30 14:39:27 +0000

    direwolf: update 1.8 bottle.

--- Formula/d/direwolf.rb
+++ Formula/d/direwolf.rb
@@ -11,14 +11,12 @@ class Direwolf < Formula
   head "https://github.com/wb2osz/direwolf.git", branch: "master"
 
   bottle do
-    sha256                               arm64_tahoe:   "9cc1935dc9cdff6065923b9b6881beaba7334bb2681f1383713c962e0ed2303a"
-    sha256                               arm64_sequoia: "f99bcf9fef1cc83130d68c43ae4da0bb86cf3020c53898272c9f3dc3fe1a69b6"
-    sha256                               arm64_sonoma:  "d99b828167a2e694d76319a97c1a5806be165f0005849fad1f3c2ed31b8b3486"
-    sha256                               arm64_ventura: "32e606d1d65f94fb27a348abf01abadd78c74601c89c47d4ad84bc957fe0c071"
-    sha256                               sonoma:        "f34cd15417a27425220ab4f6624f6c90844eb7ac5c15785cccb9122a49537f9e"
-    sha256                               ventura:       "185a41a84b513d3bbc9427a55675abdcbdde62b0dca9b736f9016f135f6a5268"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "29394877b827bdec83e7082dd20c31ff3481bccbbdb98f77c243b90788c60ff4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "89c355af2b949f2b14372ef9b9935a0de9aca700660987313d52f30887e51a8d"
+    sha256                               arm64_tahoe:   "a5a1f073383e637adb447825393d588a0c1c0562a20c1899783da6eec80c17b5"
+    sha256                               arm64_sequoia: "d8eee1f375cf3bbcbab59ca4cfda791fb9bea83badbd2386133a062f17d13084"
+    sha256                               arm64_sonoma:  "2546b37fb83a5c0f35096547fc22bd4d5610700c0598a5c58297e162604be78f"
+    sha256                               sonoma:        "042e43a3932fecd186791932f50e26a77bc8aa77efb85c6e214e85f219b95084"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "cc6687ae305f7c46779b16b57aa3846697dfcbe4a12ba70eac4afabc84931297"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "87cbcbcaa44059571a339bd152b9b3b011d9ad53c633e3ff5ace6cf5460cd9a2"
   end
 
   depends_on "cmake" => :build
