c2ea95e24e1580f43165c71f684871a78cf9458e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-30 11:25:49 +0000

    cpptrace: add 1.0.4 bottle.

--- Formula/c/cpptrace.rb
+++ Formula/c/cpptrace.rb
@@ -5,6 +5,14 @@ class Cpptrace < Formula
   sha256 "5c9f5b301e903714a4d01f1057b9543fa540f7bfcc5e3f8bd1748e652e24f9ea"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "714c67c234ab6bd2171290fd8d6738e1a79baf2ac93f3fb9ddcba22f510b8492"
+    sha256 cellar: :any,                 arm64_sequoia: "1e2015f8f8175f025a2afef193c218f2bcf6491a597b3a24383680cd608511d2"
+    sha256 cellar: :any,                 arm64_sonoma:  "ca98f143e04d55612619460edaec82fc6645d9ee938efe0ed63fc988a7acea2a"
+    sha256 cellar: :any,                 sonoma:        "6c3fa5f1410a4005238b36afa5c1e444f8e8716b540731dec1092a1a70dad5e3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "14595a27d917a2b8d41000b814f5db6dfcadec7b7a0b6a6c3ecd5ac020114e3e"
+  end
+
   depends_on "cmake" => :build
   depends_on "pkgconf" => :build
   depends_on "dwarfutils"
