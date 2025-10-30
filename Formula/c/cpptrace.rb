ac142eb7d147037f548e6a849a6e5c6d2990ec90
Anton Melnikov <antonsm21@gmail.com>
2025-10-30 11:44:33 +0000

    cpptrace: update 1.0.4 bottle.

--- Formula/c/cpptrace.rb
+++ Formula/c/cpptrace.rb
@@ -10,6 +10,7 @@ class Cpptrace < Formula
     sha256 cellar: :any,                 arm64_sequoia: "1e2015f8f8175f025a2afef193c218f2bcf6491a597b3a24383680cd608511d2"
     sha256 cellar: :any,                 arm64_sonoma:  "ca98f143e04d55612619460edaec82fc6645d9ee938efe0ed63fc988a7acea2a"
     sha256 cellar: :any,                 sonoma:        "6c3fa5f1410a4005238b36afa5c1e444f8e8716b540731dec1092a1a70dad5e3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "425e59efde03a6d782b628dda04c279f8d889e77af26cedf375dfb036c860c0f"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "14595a27d917a2b8d41000b814f5db6dfcadec7b7a0b6a6c3ecd5ac020114e3e"
   end
 
