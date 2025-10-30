b64d52ca515b2d8af407b511dd1005388037b151
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-30 20:16:51 +0000

    dagger: update 0.19.4 bottle.

--- Formula/d/dagger.rb
+++ Formula/d/dagger.rb
@@ -12,12 +12,12 @@ class Dagger < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7c1a54a45bb679f5741c1e142d9075bf2c4047a0b2e36efb3ef72873319c8708"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7c1a54a45bb679f5741c1e142d9075bf2c4047a0b2e36efb3ef72873319c8708"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7c1a54a45bb679f5741c1e142d9075bf2c4047a0b2e36efb3ef72873319c8708"
-    sha256 cellar: :any_skip_relocation, sonoma:        "b57cdfe9730a5724220f136bac792b4c9dfc038aa9c0279a89f2267ee1e62f2c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "1965001968144730b909f1cbce177b8ce7c25fb52a269b451fb46edbca324fbf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bc46ee6f2b39a82ed43d04cdf600fd654d5380052eba71399551609cea18b64b"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "26244d55e8152ef8af0605c6d0ee6e1c2b0067ad323fd4b34133cdea1e11d437"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "26244d55e8152ef8af0605c6d0ee6e1c2b0067ad323fd4b34133cdea1e11d437"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "26244d55e8152ef8af0605c6d0ee6e1c2b0067ad323fd4b34133cdea1e11d437"
+    sha256 cellar: :any_skip_relocation, sonoma:        "3b17043e9a12700a6c892e8781827bc586ec377dfddece6f014bcb9484861b78"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f4a94f7f2178a276d5007ceefb97bcd0c645424a57e1f296dca9fc7b1174fb95"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6cc3e08f0797185eae853c60d07e699ec8a4e47b3889886c75e7599325140338"
   end
 
   depends_on "go" => :build
