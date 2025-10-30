2b876a43ae80f9f68eda891dff0414592b7f4fb8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-30 17:48:15 +0000

    npq: update 3.14.0 bottle.

--- Formula/n/npq.rb
+++ Formula/n/npq.rb
@@ -6,7 +6,7 @@ class Npq < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "c077f23ad09d2b09c0493fc3fa0285549fceed2043dd378ddd9af177489c80fb"
+    sha256 cellar: :any_skip_relocation, all: "9f9b7a3f021dbb34be70405c2392e2b1f0486aeb762d238fa6f3bd6426a23485"
   end
 
   depends_on "node"
