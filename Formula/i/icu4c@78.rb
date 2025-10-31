7c91b91e24a6d51053fb29d1e9a68a4e7e81c25f
botantony <antonsm21@gmail.com>
2025-10-31 21:03:58 +0100

    icu4c@78 78.1 (new formula)
    Signed-off-by: botantony <antonsm21@gmail.com>

---
+++ Formula/i/icu4c@78.rb
@@ -0,0 +1,46 @@
+class Icu4cAT78 < Formula
+  desc "C/C++ and Java libraries for Unicode and globalization"
+  homepage "https://icu.unicode.org/home"
+  url "https://github.com/unicode-org/icu/releases/download/release-78.1/icu4c-78.1-sources.tgz"
+  sha256 "6217f58ca39b23127605cfc6c7e0d3475fe4b0d63157011383d716cb41617886"
+  license "ICU"
+
+  # We allow the livecheck to detect new `icu4c` major versions in order to
+  # automate version bumps. To make sure PRs are created correctly, we output
+  # an error during installation to notify when a new formula is needed.
+  livecheck do
+    url :stable
+    regex(/^release[._-]v?(\d+(?:[.-]\d+)+)$/i)
+    strategy :git do |tags, regex|
+      tags.filter_map { |tag| tag[regex, 1]&.tr("-", ".") }
+    end
+  end
+
+  keg_only :shadowed_by_macos, "macOS provides libicucore.dylib (but nothing else)"
+
+  def install
+    odie "Major version bumps need a new formula!" if version.major.to_s != name[/@(\d+)$/, 1]
+
+    args = %w[
+      --disable-samples
+      --disable-tests
+      --enable-static
+      --with-library-bits=64
+    ]
+
+    cd "source" do
+      system "./configure", *args, *std_configure_args
+      system "make"
+      system "make", "install"
+    end
+  end
+
+  test do
+    if File.exist? "/usr/share/dict/words"
+      system bin/"gendict", "--uchars", "/usr/share/dict/words", "dict"
+    else
+      (testpath/"hello").write "hello\nworld\n"
+      system bin/"gendict", "--uchars", "hello", "dict"
+    end
+  end
+end
