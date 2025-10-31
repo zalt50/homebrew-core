df4a3aa547e6f7888fb621bd1a8f625cd9a66596
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-31 04:59:42 +0000

    go-parquet-tools 1.37.2

--- Formula/g/go-parquet-tools.rb
+++ Formula/g/go-parquet-tools.rb
@@ -1,8 +1,8 @@
 class GoParquetTools < Formula
   desc "Utility to deal with Parquet data"
   homepage "https://github.com/hangxie/parquet-tools"
-  url "https://github.com/hangxie/parquet-tools/archive/refs/tags/v1.37.1.tar.gz"
-  sha256 "55f68f282a9f8b002e0491464685a1e747fe4bd422c540118c15993264177a13"
+  url "https://github.com/hangxie/parquet-tools/archive/refs/tags/v1.37.2.tar.gz"
+  sha256 "49de797aee4e1b07fe3b6d26bb3f3c34be73fab987b57b8066b7d37b1fc1628e"
   license "BSD-3-Clause"
   head "https://github.com/hangxie/parquet-tools.git", branch: "main"
 
