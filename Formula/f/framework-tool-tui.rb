2ed95e56e9f53f6fb99069cc08b1586091c354e6
Patrick Linnane <patrick@linnane.io>
2025-10-30 18:01:09 -0700

    framework-tool-tui 0.5.6 (new formula)
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

---
+++ Formula/f/framework-tool-tui.rb
@@ -0,0 +1,24 @@
+class FrameworkToolTui < Formula
+  desc "TUI for controlling and monitoring Framework Computers hardware"
+  homepage "https://github.com/grouzen/framework-tool-tui"
+  url "https://github.com/grouzen/framework-tool-tui/archive/refs/tags/v0.5.6.tar.gz"
+  sha256 "bc7e2544de0e400b9a432778d4ad68d908f93d9663886496f4f48ae9f24c57bb"
+  license "MIT"
+  head "https://github.com/grouzen/framework-tool-tui.git", branch: "main"
+
+  depends_on "pkgconf" => :build
+  depends_on "rust" => :build
+  depends_on arch: :x86_64
+  depends_on :linux
+  depends_on "systemd" # for libudev
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    # framework-tool-tui is a TUI application
+    assert_match "The application needs to be run with root privileges",
+      shell_output("#{bin}/framework-tool-tui 2>&1", 1)
+  end
+end
