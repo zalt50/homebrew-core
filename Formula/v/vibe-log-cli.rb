1d07c06e2ad6848fee560889835a4aea12f75bcb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-31 12:26:19 +0000

    vibe-log-cli: update 0.7.4 bottle.

--- Formula/v/vibe-log-cli.rb
+++ Formula/v/vibe-log-cli.rb
@@ -6,12 +6,12 @@ class VibeLogCli < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "c231d326670398d397ff3153ce5e23f9908fd264ace8f9bef80045b7d5e02a2b"
-    sha256 cellar: :any,                 arm64_sequoia: "c4810dd59fbfee745729f6a3387d63aa62bcc90fdb7aeb772e6a9fb260a96e82"
-    sha256 cellar: :any,                 arm64_sonoma:  "c4810dd59fbfee745729f6a3387d63aa62bcc90fdb7aeb772e6a9fb260a96e82"
-    sha256 cellar: :any,                 sonoma:        "8329062b20a68f217da43756c5c83cfdab42b5d4da7aabf2e3e4d4f93c67c965"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d9f2e12bd9f51d952c3ae76431c60163a8c37d9895cb54ffe4e8acaeba764461"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "358b0d4d6d4bd5e1a9b3751811e2444d6038acf07737021ad4ebc0a577ec31a9"
+    sha256 cellar: :any,                 arm64_tahoe:   "d8a32edb59f36371e41f8b9832a07b8c0954a86614cd1c614dd7d3003657c3ca"
+    sha256 cellar: :any,                 arm64_sequoia: "919deee0f65d467307e724e05d54575a0964b1d3b692e283d68d25afd7a9555e"
+    sha256 cellar: :any,                 arm64_sonoma:  "919deee0f65d467307e724e05d54575a0964b1d3b692e283d68d25afd7a9555e"
+    sha256 cellar: :any,                 sonoma:        "9ca01cb29945ffbd0598b72e23582bf4579ea845f84fb14589b7d0402eb1f00f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "7af7583d5d627a88e00cb8e369d51840491709cd936dce4ccdae4048d9816b42"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "18c06610794232aec2a9904492695bd54821666728d58b962aa6ced524673ed8"
   end
 
   depends_on "node"
