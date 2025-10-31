8a44b356c554caf2259cce34e9f8eb51be6930ca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-31 14:24:37 +0000

    e2b: update 2.4.0 bottle.

--- Formula/e/e2b.rb
+++ Formula/e/e2b.rb
@@ -6,7 +6,7 @@ class E2b < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "8439dd33a9c0abedb06ccfd9b43192547a5d47f93ff4103258e0c4b9996e571f"
+    sha256 cellar: :any_skip_relocation, all: "b930de100502272b2daa324c9a804d07ed6b47c1a2bed79f96f7bd3b51368e25"
   end
 
   depends_on "node"
