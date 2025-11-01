a45a912eead5776c6e641139c776d758b9c46e0e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-01 21:02:29 +0000

    topgrade: update 16.1.2 bottle.

--- Formula/t/topgrade.rb
+++ Formula/t/topgrade.rb
@@ -12,12 +12,12 @@ class Topgrade < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d40cf5766e03976879e8d895a5ab0e70008186d90e81cd19bac9eb5baf73970b"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "21a48792e6f2daa67dc43a456e98a44a200950531d1ca15ec6c5d1d8078bc743"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e65b35781852af5a695687b07300526c89848a41fbc4543aab470f0d647c27dc"
-    sha256 cellar: :any_skip_relocation, sonoma:        "3318cf6721ade4975f4b4cb8009ad4a54e028f9cd276502bc4dece5b44582009"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "e471fd67b7cb5a8d60f7f2e5214f8b481559052761a5ef8852d2014a6f0e64fd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ca7450f730d7355498595f6afc85f01ce2ed84e4f935788f2c75a380963cef9f"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "96dd09d80604a4052f1df24d239d9c3a884900106dfe05d0463b535c71d286f8"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "df0032bdc7a994892625b2bc08a29c618d88d5f7d21055ee903478042f16a45d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4f74666eb1aa2917bf450436eb69ad81a6867d595a07263d57cab28a151f86ab"
+    sha256 cellar: :any_skip_relocation, sonoma:        "fc6282b12ce7356ae4999bfe4f50242a951706cca571742a5fde045b0eed96e2"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "7ec33b3bb467390043967b14276d86d4afc4d812b7d7e6d2653fe5890cff62d8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4e612c953fdae54c80cf4c16fb679f27e4e4261e63cf570139cc8a1d766c3065"
   end
 
   depends_on "rust" => :build
