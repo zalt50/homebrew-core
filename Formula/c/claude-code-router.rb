8eb823391f3465c52934ad3c1e7eb1eb64b5f552
Rui Chen <rui@chenrui.dev>
2025-11-01 15:27:12 -0400

    claude-code-router 1.0.64 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/c/claude-code-router.rb
@@ -0,0 +1,19 @@
+class ClaudeCodeRouter < Formula
+  desc "Tool to route Claude Code requests to different models and customize any request"
+  homepage "https://github.com/musistudio/claude-code-router"
+  url "https://registry.npmjs.org/@musistudio/claude-code-router/-/claude-code-router-1.0.64.tgz"
+  sha256 "41dda7ca6afca38db8ca5c522d825673cde5e0bdbc5ac1c8b7d31a9a5ee4fcaf"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/ccr version")
+    assert_match "Status: Not Running", shell_output("#{bin}/ccr status")
+  end
+end
