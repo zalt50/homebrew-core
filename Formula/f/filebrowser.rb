c7e1ea23398dce2e743fabc7b18a3517d54747b2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-01 23:12:06 +0000

    filebrowser: add 2.45.0 bottle.

--- Formula/f/filebrowser.rb
+++ Formula/f/filebrowser.rb
@@ -6,6 +6,15 @@ class Filebrowser < Formula
   license "Apache-2.0"
   head "https://github.com/filebrowser/filebrowser.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7d243c9db264104198f80074626e4089de7f11a4403e5ac68fa3306af98fa05b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "caa18c6be7571fd395bca286fb648341e0ddb6bddb0ead9f57ff83b1a2456623"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f60f78b3beb0764fc4d30956b8ae7dc98bb8495550b56976343711dfc95001ee"
+    sha256 cellar: :any_skip_relocation, sonoma:        "fd0f3cedee2446e0b033a39ff31fd94b900a8bab05af007225a11957417b12fb"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ff2dbd142c11603d5712288d2586a7fcd95d5f987a5a341673799a4f428bfe85"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b8a32194109fc42fbb3c6e715acef8f3d5fe66cf42443abc6048fe232aabfd26"
+  end
+
   depends_on "go" => :build
 
   def install
