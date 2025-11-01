20febb1eca01a182dac6fd9cbb03829e13e0f909
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-01 22:33:50 +0000

    teamtype: add 0.9.0 bottle.

--- Formula/t/teamtype.rb
+++ Formula/t/teamtype.rb
@@ -6,6 +6,15 @@ class Teamtype < Formula
   license "AGPL-3.0-or-later"
   head "https://github.com/teamtype/teamtype.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4167d805a9ca673742f74926d93fee28f70b38f369e70ce4c1bca73a4550485c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1f9e1566b9d76dc2e9fb6bb82c952fbb73bfff45128b20657023f74148741c27"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b10337c3b94b61247e909e7092bacc678ae59b2a3f57f10355b62015be54c510"
+    sha256 cellar: :any_skip_relocation, sonoma:        "783ea10276bca6114f78ca1a0fae428dce806cca03d8d1bf54953878b996d43d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e2f429aafb4b93539d399194e25a098361019264535b20a3300c30d7271dbe7e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cbfbc77a57e1995bb4874da5f4976bfc4cd429e8ecd168e67c8c8872d746a7c9"
+  end
+
   depends_on "rust" => :build
 
   uses_from_macos "zlib"
