bde59c8307cc4424feda277607c51158413280c9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-01 23:12:20 +0000

    hack-browser-data: add 0.4.6 bottle.

--- Formula/h/hack-browser-data.rb
+++ Formula/h/hack-browser-data.rb
@@ -6,6 +6,15 @@ class HackBrowserData < Formula
   license "MIT"
   head "https://github.com/moonD4rk/HackBrowserData.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cf5e0e28a426f13b59544ca0843c67c5706df28db0a87aa9a64b2b1f0fa89752"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cf5e0e28a426f13b59544ca0843c67c5706df28db0a87aa9a64b2b1f0fa89752"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cf5e0e28a426f13b59544ca0843c67c5706df28db0a87aa9a64b2b1f0fa89752"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a14333355a3288098948e2f776dd61f0846b95e7d9a77ad3593701625c4f8274"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ab15db72f8c0abd86caa4bec50a2d736ab839583ba99ca69bca6d6b62099624b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4abbd72a5a012a3d006be2fafdb7437e73bac360fa51c200384f2df42f790f7f"
+  end
+
   depends_on "go" => :build
 
   def install
