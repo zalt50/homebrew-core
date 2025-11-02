46aff4df4be6567d84c2d921f73896af8fb2bf48
Rui Chen <rui@chenrui.dev>
2025-11-02 17:04:26 -0500

    docmd 0.2.5 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/d/docmd.rb
@@ -0,0 +1,22 @@
+class Docmd < Formula
+  desc "Minimal Markdown documentation generator"
+  homepage "https://docmd.mgks.dev/"
+  url "https://registry.npmjs.org/@mgks/docmd/-/docmd-0.2.5.tgz"
+  sha256 "89b6b3b18a466b8851f61b1353f688df00ef793a5261fefaa0fb1d7b4c498935"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/docmd --version")
+
+    system bin/"docmd", "init"
+    assert_path_exists testpath/"docmd.config.js"
+    assert_match "title: \"Welcome\"", (testpath/"docs/index.md").read
+  end
+end
