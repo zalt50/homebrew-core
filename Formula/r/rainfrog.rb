30ae1a00446092ea60665801ef22c5e5865c6804
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-02 23:57:39 +0000

    rainfrog: update 0.3.9 bottle.

--- Formula/r/rainfrog.rb
+++ Formula/r/rainfrog.rb
@@ -7,12 +7,12 @@ class Rainfrog < Formula
   head "https://github.com/achristmascarl/rainfrog.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ddbb1b8e88313e8b5699b111c6ccaa93754bfa2b1a003b6308a538b7ffdb38cf"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1349373c4f491580e3c868d5ce6df31636ed942d38c16c6adea0843ddcee412d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0d87d128889ea0c93ec776d726027f65529f98a05aba15af474d7c21a389249b"
-    sha256 cellar: :any_skip_relocation, sonoma:        "fec980ed435c47dc8ebdb6b955b0ee34b337de02ea8784f9424be40da704f136"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "5f6199f29adbc71f41d4b80ba54376859aad15c6957aa29e85cd3152f840faad"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2552f3bff4330a8b998cfead588d34811939ab89261d010560512f5933e92b05"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8bbbd875cc34c55e6074b17ddba1f777537a8d68d8133e6cafe6b1eb02cf3371"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "eb05f80c4f1e6e3f4404dbea07dadafdac35b325f39d81c471d9bd4325d0437f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c7056dbbd94ca055694c5706f9a71ec9012ea2c0b2995535214801dee5a8a9b7"
+    sha256 cellar: :any_skip_relocation, sonoma:        "85d972a05316d04d604036d4277e3567b7c1b45d726edd3bf2065173db290378"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "7333da2189db79c60abc49444384b12ecc37833ffcfe6c25a63fb341b3cb6de5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "acacbdda373a6efafcd4565489baf5ab1a152c608a1fbb8eeeab9f294719aa88"
   end
 
   depends_on "rust" => :build
