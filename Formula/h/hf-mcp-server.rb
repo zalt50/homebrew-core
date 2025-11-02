9c7b50d4c5e0f065d1085ffff264849bc7e007e8
Rui Chen <rui@chenrui.dev>
2025-11-02 14:08:06 -0500

    hf-mcp-server 0.2.38 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    hf-mcp-server: update test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/h/hf-mcp-server.rb
@@ -0,0 +1,28 @@
+class HfMcpServer < Formula
+  desc "MCP Server for Hugging Face"
+  homepage "https://github.com/evalstate/hf-mcp-server"
+  url "https://registry.npmjs.org/@llmindset/hf-mcp-server/-/hf-mcp-server-0.2.38.tgz"
+  sha256 "54cbfde64e6b8e07f884329f54e29216b9313fc09a695bade34b744606444e41"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    ENV["TRANSPORT"] = "stdio"
+    ENV["DEFAULT_HF_TOKEN"] = "hf_testtoken"
+
+    output_log = testpath/"output.log"
+    pid = spawn bin/"hf-mcp-server", [:out, :err] => output_log.to_s
+    sleep 5
+    sleep 5 if OS.mac? && Hardware::CPU.intel?
+    assert_match "Failed to authenticate with Hugging Face API", output_log.read
+  ensure
+    Process.kill("TERM", pid)
+    Process.wait(pid)
+  end
+end
