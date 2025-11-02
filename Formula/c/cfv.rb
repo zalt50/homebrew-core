48394a2fd953162398517680aee36db6956e0e3e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-02 19:54:31 +0000

    cfv 3.2.0
    cfv: remove build patch
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/c/cfv.rb
+++ Formula/c/cfv.rb
@@ -3,8 +3,8 @@ class Cfv < Formula
 
   desc "Test and create various files (e.g., .sfv, .csv, .crc., .torrent)"
   homepage "https://github.com/cfv-project/cfv"
-  url "https://files.pythonhosted.org/packages/29/ca/91cca3d1799d0e74b672e30c41f82a8135fe8d5baf7e6a8af2fdea282449/cfv-3.1.0.tar.gz"
-  sha256 "8f352fe4e99837720face2a339ac793f348dd967bacf2a0ff0f5e771340261e3"
+  url "https://files.pythonhosted.org/packages/90/e2/4982f414f04c47c5344739ecc6dfd70fe75139d0672e0b080929aea309f9/cfv-3.2.0.tar.gz"
+  sha256 "090d453fc9beeb9cf37ae8edf7ebae3d9686ac9382d4bea5df9191b429e6403c"
   license "GPL-2.0-or-later"
 
   bottle do
@@ -15,13 +15,6 @@ class Cfv < Formula
   depends_on "python@3.14"
 
   def install
-    # Fix to error: SystemError: buffer overflow
-    # Issue ref: https://github.com/cfv-project/cfv/issues/76
-    inreplace "lib/cfv/term.py" do |s|
-      s.gsub! "'\\0' * struct.calcsize('h h')", "b'\\\\0' * struct.calcsize('hhhh')"
-      s.gsub! "h, w = struct.unpack('h h'", "h, w, _, _ = struct.unpack('hhhh'"
-    end
-
     virtualenv_install_with_resources
   end
 
