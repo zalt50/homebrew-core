51970a7738cf85e1ac28498fe7d63d10e0f24946
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-02 15:57:26 +0000

    jiratui: update 1.5.0 bottle.

--- Formula/j/jiratui.rb
+++ Formula/j/jiratui.rb
@@ -10,12 +10,12 @@ class Jiratui < Formula
   no_autobump! because: "has non-PyPI resources"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "0c5e93bae7da7fe3231554f6bbf22943b61807b9b9fc25afcd129879c62006d5"
-    sha256 cellar: :any,                 arm64_sequoia: "7b514a982f4679df047dd514a9aec271db4df953ccdfd6bd32aa63cc5ed47d21"
-    sha256 cellar: :any,                 arm64_sonoma:  "0f48d0b91bf7a815efc2226724d51b14332fa96f7fb24677b064a55fe0eb6d5e"
-    sha256 cellar: :any,                 sonoma:        "60d5f03170412550f7c696aed654c6a41af86432312d0c9cb5ba6d9865e69dc6"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "21b0a20e4851a9500828b2c66a530c8ac20a72d48a9fd74299897fae7bed0c8e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6072c056edb385e129ba1e08518f76d91c9feadc0171c2123dc98b4fa9c0ec6f"
+    sha256 cellar: :any,                 arm64_tahoe:   "609bf6cb0a64414cb38213d9fcab2a4256f6e6a67b45389d4b0628866ea809be"
+    sha256 cellar: :any,                 arm64_sequoia: "fbde82a6c185fe6f4813f9df6cf7c4daed7c871af5f2b233713e67db15a80709"
+    sha256 cellar: :any,                 arm64_sonoma:  "d8194578e74865bb95d13d8fde64f670b723f97fb929af94741c3d9e191cf88c"
+    sha256 cellar: :any,                 sonoma:        "777ee8832255cb4c18f3251033a5a1b319904a81922f70e8329991415c8c3dca"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "2b91770e6899890672484f834a30ab84eeea8fb0790f29c43c5a51efc39e44bd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a8c4619563e877e44292a4230b2cb0eb50e12ff7cd54aaa4bc331a1a3d797f33"
   end
 
   depends_on "rust" => :build
