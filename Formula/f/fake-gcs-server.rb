6eefbbd589d163a760736484a3a21eba634dbf8b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-03 09:47:33 +0000

    fake-gcs-server: add 1.52.3 bottle.

--- Formula/f/fake-gcs-server.rb
+++ Formula/f/fake-gcs-server.rb
@@ -6,6 +6,15 @@ class FakeGcsServer < Formula
   license "BSD-2-Clause"
   head "https://github.com/fsouza/fake-gcs-server.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e6cf95118b41871dd78746325abf6365f28bfdb20177a5ab2619267ad07ada08"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e6cf95118b41871dd78746325abf6365f28bfdb20177a5ab2619267ad07ada08"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e6cf95118b41871dd78746325abf6365f28bfdb20177a5ab2619267ad07ada08"
+    sha256 cellar: :any_skip_relocation, sonoma:        "818e78af6caa0a6d4cb75c4b02abeff3a7370764d56362f2c86ae5270688571d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "6f070f1fde54cff07e25dc556a39171277265d0ed8b3b24cea4519d50b477cfc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8ec981d8f8814f3367950009e918675f5fabde407ea3f89997dea7569f60deb1"
+  end
+
   depends_on "go" => :build
 
   def install
