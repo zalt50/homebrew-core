e4fe5b53e32c89a72dd9bc030a8c2ffb430b6941
Michael Cho <michael@michaelcho.dev>
2025-11-03 18:31:59 -0500

    deadfinder: build ffi and nokogiri from source

--- Formula/d/deadfinder.rb
+++ Formula/d/deadfinder.rb
@@ -21,14 +21,15 @@ class Deadfinder < Formula
   uses_from_macos "libffi"
   uses_from_macos "libxml2"
   uses_from_macos "libxslt"
-  uses_from_macos "xz"
   uses_from_macos "zlib"
 
   def install
+    ENV["BUNDLE_FORCE_RUBY_PLATFORM"] = "1"
+    ENV["BUNDLE_VERSION"] = "system" # Avoid installing Bundler into the keg
+    ENV["BUNDLE_WITHOUT"] = "development test"
     ENV["GEM_HOME"] = libexec
     ENV["NOKOGIRI_USE_SYSTEM_LIBRARIES"] = "1"
 
-    system "bundle", "config", "set", "without", "development", "test"
     system "bundle", "install"
     system "gem", "build", "#{name}.gemspec"
     system "gem", "install", "#{name}-#{version}.gem"
