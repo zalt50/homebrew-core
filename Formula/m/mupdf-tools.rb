b9412f2c1ea98ee6330175226c364ae5ae76a43b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-03 14:07:47 +0000

    mupdf-tools 1.26.11

--- Formula/m/mupdf-tools.rb
+++ Formula/m/mupdf-tools.rb
@@ -1,8 +1,8 @@
 class MupdfTools < Formula
   desc "Lightweight PDF and XPS viewer"
   homepage "https://mupdf.com/"
-  url "https://mupdf.com/downloads/archive/mupdf-1.26.10-source.tar.gz"
-  sha256 "1653f35bd8fbd970f05523efdc7f86e41e9728e2564a3295296e03cf59a51437"
+  url "https://mupdf.com/downloads/archive/mupdf-1.26.11-source.tar.gz"
+  sha256 "eee47fdb64de309124df21081d4a4da4ad0e917824ab2ed68fc8008f6b523979"
   license "AGPL-3.0-or-later"
   head "git://git.ghostscript.com/mupdf.git", branch: "master"
 
