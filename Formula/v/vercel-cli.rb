c4d256e17f2cb22dc0ccab31d83643b2465f0b37
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-03 22:00:36 +0000

    vercel-cli: update 48.8.1 bottle.

--- Formula/v/vercel-cli.rb
+++ Formula/v/vercel-cli.rb
@@ -6,12 +6,12 @@ class VercelCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "b64ff3d933ee92eca178db81b4a05a13f51cd9ce4bd68fcdbbfcfd74c5a5eac4"
-    sha256 cellar: :any,                 arm64_sequoia: "aa6160fb8d1d98e08bde8d75221289f76514f3b72845229ef7af0bd8deb724c3"
-    sha256 cellar: :any,                 arm64_sonoma:  "aa6160fb8d1d98e08bde8d75221289f76514f3b72845229ef7af0bd8deb724c3"
-    sha256 cellar: :any,                 sonoma:        "b10641f2a728c5e38423d374f1344e302c5650f20156fb50845dc75b4ba2d315"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "6b1c7b04d320c7a678388e7322c00e83e2db2198fb2920942d3a64d1fb543529"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1266d2021454d6997a3b16b8beca935f9bef150a280fce037f5f7c7398631904"
+    sha256 cellar: :any,                 arm64_tahoe:   "2fca8184fc9a93c6364fb94d2c9dc059f58cb25eb0418924f7c15f691000e810"
+    sha256 cellar: :any,                 arm64_sequoia: "ee9c72e4033297bb455904cabdf042d31028c207de3e89b20868859c971923fc"
+    sha256 cellar: :any,                 arm64_sonoma:  "ee9c72e4033297bb455904cabdf042d31028c207de3e89b20868859c971923fc"
+    sha256 cellar: :any,                 sonoma:        "e4554e8f5c2eca160808d2e5a9daf6b6448614e5a110d2147038d57a6ec00bf4"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1df9dfef1ffc53b5c4f2dc68741e857f0c7cbab11930b09113654147ea92e6da"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f367f8e4e8f04f0bb1ba52771da5d73c26c82819fccf7e388e4e0a9d8b07533e"
   end
 
   depends_on "node"
