f984795225e17c24eb3f2a2b128d18f7e8edbc0c
Michael Cho <michael@michaelcho.dev>
2025-11-04 14:17:06 -0500

    shared-mime-info: revision bump (libxml2 2.15.1)

--- Formula/s/shared-mime-info.rb
+++ Formula/s/shared-mime-info.rb
@@ -4,6 +4,7 @@ class SharedMimeInfo < Formula
   url "https://gitlab.freedesktop.org/xdg/shared-mime-info/-/archive/2.4/shared-mime-info-2.4.tar.bz2"
   sha256 "32dc32ae39ff1c1bf8434dd3b36770b48538a1772bc0298509d034f057005992"
   license "GPL-2.0-only"
+  revision 1
   head "https://gitlab.freedesktop.org/xdg/shared-mime-info.git", branch: "master"
 
   livecheck do
