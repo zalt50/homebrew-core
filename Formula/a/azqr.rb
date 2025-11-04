84dd3929f32c09db962ebf49b35fef6c0a05e2d8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-04 13:54:22 +0000

    azqr 2.12.2

--- Formula/a/azqr.rb
+++ Formula/a/azqr.rb
@@ -3,8 +3,8 @@ class Azqr < Formula
   homepage "https://azure.github.io/azqr/"
   # pull from git tag to get submodules
   url "https://github.com/Azure/azqr.git",
-      tag:      "v.2.12.1",
-      revision: "a3e792656713f046d0242590d88ba5c63f09e3da"
+      tag:      "v.2.12.2",
+      revision: "6191b1d0adab0dc0c8d8f416ffb1b98308714374"
   license "MIT"
   head "https://github.com/Azure/azqr.git", branch: "main"
 
