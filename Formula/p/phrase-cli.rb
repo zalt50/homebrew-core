bb95b0e0d0e89685871e302fa47bfdf57e6a3298
Phrase <phrase-bot@users.noreply.github.com>
2025-11-04 08:42:19 +0000

    phrase-cli 2.50.1

--- Formula/p/phrase-cli.rb
+++ Formula/p/phrase-cli.rb
@@ -1,8 +1,8 @@
 class PhraseCli < Formula
   desc "Tool to interact with the Phrase API"
   homepage "https://phrase.com/"
-  url "https://github.com/phrase/phrase-cli/archive/refs/tags/2.50.0.tar.gz"
-  sha256 "21e5f55f6adab9b4d7ec6d9147253ce1edb64020af4f26d8b43de7cbd3920359"
+  url "https://github.com/phrase/phrase-cli/archive/refs/tags/2.50.1.tar.gz"
+  sha256 "227f52bb4bd58d588df7ed0b49a8ed1b0e64dba390b5c6cfe80a370c507fe91e"
   license "MIT"
 
   no_autobump! because: :bumped_by_upstream
