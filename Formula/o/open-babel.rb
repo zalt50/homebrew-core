03fa05849bcf6d8dc634802229c7a82008da91d9
Michael Cho <michael@michaelcho.dev>
2025-11-04 14:27:32 -0500

    open-babel: revision bump (libxml2 2.15.1)

--- Formula/o/open-babel.rb
+++ Formula/o/open-babel.rb
@@ -2,7 +2,7 @@ class OpenBabel < Formula
   desc "Chemical toolbox"
   homepage "https://github.com/openbabel/openbabel"
   license "GPL-2.0-only"
-  revision 3
+  revision 4
   head "https://github.com/openbabel/openbabel.git", branch: "master"
 
   stable do
