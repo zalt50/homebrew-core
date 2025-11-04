9f3c7849657126315ddba2e99bf24a0908c748a6
Michael Cho <michael@michaelcho.dev>
2025-11-04 12:27:52 -0500

    s3fs: revision bump (libxml2 2.15.1)

--- Formula/s/s3fs.rb
+++ Formula/s/s3fs.rb
@@ -4,6 +4,7 @@ class S3fs < Formula
   url "https://github.com/s3fs-fuse/s3fs-fuse/archive/refs/tags/v1.95.tar.gz"
   sha256 "0c97b8922f005500d36f72aee29a1345c94191f61d795e2a7b79fb7e3e6f5517"
   license "GPL-2.0-or-later"
+  revision 1
   head "https://github.com/s3fs-fuse/s3fs-fuse.git", branch: "master"
 
   bottle do
