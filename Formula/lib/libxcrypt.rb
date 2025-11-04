5c348610841b372fbec4507a2063ceb7f73caf12
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-04 14:16:51 +0000

    libxcrypt 4.5.0

--- Formula/lib/libxcrypt.rb
+++ Formula/lib/libxcrypt.rb
@@ -1,8 +1,8 @@
 class Libxcrypt < Formula
   desc "Extended crypt library for descrypt, md5crypt, bcrypt, and others"
   homepage "https://github.com/besser82/libxcrypt"
-  url "https://github.com/besser82/libxcrypt/releases/download/v4.4.38/libxcrypt-4.4.38.tar.xz"
-  sha256 "80304b9c306ea799327f01d9a7549bdb28317789182631f1b54f4511b4206dd6"
+  url "https://github.com/besser82/libxcrypt/releases/download/v4.5.0/libxcrypt-4.5.0.tar.xz"
+  sha256 "825e764e4ff2e6304adb814cda297074b222d54a04edbd8ebc7cf58fc3af857d"
   license "LGPL-2.1-or-later"
 
   livecheck do
