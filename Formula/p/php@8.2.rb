2feb63acd030cd02cee4dd8261803e7e0131c8f6
Michael Cho <michael@michaelcho.dev>
2025-11-04 15:57:44 -0500

    php@8.2: revision bump (libxml2 2.15.1)

--- Formula/p/php@8.2.rb
+++ Formula/p/php@8.2.rb
@@ -6,6 +6,7 @@ class PhpAT82 < Formula
   mirror "https://fossies.org/linux/www/php-8.2.29.tar.xz"
   sha256 "475f991afd2d5b901fb410be407d929bc00c46285d3f439a02c59e8b6fe3589c"
   license "PHP-3.01"
+  revision 1
 
   livecheck do
     url "https://www.php.net/downloads?source=Y"
