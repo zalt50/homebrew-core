7607e3b841264830dd1ee9ea44135078307acb7b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-04 01:54:52 +0000

    rails-mcp-server: add 1.2.1 bottle.

--- Formula/r/rails-mcp-server.rb
+++ Formula/r/rails-mcp-server.rb
@@ -5,6 +5,15 @@ class RailsMcpServer < Formula
   sha256 "61b678c95903f671b3915eaabe267cc873eabe0deeb6ca12bcb9b38c55283683"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "1280efdc8d30786a99c2c1e41a51980e23ec5294f6484d59d40a589886ce6145"
+    sha256 cellar: :any,                 arm64_sequoia: "2503ac25e39bf482d53609ca7f97bc1c8298dc1160536c2deec926f05fc9deab"
+    sha256 cellar: :any,                 arm64_sonoma:  "ba7d35ff45257897bea13877bf237318a06924380d90f4713a678667fe800559"
+    sha256 cellar: :any,                 sonoma:        "55f25b5a61f55d95ba0cb32147a84c163353461a9b2f969f8f8c2c92166d9b19"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1247d44035a235d136ee9c0f68769deb49ac893ea19e0cc2e07c1e2d17136f89"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "750a710534c0065d6415e12e12a7c167d605ea00047a18f08d0d50b8f0563bb0"
+  end
+
   depends_on "openssl@3"
   depends_on "ruby"
 
