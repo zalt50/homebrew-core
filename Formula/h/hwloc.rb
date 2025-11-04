d6132638c3eb5cb2d09f91a97378546ed1f11950
Michael Cho <michael@michaelcho.dev>
2025-11-04 11:24:16 -0500

    hwloc: revision bump (libxml2 2.15.1)

--- Formula/h/hwloc.rb
+++ Formula/h/hwloc.rb
@@ -4,6 +4,7 @@ class Hwloc < Formula
   url "https://download.open-mpi.org/release/hwloc/v2.12/hwloc-2.12.2.tar.bz2"
   sha256 "563e61d70febb514138af0fac36b97621e01a4aacbca07b86e7bd95b85055ba0"
   license "BSD-3-Clause"
+  revision 1
 
   livecheck do
     url "https://www.open-mpi.org/software/hwloc/current/downloads/latest_release.txt"
