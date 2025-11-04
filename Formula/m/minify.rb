1f211629b9a22d9bf5b9848bec88181ab510b36d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-04 11:40:35 +0000

    minify: update 2.24.6 bottle.

--- Formula/m/minify.rb
+++ Formula/m/minify.rb
@@ -7,12 +7,12 @@ class Minify < Formula
   head "https://github.com/tdewolff/minify.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cf6bb6788b179c576ffbf6ee70e90bfbd268095d685fb2ce849b76f1154c247f"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cf6bb6788b179c576ffbf6ee70e90bfbd268095d685fb2ce849b76f1154c247f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cf6bb6788b179c576ffbf6ee70e90bfbd268095d685fb2ce849b76f1154c247f"
-    sha256 cellar: :any_skip_relocation, sonoma:        "1573413b45d3a43816f284e3876b2a03f43baf8b1c75df323ac0136b871716cf"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a823e1530967112f24f458414048942cdf596d77fc6d66e1ca79b40fbe2547c8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e3f91f79eeabecab095d893b328f63ff75570ac89c7f58915131ec2ef7a63c6a"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "25f9cb306efb588b1225b7a8f0842719ac48d6c59b9489efb4add0710029ff68"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "25f9cb306efb588b1225b7a8f0842719ac48d6c59b9489efb4add0710029ff68"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "25f9cb306efb588b1225b7a8f0842719ac48d6c59b9489efb4add0710029ff68"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a1ad5ef6643da1fbffe8232bae3f28900900af8ff8cb96b9a2f30977de9342bf"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a094ccdf847ed64592235e322121c29e42ea799094b9133551c893f466ac50a5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5d194306fde994dc2b3ee9c761bf4717f69c827fc24a41bf2fd0b49339e25b66"
   end
 
   depends_on "go" => :build
