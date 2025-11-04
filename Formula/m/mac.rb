5fc3dbe7866dbecba598ffb67dd9c79fa0bc78ac
botantony <antonsm21@gmail.com>
2025-11-04 10:01:32 +0100

    mac 11.80
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Formula/m/mac.rb
+++ Formula/m/mac.rb
@@ -1,9 +1,9 @@
 class Mac < Formula
   desc "Monkey's Audio lossless codec"
   homepage "https://www.monkeysaudio.com"
-  url "https://monkeysaudio.com/files/MAC_1171_SDK.zip"
-  version "11.71"
-  sha256 "51665d0d3fa6ad3969b2a2208ef7568b0f82c1c2f99710321d50d1fdb31ae6b6"
+  url "https://monkeysaudio.com/files/MAC_1180_SDK.zip"
+  version "11.80"
+  sha256 "d78310017e42d05188dbc4002d358f17502368dc450123d1d791eb0ba50af575"
   license "BSD-3-Clause"
 
   livecheck do
