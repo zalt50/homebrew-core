2bf983a358a49a9fa8f7659258dc62441929f4ca
Michael Cho <michael@michaelcho.dev>
2025-11-05 16:17:09 -0500

    kdoctools: revision bump (libxml2 2.15.1)

--- Formula/k/kdoctools.rb
+++ Formula/k/kdoctools.rb
@@ -9,6 +9,7 @@ class Kdoctools < Formula
     "LGPL-2.1-or-later",
     any_of: ["LGPL-2.1-only", "LGPL-3.0-only"],
   ]
+  revision 1
   head "https://invent.kde.org/frameworks/kdoctools.git", branch: "master"
 
   livecheck do
