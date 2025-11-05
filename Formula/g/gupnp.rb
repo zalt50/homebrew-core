80144fc95b16b382b00ff2dba095d05f68fadf50
Michael Cho <michael@michaelcho.dev>
2025-11-04 21:02:05 -0500

    gupnp: revision bump (libxml2 2.15.1)

--- Formula/g/gupnp.rb
+++ Formula/g/gupnp.rb
@@ -6,6 +6,7 @@ class Gupnp < Formula
   url "https://download.gnome.org/sources/gupnp/1.6/gupnp-1.6.9.tar.xz"
   sha256 "2edb6ee3613558e62f538735368aee27151b7e09d4e2e2c51606833da801869b"
   license "LGPL-2.0-or-later"
+  revision 1
 
   bottle do
     rebuild 1
