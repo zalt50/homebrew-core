9810848a77a7f15c92b3d1b8341519e0305e146a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-05 21:23:46 +0000

    gptscript: update 0.9.7 bottle.

--- Formula/g/gptscript.rb
+++ Formula/g/gptscript.rb
@@ -7,12 +7,12 @@ class Gptscript < Formula
   head "https://github.com/gptscript-ai/gptscript.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9f147b2ce9e1035a25dcbe301ecbe2c5891c5ba541eef2f652b1d6b6cfb688ab"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9f147b2ce9e1035a25dcbe301ecbe2c5891c5ba541eef2f652b1d6b6cfb688ab"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9f147b2ce9e1035a25dcbe301ecbe2c5891c5ba541eef2f652b1d6b6cfb688ab"
-    sha256 cellar: :any_skip_relocation, sonoma:        "0b81d404e60836e358afb6e2fb32f09544f9f00862450c32a5b145bf1c4a0376"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "729500ad1b7263a85d289b11fc08a7cdcac9a973d77204c305344adc30e719b1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "687f4b607f1800fd154839d8e37a399f4c4b88fda9fe7375a10f9a533f596309"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9c07b26c14d5743725320bc0278e538ec58f6be02f43cddcf8ade03bff290fcf"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9c07b26c14d5743725320bc0278e538ec58f6be02f43cddcf8ade03bff290fcf"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9c07b26c14d5743725320bc0278e538ec58f6be02f43cddcf8ade03bff290fcf"
+    sha256 cellar: :any_skip_relocation, sonoma:        "579c7c3e34bef2d478e764e2aaf5d63b4c6e5bbbbc15798a0582a5f0dace897a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1cb2cff551ce0f291592f83cb3dbf28d52c6e041a75ab585f8ed648e27e2d65b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "549dac1f76d4938b6bde125cfd16157eca990725c5a00441215cdf36b9d70394"
   end
 
   depends_on "go" => :build
