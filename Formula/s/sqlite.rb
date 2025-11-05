dc7c92759f35c617b571594f1737213141156928
MoonFruit <dkmoonfruit@gmail.com>
2025-11-05 14:42:38 +0800

    sqlite 3.51.0

--- Formula/s/sqlite.rb
+++ Formula/s/sqlite.rb
@@ -1,9 +1,9 @@
 class Sqlite < Formula
   desc "Command-line interface for SQLite"
   homepage "https://sqlite.org/index.html"
-  url "https://www.sqlite.org/2025/sqlite-autoconf-3500400.tar.gz"
-  version "3.50.4"
-  sha256 "a3db587a1b92ee5ddac2f66b3edb41b26f9c867275782d46c3a088977d6a5b18"
+  url "https://sqlite.org/2025/sqlite-autoconf-3510000.tar.gz"
+  version "3.51.0"
+  sha256 "42e26dfdd96aa2e6b1b1be5c88b0887f9959093f650d693cb02eb9c36d146ca5"
   license "blessing"
 
   livecheck do
