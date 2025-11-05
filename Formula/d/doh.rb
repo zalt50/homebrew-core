a5118fadcffdc10b1643e14bfe31554618dc8efe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-05 18:00:40 +0000

    doh: add 0.1 bottle.

--- Formula/d/doh.rb
+++ Formula/d/doh.rb
@@ -6,6 +6,15 @@ class Doh < Formula
   license "MIT"
   head "https://github.com/curl/doh.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b9c8bb12c19ed1f9ee5a81150ab5238dc1521692b97ee7e2ac9f7b332c98de28"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5c39c61062f58d2554ce37add6e00e95dc8628fcd7a597699afb3f53d095e279"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "88e7c35829b8a8f2da88089b4ea6eecc482de447ac2db4664ad8c3bca1a66255"
+    sha256 cellar: :any_skip_relocation, sonoma:        "17325afc86aa5389af5b957dcbd13155bff5ec0372e7c89fb5608f84deac154b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "75b94ee73ea07d08efdc21cbc6ddd11298e438c7c41efcb7d35bdce181a8e7e8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0272e7f336538f7360c59c54889b19bb5d9f0cd723f44bc87e641bcd1f124de2"
+  end
+
   uses_from_macos "curl"
 
   def install
