f4920273fe03b8a389f74d50877df51e23cd3e6d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-06 16:39:44 +0000

    tuios: add 0.3.3 bottle.

--- Formula/t/tuios.rb
+++ Formula/t/tuios.rb
@@ -6,6 +6,15 @@ class Tuios < Formula
   license "MIT"
   head "https://github.com/Gaurav-Gosain/tuios.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "47193836706a57a06e3f2c7a9c51ad9d5e410938bc6cd6c635431536c9db6979"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2942ee844d88826192da49b9883d90803bef831cb97fff4ebcf2d364b546083c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "70011e72d2965f95e7cdc7ebd84fb5b3448ea27e84abc9d0fab9f3463680fbbd"
+    sha256 cellar: :any_skip_relocation, sonoma:        "4db9dfcd8d1e54e0f69f231e5296d5672c577728f4f9ed96dff031943b34a815"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "40629751a3e69c732d82d3eed1a61a19475285c019b585dfeb5405caf15ae21c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "205e4e5672bd100afe23b27bfbab1e78a8b66a24a1f52069792f2e9c2a2b3f8b"
+  end
+
   depends_on "go" => :build
 
   def install
