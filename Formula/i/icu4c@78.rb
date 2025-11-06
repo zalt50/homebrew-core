5315e7eaf196e47fd2ae8818a620dd921beb8b6c
Anton Melnikov <antonsm21@gmail.com>
2025-11-06 15:53:05 +0000

    icu4c@78: add 78.1 bottle.

--- Formula/i/icu4c@78.rb
+++ Formula/i/icu4c@78.rb
@@ -16,6 +16,15 @@ class Icu4cAT78 < Formula
     end
   end
 
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "21cb6bc12dba28870ae05577dd4b8ec3019e6c2d7d8813518a068deccbff2bd5"
+    sha256 cellar: :any,                 arm64_sequoia: "020c5b916bec54ecc4b767886c5077401245cde08ffee81e1d08b393fc907376"
+    sha256 cellar: :any,                 arm64_sonoma:  "108f8e427aa09d1f639c1675e5477197f65081e5a9dfa9c3ce3327fa896711ea"
+    sha256 cellar: :any,                 sonoma:        "36ae0d43501c1a63baf42bf9574684a3d64b58bcc034f641bcb1b68efac4ff74"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "75e1823a9b274e7d97377ee681a6a54509125b3350e060fca17b4bd344f7a10e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "74b3f8216aba75f00256dc91e060e2569f65908acbb705a2dd8f61af4022da1b"
+  end
+
   keg_only :shadowed_by_macos, "macOS provides libicucore.dylib (but nothing else)"
 
   def install
