00f48dffd1d5ba33d103fb515355470b6a57426e
botantony <antonsm21@gmail.com>
2025-11-07 13:44:26 +0100

    zebra: revision bump (icu4c@78 78.1)
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Formula/z/zebra.rb
+++ Formula/z/zebra.rb
@@ -4,7 +4,7 @@ class Zebra < Formula
   url "https://ftp.indexdata.com/pub/zebra/idzebra-2.2.8.tar.gz"
   sha256 "879e402b91912e9074275753f24408ed2ac06b3b7c30b2a2d5a70718c869a542"
   license "GPL-2.0-or-later"
-  revision 1
+  revision 2
 
   livecheck do
     url "https://ftp.indexdata.com/pub/zebra/"
@@ -20,7 +20,7 @@ class Zebra < Formula
     sha256 x86_64_linux:  "3bf2b3a934faa8509dfff154342d362dc309519dd09fd5035a393d3701a5a581"
   end
 
-  depends_on "icu4c@77"
+  depends_on "icu4c@78"
   depends_on "yaz"
 
   uses_from_macos "bzip2"
