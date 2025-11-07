6098798c8cd42a421ec599fc0268ff9832532e25
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-07 19:49:49 +0000

    bats-core 1.13.0

--- Formula/b/bats-core.rb
+++ Formula/b/bats-core.rb
@@ -1,8 +1,8 @@
 class BatsCore < Formula
   desc "Bash Automated Testing System"
   homepage "https://github.com/bats-core/bats-core"
-  url "https://github.com/bats-core/bats-core/archive/refs/tags/v1.12.0.tar.gz"
-  sha256 "e36b020436228262731e3319ed013d84fcd7c4bd97a1b34dee33d170e9ae6bab"
+  url "https://github.com/bats-core/bats-core/archive/refs/tags/v1.13.0.tar.gz"
+  sha256 "a85e12b8828271a152b338ca8109aa23493b57950987c8e6dff97ba492772ff3"
   license "MIT"
 
   livecheck do
