a212f17836501e53930937d87169f50b73ee6e96
botantony <antonsm21@gmail.com>
2025-11-07 15:31:52 +0100

    postgresql@17: revision bump (icu4c@78 78.1)
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Formula/p/postgresql@17.rb
+++ Formula/p/postgresql@17.rb
@@ -4,7 +4,7 @@ class PostgresqlAT17 < Formula
   url "https://ftp.postgresql.org/pub/source/v17.6/postgresql-17.6.tar.bz2"
   sha256 "e0630a3600aea27511715563259ec2111cd5f4353a4b040e0be827f94cd7a8b0"
   license "PostgreSQL"
-  revision 1
+  revision 2
 
   livecheck do
     url "https://ftp.postgresql.org/pub/source/"
@@ -29,7 +29,7 @@ class PostgresqlAT17 < Formula
   depends_on "docbook-xsl" => :build
   depends_on "gettext" => :build
   depends_on "pkgconf" => :build
-  depends_on "icu4c@77"
+  depends_on "icu4c@78"
   # GSSAPI provided by Kerberos.framework crashes when forked.
   # See https://github.com/Homebrew/homebrew-core/issues/47494.
   depends_on "krb5"
