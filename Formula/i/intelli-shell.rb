2bd4358616fcadce35499e758c7e54b0aa468622
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-07 12:58:38 +0000

    intelli-shell: add 3.3.0 bottle.

--- Formula/i/intelli-shell.rb
+++ Formula/i/intelli-shell.rb
@@ -6,6 +6,15 @@ class IntelliShell < Formula
   license "Apache-2.0"
   head "https://github.com/lasantosr/intelli-shell.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "d76ccec3bc031aaa14c7c67ed6eff2249fafb9182c65ec4c41bfda8ab9a8b712"
+    sha256 cellar: :any,                 arm64_sequoia: "84b8db57401d9f3da44a060ed9782d609a9c515334f271c51d5b48296797da34"
+    sha256 cellar: :any,                 arm64_sonoma:  "9f47e29fd3185f63a8ca8917939e313c33e2de4fa2ffcafffda140f10e9630b0"
+    sha256 cellar: :any,                 sonoma:        "e51e5e450847504203c054dc8b0a5aa815ce4eb7cbe8ace1f6432c445ebc753c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "26662996b05f84b80342e5271618fb2993dc542e8e7e504a07b7c8e1e33f18e8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7506a29fa4a1d1dd381bfe6bc23f817ae57be8d8a6f30f48dbb89fd3d862578e"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "openssl@3"
