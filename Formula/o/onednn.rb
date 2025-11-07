a93c44e318bbacd88be98c5197675e962948a8e9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-07 20:09:04 +0000

    onednn 3.10

--- Formula/o/onednn.rb
+++ Formula/o/onednn.rb
@@ -1,8 +1,8 @@
 class Onednn < Formula
   desc "Basic building blocks for deep learning applications"
   homepage "https://www.oneapi.io/open-source/"
-  url "https://github.com/uxlfoundation/oneDNN/archive/refs/tags/v3.9.2.tar.gz"
-  sha256 "2a4495e7070a8fcb4366b5a82ff1ca098d8526a6f127ae864aa1ca4bebeae125"
+  url "https://github.com/uxlfoundation/oneDNN/archive/refs/tags/v3.10.tar.gz"
+  sha256 "ba5834a1fdbb6d1c1b1c065dfd789438e7aa42c03fc52d92c02af85d78d1c75c"
   license "Apache-2.0"
   head "https://github.com/uxlfoundation/oneDNN.git", branch: "main"
 
