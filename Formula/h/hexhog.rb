9c1c0c2762e6f73757149b4eb23df7c6b06349c5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-07 12:58:30 +0000

    hexhog: add 0.1.2 bottle.

--- Formula/h/hexhog.rb
+++ Formula/h/hexhog.rb
@@ -6,6 +6,15 @@ class Hexhog < Formula
   license "MIT"
   head "https://github.com/DVDTSB/hexhog.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1b2e7f595bf4326c75c12c57e2a68871a8d5fea82012add3481c19924d7a1640"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d8b84d49eae0805cbf492bd7132e3f0d264fc5d0bac3c8de48924152e7b7f859"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "139feabeaed76e99b95211d1873eec9da003514c249080f622bedbe255aae729"
+    sha256 cellar: :any_skip_relocation, sonoma:        "491a18427c27d596123b3aba3eb23727baa761170a370c554b63e735e0b676e4"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d4eee337de8005b314544f5269a1933ec14b795a7384ed164a6549d3f9e054a4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4ca7fa34b0392291d065c22ee2e034a4d05a6fbdb626386d9dab837736cddb46"
+  end
+
   depends_on "rust" => :build
 
   def install
