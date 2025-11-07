02a76608cd29894cb1f9a198f5b006f5da8b5683
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-07 13:52:02 +0000

    basedpyright 1.33.0

--- Formula/b/basedpyright.rb
+++ Formula/b/basedpyright.rb
@@ -1,8 +1,8 @@
 class Basedpyright < Formula
   desc "Pyright fork with various improvements and built-in pylance features"
   homepage "https://github.com/DetachHead/basedpyright"
-  url "https://registry.npmjs.org/basedpyright/-/basedpyright-1.32.1.tgz"
-  sha256 "4432a781e7a84a54404980934c1d7f7d2a9655e8d9adcbebf34d8680ef2cd392"
+  url "https://registry.npmjs.org/basedpyright/-/basedpyright-1.33.0.tgz"
+  sha256 "5a2a2007d53677cd0604f864c0c994a1ea548aed03a7d7496ca877b6645a852c"
   license "MIT"
 
   bottle do
@@ -15,6 +15,9 @@ class Basedpyright < Formula
     system "npm", "install", *std_npm_args
     bin.install_symlink libexec/"bin/pyright" => "basedpyright"
     bin.install_symlink libexec/"bin/pyright-langserver" => "basedpyright-langserver"
+
+    # Remove empty folder to make :all bottle
+    rm_r libexec/"lib/node_modules/basedpyright/node_modules" if OS.mac?
   end
 
   test do
