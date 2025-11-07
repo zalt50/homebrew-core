5c623c6a7b94ea94f978028efa6e3dcbe3c509fd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-07 23:57:34 +0000

    magika: add 1.0.1 bottle.

--- Formula/m/magika.rb
+++ Formula/m/magika.rb
@@ -12,6 +12,15 @@ class Magika < Formula
     regex(%r{^cli/v?(\d+(?:\.\d+)+)$}i)
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d93f626ec1454d84cbc0b52e6e5c1fc1eb8552a2cdc29677a99a96e91b1e4444"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "47f3471c44e457082fb3a995f41069cfba54a49dab6394c28342b807edb7c954"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "388505c16689f93648258a297ee037950e3caa83f1fa4f07f50f2e688ab00866"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a6fe3680a0f5b6f8256e96ae53eba014b798db39a2d9ea7656dc55a94084e3bd"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "05f13de23f78e84aa077b96a4aef51865666ac158578472bada2bb1fbc8c5a5c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9010cda89964baceb390e0121198c0a9c55729455affeaa83956b3f290b0650c"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "openssl@3"
