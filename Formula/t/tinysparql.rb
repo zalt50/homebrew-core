5268281a9771df30cf3c0db51d33a9faf1e1e6db
botantony <antonsm21@gmail.com>
2025-11-07 14:02:35 +0100

    tinysparql: revision bump (icu4c@78 78.1)
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Formula/t/tinysparql.rb
+++ Formula/t/tinysparql.rb
@@ -4,7 +4,7 @@ class Tinysparql < Formula
   url "https://download.gnome.org/sources/tinysparql/3.10/tinysparql-3.10.1.tar.xz"
   sha256 "5a7f3e789db6671a550ed6280ed4f60a60bea77368da92be68dc7d8d7e230265"
   license all_of: ["LGPL-2.1-or-later", "GPL-2.0-or-later"]
-  revision 1
+  revision 2
   head "https://gitlab.gnome.org/GNOME/tinysparql.git", branch: "main"
 
   # TinySPARQL doesn't follow GNOME's "even-numbered minor is stable" version
@@ -33,7 +33,7 @@ class Tinysparql < Formula
 
   depends_on "dbus"
   depends_on "glib"
-  depends_on "icu4c@77"
+  depends_on "icu4c@78"
   depends_on "json-glib"
   depends_on "libsoup"
   depends_on "sqlite"
