a436ad805baa307ff95a5ead684cfb89dfc8ac5d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-07 23:17:37 +0000

    partio 1.19.2

--- Formula/p/partio.rb
+++ Formula/p/partio.rb
@@ -1,8 +1,8 @@
 class Partio < Formula
   desc "Particle library for 3D graphics"
   homepage "https://github.com/wdas/partio"
-  url "https://github.com/wdas/partio/archive/refs/tags/v1.19.0.tar.gz"
-  sha256 "128913266a88a0939aaa4bc951ae4d4da1c380408bcc0ea9e7f526556afeaad0"
+  url "https://github.com/wdas/partio/archive/refs/tags/v1.19.2.tar.gz"
+  sha256 "b59fbc8e64561368f0e2f94ae2f3f2083babfc32ff7ae97e833ab15df4588ade"
   license "BSD-3-Clause"
 
   bottle do
