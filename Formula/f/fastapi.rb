d6834fd4b61382a9e4c10021a445ffed40613f4a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-08 23:35:04 +0000

    fastapi: update 0.121.1 bottle.

--- Formula/f/fastapi.rb
+++ Formula/f/fastapi.rb
@@ -8,13 +8,12 @@ class Fastapi < Formula
   license "MIT"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_tahoe:   "09d4dd7c25da058e1347ea8056bef7d7820e2109645d66cb1622e982db4eeb44"
-    sha256 cellar: :any,                 arm64_sequoia: "98bfc4cced78cc2d0e823852c453af543c485aade19c26f9cd2087b0cd5ed05b"
-    sha256 cellar: :any,                 arm64_sonoma:  "37550587312b3202394e3ef5f857ca3320cc7d66b96fc7638b50dbbe3a973162"
-    sha256 cellar: :any,                 sonoma:        "45710812b516126f008ac792c35b9bc40b80d14f4c233d9671fdb7fa0208303d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "0296feac3be236330d23602cf8c9543755fee99c841ae534f8e415844e9cc11a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "af224e055258d61d9d458ed71af10da7323c27c88f9881b06cd1366062b369f5"
+    sha256 cellar: :any,                 arm64_tahoe:   "e247bb9fa67e5fdf3a6dc1e6d21bc9cd5a89777fc99da2832d76650131698645"
+    sha256 cellar: :any,                 arm64_sequoia: "e6f23f852d8ebe555ca6fc6ac3cf9ac91aeffffb984707e5f8559f596e1a7e7d"
+    sha256 cellar: :any,                 arm64_sonoma:  "0cafe9eea25db9a45bfacdadd9aa1212e296576f527d46fd1e926a83e78fc2d8"
+    sha256 cellar: :any,                 sonoma:        "64df09bd05e4eb2d0ac3e39a3f4764149ff24ae2e21638a2a4367abb42208171"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "528d455c926c71a92863ad0d022d4a8dc4b237bb48546165c7e7868c19efe377"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "13be9d2deb71ff4f76e5ca319abdab1e88ceaf54290be9552a22400249cb9cc4"
   end
 
   depends_on "rust" => :build # for annotated-doc
