c197994aa93d5d3cdde1b8173e3cffb45cef6a20
Nicolas Karolak <nicolas@karolak.fr>
2025-11-09 10:12:59 +0100

    rumdl 0.0.173 (new formula)

---
+++ Formula/r/rumdl.rb
@@ -0,0 +1,32 @@
+class Rumdl < Formula
+  desc "Markdown Linter and Formatter written in Rust"
+  homepage "https://github.com/rvben/rumdl"
+  url "https://github.com/rvben/rumdl/archive/refs/tags/v0.0.173.tar.gz"
+  sha256 "b52d7d6c4baa0ff6a29c64bd7a66607d51fd71eb3443612e7db44404c3299732"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/rumdl version")
+
+    (testpath/"test-bad.md").write <<~MARKDOWN
+      # Header 1
+      body
+    MARKDOWN
+    (testpath/"test-good.md").write <<~MARKDOWN
+      # Header 1
+
+      body
+    MARKDOWN
+
+    assert_match "Success", shell_output("#{bin}/rumdl check test-good.md")
+    assert_match "MD022", shell_output("#{bin}/rumdl check test-bad.md 2>&1", 1)
+    assert_match "Fixed", shell_output("#{bin}/rumdl fmt test-bad.md")
+    assert_equal (testpath/"test-good.md").read, (testpath/"test-bad.md").read
+  end
+end
