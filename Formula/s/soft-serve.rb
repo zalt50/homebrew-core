885d306e032e98480b17db03c70e27f1d3865a91
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-10 15:26:24 +0000

    soft-serve 0.11.1

--- Formula/s/soft-serve.rb
+++ Formula/s/soft-serve.rb
@@ -1,8 +1,8 @@
 class SoftServe < Formula
   desc "Mighty, self-hostable Git server for the command-line"
   homepage "https://github.com/charmbracelet/soft-serve"
-  url "https://github.com/charmbracelet/soft-serve/archive/refs/tags/v0.11.0.tar.gz"
-  sha256 "473359acba9a4da8b3d01485bf1d17a9ae885c3e77450ca551e2987b0b1fa9cc"
+  url "https://github.com/charmbracelet/soft-serve/archive/refs/tags/v0.11.1.tar.gz"
+  sha256 "c0d9b4e7f1b2cbe3d2abc161b61846ce03743f365d961619fb43b4fa35da11b7"
   license "MIT"
   head "https://github.com/charmbracelet/soft-serve.git", branch: "main"
 
