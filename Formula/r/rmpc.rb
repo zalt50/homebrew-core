679a3fcc744899540932fd426e6df7cf2d056159
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-11 20:14:34 +0000

    rmpc 0.10.0

--- Formula/r/rmpc.rb
+++ Formula/r/rmpc.rb
@@ -1,8 +1,8 @@
 class Rmpc < Formula
   desc "Terminal based Media Player Client with album art support"
   homepage "https://mierak.github.io/rmpc/"
-  url "https://github.com/mierak/rmpc/archive/refs/tags/v0.9.0.tar.gz"
-  sha256 "c0dca5f4be1222591a69b82e77d6fe42df259e6130ea7ea7dd6372515b5f4357"
+  url "https://github.com/mierak/rmpc/archive/refs/tags/v0.10.0.tar.gz"
+  sha256 "86e6b465707cdca82f53aed2a3bef600430edbf4e626124d194ac346ee989cbd"
   license "BSD-3-Clause"
 
   bottle do
