67d0944793808768a5d0c7e045d14669461b093f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-11 14:16:51 +0000

    packetbeat 9.2.1

--- Formula/p/packetbeat.rb
+++ Formula/p/packetbeat.rb
@@ -2,8 +2,8 @@ class Packetbeat < Formula
   desc "Lightweight Shipper for Network Data"
   homepage "https://www.elastic.co/products/beats/packetbeat"
   url "https://github.com/elastic/beats.git",
-      tag:      "v9.2.0",
-      revision: "09b547febe1cc9102a5d3f80ac8fbf68a5fd84f5"
+      tag:      "v9.2.1",
+      revision: "6485e11edf8854d7792dfc2999bf19db37315ea4"
   license "Apache-2.0"
   head "https://github.com/elastic/beats.git", branch: "main"
 
