67d5ed8c1a232503f5819d8dac89fa32e2320a54
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-11 23:20:24 +0000

    rustic 0.10.2

--- Formula/r/rustic.rb
+++ Formula/r/rustic.rb
@@ -1,8 +1,8 @@
 class Rustic < Formula
   desc "Fast, encrypted, and deduplicated backups powered by Rust"
   homepage "https://rustic.cli.rs"
-  url "https://github.com/rustic-rs/rustic/archive/refs/tags/v0.10.1.tar.gz"
-  sha256 "efd310855d44758ed0e3cc4bb51ffc8bff59ff160c942f46988f03ef978764cf"
+  url "https://github.com/rustic-rs/rustic/archive/refs/tags/v0.10.2.tar.gz"
+  sha256 "e8305efe543e68f75daec8ac5d7b7831a6ac5860f3dc37a8cfdf40ecf7d1e45f"
   license "Apache-2.0"
   head "https://github.com/rustic-rs/rustic.git", branch: "main"
 
