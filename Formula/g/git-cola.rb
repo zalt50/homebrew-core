7546ffeb9418549e6dac762a74fd291a70e39519
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-12 11:47:08 +0000

    git-cola: update 4.16.1 bottle.

--- Formula/g/git-cola.rb
+++ Formula/g/git-cola.rb
@@ -9,7 +9,7 @@ class GitCola < Formula
   head "https://github.com/git-cola/git-cola.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "2813fc10155552412bcdcf486694c0a67802df3666bdc6c43dcfc108731d3843"
+    sha256 cellar: :any_skip_relocation, all: "5698d5131f01f052703da181ca11986b25dbf19846dfb6a71b42b06f79ed5d4e"
   end
 
   depends_on "git-gui"
