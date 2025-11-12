0d5681a0eb9c6443827566dc40feb3b1f342c100
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-12 17:25:52 +0000

    yo 6.0.0

--- Formula/y/yo.rb
+++ Formula/y/yo.rb
@@ -1,8 +1,8 @@
 class Yo < Formula
   desc "CLI tool for running Yeoman generators"
   homepage "https://yeoman.io"
-  url "https://registry.npmjs.org/yo/-/yo-5.1.0.tgz"
-  sha256 "b3b2abe646c47d8e60b93394ed219779dce26347c59d8ba337a62ea4226e1ec5"
+  url "https://registry.npmjs.org/yo/-/yo-6.0.0.tgz"
+  sha256 "db7b3bf350eb65def5b4dff427d5c164884937a713a72a018cec9271a3a53ebc"
   license "BSD-2-Clause"
   head "https://github.com/yeoman/yo.git", branch: "main"
 
