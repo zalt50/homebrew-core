2a60a8c2e3b56a82c2829e1844bc1b24cb06e9c9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-13 19:01:34 +0000

    gh: update 2.83.1 bottle.

--- Formula/g/gh.rb
+++ Formula/g/gh.rb
@@ -14,12 +14,12 @@ class Gh < Formula
   no_autobump! because: :bumped_by_upstream
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b90051fe887c8f7aa12f065feacda0c33e23d30483cfcfec1852eae2c38b221d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b90051fe887c8f7aa12f065feacda0c33e23d30483cfcfec1852eae2c38b221d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b90051fe887c8f7aa12f065feacda0c33e23d30483cfcfec1852eae2c38b221d"
-    sha256 cellar: :any_skip_relocation, sonoma:        "0ecd7f4a8092db29a7e4cf7f8035abf033778f17d6a69b233d02c6bfbc770386"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a09687ade353bfc0c0ed771ff23aca3fb1942f970a7c1a3e0d541bfdd398d7dc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6ae4e60959ef64deb60c692d14adfe7436dbd3225a340e775e34663cc637cbc3"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d37c496644cf8ee251e451b46ab6a517d17c316e723bd0fd73e065587c8a2a4e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d37c496644cf8ee251e451b46ab6a517d17c316e723bd0fd73e065587c8a2a4e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d37c496644cf8ee251e451b46ab6a517d17c316e723bd0fd73e065587c8a2a4e"
+    sha256 cellar: :any_skip_relocation, sonoma:        "3111d1c388d4f8574c6fc78dedd015ed091e3503119f891ba3777415e938701f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "8fc552a4859ae88829ae79a8f3c97b66040e6a82e410da08166968b997ab9d5d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "abe8930389df52d31914d41d478d9d193044544ba5bad7c0f02e63c17a3b3005"
   end
 
   depends_on "go" => :build
