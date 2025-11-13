05c4b22a7d7c46b1c889978265b47946a0b8e4bf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-13 10:53:49 +0000

    cariddi 1.4.4

--- Formula/c/cariddi.rb
+++ Formula/c/cariddi.rb
@@ -1,8 +1,8 @@
 class Cariddi < Formula
   desc "Scan for endpoints, secrets, API keys, file extensions, tokens and more"
   homepage "https://github.com/edoardottt/cariddi"
-  url "https://github.com/edoardottt/cariddi/archive/refs/tags/v1.4.3.tar.gz"
-  sha256 "cc8d9202af6e13fc44a79e6d4380592df3c28bdf7c078a92ff0f62732b9c6882"
+  url "https://github.com/edoardottt/cariddi/archive/refs/tags/v1.4.4.tar.gz"
+  sha256 "203d7f4d09ae9ba6e15ec0d6adde9c499ec280d521746de4a4b10775af9e1469"
   license "GPL-3.0-or-later"
   head "https://github.com/edoardottt/cariddi.git", branch: "main"
 
