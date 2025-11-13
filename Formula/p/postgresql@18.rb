e6861b454e547f1e1d16d570af5861d69ac2e921
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-13 17:17:01 +0000

    postgresql@18 18.1

--- Formula/p/postgresql@18.rb
+++ Formula/p/postgresql@18.rb
@@ -1,10 +1,9 @@
 class PostgresqlAT18 < Formula
   desc "Object-relational database system"
   homepage "https://www.postgresql.org/"
-  url "https://ftp.postgresql.org/pub/source/v18.0/postgresql-18.0.tar.bz2"
-  sha256 "0d5b903b1e5fe361bca7aa9507519933773eb34266b1357c4e7780fdee6d6078"
+  url "https://ftp.postgresql.org/pub/source/v18.1/postgresql-18.1.tar.bz2"
+  sha256 "ff86675c336c46e98ac991ebb306d1b67621ece1d06787beaade312c2c915d54"
   license "PostgreSQL"
-  revision 2
 
   livecheck do
     url "https://ftp.postgresql.org/pub/source/"
