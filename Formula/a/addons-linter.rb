2a0609e9e881154bc1358e3d9aea5aad48049fb1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-13 14:14:54 +0000

    addons-linter: update 9.0.0 bottle.

--- Formula/a/addons-linter.rb
+++ Formula/a/addons-linter.rb
@@ -6,7 +6,7 @@ class AddonsLinter < Formula
   license "MPL-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "8c98b516f61e469d0e0bb606ee3feba6c810facff5bc01806bd8a9d6ac22b5fa"
+    sha256 cellar: :any_skip_relocation, all: "0d1eed402837c6b9b859da77bda4891f8a66119fcac69edf824c578604d318ae"
   end
 
   depends_on "node"
