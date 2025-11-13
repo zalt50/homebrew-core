99bad632989b884ddb77566e5d0bad9f884b316f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-13 23:55:15 +0000

    forgejo-cli: add 0.3.0 bottle.

--- Formula/f/forgejo-cli.rb
+++ Formula/f/forgejo-cli.rb
@@ -6,6 +6,15 @@ class ForgejoCli < Formula
   license any_of: ["Apache-2.0", "MIT"]
   head "https://codeberg.org/forgejo-contrib/forgejo-cli.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "34a8d50ac5226006b6e236440d52f6822cdbd7ddc57b073f676655ecef4b4efb"
+    sha256 cellar: :any,                 arm64_sequoia: "26ecba9bb5c54bef4b13ed743f6a43c2f9e4f1ecfe337d7cc55d9addde8dfe28"
+    sha256 cellar: :any,                 arm64_sonoma:  "c494757ae9acb634899888f46286a9beb820f065e959d43ce379bb6fc85bab9d"
+    sha256 cellar: :any,                 sonoma:        "1e1470d0dd201bf6d03a5e89994718575340059ec4c840bba0c99483d6f0f540"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "14ad45180433146f94e617cc5cc532a2282bfe88cfb2b9644b21f0b802be7570"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2e4d59c45352f47fde1ff63657e159c980b987e6eba3a88c9dddaf6c5c489f07"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "openssl@3"
