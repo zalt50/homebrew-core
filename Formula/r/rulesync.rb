c79ed9766a82f9711cdc2f927da28be2e45154d9
Patrick Linnane <patrick@linnane.io>
2025-11-14 05:49:20 +0000

    rulesync 3.21.0

--- Formula/r/rulesync.rb
+++ Formula/r/rulesync.rb
@@ -1,8 +1,8 @@
 class Rulesync < Formula
   desc "Unified AI rules management CLI tool"
   homepage "https://github.com/dyoshikawa/rulesync"
-  url "https://registry.npmjs.org/rulesync/-/rulesync-3.20.0.tgz"
-  sha256 "c276887445a24dc71f34a0fe1c2dcf310649da0c9a137ebbb5de338c333245ac"
+  url "https://registry.npmjs.org/rulesync/-/rulesync-3.21.0.tgz"
+  sha256 "df238bfc26d8f5d16648428c351c2728a55ba4ada9f72e63379aa9f69e5738d6"
   license "MIT"
 
   bottle do
