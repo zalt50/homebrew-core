00b6fe68f1b0926af9350fb527f3c3aa91f87c42
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-14 08:28:35 +0000

    fern-api: update 1.0.3 bottle.

--- Formula/f/fern-api.rb
+++ Formula/f/fern-api.rb
@@ -6,7 +6,7 @@ class FernApi < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "208a02786d1233cab04b33de06f7f376ed03bc8f84cc124c94127ebffcf11383"
+    sha256 cellar: :any_skip_relocation, all: "fcd548263c14dc4bed82017170ef39b2d1e543f8ce4f64262f491bcf9c32eafc"
   end
 
   depends_on "node"
