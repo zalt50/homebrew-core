3ba03ed9073b9dfbecf6b33236d782643ca3456e
Patrick Linnane <patrick@linnane.io>
2025-11-14 05:55:20 +0000

    utf8proc 2.11.1

--- Formula/u/utf8proc.rb
+++ Formula/u/utf8proc.rb
@@ -1,8 +1,8 @@
 class Utf8proc < Formula
   desc "Clean C library for processing UTF-8 Unicode data"
   homepage "https://juliastrings.github.io/utf8proc/"
-  url "https://github.com/JuliaStrings/utf8proc/archive/refs/tags/v2.11.0.tar.gz"
-  sha256 "c24379b5fa0a429a1f9a3fc23b44a75f2b141a34c09146a529a55d20a5808070"
+  url "https://github.com/JuliaStrings/utf8proc/archive/refs/tags/v2.11.1.tar.gz"
+  sha256 "dc146fd279eacbbf399d3f70932ce66f516aac2d13f8ec2d26a30f8ed70aa5b4"
   license all_of: ["MIT", "Unicode-DFS-2015"]
   head "https://github.com/JuliaStrings/utf8proc.git", branch: "master"
 
