d50b81b0ec7500e34da9e998c86da81f40d22fde
Patrick Linnane <patrick@linnane.io>
2025-11-14 05:16:09 +0000

    blocky 0.28.1

--- Formula/b/blocky.rb
+++ Formula/b/blocky.rb
@@ -1,8 +1,8 @@
 class Blocky < Formula
   desc "Fast and lightweight DNS proxy as ad-blocker for local network"
   homepage "https://0xerr0r.github.io/blocky/"
-  url "https://github.com/0xerr0r/blocky/archive/refs/tags/v0.28.0.tar.gz"
-  sha256 "959b8182a4a4cdb0d58e86cf0417d092105b3023914d723cd72e09740839007a"
+  url "https://github.com/0xerr0r/blocky/archive/refs/tags/v0.28.1.tar.gz"
+  sha256 "28afb06551a0d76790db86a90783abde287531d2dc095164c0bd8647e78bcb36"
   license "Apache-2.0"
   head "https://github.com/0xerr0r/blocky.git", branch: "main"
 
