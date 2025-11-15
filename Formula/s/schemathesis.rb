23207613e54647adab64b57c5a1d962bb38651c3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-15 17:15:56 +0000

    schemathesis 4.5.2

--- Formula/s/schemathesis.rb
+++ Formula/s/schemathesis.rb
@@ -3,8 +3,8 @@ class Schemathesis < Formula
 
   desc "Testing tool for web applications with specs"
   homepage "https://schemathesis.readthedocs.io/"
-  url "https://files.pythonhosted.org/packages/88/12/9dbd8c72a9f121a65c44c4d0a67c38eebbbd71ae6662563a63e9d0c33f83/schemathesis-4.5.1.tar.gz"
-  sha256 "c7ab2ee28d4f3b317f0f85e837f68e553dfa9e1cf5e5ad016c7ae98059ed4787"
+  url "https://files.pythonhosted.org/packages/41/b6/829832f7ddbded17dd6586cb79a497e2865c3abcbd7fcc17805111608439/schemathesis-4.5.2.tar.gz"
+  sha256 "283a95090f5f7bdc725cd74a66065553a600c42f2163f718ed49a631a614bbad"
   license "MIT"
 
   bottle do
@@ -86,8 +86,8 @@ class Schemathesis < Formula
   end
 
   resource "hypothesis" do
-    url "https://files.pythonhosted.org/packages/f3/53/e19fe74671fd60db86344a4623c818fac58b813cc3efbb7ea3b3074dcb71/hypothesis-6.147.0.tar.gz"
-    sha256 "72e6004ea3bd1460bdb4640b6389df23b87ba7a4851893fd84d1375635d3e507"
+    url "https://files.pythonhosted.org/packages/6e/a5/8565e19f407fb7a2f57cccecf6894e05c047414f2b50a51a37af49a9f5ae/hypothesis-6.148.0.tar.gz"
+    sha256 "d61cca7f7cb56f2941b14d288fd134ba846af0ba8ed12374b581ab865d4e14a0"
   end
 
   resource "hypothesis-graphql" do
