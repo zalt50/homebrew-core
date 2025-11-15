4f540703b01a3fddbe8bcb24768da489fe46650c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-15 16:53:39 +0000

    cspell 9.3.2

--- Formula/c/cspell.rb
+++ Formula/c/cspell.rb
@@ -1,8 +1,8 @@
 class Cspell < Formula
   desc "Spell checker for code"
   homepage "https://cspell.org"
-  url "https://registry.npmjs.org/cspell/-/cspell-9.3.1.tgz"
-  sha256 "9be8be66f0e39facc852e3a7fbd423b3e494842c830d43de5b4271f27b9ca770"
+  url "https://registry.npmjs.org/cspell/-/cspell-9.3.2.tgz"
+  sha256 "c0d9d9e91a7cd4e8636b2e4a58b7f55f48743a7d530a7e257b7926facde6dddd"
   license "MIT"
 
   bottle do
