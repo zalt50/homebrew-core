542f3fa5a3f2af7e4ed992aeb45421d483f26451
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-15 05:10:54 +0000

    libdecor: add 0.2.4 bottle.

--- Formula/lib/libdecor.rb
+++ Formula/lib/libdecor.rb
@@ -5,6 +5,11 @@ class Libdecor < Formula
   sha256 "b17cf420e8dcb526bf82da5d36f8443a91fad0777083fa4ec5c1df8ee877416f"
   license "MIT"
 
+  bottle do
+    sha256 arm64_linux:  "0ce1db47f5f6e33e2eb9e56441bfd76e0743d3d73fe36ec03fc70706546e1c0b"
+    sha256 x86_64_linux: "01ed6ba19998c4ec2aa7093b943d75a8c491616c0a3638779c7297e4da276ea1"
+  end
+
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkgconf" => :build
