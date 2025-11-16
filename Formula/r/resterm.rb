2c42560a2c9906abd047b138c6ec969382f589e2
Rui Chen <rui@chenrui.dev>
2025-11-16 16:42:26 -0500

    resterm 0.7.3 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/r/resterm.rb
@@ -0,0 +1,60 @@
+class Resterm < Formula
+  desc "Terminal client for .http/.rest files with HTTP, GraphQL, and gRPC support"
+  homepage "https://github.com/unkn0wn-root/resterm"
+  url "https://github.com/unkn0wn-root/resterm/archive/refs/tags/v0.7.3.tar.gz"
+  sha256 "1b0241ea823d69d99d51c06c8c5229db0dc635775a6a6df3dee779ec3da476fd"
+  license "Apache-2.0"
+  head "https://github.com/unkn0wn-root/resterm.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X main.version=#{version} -X main.commit=#{tap.user} -X main.date=#{time.iso8601}"
+    system "go", "build", *std_go_args(ldflags:), "./cmd/resterm"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/resterm -version")
+
+    (testpath/"openapi.yml").write <<~YAML
+      openapi: 3.0.0
+      info:
+        title: Test API
+        version: 1.0.0
+        description: A simple test API
+      servers:
+        - url: https://api.example.com
+          description: Production server
+      paths:
+        /ping:
+          get:
+            summary: Ping endpoint
+            operationId: ping
+            responses:
+              "200":
+                description: Successful response
+                content:
+                  application/json:
+                    schema:
+                      type: object
+                      properties:
+                        message:
+                          type: string
+                          example: "pong"
+      components:
+        schemas:
+          PingResponse:
+            type: object
+            properties:
+              message:
+                type: string
+    YAML
+
+    system bin/"resterm", "--from-openapi", testpath/"openapi.yml",
+                          "--http-out",     testpath/"out.http",
+                          "--openapi-base-var", "apiBase",
+                          "--openapi-server-index", "0"
+
+    assert_match "GET {{apiBase}}/ping", (testpath/"out.http").read
+  end
+end
