03f183d095d5e099570938b89bb502c525237b6a
botantony <antonsm21@gmail.com>
2025-11-16 16:45:10 +0100

    tflint 0.60.0
    tflint: bumped by upstream
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/t/tflint.rb
+++ Formula/t/tflint.rb
@@ -1,12 +1,12 @@
 class Tflint < Formula
   desc "Linter for Terraform files"
   homepage "https://github.com/terraform-linters/tflint"
-  url "https://github.com/terraform-linters/tflint/archive/refs/tags/v0.59.1.tar.gz"
-  sha256 "9b45910e897fd2028d748387abc781f13c57127bacde97b083aed2198c7b105d"
+  url "https://github.com/terraform-linters/tflint/archive/refs/tags/v0.60.0.tar.gz"
+  sha256 "4a0c84fc4052de551f97e5c4b0b81f869e3ec708e4f27eff5157813c8b46fea3"
   license "MPL-2.0"
   head "https://github.com/terraform-linters/tflint.git", branch: "master"
 
-  no_autobump! because: :requires_manual_review
+  no_autobump! because: :bumped_by_upstream
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f873357d065fdce6b58511cc37b38ef5d36135e34d8c1a707b993bb8a26b786f"
