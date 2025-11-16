413c8bd17671832a7e84fdd155f7b856d675525e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-16 10:59:36 +0000

    immich-go 0.30.0

--- Formula/i/immich-go.rb
+++ Formula/i/immich-go.rb
@@ -1,8 +1,8 @@
 class ImmichGo < Formula
   desc "Alternative to the official immich-CLI command written in Go"
   homepage "https://github.com/simulot/immich-go"
-  url "https://github.com/simulot/immich-go/archive/refs/tags/v0.29.0.tar.gz"
-  sha256 "c9a5ba5dd0c104015cfaf08d00f25932ce77d2b7e684a6b9435d062f026562d6"
+  url "https://github.com/simulot/immich-go/archive/refs/tags/v0.30.0.tar.gz"
+  sha256 "7d8762bef1514bd31a485b2420d2d4b766faa5c882a52a50414666eeb745938d"
   license "AGPL-3.0-only"
   head "https://github.com/simulot/immich-go.git", branch: "main"
 
