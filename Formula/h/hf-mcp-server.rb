c293d5169810dea9af4e9c3dae84173b64f9bec9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-16 23:33:22 +0000

    hf-mcp-server: add 0.2.38 bottle.

--- Formula/h/hf-mcp-server.rb
+++ Formula/h/hf-mcp-server.rb
@@ -5,6 +5,15 @@ class HfMcpServer < Formula
   sha256 "54cbfde64e6b8e07f884329f54e29216b9313fc09a695bade34b744606444e41"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "95c605c571b4cc13c74020e347f7bb5f1a46cf1a98945c43debfff7bef2dfe08"
+    sha256 cellar: :any,                 arm64_sequoia: "bd0821f437c2bd4fcfe354a5585a584aca1deca4d62c2ad274c3579869afc613"
+    sha256 cellar: :any,                 arm64_sonoma:  "bd0821f437c2bd4fcfe354a5585a584aca1deca4d62c2ad274c3579869afc613"
+    sha256 cellar: :any,                 sonoma:        "6fbc465e0a7496f349ef57d6b971bc2d49998ba720f59327079a647a61ecc11c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "4f40f7fe847653e315afad7720f04fb6bb971b057167d619bd6458dc6f296783"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bc390e01c5d7401bc454054ef29120b8c88ebb196a48c4d1a99472148aac93a9"
+  end
+
   depends_on "node"
 
   def install
