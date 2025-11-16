2c5d1edc2142cb751b65cb85fb156f12e60e5e08
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-16 13:54:08 +0000

    claude-code-router 1.0.67

--- Formula/c/claude-code-router.rb
+++ Formula/c/claude-code-router.rb
@@ -1,8 +1,8 @@
 class ClaudeCodeRouter < Formula
   desc "Tool to route Claude Code requests to different models and customize any request"
   homepage "https://github.com/musistudio/claude-code-router"
-  url "https://registry.npmjs.org/@musistudio/claude-code-router/-/claude-code-router-1.0.66.tgz"
-  sha256 "5876f78b4ac9849abc0623066445f6a0a66815d33972dd6229bf5a261b5c7ecf"
+  url "https://registry.npmjs.org/@musistudio/claude-code-router/-/claude-code-router-1.0.67.tgz"
+  sha256 "9c47182ecd127c0367fb6fd8774bfdd3b073d1715933188d52cbf15f6e2f24c5"
   license "MIT"
 
   bottle do
