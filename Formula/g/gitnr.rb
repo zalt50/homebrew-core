accc56f2a22a7abfc14abc785db60d38350eaf61
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-17 14:58:31 +0000

    gitnr: add 0.2.2 bottle.

--- Formula/g/gitnr.rb
+++ Formula/g/gitnr.rb
@@ -6,6 +6,15 @@ class Gitnr < Formula
   license "MIT"
   head "https://github.com/reemus-dev/gitnr.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e6f55ca6ffd7a5014925821fe4377caf19874f75837f9a5396dbd12482221d0a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8a46abfed2d446a26fd0bdf62c04f7136282411a585f6191f8c95a2b2018586a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f43cc5851f5fc45ffada1fc2f12ec45ea95358659833402f29821541579012ec"
+    sha256 cellar: :any_skip_relocation, sonoma:        "96444e77d1b7399ac511fdd49fe358bffe34717f97f820047d09e21f59981f9c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d72e48d034a9e4298b71f1cf912286668a9b898381ab8ffc2c960918d18df611"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fcb4d3b8d42018f3f4c5273361a9f375000bfecccfb59b32f04f45246ab166a0"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "openssl@3"
