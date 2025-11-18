9b31ad63264b0e3d9dc1362a6501f6717eacc590
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-18 17:15:38 +0000

    cagent: update 1.9.14 bottle.

--- Formula/c/cagent.rb
+++ Formula/c/cagent.rb
@@ -7,12 +7,12 @@ class Cagent < Formula
   head "https://github.com/docker/cagent.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f2118b76b21b07202b33bfa8b81b01fa7b0d5245a1090135f9a7e6c6a9f89bb8"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f2118b76b21b07202b33bfa8b81b01fa7b0d5245a1090135f9a7e6c6a9f89bb8"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f2118b76b21b07202b33bfa8b81b01fa7b0d5245a1090135f9a7e6c6a9f89bb8"
-    sha256 cellar: :any_skip_relocation, sonoma:        "ac366786d3cb1c9f303ef3da40d9a12f4d14d69944388a56a6cab929f675f81b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "006b2194f50f182bed35f93bc5b1432d7a250fdd3e67221393b2f8febf9e60f9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e390e179678f35f0d1ff7324a2a1fc5f3979f9ed94e8e250ce5197fb3250b5e3"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2521fc198dfc9fe6812326a84344dafe1dbb8d1829d3e109c7bb8206e0b61aaa"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2521fc198dfc9fe6812326a84344dafe1dbb8d1829d3e109c7bb8206e0b61aaa"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2521fc198dfc9fe6812326a84344dafe1dbb8d1829d3e109c7bb8206e0b61aaa"
+    sha256 cellar: :any_skip_relocation, sonoma:        "8d065bbc6a9f5918afd21b7cf2d49b6b51e758132d9b48719aaa07a908b96248"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d3a1906011b53a45c7cb26c92bf398d0e859b85ce971e6f06caafd77c76cbbbb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3b90609dfb030a96ed56dd2572e803ebe4a264c0e07b3400ce8da92a56f9401b"
   end
 
   depends_on "go" => :build
