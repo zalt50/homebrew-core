7fd63400a9261840d4391fb042e826ac62a1b7e6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-18 17:09:17 +0000

    gtree 1.13.1

--- Formula/g/gtree.rb
+++ Formula/g/gtree.rb
@@ -1,8 +1,8 @@
 class Gtree < Formula
   desc "Generate directory trees and directories using Markdown or programmatically"
   homepage "https://ddddddo.github.io/gtree/"
-  url "https://github.com/ddddddO/gtree/archive/refs/tags/v1.13.0.tar.gz"
-  sha256 "e35358895bbd2f2fa88318a7c52c93b8fb61685f0314422632ac8a7946fbb01c"
+  url "https://github.com/ddddddO/gtree/archive/refs/tags/v1.13.1.tar.gz"
+  sha256 "6523c8839a1904fc5475194dd4b27ebd1d94b07535d2d568ccffb7af0dfbd2cc"
   license "BSD-2-Clause"
   head "https://github.com/ddddddO/gtree.git", branch: "master"
 
