ada2f855e7a6f9a0d464f16ef34edf0274583bac
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-19 14:14:46 +0000

    pandocomatic 2.2.0
    pandocomatic: add libyaml dep and remove `mkmf.log`
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/p/pandocomatic.rb
+++ Formula/p/pandocomatic.rb
@@ -1,14 +1,11 @@
 class Pandocomatic < Formula
   desc "Automate the use of pandoc"
   homepage "https://heerdebeer.org/Software/markdown/pandocomatic/"
-  url "https://github.com/htdebeer/pandocomatic/archive/refs/tags/2.1.0.tar.gz"
-  sha256 "ca7dd1b325aa2f8abbec3d7cadabb71fe9ec803f044121589b1c89a13f66b245"
+  url "https://github.com/htdebeer/pandocomatic/archive/refs/tags/2.2.0.tar.gz"
+  sha256 "8138b403aaa1d23db11701bf2bbcbf14447d89b7b05030fc2faf91d6fe11163c"
   license "GPL-3.0-or-later"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, all: "2b46f58006899e9aa3af9e460becf6821fd7d4bb87fb45ff5d4187158a56d9eb"
-  end
-
+  depends_on "libyaml"
   depends_on "pandoc"
   depends_on "ruby"
 
@@ -24,6 +21,9 @@ class Pandocomatic < Formula
 
     bin.install libexec/"bin/#{name}"
     bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
+
+    # Remove mkmf.log files to avoid shims references
+    rm Dir["#{libexec}/extensions/*/*/*/mkmf.log"]
   end
 
   test do
