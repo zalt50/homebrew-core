6ac5838a9234ef1313fe012e44eaab30e8b518c9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-19 21:29:50 +0000

    fnox: add 1.5.2 bottle.

--- Formula/f/fnox.rb
+++ Formula/f/fnox.rb
@@ -6,6 +6,15 @@ class Fnox < Formula
   license "MIT"
   head "https://github.com/jdx/fnox.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9d14f559608e0e3c6028c2087ad23a3685720ccec6dd6a652e46e2f42331a584"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d01cd1d29c7a2ea35057f6a541511b4121d6d9493197c5ddcbd5dcbca9f2ddee"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c93fc57d70ad41f741644d0e21874fdc8ab9f8be4c0b31ad2ffa65536e661472"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e2b3eb2bc7cf9ff61fc2656accc94b276763b520574fc43cc11a70ad1d8561b7"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ba5e7319c1eb7c94c41648eeb9ff6f4e06b4df258b2ba2b0bf24bf397841dba5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cf4064aefd82c5a176ab9c35e7ca565c68b5a2411502fa10e18b11052a3eb950"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "age" => :test
