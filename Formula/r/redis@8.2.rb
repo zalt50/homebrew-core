a845b39a65a8edecbefd150d8c17e7f83dce7357
Mohamed Hafez <mohamed.m.m.hafez@gmail.com>
2025-11-20 12:10:57 +0000

    redis@8.2 8.2.3 (new formula)
    redis 8.4.0 was just released, but not everyone will want to be on the bleeding edge and might want to stay on the previous major release until they are able to test things out. redis 8.2 will be maintained for another 18 months according to the redis maintenance policy.
    
    Co-authored-by: Anton Melnikov <antonsm21@gmail.com>

---
+++ Formula/r/redis@8.2.rb
@@ -0,0 +1,51 @@
+class RedisAT82 < Formula
+  desc "Persistent key-value database, with built-in net interface"
+  homepage "https://redis.io/"
+  url "https://download.redis.io/releases/redis-8.2.3.tar.gz"
+  sha256 "d88f2361fdf3a3a8668fe5753e29915566109dca07b4cb036427ea6dc7783671"
+  license all_of: [
+    "AGPL-3.0-only",
+    "BSD-2-Clause", # deps/jemalloc, deps/linenoise, src/lzf*
+    "BSL-1.0", # deps/fpconv
+    "MIT", # deps/lua
+    any_of: ["CC0-1.0", "BSD-2-Clause"], # deps/hdr_histogram
+  ]
+
+  livecheck do
+    url "https://download.redis.io/releases/"
+    regex(/href=.*?redis[._-]v?(8\.2(?:\.\d+)*)\.t/i)
+  end
+
+  keg_only :versioned_formula
+
+  depends_on "openssl@3"
+
+  def install
+    system "make", "install", "PREFIX=#{prefix}", "CC=#{ENV.cc}", "BUILD_TLS=yes"
+
+    %w[run db/redis log].each { |p| (var/p).mkpath }
+
+    # Fix up default conf file to match our paths
+    inreplace "redis.conf" do |s|
+      s.gsub! "/var/run/redis_6379.pid", var/"run/redis.pid"
+      s.gsub! "dir ./", "dir #{var}/db/redis/"
+      s.sub!(/^bind .*$/, "bind 127.0.0.1 ::1")
+    end
+
+    etc.install "redis.conf"
+    etc.install "sentinel.conf" => "redis-sentinel.conf"
+  end
+
+  service do
+    run [opt_bin/"redis-server", etc/"redis.conf"]
+    keep_alive true
+    error_log_path var/"log/redis.log"
+    log_path var/"log/redis.log"
+    working_dir var
+  end
+
+  test do
+    system bin/"redis-server", "--test-memory", "2"
+    %w[run db/redis log].each { |p| assert_path_exists var/p, "#{var/p} doesn't exist!" }
+  end
+end
