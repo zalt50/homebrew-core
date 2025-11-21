12aeba54e1f1d781df0ca6c249b20fa046739db3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-21 13:52:34 +0000

    bpmnlint 11.7.1

--- Formula/b/bpmnlint.rb
+++ Formula/b/bpmnlint.rb
@@ -1,8 +1,8 @@
 class Bpmnlint < Formula
   desc "Validate BPMN diagrams based on configurable lint rules"
   homepage "https://github.com/bpmn-io/bpmnlint"
-  url "https://registry.npmjs.org/bpmnlint/-/bpmnlint-11.7.0.tgz"
-  sha256 "aec370e9ab75d4afe9843a093e57479c392fd9a5b9891b3ee5f2199be0cd1919"
+  url "https://registry.npmjs.org/bpmnlint/-/bpmnlint-11.7.1.tgz"
+  sha256 "1c888468bf62470156684562238b038e391390d1e48e21515f0112516d82d2b4"
   license "MIT"
 
   bottle do
