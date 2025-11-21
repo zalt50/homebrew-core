38077d1d4f6447a3070c97ae60a1d2c986e2f513
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-21 20:21:15 +0000

    sqlcmd 1.9.0

--- Formula/s/sqlcmd.rb
+++ Formula/s/sqlcmd.rb
@@ -1,8 +1,8 @@
 class Sqlcmd < Formula
   desc "Microsoft SQL Server command-line interface"
   homepage "https://github.com/microsoft/go-sqlcmd"
-  url "https://github.com/microsoft/go-sqlcmd/archive/refs/tags/v1.8.2.tar.gz"
-  sha256 "ee600e4a147fd1e3c5e6cea834f14465fbccbd96f6b32bd3b4c9e104e094b9b2"
+  url "https://github.com/microsoft/go-sqlcmd/archive/refs/tags/v1.9.0.tar.gz"
+  sha256 "c11ff96b6c31314f258a7a58e52e525c525452865591a581c98a37f6c0a7df60"
   license "MIT"
 
   livecheck do
