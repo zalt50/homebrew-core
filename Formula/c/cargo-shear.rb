4c612006c9effd3fab1203a54534485e33b33a52
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-22 08:40:48 +0000

    cargo-shear: update 1.6.4 bottle.

--- Formula/c/cargo-shear.rb
+++ Formula/c/cargo-shear.rb
@@ -7,12 +7,12 @@ class CargoShear < Formula
   head "https://github.com/Boshen/cargo-shear.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "19aeff792b6925b66fa8795bf22cb0f81b2da5b732e6e6ee5bc92acd3fe927d1"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b3c671f2f12dbefbb784fe58ab9706df7631e5ce1d046b5be25e2d49b036ca7a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "16037905e65816b6373f5d25d49a6bc43cb340e9281f603a0e0766d4c4878ad4"
-    sha256 cellar: :any_skip_relocation, sonoma:        "f5bbb5bce277ee4914c5577513fc183bcc9f0b6c8721b51f6d0b8709a2263f29"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "462db000c7a1daa426b8fbb726a196706cdcedf7ca917287123541d8df0a34f5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2909696ee98c76ba6fd608f9c90ce2ea376fa12baaf381fb237fd9a5c8ba55ad"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cfa021e566641d72253c5380130b928c0262b90f71bcaa91bb7404b84b828d3b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e50d8035d1458a3ecba916ba0624b1833c3a6f3069a603b4876fe79f968d8078"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2edce79b728eef73026a58697a7352def358c8e0968693f2549f7afe8fa38126"
+    sha256 cellar: :any_skip_relocation, sonoma:        "76d9dbaf60c5389f24247e1ec66b64e7a6afbd3b1695802897662a9848fc79a3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e13e07c1e10510a44fc66ea2820e8e02f6282f3a0a16e32211c614b587000d04"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c8a3c2ddaf2250c0311cd38eaed03b8cdef9da2ccef659e8d8dead7aab23f33c"
   end
 
   depends_on "rust" => :build
