90562e4b35334296eedf6af94b8abb41302ad176
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-23 17:03:04 +0000

    jjui 0.9.6

--- Formula/j/jjui.rb
+++ Formula/j/jjui.rb
@@ -1,8 +1,8 @@
 class Jjui < Formula
   desc "TUI for interacting with the Jujutsu version control system"
   homepage "https://github.com/idursun/jjui"
-  url "https://github.com/idursun/jjui/archive/refs/tags/v0.9.5.tar.gz"
-  sha256 "d2ce49499512f7d2f830c2d425d858835e8cc58a68f97af018e05ae0e6a26fca"
+  url "https://github.com/idursun/jjui/archive/refs/tags/v0.9.6.tar.gz"
+  sha256 "7e739c8cad3362d18e83d60458cb594a5851e3543607d90b9dd071a4c479c430"
   license "MIT"
 
   bottle do
