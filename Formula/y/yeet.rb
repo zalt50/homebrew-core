dc816a8ae0337e060f67f9f7839d6a2a0b9a494d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-24 05:48:56 +0000

    yeet: update 0.8.1 bottle.

--- Formula/y/yeet.rb
+++ Formula/y/yeet.rb
@@ -7,12 +7,12 @@ class Yeet < Formula
   head "https://github.com/TecharoHQ/yeet.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "20fc2b2854628aee38648bb20671427ec86ef9872d58d6d1df92934766428c78"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "20fc2b2854628aee38648bb20671427ec86ef9872d58d6d1df92934766428c78"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "20fc2b2854628aee38648bb20671427ec86ef9872d58d6d1df92934766428c78"
-    sha256 cellar: :any_skip_relocation, sonoma:        "c7ade8c9f0e28dde1e21e088970e8be2500ad75e52d54e77a6ed5b52acddaa85"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "82b3c64e4df27cda60d14bf4b2bf9a60551e8d904ffce500d6957880b82621c7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cd3a1031582e973ad2b2ab73758dee95ebd1f1d01c076e72a620244d5d48979c"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8bd1f148b7ab439880382186e01053c7328faf48fc31e516d3e2219a2914dbd6"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8bd1f148b7ab439880382186e01053c7328faf48fc31e516d3e2219a2914dbd6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8bd1f148b7ab439880382186e01053c7328faf48fc31e516d3e2219a2914dbd6"
+    sha256 cellar: :any_skip_relocation, sonoma:        "f8e39b4014eabde128677677002de5918e80af8cd776d7a75a40afae9c2aa6e6"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "81aaede03fd120dc85d7ded8a3efc832c378a53335775b81efa62dcc4edfcfd0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3c3e9b479d7a14d8aea22105e7286d145b6dc1edcbe9dd4c420bcf2e1d764590"
   end
 
   depends_on "go" => :build
