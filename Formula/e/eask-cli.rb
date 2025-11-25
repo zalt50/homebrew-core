3160c9cab344528354d958bf138b8bf4d00b4eb0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-25 19:55:57 +0000

    eask-cli 0.11.9

--- Formula/e/eask-cli.rb
+++ Formula/e/eask-cli.rb
@@ -1,8 +1,8 @@
 class EaskCli < Formula
   desc "CLI for building, running, testing, and managing your Emacs Lisp dependencies"
   homepage "https://emacs-eask.github.io/"
-  url "https://registry.npmjs.org/@emacs-eask/cli/-/cli-0.11.8.tgz"
-  sha256 "201d03d1002e9c95ab99600cf7c1b270f5ece49c268eefe70a8c8587215ea430"
+  url "https://registry.npmjs.org/@emacs-eask/cli/-/cli-0.11.9.tgz"
+  sha256 "abf650008e51b200a8630e8e5e712dbe243999cfcfd631d0bb4c7d33fc4e2d83"
   license "GPL-3.0-or-later"
 
   bottle do
