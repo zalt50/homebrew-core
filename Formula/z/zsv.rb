ad70a0c744703591ce13fb1d9ab169b7a3e1470e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-25 11:14:41 +0000

    zsv: update 1.2.0 bottle.

--- Formula/z/zsv.rb
+++ Formula/z/zsv.rb
@@ -7,12 +7,12 @@ class Zsv < Formula
   head "https://github.com/liquidaty/zsv.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5a70db05c2a5eb7d47a2495d300f9dfbca9cf06fa9bf36a470392d73afcbb25a"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9add2e77a8fdfcb160a17ba859e7c1b8e338062ee6878f508a096d17b2072265"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6db30c39005b391e187cf884b5b227ebe12494d4baea0f1cfcc996ffe2e616c2"
-    sha256 cellar: :any_skip_relocation, sonoma:        "75c0c47d69b260a5998933e759df7c44013a94e691dd118e75b423839d213a1c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "6626310746cc95e6fb2f65cf80f2010012bf0ee7b38f30b0cdabd95695839960"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f2b487e99f0312f0342f9a79e47a3cebf1fc1f8e8cfb8d1c78c042cd2d0ff5b1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "eb226634dfea7ba2f6c101ea87c38ad78cb9b50daf6e494f13da7a6ec94cf730"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9c73bb29f2215d4dbcb677ae6a80aa2651769c58841e7af2d0aae486a32ad55a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "daeba412aae30ce92672b27817df5c56b56aafe4b9c9c5a73e16a6a395c6aaea"
+    sha256 cellar: :any_skip_relocation, sonoma:        "263aa15d9c24eb9ec949f63d273bc747d4642a13d45896f3f0cba45b053f0901"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "cc7ffc41a8c78ab8b07676a2e72c2c1025daf2dc03761584e3c2c837def8db3c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2dcbc9ec6b71626102095e4f369da25f811c96d23dd80827c2a9a356bb60b8f6"
   end
 
   depends_on "jq"
