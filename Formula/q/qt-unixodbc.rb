7c4e3947b85002d82141c6f897718ccd5b1e687e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-25 17:44:59 +0000

    qt-unixodbc: update 6.9.3 bottle.

--- Formula/q/qt-unixodbc.rb
+++ Formula/q/qt-unixodbc.rb
@@ -12,12 +12,13 @@ class QtUnixodbc < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "1589d4a98b18cf52d15a61dace7830fcd03d6211f69b64e1b0c4863768e6d8c5"
-    sha256 cellar: :any,                 arm64_sequoia: "1b5461acec7bba9750afa1b51ef716925c4ff20d09964752492adbce9f500682"
-    sha256 cellar: :any,                 arm64_sonoma:  "5fabce420fa3ed6a8d5021867eaaeb9b3f9e34d0dff991ced59092dd0bed52c0"
-    sha256 cellar: :any,                 sonoma:        "1d1036ee7ba9efad7060650522a428d46245b69adeaab8458ca81620ae72e736"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "de6b1187ab26141901825b1c538d2f3347c09cb3083e21afc98e26029172dbf1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5e86a8aaf55ee4bce3c938528d25666bc1fb980cca69953d714d18dd4c2ebfa5"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_tahoe:   "759c552b47b2079674aa8e32dfe68fe07d105617836d0d592ea2c739d1214a5a"
+    sha256 cellar: :any,                 arm64_sequoia: "28723ba39c3a5f96660da5b402a1e963cd5415973f37c21e1d64c4ed93a9676e"
+    sha256 cellar: :any,                 arm64_sonoma:  "73d6f8b034d1782597c3f59f67aa431c1d66738b2f53113630ad7780fc026ca4"
+    sha256 cellar: :any,                 sonoma:        "544c549fcb7475de2c28c0c9d753b99cc3293f0459ce1d94af1ebb2091d35d29"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "36153e0040372b78e175dc304af0cc100431d868e6f436e5bbb8065421533fee"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5c45588734da878f2733abf7b7e1c3b6644cc7b1baf99eba9317c56ddd97d358"
   end
 
   depends_on "cmake" => [:build, :test]
