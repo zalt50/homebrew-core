373ff6027d6bd79c10171c3f36c18d9b1cac58ac
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-25 14:46:13 +0000

    wasm-micro-runtime: update 2.4.4 bottle.

--- Formula/w/wasm-micro-runtime.rb
+++ Formula/w/wasm-micro-runtime.rb
@@ -15,12 +15,12 @@ class WasmMicroRuntime < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "813abc6b66e97f8b07b4513831a6f2ce0d649d9cafb2a34bf6b68f1eb0117a42"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3f24a396d0a48d07d77f7afcc570b8da6ab9a3b69baaec5efe0ea929b0cdb061"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "20a4dca96df6eeff3a08822169839c9b62043b4a7216236424cdaa8dbfe30d45"
-    sha256 cellar: :any_skip_relocation, sonoma:        "3a30eb9519310b933486e2b03f0f5733eca3d865061e51e522d5fc44c60f32f2"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "dd0735a1ef01e09504202bd529143fc37945512de0a87a736bf90dc17769952a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "10d759a5ac9d54f7b5975357e647aad6f04967a0e6260b077bb12c9c0e0146ca"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cb1fb11460b0ba366b42a53826db2f0dc32c572650a5658f741696f76a0af5aa"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4ca95524c2802e0fa4abc778ae0a597688ff236d4a250c23fe60325899e37341"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "887b91d48d7b797b7b8fed98196dfa5c3bed74193195143df8df7c5f8255e0d8"
+    sha256 cellar: :any_skip_relocation, sonoma:        "1f916c8ea659a2ea61fa5e5058475823a22002ee0b282cf45c49861428b95e88"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c465af087d23419ecdf9895fe47703c6dcff7a9707c923ee6d8330d9f135ee3f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "50a35bc9d1fd6da9029443fd5120883f0deee39044e87bbb755f109c5197c374"
   end
 
   depends_on "cmake" => :build
