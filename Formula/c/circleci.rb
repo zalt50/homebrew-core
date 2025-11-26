683778cba24cf79c79c041e22aeb759313243ee6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-26 16:59:11 +0000

    circleci 0.1.33826

--- Formula/c/circleci.rb
+++ Formula/c/circleci.rb
@@ -3,8 +3,8 @@ class Circleci < Formula
   homepage "https://circleci.com/docs/guides/toolkit/local-cli/"
   # Updates should be pushed no more frequently than once per week.
   url "https://github.com/CircleCI-Public/circleci-cli.git",
-      tag:      "v0.1.33721",
-      revision: "340a1059a410fdb62f5815de25ae6f1de69a6674"
+      tag:      "v0.1.33826",
+      revision: "5fa3c151f3c492119baeeab328c9a47aca34bba9"
   license "MIT"
   head "https://github.com/CircleCI-Public/circleci-cli.git", branch: "main"
 
