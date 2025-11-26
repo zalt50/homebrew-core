e9aa13663774423221629d5e81f5b3c3c6e5fc85
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-26 14:02:59 +0000

    fcgi 2.4.7

--- Formula/f/fcgi.rb
+++ Formula/f/fcgi.rb
@@ -2,8 +2,8 @@ class Fcgi < Formula
   desc "Protocol for interfacing interactive programs with a web server"
   # Last known good original homepage: https://web.archive.org/web/20080906064558/www.fastcgi.com/
   homepage "https://fastcgi-archives.github.io/"
-  url "https://github.com/FastCGI-Archives/fcgi2/archive/refs/tags/2.4.6.tar.gz"
-  sha256 "39af4fb21a6d695a5f0b1c4fa95776d2725f6bc6c77680943a2ab314acd505c1"
+  url "https://github.com/FastCGI-Archives/fcgi2/archive/refs/tags/2.4.7.tar.gz"
+  sha256 "e41ddc3a473b555bdc0cbd80703dcb1f4610c1a7700d3b9d3d0c14a416e1074b"
   license "OML"
 
   bottle do
