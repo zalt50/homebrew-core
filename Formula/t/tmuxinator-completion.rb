4742874c1ae2616a36503c4eea969ea82fc00ecc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-26 20:19:07 +0000

    tmuxinator-completion 3.3.6

--- Formula/t/tmuxinator-completion.rb
+++ Formula/t/tmuxinator-completion.rb
@@ -1,8 +1,8 @@
 class TmuxinatorCompletion < Formula
   desc "Shell completion for Tmuxinator"
   homepage "https://github.com/tmuxinator/tmuxinator"
-  url "https://github.com/tmuxinator/tmuxinator/archive/refs/tags/v3.3.5.tar.gz"
-  sha256 "56dc5ca39d11bad38f7fb602ce412a0f564dc0757765ad105a2cccbe84fcd2ca"
+  url "https://github.com/tmuxinator/tmuxinator/archive/refs/tags/v3.3.6.tar.gz"
+  sha256 "ea1b9a18f45ed26fdb51fd6edd2aada341f338b2df34868746d73c7e7a7b7dff"
   license "MIT"
   head "https://github.com/tmuxinator/tmuxinator.git", branch: "master"
 
