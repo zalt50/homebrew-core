a51bcfc9e2769f149dee9cf6bf0223c44b7d33b4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-26 01:26:16 +0000

    dart-sdk: update 3.10.2 bottle.

--- Formula/d/dart-sdk.rb
+++ Formula/d/dart-sdk.rb
@@ -6,12 +6,12 @@ class DartSdk < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "27b50fc0c63f4be0e61dee01776ffce3180361a1729c3ef26ab3346e18a4ed0d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2f3c7af5bee05f95eb0d83d5a54a3de737f85edbbf2334b0ee6459009dbc597e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "40ff9d613f43661d271dfe47d2fdf9820b299d87f02956eba6bca6d805d21092"
-    sha256 cellar: :any_skip_relocation, sonoma:        "960ef836bf2f6510a1c9bc49c1193139484271f756fd40a3a355d9b60316d97b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "101b27a8fb07008e297a2cd10d3d1bfb18fee4d1a1a7a54a1050bce751566b97"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "115633af188d603b33061f3580be2d5d357da743fd9c157ff9c80e3be78f8624"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "067bed994e944cfea5c601f0f47d306df1254d2bc70d52de28b44ec5e2f12de1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4d4285eff65acfdf605d36f8030a3c970c13f2bdaad031c78a28f524ec37e6a8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e7402fe17d15f93d0989dcaa413106844a05f798eb9da120afb63c1e74696992"
+    sha256 cellar: :any_skip_relocation, sonoma:        "931dce188d3dfcd0f4c4f09bc57078aa5931b4cc15ff5337f057488ae96d71e8"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "651f6e9359afcd3fb70a2023e3645e17e6d3093dcded88ea74c402405fc825a2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2d4f4059650e3c4a6df2515b1aa59618eec289a9d94da0d98580fc88554f7c81"
   end
 
   depends_on "ninja" => :build
