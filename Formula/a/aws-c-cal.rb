a358c5b5b23cfb0ce7ee1a4d41df6b379d44b791
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-26 19:49:44 +0000

    aws-c-cal 0.9.13

--- Formula/a/aws-c-cal.rb
+++ Formula/a/aws-c-cal.rb
@@ -1,8 +1,8 @@
 class AwsCCal < Formula
   desc "AWS Crypto Abstraction Layer"
   homepage "https://github.com/awslabs/aws-c-cal"
-  url "https://github.com/awslabs/aws-c-cal/archive/refs/tags/v0.9.12.tar.gz"
-  sha256 "05694cf5c4ce8b0009a753e0cc36e1c09622800e6e29cb8f2fe40a313a6537b4"
+  url "https://github.com/awslabs/aws-c-cal/archive/refs/tags/v0.9.13.tar.gz"
+  sha256 "80b7c6087b0af461b4483e4c9483aea2e0dac5d9fb2289b057159ea6032409e1"
   license "Apache-2.0"
 
   bottle do
