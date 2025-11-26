f6e24abd8805f34e14846f928e80d56688dc8a9e
botantony <antonsm21@gmail.com>
2025-11-26 01:51:06 +0100

    pulumi 3.208.0

--- Formula/p/pulumi.rb
+++ Formula/p/pulumi.rb
@@ -2,8 +2,8 @@ class Pulumi < Formula
   desc "Cloud native development platform"
   homepage "https://pulumi.io/"
   url "https://github.com/pulumi/pulumi.git",
-      tag:      "v3.207.0",
-      revision: "5e3a50056a84e3c5202b0bd77dce8cd5e70d7e93"
+      tag:      "v3.208.0",
+      revision: "2134db3da537219feabe3b7a1631303dd7f1fae0"
   license "Apache-2.0"
   head "https://github.com/pulumi/pulumi.git", branch: "master"
 
