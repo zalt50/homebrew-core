cd3a917fcc99420e1e2f059ae07e2972963e7a81
Anton Melnikov <antonsm21@gmail.com>
2025-11-27 23:23:17 +0000

    auto-editor: add 29.3.1 bottle.

--- Formula/a/auto-editor.rb
+++ Formula/a/auto-editor.rb
@@ -6,6 +6,15 @@ class AutoEditor < Formula
   license "Unlicense"
   head "https://github.com/WyattBlue/auto-editor.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "d88dd9dffd9f729473eadd4ae42504dca4db8909cac72ac797bd5391313db41f"
+    sha256 cellar: :any,                 arm64_sequoia: "2951573d60a912187a20feeb7365e43500e6da6e107c895be512f4f59bf46b00"
+    sha256 cellar: :any,                 arm64_sonoma:  "1427c7e919ac17e825fc2ff5d5907f5de607f8bf8b24dd2a3c4f29473a105891"
+    sha256 cellar: :any,                 sonoma:        "5e93bc0f2296bcdc6496e99a69a3d30c833104d8e199fbcea1d6305fe7754651"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "8aa152a377b112a7e07ee2470c3cd8328187814be62c725538563dcea6cf030e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1038847ce65836fce830d9c981825a7b64b188a3fd86502ac5715862a1b76d89"
+  end
+
   depends_on "nim" => :build
   depends_on "pkgconf" => :build
   depends_on "dav1d"
