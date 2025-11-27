6812a28e59bf9ab3c9abd969db61ca9b16144481
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-27 06:22:19 +0000

    gitlogue: add 0.4.1 bottle.

--- Formula/g/gitlogue.rb
+++ Formula/g/gitlogue.rb
@@ -6,6 +6,15 @@ class Gitlogue < Formula
   license "ISC"
   head "https://github.com/unhappychoice/gitlogue.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "055614d8ea1ff0fcebf6b1f6e325ae379547c205ac48df025315a8ca5be2b939"
+    sha256 cellar: :any,                 arm64_sequoia: "bc3dfab7e5bd9b569c55cc7131b3e00a47f3471c90efd04278a9804f1292f391"
+    sha256 cellar: :any,                 arm64_sonoma:  "4efa297484198920e972343096a2688aaa1fb3a2365dcd4f73a530b7961883a9"
+    sha256 cellar: :any,                 sonoma:        "9b90fc6ab8f2aa663cd007e64ad07cbe77ff317cd546f46347e61e7cb69ff65d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "405ed62df60a3c3b09e949021135907c9a88bceb5cc3acd99d8571806860efb3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "587eb12be72682b9dcdfefe54ece2e9bf6368f6652898ed48ef2b84a67edc455"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "openssl@3"
