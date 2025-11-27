4f998e3d4830d28b7c37ac35987fe347870ffaa5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-27 16:29:45 +0000

    thorvg: add 0.15.16 bottle.

--- Formula/t/thorvg.rb
+++ Formula/t/thorvg.rb
@@ -6,6 +6,15 @@ class Thorvg < Formula
   license "MIT"
   head "https://github.com/thorvg/thorvg.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "9d87490684470b10afc64c6fe45a03468145bed36759cf41365e04352953ee24"
+    sha256 cellar: :any,                 arm64_sequoia: "69d5298cd5ac913d954b5ee999023788c0db21d00c5e63de95c79aea3a64e783"
+    sha256 cellar: :any,                 arm64_sonoma:  "a8ccd6d8af86d06fb8c5f782185b3b6e70d29d9cf00b03eeded13eba58c3bad2"
+    sha256 cellar: :any,                 sonoma:        "653d87f6b405b6f5aa45abc702f25fab2200d2f8ab5c3f86b079b8404931747a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "0138144a2810b47dcebdba850819b1f1b50c04ad303f829014fae922e8470b17"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "aab00cb963c786ef524be4a1550989521a9970beb22d4265888df40e099a1f55"
+  end
+
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkgconf" => [:build, :test]
