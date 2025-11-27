b93e33994744b5f03ff2d028a8ed0aec02861501
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-27 23:35:04 +0000

    nak: update 0.17.1 bottle.

--- Formula/n/nak.rb
+++ Formula/n/nak.rb
@@ -7,14 +7,12 @@ class Nak < Formula
   head "https://github.com/fiatjaf/nak.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "be454c25e7c873cb227a947a19bc40129e6608483114d6e4fa0205ad0c5d2fd7"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "be454c25e7c873cb227a947a19bc40129e6608483114d6e4fa0205ad0c5d2fd7"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "be454c25e7c873cb227a947a19bc40129e6608483114d6e4fa0205ad0c5d2fd7"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "be454c25e7c873cb227a947a19bc40129e6608483114d6e4fa0205ad0c5d2fd7"
-    sha256 cellar: :any_skip_relocation, sonoma:        "04bc2d81fd2004bfc8c9a2187d795807de89d347ca7338bc1d3307732256d62e"
-    sha256 cellar: :any_skip_relocation, ventura:       "04bc2d81fd2004bfc8c9a2187d795807de89d347ca7338bc1d3307732256d62e"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "9674237330fa2e02dc819fa92e8c9d2a5001affff399657ed5cf3c0c3fc50bca"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "58f5e7a2435a994fd1a08f94edf7abc8d7e68add6c2a2cd3dff8ef330c92359e"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3bd87d9decc1c64dff6955a9778f1830414004ece0688a3a24f49aeed5d439ac"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3bd87d9decc1c64dff6955a9778f1830414004ece0688a3a24f49aeed5d439ac"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3bd87d9decc1c64dff6955a9778f1830414004ece0688a3a24f49aeed5d439ac"
+    sha256 cellar: :any_skip_relocation, sonoma:        "be59b2edc7fb80d972227f1a05a63f3ac063f954a899af51a9c1f5f8fb3c4f12"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c70fddb8fefd4288c0b24ab2a72832b6811925604bf0011b2577598c98b3f407"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dc440768d18a83be53b79736e57a2e54c130ace91fb87240f9e9dde14bf31633"
   end
 
   depends_on "go" => :build
