7bd4605ec27f8018adca9f7391c6a26a909f5ebd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-28 02:34:32 +0000

    helmfile 1.2.2

--- Formula/h/helmfile.rb
+++ Formula/h/helmfile.rb
@@ -1,8 +1,8 @@
 class Helmfile < Formula
   desc "Deploy Kubernetes Helm Charts"
   homepage "https://github.com/helmfile/helmfile"
-  url "https://github.com/helmfile/helmfile/archive/refs/tags/v1.2.1.tar.gz"
-  sha256 "da8162000e9cdf7a8c95d5191f3779df8b065ef5c722f292e77f6e8f77a7864e"
+  url "https://github.com/helmfile/helmfile/archive/refs/tags/v1.2.2.tar.gz"
+  sha256 "8992127362d5fdff46695a30656af0fed32cfec4dda3645f20dde0b8d2a703f4"
   license "MIT"
   version_scheme 1
   head "https://github.com/helmfile/helmfile.git", branch: "main"
