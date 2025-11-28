21cff698034142aa32bb60a4f7f271ccfbf1099c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-28 14:23:17 +0000

    open-mpi 5.0.9

--- Formula/o/open-mpi.rb
+++ Formula/o/open-mpi.rb
@@ -1,8 +1,8 @@
 class OpenMpi < Formula
   desc "High performance message passing library"
   homepage "https://www.open-mpi.org/"
-  url "https://download.open-mpi.org/release/open-mpi/v5.0/openmpi-5.0.8.tar.bz2"
-  sha256 "53131e1a57e7270f645707f8b0b65ba56048f5b5ac3f68faabed3eb0d710e449"
+  url "https://download.open-mpi.org/release/open-mpi/v5.0/openmpi-5.0.9.tar.bz2"
+  sha256 "dfb72762531170847af3e4a0f21d77d7b23cf36f67ce7ce9033659273677d80b"
   license "BSD-3-Clause"
 
   livecheck do
