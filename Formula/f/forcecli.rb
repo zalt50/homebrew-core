f48932432b47b5b21665016aeaf7610b202233ca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-28 20:18:30 +0000

    forcecli: update 1.5.0 bottle.

--- Formula/f/forcecli.rb
+++ Formula/f/forcecli.rb
@@ -7,12 +7,12 @@ class Forcecli < Formula
   head "https://github.com/ForceCLI/force.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "43fc7cb45ecb1bc3562fd405d8838bebb7c6fa1efe2fb27db4aaf6bd4d95f9dd"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "43fc7cb45ecb1bc3562fd405d8838bebb7c6fa1efe2fb27db4aaf6bd4d95f9dd"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "43fc7cb45ecb1bc3562fd405d8838bebb7c6fa1efe2fb27db4aaf6bd4d95f9dd"
-    sha256 cellar: :any_skip_relocation, sonoma:        "0abdb2961407b483247e6c65dc054a35c3d6d6a833bd9c7aeeb02ca2dda505d5"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "7bb8d6c2365771e576b2e65f2e8ef242c4a616d1c1df09241b8d7106aa55fcbc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f7e00e70481c04d03b6418051078f2dd0576e57a51576770ba2ec8e639cfe4d5"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3e60a77fa6b93b645b4f3180eb7e7c6d2aaedcb0f7923dd10ca389f79b859a86"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3e60a77fa6b93b645b4f3180eb7e7c6d2aaedcb0f7923dd10ca389f79b859a86"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3e60a77fa6b93b645b4f3180eb7e7c6d2aaedcb0f7923dd10ca389f79b859a86"
+    sha256 cellar: :any_skip_relocation, sonoma:        "4f78a475d5a845b6c25e65e355044822683a2d4e393ed948caf01a347777e260"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "4aea5100928b4404cbedfae4ab8ca082047d0776d653f5402d3e0062c592c30d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2256911fb347c49deac84bdb2bdf77746c154633fd1785fd0ab762b1308485d0"
   end
 
   depends_on "go" => :build
