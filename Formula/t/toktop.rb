6be607038a5a0f56c59bd3d0a33cf52ef776f703
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-29 02:30:16 +0000

    toktop: add 0.1.4 bottle.

--- Formula/t/toktop.rb
+++ Formula/t/toktop.rb
@@ -6,6 +6,15 @@ class Toktop < Formula
   license "MIT"
   head "https://github.com/htin1/toktop.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e30a30bd265db19de073758917740442772cdd6712db3500e5378a218d69b5c6"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2d39463c6f12f08803bf39c5ab6d7e2dd4e8f8fe40033ea9e9ca4f3227f27213"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4abd965ad2bfed149e2b478d29ffe90dd4b5cac857bfcd02d31676ceb7533ae9"
+    sha256 cellar: :any_skip_relocation, sonoma:        "992187eaddc4f89f27a20b165d312b246db15d4da3b5b64a717ffa587ba7db32"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "30d2da44a7b61babb2e34406fa30abdb5756a4b7e78be143fcde6904d367c90f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3a402e018dd9765740a1545bf320bc0e07348f2a47cf815c87f1c37e311d2f47"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
 
