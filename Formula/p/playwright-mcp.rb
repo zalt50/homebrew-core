c00d91b93e6134bbe726544bb93b2589f78f2ab9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-30 20:25:20 +0000

    playwright-mcp 0.0.49

--- Formula/p/playwright-mcp.rb
+++ Formula/p/playwright-mcp.rb
@@ -1,19 +1,18 @@
 class PlaywrightMcp < Formula
   desc "MCP server for Playwright"
   homepage "https://github.com/microsoft/playwright-mcp"
-  url "https://registry.npmjs.org/@playwright/mcp/-/mcp-0.0.48.tgz"
-  sha256 "5fe91d0235b16cc2ba682d428798db13c192437489dc3c724382d5c7ea45730a"
+  url "https://registry.npmjs.org/@playwright/mcp/-/mcp-0.0.49.tgz"
+  sha256 "2bc7be6e7b8750e46e2de0f1d6b0ebd1ce2ee7874e0b9eb3b8007f4f07b525c4"
   license "Apache-2.0"
-
-  bottle do
-    sha256 cellar: :any_skip_relocation, all: "e712b1adebb195f8b3ea51103e5041c48d5f667431271c54d5b6accc136b6f65"
-  end
 
   depends_on "node"
 
   def install
     system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
+
+    node_modules = libexec/"lib/node_modules/@playwright/mcp/node_modules"
+    deuniversalize_machos node_modules/"fsevents/fsevents.node" if OS.mac?
   end
 
   test do
