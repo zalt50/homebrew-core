ebc9aee77ef6706a2485d530e629e9b7a2d5eede
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-30 20:18:27 +0000

    minijinja-cli 2.13.0

--- Formula/m/minijinja-cli.rb
+++ Formula/m/minijinja-cli.rb
@@ -1,8 +1,8 @@
 class MinijinjaCli < Formula
   desc "Render Jinja2 templates directly from the command-line to stdout"
   homepage "https://docs.rs/minijinja/latest/minijinja/"
-  url "https://github.com/mitsuhiko/minijinja/archive/refs/tags/2.12.0.tar.gz"
-  sha256 "b70d30f5165527d5b67712bf01226fb6add6b8ec38ada194c56f28889a4c6abe"
+  url "https://github.com/mitsuhiko/minijinja/archive/refs/tags/2.13.0.tar.gz"
+  sha256 "c5af57b4403a7283e2057efff6c90990b933d79e436f54ef88ef2bfe3f21e309"
   license "Apache-2.0"
   head "https://github.com/mitsuhiko/minijinja.git", branch: "main"
 
