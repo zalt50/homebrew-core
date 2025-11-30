3003974d860d20c92c075eed412cc5e0ce799017
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-30 06:01:51 +0000

    cloud-provider-kind: update 0.10.0 bottle.

--- Formula/c/cloud-provider-kind.rb
+++ Formula/c/cloud-provider-kind.rb
@@ -7,12 +7,12 @@ class CloudProviderKind < Formula
   head "https://github.com/kubernetes-sigs/cloud-provider-kind.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "bd6809959e5c4db2f5d16a0a3377d298d23831a8aa0889b15d5e43c1cdefdeb3"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "501076b26258448230a71be544bf5a3f11cb7c160f69d3c87123b0f46191112e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f662274498463cd4691da354b29effaf4c8f9650c7f767e6801bb362a42e0931"
-    sha256 cellar: :any_skip_relocation, sonoma:        "0cc93b9511006780da356d6c60689522cf1b346f4a890f80cd22fd64eaa3b5a2"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "571409c17bfae88192e6422fefb598b2ccdb3ed7467c3506cfb6e850973a4ce5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "32d44aaa54e496caea95f534a143997243d4e96acda85d7fb8f42b7aac59bf3e"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b4867a99f4f151a5fc46993d341205c0284b3ca70e29ff5e232a27569f88e5ed"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "65e855c0f1b8e5f3365803e50fe681f8e19ed9afd6e73edba7bffcb558f26527"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "51ae3dec41af8ae6e0df03af05d71aa9069f5ea8e4ea423c33342cbf25f81674"
+    sha256 cellar: :any_skip_relocation, sonoma:        "7eee4fa016c965fefadc4372d845937c6cb17306e0be785c1d6da5189ee95f92"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "4b665a78847892a53c75e2fadda0c56cc4921661afef1cdcc303c536c911b298"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "93f92eeade6d3ad52e1eeef18fb0907ed03e19de6ba5ceafaface4803cf1d578"
   end
 
   depends_on "go" => :build
