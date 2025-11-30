9df7465297d3d7cfc03e41585097eea376c06157
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-30 07:57:03 +0000

    docmd 0.3.0

--- Formula/d/docmd.rb
+++ Formula/d/docmd.rb
@@ -1,19 +1,23 @@
 class Docmd < Formula
   desc "Minimal Markdown documentation generator"
   homepage "https://docmd.mgks.dev/"
-  url "https://registry.npmjs.org/@mgks/docmd/-/docmd-0.2.6.tgz"
-  sha256 "3db8d1c514d57131c9ec2fc75bb8cf004ae3a783d52bc618ab398a7b6cb883f7"
+  url "https://registry.npmjs.org/@mgks/docmd/-/docmd-0.3.0.tgz"
+  sha256 "4cdec18c164926472728b7a0ae056a700170b0a4b6ce49efd2131321577c6343"
   license "MIT"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "f39a0ddadee88ca8948628c6e0df93f6ac16cc74bd00a00e87ed93e63ab8fbff"
   end
 
+  depends_on "esbuild" # for prebuilt binaries
   depends_on "node"
 
   def install
     system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
+
+    # Remove pre-built binaries
+    rm_r(libexec/"lib/node_modules/@mgks/docmd/node_modules/@esbuild")
   end
 
   test do
