42ce16fe6fa3efb886adda423b50f282a99be169
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-01 03:22:33 +0000

    mihomo: update 1.19.17 bottle.

--- Formula/m/mihomo.rb
+++ Formula/m/mihomo.rb
@@ -7,12 +7,12 @@ class Mihomo < Formula
   head "https://github.com/MetaCubeX/mihomo.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1e8606551854513a93cf4dde773707131aca7758aad988b72620fb6fbeef5161"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a7c764b12014195c7c4a85585e69ef313977aa35aa0ed06a4a77fcd2a0350d95"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9329eb9e9bc68acd842ce0a7cc0a17c13f9044422a2354cdf1007bb8e8917e81"
-    sha256 cellar: :any_skip_relocation, sonoma:        "ea75b1366af71360f4c3ad92f33a6527396b9834d59a76528c0d0f985e7d361c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f689093672e2ce5f11b7c942480bcc39a5b42ff73b377b45a3a7243726696712"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2647d09b9870b31efe0754902b065ffaaed59413a2bf30460105b86c908bec6b"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a06f2714a07be0ded85f3d49b23f34ffdddc3b294a4b8afe6468277e490020fb"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2a60fafbecf85be0cb42e262d92a35d04d5839c46b40d2f8b4c9bcf8824b0572"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b3989b420a5131996be6e36116f4a3850870e028972a29efc0e1d77e922677a8"
+    sha256 cellar: :any_skip_relocation, sonoma:        "1d082d85993f4323b3d6efa2457dfcdc3f4898b69aa713b29bb38c2dee69dff2"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1e66cacc26aac00caaec2a0956ce1d8f5f80213ad2fbfc41cd2d44461a7a1492"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2a3211c42d89bc7daa262a68b98bf6ad7e887ebb8502e953237e7824a2743472"
   end
 
   depends_on "go" => :build
