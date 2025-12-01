cea7c83721aa8b87c629da6e7b82f7aed5523117
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-01 02:54:04 +0000

    bashunit: update 0.28.0 bottle.

--- Formula/b/bashunit.rb
+++ Formula/b/bashunit.rb
@@ -6,7 +6,7 @@ class Bashunit < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "e40103db93e1089fa018d4551183c84264c3b912dd887d7e60c8185fc0aec6bf"
+    sha256 cellar: :any_skip_relocation, all: "818ad797f143dd973c21360ca282b1d4b5923b917bf775e0e8bfcf7b4536df7e"
   end
 
   def install
