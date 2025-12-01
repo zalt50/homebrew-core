806a50beaf37bc9624da40feb72720886e598e41
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-01 19:55:02 +0000

    clusterawsadm 2.10.0

--- Formula/c/clusterawsadm.rb
+++ Formula/c/clusterawsadm.rb
@@ -2,8 +2,8 @@ class Clusterawsadm < Formula
   desc "Home for bootstrapping, AMI, EKS, and other helpers in Cluster API Provider AWS"
   homepage "https://cluster-api-aws.sigs.k8s.io/clusterawsadm/clusterawsadm.html"
   url "https://github.com/kubernetes-sigs/cluster-api-provider-aws.git",
-      tag:      "v2.9.3",
-      revision: "731046cc94317f2484d8768764afe94d68c482b7"
+      tag:      "v2.10.0",
+      revision: "175074f222a2984ec569ab743fd352e6683b27cf"
   license "Apache-2.0"
   head "https://github.com/kubernetes-sigs/cluster-api-provider-aws.git", branch: "main"
 
