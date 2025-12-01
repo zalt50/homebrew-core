cb97948123b3af3869799a226ebcfd3c3b4507c0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-01 03:54:21 +0000

    diffnav: add 0.3.1 bottle.

--- Formula/d/diffnav.rb
+++ Formula/d/diffnav.rb
@@ -6,6 +6,15 @@ class Diffnav < Formula
   license "MIT"
   head "https://github.com/dlvhdr/diffnav.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "766b8a324775eb82135648ab03c7413b3aeb71d71b43d3c9ee05e8abbf66d6b7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "766b8a324775eb82135648ab03c7413b3aeb71d71b43d3c9ee05e8abbf66d6b7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "766b8a324775eb82135648ab03c7413b3aeb71d71b43d3c9ee05e8abbf66d6b7"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a6f1be8aef93a0ec85cd7cc2b9724004cc5497fbd115e61a29114b7b0a5ad064"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e236211305c388cf45c6d4b47ebb0da64bcc36de572cc1b2e553736e2998ed0e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9cc107b6a49e2a02038cd986376362205fac05a7597662e8dc270ac7ee34869f"
+  end
+
   depends_on "go" => :build
   depends_on "git-delta"
 
