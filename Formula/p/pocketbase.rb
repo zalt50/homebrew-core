2020471269ecd012102d2ab6e8b9bdba4d02db41
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-02 21:19:59 +0000

    pocketbase: update 0.34.1 bottle.

--- Formula/p/pocketbase.rb
+++ Formula/p/pocketbase.rb
@@ -6,12 +6,12 @@ class Pocketbase < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8fbc61111be8dcbac58746bc648e978ad1f16657dd01b880005a939dc420325f"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8fbc61111be8dcbac58746bc648e978ad1f16657dd01b880005a939dc420325f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8fbc61111be8dcbac58746bc648e978ad1f16657dd01b880005a939dc420325f"
-    sha256 cellar: :any_skip_relocation, sonoma:        "8e48c628d505e3b67356d8ecad442c9349ee96d6c5a6e20e59f47321d2a0f986"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "38396f6d1516dc7c38dcc5afaca2b71d76e832992fd9102888e5e93943a459fc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7dde87080c0e04b9ebcce201550d8b5d29788f13f240b9deb79f37125606035d"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "74640b7ccacccbc28bc9b2eb082cba90d72e4ad154628cf1d762fcede0ee7e67"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "74640b7ccacccbc28bc9b2eb082cba90d72e4ad154628cf1d762fcede0ee7e67"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "74640b7ccacccbc28bc9b2eb082cba90d72e4ad154628cf1d762fcede0ee7e67"
+    sha256 cellar: :any_skip_relocation, sonoma:        "9351b85f7b5d28f5eb4a464ad71297471a6040edf43a4aa13c253a84fec984ea"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "31440c6bb241f75a0377392a3f77367903dae078f0ec9878b4898c022f7ffeea"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "684c17674502625f1a4430b67ddf3b7d2083900b8e2c896434193e1c13bc0fe7"
   end
 
   depends_on "go" => :build
