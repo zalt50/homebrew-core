fcb707a1cf6c2d710ac00936b074e04104c62637
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-03 11:10:42 +0000

    bumpp: add 10.3.2 bottle.

--- Formula/b/bumpp.rb
+++ Formula/b/bumpp.rb
@@ -5,6 +5,10 @@ class Bumpp < Formula
   sha256 "ed7fa2dd6b98e51fac498aa8990dc570dad81504dcc713f677874490cb24da8c"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "feee8f6cd971b963f615a3ff44ffe98b692213da7f1ab5d44b662448819f8a25"
+  end
+
   depends_on "node"
 
   def install
