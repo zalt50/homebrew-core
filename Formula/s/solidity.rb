d0b4e6ecd377b707f01e535b25a47f553196d2f3
Matheus Aguiar <matheus.pit@gmail.com>
2025-12-03 11:31:11 -0300

    solidity 0.8.31

--- Formula/s/solidity.rb
+++ Formula/s/solidity.rb
@@ -1,10 +1,9 @@
 class Solidity < Formula
   desc "Contract-oriented programming language"
   homepage "https://soliditylang.org"
-  url "https://github.com/ethereum/solidity/releases/download/v0.8.30/solidity_0.8.30.tar.gz"
-  sha256 "5e8d58dff551a18205e325c22f1a3b194058efbdc128853afd75d31b0568216d"
+  url "https://github.com/argotorg/solidity/releases/download/v0.8.31/solidity_0.8.31.tar.gz"
+  sha256 "1efcf5af92e39499ce64d9cb33ba1cc1aa43d0aba107472915d732bf4a31c837"
   license all_of: ["GPL-3.0-or-later", "MIT", "BSD-3-Clause", "Apache-2.0", "CC0-1.0"]
-  revision 1
 
   livecheck do
     url :stable
@@ -32,12 +31,6 @@ class Solidity < Formula
   depends_on "z3"
 
   conflicts_with "solc-select", because: "both install `solc` binaries"
-
-  # Fix build with Boost 1.89.0, pr ref: https://github.com/ethereum/solidity/pull/16163
-  patch do
-    url "https://github.com/ethereum/solidity/commit/1c6000917619c69feaa9fd14fe69c0445cc05f20.patch?full_index=1"
-    sha256 "bf839570085ccd9baa227f30f91456f3ff72e9754d63019d33b34449bbb4c34e"
-  end
 
   def install
     rm_r("deps")
