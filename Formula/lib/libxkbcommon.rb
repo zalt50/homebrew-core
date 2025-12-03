4536fe8e5959efc6a1803ee7ce4b85273b099609
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-03 17:12:58 +0000

    libxkbcommon 1.13.1

--- Formula/lib/libxkbcommon.rb
+++ Formula/lib/libxkbcommon.rb
@@ -1,8 +1,8 @@
 class Libxkbcommon < Formula
   desc "Keyboard handling library"
   homepage "https://xkbcommon.org/"
-  url "https://github.com/xkbcommon/libxkbcommon/archive/refs/tags/xkbcommon-1.13.0.tar.gz"
-  sha256 "cd9367eec501867dfe7ddc3f6b18a026f2a2844a89d19108448d376cb849c9ed"
+  url "https://github.com/xkbcommon/libxkbcommon/archive/refs/tags/xkbcommon-1.13.1.tar.gz"
+  sha256 "aeb951964c2f7ecc08174cb5517962d157595e9e3f38fc4a130b91dc2f9fec18"
   license "MIT"
   head "https://github.com/xkbcommon/libxkbcommon.git", branch: "master"
 
