f85e236422d5bb8d3b9e9fb649dceffbec14cdbc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-03 08:02:39 +0000

    iguana 1.1.0

--- Formula/i/iguana.rb
+++ Formula/i/iguana.rb
@@ -1,8 +1,8 @@
 class Iguana < Formula
   desc "Universal serialization engine"
   homepage "https://github.com/qicosmos/iguana"
-  url "https://github.com/qicosmos/iguana/archive/refs/tags/1.0.9.tar.gz"
-  sha256 "b6e3f11a0c37538e84e25397565f5f12b0e6810e582bce7f3ca046425b0b1edf"
+  url "https://github.com/qicosmos/iguana/archive/refs/tags/1.1.0.tar.gz"
+  sha256 "86bf230b5ccea629ad806e4b007d6dcd2e9f0cba8a477c123c920ee0e3329abf"
   license "Apache-2.0"
   head "https://github.com/qicosmos/iguana.git", branch: "master"
 
