2f08d5542bea46ecd1b8b1f86c929bac32dd6d3b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-04 05:28:35 +0000

    pwned 13.1.1

--- Formula/p/pwned.rb
+++ Formula/p/pwned.rb
@@ -1,8 +1,8 @@
 class Pwned < Formula
   desc "CLI for the 'Have I been pwned?' service"
   homepage "https://github.com/wKovacs64/pwned"
-  url "https://registry.npmjs.org/pwned/-/pwned-13.1.0.tgz"
-  sha256 "66382d10e91e06bb0b372b6a10312eb348c42ac45b39eaaa0af0382d765b4216"
+  url "https://registry.npmjs.org/pwned/-/pwned-13.1.1.tgz"
+  sha256 "b374ccb2a737166cf623d53e9c2fc146055bb59eb318283df0493bd35839ce6c"
   license "MIT"
 
   bottle do
