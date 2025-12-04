18212809af02b98104a5c4ff0081c22dd990d6a9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-04 11:36:10 +0000

    retire 5.4.0

--- Formula/r/retire.rb
+++ Formula/r/retire.rb
@@ -1,8 +1,8 @@
 class Retire < Formula
   desc "Scanner detecting the use of JavaScript libraries with known vulnerabilities"
   homepage "https://retirejs.github.io/retire.js/"
-  url "https://registry.npmjs.org/retire/-/retire-5.3.0.tgz"
-  sha256 "e7b322111bec8b80b45b94d3276f4fe2e5ec986b835ee9c46d05d2a2de06a9b3"
+  url "https://registry.npmjs.org/retire/-/retire-5.4.0.tgz"
+  sha256 "b72eb1e11c5928c32096b3535d3e388f4f39de876aaf26cf8de97888d54e01f6"
   license "Apache-2.0"
 
   bottle do
