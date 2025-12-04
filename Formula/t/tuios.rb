bb9d4cc38184c0a3ff6094409b54e3d5615dc5e9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-04 05:36:51 +0000

    tuios 0.4.3

--- Formula/t/tuios.rb
+++ Formula/t/tuios.rb
@@ -1,8 +1,8 @@
 class Tuios < Formula
   desc "Terminal UI OS (Terminal Multiplexer)"
   homepage "https://github.com/Gaurav-Gosain/tuios"
-  url "https://github.com/Gaurav-Gosain/tuios/archive/refs/tags/v0.4.2.tar.gz"
-  sha256 "7540f754acf80d1be9adcdc1b4db18b5db18086fd979465bfb3032d448c0351a"
+  url "https://github.com/Gaurav-Gosain/tuios/archive/refs/tags/v0.4.3.tar.gz"
+  sha256 "be365d6a7568193655f2d049b2f2f95c8f27fd86bc3e6d15dca9429631ab5d12"
   license "MIT"
   head "https://github.com/Gaurav-Gosain/tuios.git", branch: "main"
 
