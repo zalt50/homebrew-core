40b73acdcede009568495a59c7290bbf11276fe0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-04 11:25:40 +0000

    mjml 4.17.2

--- Formula/m/mjml.rb
+++ Formula/m/mjml.rb
@@ -1,19 +1,18 @@
 class Mjml < Formula
   desc "JavaScript framework that makes responsive-email easy"
   homepage "https://mjml.io"
-  url "https://registry.npmjs.org/mjml/-/mjml-4.17.1.tgz"
-  sha256 "0867bdcfefa691a3c0838eba91e76b2d836aeafe058e12e86d2381ffc84f9e68"
+  url "https://registry.npmjs.org/mjml/-/mjml-4.17.2.tgz"
+  sha256 "28a0ebae7bcf5b094b44ae00bdce8a7de43f0098fff976a889d755f775270f12"
   license "MIT"
-
-  bottle do
-    sha256 cellar: :any_skip_relocation, all: "d64e58eb4ff897f191a297bcee932770cb9baf165854c3fad44b89697676f318"
-  end
 
   depends_on "node"
 
   def install
     system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
+
+    node_modules = libexec/"lib/node_modules/mjml/node_modules"
+    deuniversalize_machos node_modules/"fsevents/fsevents.node" if OS.mac?
   end
 
   test do
