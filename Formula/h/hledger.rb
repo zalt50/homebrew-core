05af47d6ebe43b69dc5414390ea2a649ca33689f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-05 05:05:17 +0000

    hledger: update 1.50.4 bottle.

--- Formula/h/hledger.rb
+++ Formula/h/hledger.rb
@@ -15,13 +15,12 @@ class Hledger < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_tahoe:   "5a8e267d01f4a511cb6018341b78371f37cc67098274fb014992fd386b4b1e41"
-    sha256 cellar: :any,                 arm64_sequoia: "8e9c08d2e0f72d92a6d0fc2254d3cc4d9a6b99cb4603e192f5d16a0293820ddb"
-    sha256 cellar: :any,                 arm64_sonoma:  "21f374782166e3ac50a148c3bf43398d503bf2207d34fb4718283237a16ef7db"
-    sha256 cellar: :any,                 sonoma:        "837bc34c4943b2c8b208feef7c9828b5634a5712a39c20551282760a34879d8e"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d48fcadfa35ab1c701eff1ae95e3c3cb314459811f1a049883db7868fcacdeae"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c00be0968a21e308a5df650b40ae7eb629a4337dcad34cfc942a475f28fea2c4"
+    sha256 cellar: :any,                 arm64_tahoe:   "6dff0cbd1fdfcb2bb2db88506c70fce9daa72f08bc2ff398eba470e869d4fb88"
+    sha256 cellar: :any,                 arm64_sequoia: "389a0c69a63f3f904b7012c9ff5c7673c2782a625a9c41e7150c5fcd52cdccff"
+    sha256 cellar: :any,                 arm64_sonoma:  "a309ca602adb98d1efaad0f7c5b428776d55f413654b13717ed47cf1064da67c"
+    sha256 cellar: :any,                 sonoma:        "7d2440f3cf95172dd45fe71bed7c36bec77e5efb6987ab716c6325f9d122b6da"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3f281762c39d77ab34945839d2e9634772dac8ec7a3cf442f85ff7c7e70bc075"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b2e4f5cbc266c52b97bc572aafc3c554cb9ef067fecfccfae4ab48b7ba1a40e6"
   end
 
   depends_on "ghc" => :build
