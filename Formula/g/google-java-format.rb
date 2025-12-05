194c34f8d9543672f524b0f8b372c53a0f053ac1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-05 11:05:40 +0000

    google-java-format 1.33.0

--- Formula/g/google-java-format.rb
+++ Formula/g/google-java-format.rb
@@ -3,8 +3,8 @@ class GoogleJavaFormat < Formula
 
   desc "Reformats Java source code to comply with Google Java Style"
   homepage "https://github.com/google/google-java-format"
-  url "https://github.com/google/google-java-format/releases/download/v1.32.0/google-java-format-1.32.0-all-deps.jar"
-  sha256 "b8efc4a512242392aaed8fb6d5baa5373e7050190f63dd05c67b8ec255c017ff"
+  url "https://github.com/google/google-java-format/releases/download/v1.33.0/google-java-format-1.33.0-all-deps.jar"
+  sha256 "697707af07c7753f29cba415c6a76b7882702ff464f807da98b28069b8751910"
   license "Apache-2.0"
 
   bottle do
@@ -16,7 +16,7 @@ class GoogleJavaFormat < Formula
   uses_from_macos "python"
 
   resource "google-java-format-diff" do
-    url "https://raw.githubusercontent.com/google/google-java-format/v1.32.0/scripts/google-java-format-diff.py"
+    url "https://raw.githubusercontent.com/google/google-java-format/v1.33.0/scripts/google-java-format-diff.py"
     sha256 "c1f2c6e8af0fc34a04adfcb01b35e522a359df5da1f5db5102ca9e0ca1f670fd"
 
     livecheck do
