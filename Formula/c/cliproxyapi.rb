dd06be9c9fb22245d64dab1cbdec5aa4e3d9d2d2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-05 20:44:13 +0000

    cliproxyapi: update 6.5.48 bottle.

--- Formula/c/cliproxyapi.rb
+++ Formula/c/cliproxyapi.rb
@@ -6,12 +6,12 @@ class Cliproxyapi < Formula
   license "MIT"
 
   bottle do
-    sha256                               arm64_tahoe:   "f56563d1f1f6e2b5afe7bc5667220bccbedfacb7e34a10840216f2685c96e469"
-    sha256                               arm64_sequoia: "f56563d1f1f6e2b5afe7bc5667220bccbedfacb7e34a10840216f2685c96e469"
-    sha256                               arm64_sonoma:  "f56563d1f1f6e2b5afe7bc5667220bccbedfacb7e34a10840216f2685c96e469"
-    sha256 cellar: :any_skip_relocation, sonoma:        "0ebf48dd9b5cb88cf25f75200ff77efa18ba43cc70546feca62476428407ee20"
-    sha256                               arm64_linux:   "139d591eda6a209ae7c2cd6626df15068cdccdd4dbb1fab6f48de76131d4edeb"
-    sha256                               x86_64_linux:  "3708591227fb19452fda644fb7a62a8a1bb3e1e985d912a2b95ade369085a1af"
+    sha256                               arm64_tahoe:   "b2427944424ffe2df29951e5a4c97c1a8bab69d636f88a06dd8aa63fe3c5c185"
+    sha256                               arm64_sequoia: "b2427944424ffe2df29951e5a4c97c1a8bab69d636f88a06dd8aa63fe3c5c185"
+    sha256                               arm64_sonoma:  "b2427944424ffe2df29951e5a4c97c1a8bab69d636f88a06dd8aa63fe3c5c185"
+    sha256 cellar: :any_skip_relocation, sonoma:        "c951bb7f18e1080c22b17613d711428ed8454ee3b8cdb5c45be34a11ff1c71c8"
+    sha256                               arm64_linux:   "c0e97668607e97cb0c3b407c6145326dbe7149e04237cc138720b08c2d22a0f4"
+    sha256                               x86_64_linux:  "9d2c5035c6c7ffc3fd1866dd6e4f703116c3042923f56a21bbb3b12b29c7754f"
   end
 
   depends_on "go" => :build
