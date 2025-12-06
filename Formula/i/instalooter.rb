197993ce7bddaabd5b9c1c09e50758857fbf5282
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-06 04:27:04 +0000

    instalooter: update 2.4.4_14 bottle.

--- Formula/i/instalooter.rb
+++ Formula/i/instalooter.rb
@@ -9,13 +9,12 @@ class Instalooter < Formula
   revision 14
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6d445c1163d785b9320400d42d28c9056cd32d12d2aa0346dda30ecdf7ade1d7"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6d445c1163d785b9320400d42d28c9056cd32d12d2aa0346dda30ecdf7ade1d7"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6d445c1163d785b9320400d42d28c9056cd32d12d2aa0346dda30ecdf7ade1d7"
-    sha256 cellar: :any_skip_relocation, sonoma:        "181ac15b7d74d251f2101e3d5c1fb57d196f470bbe37653a86b60dbd2251d5ec"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "181ac15b7d74d251f2101e3d5c1fb57d196f470bbe37653a86b60dbd2251d5ec"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "181ac15b7d74d251f2101e3d5c1fb57d196f470bbe37653a86b60dbd2251d5ec"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "50d75e25afec7336088d8b88f1b5cec4a397451e8d61da048de4f0bdc694d68c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "50d75e25afec7336088d8b88f1b5cec4a397451e8d61da048de4f0bdc694d68c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "50d75e25afec7336088d8b88f1b5cec4a397451e8d61da048de4f0bdc694d68c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "af3d2b5811fb8eaec6504318825fd346d07d207efa2e17984e52a6c5e39ecc9b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "af3d2b5811fb8eaec6504318825fd346d07d207efa2e17984e52a6c5e39ecc9b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "af3d2b5811fb8eaec6504318825fd346d07d207efa2e17984e52a6c5e39ecc9b"
   end
 
   depends_on "certifi" => :no_linkage
