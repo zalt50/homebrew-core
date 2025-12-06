2449367fc57430922a3ad8824bacf6ead5028a9d
botantony <antonsm21@gmail.com>
2025-12-06 04:51:36 +0100

    fastnetmon: revision bump (protobuf 33.2)

--- Formula/f/fastnetmon.rb
+++ Formula/f/fastnetmon.rb
@@ -4,7 +4,7 @@ class Fastnetmon < Formula
   url "https://github.com/pavel-odintsov/fastnetmon/archive/refs/tags/v1.2.8.tar.gz"
   sha256 "d16901b00963f395241c818d02ad2751f14e33fd32ed3cb3011641ab680e0d01"
   license "GPL-2.0-only"
-  revision 18
+  revision 19
 
   bottle do
     sha256 cellar: :any, arm64_tahoe:   "d195bdcd05c4c55fd7adadb3ffbce89789856b4b6c6c62fbc3b8566e1e3662cc"
