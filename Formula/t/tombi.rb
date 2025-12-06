7e186ea570e360e2950844e3f185321063a21097
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-06 02:59:49 +0000

    tombi 0.7.1

--- Formula/t/tombi.rb
+++ Formula/t/tombi.rb
@@ -1,8 +1,8 @@
 class Tombi < Formula
   desc "TOML formatter, linter and language server"
   homepage "https://github.com/tombi-toml/tombi"
-  url "https://github.com/tombi-toml/tombi/archive/refs/tags/v0.7.0.tar.gz"
-  sha256 "929065d6b56869ae506673ff04b7edb217b7e6e844bec6fdb454a8f96181b63d"
+  url "https://github.com/tombi-toml/tombi/archive/refs/tags/v0.7.1.tar.gz"
+  sha256 "f4cc5a7e6143bd5bb894cfdf5b8708a2de82f8240e03f4dd1badcc5f6eb5034d"
   license "MIT"
   head "https://github.com/tombi-toml/tombi.git", branch: "main"
 
