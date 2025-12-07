48b8bdc1e11f6bfe4d9a98e5f7c24a3a5916a132
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-07 20:59:43 +0000

    redu: add 0.2.14 bottle.

--- Formula/r/redu.rb
+++ Formula/r/redu.rb
@@ -6,6 +6,15 @@ class Redu < Formula
   license "MIT"
   head "https://github.com/drdo/redu.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5ab87218064df0d994b2bdd1c95004ff0f9332b12387320b5376d58c3d55ceb9"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "158b7091134e0e26454553a74f9d09da312280635e17a0269879f0efa4cb757a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e35f33a54ee2f8b77879f0df0dc6d52e4569418e8d1012dedb82f48439bc8dbe"
+    sha256 cellar: :any_skip_relocation, sonoma:        "1f2ff2f7475f615f8d3b948395e267f759e52497549ff0e13678012d8e374cfa"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "19e54207d47b900777fdd9ea74fd13a3245af0a16409db0d4e1b96a631684491"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2605c8a58b070ff549446acbb804998d9e2dc2a56ca3b56178e8dfe66d0502b9"
+  end
+
   depends_on "rust" => :build
 
   def install
