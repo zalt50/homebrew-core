90eb18b1a8b81e3be1fdca22d33922814db7b59c
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-07 17:04:17 +0900

    cmix 21

--- Formula/c/cmix.rb
+++ Formula/c/cmix.rb
@@ -1,11 +1,25 @@
 class Cmix < Formula
   desc "Data compression program with high compression ratio"
   homepage "https://www.byronknoll.com/cmix.html"
-  url "https://github.com/byronknoll/cmix/archive/refs/tags/v20.tar.gz"
-  sha256 "a95b0d7430d61b558731e7627f41e170cb7802d1a8a862f38628f8d921dc61b2"
   license "GPL-3.0-or-later"
+  head "https://github.com/byronknoll/cmix.git", branch: "master"
 
-  no_autobump! because: :requires_manual_review
+  stable do
+    url "https://github.com/byronknoll/cmix/archive/refs/tags/v21.tar.gz"
+    sha256 "c0ff50f24604121bd7ccb843045c0946db1077cfb9ded10fe4c181883e6dbb42"
+
+    # patch makefile, upstream pr ref, https://github.com/byronknoll/cmix/pull/69
+    patch do
+      url "https://github.com/byronknoll/cmix/commit/702022a974cbf7906bcbaed898f1de95d3cbb32d.patch?full_index=1"
+      sha256 "62143fadb5dda1024b0d51c1bb86263eb15d842193e02550a65924b3ac86c28a"
+    end
+
+    # Workaround for the error: "This header is only meant to be used on x86 and x64 architecture"
+    patch do
+      url "https://github.com/byronknoll/cmix/commit/51c8f57570e4c1eb08056f929a96b3101c0156bb.patch?full_index=1"
+      sha256 "c199390a27bce681e42ac23c8adfaa7261d4ec11fd76f14f9aab00dc629c2d33"
+    end
+  end
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_tahoe:    "087b737dfd467bda8b0ce62227d8bf0946754ee55507130a0cd84ddbdf3b42c3"
@@ -20,8 +34,15 @@ class Cmix < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "0092c27accca6642f12d8c0d79423e5722dd17cbd20a93c194f231999d0e8655"
   end
 
+  # Fix to error: unknown type name '__m128i' on intel architectures
+  # PR ref: https://github.com/byronknoll/cmix/pull/74
+  patch do
+    url "https://github.com/byronknoll/cmix/commit/b5b77acd112985cf8577ec01910c74fb70c98f36.patch?full_index=1"
+    sha256 "e9ea39d1d343bd5bc59de497e899d6e124e4d2aa8768e8d0a7fe47ff7a80dc38"
+  end
+
   def install
-    system "make"
+    system "make", "CXX=#{ENV.cxx}"
     bin.install "cmix"
   end
 
