5579609c70efefbad226e52ee150baebfd396ae0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-07 20:57:28 +0000

    carl: update 0.5.0 bottle.

--- Formula/c/carl.rb
+++ Formula/c/carl.rb
@@ -7,12 +7,12 @@ class Carl < Formula
   head "https://github.com/b1rger/carl.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "67619db42f304f3324f52af71184895920b2d14d7d0bc3ddcf9a5410f6a69284"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "aacff1cdafd7b3666449af76e70495fbbbb0cea021befee5d9a174c8ca966173"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "946ecb3c5558effa565bb0171e54b2f4fbdfffe4bb856b7494645ed5b50d1231"
-    sha256 cellar: :any_skip_relocation, sonoma:        "4f3751b7351e9dac043acdb36a57c6b171a93a2a84ba6c472a4ed5b85ed18624"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a427118fd864b19041afd8567e9bc06154c859527eae71ef59de1d6ff133a970"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d3e8fd70a8d03dc9f9f97835b51d3970a19890c67e09509acb563713f2940b4a"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "fafc29ae2ab8e50cc0f7d547010cf7fa7c3a02fa69bb96f5a895afda858e5077"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a24e089317286996916a752bbe9db465f4be90e7e266f0caa35635982086d9e2"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9be8efd8f1f3a08c2cd598eef4014d479c0884befdd20720a1f0a39355a61013"
+    sha256 cellar: :any_skip_relocation, sonoma:        "b3bf67b636554ec5e2d7dd4d56d299ca46f04bc402af5cfba091be09769d00a0"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "5836b6a32a188ff23e42be42af8773a984e7aa9cd6404d2b64c876d45d2ed24b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8a4d985cb1d18aaaabe14401811aafa79245f504a816ec200b09600d647fabcb"
   end
 
   depends_on "rust" => :build
