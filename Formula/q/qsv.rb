229ec71c1efe0a77a43af64c9c566bc6eba9d28f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-08 14:19:17 +0000

    qsv 11.0.2

--- Formula/q/qsv.rb
+++ Formula/q/qsv.rb
@@ -1,8 +1,8 @@
 class Qsv < Formula
   desc "Ultra-fast CSV data-wrangling toolkit"
   homepage "https://qsv.dathere.com/"
-  url "https://github.com/dathere/qsv/archive/refs/tags/10.0.0.tar.gz"
-  sha256 "7c263a9ec84e97fa8879698f6087f76ab97a1ad4b5a7794afd61d9c31c684949"
+  url "https://github.com/dathere/qsv/archive/refs/tags/11.0.2.tar.gz"
+  sha256 "239838d64ed956be64004beb38d382af3282d2b76e19c6de15b669292be00ca2"
   license any_of: ["MIT", "Unlicense"]
   head "https://github.com/dathere/qsv.git", branch: "master"
 
@@ -46,9 +46,9 @@ class Qsv < Formula
   test do
     (testpath/"test.csv").write("first header,second header")
     assert_equal <<~EOS, shell_output("#{bin}/qsv stats test.csv")
-      field,type,is_ascii,sum,min,max,range,sort_order,sortiness,min_length,max_length,sum_length,avg_length,stddev_length,variance_length,cv_length,mean,sem,geometric_mean,harmonic_mean,stddev,variance,cv,nullcount,max_precision,sparsity
-      first header,NULL,,,,,,,,,,,,,,,,,,,,,,0,,
-      second header,NULL,,,,,,,,,,,,,,,,,,,,,,0,,
+      field,type,is_ascii,sum,min,max,range,sort_order,sortiness,min_length,max_length,sum_length,avg_length,stddev_length,variance_length,cv_length,mean,sem,geometric_mean,harmonic_mean,stddev,variance,cv,nullcount,n_negative,n_zero,n_positive,max_precision,sparsity
+      first header,NULL,,,,,,,,,,,,,,,,,,,,,,0,,,,,
+      second header,NULL,,,,,,,,,,,,,,,,,,,,,,0,,,,,
     EOS
   end
 end
