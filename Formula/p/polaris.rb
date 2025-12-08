f684ba41c08baaa5471c57b14b43bc15ce52597c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-08 17:22:00 +0000

    polaris 10.1.3

--- Formula/p/polaris.rb
+++ Formula/p/polaris.rb
@@ -1,8 +1,8 @@
 class Polaris < Formula
   desc "Validation of best practices in your Kubernetes clusters"
   homepage "https://www.fairwinds.com/polaris"
-  url "https://github.com/FairwindsOps/polaris/archive/refs/tags/10.1.2.tar.gz"
-  sha256 "16897c2e419b3ad28a0330113b4a1ed3ad2ced376e0b2e7c7bb92ccb88bfdea5"
+  url "https://github.com/FairwindsOps/polaris/archive/refs/tags/10.1.3.tar.gz"
+  sha256 "eaa15224739aea756f67ae84a2e086ec1f6716539245ed37572c064d3d598f46"
   license "Apache-2.0"
   head "https://github.com/FairwindsOps/polaris.git", branch: "master"
 
