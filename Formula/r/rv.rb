b8dce17bf20525357448ac5f25ae134320928b3a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-08 05:16:40 +0000

    rv 0.3.0

--- Formula/r/rv.rb
+++ Formula/r/rv.rb
@@ -1,8 +1,8 @@
 class Rv < Formula
   desc "Ruby version manager"
   homepage "https://github.com/spinel-coop/rv"
-  url "https://github.com/spinel-coop/rv/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "828048689c60ed0d5e4d9a27fcd643821f5f2acf4b782f447f5b0b17904adee1"
+  url "https://github.com/spinel-coop/rv/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "3fe6617b49cba218ae6133f1bdf64008bac88ddbf94c13d24014716ea65012fc"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/spinel-coop/rv.git", branch: "main"
 
