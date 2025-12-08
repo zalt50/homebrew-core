77081264c24d1d9245761fcb0f95ee8beee4ea02
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-08 13:22:47 +0000

    mas: update 4.1.0 bottle.

--- Formula/m/mas.rb
+++ Formula/m/mas.rb
@@ -15,10 +15,10 @@ class Mas < Formula
   no_autobump! because: :requires_manual_review
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "acf4efa41ce9fc523c1abe24298b2194ce633b9790bc89210b5c81e7dfb11cc6"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "44b12eaa7725b628e9d14ef0d03df4a5580225d14da86c623159ff539a0d6cb1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "aede8a124427ccbe1103b89de25c62c2409272e874eb6f9bb6def619c3bd37c8"
-    sha256 cellar: :any_skip_relocation, sonoma:        "b9005575e04c459782434b97e9183b090cbf0da2a494d5a5d2a6704b0c3493d1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "12304836d631b809d92158bbd29619dbd3f7343da190e4e086481fd8b7c59182"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "db78583f34bebbaf021176e05715ab62c5dd3d96751fe3076f3d60d4cb7b04eb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d8cbb5cf3f53fac08e59ecefa60a4859d520a9a289f53f2faae26edcb9eda611"
+    sha256 cellar: :any_skip_relocation, sonoma:        "c267f3e0476ba3bdb4fdd21e266c876b607037c2be4a1d52eb5facbeaca78a9b"
   end
 
   depends_on xcode: ["16.0", :build]
