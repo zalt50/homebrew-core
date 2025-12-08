e0541fb6c4f5f9d0accc852306e219dd8093b586
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-08 10:07:38 +0000

    mq: add 0.5.5 bottle.

--- Formula/m/mq.rb
+++ Formula/m/mq.rb
@@ -6,6 +6,15 @@ class Mq < Formula
   license "MIT"
   head "https://github.com/harehare/mq.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "53c3c475d27b97bc272f922e45cd92585de08d0ce137ff8e16fab0bdea18848c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "47e67854c432382998ea7ed93a58af7f9805746596ae1fb19c68e9d643cb7a3a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c73f6d0bb8a3904b02d5ffd61cdd716ddbbc8097b42dd4de43a7ba2fa2647ebb"
+    sha256 cellar: :any_skip_relocation, sonoma:        "7621825135fdb51b17dd8a1e23b0c881e8193daf4bf8efc2f702b4dea3254c72"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "b68ee537f0ddb04bb3240c179bb0731c4282468b5841fc646195788a34a0e871"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8479c47dc80d33be680f9d4abd7e16d92b67146c009be40527ff3096c3816cdb"
+  end
+
   depends_on "rust" => :build
 
   def install
