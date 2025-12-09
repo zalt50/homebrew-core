5973d8f0b39774e0b466e0505a5e099e04b132e1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-09 06:48:26 +0000

    git-pages-cli: add 1.5.1 bottle.

--- Formula/g/git-pages-cli.rb
+++ Formula/g/git-pages-cli.rb
@@ -6,6 +6,15 @@ class GitPagesCli < Formula
   license "0BSD"
   head "https://codeberg.org/git-pages/git-pages-cli.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b57bf1db4a6904066cfeff95489d237ec691d5dfaa9af45cd4a5ec5623c107f8"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b57bf1db4a6904066cfeff95489d237ec691d5dfaa9af45cd4a5ec5623c107f8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b57bf1db4a6904066cfeff95489d237ec691d5dfaa9af45cd4a5ec5623c107f8"
+    sha256 cellar: :any_skip_relocation, sonoma:        "4c8c8b9aa133bf138469ee799d4e040565a08b1f53285c7fbf49dab9b59733f0"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "7184e911920c6d74d6d61b19673ad4307f5ec19c71bb94c41d93d87cba3f3256"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "856e2ab8c6a471595bb45d4b0602f0419d3edc2579eb65438f518c08f2b903db"
+  end
+
   depends_on "go" => :build
 
   def install
