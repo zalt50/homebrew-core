9807657ee8ece3e391515ff3394c2393c2ff1cea
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-10 11:44:28 +0000

    nanobind: update 2.10.2 bottle.

--- Formula/n/nanobind.rb
+++ Formula/n/nanobind.rb
@@ -7,7 +7,7 @@ class Nanobind < Formula
   head "https://github.com/wjakob/nanobind.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "3a104f318e0d02418332f017a5f18b51f69c12629182d1d35928b1bc84df4269"
+    sha256 cellar: :any_skip_relocation, all: "e19a2b37eb28cbf326cfe3ed89574e93f7cc60560e3f8288565c106242eca310"
   end
 
   depends_on "cmake" => [:build, :test]
