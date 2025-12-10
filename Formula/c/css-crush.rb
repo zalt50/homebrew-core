a1f9e113f41d2cf44ce46073babe2f1c04318928
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-10 10:58:56 +0000

    css-crush 5.0.1

--- Formula/c/css-crush.rb
+++ Formula/c/css-crush.rb
@@ -1,8 +1,8 @@
 class CssCrush < Formula
   desc "Extensible PHP based CSS preprocessor"
   homepage "https://the-echoplex.net/csscrush"
-  url "https://github.com/peteboere/css-crush/archive/refs/tags/v5.0.0.tar.gz"
-  sha256 "4fde4e991fa64e97f28c796c2267e155bca9de963713cc20d93c1618fd5285b6"
+  url "https://github.com/peteboere/css-crush/archive/refs/tags/v5.0.1.tar.gz"
+  sha256 "98b93ae9acb1586a1bdefeb377065206f62cb5f00f3ac065b65dff9dc5d0a979"
   license "MIT"
   head "https://github.com/peteboere/css-crush.git", branch: "master"
 
