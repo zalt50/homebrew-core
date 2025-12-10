700e2594532a03bac6f1de0c96eff1e3932eab3a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-10 20:25:49 +0000

    rainfrog 0.3.11

--- Formula/r/rainfrog.rb
+++ Formula/r/rainfrog.rb
@@ -1,8 +1,8 @@
 class Rainfrog < Formula
   desc "Database management TUI for PostgreSQL/MySQL/SQLite"
   homepage "https://github.com/achristmascarl/rainfrog"
-  url "https://github.com/achristmascarl/rainfrog/archive/refs/tags/v0.3.10.tar.gz"
-  sha256 "0b0717df943d8fe49a0b27cb57f1e741fcbcaa801fc37870762c4403533b09a4"
+  url "https://github.com/achristmascarl/rainfrog/archive/refs/tags/v0.3.11.tar.gz"
+  sha256 "3ec2612779499e152b137a4a78fcb46b28c22973ee171ca494e3b5ecd8dd4063"
   license "MIT"
   head "https://github.com/achristmascarl/rainfrog.git", branch: "main"
 
