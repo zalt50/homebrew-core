e00b0104ee3612e5413706f5b31daddb02b8eeac
Michael Cho <michael@michaelcho.dev>
2025-12-10 22:36:35 -0500

    sfcgal: revision bump (boost 1.90.0)

--- Formula/s/sfcgal.rb
+++ Formula/s/sfcgal.rb
@@ -4,7 +4,7 @@ class Sfcgal < Formula
   url "https://gitlab.com/sfcgal/SFCGAL/-/archive/v2.2.0/SFCGAL-v2.2.0.tar.gz"
   sha256 "bb6bb77ddb58523d8c229764de23699f99c1a7011d873419afd2a67df85602a2"
   license "LGPL-2.0-or-later"
-  revision 1
+  revision 2
 
   bottle do
     sha256 cellar: :any,                 arm64_tahoe:   "96e20b32b551e70b2ada85525d29c9eacf71c7bd9801a596a9fc69aed2a11ccf"
