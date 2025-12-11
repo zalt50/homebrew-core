97d30896797fd007e83608ad61b60679bb990bdc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-11 16:57:50 +0000

    aws-cdk 2.1034.0
    aws-cdk: deuniversalize binaries
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    
    aws-cdk: remove `:all` bottle
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Formula/a/aws-cdk.rb
+++ Formula/a/aws-cdk.rb
@@ -1,13 +1,9 @@
 class AwsCdk < Formula
   desc "AWS Cloud Development Kit - framework for defining AWS infra as code"
   homepage "https://github.com/aws/aws-cdk"
-  url "https://registry.npmjs.org/aws-cdk/-/aws-cdk-2.1033.0.tgz"
-  sha256 "95f1274d21d858d95582d0a3ceaeab98713be462eabe653de1c6617fc9d3c49e"
+  url "https://registry.npmjs.org/aws-cdk/-/aws-cdk-2.1034.0.tgz"
+  sha256 "0715bfdd9cce943d89a3e306543e2215bf1947b3c565945ee8cb4c175eaa0117"
   license "Apache-2.0"
-
-  bottle do
-    sha256 cellar: :any_skip_relocation, all: "9a642e782f7183be5b32b6a8950cb00bfaabb5a97a318d95bff9eb4c14918977"
-  end
 
   depends_on "node"
 
@@ -15,8 +11,8 @@ class AwsCdk < Formula
     system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
 
-    # Remove empty `node_modules` directory on macOS
-    rmdir libexec/"lib/node_modules/aws-cdk/node_modules" if OS.mac?
+    node_modules = libexec/"lib/node_modules/aws-cdk/node_modules"
+    deuniversalize_machos node_modules/"fsevents/fsevents.node" if OS.mac?
   end
 
   test do
