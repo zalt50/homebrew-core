08fd1ce173e323f0a11f4e06acda1356f4d1e564
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-12 22:24:36 +0000

    jump: update 0.67.0 bottle.

--- Formula/j/jump.rb
+++ Formula/j/jump.rb
@@ -7,15 +7,12 @@ class Jump < Formula
   head "https://github.com/gsamokovarov/jump.git", branch: "main"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d618b1643851a787269a79deabe1e1085c058b1f1c7738169b8e8ed05b9acef6"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ecddcb16acf0ec0cfd882ba188e731b1a82afc22aa30131f15331ccea1135ece"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ecddcb16acf0ec0cfd882ba188e731b1a82afc22aa30131f15331ccea1135ece"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "ecddcb16acf0ec0cfd882ba188e731b1a82afc22aa30131f15331ccea1135ece"
-    sha256 cellar: :any_skip_relocation, sonoma:        "3a6bef70aca4c86f24e680096a7137ebba478b1420acca74e131987f3fdb2fc8"
-    sha256 cellar: :any_skip_relocation, ventura:       "3a6bef70aca4c86f24e680096a7137ebba478b1420acca74e131987f3fdb2fc8"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "705352accfdad579ce325bda4651dc9190a67437b026fd20f07d67e9fa0a23fc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5db113fb8a6cb2652adb039f6cc0056341c95df09201ead5d5dd306e2c7f5921"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "748682eed44f56971f34905be8df1497ac2db7e407ca0a6e3f5b2b0e2aa9eff4"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "748682eed44f56971f34905be8df1497ac2db7e407ca0a6e3f5b2b0e2aa9eff4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "748682eed44f56971f34905be8df1497ac2db7e407ca0a6e3f5b2b0e2aa9eff4"
+    sha256 cellar: :any_skip_relocation, sonoma:        "c968580a05c97faac5046d4e03aa8faf2bc89008db64ff81e83b55cefaea2029"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "7d762b4bcfa955462e651c27b6781955bdc6d82f80f5975abfb0ba0421e3009a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cb2c9042ebf1cdd8b15dcfc1f541e862917588fbfed90a40f5a60b69ed726d65"
   end
 
   depends_on "go" => :build
