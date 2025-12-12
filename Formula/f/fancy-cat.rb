fb6aa4e19e3097c0e5df54c06f315d6dcb212832
Rui Chen <rui@chenrui.dev>
2025-12-12 09:22:12 -0500

    fancy-cat: revision bump (mupdf 1.27.0)
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    fancy-cat: patch for zig 0.15.2

--- Formula/f/fancy-cat.rb
+++ Formula/f/fancy-cat.rb
@@ -4,7 +4,7 @@ class FancyCat < Formula
   url "https://github.com/freref/fancy-cat/archive/refs/tags/v0.5.0.tar.gz"
   sha256 "7191c8b6259f8124d2bef4c38ab0bcb7f13923dd84a6ec5cb5512f729765f5b5"
   license "AGPL-3.0-or-later"
-  revision 2
+  revision 3
 
   bottle do
     sha256 cellar: :any,                 arm64_tahoe:   "fa2247970af16f34db31eadc37f2786a9a2537f6acfc189964d0bcc958a6b370"
@@ -16,7 +16,27 @@ class FancyCat < Formula
   end
 
   depends_on "zig" => :build
+  depends_on "mujs"
   depends_on "mupdf"
+
+  # Patches for zig patch prior to that
+  patch do
+    url "https://github.com/freref/fancy-cat/commit/9839e77a0949f44ffa6615517e6b866a8f6e0222.patch?full_index=1"
+    sha256 "0bd4725ee0e3415a1c33816bad5ec713806506c5cce6ccf474f400f0a623aee9"
+  end
+  patch do
+    url "https://github.com/freref/fancy-cat/commit/50b106635f03163e12b7407ba3fe7e5a5563f545.patch?full_index=1"
+    sha256 "e9250d6b033794c84d2af9827233f8f71939d97fbb4b3b834ea3c1328f222637"
+  end
+  patch do
+    url "https://github.com/freref/fancy-cat/commit/033d32d5bb3f11cc194deaa80c7eb8cd4d583c04.patch?full_index=1"
+    sha256 "5818a2845f396371bebc421c882ed190eca544035c818a86a5db15475ce361d9"
+  end
+  # Fix build with Zig 0.15.2
+  patch do
+    url "https://github.com/freref/fancy-cat/commit/f304c5907e52a62f4cf42953e9043d21ad9b47e9.patch?full_index=1"
+    sha256 "9e4400ca09aac07f0641666b9da70f822c04c3c79811c7beddbd0aa0ac075ac2"
+  end
 
   def install
     # Fix illegal instruction errors when using bottles on older CPUs.
@@ -30,7 +50,7 @@ class FancyCat < Formula
     args = []
     args << "-Dcpu=#{cpu}" if build.bottle?
 
-    system "zig", "build", *std_zig_args, *args
+    system "zig", "build", *args, *std_zig_args
   end
 
   test do
