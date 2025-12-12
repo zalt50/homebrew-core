f2a3b890ac98faf4e69f51ea34be5f1e7899b280
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-12 04:11:18 +0000

    pulumi: update 3.211.0 bottle.

--- Formula/p/pulumi.rb
+++ Formula/p/pulumi.rb
@@ -10,12 +10,12 @@ class Pulumi < Formula
   no_autobump! because: :bumped_by_upstream
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9101df4c3b21a014c263b39ce5147a627430fbcd4e9eb116db5aa86e117906df"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ba4bb4e1316c4abfffad2e8e36997f05c0128f48968e02f668ea51a1ddfb9757"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0452f643b2e49be482f641adb1145d74e723d108a7a1aa5587810feea2e84c2d"
-    sha256 cellar: :any_skip_relocation, sonoma:        "c4b61632283f3a6daef9d92af2b7b588de2cf7086a12df43f9e0f1d25e4871cb"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "6201e75713500eb27056ea78300223c82d0d40bfee426a956e56068d6ab47bd1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "88002cfc00c3dda88aa1c14306c1c9e17f7f450ce43edb8ce40fe6890a36419f"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "08a8cfeab319c9df0bf28dbced466cda167bac9764af6102f17b93d384fdb1c3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c1de61267cd2a29797d41ca76c94990e4a047ffc3b71839efab59354ea0aa6db"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5d5407f2e5078570d8de11e2643ce52e99e7f1629197ed566d63e6813c95ebeb"
+    sha256 cellar: :any_skip_relocation, sonoma:        "488bb161a37381d08bac8db3f818b6c56b5146db21c32a9a70484c1927ac044e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ed71cdceb006d87fe55f6f34a538039967a83221cc755bbabaaa7039fe43cda6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "129e30919530eb15a7b79da32b386db94c997f94c835446da74aeb83f5245ecb"
   end
 
   depends_on "go" => :build
