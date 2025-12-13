1672fd3376a27f9e299a9c467fba74a4591c243e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-13 16:18:46 +0000

    rad: add 0.6.23 bottle.

--- Formula/r/rad.rb
+++ Formula/r/rad.rb
@@ -6,6 +6,15 @@ class Rad < Formula
   license "Apache-2.0"
   head "https://github.com/amterp/rad.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "028ee1954b2221181986a3827a302fb09aad744b5b768c968f2921a0909f600e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d59690c6570abc602830838c1a127ff9d93b9af99dfb7901f80cf0812ce68393"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3664ab74caa650e9cdef4f36c31aabd1c5547a3ed142d6e63e2bb237a61f7271"
+    sha256 cellar: :any_skip_relocation, sonoma:        "ad438a9461fed6fcf121360e4dbf789385a73b8c37d32410c30059767717db83"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "5bb5fc23cfe3ccf23481e0c2f0a63f6491dac6557cf5c434fd643494c6f01c3b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0ba74c8df51c63eea339920f868d92c414fce88d13c7111163c88bb737bc9130"
+  end
+
   depends_on "go" => :build
 
   def install
