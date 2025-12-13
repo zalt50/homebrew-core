6104daf6f63306057502a12ff1c6320052920ae2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-13 08:20:10 +0000

    vtcode 0.48.3

--- Formula/v/vtcode.rb
+++ Formula/v/vtcode.rb
@@ -1,8 +1,8 @@
 class Vtcode < Formula
   desc "CLI Semantic Coding Agent"
   homepage "https://github.com/vinhnx/vtcode"
-  url "https://static.crates.io/crates/vtcode/vtcode-0.48.2.crate"
-  sha256 "6a406be7b86a814d87a0990a697b5ed62aa2f94c097f080dab34f33fb83e9774"
+  url "https://static.crates.io/crates/vtcode/vtcode-0.48.3.crate"
+  sha256 "503981e51822433c9ca1146161168d3b09c1009066e6314cf5011ca6df2843e7"
   license "MIT"
   head "https://github.com/vinhnx/vtcode.git", branch: "main"
 
