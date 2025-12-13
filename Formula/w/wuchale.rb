2356aa6281b7b27c788240fcb8c83ebb2471e044
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-13 05:46:39 +0000

    wuchale: add 0.18.7 bottle.

--- Formula/w/wuchale.rb
+++ Formula/w/wuchale.rb
@@ -5,6 +5,10 @@ class Wuchale < Formula
   sha256 "56cea728640e671b13885f6c8b4eeccd68e870afe6577731416959eeb6986e24"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "87a251ffd68bb0d4343f5b50d092b5b68d6ac8ee598790e10df78a952447677a"
+  end
+
   depends_on "node"
 
   def install
