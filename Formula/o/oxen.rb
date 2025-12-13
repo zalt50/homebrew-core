1f63ab7c731fe75eecff133eaf2f295bb20f6869
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-13 15:19:57 +0000

    oxen: update 0.41.3 bottle.

--- Formula/o/oxen.rb
+++ Formula/o/oxen.rb
@@ -16,12 +16,12 @@ class Oxen < Formula
   no_autobump! because: :bumped_by_upstream
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9c8f5f7cc31bf9588fafacf4d5bb5628980041bbf8403dd1faee39974aeb6034"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "24e80774be189bae60316dc739f4bd4696e81c051728d1eede50ac3bd6a77fa2"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "84d3195092876c43557d821c88b390d1acd50158d44caef7bfdeb9d94f85eb3d"
-    sha256 cellar: :any_skip_relocation, sonoma:        "f1132109c749e0046ca044f3ab1b4668aac866845d821f5d1c8e720051e4714a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d4bab43555cb10476fe7adcc3cdd3bf77375abc7b560e290536c7cca75e1f0c4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "113c16a6239560a8773a61828ee96a6021bb6134c4b3f4414f095ff5dc3feccf"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "45cd29334ccd3c46f2e476d41c137732fec5b0a0b1828ff17f6509f5ac8d96d7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cac22e8c2d7415f8b1bf35116ab8eca8e41ae80fdd6880e447cc78837afa5c04"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f78b128dce9408e23a00b04cefa4f377de56f03843434037174e11b633ca87a7"
+    sha256 cellar: :any_skip_relocation, sonoma:        "359137283005ed7069dc07e9d895e70c49397234200bff9f7b2b36a237fabc6b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "4a2f51b64a369d1214680ed390bd19c7edcbfdd0f5b5985a58dc1570ccd755e1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "58d0c36969cb94bdb6722ef583eac6f86aadd0bd55f048a003a331de90e3e00d"
   end
 
   depends_on "cmake" => :build # for libz-ng-sys
