8eecc3159ba6ca49e2a36361edc2ecfa8086aa2f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-13 17:19:38 +0000

    uutils-coreutils 0.5.0

--- Formula/u/uutils-coreutils.rb
+++ Formula/u/uutils-coreutils.rb
@@ -1,8 +1,8 @@
 class UutilsCoreutils < Formula
   desc "Cross-platform Rust rewrite of the GNU coreutils"
   homepage "https://uutils.github.io/coreutils/"
-  url "https://github.com/uutils/coreutils/archive/refs/tags/0.4.0.tar.gz"
-  sha256 "5f0c3f97b807e72edccc844c6a685ec9862199f16a665df07de5b1d20ec21233"
+  url "https://github.com/uutils/coreutils/archive/refs/tags/0.5.0.tar.gz"
+  sha256 "83535e10c3273c31baa2f553dfa0ceb4148914e9c1a9c5b00d19fbda5b2d4d7d"
   license "MIT"
   head "https://github.com/uutils/coreutils.git", branch: "main"
 
