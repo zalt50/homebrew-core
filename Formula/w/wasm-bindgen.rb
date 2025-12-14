98063d64b9effc4b779b08981e0f1ee79ea345e4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-14 18:50:08 +0000

    wasm-bindgen: add 0.2.106 bottle.

--- Formula/w/wasm-bindgen.rb
+++ Formula/w/wasm-bindgen.rb
@@ -5,6 +5,15 @@ class WasmBindgen < Formula
   sha256 "596b8014ba94cb9229fe15fc622078c02986ea5ec5b4e018f3116ad77cb63a85"
   license any_of: ["Apache-2.0", "MIT"]
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ce0138f72e4c65eef5878783008916b13c03b74428c9d9daa6420e46c111140c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e33000f10e77d8b28bbf62ab4929e4961b88c0c77eb81057a9b7535198bab409"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3d22c972e25ab976c789573f14ff6879d5625c680338d388c3503462679776ba"
+    sha256 cellar: :any_skip_relocation, sonoma:        "ec411ae0cc7f8247d20a5a550364a3c905bd855da100d8f81c72560a3526ddae"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "8635674aeef13e03e638fba036156d62db0d60d93a45bff2119adba04549b0f1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8594ea3a6468de75435121d38d0a72278d1f9554dae4a245b7162be34240dae6"
+  end
+
   depends_on "rust" => :build
   depends_on "node" => :test
   depends_on "rustup" => :test
