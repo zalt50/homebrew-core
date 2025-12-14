43fbedcbb28ec54549cccc6d534a2d4679b9b52f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-14 18:49:44 +0000

    mapscii: add 0.3.1 bottle.

--- Formula/m/mapscii.rb
+++ Formula/m/mapscii.rb
@@ -5,6 +5,10 @@ class Mapscii < Formula
   sha256 "21f18b687975f1decef998664ef6d292cf2379d9e4308a5b4cbb66af240341b3"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "5f4eaa8d74d98faad1c1423bb6f7c54efe68e47cd932192fdea23cca28cfb831"
+  end
+
   depends_on "node"
 
   def install
