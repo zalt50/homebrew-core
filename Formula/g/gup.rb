ba8f0fb19b76555e199938f6d49c05125a909581
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-14 18:49:29 +0000

    gup: add 0.28.1 bottle.

--- Formula/g/gup.rb
+++ Formula/g/gup.rb
@@ -6,6 +6,15 @@ class Gup < Formula
   license "Apache-2.0"
   head "https://github.com/nao1215/gup.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "408358c22b26f43361fd7539e8210a240fbb60bbf98a88ce81cf1b35ed98ac14"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ea960a208c44babc514aabe7fed8defbbb34b01b6fd5310359d12cf1fb79559b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "21f8ff3ec8f18eeca6a2269d2a1e877b9d100734fb10e7cc88dcdc62423dc0e5"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a720365235b549e4e1aeee93790fef8724298a9f3b0a9147525f469b31a15987"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "05c2e9fb21564622b44312adf83efdc21d7a1ba6b50f6f793600d2d70595381d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "576c3bd0be7676ccb53ad70a98da4387568a66a8346d9a7927ae48bef9832bb6"
+  end
+
   depends_on "go"
 
   def install
