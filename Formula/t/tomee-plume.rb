eb2ef8a6994c01499a41ebc61e4f52bd984e2e08
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-14 14:27:24 +0000

    tomee-plume 10.1.3

--- Formula/t/tomee-plume.rb
+++ Formula/t/tomee-plume.rb
@@ -1,9 +1,9 @@
 class TomeePlume < Formula
   desc "Apache TomEE Plume"
   homepage "https://tomee.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=tomee/tomee-10.1.2/apache-tomee-10.1.2-plume.tar.gz"
-  mirror "https://archive.apache.org/dist/tomee/tomee-10.1.2/apache-tomee-10.1.2-plume.tar.gz"
-  sha256 "58066f8da82c4b9194a579dd31f55e0ba7bd08fe073fe1defd86f60b89c41b4c"
+  url "https://www.apache.org/dyn/closer.lua?path=tomee/tomee-10.1.3/apache-tomee-10.1.3-plume.tar.gz"
+  mirror "https://archive.apache.org/dist/tomee/tomee-10.1.3/apache-tomee-10.1.3-plume.tar.gz"
+  sha256 "cc893c816adb5c58addae198869b2189cd9e8e698c0176ba30cc985606dfbd2b"
   license "Apache-2.0"
 
   bottle do
