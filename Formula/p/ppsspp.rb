ed47bfe638bd40ee1ece59bdb920cb2f6a308f16
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-15 22:43:00 +0900

    ppsspp 1.19.3

--- Formula/p/ppsspp.rb
+++ Formula/p/ppsspp.rb
@@ -1,21 +1,10 @@
 class Ppsspp < Formula
   desc "PlayStation Portable emulator"
   homepage "https://ppsspp.org/"
+  url "https://github.com/hrydgard/ppsspp/releases/download/v1.19.3/ppsspp-1.19.3.tar.xz"
+  sha256 "054401fa7fffbd99b7fd80e98a2951d6f0c3de83cb4b54719899c98bfad99614"
   license all_of: ["GPL-2.0-or-later", "BSD-3-Clause"]
-  revision 4
   head "https://github.com/hrydgard/ppsspp.git", branch: "master"
-
-  # TODO: Can remove CMAKE_POLICY_VERSION_MINIMUM when bumping version to 1.18+
-  # https://github.com/hrydgard/ppsspp/commit/fe91f246b2d22a25fcd52deb57211f1e86717c35
-  stable do
-    url "https://github.com/hrydgard/ppsspp/releases/download/v1.17.1/ppsspp-1.17.1.tar.xz"
-    sha256 "23e0b8649cc8124b0c22a62d4d41b592b6bd4064bce8c09b0d4abce895e132ae"
-
-    # miniupnpc 2.2.8 compatibility patch
-    patch :DATA
-  end
-
-  no_autobump! because: :requires_manual_review
 
   bottle do
     sha256 cellar: :any, arm64_tahoe:   "a66702cd0e81bd04a626ebc871b728043ea4bda90d198b1212c06c40e14a0a16"
@@ -29,8 +18,8 @@ class Ppsspp < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on "nasm" => :build
   depends_on "pkgconf" => :build
+  depends_on "yasm" => :build
 
   depends_on "libzip"
   depends_on "miniupnpc"
@@ -72,12 +61,14 @@ class Ppsspp < Formula
       end
     end
 
+    # Workaround for error: use of undeclared identifier `fseeko|ftello|ftruncate`
+    ENV.append_to_cflags "-D_DARWIN_C_SOURCE" if OS.mac?
+
     # Replace bundled MoltenVK dylib with symlink to Homebrew-managed dylib
     vulkan_frameworks = buildpath/"ext/vulkan/macOS/Frameworks"
     vulkan_frameworks.install_symlink Formula["molten-vk"].opt_lib/"libMoltenVK.dylib"
 
     args = %w[
-      -DCMAKE_POLICY_VERSION_MINIMUM=3.5
       -DUSE_SYSTEM_LIBZIP=ON
       -DUSE_SYSTEM_SNAPPY=ON
       -DUSE_SYSTEM_LIBSDL2=ON
@@ -112,83 +103,3 @@ class Ppsspp < Formula
     end
   end
 end
-
-__END__
-diff --git a/Core/Util/PortManager.cpp b/Core/Util/PortManager.cpp
-index cfb81e9dbd..dfd6a8e583 100644
---- a/Core/Util/PortManager.cpp
-+++ b/Core/Util/PortManager.cpp
-@@ -48,7 +48,7 @@ std::thread upnpServiceThread;
- std::recursive_mutex upnpLock;
- std::deque<UPnPArgs> upnpReqs;
- 
--PortManager::PortManager(): 
-+PortManager::PortManager():
- 	m_InitState(UPNP_INITSTATE_NONE),
- 	m_LocalPort(UPNP_LOCAL_PORT_ANY),
- 	m_leaseDuration("43200") {
-@@ -99,7 +99,7 @@ bool PortManager::Initialize(const unsigned int timeout) {
- 	int ipv6 = 0; // 0 = IPv4, 1 = IPv6
- 	unsigned char ttl = 2; // defaulting to 2
- 	int error = 0;
--	
-+
- 	VERBOSE_LOG(SCENET, "PortManager::Initialize(%d)", timeout);
- 	if (!g_Config.bEnableUPnP) {
- 		ERROR_LOG(SCENET, "PortManager::Initialize - UPnP is Disabled on Networking Settings");
-@@ -161,9 +161,21 @@ bool PortManager::Initialize(const unsigned int timeout) {
- 
- 		// Get LAN IP address that connects to the router
- 		char lanaddr[64] = "unset";
--		int status = UPNP_GetValidIGD(devlist, urls, datas, lanaddr, sizeof(lanaddr)); //possible "status" values, 0 = NO IGD found, 1 = A valid connected IGD has been found, 2 = A valid IGD has been found but it reported as not connected, 3 = an UPnP device has been found but was not recognized as an IGD
-+
-+		// possible "status" values:
-+		// -1 = Internal error
-+		//  0 = NO IGD found
-+		//  1 = A valid connected IGD has been found
-+		//  2 = A valid connected IGD has been found but its IP address is reserved (non routable)
-+		//  3 = A valid IGD has been found but it reported as not connected
-+		//  4 = an UPnP device has been found but was not recognized as an IGD
-+#if (MINIUPNPC_API_VERSION >= 18)
-+		int status = UPNP_GetValidIGD(devlist, urls, datas, lanaddr, sizeof(lanaddr), nullptr, 0);
-+#else
-+		int status = UPNP_GetValidIGD(devlist, urls, datas, lanaddr, sizeof(lanaddr));
-+#endif
- 		m_lanip = std::string(lanaddr);
--		INFO_LOG(SCENET, "PortManager - Detected LAN IP: %s", m_lanip.c_str());
-+		INFO_LOG(SCENET, "PortManager - Detected LAN IP: %s (status=%d)", m_lanip.c_str(), status);
- 
- 		// Additional Info
- 		char connectionType[64] = "";
-@@ -206,7 +218,7 @@ bool PortManager::Add(const char* protocol, unsigned short port, unsigned short
- 	char intport_str[16];
- 	int r;
- 	auto n = GetI18NCategory(I18NCat::NETWORKING);
--	
-+
- 	if (intport == 0)
- 		intport = port;
- 	INFO_LOG(SCENET, "PortManager::Add(%s, %d, %d)", protocol, port, intport);
-@@ -325,7 +337,7 @@ bool PortManager::Restore() {
- 				}
- 			}
- 			// Add the original owner back
--			r = UPNP_AddPortMapping(urls->controlURL, datas->first.servicetype, 
-+			r = UPNP_AddPortMapping(urls->controlURL, datas->first.servicetype,
- 				it->extPort_str.c_str(), it->intPort_str.c_str(), it->lanip.c_str(), it->desc.c_str(), it->protocol.c_str(), it->remoteHost.c_str(), it->duration.c_str());
- 			if (r == 0) {
- 				it->taken = false;
-@@ -334,7 +346,7 @@ bool PortManager::Restore() {
- 				ERROR_LOG(SCENET, "PortManager::Restore - AddPortMapping failed (error: %i)", r);
- 				if (r == UPNPCOMMAND_HTTP_ERROR)
- 					return false; // Might be better not to exit here, but exiting a loop will avoid long timeouts in the case the router is no longer reachable
--			}		
-+			}
- 		}
- 	}
- 	return true;
-@@ -538,4 +550,3 @@ void UPnP_Remove(const char* protocol, unsigned short port) {
- 	std::lock_guard<std::recursive_mutex> upnpGuard(upnpLock);
- 	upnpReqs.push_back({ UPNP_CMD_REMOVE, protocol, port, port });
- }
--
