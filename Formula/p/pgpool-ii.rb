32caa263dd075de6816c7704b35c42759ea67472
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-15 08:19:21 +0000

    pgpool-ii 4.6.5

--- Formula/p/pgpool-ii.rb
+++ Formula/p/pgpool-ii.rb
@@ -1,8 +1,8 @@
 class PgpoolIi < Formula
   desc "PostgreSQL connection pool server"
   homepage "https://www.pgpool.net/mediawiki/index.php/Main_Page"
-  url "https://www.pgpool.net/mediawiki/images/pgpool-II-4.6.4.tar.gz"
-  sha256 "ef0d2e91a9a11d737c6476247219e679f718bec53550646189594ef9aefd298d"
+  url "https://www.pgpool.net/mediawiki/images/pgpool-II-4.6.5.tar.gz"
+  sha256 "43dcb860e7099d3e322418378e856935f76bb4f3f09b9024c9b7d65af55e4036"
   license all_of: ["HPND", "ISC"] # ISC is only for src/utils/strlcpy.c
 
   livecheck do
@@ -31,6 +31,11 @@ class PgpoolIi < Formula
   end
 
   def install
+    # Workaround for use of `strchrnul`, which is not available on macOS
+    inreplace "src/utils/pool_process_reporting.c",
+              "*(strchrnul(status[i].value, '\\n')) = '\\0';",
+              "char *p = strchr(status[i].value, '\\n');\nif (p) *p = '\\\\0';"
+
     system "./configure", "--sysconfdir=#{etc}",
                           "--with-memcached=#{Formula["libmemcached"].opt_include}",
                           *std_configure_args
