c2a2d6aba85fbc0ea1b8b8231ab015c1976cec70
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-15 14:26:52 +0000

    vroom: update 1.14.0 bottle.

--- Formula/v/vroom.rb
+++ Formula/v/vroom.rb
@@ -9,17 +9,12 @@ class Vroom < Formula
   no_autobump! because: :requires_manual_review
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_tahoe:    "329045c3316d3e16c16a145b645c51070f0b88bc114336e0816ffea599dbf486"
-    sha256 cellar: :any,                 arm64_sequoia:  "1c0df6c3a21095891a7cbf1508accd44318e83734b741b0ebc9aea5e99b61cd7"
-    sha256 cellar: :any,                 arm64_sonoma:   "6165a7cb235b8a0bc6e57479ec80257751698945e9a4b699115d3163fa1a0add"
-    sha256 cellar: :any,                 arm64_ventura:  "0c57cf1a0b33c08327c768bf70580b3c9687fa18694466965b8d3f2e794f9093"
-    sha256 cellar: :any,                 arm64_monterey: "e9748811a768dafc95d402f4626c04e0a63b69aa3e503a22c835334b4503d814"
-    sha256 cellar: :any,                 sonoma:         "7cd025997ecb18d3d5fc35a953720fb96ce631ede626a9cb8bcf315187ce83c8"
-    sha256 cellar: :any,                 ventura:        "788ecd2b38d2c912a0a573f7a0a7b2f7ac926dcf0ea9de61a1bb7e7ed8111d88"
-    sha256 cellar: :any,                 monterey:       "bb59f660179205c05dc8ab2990b8fd3064be81b436f4b9ec12466906fa17c2fd"
-    sha256 cellar: :any_skip_relocation, arm64_linux:    "0e679e29f0cff84510a44089366d2109889217dc40e4ff8eb8df3c72cde24ea6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "42d4abf7176dba741db892b3708064ea7ffaa687892edd30dc0146d9144fe767"
+    sha256 cellar: :any,                 arm64_tahoe:   "603f9db571c5a11951f2d2f7a07e4a1108c07798cd1f347dbf4736e6baf6d7b3"
+    sha256 cellar: :any,                 arm64_sequoia: "e6cb0a6729f091b1d219c0ba4bfb995dfd3e2e2a445091550743ed273e1c5812"
+    sha256 cellar: :any,                 arm64_sonoma:  "a536b93c09c30d3c6ba2970a4b5dbb00dc0b1b87d9edc006b1d639129ecf113c"
+    sha256 cellar: :any,                 sonoma:        "e4173177423d3cc154d5a13b740793bd9045ad5c410e224e91766d35a8fa1b33"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3c01b046eb63ab22c7567f7ecfe3f48dabdb17f410658184e11eecf32ddb5ff2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8d7e36a54cfde07f9cf24edfdc3ff570621414264cd20f4d4b9a83c2e9b8dab3"
   end
 
   depends_on "cxxopts" => :build
