497903fb15c91c5bb30f8a1705c0d375dd113059
botantony <antonsm21@gmail.com>
2025-12-15 21:49:32 +0100

    device-mapper 2.03.38
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Formula/d/device-mapper.rb
+++ Formula/d/device-mapper.rb
@@ -2,8 +2,8 @@ class DeviceMapper < Formula
   desc "Userspace library and tools for logical volume management"
   homepage "https://sourceware.org/dm"
   url "https://sourceware.org/git/lvm2.git",
-      tag:      "v2_03_37",
-      revision: "d34fc799fd86538756ba56b37e1ab56ab1b75d13"
+      tag:      "v2_03_38",
+      revision: "657e10bd75fcb3dffbd40bb1ffc6f1bfd768a10e"
   license "LGPL-2.1-only"
 
   livecheck do
