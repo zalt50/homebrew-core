fe771bf64f74b808ebfdb7ce107384e1747725da
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-16 23:28:56 +0000

    container-structure-test: update 1.22.1 bottle.

--- Formula/c/container-structure-test.rb
+++ Formula/c/container-structure-test.rb
@@ -7,12 +7,12 @@ class ContainerStructureTest < Formula
   head "https://github.com/GoogleContainerTools/container-structure-test.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "985a9d8cdb2a082e042602c9e185ee354abfcfdbe51824dad8752679f66aa244"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "985a9d8cdb2a082e042602c9e185ee354abfcfdbe51824dad8752679f66aa244"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "985a9d8cdb2a082e042602c9e185ee354abfcfdbe51824dad8752679f66aa244"
-    sha256 cellar: :any_skip_relocation, sonoma:        "b6c0bcd37369f2f03e8eaa6d0962f5e1a4e058f16a0430f304907d03fefd0886"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d3a65c251fb9281c834acf28d00ea86ff2e43ffd47fd9830f98a06762d1dd874"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1438c34759acb2d059ac82ca330d233f1ed50e2d2a1aa47b8e7a5b8e8879b7a8"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ffadb4b8e066a730a44df161a8994706c19299d4b2016e1f6446c4b87f4300d7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ffadb4b8e066a730a44df161a8994706c19299d4b2016e1f6446c4b87f4300d7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ffadb4b8e066a730a44df161a8994706c19299d4b2016e1f6446c4b87f4300d7"
+    sha256 cellar: :any_skip_relocation, sonoma:        "9e1bbe8f1cf6043c62b4108fca2107d9e967138b2bc064bb39a23a4f4762f607"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "dcac556f58434bb4a0886090be35ee58c844b20507704c679a30043680c36cce"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0a5370012785c55e9fec1d69ccc950b88e84a36d4f6de86f322ba9a3919c3099"
   end
 
   depends_on "go" => :build
