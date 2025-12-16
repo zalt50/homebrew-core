afd1154847603404fe2652cacc4bf381f9d35d4b
nbari <nbari@tequila.io>
2025-12-16 22:53:15 +0100

    ssh-vault 1.2.0
    Created by https://github.com/mislav/bump-homebrew-formula-action
    
    ssh-vault: remove openssl dep for linux build
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/s/ssh-vault.rb
+++ Formula/s/ssh-vault.rb
@@ -1,8 +1,8 @@
 class SshVault < Formula
   desc "Encrypt/decrypt using SSH keys"
   homepage "https://ssh-vault.com/"
-  url "https://github.com/ssh-vault/ssh-vault/archive/refs/tags/1.1.5.tar.gz"
-  sha256 "73d73ec173400dafb290a237bb75a18e555b4216ee01be22a54c09ff2a51bebe"
+  url "https://github.com/ssh-vault/ssh-vault/archive/refs/tags/1.2.0.tar.gz"
+  sha256 "293db51df95c2641540f7efb9d5d5e12fabcc70034fe9ad510bf9b158924f001"
   license "BSD-3-Clause"
   head "https://github.com/ssh-vault/ssh-vault.git", branch: "main"
 
@@ -17,37 +17,16 @@ class SshVault < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "cb2b50f4ba9fd803b89ac50688d2643a00646c42f474bef76bb287e7549dd3cb"
   end
 
-  depends_on "pkgconf" => :build
   depends_on "rust" => :build
 
-  on_linux do
-    depends_on "openssl@3"
-  end
-
   def install
-    # Ensure that the `openssl` crate picks up the intended library.
-    ENV["OPENSSL_DIR"] = Formula["openssl@3"].opt_prefix
-    ENV["OPENSSL_NO_VENDOR"] = "1"
-
     system "cargo", "install", *std_cargo_args
   end
 
   test do
-    require "utils/linkage"
-
     test_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINixf2m2nj8TDeazbWuemUY8ZHNg7znA7hVPN8TJLr2W"
     (testpath/"public_key").write test_key
     cmd = "#{bin}/ssh-vault f -k  #{testpath}/public_key"
     assert_match "SHA256:hgIL5fEHz5zuOWY1CDlUuotdaUl4MvYG7vAgE4q4TzM", shell_output(cmd)
-
-    if OS.linux?
-      [
-        Formula["openssl@3"].opt_lib/shared_library("libssl"),
-        Formula["openssl@3"].opt_lib/shared_library("libcrypto"),
-      ].each do |library|
-        assert Utils.binary_linked_to_library?(bin/"ssh-vault", library),
-              "No linkage with #{library.basename}! Cargo is likely using a vendored version."
-      end
-    end
   end
 end
