b34c202c2cc431a3e642a2ae76c62aea3adb92f9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-17 02:53:44 +0000

    supabase 2.67.1

--- Formula/s/supabase.rb
+++ Formula/s/supabase.rb
@@ -1,8 +1,8 @@
 class Supabase < Formula
   desc "Open source Firebase alternative"
   homepage "https://supabase.com/docs/reference/cli/about"
-  url "https://github.com/supabase/cli/archive/refs/tags/v2.65.5.tar.gz"
-  sha256 "3dd456363d9949f0f3a591c9cba62f744928228a8d99c469b7ea36dc243975df"
+  url "https://github.com/supabase/cli/archive/refs/tags/v2.67.1.tar.gz"
+  sha256 "61dcf4b34dd2ef31caf6233964ff94575dbd5951951a47ce855672f7fb31ebe7"
   license "MIT"
   head "https://github.com/supabase/cli.git", branch: "develop"
 
