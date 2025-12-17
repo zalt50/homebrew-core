d42eeaab875fb6393ecb75812b9b5a7ca96cf045
Joe George <joe@externl.com>
2025-12-17 11:36:49 -0500

    ice 3.8.0

--- Formula/i/ice.rb
+++ Formula/i/ice.rb
@@ -1,16 +1,14 @@
 class Ice < Formula
   desc "Comprehensive RPC framework"
   homepage "https://zeroc.com"
-  url "https://github.com/zeroc-ice/ice/archive/refs/tags/v3.7.10.tar.gz"
-  sha256 "b90e9015ca9124a9eadfdfc49c5fba24d3550c547f166f3c9b2b5914c00fb1df"
+  url "https://github.com/zeroc-ice/ice/archive/refs/tags/v3.8.0.tar.gz"
+  sha256 "ec03e18b1bb0e83547744f3d0d0b73b60e5b497bed10de6f933b54525802a3cb"
   license "GPL-2.0-only"
 
   livecheck do
     url :stable
     strategy :github_latest
   end
-
-  no_autobump! because: :requires_manual_review
 
   bottle do
     sha256 cellar: :any,                 arm64_tahoe:    "163b12ea66fc9210f121ee9c2f0fc18404d8f660a963d3bf492af0b807eb19d8"
@@ -38,22 +36,8 @@ class Ice < Formula
   end
 
   def install
-    extra_cxxflags = []
-
-    # Workaround for Xcode 16 (LLVM 17) Clang bug that causes:
-    # include/Ice/OutgoingAsync.h: error: declaration shadows a local variable [-Werror,-Wshadow-uncaptured-local]
-    # Ref: https://github.com/llvm/llvm-project/issues/81307
-    # Ref: https://github.com/llvm/llvm-project/issues/71976
-    extra_cxxflags << "-Wno-shadow-uncaptured-local" if DevelopmentTools.clang_build_version >= 1600
-
-    # Workaround for macOS 26 SDK
-    extra_cxxflags << "-Wno-deprecated-declarations" if DevelopmentTools.clang_build_version >= 1700
-
-    # Workaround for Xcode 26 (Clang 17)
-    extra_cxxflags << "-Wno-reserved-user-defined-literal" if DevelopmentTools.clang_build_version >= 1700
-
-    unless extra_cxxflags.empty?
-      inreplace "config/Make.rules.Darwin", "-Wdocumentation ", "\\0#{extra_cxxflags.join(" ")} "
+    if DevelopmentTools.clang_build_version < 1700
+      inreplace "config/Make.rules.Darwin", "-Wl,-max_default_common_align,0x4000", ""
     end
 
     args = [
@@ -62,33 +46,11 @@ class Ice < Formula
       "USR_DIR_INSTALL=yes", # ensure slice and man files are installed to share
       "MCPP_HOME=#{Formula["mcpp"].opt_prefix}",
       "LMDB_HOME=#{Formula["lmdb"].opt_prefix}",
-      "CONFIGS=shared cpp11-shared xcodesdk cpp11-xcodesdk",
+      "CONFIGS=all",
       "PLATFORMS=all",
-      "SKIP=slice2confluence",
       "LANGUAGES=cpp",
     ]
-
-    # Fails with Xcode < 12.5
-    inreplace "cpp/include/Ice/Object.h", /^#.+"-Wdeprecated-copy-dtor"+/, "" if OS.mac? && MacOS.version <= :catalina
-
     system "make", "install", *args
-
-    # We install these binaries to libexec because they conflict with those
-    # installed along with the ice packages from PyPI, RubyGems and npm.
-    (libexec/"bin").mkpath
-    %w[slice2py slice2rb slice2js].each do |r|
-      mv bin/r, libexec/"bin"
-    end
-  end
-
-  def caveats
-    <<~EOS
-      slice2py, slice2js and slice2rb were installed in:
-
-        #{opt_libexec}/bin
-
-      You may wish to add this directory to your PATH.
-    EOS
   end
 
   test do
@@ -105,13 +67,13 @@ class Ice < Formula
     port = free_port
 
     (testpath/"Test.cpp").write <<~CPP
+      #include "Hello.h"
       #include <Ice/Ice.h>
-      #include <Hello.h>
 
       class HelloI : public Test::Hello
       {
       public:
-        virtual void sayHello(const Ice::Current&) override {}
+          void sayHello(const Ice::Current&) override {}
       };
 
       int main(int argc, char* argv[])
@@ -125,9 +87,9 @@ class Ice < Formula
     CPP
 
     system bin/"slice2cpp", "Hello.ice"
-    system ENV.cxx, "-DICE_CPP11_MAPPING", "-std=c++11", "-c", "-I#{include}", "-I.", "Hello.cpp"
-    system ENV.cxx, "-DICE_CPP11_MAPPING", "-std=c++11", "-c", "-I#{include}", "-I.", "Test.cpp"
-    system ENV.cxx, "-L#{lib}", "-o", "test", "Test.o", "Hello.o", "-lIce++11", "-pthread"
+    system ENV.cxx, "-std=c++20", "-c", "-I#{include}", "Hello.cpp"
+    system ENV.cxx, "-std=c++20", "-c", "-I#{include}", "Test.cpp"
+    system ENV.cxx, "-L#{lib}", "-o", "test", "Test.o", "Hello.o", "-lIce", "-lpthread"
     system "./test"
   end
 end
