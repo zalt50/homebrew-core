d83240f6bc3211e9691c230f628421f15969b9c9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-17 19:50:04 +0000

    aws-crt-cpp 0.36.1

--- Formula/a/aws-crt-cpp.rb
+++ Formula/a/aws-crt-cpp.rb
@@ -1,8 +1,8 @@
 class AwsCrtCpp < Formula
   desc "C++ wrapper around the aws-c-* libraries"
   homepage "https://github.com/awslabs/aws-crt-cpp"
-  url "https://github.com/awslabs/aws-crt-cpp/archive/refs/tags/v0.36.0.tar.gz"
-  sha256 "6ef0781929ef2cba0b21f1e3f585491b6a656e48acf3cab041f8f82c569ab642"
+  url "https://github.com/awslabs/aws-crt-cpp/archive/refs/tags/v0.36.1.tar.gz"
+  sha256 "5e0fcfbb90fdb6ffd7a0cb87798429ee2bf364e309d549ba7c3349bc470f8501"
   license "Apache-2.0"
 
   bottle do
