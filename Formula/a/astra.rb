499fdf6d872646b62a1769fcb500ab0675d9281d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-17 11:16:01 +0000

    astra: add 1.0.2 bottle.

--- Formula/a/astra.rb
+++ Formula/a/astra.rb
@@ -5,6 +5,15 @@ class Astra < Formula
   sha256 "21404f4d26b9608a85d8cd65d52c93555bc6030398cb1208b6d2e7ee07aed542"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "05f012277104085caa7577de3031dbef897ad77c6b3ead1c77cdd099310cd4aa"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "eea64e938f064b8efb6ace7cd542c56858351185056536f43d040761124066a2"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "20c0b8598d8853819157889912cf431f3df76448b8cb84fef1d269fc2cf4b639"
+    sha256 cellar: :any_skip_relocation, sonoma:        "05f012277104085caa7577de3031dbef897ad77c6b3ead1c77cdd099310cd4aa"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "276aa74f753a696fa6538de95ec2351ae2612d9323e2445d40990296baa7a0dc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "276aa74f753a696fa6538de95ec2351ae2612d9323e2445d40990296baa7a0dc"
+  end
+
   depends_on "openjdk"
 
   def install
