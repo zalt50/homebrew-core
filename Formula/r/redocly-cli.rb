730ff45303fc30927f3d88f748a5eba65c37567d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-17 18:46:59 +0000

    redocly-cli: update 2.13.0 bottle.

--- Formula/r/redocly-cli.rb
+++ Formula/r/redocly-cli.rb
@@ -6,12 +6,12 @@ class RedoclyCli < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f68f9f9e605e048fa03ad3dcacef3453bc8d2eb2d19f515f228adf22e348f474"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d6c8ef8b4102cc628814f34caef4c2a7444bc4f7b33de523b48a4036ccbb74e8"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d6c8ef8b4102cc628814f34caef4c2a7444bc4f7b33de523b48a4036ccbb74e8"
-    sha256 cellar: :any_skip_relocation, sonoma:        "bc8a5b6ca77842d1d0184525fc84eaa81b544696eacf6b6ecea6b547a06bbc6e"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "23e3385ae4550fe961a894aac707fd664de7e3884593fd660b80d984d8f6bee7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "23e3385ae4550fe961a894aac707fd664de7e3884593fd660b80d984d8f6bee7"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e5eadc5506c27dad2c0860ed2227f0c5c6615325bcc07acc9048809f14363b55"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a66b5f6d899e66034ac35b6699b8002017912a6e6b32dda06d028d7fbd3649e1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a66b5f6d899e66034ac35b6699b8002017912a6e6b32dda06d028d7fbd3649e1"
+    sha256 cellar: :any_skip_relocation, sonoma:        "710e2a10b96288980c3cdb03a3faa892269c0cb2c6d72c3c183548d3cd88d6e5"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d716cc743b576aebe0a55427e5c8464554449cf231651839ac1326368af6f44a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d716cc743b576aebe0a55427e5c8464554449cf231651839ac1326368af6f44a"
   end
 
   depends_on "node"
