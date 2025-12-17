d3fea98dce5e840c823f564b5158ab2c75aed438
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-17 22:08:05 +0000

    thors-anvil: add 8.0.04 bottle.

--- Formula/t/thors-anvil.rb
+++ Formula/t/thors-anvil.rb
@@ -6,6 +6,15 @@ class ThorsAnvil < Formula
       revision: "f7bf4c45fa33d7fb4bf924d424d29a537ec56b01"
   license "GPL-3.0-only"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "4cd153e8c806c960a028af98f16aac3b98a399f33ca4ce257182f9d94bad5f92"
+    sha256 cellar: :any,                 arm64_sequoia: "0d78a2c2bc758cef78e5231ca7cdcfa40029526f0aa65fe3d4856fd919a61727"
+    sha256 cellar: :any,                 arm64_sonoma:  "c4f90e9403812f492424a157d415bb0aa7971773b243dd36c3d3716ae5baff95"
+    sha256 cellar: :any,                 sonoma:        "49014165cc31b49028797812625303fd8e846c1fec0816c66fc861cba8b9cad3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "611f80abdf2a31d991e4c816294d37ebed07131e2dd7d3829415c1acaf0fd4e7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c3b48bbe8478d55dda9395e9e7ee1e7f41efd8aab46bf3adf86adfaa2fd6827d"
+  end
+
   depends_on "cmake" => :build
   depends_on "boost"
   depends_on "libevent"
