8bba5e2d7c095cb82716876f132a5b02e8a6c672
l5io <47201130+l5io@users.noreply.github.com>
2025-12-19 04:00:10 +0530

    mesheryctl 0.8.190
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/m/mesheryctl.rb
+++ Formula/m/mesheryctl.rb
@@ -2,8 +2,8 @@ class Mesheryctl < Formula
   desc "Command-line utility for Meshery, the cloud native management plane"
   homepage "https://meshery.io"
   url "https://github.com/meshery/meshery.git",
-      tag:      "v0.8.181",
-      revision: "a0e17936228afcefc003c6377c33e9c06a699a92"
+      tag:      "v0.8.190",
+      revision: "3f4d5e8ed7c9431c0b566c60017b8fc51be3ab5a"
   license "Apache-2.0"
   head "https://github.com/meshery/meshery.git", branch: "master"
 
@@ -21,7 +21,7 @@ class Mesheryctl < Formula
   depends_on "go" => :build
 
   def install
-    ENV["CGO_ENABLED"] = "0"
+    ENV["CGO_ENABLED"] = "0" if OS.linux?
 
     ldflags = %W[
       -s -w
