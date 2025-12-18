a702f4ca31dc264329663ac28fc1c6c1a1e26a11
botantony <antonsm21@gmail.com>
2025-12-18 17:24:12 +0100

    java-service-wrapper 3.6.4
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Formula/j/java-service-wrapper.rb
+++ Formula/j/java-service-wrapper.rb
@@ -1,8 +1,8 @@
 class JavaServiceWrapper < Formula
   desc "Simplify the deployment, launch and monitoring of Java applications"
   homepage "https://wrapper.tanukisoftware.com/"
-  url "https://downloads.sourceforge.net/project/wrapper/wrapper_src/Wrapper_3.6.3_20250910/wrapper_3.6.3_src.tar.gz"
-  sha256 "7bc5ad89ed21a39b4a4ed548dfe92fc587aed40e6c75059a71309fb8042ff580"
+  url "https://downloads.sourceforge.net/project/wrapper/wrapper_src/Wrapper_3.6.4_20251218/wrapper_3.6.4_src.tar.gz"
+  sha256 "fb22e05d89f0a6a12bcd5fd8d4742ab42cbfba5324e367a87810a6a67f595a12"
   license any_of: ["GPL-2.0-only", "GPL-3.0-only"]
 
   no_autobump! because: :incompatible_version_format
