b756025f97d02e8c64a1e5ca9dc3263b1c1e682d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-19 08:14:06 +0000

    sqlparse 0.5.5

--- Formula/s/sqlparse.rb
+++ Formula/s/sqlparse.rb
@@ -3,8 +3,8 @@ class Sqlparse < Formula
 
   desc "Non-validating SQL parser"
   homepage "https://github.com/andialbrecht/sqlparse"
-  url "https://files.pythonhosted.org/packages/18/67/701f86b28d63b2086de47c942eccf8ca2208b3be69715a1119a4e384415a/sqlparse-0.5.4.tar.gz"
-  sha256 "4396a7d3cf1cd679c1be976cf3dc6e0a51d0111e87787e7a8d780e7d5a998f9e"
+  url "https://files.pythonhosted.org/packages/90/76/437d71068094df0726366574cf3432a4ed754217b436eb7429415cf2d480/sqlparse-0.5.5.tar.gz"
+  sha256 "e20d4a9b0b8585fdf63b10d30066c7c94c5d7a7ec47c889a2d83a3caa93ff28e"
   license "BSD-3-Clause"
 
   bottle do
