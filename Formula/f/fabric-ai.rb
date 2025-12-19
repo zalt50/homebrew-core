eeb06340407648f8684c3411b1f6acbf4edcef16
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-19 21:18:24 +0000

    fabric-ai: update 1.4.354 bottle.

--- Formula/f/fabric-ai.rb
+++ Formula/f/fabric-ai.rb
@@ -7,12 +7,12 @@ class FabricAi < Formula
   head "https://github.com/danielmiessler/fabric.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "fd0ade653ea52c37c016f36f99a45ca49e0c1a9dbf73054d8c207654885fa4d6"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fd0ade653ea52c37c016f36f99a45ca49e0c1a9dbf73054d8c207654885fa4d6"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fd0ade653ea52c37c016f36f99a45ca49e0c1a9dbf73054d8c207654885fa4d6"
-    sha256 cellar: :any_skip_relocation, sonoma:        "4596410363adb566ed3ff9701b757ce736140dbea244f7f8058e1eabebb74c62"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3f8f0d177f8cf0162b3da8546d1ed93c0e672c3e3b71f63c5df8df4333ac73e0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c762959fb4443d30f91fc90e75a7baae492516732aa8de459c3f9d717fbcf94a"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "562961ebdc99464cc0dccbc53f7721e3208f24739f56a01904d589fed6df5774"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "562961ebdc99464cc0dccbc53f7721e3208f24739f56a01904d589fed6df5774"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "562961ebdc99464cc0dccbc53f7721e3208f24739f56a01904d589fed6df5774"
+    sha256 cellar: :any_skip_relocation, sonoma:        "1cb5eb51045fddc2d7f2eee4725bfa2a59a1b10e81a3b9837b764f9ab85e3208"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ee160dcf1df4278ffd150d6c2fa6eb95f3298dc9de2fffb50e2c168121d34734"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "35f2bf68b0fb15a47dbfd84fc798d2078b692e7c7eb6769007a4607ca7e7d5e3"
   end
 
   depends_on "go" => :build
