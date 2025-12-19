6e830931ca4106100aa0e54c0e1894303d530a44
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-19 13:13:58 +0900

    fuse-overlayfs: reivison bump (libfuse 3.18.0)
    fuse-overlayfs: update test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/f/fuse-overlayfs.rb
+++ Formula/f/fuse-overlayfs.rb
@@ -4,6 +4,7 @@ class FuseOverlayfs < Formula
   url "https://github.com/containers/fuse-overlayfs/archive/refs/tags/v1.16.tar.gz"
   sha256 "45968517603389ead067222d234bc8d8ed33e4b4f8ba16216bdd3e6aedcccea9"
   license "GPL-2.0-or-later"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_linux:  "f56b8eaeddb077f80c3742194033850eeb11f2c9eb266cd317ba02879a49dc90"
@@ -32,7 +33,7 @@ class FuseOverlayfs < Formula
     mkdir "merged"
     test_cmd = "fuse-overlayfs -o lowerdir=lowerdir/a:lowerdir/b,upperdir=up,workdir=workdir merged 2>&1"
     output = shell_output(test_cmd, 1)
-    assert_match "fuse: device not found, try 'modprobe fuse' first", output
+    assert_match "fuse: device /dev/fuse not found. Kernel module not loaded?", output
     assert_match "fuse-overlayfs: cannot mount: No such file or directory", output
   end
 end
