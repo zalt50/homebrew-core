bcc23bb6fb985ce562befbf1bac2d494f88f1a4d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-19 02:46:33 +0000

    php@8.4 8.4.16

--- Formula/p/php@8.4.rb
+++ Formula/p/php@8.4.rb
@@ -2,9 +2,9 @@ class PhpAT84 < Formula
   desc "General-purpose scripting language"
   homepage "https://www.php.net/"
   # Should only be updated if the new version is announced on the homepage, https://www.php.net/
-  url "https://www.php.net/distributions/php-8.4.15.tar.xz"
-  mirror "https://fossies.org/linux/www/php-8.4.15.tar.xz"
-  sha256 "a060684f614b8344f9b34c334b6ba8db1177555997edb5b1aceab0a4b807da7e"
+  url "https://www.php.net/distributions/php-8.4.16.tar.xz"
+  mirror "https://fossies.org/linux/www/php-8.4.16.tar.xz"
+  sha256 "f66f8f48db34e9e29f7bfd6901178e9cf4a1b163e6e497716dfcb8f88bcfae30"
   license all_of: [
     "PHP-3.01",
 
