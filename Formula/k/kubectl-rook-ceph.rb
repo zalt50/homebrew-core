fcd85467f23289a65bfe0b852882e1424a8f30c4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-19 18:21:28 +0000

    kubectl-rook-ceph: update 0.9.5 bottle.

--- Formula/k/kubectl-rook-ceph.rb
+++ Formula/k/kubectl-rook-ceph.rb
@@ -7,12 +7,13 @@ class KubectlRookCeph < Formula
   head "https://github.com/rook/kubectl-rook-ceph.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c49feed78f61fab9f69789adc602205c3f2fe27567c7902b3a50e1a6a35daa12"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c49feed78f61fab9f69789adc602205c3f2fe27567c7902b3a50e1a6a35daa12"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c49feed78f61fab9f69789adc602205c3f2fe27567c7902b3a50e1a6a35daa12"
-    sha256 cellar: :any_skip_relocation, sonoma:        "fd8066e09c94c045b292558de6418d77f3ba833b4722dee51f8cd95a35eef37f"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a5a90b3d9ece647a7c2b7479dc4b7ed8369803ad86b3c67dd29e36101c1ddd3c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e286057ec852ee1fff033e4904fa1c485c7186dc52bbac039a7ee93b91def35e"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "38ef6b6b5600bafd151530f7cd750328fe5ae537e29a8f0256c48c00b888ee51"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "38ef6b6b5600bafd151530f7cd750328fe5ae537e29a8f0256c48c00b888ee51"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "38ef6b6b5600bafd151530f7cd750328fe5ae537e29a8f0256c48c00b888ee51"
+    sha256 cellar: :any_skip_relocation, sonoma:        "ca5cffd2a78ceed9a0d2777c1eccb1b555e75a23215dbe9d1d42f1fc6d0f5884"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f31f6d9ed0bcf0b233bf2deef70c717b46cc8a232d9ccd26f971fe376298b642"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c67ffb743c4d5fb29bfe87cb63617614c1fe094681bf390f937ae17aadeca7cc"
   end
 
   depends_on "go" => :build
