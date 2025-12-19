1c191e6b667b0976c7a6a14ce3bcd94429441502
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-19 22:56:16 +0000

    clipper2 2.0.1

--- Formula/c/clipper2.rb
+++ Formula/c/clipper2.rb
@@ -1,8 +1,8 @@
 class Clipper2 < Formula
   desc "Polygon clipping and offsetting library"
   homepage "https://github.com/AngusJohnson/Clipper2"
-  url "https://github.com/AngusJohnson/Clipper2/releases/download/Clipper2_1.5.4/Clipper2_1.5.4.zip"
-  sha256 "e5cbe4acdfbd381496feacd5692110f60914ce2998e7350b124fb11429574f75"
+  url "https://github.com/AngusJohnson/Clipper2/releases/download/Clipper2_2.0.1/Clipper2_2.0.1.zip"
+  sha256 "63e893fc40c3453c9d14cbe98bc7647f16a9d5846ae25b513d8a1ed5b8421144"
   license "BSL-1.0"
 
   livecheck do
@@ -29,28 +29,26 @@ class Clipper2 < Formula
       -DCLIPPER2_TESTS=OFF
       -DBUILD_SHARED_LIBS=ON
     ]
-
-    system "cmake", "-S", "CPP", "-B", "build", *std_cmake_args, *args
+    system "cmake", "-S", "CPP", "-B", "build", *args, *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
 
     inreplace "CPP/Examples/SimpleClipping/SimpleClipping.cpp" do |s|
-      s.gsub! "\"clipper2/clipper.h\"", "<clipper2/clipper.h>"
+      s.gsub! "\"../../Utils/clipper.svg.h\"", "\"clipper.svg.h\""
       s.gsub! "\"../../Utils/clipper.svg.utils.h\"", "\"clipper.svg.utils.h\""
-      s.gsub!(/System\(".*"\);/, "")
     end
 
-    inreplace "CPP/Utils/clipper.svg.utils.h", "\"clipper.svg.h\"", "<clipper2/Utils/clipper.svg.h>"
-
-    pkgshare.install "CPP/Examples/SimpleClipping/SimpleClipping.cpp", "CPP/Utils/clipper.svg.utils.h"
+    pkgshare.install "CPP/Examples/SimpleClipping/SimpleClipping.cpp",
+                     "CPP/Utils/clipper.svg.cpp",
+                     "CPP/Utils/clipper.svg.h",
+                     "CPP/Utils/clipper.svg.utils.h"
   end
 
   test do
-    system ENV.cxx, pkgshare/"SimpleClipping.cpp",
-                    "-std=c++17", "-I#{include}", "-L#{lib}",
-                    "-lClipper2", "-lClipper2utils",
+    system ENV.cxx, pkgshare/"SimpleClipping.cpp", pkgshare/"clipper.svg.cpp",
+                    "-std=c++17", "-I#{include}", "-L#{lib}", "-lClipper2",
                     "-o", "test"
     system "./test"
-    refute_empty (testpath/"solution.svg").read
+    refute_empty (testpath/"Intersect Paths.SVG").read
   end
 end
