16889514231e7c5af2334779f294dbfe03b5c858
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-19 14:32:29 +0000

    resterm 0.11.7

--- Formula/r/resterm.rb
+++ Formula/r/resterm.rb
@@ -1,8 +1,8 @@
 class Resterm < Formula
   desc "Terminal client for .http/.rest files with HTTP, GraphQL, and gRPC support"
   homepage "https://github.com/unkn0wn-root/resterm"
-  url "https://github.com/unkn0wn-root/resterm/archive/refs/tags/v0.11.6.tar.gz"
-  sha256 "32d00001ab7e18f2dd423c1eb61a951ff6aa5669a9cea2432951e9e558bac58d"
+  url "https://github.com/unkn0wn-root/resterm/archive/refs/tags/v0.11.7.tar.gz"
+  sha256 "3f3890711502fc5acfe1787908093d095312c4b9b1f9648df66450548481e8f1"
   license "Apache-2.0"
   head "https://github.com/unkn0wn-root/resterm.git", branch: "main"
 
