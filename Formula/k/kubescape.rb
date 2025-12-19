0048f88a0236ac606875d04194a9fe6bc961cf87
Štefan Baebler <319826+stefanb@users.noreply.github.com>
2025-12-19 17:17:24 +0100

    kubescape: checksum update

--- Formula/k/kubescape.rb
+++ Formula/k/kubescape.rb
@@ -2,7 +2,7 @@ class Kubescape < Formula
   desc "Kubernetes testing according to Hardening Guidance by NSA and CISA"
   homepage "https://kubescape.io"
   url "https://github.com/kubescape/kubescape/archive/refs/tags/v3.0.47.tar.gz"
-  sha256 "3782a959a87ea9b0f3409f335e87edda54b0f1d7285c1113e5ec220ff998aef3"
+  sha256 "1d8c4820f341823dc1fc50d575044099dbf5cbfe2a05fc9e12976715efb41ae9"
   license "Apache-2.0"
   head "https://github.com/kubescape/kubescape.git", branch: "master"
 
