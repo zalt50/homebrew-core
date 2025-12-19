1ba4203afd45e7b596ae9ba2d9ce7b6d85afe243
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-19 13:53:12 +0000

    alsa-lib 1.2.15.1

--- Formula/a/alsa-lib.rb
+++ Formula/a/alsa-lib.rb
@@ -1,8 +1,8 @@
 class AlsaLib < Formula
   desc "Provides audio and MIDI functionality to the Linux operating system"
   homepage "https://www.alsa-project.org/"
-  url "https://www.alsa-project.org/files/pub/lib/alsa-lib-1.2.15.tar.bz2"
-  sha256 "83770841585e766a60c99fd23f8c574c22643ae0cb1f2d20b793c3d84eb95a8d"
+  url "https://www.alsa-project.org/files/pub/lib/alsa-lib-1.2.15.1.tar.bz2"
+  sha256 "7f983ca89ca420872ca16e8a9f8f97fb63db6c1c6e2585b91737a08bb03f566c"
   license all_of: ["LGPL-2.1-or-later", "GPL-2.0-or-later"]
 
   livecheck do
