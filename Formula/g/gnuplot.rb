8568e553e88d21097b5eae84a8370df74ff88ac8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-20 07:57:54 +0000

    gnuplot 6.0.4

--- Formula/g/gnuplot.rb
+++ Formula/g/gnuplot.rb
@@ -1,8 +1,8 @@
 class Gnuplot < Formula
   desc "Command-driven, interactive function plotting"
   homepage "http://www.gnuplot.info/"
-  url "https://downloads.sourceforge.net/project/gnuplot/gnuplot/6.0.3/gnuplot-6.0.3.tar.gz"
-  sha256 "ec52e3af8c4083d4538152b3f13db47f6d29929a3f6ecec5365c834e77f251ab"
+  url "https://downloads.sourceforge.net/project/gnuplot/gnuplot/6.0.4/gnuplot-6.0.4.tar.gz"
+  sha256 "458d94769625e73d5f6232500f49cbadcb2b183380d43d2266a0f9701aeb9c5b"
   license "gnuplot"
 
   livecheck do
