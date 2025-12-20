d501d60107f893be3390948f9db6a5b7326dc083
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-20 13:54:56 +0000

    kubernetes-cli@1.34: add 1.34.3 bottle.

--- Formula/k/kubernetes-cli@1.34.rb
+++ Formula/k/kubernetes-cli@1.34.rb
@@ -11,6 +11,15 @@ class KubernetesCliAT134 < Formula
     regex(/^v?(1\.34(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "bc996eabe837f4cc195c22b732d0c9076c21270b589e9e634b9c88bb4d3f3fd8"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3d25fa75b7eced68467950da9a1f8052264e39725468c4048ec18d48f01fc6a7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "37ef791b26fff253cc9b2829105df60a178d01d015e983cf991a1a12f4e437ae"
+    sha256 cellar: :any_skip_relocation, sonoma:        "5f62627739fcc9a86f0e6b75e5896b30cda24c1a15f38a3b463c6da4dadc26e3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "167e5d458b44e2f47bbcd584f6dbcbc33848191ce2f77cab2398c3c55dacb7b7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1c6ed95b4f152f02bd8a6a210d3d117d6c01bb53564d39f96166cd05bac65f85"
+  end
+
   keg_only :versioned_formula
 
   # https://kubernetes.io/releases/patch-releases/#1-34
