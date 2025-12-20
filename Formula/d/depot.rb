0e898857385696cfff0bcaaee11d6013d8c054ce
Rui Chen <rui@chenrui.dev>
2025-12-19 21:38:30 -0500

    depot 2.100.14 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/d/depot.rb
@@ -0,0 +1,29 @@
+class Depot < Formula
+  desc "Build your Docker images in the cloud"
+  homepage "https://depot.dev/"
+  url "https://github.com/depot/cli/archive/refs/tags/v2.100.14.tar.gz"
+  sha256 "a0c37e6498e883a796ab2eda9eeb9503c0e83c606ae626048c4094ad52ad2624"
+  license "MIT"
+  head "https://github.com/depot/cli.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/depot/cli/internal/build.Version=#{version}
+      -X github.com/depot/cli/internal/build.Date=#{time.iso8601}
+      -X github.com/depot/cli/internal/build.SentryEnvironment=release
+    ]
+
+    system "go", "build", *std_go_args(ldflags:), "./cmd/depot"
+
+    generate_completions_from_executable(bin/"depot", "completion", shells: [:bash, :zsh, :fish, :pwsh])
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/depot --version")
+    output = shell_output("#{bin}/depot list builds 2>&1", 1)
+    assert_match "Error: unknown project ID", output
+  end
+end
