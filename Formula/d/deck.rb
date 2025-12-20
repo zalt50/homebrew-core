a7b8de18ce7903228ff4839ddcf9efdb4ba8a10f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-20 14:13:58 +0000

    deck: update 1.22.1 bottle.

--- Formula/d/deck.rb
+++ Formula/d/deck.rb
@@ -7,12 +7,12 @@ class Deck < Formula
   head "https://github.com/k1LoW/deck.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "dca80b570e848b7fa693a3261065c137b1c4c797e82b28457489b82e5273649f"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "dca80b570e848b7fa693a3261065c137b1c4c797e82b28457489b82e5273649f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "dca80b570e848b7fa693a3261065c137b1c4c797e82b28457489b82e5273649f"
-    sha256 cellar: :any_skip_relocation, sonoma:        "d5f93ccc67fd50bdd66b209fa3ffbdd9d7ab5d53418eb0006fd0424b906d6023"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "345ad8412416161cf76fc4c0c5008069f071cc173baf42915b084b76d975b38b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2d1a7d134f4b93a2e1f999fc0301c8b63d78d09fccda682f669250776c027649"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f39c43993b3b43ec5056c0a9b24d3dc4b4a5321e5d12f8dfb2f8aab5fd2a426d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f39c43993b3b43ec5056c0a9b24d3dc4b4a5321e5d12f8dfb2f8aab5fd2a426d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f39c43993b3b43ec5056c0a9b24d3dc4b4a5321e5d12f8dfb2f8aab5fd2a426d"
+    sha256 cellar: :any_skip_relocation, sonoma:        "b0fc45fc6306b86c2a374c35498269ffacc2706fa1a5315b499399726ed0ec21"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "168f3636894fcba8844201e6595b14a6bdcf3b8afd6fdcc8d95a49cdcdded4cd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fe2444b3939c4d30d14708cb46055b2382ce6481e470298052e25ea64c78fff5"
   end
 
   depends_on "go" => :build
