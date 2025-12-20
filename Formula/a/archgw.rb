30324160198cbb88af36608eded615ff7f329732
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-20 18:29:09 +0900

    archgw: add completions

--- Formula/a/archgw.rb
+++ Formula/a/archgw.rb
@@ -89,6 +89,8 @@ class Archgw < Formula
     ENV["SOURCE_DATE_EPOCH"] = "1451574000"
 
     virtualenv_install_with_resources
+
+    generate_completions_from_executable(bin/"archgw", shell_parameter_format: :click)
   end
 
   test do
