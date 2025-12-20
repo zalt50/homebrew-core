cb042d495528cc876fc448022185aa430bf62f4a
Rui Chen <rui@chenrui.dev>
2025-12-20 17:47:42 -0500

    vacuum 0.21.6 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/v/vacuum.rb
@@ -0,0 +1,37 @@
+class Vacuum < Formula
+  desc "World's fastest OpenAPI & Swagger linter"
+  homepage "https://quobix.com/vacuum/"
+  url "https://github.com/daveshanley/vacuum/archive/refs/tags/v0.21.6.tar.gz"
+  sha256 "99b96080b373af0feaa8179121ac3f2862beadbb9537476255bc186469ae666a"
+  license "MIT"
+  head "https://github.com/daveshanley/vacuum.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X main.version=#{version} -X main.commit=#{tap.user} -X main.date=#{time.iso8601}"
+    system "go", "build", *std_go_args(ldflags:)
+
+    generate_completions_from_executable(bin/"vacuum", "completion", shells: [:bash, :zsh, :fish, :pwsh])
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/vacuum version")
+
+    (testpath/"test-openapi.yml").write <<~YAML
+      openapi: 3.0.0
+      info:
+        title: Test API
+        version: 1.0.0
+      paths:
+        /test:
+          get:
+            responses:
+              '200':
+                description: Successful response
+    YAML
+
+    output = shell_output("#{bin}/vacuum lint #{testpath}/test-openapi.yml 2>&1", 1)
+    assert_match "Failed with 2 errors, 3 warnings and 0 informs.", output
+  end
+end
