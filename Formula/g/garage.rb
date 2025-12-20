7b1c8b499ecf4e2fe0aa134d07e3d12fde80acf3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-20 12:04:32 +0000

    garage: add 2.1.0 bottle.

--- Formula/g/garage.rb
+++ Formula/g/garage.rb
@@ -6,6 +6,15 @@ class Garage < Formula
   license "AGPL-3.0-or-later"
   head "https://git.deuxfleurs.fr/Deuxfleurs/garage.git", branch: "main-v2"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3fe9cdfc6135cb389c27b86d862f83cb68543264af27b7ac5e68930c26d3c333"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4ea2e5f244b5e882010eca9c928302deec305b3104692008da40e499e0633769"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0edd0db35051b652318dd4c7336a162de522634c0453bf5259462a3b4ddf7b58"
+    sha256 cellar: :any_skip_relocation, sonoma:        "dae8779b0b79b263ec567c6502ecfe137724fd015f266181392947a4fd44379f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c5a35bfcd3dc76012dcfa987d2f8b5b4540d79933c6d7ef5d3feca2c366223dd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "15bfa2bf012459491ccc1db5c6136581ed9c6322c1be2f6ee1c3febb4baa2a43"
+  end
+
   depends_on "rust" => :build
 
   def install
