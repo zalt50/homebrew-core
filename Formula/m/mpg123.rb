cac7d3fb3666372adf6118bee3fcdd3d2a63ae04
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-20 14:13:10 +0000

    mpg123 1.33.4

--- Formula/m/mpg123.rb
+++ Formula/m/mpg123.rb
@@ -1,9 +1,9 @@
 class Mpg123 < Formula
   desc "MP3 player for Linux and UNIX"
   homepage "https://www.mpg123.de/"
-  url "https://www.mpg123.de/download/mpg123-1.33.3.tar.bz2"
-  mirror "https://downloads.sourceforge.net/project/mpg123/mpg123/1.33.3/mpg123-1.33.3.tar.bz2"
-  sha256 "6a0c6472dd156e213c2068f40115ebbb73978c2d873e66bae2a250e2d2198d26"
+  url "https://www.mpg123.de/download/mpg123-1.33.4.tar.bz2"
+  mirror "https://downloads.sourceforge.net/project/mpg123/mpg123/1.33.4/mpg123-1.33.4.tar.bz2"
+  sha256 "3ae8c9ff80a97bfc0e22e89fbcd74687eca4fc1db315b12607f27f01cb5a47d9"
   license "LGPL-2.1-only"
 
   livecheck do
