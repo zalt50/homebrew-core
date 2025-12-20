5a50b46be97409e49a70e73aafd93cc22fab16f6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-20 14:30:16 +0000

    vue-language-server 3.2.0

--- Formula/v/vue-language-server.rb
+++ Formula/v/vue-language-server.rb
@@ -1,8 +1,8 @@
 class VueLanguageServer < Formula
   desc "Vue.js language server"
   homepage "https://github.com/vuejs/language-tools"
-  url "https://registry.npmjs.org/@vue/language-server/-/language-server-3.1.8.tgz"
-  sha256 "7cef20586db0fddf51ce0765bb3538482b50d5d038b969347859506323663fb8"
+  url "https://registry.npmjs.org/@vue/language-server/-/language-server-3.2.0.tgz"
+  sha256 "8b942146c111edd08f1eec1f8b439e1e52fe5a981d2fe2c9aa0905638fe0c958"
   license "MIT"
 
   bottle do
