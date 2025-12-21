48e9ddf0eeed5d3bd873f07abe24d7c61a96f404
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-21 17:20:44 +0000

    tweakcc 3.2.2

--- Formula/t/tweakcc.rb
+++ Formula/t/tweakcc.rb
@@ -1,8 +1,8 @@
 class Tweakcc < Formula
   desc "Customize your Claude Code themes, thinking verbs, and more"
   homepage "https://github.com/Piebald-AI/tweakcc"
-  url "https://registry.npmjs.org/tweakcc/-/tweakcc-3.2.1.tgz"
-  sha256 "6489441ada91a6db15e4b6219c0879a2e85028e845d3645f8d09e162eec4b23c"
+  url "https://registry.npmjs.org/tweakcc/-/tweakcc-3.2.2.tgz"
+  sha256 "4a07c4054b044e72561ffd7017b9f9c6e37e87d73c2444f5da31f0916afb73ad"
   license "MIT"
 
   bottle do
