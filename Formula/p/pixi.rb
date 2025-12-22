236a4f757cf3481b45e030e42fcb25a471db8db7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-22 11:23:20 +0000

    pixi 0.62.2

--- Formula/p/pixi.rb
+++ Formula/p/pixi.rb
@@ -1,8 +1,8 @@
 class Pixi < Formula
   desc "Package management made easy"
   homepage "https://pixi.sh"
-  url "https://github.com/prefix-dev/pixi/archive/refs/tags/v0.62.1.tar.gz"
-  sha256 "09c670ba96c0d05a67eb5823ee2398971351e75a61cc225b83e71b3066483820"
+  url "https://github.com/prefix-dev/pixi/archive/refs/tags/v0.62.2.tar.gz"
+  sha256 "dba8db9c836a3bf3c6054588f4334150fe3bb7b4960c778e76f4001a18fa8e2f"
   license "BSD-3-Clause"
   head "https://github.com/prefix-dev/pixi.git", branch: "main"
 
