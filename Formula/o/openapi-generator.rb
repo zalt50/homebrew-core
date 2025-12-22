c6bf6339848fb16a28373c9eb94d3dbf88d4f75f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-22 11:04:26 +0000

    openapi-generator: update 7.18.0 bottle.

--- Formula/o/openapi-generator.rb
+++ Formula/o/openapi-generator.rb
@@ -13,7 +13,7 @@ class OpenapiGenerator < Formula
   no_autobump! because: :requires_manual_review
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "537abe185b2907ab050a65a9810d3b300b339a0b8b3fc315568858f2c00adbb3"
+    sha256 cellar: :any_skip_relocation, all: "da2e25c6faa4675f9892ab2645ec39874c85daea8b9944d454fd58e05789f658"
   end
 
   head do
