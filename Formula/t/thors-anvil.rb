b7af9203f53dfd6a02fca4db12422acc42aa85fe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-22 09:43:20 +0000

    thors-anvil: update 8.0.09 bottle.

--- Formula/t/thors-anvil.rb
+++ Formula/t/thors-anvil.rb
@@ -7,12 +7,12 @@ class ThorsAnvil < Formula
   license "GPL-3.0-only"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "4cd153e8c806c960a028af98f16aac3b98a399f33ca4ce257182f9d94bad5f92"
-    sha256 cellar: :any,                 arm64_sequoia: "0d78a2c2bc758cef78e5231ca7cdcfa40029526f0aa65fe3d4856fd919a61727"
-    sha256 cellar: :any,                 arm64_sonoma:  "c4f90e9403812f492424a157d415bb0aa7971773b243dd36c3d3716ae5baff95"
-    sha256 cellar: :any,                 sonoma:        "49014165cc31b49028797812625303fd8e846c1fec0816c66fc861cba8b9cad3"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "611f80abdf2a31d991e4c816294d37ebed07131e2dd7d3829415c1acaf0fd4e7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c3b48bbe8478d55dda9395e9e7ee1e7f41efd8aab46bf3adf86adfaa2fd6827d"
+    sha256 cellar: :any,                 arm64_tahoe:   "bf29582a85bdce501a354f0c5bdf52fff8c1aba4444d0d0daa2ef1980255e267"
+    sha256 cellar: :any,                 arm64_sequoia: "1919a23d33ccd54fbaf2b6feda2ae894bb463b610da50266e601a4ce7de54544"
+    sha256 cellar: :any,                 arm64_sonoma:  "636f9a2c2e7ad5ca49a9bbc1549febf3fcb58567d938d82428f4b416448ee783"
+    sha256 cellar: :any,                 sonoma:        "f14e8b805205b9c30de4168e6f7b93db525ae64135e9148ccd7c23c5cb873f65"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f9a26efd5212a71006bd5ad90ba10ab4ba06700c6beadbaf56890765e82a9ed2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "39ec255c47f74977dff311332867b5f8d77caf51d5d2f228ebf272c291e5f663"
   end
 
   depends_on "cmake" => :build
