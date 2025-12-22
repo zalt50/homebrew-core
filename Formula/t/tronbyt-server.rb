16e4a9dd10a22cbcb1d3be81759deaa2492da55c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-22 12:33:41 +0000

    tronbyt-server: add 2.0.0 bottle.

--- Formula/t/tronbyt-server.rb
+++ Formula/t/tronbyt-server.rb
@@ -6,6 +6,15 @@ class TronbytServer < Formula
   license "Apache-2.0"
   head "https://github.com/tronbyt/server.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "86dd78a8619d0133c1ebeca1cb2a756a437ed2f65dd8e8e6dc79b53c25452e9f"
+    sha256 cellar: :any,                 arm64_sequoia: "926d6424760354e0b3261b0271b2ff62ee80b5fcdc7cd4ad68006de9ed9b6848"
+    sha256 cellar: :any,                 arm64_sonoma:  "b001f37a0f15f03ac9d89ba0a96d60fcffb78a65ae8f99aa4072fa044feb07be"
+    sha256 cellar: :any,                 sonoma:        "4ad66c3652911b4f6b7826a0349556e9e7e64efa3c5639791f6dc97ab8f8fb63"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "942d32e8273792f90e4694488abcffa35f91deb8c7ab4462e98e1d9dafeb74ec"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1b413339961fd7bf1b31b3c0cabc39935053a032dbaefef5c040a343e9a48e56"
+  end
+
   depends_on "go" => :build
   depends_on "pkgconf" => :build
   depends_on "webp"
