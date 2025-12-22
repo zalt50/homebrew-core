fc5e1f2fa0c9cd7ee01e32b3eca47368d602881e
Rui Chen <rui@chenrui.dev>
2025-12-22 08:37:04 -0500

    logstalgia: revision bump (glew 2.3.0)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/l/logstalgia.rb
+++ Formula/l/logstalgia.rb
@@ -4,7 +4,7 @@ class Logstalgia < Formula
   url "https://github.com/acaudwell/Logstalgia/releases/download/logstalgia-1.1.4/logstalgia-1.1.4.tar.gz"
   sha256 "c049eff405e924035222edb26bcc6c7b5f00a08926abdb7b467e2449242790a9"
   license "GPL-3.0-or-later"
-  revision 9
+  revision 10
 
   no_autobump! because: :requires_manual_review
 
