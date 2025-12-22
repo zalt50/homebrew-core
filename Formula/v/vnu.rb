00c3b260d6c117016e0da89fb65f086ea2669a44
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-22 10:48:21 +0000

    vnu: update 25.12.22 bottle.

--- Formula/v/vnu.rb
+++ Formula/v/vnu.rb
@@ -7,7 +7,7 @@ class Vnu < Formula
   version_scheme 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "3d6cfd07608f007c22f2f71a9635f395228f5f0d9d1268ccdac6faccb70a72fb"
+    sha256 cellar: :any_skip_relocation, all: "b40794d9bfe750852fc719e526a1b571b99a66de97280b84988e42dc569ab76c"
   end
 
   depends_on "openjdk"
