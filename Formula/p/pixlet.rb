b7580ac7ff4315ee76c5f1de89b1007f3c51731e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-22 17:26:33 +0000

    pixlet: add 0.49.7 bottle.

--- Formula/p/pixlet.rb
+++ Formula/p/pixlet.rb
@@ -6,6 +6,15 @@ class Pixlet < Formula
   license "Apache-2.0"
   head "https://github.com/tronbyt/pixlet.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "e7006149ff3de6757812317efb418126206df80e3698735fe0784b5bd6518fbe"
+    sha256 cellar: :any,                 arm64_sequoia: "4fa36e2efc607018d783f55ae89c3d2f8bc76bc8ee495a915335a5b8cca4ec73"
+    sha256 cellar: :any,                 arm64_sonoma:  "6557d738b0e023ae751f77104202a21d9aca15555ab31fdc75c5204252ba6f93"
+    sha256 cellar: :any,                 sonoma:        "453d460cc022d5f5ede9966f6af86d01fc39e9d63c7f44cc73525e732f1bda95"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3e8224ed39649220dd25a69060698f198e2508b589c925eade6f80cc4a1b9f23"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8255890a67492eaa1554e5f87da53a402cc4a15d003fcfd58d69c2b0de02ef5c"
+  end
+
   depends_on "go" => :build
   depends_on "node" => :build
   depends_on "webp"
