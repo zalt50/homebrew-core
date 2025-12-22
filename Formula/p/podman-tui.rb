65d14ea75a0ead24701fb62d59b604a6bf59e57d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-22 01:44:40 +0000

    podman-tui: update 1.10.0 bottle.

--- Formula/p/podman-tui.rb
+++ Formula/p/podman-tui.rb
@@ -9,12 +9,12 @@ class PodmanTui < Formula
   no_autobump! because: :bumped_by_upstream
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "159fe3cbe81cb90352a7cdd6e9fecda9ffbe0d4ed70b8238d23b28a3a577cad3"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ae8d3b12b6d240b955412b516b7481d078f0df567ee1bf2505b6f5d7fa0fee2f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2ce978d60030735023a0ea7b7c60654264d8a78766ec62f04cebb5fce418be85"
-    sha256 cellar: :any_skip_relocation, sonoma:        "0c3901ccd6a710f3a1ba420543192de39ef4ed1682e3259a6612b4cdc382f031"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "fb3be056e3171386c8d9c3819f52b91e44c318d2af71e9689dd744a16f1fd134"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "04453e9ee308df6601ac642852498519989e8213cac4dcf1699fd9495d2e7c3f"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f218ff4bbdb0c77eb22065a91b58b4e59946127ea07c5058523ac4ac0da1ac62"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f218ff4bbdb0c77eb22065a91b58b4e59946127ea07c5058523ac4ac0da1ac62"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f218ff4bbdb0c77eb22065a91b58b4e59946127ea07c5058523ac4ac0da1ac62"
+    sha256 cellar: :any_skip_relocation, sonoma:        "9d6860b0fc32d058effc6bb0b4c2ba72dbfc426de00e35a06d2c0b018a54f4d6"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a6386fd9b908a05dba101126a17b574838bb21596e831e25a714115c8ef0b810"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b7a9c505b045074bc7215181bc4d6c5bc5eec600a126888023ca7fee98a19b05"
   end
 
   depends_on "go" => :build
