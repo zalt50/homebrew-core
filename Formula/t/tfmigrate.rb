cb5c4356c30d6b9e3c6552f672b97659e2b497e3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-22 02:59:40 +0000

    tfmigrate 0.4.4

--- Formula/t/tfmigrate.rb
+++ Formula/t/tfmigrate.rb
@@ -1,8 +1,8 @@
 class Tfmigrate < Formula
   desc "Terraform/OpenTofu state migration tool for GitOps"
   homepage "https://github.com/minamijoyo/tfmigrate"
-  url "https://github.com/minamijoyo/tfmigrate/archive/refs/tags/v0.4.3.tar.gz"
-  sha256 "6630aae0d443827e222a0344096e189d05a4b9897afd1b8a9499ba6173d6eb8d"
+  url "https://github.com/minamijoyo/tfmigrate/archive/refs/tags/v0.4.4.tar.gz"
+  sha256 "85d8fa6881efdd444a3102dfbddc0559380643d2d72d871c1cbe3cfd3df1902d"
   license "MIT"
   head "https://github.com/minamijoyo/tfmigrate.git", branch: "master"
 
