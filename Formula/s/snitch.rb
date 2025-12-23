43aeba286e6dd9b3ca026b84464ab291e8fbd9ca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-23 13:27:08 +0000

    snitch: add 0.1.8 bottle.

--- Formula/s/snitch.rb
+++ Formula/s/snitch.rb
@@ -6,6 +6,15 @@ class Snitch < Formula
   license "MIT"
   head "https://github.com/karol-broda/snitch.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2e246e974728965429127e0c3643de8eba12f6ea805ede435973f4ccca7c3fc7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3aeb8145f36d69a6be6e92cbc399ff1d5ffcf26b807f54422224ab73b0107fcf"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0a65217b5e9ede694b59593bf875e1cee96eb9cf8127b134673b9f0547754f04"
+    sha256 cellar: :any_skip_relocation, sonoma:        "143199be11d530c3abaac5b0433033bcdc1e76d6e703d16d5e9eb4325d6246fa"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "633d992f34afa3d47f223209ef13b5c5930331ec9de641dd61e6dcdcf7dd0472"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7b338548750f5ea0cf7cfd9c99532f570b1c829b0ea2334e7aba53c9432c82b4"
+  end
+
   depends_on "go" => :build
 
   def install
