27578b02b9dbf0025bfe2e564f8f26449eb277b2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-23 13:01:18 +0000

    glew: update 2.3.0 bottle.

--- Formula/g/glew.rb
+++ Formula/g/glew.rb
@@ -7,20 +7,12 @@ class Glew < Formula
   head "https://github.com/nigels-com/glew.git", branch: "master"
 
   bottle do
-    rebuild 2
-    sha256 cellar: :any,                 arm64_tahoe:    "af7df563c00e50d3ab300d1daba874e67c997b9f18a57581ee69d9d2216d4745"
-    sha256 cellar: :any,                 arm64_sequoia:  "4ac8264612c4af3b6864eed07564e14ddf81c25a050aa2bc91953966d12e73e4"
-    sha256 cellar: :any,                 arm64_sonoma:   "05aa1fad57b8dd0d68045a54b66ad9d61c494584560a55512a2123d22849e467"
-    sha256 cellar: :any,                 arm64_ventura:  "33b1499e0219c3980310dee9e6b115af3ef0324723af7c3a0ff9a68ac7b3e841"
-    sha256 cellar: :any,                 arm64_monterey: "a116faecf407ee2a00cb775a3b668fe0f5753ceecd73678d20b3656e6c56d163"
-    sha256 cellar: :any,                 arm64_big_sur:  "088dedfcd45fe37b0d027b52bb1c730e01aeacda4d7b00ce14f67a19d1961bce"
-    sha256 cellar: :any,                 sonoma:         "b09c9df6478e5d9c3337bffd756f78c2cf6bf5e5de0bf7db8066e8683c2bb3a1"
-    sha256 cellar: :any,                 ventura:        "a9850b75eb81c4b3d5f81209fe7a9b3cd848444df83c6a391ff9d77096ba6e58"
-    sha256 cellar: :any,                 monterey:       "9d8d8c93eec4287a9231cd0378b45ee3b9735afca387fc1f5def7e2c68533097"
-    sha256 cellar: :any,                 big_sur:        "728e40242af0b9a53ae837de3d2658f205e121a04285de29f3964c2dd7512a9d"
-    sha256 cellar: :any,                 catalina:       "ee50985ccbbcd0ec1980960b7fb31fce80e99450f14ae02a751a731056182d34"
-    sha256 cellar: :any_skip_relocation, arm64_linux:    "4299aaba365fcecffb07e5d87bff754833e8e9b7a26ba648691185a97a592fab"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7bc36f86706af951931a2c4c905b8b680cf67606406d238fbfd8923f6109e626"
+    sha256 cellar: :any,                 arm64_tahoe:   "2609a8eb123eb636047472bd9b50b7dc680cfaf777a3bbc78dc357f6b3b0b92a"
+    sha256 cellar: :any,                 arm64_sequoia: "9ec2825bcfd6801de72ccc23615dd322f563b7e8179374daf310bbcbd62f60d0"
+    sha256 cellar: :any,                 arm64_sonoma:  "ccda6940121477bff671c1bead8402196779ccef74dbe722a3ed52c44a5ff88c"
+    sha256 cellar: :any,                 sonoma:        "28db4c9a63529fafc90f3f63fc487f5c5905faea7ff01561813cd28f16340ff1"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f68c506af46645931cef5812fdd7b515422571a5319c3572e6c5ebf99746fe54"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3c738eba52384ae853e552d06fb8ab7d8df51a725c6a0c0646356bb7850a2892"
   end
 
   depends_on "cmake" => [:build, :test]
