c1a9d3f4add6e4d523132a45ce34ca76ab026514
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-24 02:17:41 +0900

    fastapi: update `:typer` completion

--- Formula/f/fastapi.rb
+++ Formula/f/fastapi.rb
@@ -204,8 +204,7 @@ class Fastapi < Formula
     virtualenv_install_with_resources
     bin.install_symlink libexec/"bin/fastapi"
 
-    ENV["_TYPER_COMPLETE_TEST_DISABLE_SHELL_DETECTION"] = "1"
-    generate_completions_from_executable(bin/"fastapi", "--show-completion", shells: [:bash, :zsh, :fish])
+    generate_completions_from_executable(bin/"fastapi", shell_parameter_format: :typer)
   end
 
   test do
