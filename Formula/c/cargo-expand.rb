a84115083a7869744b002ab5ec242200c82fefa3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-23 20:20:40 +0000

    cargo-expand: update 1.0.119 bottle.

--- Formula/c/cargo-expand.rb
+++ Formula/c/cargo-expand.rb
@@ -7,12 +7,12 @@ class CargoExpand < Formula
   head "https://github.com/dtolnay/cargo-expand.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "eccf5f8013596a6fa109d66722e400655fcc00845e84c1b59a61a18df1963017"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2ea640cfa397909111bb29d08658950c5aa32049831ef23eb56270728604bc9e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7bf5d6ba0b646360a2f4604cc5719b40676f7d2ab3896c577e6993f31211d133"
-    sha256 cellar: :any_skip_relocation, sonoma:        "1ddc9072c7f9c58eca6ee2e388bd8ef6bdc36dda7fceaba05f15caff32306dba"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f5243cda443ca4b58cac6930c1a48f79b20644b2401acf55bcad1e40c4c5890e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a3781dfaca54afef9a33b0fd12ad00552894d6b84e026e0e757b1149f7a2a6bb"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "edf4e8b9e99bdbdea745dd5ede756debf8984ccf0dd61071ef6d81b7140950c4"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "363b9b225b848bb67b5b111c834cbd42e0408d4c8bb390913dd2bd69084a5989"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9a6260c40dbe842fa826bedede2da7a344f171306b8a2829a37119dcb38097e5"
+    sha256 cellar: :any_skip_relocation, sonoma:        "5c9b272c580e62e6438fd39043703709d0f84554111b79e4d32467dc3bc9ad00"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d85452155ab6c0b1549a0705b198c330c9d5cf0054296359c6c3f344d7665814"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "eb6b38bdf57f68ab2ad2897db12473d31e8714d226a34b528fa2f21cc138ce28"
   end
 
   depends_on "rust" => :build
