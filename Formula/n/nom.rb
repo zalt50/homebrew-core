d055f3f5106f9e718a9faa947f9c42ed47413b63
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-23 17:23:58 +0000

    nom 3.0.0

--- Formula/n/nom.rb
+++ Formula/n/nom.rb
@@ -1,8 +1,8 @@
 class Nom < Formula
   desc "RSS reader for the terminal"
   homepage "https://github.com/guyfedwards/nom"
-  url "https://github.com/guyfedwards/nom/archive/refs/tags/v2.20.0.tar.gz"
-  sha256 "cba8e70ed7e5b9da97dfbcf5929f8ee134a10115a64bb5d9167acfa0a62650d9"
+  url "https://github.com/guyfedwards/nom/archive/refs/tags/v3.0.0.tar.gz"
+  sha256 "c034527ed46c55b7ac896ce41d05df7425531343abcc97001eb020065e744d3d"
   license "GPL-3.0-only"
   head "https://github.com/guyfedwards/nom.git", branch: "master"
 
