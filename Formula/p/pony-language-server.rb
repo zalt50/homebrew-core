913a3faaa024b75b1fa69c4934e9c070f5edb201
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-23 14:32:46 +0000

    pony-language-server: add 0.2.2 bottle.

--- Formula/p/pony-language-server.rb
+++ Formula/p/pony-language-server.rb
@@ -6,6 +6,15 @@ class PonyLanguageServer < Formula
   license "MIT"
   head "https://github.com/ponylang/pony-language-server.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4f60d5a06961779fc4e76ec4812e0dd5b5f2c2b47e0ba5de68ede41e7a9145cc"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e2fd0cb00af203cd447072ca33793e773b2e10cf893568b8bdbec29b56456d81"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a3e32a0caa61451f6c6ae5e7817be7a72db317932d33a93bc3c3b0bdeeb8d558"
+    sha256 cellar: :any_skip_relocation, sonoma:        "11b338e57a2671b0187a4ef14332ee6729390dbc5d3497e8a1fd7392c203ab77"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ee42c413ed4c9377b2277d93dd955c5b17ade08ebafa47d2e0d8d27b36ff612a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "364ecccee334c255b515a75e868708a6c0ac4e0eb4cfe1ba98db7fe46fc29874"
+  end
+
   depends_on "corral" => :build
   depends_on "ponyc" => :build
 
