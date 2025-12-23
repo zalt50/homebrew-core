b776c5ab184646018cb381815bac2fcd77138ab4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-23 17:36:29 +0000

    snitch 0.1.9

--- Formula/s/snitch.rb
+++ Formula/s/snitch.rb
@@ -1,8 +1,8 @@
 class Snitch < Formula
   desc "Prettier way to inspect network connections"
   homepage "https://github.com/karol-broda/snitch"
-  url "https://github.com/karol-broda/snitch/archive/refs/tags/v0.1.8.tar.gz"
-  sha256 "308d7f5a91deb55feaf525bc2d8e02c04f9717090a17b1ac14a275c0363e3f2d"
+  url "https://github.com/karol-broda/snitch/archive/refs/tags/v0.1.9.tar.gz"
+  sha256 "5dce1da7674ffd46ad9aefdd638a52b06fd4f1862fb19d5d087dcd16a429bf76"
   license "MIT"
   head "https://github.com/karol-broda/snitch.git", branch: "master"
 
@@ -18,7 +18,12 @@ class Snitch < Formula
   depends_on "go" => :build
 
   def install
-    ldflags = "-s -w -X snitch/cmd.Version=#{version} -X snitch/cmd.Commit=#{tap.user}"
+    ldflags = %W[
+      -s -w
+      -X github.com/karol-broda/snitch/cmd.Version=#{version}
+      -X github.com/karol-broda/snitch/cmd.Date=#{time.iso8601}
+      -X github.com/karol-broda/snitch/cmd.Commit=#{tap.user}
+    ]
     system "go", "build", *std_go_args(ldflags:)
     generate_completions_from_executable(bin/"snitch", "completion")
   end
