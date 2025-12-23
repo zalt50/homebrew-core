f8e84d103199285b5ce85c2defc94938b1c4966f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-23 17:10:40 +0000

    go-passbolt-cli 0.4.0

--- Formula/g/go-passbolt-cli.rb
+++ Formula/g/go-passbolt-cli.rb
@@ -1,8 +1,8 @@
 class GoPassboltCli < Formula
   desc "CLI for passbolt"
   homepage "https://www.passbolt.com/"
-  url "https://github.com/passbolt/go-passbolt-cli/archive/refs/tags/v0.3.2.tar.gz"
-  sha256 "4e88aa088b68a7101ca89a97184eb5427aa9b70d52df077f7d56c2ff3672fe06"
+  url "https://github.com/passbolt/go-passbolt-cli/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "009042a200146ddb1c8c2ed20084efb1fc65047219ef21a21d6c490af228f0cf"
   license "MIT"
   head "https://github.com/passbolt/go-passbolt-cli.git", branch: "main"
 
