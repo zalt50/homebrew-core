6a460da46b060c44857af39f622ab72dbb87ce37
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-24 11:37:55 +0000

    osctrl-cli: update 0.4.9 bottle.

--- Formula/o/osctrl-cli.rb
+++ Formula/o/osctrl-cli.rb
@@ -7,12 +7,12 @@ class OsctrlCli < Formula
   head "https://github.com/jmpsec/osctrl.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e8506350927946f262d00c631bb15150454f133d46c739f47a23fbcb435bbd33"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4902066b1a8081d124c7fee1a05ef27bb5c984011966117a6547266e8cc38673"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cb823bdd37c50898abab6b97fba031ddf30cc04acbdc344d3426405e8fda05c1"
-    sha256 cellar: :any_skip_relocation, sonoma:        "e66735b66dd72b618e10e677233fbce78ef91a80ecfcc29c1ee060d3b67c2fa1"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f173b89ded7563601558a11bd58b9d428a7ba9354012c09524fecf6fdeabe47b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f431a137098445549637dd5e864690556a870b20fe833a31a48afd81424ae439"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1e60848915cfcbcbb8af8f1e310b8f917a698c30d896e06aa162595400356cea"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "982f6c20e7828d73bcecb383114ca8185ffac508e5f437bd774133e7b89aab90"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "dd65025c23f3ccc7cbb77d143f07ea5d4e57129b7e41581264ff1cb59bf17555"
+    sha256 cellar: :any_skip_relocation, sonoma:        "b3be7576566a70a7b2012796e0b13ea5cf8c85479ae436f5dc26fa7bb1ca53f1"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3771728b89f63b2c491aa01b342f1fb29e951250907474c28904472f766833fa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "acb9aa2168fb41a142e3fe369410fdcb9d9fb88c3100391136a64bf49c586590"
   end
 
   depends_on "go" => :build
