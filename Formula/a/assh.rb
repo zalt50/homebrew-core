13763d513ea72a04901081a7b46999c46c6c6e52
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-24 12:54:48 +0900

    assh: update :cobra completion

--- Formula/a/assh.rb
+++ Formula/a/assh.rb
@@ -24,7 +24,7 @@ class Assh < Formula
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w")
 
-    generate_completions_from_executable(bin/"assh", "completion")
+    generate_completions_from_executable(bin/"assh", shell_parameter_format: :cobra)
   end
 
   test do
