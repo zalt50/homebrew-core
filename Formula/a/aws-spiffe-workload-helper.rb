a15dbda511ddb33eacd4dd365b8aacbe462fc0c9
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-24 12:58:11 +0900

    aws-spiffe-workload-helper: update :cobra completion

--- Formula/a/aws-spiffe-workload-helper.rb
+++ Formula/a/aws-spiffe-workload-helper.rb
@@ -20,8 +20,7 @@ class AwsSpiffeWorkloadHelper < Formula
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"), "./cmd"
 
-    generate_completions_from_executable(bin/"aws-spiffe-workload-helper",
-                                             "completion", shells: [:bash, :zsh, :fish, :pwsh])
+    generate_completions_from_executable(bin/"aws-spiffe-workload-helper", shell_parameter_format: :cobra)
   end
 
   test do
