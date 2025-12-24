c4032ee88d1d0f673b83758c9ee8c4d0ac586536
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-24 03:27:23 +0000

    superseedr: update 0.9.29 bottle.

--- Formula/s/superseedr.rb
+++ Formula/s/superseedr.rb
@@ -7,12 +7,12 @@ class Superseedr < Formula
   head "https://github.com/Jagalite/superseedr.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b3bd5e2cc68207cc0b7e46351d47d9ebe1e5d9ab86dee8c5437d51064f10261b"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "578c6d088af3818e652629ae3a39ba85a63e5c70ecd4cc8f0e9e592f6a2a579d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "90c6a4c9e07e51fff39e31a04b024f949227e4241693f6b00da7e647751e4c0e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "2d03b548485055980f3f0f11b8b4c4c0fa045027447c28581e8d05cb7456e9c5"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d7e7f425a212e6b8a8e4791535392f6d1ddf84f35d0acc9d673636ed4a6280d2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "657bf70b2994aec8704dceb2b8865226a4b13a9b13ac9d8a36dc90b073d15704"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e427f53ad32344829432e486ec281d9254011c7c1b22079351c6df174bc3678f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "73c94bec07f5710ad26170746581797d46400e0c91d96b4b9c783285c7354e1f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "66ce35864afb9bea221931bb043fded2c5f6031bc5cd3dc7e21c338fdc360b62"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a10a1c2bca4ffc8cf065f04bdd728d8ebcb62699e414b55e3f21f882db193fa2"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "8c54ffd817599933f3cc4ee0abc4bac8ae07b6c2a337a496aea8f80eeffb7695"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bc68b417a4fe4a00ada07208454c36828a8a008bdb5157106da6a57c01f86c00"
   end
 
   depends_on "pkgconf" => :build
