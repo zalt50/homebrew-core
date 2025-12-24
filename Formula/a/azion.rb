1c3542541ffa64534ebe3e718f85cb381468b2cf
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-24 13:03:38 +0900

    azion: update :cobra completion

--- Formula/a/azion.rb
+++ Formula/a/azion.rb
@@ -28,7 +28,7 @@ class Azion < Formula
     ]
     system "go", "build", *std_go_args(ldflags:), "./cmd/azion"
 
-    generate_completions_from_executable(bin/"azion", "completion")
+    generate_completions_from_executable(bin/"azion", shell_parameter_format: :cobra)
   end
 
   test do
