99cf35796c1f5f140a90b7d1411bf8b85a4198fc
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-24 12:31:04 +0900

    arduino-cli: update :cobra completion

--- Formula/a/arduino-cli.rb
+++ Formula/a/arduino-cli.rb
@@ -31,7 +31,7 @@ class ArduinoCli < Formula
     ]
     system "go", "build", *std_go_args(ldflags:)
 
-    generate_completions_from_executable(bin/"arduino-cli", "completion")
+    generate_completions_from_executable(bin/"arduino-cli", shell_parameter_format: :cobra)
   end
 
   test do
