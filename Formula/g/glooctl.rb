d3196a0380c9f9eda14eff8697e292dd484d969c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-24 17:02:42 +0000

    glooctl 1.20.6

--- Formula/g/glooctl.rb
+++ Formula/g/glooctl.rb
@@ -4,8 +4,8 @@ class Glooctl < Formula
   # NOTE: Please wait until the newest stable release is finished building and
   # no longer marked as "Pre-release" before creating a PR for a new version.
   url "https://github.com/solo-io/gloo.git",
-      tag:      "v1.20.5",
-      revision: "2958882043c371a422c8fe964c4cef8dece7461a"
+      tag:      "v1.20.6",
+      revision: "79a4e04862fc0a5948bba9fc9db58d579480bef5"
   license "Apache-2.0"
   head "https://github.com/solo-io/gloo.git", branch: "main"
 
