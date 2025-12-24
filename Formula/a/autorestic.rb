4e41aae156c32b9719483ee47aa0f0f814ccb64e
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-24 12:57:25 +0900

    autorestic: update :cobra completion

--- Formula/a/autorestic.rb
+++ Formula/a/autorestic.rb
@@ -24,7 +24,7 @@ class Autorestic < Formula
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w")
-    generate_completions_from_executable(bin/"autorestic", "completion")
+    generate_completions_from_executable(bin/"autorestic", shell_parameter_format: :cobra)
   end
 
   test do
