267dfa87abadca97639c4cd3f290db045ad0c96d
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-25 17:46:26 +0900

    gwctl: update :cobra completion

--- Formula/g/gwctl.rb
+++ Formula/g/gwctl.rb
@@ -26,7 +26,7 @@ class Gwctl < Formula
     ]
     system "go", "build", *std_go_args(ldflags:)
 
-    generate_completions_from_executable(bin/"gwctl", "completion", shells: [:bash, :zsh, :fish, :pwsh])
+    generate_completions_from_executable(bin/"gwctl", shell_parameter_format: :cobra)
   end
 
   test do
