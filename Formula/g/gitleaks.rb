b9613970174cad4a29d4ad47998ad2e3b3973599
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-25 17:32:24 +0900

    gitleaks: update :cobra completion

--- Formula/g/gitleaks.rb
+++ Formula/g/gitleaks.rb
@@ -29,7 +29,7 @@ class Gitleaks < Formula
     ldflags = "-s -w -X github.com/zricethezav/gitleaks/v#{version.major}/version.Version=#{version}"
     system "go", "build", *std_go_args(ldflags:)
 
-    generate_completions_from_executable(bin/"gitleaks", "completion")
+    generate_completions_from_executable(bin/"gitleaks", shell_parameter_format: :cobra)
   end
 
   test do
