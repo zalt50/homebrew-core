a7b7248fef05763aa24460b19bb22b4698878d86
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-25 17:42:18 +0900

    go-blueprint: update :cobra completion

--- Formula/g/go-blueprint.rb
+++ Formula/g/go-blueprint.rb
@@ -22,7 +22,7 @@ class GoBlueprint < Formula
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/melkeydev/go-blueprint/cmd.GoBlueprintVersion=#{version}")
 
-    generate_completions_from_executable(bin/"go-blueprint", "completion")
+    generate_completions_from_executable(bin/"go-blueprint", shell_parameter_format: :cobra)
   end
 
   test do
