9f1f1b9c280ad84d733667d55bc7287663200052
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-25 15:06:48 +0900

    changie: update :cobra completion

--- Formula/c/changie.rb
+++ Formula/c/changie.rb
@@ -21,7 +21,7 @@ class Changie < Formula
     ldflags = "-s -w -X main.version=#{version}"
     system "go", "build", *std_go_args(ldflags:)
 
-    generate_completions_from_executable(bin/"changie", "completion")
+    generate_completions_from_executable(bin/"changie", shell_parameter_format: :cobra)
   end
 
   test do
