2c9311d395df42985fd9f50dabfeb4d7c77a94be
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-25 15:18:37 +0900

    cobra-cli: update :cobra completion

--- Formula/c/cobra-cli.rb
+++ Formula/c/cobra-cli.rb
@@ -23,7 +23,7 @@ class CobraCli < Formula
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w")
-    generate_completions_from_executable(bin/"cobra-cli", "completion")
+    generate_completions_from_executable(bin/"cobra-cli", shell_parameter_format: :cobra)
   end
 
   test do
