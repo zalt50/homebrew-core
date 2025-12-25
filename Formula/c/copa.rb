112bc63136ca057bed266e61eb53f9281ec7a4b2
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-25 15:19:06 +0900

    copa: update :cobra completion

--- Formula/c/copa.rb
+++ Formula/c/copa.rb
@@ -27,7 +27,7 @@ class Copa < Formula
       -X main.version=#{version}
     ]
     system "go", "build", *std_go_args(ldflags:)
-    generate_completions_from_executable(bin/"copa", "completion", shells: [:bash, :zsh, :fish, :pwsh])
+    generate_completions_from_executable(bin/"copa", shell_parameter_format: :cobra)
   end
 
   test do
