b06997549ecf390ad76120fb0398bbbfb33f980a
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-25 15:09:14 +0900

    cloud-sql-proxy: update :cobra completion

--- Formula/c/cloud-sql-proxy.rb
+++ Formula/c/cloud-sql-proxy.rb
@@ -20,7 +20,7 @@ class CloudSqlProxy < Formula
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w")
-    generate_completions_from_executable(bin/"cloud-sql-proxy", "completion", shells: [:bash, :zsh, :fish, :pwsh])
+    generate_completions_from_executable(bin/"cloud-sql-proxy", shell_parameter_format: :cobra)
   end
 
   test do
