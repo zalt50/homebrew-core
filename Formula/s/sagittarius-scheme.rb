f69a0018ab1f2ed14c3f6a80517031a9f8675ef6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-25 03:03:18 +0000

    sagittarius-scheme 0.9.14

--- Formula/s/sagittarius-scheme.rb
+++ Formula/s/sagittarius-scheme.rb
@@ -1,8 +1,8 @@
 class SagittariusScheme < Formula
   desc "Free Scheme implementation supporting R6RS and R7RS"
   homepage "https://github.com/ktakashi/sagittarius-scheme"
-  url "https://github.com/ktakashi/sagittarius-scheme/releases/download/v0.9.13/sagittarius-0.9.13.tar.gz"
-  sha256 "8cf812190c43738a9d6021e677c928a80c05cad1a03047c1868c865ccfd65773"
+  url "https://github.com/ktakashi/sagittarius-scheme/releases/download/v0.9.14/sagittarius-0.9.14.tar.gz"
+  sha256 "2f464a0a249b9de59ed7e7338fcdf1f9b0873d9c35bae612749c0a1e9e2a4e79"
   license "BSD-2-Clause"
 
   livecheck do
