1dd0d9ca203ae9f9ee77f6eae5fe2767f86db1e8
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-25 19:28:00 +0900

    k8sgpt: update :cobra completion

--- Formula/k/k8sgpt.rb
+++ Formula/k/k8sgpt.rb
@@ -21,7 +21,7 @@ class K8sgpt < Formula
     ldflags = "-s -w -X main.version=#{version} -X main.commit=#{tap.user} -X main.date=#{time.iso8601}"
     system "go", "build", *std_go_args(ldflags:)
 
-    generate_completions_from_executable(bin/"k8sgpt", "completion")
+    generate_completions_from_executable(bin/"k8sgpt", shell_parameter_format: :cobra)
   end
 
   test do
