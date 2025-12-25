f7f509908549ff4a98b50069e8dfa427bfce2537
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-25 19:39:53 +0900

    kubectl-ai: update :cobra completion

--- Formula/k/kubectl-ai.rb
+++ Formula/k/kubectl-ai.rb
@@ -25,7 +25,7 @@ class KubectlAi < Formula
     ldflags = "-s -w -X main.version=#{version} -X main.commit=#{tap.user} -X main.date=#{time.iso8601}"
     system "go", "build", *std_go_args(ldflags:), "./cmd"
 
-    generate_completions_from_executable(bin/"kubectl-ai", "completion", shells: [:bash, :zsh, :fish, :pwsh])
+    generate_completions_from_executable(bin/"kubectl-ai", shell_parameter_format: :cobra)
   end
 
   test do
