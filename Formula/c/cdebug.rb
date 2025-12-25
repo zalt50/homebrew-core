4f0a337d53a352ab805459e63492e6b21b3cc03a
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-25 15:06:01 +0900

    cdebug: update :cobra completion

--- Formula/c/cdebug.rb
+++ Formula/c/cdebug.rb
@@ -30,7 +30,7 @@ class Cdebug < Formula
     ]
     system "go", "build", *std_go_args(ldflags:)
 
-    generate_completions_from_executable(bin/"cdebug", "completion")
+    generate_completions_from_executable(bin/"cdebug", shell_parameter_format: :cobra)
   end
 
   test do
