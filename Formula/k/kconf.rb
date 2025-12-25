f352fcc12265e00472b02bed3246132e5b0498c7
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-25 19:28:37 +0900

    kconf: update :cobra completion

--- Formula/k/kconf.rb
+++ Formula/k/kconf.rb
@@ -33,7 +33,7 @@ class Kconf < Formula
 
     system "go", "build", *std_go_args(ldflags:)
 
-    generate_completions_from_executable(bin/"kconf", "completion")
+    generate_completions_from_executable(bin/"kconf", shell_parameter_format: :cobra)
   end
 
   test do
