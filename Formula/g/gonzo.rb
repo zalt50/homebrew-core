0fb683f820862f2c6e19a5ab43ecb8f9fb13317c
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-25 17:42:59 +0900

    gonzo: update :cobra completion

--- Formula/g/gonzo.rb
+++ Formula/g/gonzo.rb
@@ -27,7 +27,7 @@ class Gonzo < Formula
     ]
     system "go", "build", *std_go_args(ldflags:), "./cmd/gonzo"
 
-    generate_completions_from_executable(bin/"gonzo", "completion", shells: [:bash, :zsh, :fish, :pwsh])
+    generate_completions_from_executable(bin/"gonzo", shell_parameter_format: :cobra)
   end
 
   test do
