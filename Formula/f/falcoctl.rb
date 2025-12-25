a2ff221d2032f9bb7000fb4f22f6847b12b8d3bf
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-25 17:10:07 +0900

    falcoctl: update :cobra completion

--- Formula/f/falcoctl.rb
+++ Formula/f/falcoctl.rb
@@ -28,7 +28,7 @@ class Falcoctl < Formula
 
     system "go", "build", *std_go_args(ldflags:), "."
 
-    generate_completions_from_executable(bin/"falcoctl", "completion")
+    generate_completions_from_executable(bin/"falcoctl", shell_parameter_format: :cobra)
   end
 
   test do
