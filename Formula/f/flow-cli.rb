a6e0a84d9d957a9fc20890197c5b284d3633c532
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-25 17:10:51 +0900

    flow-cli: update :cobra completion

--- Formula/f/flow-cli.rb
+++ Formula/f/flow-cli.rb
@@ -28,7 +28,7 @@ class FlowCli < Formula
     system "make", "cmd/flow/flow", "VERSION=v#{version}"
     bin.install "cmd/flow/flow"
 
-    generate_completions_from_executable(bin/"flow", "completion")
+    generate_completions_from_executable(bin/"flow", shell_parameter_format: :cobra)
   end
 
   test do
