1bfa89d243f463014c177829c87ba2ca31e93f50
Thierry Moisan <thierry.moisan@gmail.com>
2025-12-25 11:55:04 -0500

    dagu: add shell completions

--- Formula/d/dagu.rb
+++ Formula/d/dagu.rb
@@ -26,6 +26,7 @@ class Dagu < Formula
 
     ldflags = "-s -w -X main.version=#{version}"
     system "go", "build", *std_go_args(ldflags:), "./cmd"
+    generate_completions_from_executable(bin/"dagu", shell_parameter_format: :cobra)
   end
 
   service do
