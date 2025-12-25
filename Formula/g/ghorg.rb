e12753f6e329c65be7768cb30086860376b98d52
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-25 17:31:57 +0900

    ghorg: update :cobra completion

--- Formula/g/ghorg.rb
+++ Formula/g/ghorg.rb
@@ -20,7 +20,7 @@ class Ghorg < Formula
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w")
 
-    generate_completions_from_executable(bin/"ghorg", "completion")
+    generate_completions_from_executable(bin/"ghorg", shell_parameter_format: :cobra)
   end
 
   test do
