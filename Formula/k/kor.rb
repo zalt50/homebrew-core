688f32410273e607d29eb6bace89439559fabac6
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-25 19:32:21 +0900

    kor: update :cobra completion

--- Formula/k/kor.rb
+++ Formula/k/kor.rb
@@ -21,7 +21,7 @@ class Kor < Formula
     ldflags = "-s -w -X github.com/yonahd/kor/pkg/utils.Version=#{version}"
     system "go", "build", *std_go_args(ldflags:)
 
-    generate_completions_from_executable(bin/"kor", "completion", shells: [:bash, :zsh, :fish, :pwsh])
+    generate_completions_from_executable(bin/"kor", shell_parameter_format: :cobra)
   end
 
   test do
