8c27c45ce00b4bfaffd22c2488c52fa3938b7457
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-25 17:32:03 +0900

    gibo: update :cobra completion

--- Formula/g/gibo.rb
+++ Formula/g/gibo.rb
@@ -25,7 +25,7 @@ class Gibo < Formula
       -X github.com/simonwhitaker/gibo/cmd.version=#{version}
     ]
     system "go", "build", *std_go_args(ldflags:)
-    generate_completions_from_executable(bin/"gibo", "completion")
+    generate_completions_from_executable(bin/"gibo", shell_parameter_format: :cobra)
   end
 
   test do
