9f9b8570afdf22c39e2bcd69edfc7c0aad1006ad
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-25 15:08:42 +0900

    cliam: update :cobra completion

--- Formula/c/cliam.rb
+++ Formula/c/cliam.rb
@@ -33,7 +33,7 @@ class Cliam < Formula
     ]
     system "go", "build", *std_go_args(ldflags:), "./cli"
 
-    generate_completions_from_executable(bin/"cliam", "completion")
+    generate_completions_from_executable(bin/"cliam", shell_parameter_format: :cobra)
   end
 
   test do
