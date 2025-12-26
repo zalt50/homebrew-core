654c284cd62c27a8fbf91b48995eda285a5923fe
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-26 12:53:57 +0900

    kustomizer: update :cobra completion

--- Formula/k/kustomizer.rb
+++ Formula/k/kustomizer.rb
@@ -29,7 +29,7 @@ class Kustomizer < Formula
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w -X main.VERSION=#{version}"), "./cmd/kustomizer"
 
-    generate_completions_from_executable(bin/"kustomizer", "completion")
+    generate_completions_from_executable(bin/"kustomizer", shell_parameter_format: :cobra)
   end
 
   test do
