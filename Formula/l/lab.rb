2193244ce5ca54af5f38d89258bbf8289e4e7fcf
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-26 14:38:44 +0900

    lab: update :cobra completion

--- Formula/l/lab.rb
+++ Formula/l/lab.rb
@@ -30,7 +30,7 @@ class Lab < Formula
     ldflags = "-X main.version=#{version} -s -w"
     system "go", "build", *std_go_args(ldflags:)
 
-    generate_completions_from_executable(bin/"lab", "completion")
+    generate_completions_from_executable(bin/"lab", shell_parameter_format: :cobra)
   end
 
   test do
