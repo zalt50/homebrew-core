c061b9d71df69d8ae2d5c50a4d70d3761872b98b
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-26 17:43:33 +0900

    mmdbctl: update :cobra completion

--- Formula/m/mmdbctl.rb
+++ Formula/m/mmdbctl.rb
@@ -22,7 +22,7 @@ class Mmdbctl < Formula
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w")
 
-    generate_completions_from_executable(bin/"mmdbctl", "completion")
+    generate_completions_from_executable(bin/"mmdbctl", shell_parameter_format: :cobra)
   end
 
   test do
