01af5c60b2bc16f0afd98837c2ef89ab899af2a2
Patrick Linnane <patrick@linnane.io>
2025-12-25 17:08:27 -0800

    ronn-ng: revision bump (ruby 4.0.0)
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Formula/r/ronn-ng.rb
+++ Formula/r/ronn-ng.rb
@@ -4,7 +4,7 @@ class RonnNg < Formula
   url "https://github.com/apjanke/ronn-ng/archive/refs/tags/v0.10.1.tar.gz"
   sha256 "180f18015ce01be1d10c24e13414134363d56f9efb741fda460358bb67d96684"
   license "MIT"
-  revision 1
+  revision 2
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_tahoe:   "95544c3a98367e90154643f7ba9a8f1294be476483e7f2faa32a65344ed23cb0"
@@ -17,8 +17,10 @@ class RonnNg < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "1b303b18ed67bc5b328597d421c4a2c0449a5e65372706b04ed58f0f9319d10e"
   end
 
-  # Nokogiri 1.9 requires a newer Ruby
   depends_on "ruby"
+  depends_on "xz"
+
+  uses_from_macos "zlib"
 
   conflicts_with "ronn", because: "both install `ronn` binaries"
 
