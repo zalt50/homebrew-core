6cf23643d80e494c8cf78b3ad4bc5400555ead60
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-26 05:53:29 +0000

    bookokrat: add 0.2.0 bottle.

--- Formula/b/bookokrat.rb
+++ Formula/b/bookokrat.rb
@@ -6,6 +6,15 @@ class Bookokrat < Formula
   license "MIT"
   head "https://github.com/bugzmanov/bookokrat.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "653994a27f74b03f605264e10c2cea10a270383fdc5122f6d436cd1a4e812523"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4ddb19ac97fbe8528153cd1b1c595bf08025d3b2fd12402622f4e6f1b158de09"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0c485022c6219b1958b5dd5dc53516ec3c8163b9d8e2b4a5b145bbbc6652fe8a"
+    sha256 cellar: :any_skip_relocation, sonoma:        "c6f8ab1e9277e26963bd5a8611a2af6d028bd7e3760cf80f65115f9ccaefe67f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "27fd16679ff931c576d7414ba71f87e7b12afad617bb5576f46a26c9b2310db5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c05b1b72c873968661f320291bb2785d2b10e11d8e30f51673a13b87a3432bee"
+  end
+
   depends_on "rust" => :build
 
   def install
