f3e70166071bdc6fa7676fc8aaab5c3409651775
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-26 17:57:55 +0900

    neosync: update :cobra completion

--- Formula/n/neosync.rb
+++ Formula/n/neosync.rb
@@ -30,7 +30,7 @@ class Neosync < Formula
     ]
     system "go", "build", *std_go_args(ldflags:), "./cli/cmd/neosync"
 
-    generate_completions_from_executable(bin/"neosync", "completion")
+    generate_completions_from_executable(bin/"neosync", shell_parameter_format: :cobra)
   end
 
   test do
