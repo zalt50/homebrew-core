a16f41510deefed918cd7127005de5bd35601437
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-26 14:44:43 +0900

    melt: update :cobra completion

--- Formula/m/melt.rb
+++ Formula/m/melt.rb
@@ -22,7 +22,7 @@ class Melt < Formula
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/melt"
 
-    generate_completions_from_executable(bin/"melt", "completion", shells: [:bash, :zsh, :fish, :pwsh])
+    generate_completions_from_executable(bin/"melt", shell_parameter_format: :cobra)
   end
 
   test do
