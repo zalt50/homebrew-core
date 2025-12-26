94a11af93267cfd2138f5dea2eec6bfe3f490332
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-26 19:54:33 +0900

    ots: update :cobra completion

--- Formula/o/ots.rb
+++ Formula/o/ots.rb
@@ -23,7 +23,7 @@ class Ots < Formula
     ldflags = "-s -w -X github.com/sniptt-official/ots/build.Version=#{version}"
     system "go", "build", *std_go_args(ldflags:)
 
-    generate_completions_from_executable(bin/"ots", "completion")
+    generate_completions_from_executable(bin/"ots", shell_parameter_format: :cobra)
   end
 
   test do
