9925902fc33630b7ea5339b76e519879cb614576
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-26 05:18:14 +0000

    rbspy 0.41.0

--- Formula/r/rbspy.rb
+++ Formula/r/rbspy.rb
@@ -1,8 +1,8 @@
 class Rbspy < Formula
   desc "Sampling profiler for Ruby"
   homepage "https://rbspy.github.io/"
-  url "https://github.com/rbspy/rbspy/archive/refs/tags/v0.40.0.tar.gz"
-  sha256 "b9e50943ea70a19bc24a0ac8ef9b938278cca64e0ad136375bbfa63072a0c702"
+  url "https://github.com/rbspy/rbspy/archive/refs/tags/v0.41.0.tar.gz"
+  sha256 "5867b0fc5edf0862c51692c820487aef8c640d2f51579c566facd841ead88ff1"
   license "MIT"
 
   bottle do
