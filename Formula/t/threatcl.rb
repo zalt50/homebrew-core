c103bdb093aa852c9423c95c0365dade85208659
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-26 05:22:12 +0000

    threatcl 0.3.1

--- Formula/t/threatcl.rb
+++ Formula/t/threatcl.rb
@@ -1,8 +1,8 @@
 class Threatcl < Formula
   desc "Documenting your Threat Models with HCL"
   homepage "https://github.com/threatcl/threatcl"
-  url "https://github.com/threatcl/threatcl/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "407434c1e61f80a96d32260e9a7aa0d682057da70b1625fb4f4901878a339ec7"
+  url "https://github.com/threatcl/threatcl/archive/refs/tags/v0.3.1.tar.gz"
+  sha256 "e700fcc1fecaaec3f90e8b2df7d2fc3ee25bc6b6cef06f8532e07774d6363e7e"
   license "MIT"
   head "https://github.com/threatcl/threatcl.git", branch: "main"
 
