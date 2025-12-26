a56314bb2f39fc52cf249f3bbf768ab4b5efc475
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-26 14:44:29 +0900

    mcptools: update :cobra completion

--- Formula/m/mcptools.rb
+++ Formula/m/mcptools.rb
@@ -22,7 +22,7 @@ class Mcptools < Formula
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w -X main.Version=#{version}"), "./cmd/mcptools"
 
-    generate_completions_from_executable(bin/"mcptools", "completion", shells: [:bash, :zsh, :fish, :pwsh])
+    generate_completions_from_executable(bin/"mcptools", shell_parameter_format: :cobra)
   end
 
   test do
