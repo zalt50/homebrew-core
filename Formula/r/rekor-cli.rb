f25d71b4e27bc6814bdd54b99229f6102922c6ef
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-27 17:17:33 +0900

    rekor-cli: update :cobra completion

--- Formula/r/rekor-cli.rb
+++ Formula/r/rekor-cli.rb
@@ -31,7 +31,7 @@ class RekorCli < Formula
     ]
     system "go", "build", *std_go_args(ldflags:), "./cmd/rekor-cli"
 
-    generate_completions_from_executable(bin/"rekor-cli", "completion")
+    generate_completions_from_executable(bin/"rekor-cli", shell_parameter_format: :cobra)
   end
 
   test do
