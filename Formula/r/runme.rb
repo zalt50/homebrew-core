35956e3bca2de4dc4edd370cb40a3a0dfe1a9221
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-27 17:20:22 +0900

    runme: update :cobra completion

--- Formula/r/runme.rb
+++ Formula/r/runme.rb
@@ -26,7 +26,7 @@ class Runme < Formula
     ]
 
     system "go", "build", *std_go_args(ldflags:)
-    generate_completions_from_executable(bin/"runme", "completion")
+    generate_completions_from_executable(bin/"runme", shell_parameter_format: :cobra)
   end
 
   test do
