f2f07e14325399de2315a807722fcc1f97c97c4d
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-27 16:19:37 +0900

    policy-engine: update :cobra completion

--- Formula/p/policy-engine.rb
+++ Formula/p/policy-engine.rb
@@ -23,7 +23,7 @@ class PolicyEngine < Formula
     ldflags = "-s -w -X github.com/snyk/policy-engine/pkg/version.Version=#{version}"
     system "go", "build", *std_go_args(ldflags:)
 
-    generate_completions_from_executable(bin/"policy-engine", "completion")
+    generate_completions_from_executable(bin/"policy-engine", shell_parameter_format: :cobra)
   end
 
   test do
