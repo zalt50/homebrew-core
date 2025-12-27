27eeb7e96643cc1e474ace15e4441170b0a7f74d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-27 22:02:10 +0000

    macchanger: add 0.2.1 bottle.

--- Formula/m/macchanger.rb
+++ Formula/m/macchanger.rb
@@ -5,6 +5,13 @@ class Macchanger < Formula
   sha256 "87192a3549baa771736ec3f9c2017f533d304efc7b7d968aadc1226e08f1673b"
   license "GPL-2.0-only"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6be915a49e29f68f13dfdbc86259fe3edc1a5ae0c06c938aa59560689dd8a2fe"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5c127da61a8720c9996f555bfd159e581e439628d14507d4ca35dcd49d9b94d9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "adc4d071a13a746234f10c4db647be0800d3b6484bf1709815685344c72b62b8"
+    sha256 cellar: :any_skip_relocation, sonoma:        "d84103330c6ed15cdc830bd581cf0fe62bd6876abac6364aa1fc430c874b13aa"
+  end
+
   depends_on :macos
 
   def install
