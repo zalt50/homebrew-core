fe11a0e8ba33884832e10d30670bc5f39363810e
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-27 16:22:09 +0900

    pulumictl: update :cobra completion

--- Formula/p/pulumictl.rb
+++ Formula/p/pulumictl.rb
@@ -23,7 +23,7 @@ class Pulumictl < Formula
     ldflags = "-s -w -X github.com/pulumi/pulumictl/pkg/version.Version=#{version}"
     system "go", "build", *std_go_args(ldflags:), "./cmd/pulumictl"
 
-    generate_completions_from_executable(bin/"pulumictl", "completion")
+    generate_completions_from_executable(bin/"pulumictl", shell_parameter_format: :cobra)
   end
 
   test do
