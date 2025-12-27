30604f1ea3dc80281cc95af0f0c5b6c6502949f4
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-27 17:17:19 +0900

    regclient: update :cobra completion

--- Formula/r/regclient.rb
+++ Formula/r/regclient.rb
@@ -22,7 +22,7 @@ class Regclient < Formula
     ["regbot", "regctl", "regsync"].each do |f|
       system "go", "build", *std_go_args(ldflags:, output: bin/f), "./cmd/#{f}"
 
-      generate_completions_from_executable(bin/f, "completion")
+      generate_completions_from_executable(bin/f, shell_parameter_format: :cobra)
     end
   end
 
