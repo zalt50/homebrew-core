e55ab55c900aefebd5799a287af350a6936e0404
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-27 16:21:18 +0900

    poutine: update :cobra completion

--- Formula/p/poutine.rb
+++ Formula/p/poutine.rb
@@ -27,7 +27,7 @@ class Poutine < Formula
 
     system "go", "build", *std_go_args(ldflags:)
 
-    generate_completions_from_executable(bin/"poutine", "completion")
+    generate_completions_from_executable(bin/"poutine", shell_parameter_format: :cobra)
   end
 
   test do
