898d880a2d7348288fd3f8394956c903807ad951
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-27 18:04:21 +0900

    supabase: update :cobra completion

--- Formula/s/supabase.rb
+++ Formula/s/supabase.rb
@@ -28,7 +28,7 @@ class Supabase < Formula
       -X github.com/supabase/cli/internal/utils.Version=#{version}
     ]
     system "go", "build", *std_go_args(ldflags:)
-    generate_completions_from_executable(bin/"supabase", "completion")
+    generate_completions_from_executable(bin/"supabase", shell_parameter_format: :cobra)
   end
 
   test do
