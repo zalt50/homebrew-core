4fc4504a65c6f0180a5b5975c72e5454d51b7397
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-28 23:18:08 +0900

    telegram-downloader: update :cobra completion

--- Formula/t/telegram-downloader.rb
+++ Formula/t/telegram-downloader.rb
@@ -28,7 +28,7 @@ class TelegramDownloader < Formula
     ]
     system "go", "build", *std_go_args(ldflags:, output: bin/"tdl")
 
-    generate_completions_from_executable(bin/"tdl", "completion")
+    generate_completions_from_executable(bin/"tdl", shell_parameter_format: :cobra)
   end
 
   test do
