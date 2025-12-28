a5529c98c74fa45918ca1f8ab99a19f050b14837
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-28 23:44:30 +0900

    vibecheck: update :cobra completion

--- Formula/v/vibecheck.rb
+++ Formula/v/vibecheck.rb
@@ -21,7 +21,7 @@ class Vibecheck < Formula
     ldflags = "-s -w -X github.com/rshdhere/vibecheck/cmd.version=#{version}"
     system "go", "build", *std_go_args(ldflags:)
 
-    generate_completions_from_executable(bin/"vibecheck", "completion", shells: [:bash, :zsh, :fish, :pwsh])
+    generate_completions_from_executable(bin/"vibecheck", shell_parameter_format: :cobra)
   end
 
   test do
