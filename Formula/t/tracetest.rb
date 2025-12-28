0e0a3500fada4ced4bba6dce0c0f1cd0b78bed98
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-28 23:37:06 +0900

    tracetest: update :cobra completion

--- Formula/t/tracetest.rb
+++ Formula/t/tracetest.rb
@@ -21,7 +21,7 @@ class Tracetest < Formula
     ldflags = "-s -w -X github.com/kubeshop/tracetest/cli/config.Version=#{version}"
     system "go", "build", *std_go_args(ldflags:), "./cli"
 
-    generate_completions_from_executable(bin/"tracetest", "completion", shells: [:bash, :zsh, :fish, :pwsh])
+    generate_completions_from_executable(bin/"tracetest", shell_parameter_format: :cobra)
   end
 
   test do
