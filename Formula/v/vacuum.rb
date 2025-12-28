3c487da53f3b1184f4f49d500c8c466a5cc6aa18
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-28 23:43:42 +0900

    vacuum: update :cobra completion

--- Formula/v/vacuum.rb
+++ Formula/v/vacuum.rb
@@ -21,7 +21,7 @@ class Vacuum < Formula
     ldflags = "-s -w -X main.version=#{version} -X main.commit=#{tap.user} -X main.date=#{time.iso8601}"
     system "go", "build", *std_go_args(ldflags:)
 
-    generate_completions_from_executable(bin/"vacuum", "completion", shells: [:bash, :zsh, :fish, :pwsh])
+    generate_completions_from_executable(bin/"vacuum", shell_parameter_format: :cobra)
   end
 
   test do
