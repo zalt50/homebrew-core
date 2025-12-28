168c33dac7e0ed45e34c35afb21efa2a87d02392
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-28 23:38:58 +0900

    tygo: update :cobra completion

--- Formula/t/tygo.rb
+++ Formula/t/tygo.rb
@@ -28,7 +28,7 @@ class Tygo < Formula
 
     system "go", "build", *std_go_args(ldflags:)
 
-    generate_completions_from_executable(bin/"tygo", "completion")
+    generate_completions_from_executable(bin/"tygo", shell_parameter_format: :cobra)
     pkgshare.install "examples"
   end
 
