d940b3fb4018da2518de415bee3a62402ad81aa5
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-12-29 00:37:21 +0900

    yutu: update :cobra completion

--- Formula/y/yutu.rb
+++ Formula/y/yutu.rb
@@ -34,7 +34,7 @@ class Yutu < Formula
     ]
     system "go", "build", *std_go_args(ldflags:)
 
-    generate_completions_from_executable(bin/"yutu", "completion", shells: [:bash, :zsh, :fish, :pwsh])
+    generate_completions_from_executable(bin/"yutu", shell_parameter_format: :cobra)
   end
 
   test do
