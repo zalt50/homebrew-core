5e0d44d38d17ce2066c54f3917a4156be5da34a8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-29 17:26:16 +0000

    wuchale 0.19.0

--- Formula/w/wuchale.rb
+++ Formula/w/wuchale.rb
@@ -1,8 +1,8 @@
 class Wuchale < Formula
   desc "Protobuf-like i18n from plain code"
   homepage "https://wuchale.dev/"
-  url "https://registry.npmjs.org/wuchale/-/wuchale-0.18.11.tgz"
-  sha256 "9a39873dcc10d7357335eb36aa1d205980c7c102930a47f7595f0686bbbb1a9d"
+  url "https://registry.npmjs.org/wuchale/-/wuchale-0.19.0.tgz"
+  sha256 "e309caf2afe80831db88aa9cf4dcf6848b5eea32f595585f129b1ea191cc67c4"
   license "MIT"
 
   bottle do
@@ -18,11 +18,10 @@ class Wuchale < Formula
 
   test do
     (testpath/"wuchale.config.mjs").write <<~EOS
-      export default {};
+      export default {
+        locales: ["en"]
+      };
     EOS
-
-    system bin/"wuchale", "--config", testpath/"wuchale.config.mjs", "init"
-    assert_path_exists testpath/"wuchale.config.mjs"
 
     output = shell_output("#{bin}/wuchale --config #{testpath}/wuchale.config.mjs status")
     assert_match "Locales: \e[36men (English)\e[0m", output
