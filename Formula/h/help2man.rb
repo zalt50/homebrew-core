d56af93021a04b09f0181769d68fe785c1197697
Patrick Linnane <patrick@linnane.io>
2025-12-28 22:03:55 -0800

    help2man: revision bump (perl 5.42.0)

--- Formula/h/help2man.rb
+++ Formula/h/help2man.rb
@@ -5,7 +5,7 @@ class Help2man < Formula
   mirror "https://ftp.gnu.org/gnu/help2man/help2man-1.49.3.tar.xz"
   sha256 "4d7e4fdef2eca6afe07a2682151cea78781e0a4e8f9622142d9f70c083a2fd4f"
   license "GPL-3.0-or-later"
-  revision 3
+  revision 4
 
   no_autobump! because: :requires_manual_review
 
