7c5809ecaab944e4c9510c5c34e8bf4668f9b662
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-29 15:12:19 +0000

    pygit2: update 1.19.1 bottle.

--- Formula/p/pygit2.rb
+++ Formula/p/pygit2.rb
@@ -7,12 +7,12 @@ class Pygit2 < Formula
   head "https://github.com/libgit2/pygit2.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "c423238a2a9248d8e87bd250c401b2f88f50f476fcefc69f660eb868303b71de"
-    sha256 cellar: :any,                 arm64_sequoia: "a5ff092a5dd0c38f6786ecc4b444640678c7d9e1a4734a7c3cddf3a69cf9ea85"
-    sha256 cellar: :any,                 arm64_sonoma:  "bdb60a77d388a037941975a278119509510163c692094b7c72d57f5cc959a5ad"
-    sha256 cellar: :any,                 sonoma:        "f08387023d4714e1308680923feafda014e4b539e164a9dc278f461170f44520"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "97833a1c9ad4c54da82fcc7af8265979575a675a00f70e8163ae8b5e492e0cef"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "119c10991289e7b902f9540d8624b2d04d5d79b97c130386848016461effc2fb"
+    sha256 cellar: :any,                 arm64_tahoe:   "15f26a039eaeb8cf8650eaec7fb59014823ed90e02c161b9b982683382499fe9"
+    sha256 cellar: :any,                 arm64_sequoia: "fcf1dcb11ab25e0fae1c66ecfb8641c61b77e617156ce2773cae310bf40d8079"
+    sha256 cellar: :any,                 arm64_sonoma:  "a2742cf033a1434640c0b70cdf44447d7ccce26c3e19aa34eb2777c6c3936ecf"
+    sha256 cellar: :any,                 sonoma:        "095d41bf399bc7b46c6f531c6ffec33913b2dd70a42c08ac44d37b5b21df3581"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3c02462e83c3919b1125f1e442b252ae3ab69f993dcd7ee82e8d6ca5347326b9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "76d50bbc5185231ce8bcb987b07afaa758c5f8af3dbc888315f3c4082fa325cb"
   end
 
   depends_on "python@3.13" => [:build, :test]
