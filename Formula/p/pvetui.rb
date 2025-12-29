a96b198ad74699b338809a7f1f1e1cae4187b627
Leon Adomaitis <leon@adomaitis.de>
2025-12-29 02:53:16 +0100

    pvetui 1.0.15 (new formula)

---
+++ Formula/p/pvetui.rb
@@ -0,0 +1,25 @@
+class Pvetui < Formula
+  desc "Terminal UI for Proxmox VE"
+  homepage "https://github.com/devnullvoid/pvetui"
+  url "https://github.com/devnullvoid/pvetui/archive/refs/tags/v1.0.15.tar.gz"
+  sha256 "6be2a3937d0e7943d04839f582780cf814323744b06ce96923d2c7725bdd8368"
+  license "MIT"
+  head "https://github.com/devnullvoid/pvetui.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/devnullvoid/pvetui/internal/version.version=#{version}
+      -X github.com/devnullvoid/pvetui/internal/version.commit=#{tap.user}
+      -X github.com/devnullvoid/pvetui/internal/version.buildDate=#{time.iso8601}
+    ]
+    system "go", "build", *std_go_args(ldflags:), "./cmd/pvetui"
+  end
+
+  test do
+    assert_match "It looks like this is your first time running pvetui.", pipe_output(bin/"pvetui", "n")
+    assert_match version.to_s, shell_output("#{bin}/pvetui --version")
+  end
+end
