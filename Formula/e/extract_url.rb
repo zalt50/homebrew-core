2d1eabe4255b48d209cba64b8dc1ce6653ab3a00
Patrick Linnane <patrick@linnane.io>
2025-12-28 22:03:49 -0800

    extract_url: revision bump (perl 5.42.0)

--- Formula/e/extract_url.rb
+++ Formula/e/extract_url.rb
@@ -4,7 +4,7 @@ class ExtractUrl < Formula
   url "https://github.com/m3m0ryh0l3/extracturl/archive/refs/tags/v1.6.2.tar.gz"
   sha256 "5f0b568d5c9449f477527b4077d8269f1f5e6d6531dfa5eb6ca72dbacab6f336"
   license "BSD-2-Clause"
-  revision 3
+  revision 4
 
   no_autobump! because: :requires_manual_review
 
