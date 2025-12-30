a6f2a854949ee1195ba4a2f530777e9f91082948
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-30 10:58:08 +0000

    context7-mcp 2.0.1

--- Formula/c/context7-mcp.rb
+++ Formula/c/context7-mcp.rb
@@ -1,8 +1,8 @@
 class Context7Mcp < Formula
   desc "Up-to-date code documentation for LLMs and AI code editors"
   homepage "https://github.com/upstash/context7"
-  url "https://registry.npmjs.org/@upstash/context7-mcp/-/context7-mcp-2.0.0.tgz"
-  sha256 "260b2fcfee88fb5bd57f69a65ddb039aacd0e7e3b856a579edac0553ea7f0aec"
+  url "https://registry.npmjs.org/@upstash/context7-mcp/-/context7-mcp-2.0.1.tgz"
+  sha256 "647b805e68bdcb7dc86e0d264662d616b20a0181b33c174d0afbca25745efa65"
   license "MIT"
 
   bottle do
