33265aaa0d215cd0b1f84fb21a919b56ee95c75b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-30 02:52:27 +0000

    pvetui: add 1.0.15 bottle.

--- Formula/p/pvetui.rb
+++ Formula/p/pvetui.rb
@@ -6,6 +6,15 @@ class Pvetui < Formula
   license "MIT"
   head "https://github.com/devnullvoid/pvetui.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1d4e49ebf349a60d755262aafefff0222630e03675b3988c55ffe5ee2c30c5c5"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1d4e49ebf349a60d755262aafefff0222630e03675b3988c55ffe5ee2c30c5c5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1d4e49ebf349a60d755262aafefff0222630e03675b3988c55ffe5ee2c30c5c5"
+    sha256 cellar: :any_skip_relocation, sonoma:        "607fbf026e46c5a552db2f8c6a9f0b7348054b8f272f7ef8cb717018687a140f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "dc8f8affa7c34cf5dfa4e6e015746e9adfb2b398d7a1d4dce2c0e012bd9be60c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "53a0262be9a47091e3ff2a4ddf864c441824f9b4de00c67731025c7a14b9af4d"
+  end
+
   depends_on "go" => :build
 
   def install
