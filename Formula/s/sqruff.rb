7d9957b70319479a6c0a058d9d2b429619398afd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-30 11:29:41 +0000

    sqruff 0.31.0

--- Formula/s/sqruff.rb
+++ Formula/s/sqruff.rb
@@ -1,8 +1,8 @@
 class Sqruff < Formula
   desc "Fast SQL formatter/linter"
   homepage "https://github.com/quarylabs/sqruff"
-  url "https://github.com/quarylabs/sqruff/archive/refs/tags/v0.30.1.tar.gz"
-  sha256 "6cdd5190a33342963a8c970b62c11e50cd04e9386e3a2601af0e3fcd286bc05a"
+  url "https://github.com/quarylabs/sqruff/archive/refs/tags/v0.31.0.tar.gz"
+  sha256 "2970a5eb8e4df663ec6abcae148b19667c8bc0fd3c48a20d3eb57603b8450176"
   license "Apache-2.0"
   head "https://github.com/quarylabs/sqruff.git", branch: "main"
 
