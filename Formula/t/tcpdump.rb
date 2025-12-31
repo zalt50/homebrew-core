d93567a8c5938f279a68e9dd0e2707a0bd794d28
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-31 11:07:57 +0000

    tcpdump: update 4.99.6 bottle.

--- Formula/t/tcpdump.rb
+++ Formula/t/tcpdump.rb
@@ -12,12 +12,13 @@ class Tcpdump < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "cecfbd8582f8567f44e64be800a4c1485f1194d5e18e149604b30a460eddbbe8"
-    sha256 cellar: :any,                 arm64_sequoia: "8fe09be0df83c85a118ff5c5e0d9e5f226ccbea68cbd15ff58e9881a87da539d"
-    sha256 cellar: :any,                 arm64_sonoma:  "0db0ad3d88b277b82f4856514b59f9625daba8c1274034709ede5e21c5d0758e"
-    sha256 cellar: :any,                 sonoma:        "8272a3c5993d0fef9408ed6314d9496517a46552f9a50b4d64ac3ccb58192283"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "7859a1f33a16e401345553367d69aec1b0344cb0bcdcb8e5688adeb5e64007ac"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a41f3c3553e2199b6f215599e4cb7772981947a4d9f19703ce2d91c5ab9e081a"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_tahoe:   "a666fc57f87e8f3947aaf879dc686caa9a007e1a774978a8c4aeb6aaa824418a"
+    sha256 cellar: :any,                 arm64_sequoia: "06303bf329b37adf4025e8db096ae455ee5387d4e1ed50f2a2322402a557f262"
+    sha256 cellar: :any,                 arm64_sonoma:  "ded86f8b06bd536b27ab058b16017e2e624d4471c551b83ac73802990138c6f5"
+    sha256 cellar: :any,                 sonoma:        "bd53114b2966e8390780c8551d68796f44d0cc4223aca8c84205df81de2c3209"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ebbebd3ee52544f48fdec76b8efa5c62003ba1055f8247d0422eb097d7b6d704"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2acf63d0a7c95b63db681253f76b43d355e826265c33ccc55d8cd2b70cf6c631"
   end
 
   depends_on "libpcap"
