84e521a959d92881e8516ce35a4ec1beb1d055b2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-31 17:19:19 +0000

    sqlboiler 4.19.6
    sqlboiler: add patch

--- Formula/s/sqlboiler.rb
+++ Formula/s/sqlboiler.rb
@@ -1,8 +1,8 @@
 class Sqlboiler < Formula
   desc "Generate a Go ORM tailored to your database schema"
   homepage "https://github.com/volatiletech/sqlboiler"
-  url "https://github.com/volatiletech/sqlboiler/archive/refs/tags/v4.19.5.tar.gz"
-  sha256 "fae160e36637c5d0c57db53bafc11439cf61b02dc30656277d7e90c546b04a4d"
+  url "https://github.com/volatiletech/sqlboiler/archive/refs/tags/v4.19.6.tar.gz"
+  sha256 "e016d069ec5c6a363019899857aa522dbb14e72c8034048d490be4e045dc7073"
   license "BSD-3-Clause"
   head "https://github.com/volatiletech/sqlboiler.git", branch: "master"
 
@@ -18,6 +18,13 @@ class Sqlboiler < Formula
   end
 
   depends_on "go" => :build
+
+  # Fix incorrect version reporting.
+  # Upstream PR ref: https://github.com/aarondl/sqlboiler/pull/1468
+  patch do
+    url "https://github.com/aarondl/sqlboiler/commit/59f05ce1e989295571789514f68bbd0bff18b730.patch?full_index=1"
+    sha256 "a892cdea048c66d4ffbb701c5983c158b6eb74bcf23cc2239728b184aacfdd88"
+  end
 
   def install
     %w[mssql mysql psql sqlite3].each do |driver|
