1eed5a73f532c41c9adfdf693df1b588c6b2d911
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-31 17:47:45 +0000

    vibecheck: update 1.6.0 bottle.

--- Formula/v/vibecheck.rb
+++ Formula/v/vibecheck.rb
@@ -7,12 +7,12 @@ class Vibecheck < Formula
   head "https://github.com/rshdhere/vibecheck.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4e33813ef2357efe125fa9a5a487a63755538370fd7a68a168da4a59e9d2f39d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4e33813ef2357efe125fa9a5a487a63755538370fd7a68a168da4a59e9d2f39d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4e33813ef2357efe125fa9a5a487a63755538370fd7a68a168da4a59e9d2f39d"
-    sha256 cellar: :any_skip_relocation, sonoma:        "4e281e888bc134fab3dd7b3e79730171a95c7a2288b166384e0e701c45937129"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "fc0b9f1b15faf9e62081ff581053f7e8624411ae959148cc662e7a8011041072"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6ef5c72eb70844b5b5eaf9b59907e2b406e6e2d9d77f756fc33020ddc8de0957"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e847b1d8843ec2affa176576d7c66fbedc961f8a67a97ea678f8850e43ab4d5a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e847b1d8843ec2affa176576d7c66fbedc961f8a67a97ea678f8850e43ab4d5a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e847b1d8843ec2affa176576d7c66fbedc961f8a67a97ea678f8850e43ab4d5a"
+    sha256 cellar: :any_skip_relocation, sonoma:        "6c04048dd848fc03713350aba34f4ab7badb7eed7d3e205376ae4277e70aed1a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "adb1f66d909ba50b45d0310fa8db6e62ec6d443ef8d5520ab06980581915bf9b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "07aa29d75540113dadd64a02cf033f06fd0b1822e62539340d7309bb8d7ce0da"
   end
 
   depends_on "go" => :build
