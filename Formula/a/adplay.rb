357b103150d5ef68959b44a41c1f77e0a80f1bc8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-31 00:37:08 +0000

    adplay: add 1.9 bottle.

--- Formula/a/adplay.rb
+++ Formula/a/adplay.rb
@@ -5,6 +5,15 @@ class Adplay < Formula
   sha256 "949b2618092a3aae5c278a98dfa3231130ef35a791b3afcaa0ebe45443ce82c8"
   license "GPL-2.0-or-later"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "09db45033118afa94298a58b3514d0265fb3f11c90341760e20899663ccc4fd8"
+    sha256 cellar: :any,                 arm64_sequoia: "967f04bd760c74ad7d1f31dc4e4baffeabe9db557959465fd6d27a9834a42aa1"
+    sha256 cellar: :any,                 arm64_sonoma:  "20299de36acaf9dcbba5b1b827031a716014d6b7fd8846cf17234e614da0376a"
+    sha256 cellar: :any,                 sonoma:        "62f0e81f88d53e2f0882ff204ef39ac34398c59111d90b1a86e1c4903710ef0c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "beeaca960d641fa18e2386ef83ab7ab2df2b030aa683e6eb724eecb3251d09c8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5802a081b95a8a5610e8c27fe5100048e4eb89ca17ffef6fcf07fadda24e671c"
+  end
+
   head do
     url "https://github.com/adplug/adplay-unix.git", branch: "master"
 
