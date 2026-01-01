97233bf97bd8256ac64515779f80c2eb1e688c69
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2026-01-01 22:47:20 +0900

    nauty 2.9.3

--- Formula/n/nauty.rb
+++ Formula/n/nauty.rb
@@ -1,9 +1,9 @@
 class Nauty < Formula
   desc "Automorphism groups of graphs and digraphs"
   homepage "https://pallini.di.uniroma1.it/"
-  url "https://pallini.di.uniroma1.it/nauty2_9_1.tar.gz"
-  mirror "https://users.cecs.anu.edu.au/~bdm/nauty/nauty2_9_1.tar.gz"
-  sha256 "488fa906d10a372c72d2364c5dee48e0f7307004fbe52c2bce50c52de8cd873e"
+  url "https://pallini.di.uniroma1.it/nauty2_9_3.tar.gz"
+  mirror "https://users.cecs.anu.edu.au/~bdm/nauty/nauty2_9_3.tar.gz"
+  sha256 "9fc4edae04f88a0f5883985be3b39cf7f898fd6cc96e96b9ee25452743cc1b5b"
   license "Apache-2.0"
   version_scheme 1
 
