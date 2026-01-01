16ae6958af86dbfd3203ec55624b493c27ce3f89
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2026-01-01 22:45:16 +0900

    mt32emu 2.7.3

--- Formula/m/mt32emu.rb
+++ Formula/m/mt32emu.rb
@@ -1,8 +1,8 @@
 class Mt32emu < Formula
   desc "Multi-platform software synthesiser"
   homepage "https://github.com/munt/munt"
-  url "https://github.com/munt/munt/archive/refs/tags/libmt32emu_2_7_2.tar.gz"
-  sha256 "3b0beff64e83d2b4879baba2326eed1300edc9663b3dc52621fc74d196ca9a80"
+  url "https://github.com/munt/munt/archive/refs/tags/libmt32emu_2_7_3.tar.gz"
+  sha256 "e51f3475771c9d07116e6cb5ae2e095ef3b11b3107d92c01d3a1dc03be13ff98"
   license "LGPL-2.1-or-later"
   head "https://github.com/munt/munt.git", branch: "master"
 
@@ -27,6 +27,12 @@ class Mt32emu < Formula
   depends_on "cmake" => :build
   depends_on "libsamplerate"
   depends_on "libsoxr"
+
+  # Version fix patch, remove in next release
+  patch do
+    url "https://github.com/munt/munt/commit/e1d0fb426865ca75e0069c39bbd1b329dee1cb29.patch?full_index=1"
+    sha256 "222b22104fb1c7b232a2725dfb7cfa1d107ca831a7d5feac0dff597c7ae8fa49"
+  end
 
   def install
     system "cmake", "-S", "mt32emu", "-B", "build", *std_cmake_args
