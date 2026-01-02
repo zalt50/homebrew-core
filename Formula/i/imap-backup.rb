0b798e68c284e63f53ba47efcdfe5a518962e2c8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-02 06:32:31 +0000

    imap-backup: add 16.4.0 bottle.

--- Formula/i/imap-backup.rb
+++ Formula/i/imap-backup.rb
@@ -10,6 +10,15 @@ class ImapBackup < Formula
     strategy :github_latest
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "151c557535230bdcaa38b6243d79dc0c1d23ad242fe2085f2ae6c3425e2f99c0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "151c557535230bdcaa38b6243d79dc0c1d23ad242fe2085f2ae6c3425e2f99c0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "151c557535230bdcaa38b6243d79dc0c1d23ad242fe2085f2ae6c3425e2f99c0"
+    sha256 cellar: :any_skip_relocation, sonoma:        "34f69912f1484c51653b57de07a6b2cbd3bf17074f2e94f78c5a9560f9d541e9"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e9f3aaf814e55d0b8173e0b619c6b03439f074b4022a2813761d9ca7de6a0107"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5b6f98d54322375e638d7d8c4b4ce9151942f60b3e7a13bc3505bdc5786e53c0"
+  end
+
   depends_on "ruby"
 
   def install
