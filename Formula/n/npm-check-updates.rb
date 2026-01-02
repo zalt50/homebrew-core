d86023c439f3cae2e9f87d5dd4737951e593d12b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-02 18:39:12 +0000

    npm-check-updates: update 19.2.1 bottle.

--- Formula/n/npm-check-updates.rb
+++ Formula/n/npm-check-updates.rb
@@ -6,7 +6,7 @@ class NpmCheckUpdates < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "8c82e118ff056466960b7c9abae841c66e954d8db99a5fd10db0ad4a9aa26d27"
+    sha256 cellar: :any_skip_relocation, all: "81d876814560e25ecd9313a0a33ac6abf728efed26af7547c6c60fc4416c3e15"
   end
 
   depends_on "node"
