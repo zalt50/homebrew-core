cbab7530652a5994dd1fe00cb659d603d26fc272
Michael Cho <michael@michaelcho.dev>
2026-01-02 12:42:35 -0500

    uutils-coreutils: change prefix to `uu-` matching upstream README.md
    This helps avoid conflicts with other formulae like `unp` and helps
    reduce variations across repositories. Currently,
    * Arch Linux, Gentoo and MacPorts use `uu-`
    * Fedora uses `uu_`
    * NixOS uses `uutils-`
    
    Other changes:
    * Keep around temporary `u`-prefixed exec scripts until 0.8.0.
    * Fix broken `usha*sum` binaries which need to be run without prefix
    * Drop conflicts with `coreutils`. Part of reason for prefixing these
      commands is to avoid conflicts. Also, this lacks the special handling
      in `coreutils` which avoids problematic binaries (e.g. `dircolors`)
      and new macOS executables (e.g. `realpath`).

--- Formula/u/uutils-coreutils.rb
+++ Formula/u/uutils-coreutils.rb
@@ -25,9 +25,11 @@ class UutilsCoreutils < Formula
   depends_on "sphinx-doc" => :build
 
   on_macos do
+    # TODO: remove conflict in follow-up CI-syntax-only PR
     conflicts_with "coreutils", because: "uutils-coreutils and coreutils install the same binaries"
   end
 
+  # TODO: remove in follow-up to 0.8.0 bump PR
   conflicts_with "unp", because: "both install `ucat` binaries"
 
   def install
@@ -37,47 +39,54 @@ class UutilsCoreutils < Formula
     inreplace "GNUmakefile", "$(SELINUX_PROGS)", ""
 
     args = %W[
-      PROG_PREFIX=u
+      PROG_PREFIX=uu-
       PREFIX=#{prefix}
       SPHINXBUILD=#{Formula["sphinx-doc"].opt_bin}/sphinx-build
     ]
     # Call `make` as `gmake` to use Homebrew `make`.
     system "gmake", "install", *args
 
-    # Symlink all commands into libexec/uubin without the 'u' prefix
+    # Symlink all commands into libexec/uubin without the 'uu-' prefix
     coreutils_filenames(bin).each do |cmd|
-      (libexec/"uubin").install_symlink bin/"u#{cmd}" => cmd
+      uu_cmd = bin/"uu-#{cmd}"
+      (libexec/"uubin").install_symlink uu_cmd.realpath => cmd
+
+      # Fix symlinked commands which require running with non-prefixed name, e.g. sha1sum
+      if uu_cmd.symlink?
+        rm(uu_cmd)
+        bin.write_exec_script libexec/"uubin"/cmd
+        bin.install bin/cmd => "uu-#{cmd}"
+      end
+
+      # Create a temporary compatibility executable for previous 'u' prefix.
+      # All users should get the warning in 0.6.0. Similar to brew's odeprecate
+      # timeframe, the removal can be done after 2 minor releases, i.e. 0.8.0.
+      odie "Remove compatibility exec scripts!" if build.stable? && version >= "0.8.0"
+      (bin/"u#{cmd}").write <<~SHELL
+        #!/bin/bash
+        echo "WARNING: u#{cmd} has been renamed to uu-#{cmd} and will be removed in 0.8.0" >&2
+        exec "#{uu_cmd}" "$@"
+      SHELL
     end
 
-    # Symlink all man(1) pages into libexec/uuman without the 'u' prefix
+    # Symlink all man(1) pages into libexec/uuman without the 'uu-' prefix
     coreutils_filenames(man1).each do |cmd|
-      (libexec/"uuman/man1").install_symlink man1/"u#{cmd}" => cmd
+      (libexec/"uuman/man1").install_symlink man1/"uu-#{cmd}" => cmd
     end
 
     (libexec/"uubin").install_symlink "../uuman" => "man"
 
     # Symlink non-conflicting binaries
-    no_conflict = if OS.mac?
-      %w[
-        base32 dircolors factor hashsum hostid nproc numfmt pinky ptx realpath
-        shred shuf stdbuf tac timeout truncate
-      ]
-    else
-      %w[hashsum]
-    end
+    no_conflict = %w[hashsum]
     no_conflict.each do |cmd|
-      bin.install_symlink "u#{cmd}" => cmd
-      man1.install_symlink "u#{cmd}.1.gz" => "#{cmd}.1.gz"
+      bin.install_symlink "uu-#{cmd}" => cmd
+      man1.install_symlink "uu-#{cmd}.1.gz" => "#{cmd}.1.gz"
     end
   end
 
   def caveats
-    provided_by = "coreutils"
-    on_macos do
-      provided_by = "macOS"
-    end
     <<~EOS
-      Commands also provided by #{provided_by} have been installed with the prefix "u".
+      Commands have been installed with the prefix "uu-".
       If you need to use these commands with their normal names, you
       can add a "uubin" directory to your PATH from your bashrc like:
         PATH="#{opt_libexec}/uubin:$PATH"
@@ -89,7 +98,7 @@ class UutilsCoreutils < Formula
     dir.find do |path|
       next if path.directory? || path.basename.to_s == ".DS_Store"
 
-      filenames << path.basename.to_s.sub(/^u/, "")
+      filenames << path.basename.to_s.sub(/^uu-/, "")
     end
     filenames.sort
   end
@@ -97,7 +106,9 @@ class UutilsCoreutils < Formula
   test do
     (testpath/"test").write("test")
     (testpath/"test.sha1").write("a94a8fe5ccb19ba61c4c0873d391e987982fbbd3 test")
-    system bin/"uhashsum", "--sha1", "-c", "test.sha1"
-    system bin/"uln", "-f", "test", "test.sha1"
+    system bin/"uhashsum", "--sha1", "-c", "test.sha1" # TODO: remove in 0.8.0
+    system bin/"uu-hashsum", "--sha1", "-c", "test.sha1"
+    system bin/"uu-sha1sum", "-c", "test.sha1"
+    system bin/"uu-ln", "-f", "test", "test.sha1"
   end
 end
