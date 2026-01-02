43de2249a0166d86894048d5fcc7f25a81945f33
Colin Griffin <colin@krum.io>
2026-01-02 17:23:25 -0500

    epinio 1.13.7
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/e/epinio.rb
+++ Formula/e/epinio.rb
@@ -1,8 +1,8 @@
 class Epinio < Formula
   desc "CLI for Epinio, the Application Development Engine for Kubernetes"
   homepage "https://epinio.io/"
-  url "https://github.com/epinio/epinio/archive/refs/tags/v1.13.6.tar.gz"
-  sha256 "2d07fc5c9e7d62710ca5b6d540045b3f0312d9ff11ae197b97d34e73a4e6ae7b"
+  url "https://github.com/epinio/epinio/archive/refs/tags/v1.13.7.tar.gz"
+  sha256 "2a76233b9505b56412b6e0c94262f5e280ee994f52b274b91d2d3885f5167896"
   license "Apache-2.0"
 
   # Upstream creates a stable version tag ahead of release but a version isn't
