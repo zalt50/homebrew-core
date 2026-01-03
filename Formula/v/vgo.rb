55a375025e22a4044eea2d200aa995830e0ddc80
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-03 09:14:02 +0000

    vgo: update 0.3.0 bottle.

--- Formula/v/vgo.rb
+++ Formula/v/vgo.rb
@@ -7,13 +7,12 @@ class Vgo < Formula
   head "https://github.com/vg006/vgo.git", branch: "main"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "49332789cd1015c1e0cc9a4178c8e258207a6579aba7d4f6275322aef8ce77e6"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "49332789cd1015c1e0cc9a4178c8e258207a6579aba7d4f6275322aef8ce77e6"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "49332789cd1015c1e0cc9a4178c8e258207a6579aba7d4f6275322aef8ce77e6"
-    sha256 cellar: :any_skip_relocation, sonoma:        "e4bc7f63a8904dd9303fa6c648e166114851f1813b6e0f2616196d09f9a22e89"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "44c08e12409364cc439bfa6c9f936e9c18a4f6bb1e3dcb1a28e077b0f5776256"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cd6d56ca79564aae50cd1ccacb48c3b3fecd0dd378a12a5fddaa8fc77f75e369"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ff3f68e2c54740a2b3cdd2074e33ba392fc4d4398c7ab77c441f96b5294cd4dc"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ff3f68e2c54740a2b3cdd2074e33ba392fc4d4398c7ab77c441f96b5294cd4dc"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ff3f68e2c54740a2b3cdd2074e33ba392fc4d4398c7ab77c441f96b5294cd4dc"
+    sha256 cellar: :any_skip_relocation, sonoma:        "f1f6d163d6dc4217b8501804008210bdba10fd415b31112d4d78da74e351219a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "7d885ec1ff53867945a651083c1c1db6b0aa08fb5eef94f4bf05e47fa65e64b4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2ea7f580c8df4839b99a44f522001776ec0dba68b005afff0b08cc567aafbdd1"
   end
 
   depends_on "go" => :build
