149bcc0b4fccab6113bd3bc3573b5c87f71ad822
Ties Jan Hefting <hello@tiesjan.com>
2026-01-03 11:40:01 +0100

    lsdvd: bump to version 0.21
    Newer versions don't have a downloadable file on Sourceforge, but the
    source code can be checked out using git tag corresponding to versions.
    
    Due to upstream changes, building is now done using autoconf/automake
    alongside pkgconf, and a dependency on `libxml2` has been added.

--- Formula/l/lsdvd.rb
+++ Formula/l/lsdvd.rb
@@ -1,10 +1,10 @@
 class Lsdvd < Formula
   desc "Read the content info of a DVD"
   homepage "https://sourceforge.net/projects/lsdvd/"
-  url "https://downloads.sourceforge.net/project/lsdvd/lsdvd/lsdvd-0.17.tar.gz"
-  sha256 "7d2c5bd964acd266b99a61d9054ea64e01204e8e3e1a107abe41b1274969e488"
+  url "https://git.code.sf.net/p/lsdvd/git.git",
+      tag:      "0.21",
+      revision: "de9cf2379335076368cc848de04a60279d944b68"
   license "GPL-2.0-only"
-  revision 4
 
   no_autobump! because: :requires_manual_review
 
@@ -24,11 +24,22 @@ class Lsdvd < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "fd9145daad69de5b544d8e9824b42829c74b048322044a35b76bd9b2b355f1eb"
   end
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
   depends_on "pkgconf" => :build
   depends_on "libdvdcss"
   depends_on "libdvdread"
+  depends_on "libxml2"
+
+  # Move `dvdlogger` function out of `main()`, as Clang (rightfully) does not allow nested functions
+  # Can be removed once this has been merged: https://sourceforge.net/p/lsdvd/git/merge-requests/2/
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/homebrew-core/443cea9c2797b473f4069aad0e12ff06521333b7/Patches/lsdvd/logging.patch"
+    sha256 "5879230867a18b52264428b064e2b5f96423563da1409909f85e0f2163e0ae94"
+  end
 
   def install
+    system "autoreconf", "--force", "--install", "--verbose"
     system "./configure", "--mandir=#{man}", *std_configure_args
     system "make", "install"
   end
