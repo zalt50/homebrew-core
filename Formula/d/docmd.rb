961aa1bbbe575191491801c79dee6da78c2ebc6b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-03 07:56:46 +0000

    docmd 0.3.6

--- Formula/d/docmd.rb
+++ Formula/d/docmd.rb
@@ -1,8 +1,8 @@
 class Docmd < Formula
   desc "Minimal Markdown documentation generator"
   homepage "https://docmd.mgks.dev/"
-  url "https://registry.npmjs.org/@mgks/docmd/-/docmd-0.3.4.tgz"
-  sha256 "ca5e453021670085297927a8d0cd93fc6826779b40dea4b96d37ad045acfe50c"
+  url "https://registry.npmjs.org/@mgks/docmd/-/docmd-0.3.6.tgz"
+  sha256 "58ca4a7192526526421729a614a4802cb9e86651f8ba7308e3f71cfcc3795c91"
   license "MIT"
 
   bottle do
