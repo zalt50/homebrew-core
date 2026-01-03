78e0406e609f5dd574424a63440d202d514f3a15
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-03 12:05:08 +0000

    technitium-dns: update 14.3.0_1 bottle.

--- Formula/t/technitium-dns.rb
+++ Formula/t/technitium-dns.rb
@@ -8,11 +8,11 @@ class TechnitiumDns < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "44a0aacc0e40a4ded693196af29585b824d50a89eab604596e6baf39d75b618b"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d839fe04ce2f5a1353c3f383e38770b05aeeb2da69aceabd8d64522c5257587e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9e9f620d973dbe55836c79c2e4b2f45bde4a6ee01fea3eab47bf1087695d3ed0"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "231953d405e500b13bfdebd0feae8e0be0bf8df6487122b5629c0a057e87229b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "75da168d8b3f17970b6c90ffd0853cbc5a704bdfeff6a9240a3e398c53ddf94d"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "985294f14499dc8b8bf5e88b5864fd63c2f6d0faf4c84694bc80fc6933139788"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5ed3b7b6ac909a94f18d4f85f31448a741523b4b09d2f9d62baec3c02e572df1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "082f392340bfc89f4e507b023db57a75b8cc72b6fe105b9e7a798a0eabd5a0dc"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c4716ff2acfe129a9ec49b55f6b8bcc025276a8c0ba548b32361aa27f932991f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0b95454ad1fa1891156fec79b9c0ef3fe515e218df1823319591837524a69799"
   end
 
   depends_on "dotnet@9"
