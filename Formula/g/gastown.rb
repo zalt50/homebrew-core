0c089be1a154fb02829e7f70b229e5f92e0393e6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-05 13:58:53 +0000

    gastown: add 0.1.1 bottle.

--- Formula/g/gastown.rb
+++ Formula/g/gastown.rb
@@ -5,6 +5,15 @@ class Gastown < Formula
   sha256 "2c0bc95505675b74586bd8cbcd7e5f13c9b8bac1d6197899e76c6ee84df1f8b8"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "78b5a3ff5c2160d336324e42bdd8099226bcf7f1d0418046f60a8d3948f0c33f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "78b5a3ff5c2160d336324e42bdd8099226bcf7f1d0418046f60a8d3948f0c33f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "78b5a3ff5c2160d336324e42bdd8099226bcf7f1d0418046f60a8d3948f0c33f"
+    sha256 cellar: :any_skip_relocation, sonoma:        "295e00e1e114295f31ee0557a8fa66383edb7a8bfa25e4eea67e6e4fa23c5a56"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "870c99956616f6ff8b2b32cc1ec68a2834732342d86ecf75d79b47a5db4bce7a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7ad0ae976b5d4cac8b0baa430f5979ba5b4b54758bf0e6b239fb0899953aa206"
+  end
+
   depends_on "go" => :build
   depends_on "beads"
 
