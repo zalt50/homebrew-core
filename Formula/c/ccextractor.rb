e25d4b160a844850d90cb57ed99075142aa47ced
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-05 21:52:54 +0000

    ccextractor: add 0.96.3 bottle.

--- Formula/c/ccextractor.rb
+++ Formula/c/ccextractor.rb
@@ -5,6 +5,15 @@ class Ccextractor < Formula
   sha256 "17037e8cc52773d3c9a88dcaeb1921cec9a0e4f92ff355fcc318585da41c25b4"
   license "GPL-2.0-only"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "2f0aa09821576492695638e9a18ad46146bcf707b1d05fb21acaee0366bf47fc"
+    sha256 cellar: :any,                 arm64_sequoia: "d82db02d25dfcb70c32a72c9e9c6776fc554b2e454330d1c954229f801b1a052"
+    sha256 cellar: :any,                 arm64_sonoma:  "8772c66a99b20e38da45853be84661b6afab8f6c82786f00742535c984dc0278"
+    sha256 cellar: :any,                 sonoma:        "c05af963b7e418702626652d8a1d692b6ededdeede9f93744b72a447f1a7ad11"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "28ee674242b720a3393c2f1b0da9262a0f80e494f74478bc0675fb730a677052"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e845311c9f2490dd8548b3154b10d44dd2a1e46a9e563107204fa9c8565c3030"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "freetype"
