c51934c86d8a7525aed01cdd0f07413abc01b797
Mike McQuaid <mike@mikemcquaid.com>
2026-01-05 17:54:47 +0000

    ffmpeg: slim down formula.

--- Formula/f/ffmpeg.rb
+++ Formula/f/ffmpeg.rb
@@ -6,6 +6,8 @@ class Ffmpeg < Formula
   # None of these parts are used by default, you have to explicitly pass `--enable-gpl`
   # to configure to activate them. In this case, FFmpeg's license changes to GPL v2+.
   license "GPL-2.0-or-later"
+  revision 1
+  compatibility_version 1
   head "https://github.com/FFmpeg/FFmpeg.git", branch: "master"
 
   livecheck do
@@ -23,62 +25,18 @@ class Ffmpeg < Formula
   end
 
   depends_on "pkgconf" => :build
-  depends_on "aom"
-  depends_on "aribb24"
-  depends_on "dav1d"
-  depends_on "fontconfig"
-  depends_on "freetype"
-  depends_on "frei0r"
-  depends_on "gnutls"
-  depends_on "harfbuzz"
-  depends_on "jpeg-xl"
+
+  # Only add dependencies required for dependents in homebrew-core.
+  # Add other dependencies to ffmpeg-full formula.
   depends_on "lame"
-  depends_on "libass"
-  depends_on "libbluray"
-  depends_on "librist"
-  depends_on "libsoxr"
-  depends_on "libssh"
-  depends_on "libvidstab"
-  depends_on "libvmaf"
-  depends_on "libvorbis"
-  depends_on "libvpx"
-  depends_on "libx11"
-  depends_on "libxcb"
-  depends_on "opencore-amr"
-  depends_on "openjpeg"
   depends_on "opus"
-  depends_on "rav1e"
-  depends_on "rubberband"
   depends_on "sdl2"
-  depends_on "snappy"
-  depends_on "speex"
-  depends_on "srt"
   depends_on "svt-av1"
-  depends_on "tesseract"
-  depends_on "theora"
-  depends_on "webp"
   depends_on "x264"
-  depends_on "x265"
-  depends_on "xvid"
-  depends_on "xz"
-  depends_on "zeromq"
-  depends_on "zimg"
 
   uses_from_macos "bzip2"
   uses_from_macos "libxml2"
   uses_from_macos "zlib"
-
-  on_macos do
-    depends_on "libarchive"
-    depends_on "libogg"
-    depends_on "libsamplerate"
-  end
-
-  on_linux do
-    depends_on "alsa-lib"
-    depends_on "libxext"
-    depends_on "libxv"
-  end
 
   on_intel do
     depends_on "nasm" => :build
@@ -104,48 +62,11 @@ class Ffmpeg < Formula
       --host-cflags=#{ENV.cflags}
       --host-ldflags=#{ENV.ldflags}
       --enable-ffplay
-      --enable-gnutls
       --enable-gpl
-      --enable-libaom
-      --enable-libaribb24
-      --enable-libbluray
-      --enable-libdav1d
-      --enable-libharfbuzz
-      --enable-libjxl
-      --enable-libmp3lame
-      --enable-libopus
-      --enable-librav1e
-      --enable-librist
-      --enable-librubberband
-      --enable-libsnappy
-      --enable-libsrt
-      --enable-libssh
       --enable-libsvtav1
-      --enable-libtesseract
-      --enable-libtheora
-      --enable-libvidstab
-      --enable-libvmaf
-      --enable-libvorbis
-      --enable-libvpx
-      --enable-libwebp
+      --enable-libopus
       --enable-libx264
-      --enable-libx265
-      --enable-libxml2
-      --enable-libxvid
-      --enable-lzma
-      --enable-libfontconfig
-      --enable-libfreetype
-      --enable-frei0r
-      --enable-libass
-      --enable-libopencore-amrnb
-      --enable-libopencore-amrwb
-      --enable-libopenjpeg
-      --enable-libspeex
-      --enable-libsoxr
-      --enable-libzmq
-      --enable-libzimg
-      --disable-libjack
-      --disable-indev=jack
+      --enable-libmp3lame
     ]
 
     # Needs corefoundation, coremedia, corevideo
@@ -161,15 +82,16 @@ class Ffmpeg < Formula
     pkgshare.install buildpath/"tools/python"
   end
 
+  def caveats
+    <<~EOS
+      ffmpeg-full includes additional tools and libraries that are not included in the regular ffmpeg formula.
+    EOS
+  end
+
   test do
     # Create a 5 second test MP4
     mp4out = testpath/"video.mp4"
     system bin/"ffmpeg", "-filter_complex", "testsrc=rate=1:duration=5", mp4out
-    assert_match(/Duration: 00:00:05\.00,.*Video: h264/m, shell_output("#{bin}/ffprobe -hide_banner #{mp4out} 2>&1"))
-
-    # Re-encode it in HEVC/Matroska
-    mkvout = testpath/"video.mkv"
-    system bin/"ffmpeg", "-i", mp4out, "-c:v", "hevc", mkvout
-    assert_match(/Duration: 00:00:05\.00,.*Video: hevc/m, shell_output("#{bin}/ffprobe -hide_banner #{mkvout} 2>&1"))
+    assert_path_exists mp4out, "Failed to create video.mp4!"
   end
 end
