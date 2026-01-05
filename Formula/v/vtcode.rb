11563758da832d49af8381533996d2521c9e3c9e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-05 15:16:02 +0000

    vtcode: update 0.58.23 bottle.

--- Formula/v/vtcode.rb
+++ Formula/v/vtcode.rb
@@ -7,12 +7,12 @@ class Vtcode < Formula
   head "https://github.com/vinhnx/vtcode.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "787ca329eaf6424f76817fd0b62f8bf91b557a0d8238f3d1019b774fb28208b5"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "44c06ae015ba82ffbcc2d301f62b2d076facc8c10fdca628c383edcb8a22fe66"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5ed859b673850419f9aa50ee6e17cc586d77b84a5828b885b83d7ff2a160bc58"
-    sha256 cellar: :any_skip_relocation, sonoma:        "5f85b7db118f76bdafadc08f2a53708b53fb54d75518d94fca3084f2b7108869"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ab740ef7d0c44c83e50be7f9ec57794049d1e1ba84f3aa37fbdf205da71edc98"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cc7c0a217cd0a13fe83865660c100d51e88e08e4e7df32900442a649fd142d83"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ef263ddbd867c2f96f32d975a7ec4061b9dc0beec0ee320965492088db9e9cc1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "84496ea5ac1e32e8ba8aa40e2e68327d290b718bc951ef1cfc27ee6d7becae81"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "73c3c2c321fc916d47ee43f559d257d6e8be57dc93d0388c128aeeb50cb37403"
+    sha256 cellar: :any_skip_relocation, sonoma:        "ec6d7f61321dd074d39cf90de29df5014d2c990694f17471d4d0e8eadff92976"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "6f607e977aee696475af51dc0c7f45aa8f2533c1423ec62290709655593c11a2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "094a53932ef90290e6f72264a3bcc263af236d7210db18f39b227607165c7d49"
   end
 
   depends_on "pkgconf" => :build
