bf62c846f66edc9f16816377adfa93ff7d678bf0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-05 08:04:26 +0000

    git-grab 4.0.1

--- Formula/g/git-grab.rb
+++ Formula/g/git-grab.rb
@@ -1,8 +1,8 @@
 class GitGrab < Formula
   desc "Clone a git repository into a standard location organised by domain and path"
   homepage "https://github.com/wezm/git-grab"
-  url "https://github.com/wezm/git-grab/archive/refs/tags/4.0.0.tar.gz"
-  sha256 "b73f67da252ce02a17e7ee09c9e971374357281f6d92c14117312db4e7b4a66c"
+  url "https://github.com/wezm/git-grab/archive/refs/tags/4.0.1.tar.gz"
+  sha256 "63c080d78dd1d5213b59ae0b98418b9f374c59ccfaa444c55e99b7004fd4fe13"
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
