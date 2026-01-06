d98aea88f7b35f540d34239d7ea7fa99fe8a7c33
Lucas Carlson <lucas@carlson.net>
2026-01-06 08:50:49 -0800

    classifier: use bundle install with Gemfile.lock
    Refactor to use bundler for dependency management instead of
    manually specifying gem resources. This is the preferred pattern
    for Ruby formulas that have a Gemfile.lock.

--- Formula/c/classifier.rb
+++ Formula/c/classifier.rb
@@ -7,38 +7,14 @@ class Classifier < Formula
 
   depends_on "ruby"
 
-  resource "fast-stemmer" do
-    url "https://rubygems.org/gems/fast-stemmer-1.0.2.gem"
-    sha256 "d0aa9fd9cfbca836a09d8abb122552ac8234130271a3b0da1cb077323d650819"
-  end
-
-  resource "mutex_m" do
-    url "https://rubygems.org/gems/mutex_m-0.3.0.gem"
-    sha256 "cfcb04ac16b69c4813777022fdceda24e9f798e48092a2b817eb4c0a782b0751"
-  end
-
-  resource "matrix" do
-    url "https://rubygems.org/gems/matrix-0.4.3.gem"
-    sha256 "a0d5ab7ddcc1973ff690ab361b67f359acbb16958d1dc072b8b956a286564c5b"
-  end
-
-  resource "rake" do
-    url "https://rubygems.org/gems/rake-13.2.1.gem"
-    sha256 "46cb38dae65d7d74b6020a4ac9d48afed8eb8149c040eccf0523bec91907059d"
-  end
-
   def install
+    ENV["BUNDLE_VERSION"] = "system"
+    ENV["BUNDLE_WITHOUT"] = "development:test"
     ENV["GEM_HOME"] = libexec
 
-    resources.each do |r|
-      r.fetch
-      system "gem", "install", r.cached_download, "--ignore-dependencies",
-             "--no-document", "--install-dir", libexec
-    end
-
+    system "bundle", "install"
     system "gem", "build", "classifier.gemspec"
-    system "gem", "install", "--ignore-dependencies", "classifier-#{version}.gem",
-           "--install-dir", libexec
+    system "gem", "install", "classifier-#{version}.gem"
 
     bin.install libexec/"bin/classifier"
     bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
