b1c97853479541775ada1851d0f8cff9695dffc4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-06 05:30:55 +0000

    ipget: update 0.12.2 bottle.

--- Formula/i/ipget.rb
+++ Formula/i/ipget.rb
@@ -7,13 +7,12 @@ class Ipget < Formula
   head "https://github.com/ipfs/ipget.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7d163e6d5db5ff6882311ad197c5cd407b30233c40e421a7aefd072899ea7746"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bc2ebfd1c24295e616273a8bdade66e4d6fa0cd6e1ec0ef944f3dbd4c29624f8"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4be6deb150fe8c9e59bb3a1eae1ba9d2e603842f25183f5f2d23b69f9db24c0f"
-    sha256 cellar: :any_skip_relocation, sonoma:        "a5bac4d9522be84e422661680b6e32df0da1d7d71b2148bc79da6ecdb182b7ab"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "7a45a70d52fde5147524156ce8f357c8ce2c26faa6a5ef34dcb574cc718035d6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b09debcc769e34d7248d13df7b391f84e11ee5d782cfda9ef8b46770fa4c56fc"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "923f13f411c0c39287b54de28592d3aabb61fe5d17233ff1c7d923ac77f0500b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "dcada1f1081ce5b03656196925e3329c5bc438747961287065508dd9fa299b11"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1e1b5185d5069d07eb7850338f2a025bc1f4299db0be436ba2dbdcf286c412aa"
+    sha256 cellar: :any_skip_relocation, sonoma:        "341d791a4ac73a83042b64acc8778689b5c53115d64838ff6b4426fd2b98b578"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c865bd7b7d38f0127ea8aca3d89dbdca91e06e3a2df4771c9a2a888834291deb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "af41d59456696e27ea85444a10b1b5df77ad6b47e61065d25f2f34d3fdf46b38"
   end
 
   depends_on "go" => :build
