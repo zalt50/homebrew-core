f34e54a7f67bb246eba28a37f1327af26ffdd38e
Michael Cho <michael@michaelcho.dev>
2026-01-05 19:39:59 -0500

    qtbase 6.10.1

--- Formula/q/qtbase.rb
+++ Formula/q/qtbase.rb
@@ -1,19 +1,32 @@
 class Qtbase < Formula
   desc "Cross-platform application and UI framework"
   homepage "https://www.qt.io/"
-  # TODO: add `preserve_rpath` DSL in Qt 6.11.0 to Qt formulae
-  url "https://download.qt.io/official_releases/qt/6.9/6.9.3/submodules/qtbase-everywhere-src-6.9.3.tar.xz"
-  mirror "https://qt.mirror.constant.com/archive/qt/6.9/6.9.3/submodules/qtbase-everywhere-src-6.9.3.tar.xz"
-  mirror "https://mirrors.ukfast.co.uk/sites/qt.io/archive/qt/6.9/6.9.3/submodules/qtbase-everywhere-src-6.9.3.tar.xz"
-  sha256 "c5a1a2f660356ec081febfa782998ae5ddbc5925117e64f50e4be9cd45b8dc6e"
   license all_of: [
     { any_of: ["LGPL-3.0-only", "GPL-2.0-only", "GPL-3.0-only"] },
     { "GPL-3.0-only" => { with: "Qt-GPL-exception-1.0" } }, # qmake
     "BSD-3-Clause", # *.cmake
     "GFDL-1.3-no-invariants-only", # *.qdoc
   ]
-  revision 1
   head "https://code.qt.io/qt/qtbase.git", branch: "dev"
+
+  stable do
+    url "https://download.qt.io/official_releases/qt/6.10/6.10.1/submodules/qtbase-everywhere-src-6.10.1.tar.xz"
+    mirror "https://qt.mirror.constant.com/archive/qt/6.10/6.10.1/submodules/qtbase-everywhere-src-6.10.1.tar.xz"
+    mirror "https://mirrors.ukfast.co.uk/sites/qt.io/archive/qt/6.10/6.10.1/submodules/qtbase-everywhere-src-6.10.1.tar.xz"
+    sha256 "5a6226f7e23db51fdc3223121eba53f3f5447cf0cc4d6cb82a3a2df7a65d265d"
+
+    # Backport fix to add framework directory to Cflags on macOS
+    patch do
+      url "https://github.com/qt/qtbase/commit/dc9f359f3ce880b04c1c7b95d06bffc7b991ff09.patch?full_index=1"
+      sha256 "9fbd8b63bfae37cf1380dda0263623843c25b72d74baf9bd9c0ae9a28b31d392"
+    end
+
+    # Backport fix for macdeployqt to find rpath-referenced paths
+    patch do
+      url "https://github.com/qt/qtbase/commit/3bae28598774f1bf1776c0cd82f413e5b3282b7c.patch?full_index=1"
+      sha256 "2f091c0c2f28bbf0f384797cd27eb13029b097750e7b578e7ef745b0a473c6f6"
+    end
+  end
 
   # The first-party website doesn't make version information readily available,
   # so we check the `head` repository tags instead.
@@ -74,6 +87,7 @@ class Qtbase < Formula
     depends_on "mesa"
     depends_on "pango"
     depends_on "systemd"
+    depends_on "wayland"
     depends_on "xcb-util-cursor"
     depends_on "xcb-util-image"
     depends_on "xcb-util-keysyms"
@@ -82,13 +96,6 @@ class Qtbase < Formula
 
     # https://github.com/orgs/Homebrew/discussions/6468#discussioncomment-14687372
     pour_bottle? only_if: :default_prefix
-  end
-
-  # Add framework directory to Cflags on macOS
-  # Ref: https://codereview.qt-project.org/c/qt/qtbase/+/682915
-  patch do
-    url "https://codereview.qt-project.org/changes/qt%2Fqtbase~682915/revisions/1/patch?zip"
-    sha256 "41fc97843c891cc8c5fe513acfc5779bb42a2ac417e6c931efee08ed5eb62201"
   end
 
   def install
@@ -206,21 +213,12 @@ class Qtbase < Formula
   end
 
   def caveats
-    s = <<~CAVEATS
+    <<~CAVEATS
       You can add Homebrew's Qt to QtCreator's "Qt Versions" in:
         Preferences > Qt Versions > Link with Qt...
       pressing "Choose..." and selecting as the Qt installation path:
         #{HOMEBREW_PREFIX}
     CAVEATS
-    on_macos do
-      s += <<~CAVEATS
-
-        We plan to build Qt 6.11 with the `@rpath`-prefixed install names preserved,
-        which may require adding an RPATH into non-QMake-built binaries. Please see:
-          https://github.com/Homebrew/brew/issues/15354
-      CAVEATS
-    end
-    s
   end
 
   test do
@@ -283,8 +281,11 @@ class Qtbase < Formula
       system "./test"
     end
 
-    flags = shell_output("pkgconf --cflags --libs Qt6#{modules.join(" Qt6")}").chomp.split
-    system ENV.cxx, "-std=c++17", "main.cpp", "-o", "test", *flags
+    # Test compiler and linker flags work separately
+    cflags = shell_output("pkgconf --cflags Qt6#{modules.join(" Qt6")}").chomp.split
+    ldflags = shell_output("pkgconf --libs Qt6#{modules.join(" Qt6")}").chomp.split
+    system ENV.cxx, "-std=c++17", "-c", "main.cpp", "-o", "main.o", *cflags
+    system ENV.cxx, "main.o", "-o", "test", *ldflags
     system "./test"
 
     # Check QT_INSTALL_PREFIX is HOMEBREW_PREFIX to support split formulae
