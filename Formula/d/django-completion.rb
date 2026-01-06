c2651a64f0a6206857a0a37d1db447aef0f1ce6f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-06 19:58:05 +0000

    django-completion 6.0.1

--- Formula/d/django-completion.rb
+++ Formula/d/django-completion.rb
@@ -1,8 +1,8 @@
 class DjangoCompletion < Formula
   desc "Bash completion for Django"
   homepage "https://www.djangoproject.com/"
-  url "https://github.com/django/django/archive/refs/tags/6.0.tar.gz"
-  sha256 "a6d0e6620ec7025ee2376f107a572ee9c1d731230ce4634b0a1668a3786dba8a"
+  url "https://github.com/django/django/archive/refs/tags/6.0.1.tar.gz"
+  sha256 "a683d4435d39c4c269eddaddab07e1bf883acb987bb7d26948d62b8ff5a2640d"
   license "BSD-3-Clause"
   head "https://github.com/django/django.git", branch: "main"
 
