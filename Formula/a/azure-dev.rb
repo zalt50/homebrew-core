7f2caaf9e4faaf98486ec8e07772c7e20580fa2d
Leon Adomaitis <leon@adomaitis.de>
2026-01-06 23:31:51 +0100

    azure-dev 1.22.5 (new formula)
    azure-dev: fix install and simplifiy tests

---
+++ Formula/a/azure-dev.rb
@@ -0,0 +1,38 @@
+class AzureDev < Formula
+  desc "Developer CLI that provides commands for working with Azure resources"
+  homepage "https://aka.ms/azd"
+  url "https://github.com/Azure/azure-dev/archive/refs/tags/azure-dev-cli_1.22.5.tar.gz"
+  sha256 "a18920ce941613a9031df78ef3f29b21c9d3521ce6d700ba03d0f243c6971b21"
+  license "MIT"
+  head "https://github.com/Azure/azure-dev.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    (buildpath/".installed-by.txt").write "brew"
+
+    # Version should be in the format "<version> (commit <commit_hash>)"
+    azd_version = "#{version} (commit 0000000000000000000000000000000000000000)"
+    ldflags = %W[
+      -s -w
+      -X "github.com/azure/azure-dev/cli/azd/internal.Version=#{azd_version}"
+    ]
+    system "go", "build", "-C", "cli/azd", *std_go_args(ldflags:, output: bin/"azd")
+
+    generate_completions_from_executable(bin/"azd", shell_parameter_format: :cobra)
+  end
+
+  test do
+    ENV["AZURE_DEV_COLLECT_TELEMETRY"] = "no"
+    ENV["AZD_DISABLE_PROMPTS"] = "1"
+    ENV["AZD_CONFIG_DIR"] = (testpath/"config").to_s
+
+    assert_match version.to_s, shell_output("#{bin}/azd version")
+
+    system bin/"azd", "config", "set", "defaults.location", "eastus"
+    assert_match "eastus", shell_output("#{bin}/azd config get defaults.location")
+
+    expected = "Not logged in, run `azd auth login` to login to Azure"
+    assert_match expected, shell_output("#{bin}/azd auth login --check-status")
+  end
+end
