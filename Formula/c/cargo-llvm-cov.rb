eaf19e3d67eb614643e60b96da637d5092fe89fe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-06 16:58:32 +0000

    cargo-llvm-cov 0.6.23

--- Formula/c/cargo-llvm-cov.rb
+++ Formula/c/cargo-llvm-cov.rb
@@ -2,8 +2,8 @@ class CargoLlvmCov < Formula
   desc "Cargo subcommand to easily use LLVM source-based code coverage"
   homepage "https://github.com/taiki-e/cargo-llvm-cov"
   # cannot use github tarball due to https://github.com/taiki-e/cargo-llvm-cov/pull/152#issuecomment-1107055622
-  url "https://static.crates.io/crates/cargo-llvm-cov/cargo-llvm-cov-0.6.22.crate"
-  sha256 "5df396dc4dcec0a1423011205e00e1df9eb380f44767b3362bbf1052b2ed3cbc"
+  url "https://static.crates.io/crates/cargo-llvm-cov/cargo-llvm-cov-0.6.23.crate"
+  sha256 "0a19af1cb5512737613b5caa313dd14cd56459732867c53a755da4b967ffefe6"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/taiki-e/cargo-llvm-cov.git", branch: "main"
 
