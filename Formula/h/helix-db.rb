e571134dda263a1d580d03458b1ab75b2b452aba
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-06 19:54:48 +0000

    helix-db: add 2.1.10 bottle.

--- Formula/h/helix-db.rb
+++ Formula/h/helix-db.rb
@@ -5,6 +5,15 @@ class HelixDb < Formula
   sha256 "4f42fd3fb5b208eb76767076c1c71ecaa47dbcc80a4e672b03008e5ebe61d613"
   license "AGPL-3.0-only"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ffc34ddb02018443eac441cdaeb2f7a437c448a042d876e26b3aeb5726a5643e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5f73ff4722641d833644a0debd037ba1358f54c7ea1ac795288282542c151067"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d130c5175636df86f19b86b12ecbdd6c452ef98f6f34ca89066cb4b50665988a"
+    sha256 cellar: :any_skip_relocation, sonoma:        "56fd9e43d6cb684859ac8bada45a790226e34de7789ac83d201276dc5b8e8d1d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "51ae29f4a8e352844be23979315ec5ab2d5696424d71e79c7ed7a7fedc14a9de"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7ac4a19092115426443ac3b839dab285cd255b3749cef82e7b7987a5960a30eb"
+  end
+
   depends_on "rust"
   on_linux do
     depends_on "openssl@3"
