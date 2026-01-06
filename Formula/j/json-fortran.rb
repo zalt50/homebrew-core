a4a2db42d52640228fdf9fd06e7f113eba974371
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-06 03:15:45 +0000

    json-fortran: update 9.2.0 bottle.

--- Formula/j/json-fortran.rb
+++ Formula/j/json-fortran.rb
@@ -7,12 +7,12 @@ class JsonFortran < Formula
   head "https://github.com/jacobwilliams/json-fortran.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "a6c47be708e0e06e29694f025f7c5bb98778bec76cd6279a5a6162133212335a"
-    sha256 cellar: :any,                 arm64_sequoia: "ca9dd223cf47ee9f5e9ff9df181bca1bf816bcfe0343cc990d1d1b6788c12b97"
-    sha256 cellar: :any,                 arm64_sonoma:  "9f9e1aefbbfad9e722ad2b027494b1682b47fe879a400cfdc4f633c73dc09760"
-    sha256 cellar: :any,                 sonoma:        "f4decc76b0608c982c9ffbd185e964556b09c6bb2741503a76925edb232f4908"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "da312fff3991993ee95b8b61067154402e845839e98ddbdd08e0b6a81f7cbff7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2ca5565cbc2a7cfa5e4a7db44eaa004e14a7d57a087acbd8d6f3f4503a2d1fb8"
+    sha256 cellar: :any,                 arm64_tahoe:   "b821255d59eebecf840d21eb2611db6c581a8e87e46f41e5caab870fe7cfda24"
+    sha256 cellar: :any,                 arm64_sequoia: "1a426ceab43129311217d4bac2ca7c82c916839190e06c27f94f856a4c154e6e"
+    sha256 cellar: :any,                 arm64_sonoma:  "e129cd071c41cd14c02dc0fdd5728a2faf60ebf8c4748cf79da50b76670ccd57"
+    sha256 cellar: :any,                 sonoma:        "bedf218894bbba4af1c730534c2014ff9ac97cd7ddb4ffb859a2445b3af1cd97"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "4434c002ab2b8672dbd5f63b0cd43c51ce1bca5443b2a1b92576acd96e418eed"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1a81456d3ee0746cf86c4d2752774a35b91ba85fc7e9ba3c20ff56722f2d7c3a"
   end
 
   depends_on "cmake" => :build
