7b5078ef59963142f90b8ef6f2ad77c39b8fe260
Patrick Linnane <patrick@linnane.io>
2026-01-07 06:28:23 +0000

    headscale-cli: add 0.27.1 bottle.

--- Formula/h/headscale-cli.rb
+++ Formula/h/headscale-cli.rb
@@ -5,6 +5,15 @@ class HeadscaleCli < Formula
   sha256 "a2ba09811919e4b285d17e4cdaf7ed5aeb9a8567eda11119557436d59711632e"
   license "BSD-3-Clause"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e15061b3c727bc860a859da8491403b6e1fcacd6514232591a19d075f0ff396a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3305a23e2fdc6012550b660c2b6980b40ff2480ede1b67c36f08ae14ca5fec43"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6d71db25739ea4295686553e4593bc85cc95286ee7f654c4d3fa462543f96d5c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "f02885e6e6199593b9160501f53e64fad91598dd691fd4b10858f41bc24acb42"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "08ba60d1591482052e607b8483e59561eeed8fcb5c1b9ebe90f755aab9515fc8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e24da24a3c45a44d2c6c2f7386c222a6685785635c7ba2ea1bec5675ad319bd4"
+  end
+
   depends_on "go" => :build
 
   def install
