e26dd5bdaef5aef6ead97be3ef305acdffc7b56c
Mike McQuaid <mike@mikemcquaid.com>
2026-01-07 12:58:01 +0000

    fceux: revision bump for ffmpeg.

--- Formula/f/fceux.rb
+++ Formula/f/fceux.rb
@@ -2,7 +2,7 @@ class Fceux < Formula
   desc "All-in-one NES/Famicom Emulator"
   homepage "https://fceux.com/"
   license "GPL-2.0-only"
-  revision 7
+  revision 8
   head "https://github.com/TASEmulators/fceux.git", branch: "master"
 
   stable do
