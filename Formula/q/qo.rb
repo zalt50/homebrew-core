a0c3c62e67f945839d3863d1edacc14e9a0adfdf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-07 05:41:33 +0000

    qo: add 0.2.7 bottle.

--- Formula/q/qo.rb
+++ Formula/q/qo.rb
@@ -5,6 +5,15 @@ class Qo < Formula
   sha256 "ea692a0063499f596ceea738ee07388cc864703d33c2e989393df4fb67f1e488"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d79fbb0c4d606d5f4fefd9b0764946d794ac7309716d8cc77dab097f7b18b9b0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d79fbb0c4d606d5f4fefd9b0764946d794ac7309716d8cc77dab097f7b18b9b0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d79fbb0c4d606d5f4fefd9b0764946d794ac7309716d8cc77dab097f7b18b9b0"
+    sha256 cellar: :any_skip_relocation, sonoma:        "547f65d785291db3a32828e2c499219978219ee3b5b35ab3dd4bfdbaa216d622"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "11c337a1694c42177b048664a40aceadff6dacbf97487bbd923f8a9531ebaf24"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "425f6cc53b34f352370d7ee24efc61db9888e129400e749eb17ff66f7cb3d102"
+  end
+
   depends_on "go" => :build
 
   def install
