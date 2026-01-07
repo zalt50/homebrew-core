9d75b0e8f7c6794a89eeb1ea34d92b7ffdcf3f04
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-07 14:46:45 +0000

    robot-framework: update 7.4.1_1 bottle.

--- Formula/r/robot-framework.rb
+++ Formula/r/robot-framework.rb
@@ -10,12 +10,12 @@ class RobotFramework < Formula
   head "https://github.com/robotframework/robotframework.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "41013753d0d9d1b21bf3840d1ade5576bf2ed4a0110d02f0fb75a60ce492450c"
-    sha256 cellar: :any,                 arm64_sequoia: "c2d408db151e4ebbce078be5a1825dec943f3fb61a2acd12b86f67ee9ac5c7a1"
-    sha256 cellar: :any,                 arm64_sonoma:  "8473aa24fa0ed12f0ac280072a0b0de053ebde280cafec8432cd47c1cb5ba874"
-    sha256 cellar: :any,                 sonoma:        "fd9308b626b3d36c323663d81ab48d28a2e52e4ad8ebec0575e01f211fc2877b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "90da96fa8c9febed2be0ceded5ca86964997cbf5f9484881d213a104ee9e1b6f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3d566c5d935acc7971e43dd9e561e31767a54c87254c94cb34101c7bc12ed12c"
+    sha256 cellar: :any,                 arm64_tahoe:   "a3a04e6a35fd9fab0dbb5d4f4afc1f6fa699555860391379ed047656b75c4bb6"
+    sha256 cellar: :any,                 arm64_sequoia: "740cf0a69d2705e86dcb0f6af99e6def683ae2650df0772fc2e689bf2dabbb57"
+    sha256 cellar: :any,                 arm64_sonoma:  "f0ca98a314d993b806a0a6d8953f7fd41c192c62b84ac5f86cbdd3e81b4b9eac"
+    sha256 cellar: :any,                 sonoma:        "7bbb803f08eabcc3c5daa568f883476d2e186ab66a82f515499b6dfc6feca13d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "b90248a2026cb634db0e60641d73718264c638b9b394a30c3e0a1986b4efdab1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6b20d7ab8d262a4ee7e017636b794022250bf3551da3dd4f2246f86957eef142"
   end
 
   depends_on "rust" => :build # for bcrypt
