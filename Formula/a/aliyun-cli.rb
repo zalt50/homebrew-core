715d145b488007cdc37970b7d556e068d3163990
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-07 11:28:32 +0000

    aliyun-cli: update 3.2.6 bottle.

--- Formula/a/aliyun-cli.rb
+++ Formula/a/aliyun-cli.rb
@@ -13,12 +13,12 @@ class AliyunCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d7b91ea5eea04c7867a8200a5a2e90174b832c43070e98f50078c00236ee9f3e"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d7b91ea5eea04c7867a8200a5a2e90174b832c43070e98f50078c00236ee9f3e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d7b91ea5eea04c7867a8200a5a2e90174b832c43070e98f50078c00236ee9f3e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "83a8cc9686adecf155459cf441b9f3fadbacb139bfcdddd49b5032fde61a7944"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "909f60e6f1084d7436b03af7619ff8f45ae312d9dfec24ed550137cac648c23b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b411b52eec8546e808551755e3370000160db9828c34b7b096136b37267baf5a"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "02bb6df7ac856558e1c69fcc1103bc0ab7eb8ea339bfaf132f75b3468e119d5f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "02bb6df7ac856558e1c69fcc1103bc0ab7eb8ea339bfaf132f75b3468e119d5f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "02bb6df7ac856558e1c69fcc1103bc0ab7eb8ea339bfaf132f75b3468e119d5f"
+    sha256 cellar: :any_skip_relocation, sonoma:        "69ae0222992bbfa401653ad8745f50a3e59860fa4bee400ba22e5faa7d9b89ae"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "85c69c2b44009bf3357d56e2a054a1bbd8effddb1bb0595f64cb1aff1065d45b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "211e26a6a4952b2c7b9f1c93716174b111a50e18ba1d1ac38c674179cca95c68"
   end
 
   depends_on "go" => :build
