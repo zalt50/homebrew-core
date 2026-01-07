cab42007a33219d9656dc38d93ee68095fb7f826
MoonFruit <dkmoonfruit@gmail.com>
2026-01-07 10:11:38 +0800

    oxfmt 0.23.0 (new formula)

---
+++ Formula/o/oxfmt.rb
@@ -0,0 +1,20 @@
+class Oxfmt < Formula
+  desc "High-performance formatting tool for JavaScript and TypeScript"
+  homepage "https://oxc.rs/"
+  url "https://registry.npmjs.org/oxfmt/-/oxfmt-0.23.0.tgz"
+  sha256 "c4047cf33f0921441b13c7ff3b2c3e10004b484fe8f10c24ae2a85c2cf0ce852"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    (testpath/"test.js").write("const arr = [1,2];")
+    system bin/"oxfmt", "test.js"
+    assert_equal "const arr = [1, 2];\n", (testpath/"test.js").read
+  end
+end
