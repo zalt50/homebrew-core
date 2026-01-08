0dfab809ed336dde1f0dc7512681846b924a8619
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-08 05:04:26 +0000

    haskell-stack: update 3.9.1 bottle.

--- Formula/h/haskell-stack.rb
+++ Formula/h/haskell-stack.rb
@@ -12,14 +12,12 @@ class HaskellStack < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "af23e3d97a6a29edecd7d64cebe979329a4a1acb873e30fa9f3bb47469deebca"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f3231702c5fcba9e516315148b1d002bd8a992b023a9c9d792e319e707c3fc49"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "aa271a60946b01b795002384e6de6aec40e0b7183bb52210be9ff385845f4f75"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "0c53dca7b80ae751d1b41730be17e01a437cfca9a1118360f41566ed52434150"
-    sha256 cellar: :any_skip_relocation, sonoma:        "197800b9dd379f80b3d7e9bbe6a0a38a7099f986335e255593d594764915596a"
-    sha256 cellar: :any_skip_relocation, ventura:       "1dda2642149f8c66b3e682146eaf9a77c91eef6304ac7d50059842c68be1be76"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "0610976a50463a6df9351c0db91b583dd1032cddefa4670f5c86096149e1a1cb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8342bcb2b1b190c7f4278e29b2e91301024dbe884da0d865b756dc5ddd9b8f6e"
+    sha256 cellar: :any,                 arm64_tahoe:   "c6b50ab326697af395bb385a55167324d1f2eab1fcf52faf71b95cd25531153e"
+    sha256 cellar: :any,                 arm64_sequoia: "febdcc3b72055ced04faa19d5fcc48d1cbc419f0474d0a9f42f893935f6daa27"
+    sha256 cellar: :any,                 arm64_sonoma:  "428c6647e21e176a6988b959d8ef8aa1096b4149f07463274fda2214adfe165c"
+    sha256 cellar: :any,                 sonoma:        "4cedb63d472a8daefbf529b612549a0fc37bcd0b0c7c5ac6c646dea62ffbe5d9"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d7f0ed813cb29ccfd1e78d237dc9dd1e4bd35d0901cd0df760c31bdff2ec7856"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "302102c37c197fad07cfb8e35d34de036e27708558f796e836813bdfc889c81b"
   end
 
   depends_on "cabal-install" => :build
