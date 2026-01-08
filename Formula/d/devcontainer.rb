b62d7d9adc86695a0b036c3e295229abda1ad31b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-08 20:20:09 +0000

    devcontainer: update 0.81.0 bottle.

--- Formula/d/devcontainer.rb
+++ Formula/d/devcontainer.rb
@@ -6,7 +6,7 @@ class Devcontainer < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "ac9b6c565fb9a1cea644ba5ef043a1ac024de1b801fa13d1e4301d035f557176"
+    sha256 cellar: :any_skip_relocation, all: "b52b1514727870642f7981ac6d0f834ad4858c8889036bf53781eb8b86b1fb09"
   end
 
   depends_on "node"
