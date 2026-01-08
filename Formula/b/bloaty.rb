38e70191c88accb22abb927c2107eba663a66c60
Michael Cho <michael@michaelcho.dev>
2026-01-08 15:40:59 -0500

    bloaty: revision bump (abseil 20260107.0)

--- Formula/b/bloaty.rb
+++ Formula/b/bloaty.rb
@@ -2,7 +2,7 @@ class Bloaty < Formula
   desc "Size profiler for binaries"
   homepage "https://github.com/google/bloaty"
   license "Apache-2.0"
-  revision 44
+  revision 45
   head "https://github.com/google/bloaty.git", branch: "main"
 
   stable do
@@ -36,6 +36,9 @@ class Bloaty < Formula
   depends_on "re2"
 
   def install
+    # Workaround until new release with
+    # https://github.com/google/bloaty/commit/9677d4938ec5be44f04eae774c94e10d339fd3a7
+    ENV.append "CXXFLAGS", "-include stdexcept"
     # https://github.com/protocolbuffers/protobuf/issues/9947
     ENV.append_to_cflags "-DNDEBUG"
     # Remove vendored dependencies
