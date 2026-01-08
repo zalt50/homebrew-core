70fa0a3964560f35bedcfd5f09cb400fab82074a
Michael Cho <michael@michaelcho.dev>
2026-01-07 23:14:14 -0500

    patat: build with `ghc`

--- Formula/p/patat.rb
+++ Formula/p/patat.rb
@@ -1,10 +1,19 @@
 class Patat < Formula
   desc "Terminal-based presentations using Pandoc"
   homepage "https://github.com/jaspervdj/patat"
-  url "https://hackage.haskell.org/package/patat-0.15.2.0/patat-0.15.2.0.tar.gz"
-  sha256 "d1f182ecdf145b8db1aacee1c4d46731d197b192e6ef855c3505067c1cea2b65"
   license "GPL-2.0-or-later"
   head "https://github.com/jaspervdj/patat.git", branch: "main"
+
+  stable do
+    url "https://hackage.haskell.org/package/patat-0.15.2.0/patat-0.15.2.0.tar.gz"
+    sha256 "d1f182ecdf145b8db1aacee1c4d46731d197b192e6ef855c3505067c1cea2b65"
+
+    # Backports relaxed dependency constraints to build with GHC >= 9.12
+    patch do
+      url "https://github.com/jaspervdj/patat/commit/16d568bc414f4f0ced8b4f897c3584fd82a7797a.patch?full_index=1"
+      sha256 "1a0f9aa653b9dd8b47acbdaabee3b50b557b87ca8d2c364a85a8f8b8ec637abc"
+    end
+  end
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_tahoe:   "00aa976ca5fdacc86e38303a701c6b39f69c66dc71df0737d797222981f377f5"
@@ -18,10 +27,12 @@ class Patat < Formula
   end
 
   depends_on "cabal-install" => :build
-  depends_on "ghc@9.10" => :build
+  depends_on "ghc" => :build
   depends_on "pandoc" => :build
   depends_on "pkgconf" => :build
+  depends_on "gmp"
 
+  uses_from_macos "libffi"
   uses_from_macos "zlib"
 
   def install
