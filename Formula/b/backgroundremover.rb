28d625811ae3215eb1f3b9dfba36a5a6b6d26705
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-08 08:12:20 +0000

    backgroundremover: bump python resources

--- Formula/b/backgroundremover.rb
+++ Formula/b/backgroundremover.rb
@@ -6,6 +6,7 @@ class Backgroundremover < Formula
   url "https://files.pythonhosted.org/packages/b9/4a/18deace4c210d722e4dde75a7f445f105d96d190f767b9132d789b1503dd/backgroundremover-0.3.8.tar.gz"
   sha256 "0f6c25e500eab0268fdc3f50574e5f8aca1f5f47fcad86f4b52ea2af814a7434"
   license "MIT"
+  revision 1
 
   bottle do
     rebuild 1
@@ -174,8 +175,8 @@ class Backgroundremover < Formula
   end
 
   resource "urllib3" do
-    url "https://files.pythonhosted.org/packages/1e/24/a2a2ed9addd907787d7aa0355ba36a6cadf1768b934c652ea78acbd59dcd/urllib3-2.6.2.tar.gz"
-    sha256 "016f9c98bb7e98085cb2b4b17b87d2c702975664e4f060c6532e64d1c1a5e797"
+    url "https://files.pythonhosted.org/packages/c7/24/5f1b3bdffd70275f6661c76461e25f024d5a38a46f04aaca912426a2b1d3/urllib3-2.6.3.tar.gz"
+    sha256 "1b62b6884944a57dbe321509ab94fd4d3b307075e0c2eae991ac71ee15ad38ed"
   end
 
   resource "waitress" do
