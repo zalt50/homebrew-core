8fb65edd5212d9aea21c96481c0d5666cb214942
botantony <antonsm21@gmail.com>
2026-01-08 04:48:59 +0100

    cucumber-cpp 0.8.0

--- Formula/c/cucumber-cpp.rb
+++ Formula/c/cucumber-cpp.rb
@@ -2,11 +2,9 @@ class CucumberCpp < Formula
   desc "Support for writing Cucumber step definitions in C++"
   homepage "https://cucumber.io"
   url "https://github.com/cucumber/cucumber-cpp.git",
-      tag:      "v0.7.0",
-      revision: "ceb025fb720f59b3c8d98ab0de02925e7eab225c"
+      tag:      "v0.8.0",
+      revision: "38bd34a3caaeb3fa6ab80d09b323e1a9d6fe24b7"
   license "MIT"
-
-  no_autobump! because: :requires_manual_review
 
   bottle do
     rebuild 2
@@ -24,19 +22,8 @@ class CucumberCpp < Formula
   depends_on "asio"
   depends_on "tclap"
 
-  # Backport support for Asio 1.33+
-  patch do
-    url "https://github.com/cucumber/cucumber-cpp/commit/da6345bd1d0b0ac4cf4bc71866c98d55c72522a8.patch?full_index=1"
-    sha256 "1ad7f6513ee2d41c5625933b05249704c14484a7029acd330c5f665a11e6a66f"
-  end
-
   def install
-    # TODO: Remove Cuke args on next release as they are the defaults
     args = %w[
-      -DCUKE_ENABLE_BOOST_TEST=OFF
-      -DCUKE_ENABLE_GTEST=OFF
-      -DCUKE_ENABLE_QT=OFF
-      -DCUKE_TESTS_UNIT=OFF
       -DBUILD_SHARED_LIBS=ON
     ]
 
