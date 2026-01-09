13628af8cb9cde1ec6004bd315a184f5eb6726a3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-09 17:13:22 +0000

    meta-package-manager 6.0.2

--- Formula/m/meta-package-manager.rb
+++ Formula/m/meta-package-manager.rb
@@ -4,8 +4,8 @@ class MetaPackageManager < Formula
 
   desc "Wrapper around all package managers with a unifying CLI"
   homepage "https://kdeldycke.github.io/meta-package-manager/"
-  url "https://files.pythonhosted.org/packages/4b/ba/9c67293cbe38e51255f05800e55ea339cf9270e7e1ee7b1aa4ee0da62491/meta_package_manager-6.0.1.tar.gz"
-  sha256 "fb8c8376fc5d5d8b29d0b09e11c773f8da1f0c1f25fc9bc1bd357b6ba165d31d"
+  url "https://files.pythonhosted.org/packages/55/04/b6c2432a812ec3cbb184a124fbc45b75af63d89e6cb7bebf354635a22937/meta_package_manager-6.0.2.tar.gz"
+  sha256 "6d84f35f890414640f30d37c8ff7a3cfb35a0f96de277473ae637f2717fc304a"
   license "GPL-2.0-or-later"
   head "https://github.com/kdeldycke/meta-package-manager.git", branch: "main"
 
@@ -126,8 +126,8 @@ class MetaPackageManager < Formula
   end
 
   resource "jsonschema" do
-    url "https://files.pythonhosted.org/packages/74/69/f7185de793a29082a9f3c7728268ffb31cb5095131a9c139a74078e27336/jsonschema-4.25.1.tar.gz"
-    sha256 "e4a9655ce0da0c0b67a085847e00a3a51449e1157f4f75e9fb5aa545e122eb85"
+    url "https://files.pythonhosted.org/packages/b3/fc/e067678238fa451312d4c62bf6e6cf5ec56375422aee02f9cb5f909b3047/jsonschema-4.26.0.tar.gz"
+    sha256 "0c26707e2efad8aa1bfc5b7ce170f3fccc2e4918ff85989ba9ffa9facb2be326"
   end
 
   resource "jsonschema-specifications" do
@@ -261,8 +261,8 @@ class MetaPackageManager < Formula
   end
 
   resource "urllib3" do
-    url "https://files.pythonhosted.org/packages/1e/24/a2a2ed9addd907787d7aa0355ba36a6cadf1768b934c652ea78acbd59dcd/urllib3-2.6.2.tar.gz"
-    sha256 "016f9c98bb7e98085cb2b4b17b87d2c702975664e4f060c6532e64d1c1a5e797"
+    url "https://files.pythonhosted.org/packages/c7/24/5f1b3bdffd70275f6661c76461e25f024d5a38a46f04aaca912426a2b1d3/urllib3-2.6.3.tar.gz"
+    sha256 "1b62b6884944a57dbe321509ab94fd4d3b307075e0c2eae991ac71ee15ad38ed"
   end
 
   resource "wcmatch" do
