531572e97093059e68156936561f39bc4ad1baae
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-09 11:18:19 +0000

    maturin 1.11.5

--- Formula/m/maturin.rb
+++ Formula/m/maturin.rb
@@ -1,8 +1,8 @@
 class Maturin < Formula
   desc "Build and publish Rust crates as Python packages"
   homepage "https://github.com/PyO3/maturin"
-  url "https://github.com/PyO3/maturin/archive/refs/tags/v1.11.4.tar.gz"
-  sha256 "50aff01e366b4938f6021add0ee5f5a0a4d43f0209bbf099f35bf5dac4f4dfe2"
+  url "https://github.com/PyO3/maturin/archive/refs/tags/v1.11.5.tar.gz"
+  sha256 "4edf379859d0126ed860c1f1905da304ce4aea37f3ad58ede294a3920cb9e3aa"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/PyO3/maturin.git", branch: "main"
 
