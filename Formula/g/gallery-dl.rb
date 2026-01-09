5a2370857bbd5978960d408e7e8b65fd61adad4b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-09 08:25:12 +0000

    gallery-dl: bump python resources

--- Formula/g/gallery-dl.rb
+++ Formula/g/gallery-dl.rb
@@ -6,6 +6,7 @@ class GalleryDl < Formula
   url "https://files.pythonhosted.org/packages/89/ef/b1f6f6daf49d51bfa75b513c20885cf5c1d607be7df59e64d7c5ba4ae15c/gallery_dl-1.31.2.tar.gz"
   sha256 "4ca3f02df89d9c8e15b4b2cb665c0cfa5e51d243644e1dcbf9c23f2d841907ab"
   license "GPL-2.0-only"
+  revision 1
   head "https://github.com/mikf/gallery-dl.git", branch: "master"
 
   bottle do
@@ -33,8 +34,8 @@ class GalleryDl < Formula
   end
 
   resource "urllib3" do
-    url "https://files.pythonhosted.org/packages/1e/24/a2a2ed9addd907787d7aa0355ba36a6cadf1768b934c652ea78acbd59dcd/urllib3-2.6.2.tar.gz"
-    sha256 "016f9c98bb7e98085cb2b4b17b87d2c702975664e4f060c6532e64d1c1a5e797"
+    url "https://files.pythonhosted.org/packages/c7/24/5f1b3bdffd70275f6661c76461e25f024d5a38a46f04aaca912426a2b1d3/urllib3-2.6.3.tar.gz"
+    sha256 "1b62b6884944a57dbe321509ab94fd4d3b307075e0c2eae991ac71ee15ad38ed"
   end
 
   def install
