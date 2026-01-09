5ca5ae7bbe638651097bbf3d1356b5918e6abd3b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-09 05:22:14 +0000

    okta-aws-cli: update 2.5.2 bottle.

--- Formula/o/okta-aws-cli.rb
+++ Formula/o/okta-aws-cli.rb
@@ -12,12 +12,12 @@ class OktaAwsCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e58aa4aa282022c36d1353d265dadaf40f78be077fcd8e39cedf27ab8e7aef63"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e58aa4aa282022c36d1353d265dadaf40f78be077fcd8e39cedf27ab8e7aef63"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e58aa4aa282022c36d1353d265dadaf40f78be077fcd8e39cedf27ab8e7aef63"
-    sha256 cellar: :any_skip_relocation, sonoma:        "f8c60a47a42b009829c814017fa65c1e573499626ac6b98d7af1ad97644f2402"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "070520ed3fb031e13f9c1a836b32c89541e18c7c8178aea70f99e5c8eb3f7181"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9a93368251368737cb1180d07acfa7eb2ff185888a6c688f0affe7885d94bfce"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4ad772819bb6ad83c57c87822d410200627928d8d82f71c907edeecb7ed8a7ab"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4ad772819bb6ad83c57c87822d410200627928d8d82f71c907edeecb7ed8a7ab"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4ad772819bb6ad83c57c87822d410200627928d8d82f71c907edeecb7ed8a7ab"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e4a890a0bc52667d531355a344b0b877a103d43afb0d567236e212b2a775a6ac"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "325e1371f1df291464c6b2306d8581425e5fedaed0c47276a5d839daa4aa2a19"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a0fbbdaaf884202bf7a25da03a57668346deda8653a235b3598d6dc5df1579dc"
   end
 
   depends_on "go" => :build
