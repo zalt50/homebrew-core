965be8db68f70b2105ed41eaa6cbb3e73e2b18ad
Vicente Adolfo Bolea Sanchez <vicente.bolea@kitware.com>
2026-01-09 12:57:32 -0500

    adios2: update to v2.11.0

--- Formula/a/adios2.rb
+++ Formula/a/adios2.rb
@@ -1,10 +1,9 @@
 class Adios2 < Formula
   desc "Next generation of ADIOS developed in the Exascale Computing Program"
   homepage "https://adios2.readthedocs.io"
-  url "https://github.com/ornladios/ADIOS2/archive/refs/tags/v2.10.2.tar.gz"
-  sha256 "14cf0bcd94772194bce0f2c0e74dba187965d1cffd12d45f801c32929158579e"
+  url "https://github.com/ornladios/ADIOS2/archive/refs/tags/v2.11.0.tar.gz"
+  sha256 "0a2bd745e3f39745f07587e4a5f92d72f12fa0e2be305e7957bdceda03735dbf"
   license "Apache-2.0"
-  revision 1
   head "https://github.com/ornladios/ADIOS2.git", branch: "master"
 
   livecheck do
@@ -32,6 +31,7 @@ class Adios2 < Formula
   depends_on "mpi4py"
   depends_on "numpy"
   depends_on "open-mpi"
+  depends_on "openssl@3"
   depends_on "pugixml"
   depends_on "python@3.14"
   depends_on "sqlite"
@@ -51,6 +51,12 @@ class Adios2 < Formula
   # clang: error: clang frontend command failed due to signal (use -v to see invocation)
   # Apple clang version 14.0.0 (clang-1400.0.29.202)
   fails_with :clang if DevelopmentTools.clang_build_version == 1400
+
+  # Upstream PR: https://github.com/ornladios/ADIOS2/pull/4791
+  patch do
+    url "https://github.com/ornladios/ADIOS2/commit/1dcffdf15a90282549ce679e96ac59f35e93acde.patch?full_index=1"
+    sha256 "1133316f038abed99824d00584b70454122083cf0e2717a1322511b91a14c4dd"
+  end
 
   def python3
     "python3.14"
