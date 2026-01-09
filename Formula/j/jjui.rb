db1f3955cc86cd33c85646821f13a4f98edc40b1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-09 05:22:30 +0000

    jjui: update 0.9.9 bottle.

--- Formula/j/jjui.rb
+++ Formula/j/jjui.rb
@@ -6,12 +6,12 @@ class Jjui < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7eab8f4970c0d8bdd698d26b37ad12453bba2fb1d688de2ed0a673760126d6a3"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7eab8f4970c0d8bdd698d26b37ad12453bba2fb1d688de2ed0a673760126d6a3"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7eab8f4970c0d8bdd698d26b37ad12453bba2fb1d688de2ed0a673760126d6a3"
-    sha256 cellar: :any_skip_relocation, sonoma:        "76223739ce82fad6fdae3fe7d587cba888bcf276275a82a78b4be6b8b084296f"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "494e9c76265547bb120f327698a70ed98f59c57bbd2837fb77c4ea94fe8894fc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "420af7e16efe6da7e4e6666947f1a6eb1c7e101a5bd7f7725cbc61ee484cea02"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e52f6426042f9681f38e705a7d1d2b9f97ccd87715e3e3a3055488223a346bfd"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e52f6426042f9681f38e705a7d1d2b9f97ccd87715e3e3a3055488223a346bfd"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e52f6426042f9681f38e705a7d1d2b9f97ccd87715e3e3a3055488223a346bfd"
+    sha256 cellar: :any_skip_relocation, sonoma:        "ec3bdf0550bf9385e439ddf5dbd81e79ddc7389cb967434c5e1b10e8b7b266bd"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "511a532ccd915c2a7ee4ee04348481421266107c9faaa4959ba1fe22d55ac659"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c65f766e6b41098349f3ac2cf0606e0e7e7a897601d8ab756692aa00cf507f7e"
   end
 
   depends_on "go" => :build
