5306a8f667c0be186e272ea14cde361aa62a7722
Michael Cho <michael@michaelcho.dev>
2026-01-10 16:05:01 -0500

    colmap: backport support for `openimageio`

--- Formula/c/colmap.rb
+++ Formula/c/colmap.rb
@@ -19,16 +19,16 @@ class Colmap < Formula
   depends_on "boost"
   depends_on "ceres-solver"
   depends_on "cgal"
-  depends_on "eigen"
+  depends_on "eigen" => :no_linkage
   depends_on "faiss"
   depends_on "flann"
-  depends_on "freeimage"
   depends_on "gflags"
   depends_on "glew"
   depends_on "glog"
   depends_on "gmp"
   depends_on "lz4"
   depends_on "metis"
+  depends_on "openimageio"
   depends_on "openssl@3"
   depends_on "poselib"
   depends_on "qtbase"
@@ -45,6 +45,25 @@ class Colmap < Formula
 
   on_linux do
     depends_on "mesa"
+  end
+
+  # Backport support for OpenimageIO
+  patch :DATA # https://github.com/colmap/colmap/commit/c9e6ba0e63f1eaf9b4de985228da27ee6ec4c1f1
+  patch do
+    url "https://github.com/colmap/colmap/commit/083f4dee70b23f25729ed6d6c6fe9abc340b205e.patch?full_index=1"
+    sha256 "226dfaaf179ad650ce535f6a3f6cbb9f93bfec2587bef5eb4d2d789a2c782e44"
+  end
+  patch do
+    url "https://github.com/colmap/colmap/commit/8e014c5bc70c7e01506f1d5ef7daaf25dc3190ae.patch?full_index=1"
+    sha256 "368e697f9cc863bdb706c99383aefa81d6be2ba373285a2a398fb69aa6bb7390"
+  end
+  patch do
+    url "https://github.com/colmap/colmap/commit/74eeb69c62998c32dd8981b301956aaacec71596.patch?full_index=1"
+    sha256 "e8a8ff862a38b80d2531f2300d1817fc7f132ee8c11b0f813b1d17f02309d25b"
+  end
+  patch do
+    url "https://github.com/colmap/colmap/commit/70358de550ab47b0b83c3e5812d42ec8e2d41b22.patch?full_index=1"
+    sha256 "ffc001e4d18dee24a47d262bc0e4e30906ee51ad5f8d38e81aed6284d75b9daf"
   end
 
   def install
@@ -64,3 +83,17 @@ class Colmap < Formula
     assert_path_exists (testpath / "db")
   end
 end
+
+__END__
+diff --git a/.github/workflows/build-ubuntu.yml b/.github/workflows/build-ubuntu.yml
+index f19bac5237..e793ea64f3 100644
+--- a/.github/workflows/build-ubuntu.yml
++++ b/.github/workflows/build-ubuntu.yml
+@@ -145,6 +145,7 @@ jobs:
+             libboost-system-dev \
+             libeigen3-dev \
+             libceres-dev \
++            libsuitesparse-dev \
+             libfreeimage-dev \
+             libmetis-dev \
+             libgoogle-glog-dev \
