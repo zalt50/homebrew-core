c29c746daa5d39de596d285e77253fcb16393e2a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-10 17:04:39 +0000

    hugo 0.154.4

--- Formula/h/hugo.rb
+++ Formula/h/hugo.rb
@@ -1,8 +1,8 @@
 class Hugo < Formula
   desc "Configurable static site generator"
   homepage "https://gohugo.io/"
-  url "https://github.com/gohugoio/hugo/archive/refs/tags/v0.154.3.tar.gz"
-  sha256 "fac9968089db63cb44d6d7c60d0e328aa8c52d2b0349773c13008da2c8f46c76"
+  url "https://github.com/gohugoio/hugo/archive/refs/tags/v0.154.4.tar.gz"
+  sha256 "2cca8539316312cc532bd8a548b3ddf25cf818ad3b07586e7883bf8dad04d824"
   license "Apache-2.0"
   head "https://github.com/gohugoio/hugo.git", branch: "master"
 
