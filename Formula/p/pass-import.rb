9f5b72aa91c5c46ec774535b857ac4ea1d3946e1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-10 08:25:00 +0000

    pass-import: bump python resources

--- Formula/p/pass-import.rb
+++ Formula/p/pass-import.rb
@@ -6,7 +6,7 @@ class PassImport < Formula
   url "https://files.pythonhosted.org/packages/f1/69/1d763287f49eb2d43f14280a1af9f6c2aa54a306071a4723a9723a6fb613/pass-import-3.5.tar.gz"
   sha256 "e3e5ec38f58511904a82214f8a80780729dfe84628d7c5d6b1cedee20ff3fb23"
   license "GPL-3.0-or-later"
-  revision 6
+  revision 7
   head "https://github.com/roddhjav/pass-import.git", branch: "master"
 
   bottle do
@@ -50,8 +50,8 @@ class PassImport < Formula
   end
 
   resource "urllib3" do
-    url "https://files.pythonhosted.org/packages/1c/43/554c2569b62f49350597348fc3ac70f786e3c32e7f19d266e19817812dd3/urllib3-2.6.0.tar.gz"
-    sha256 "cb9bcef5a4b345d5da5d145dc3e30834f58e8018828cbc724d30b4cb7d4d49f1"
+    url "https://files.pythonhosted.org/packages/c7/24/5f1b3bdffd70275f6661c76461e25f024d5a38a46f04aaca912426a2b1d3/urllib3-2.6.3.tar.gz"
+    sha256 "1b62b6884944a57dbe321509ab94fd4d3b307075e0c2eae991ac71ee15ad38ed"
   end
 
   resource "zxcvbn" do
