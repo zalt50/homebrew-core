a9724ec8b8a51239ed8bc00497a10aad6c22a7bc
Michael Cho <20700669+cho-m@users.noreply.github.com>
2026-01-10 20:16:56 +0000

    qtshadertools: update 6.10.1 bottle.

--- Formula/q/qtshadertools.rb
+++ Formula/q/qtshadertools.rb
@@ -20,12 +20,12 @@ class Qtshadertools < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "bb579540bf38f45c33f8608e40022c7000e682eb4186d84264f8f240770a4e85"
-    sha256 cellar: :any,                 arm64_sequoia: "205156158b29aa23357605ad9e2be6a4d784641dbf2dd42ac6d69107375ca84d"
-    sha256 cellar: :any,                 arm64_sonoma:  "1e54af54279cb0d96388f878e4f8ffb4f1b46a22ef0ae292b86a5631b951b377"
-    sha256 cellar: :any,                 sonoma:        "21f5464d71410117011e6035ab999264e902d90d28c91649d42b2aaded0f3830"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "64925118158a7861dd2d6291a7cee093ac87bbdc69db3857f8e87876e769e2a0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "de225d737f69ffacd8b1b130fa55dc1c6e844e5df18d876c73653821a947cc5a"
+    sha256 cellar: :any,                 arm64_tahoe:   "de43cc730602f76688166af617ace911d958e18503e51e5732ca3294405bf8df"
+    sha256 cellar: :any,                 arm64_sequoia: "2040f2504f3a13a6cfe0a1e49b8fcd4ab9fa1a68920828038c832652a447f262"
+    sha256 cellar: :any,                 arm64_sonoma:  "4dd4f1fdb9f90122eb002be7159da411587ce0ee6354d45c976bfd8132d9d1a7"
+    sha256 cellar: :any,                 sonoma:        "f07b392e02000d59c61c6a7b5553bde266a51938899af71e9e68a95dea2e4774"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "35491b1ce653001872522e4a9a09a077004fd9ff5e8363400dbee8b351563e67"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7b6235aaf6ca40f9de88529d6eff14a7b1c3cf014977d2b3cbc23d53333361e9"
   end
 
   depends_on "cmake" => :build
