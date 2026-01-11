ebb5fa199876b65bf24348ce05a0941394e52733
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-11 03:15:38 +0000

    universal-ctags p6.2.20260111.0

--- Formula/u/universal-ctags.rb
+++ Formula/u/universal-ctags.rb
@@ -1,9 +1,9 @@
 class UniversalCtags < Formula
   desc "Maintained ctags implementation"
   homepage "https://ctags.io/"
-  url "https://github.com/universal-ctags/ctags/archive/refs/tags/p6.2.20260104.0.tar.gz"
-  version "p6.2.20260104.0"
-  sha256 "f05b749c4b5c1354f4aae774077800b00fffa9744ca2a29d143270938a3dd6ed"
+  url "https://github.com/universal-ctags/ctags/archive/refs/tags/p6.2.20260111.0.tar.gz"
+  version "p6.2.20260111.0"
+  sha256 "e8600e18671729140a798efd433e717e4e01efc92b13eedb16ab3562bfc80e20"
   license "GPL-2.0-only"
   head "https://github.com/universal-ctags/ctags.git", branch: "master"
 
