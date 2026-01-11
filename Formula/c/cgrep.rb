50926b5f197649bb444f6d9db1b684c14459e11a
Michael Cho <michael@michaelcho.dev>
2026-01-10 20:58:36 -0500

    cgrep: remove `pcre` and workarounds

--- Formula/c/cgrep.rb
+++ Formula/c/cgrep.rb
@@ -1,7 +1,6 @@
 class Cgrep < Formula
   desc "Context-aware grep for source code"
   homepage "https://github.com/awgn/cgrep"
-  # TODO: Check if `rawfilepath` workaround can be removed
   url "https://github.com/awgn/cgrep/archive/refs/tags/v9.0.0.tar.gz"
   sha256 "6f7be7a24446289421fabe98393d00a46a1751ce1f605d84135e83d0ddf1d49e"
   license "GPL-2.0-or-later"
@@ -20,18 +19,10 @@ class Cgrep < Formula
   depends_on "ghc" => :build
   depends_on "pkgconf" => :build
   depends_on "gmp"
-  depends_on "pcre"
 
   uses_from_macos "libffi"
 
   conflicts_with "aerleon", because: "both install `cgrep` binaries"
-
-  resource "rawfilepath" do
-    on_macos do
-      url "https://hackage.haskell.org/package/rawfilepath-1.1.1/rawfilepath-1.1.1.tar.gz"
-      sha256 "43f879da83b7b07c30e76a3c31d5362b7b6bb8e235c2698872b92b9fcce3bf69"
-    end
-  end
 
   # Fix CPP directives alignment
   # https://github.com/awgn/cgrep/pull/50
@@ -41,22 +32,11 @@ class Cgrep < Formula
   end
 
   def install
-    # Work around "error: call to undeclared function 'execvpe'" by imitating part of removed
-    # hack in https://github.com/haskell/unix/commit/b8eb2486b15d564e73ef9307e175ac24a186acd2
-    # Issue ref: https://github.com/xtendo-org/rawfilepath/issues/13
-    if OS.mac?
-      (buildpath/"cabal.project.local").write "packages: . rawfilepath/"
-      (buildpath/"rawfilepath").install resource("rawfilepath")
-      inreplace "rawfilepath/cbits/runProcess.c", " execvpe(", " __hsunix_execvpe("
-    end
-    # Help resolver pick package versions compatible with newer GHC
-    constraints = ["--constraint=async>=2"]
-
     # `base <4.16.0.0` is not available in the most recent GHC
     inreplace "cgrep.cabal", "base ^>=4.15.0.0", "base >=4.15.0.0"
 
     system "cabal", "v2-update"
-    system "cabal", "v2-install", *constraints, *std_cabal_v2_args
+    system "cabal", "v2-install", *std_cabal_v2_args
   end
 
   test do
