9ced1dc1359a4043f8beb65fd10874d0d2013209
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-11 02:08:16 +0000

    any2fasta: add 0.8.1 bottle.

--- Formula/a/any2fasta.rb
+++ Formula/a/any2fasta.rb
@@ -5,6 +5,10 @@ class Any2fasta < Formula
   sha256 "73f4f2c1eb0cb5525703a5368fe4b34ddd252e64af1f9c3aba2ba63d8f9723b6"
   license "GPL-3.0-only"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "bba7ed684a5d3c4472a5c4aa1bac8c7aadc293bf6e6e198a35ca094bfd867d28"
+  end
+
   uses_from_macos "perl"
 
   def install
