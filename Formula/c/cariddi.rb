84cf4da15dde1dce42e8c4eb975a9f2f1864cf31
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-11 11:10:25 +0000

    cariddi: update 1.4.5 bottle.

--- Formula/c/cariddi.rb
+++ Formula/c/cariddi.rb
@@ -7,12 +7,12 @@ class Cariddi < Formula
   head "https://github.com/edoardottt/cariddi.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "afa584a42874e71fc08f8d6f1bcb89f3fd9ab21d9fa3b38ee3edb88b970dfe9b"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "afa584a42874e71fc08f8d6f1bcb89f3fd9ab21d9fa3b38ee3edb88b970dfe9b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "afa584a42874e71fc08f8d6f1bcb89f3fd9ab21d9fa3b38ee3edb88b970dfe9b"
-    sha256 cellar: :any_skip_relocation, sonoma:        "bc5d3f8a999464bd7e4870c0394773f099467b32f7b9a3d7a0310e80262a3159"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "49589921c6513b84795cdb576c8cd640155cb073eff8839146f323f161f9dae5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7bfa94ebf071a9a25557db20883e24dde2ff035cae2ae461ba2fb66d35b54145"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1567c8ea44905b59e5573ef3b985d8435de0ec04142a922bd9075b3471999e1b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1567c8ea44905b59e5573ef3b985d8435de0ec04142a922bd9075b3471999e1b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1567c8ea44905b59e5573ef3b985d8435de0ec04142a922bd9075b3471999e1b"
+    sha256 cellar: :any_skip_relocation, sonoma:        "8659f20d5a386c128bb0032b05fa80b90341d7c8abd4077382e0261d99181139"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "2dc1e4f046d0bf61b1a41fc46487e30861a77eb4a7e45c3db05845db1af7d002"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0864ddf6c8669dcd49a400213d6e2bf2d0a10dc4b5d4655fead6d91ae89165b3"
   end
 
   depends_on "go" => :build
