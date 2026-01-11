1949c941790552716a13d8e19a1c984cdda60654
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-11 15:00:49 +0000

    witr: update 0.2.3 bottle.

--- Formula/w/witr.rb
+++ Formula/w/witr.rb
@@ -6,12 +6,12 @@ class Witr < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "72ad2544d65131ae6f368c7534eca33bdf71e2e45983dc03340af0c26de29843"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bf8d536dd59460d44f7441d6bb80c1058b41fcf64410e8d297ad0dc63644ff3a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "65ca02033f50ed73c40e84c01ea7ef0b952edfd4fc2e18f7c12b4ef58566bb10"
-    sha256 cellar: :any_skip_relocation, sonoma:        "07d7d14f8e3d17c4f1cb63d98c1ae1c0cd51d4db6c5270b11d37dd7330781729"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a8fa947e9095a60966fac913dfff1757c87c3a4a7e96a50376f43d8ee6918892"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6b6d76c6841727376b62fa129076cde5c91f879bd7c9946fac12e514a473f453"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "895a89f06b6e973a43334826dd3ef64d1ef29d524f5f21b8ad75180e28ea84ef"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "72485153439189a01072a5e6bf6ef9ec94fe777fa7e49d99268044fcf7031b6b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ec09813e688affe70af706920173a4fb0b56f0bd59e04a0d02567dd2b0c6d5da"
+    sha256 cellar: :any_skip_relocation, sonoma:        "04852f34f673d6c65f6888124d4ae708d97b85ac5776172dfbae26c9cf9acbaa"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f49a60f6720df4b7aab5636ce1e99f1a705b794d2b8c0e989c0c144056cfbd12"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9db19963b76a05d83ef691ece1e299bfb42dc1655c213d8efd8fee55fcac6c5f"
   end
 
   depends_on "go" => :build
