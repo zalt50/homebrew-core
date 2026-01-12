bd0db09201c1bb7dd68e6251d8adc86ac1e8ff94
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-12 00:32:26 +0000

    hindent: add 6.2.1 bottle.

--- Formula/h/hindent.rb
+++ Formula/h/hindent.rb
@@ -6,6 +6,15 @@ class Hindent < Formula
   license "BSD-3-Clause"
   head "https://github.com/mihaimaruseac/hindent.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f18ed01446a282c5a45a1dc27587c1f23de3fd5bf79c2e16894f3bee21012a19"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2df0e64d7e7f95bb8db73c8fd210caef819e13b833a5e5916b0e9268c5648c4e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "af07d6596d2ed1c545188526c22d66ad15a49f25044e2581592312d81e6797f8"
+    sha256 cellar: :any_skip_relocation, sonoma:        "4d66526b97f7fb1f4e99de9024f51a080099281a5c5900590bc469f166a2a4bc"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "42766c8e3e09c699c48d2e27b5814e709975f6c1a16df0c576e72253ef18a7f2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4bf3e51687afa14afc64e1aec02272666e4bb95c197e34b5b6592eb220ecd079"
+  end
+
   depends_on "cabal-install" => :build
   # TODO: switch to ghc@9.12 in the next release
   # https://github.com/mihaimaruseac/hindent/pull/1000
