707629b835dffcbf2c8ac063f096cb02af249baf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-12 09:30:19 +0000

    subfinder: update 2.12.0 bottle.

--- Formula/s/subfinder.rb
+++ Formula/s/subfinder.rb
@@ -7,12 +7,12 @@ class Subfinder < Formula
   head "https://github.com/projectdiscovery/subfinder.git", branch: "dev"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "42eb254129769fb86c1d4ba979a5412896991b8549e2bb226f075472e80dd428"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5f371c5f20d8999d8865198d5d497629bc09a6a42b6d979a6e750deacc90c74d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "698030463ebced9450fdb709892579a9d6c3f5faf352a4f12f3e779f1793992a"
-    sha256 cellar: :any_skip_relocation, sonoma:        "fbe3ec41e7e4fc26b60c7d84fa4d091659f6d857a7f46337efdccb0457ac519f"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "275d36473a53974844e9728d6884caf6e71cd766933b16c46fc829dc944c4007"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "33d837503105a14399d22b78e7fc2b04e2cac9c5c265a4cade79b1cc45315d65"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "fd75bc115d5695fde8217069df5b06277bf654bdfa6061134904093d75453103"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "174541bd4f90a1b0f8cdec609fb88619b58297bdf3343b981775ec16c5a2b36c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "293178dd4c231a517d57288d6d2c4d6a8e64a466be4420bfcaa06f5832730c09"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a2428423e1dfa77f8be4f4b4b835416442684d8ba68e6779ca68348a8463fd7b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "6583f4e0998441dbfe01620af8da862aea08861275217d19f3b1221b9716a462"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ac5f8f8adbe419f2d8fc3d45edde0b91ee5e02dc70f7710f1c46c0ac0d8c5535"
   end
 
   depends_on "go" => :build
