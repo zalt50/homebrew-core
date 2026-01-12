750ef9306a7693dd05c8004c4e00476d2c9eaec4
Rui Chen <rui@chenrui.dev>
2026-01-12 16:42:54 -0500

    osm-pbf: revision bump (protobuf 33.4)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/o/osm-pbf.rb
+++ Formula/o/osm-pbf.rb
@@ -4,7 +4,7 @@ class OsmPbf < Formula
   url "https://github.com/openstreetmap/OSM-binary/archive/refs/tags/v1.6.1.tar.gz"
   sha256 "54e0f234ace310a4256dc7d4fc707837f532a509cc3ef2940dacbdc4ebd9ce15"
   license "LGPL-3.0-or-later"
-  revision 1
+  revision 2
 
   bottle do
     sha256 cellar: :any, arm64_tahoe:   "18d78acd3fa531508abf3f8a75b086a134a7688ce511482def2fa9ec17cf9a5f"
