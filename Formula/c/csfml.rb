28e1f659c1b2c339864ffe00e668a462fd322442
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-14 13:41:42 +0000

    csfml: update 3.0.0 bottle.

--- Formula/c/csfml.rb
+++ Formula/c/csfml.rb
@@ -7,14 +7,12 @@ class Csfml < Formula
   head "https://github.com/SFML/CSFML.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "8e33a6a3f34a298c1d64efc5460ed274423457ed95052252dfe1b803b79c26e5"
-    sha256 cellar: :any,                 arm64_sequoia: "e1e1172b80909989105f3813e7d1dba2ce0caca2a7efdd2717724e624b7e9cb0"
-    sha256 cellar: :any,                 arm64_sonoma:  "1a1763c3710588f2e1eff5569802b195f89582cdc9e5dd842cd818135fede023"
-    sha256 cellar: :any,                 arm64_ventura: "0f5543e8c4f83ba20d16b986870afdb4d127326e9b4545b2d98bab4430be4000"
-    sha256 cellar: :any,                 sonoma:        "c63caf00bb7ab923a8d49448b410dd7c269fc1a6e8f194326286d610b972f6af"
-    sha256 cellar: :any,                 ventura:       "d2d7e9b7eef45f2cd6fa0a48c3f0e606f9690945c8bc7ef396eeeb6640bd62e1"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8233ef7ba98f94386973b6594231a3fdae9706f0c769aa5ae4b23a2ce12a103f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d4c772a79cfa52cd66d37f85c6964473519693d3661f593ede05b4103ae09983"
+    sha256 cellar: :any,                 arm64_tahoe:   "079033bd42bdbbc6d32540f4cdd0d6d4edd734e2dd2b3b29221ca1302e8b88f6"
+    sha256 cellar: :any,                 arm64_sequoia: "5908757a15a2ce67c4c38c47fdcad0dbdd9ab3eb3b4b3f9cd82e94ad70028cfa"
+    sha256 cellar: :any,                 arm64_sonoma:  "8b6e300e881791159f17fc8f0408a1186098ff0c998c098f19ce04a8ced46fa6"
+    sha256 cellar: :any,                 sonoma:        "9edacf0a72a2a907a76ee25e8a05ef1a1ab9dcab14f31bbea0a93d319d882429"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "322b12666dc4b5c0f017f359146b95c6b8efa87a2c234ee9a22af3da326febba"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1e25efc33ea956ec8be19658b2b82b81cafcd1c971b493e25258b4f8bf4a42b2"
   end
 
   depends_on "cmake" => :build
