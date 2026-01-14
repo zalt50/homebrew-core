f9c73ddc51a481ca5ce7803079858b277e2f1f6d
Michael Cho <michael@michaelcho.dev>
2026-01-13 19:53:19 -0500

    nim: apply commits from open PR to port `pcre` to `pcre2`

--- Formula/n/nim.rb
+++ Formula/n/nim.rb
@@ -28,6 +28,38 @@ class Nim < Formula
 
   conflicts_with "atlas", "mongodb-atlas-cli", because: "both install `atlas` executable"
 
+  # Apply commits from open PR to replace `pcre` with `pcre2`
+  # PR ref: https://github.com/nim-lang/Nim/pull/24405
+  # Issue ref: https://github.com/nim-lang/Nim/issues/23668
+  patch do
+    url "https://github.com/nim-lang/Nim/commit/8c2ec2a7b010ef1a43b967205324ac83d11815d1.patch?full_index=1"
+    sha256 "f9171dba1817a83aada2960aab68b988fb6b3e766aa50b9527acc3daeafa6364"
+  end
+  patch do
+    url "https://github.com/nim-lang/Nim/commit/817af7edfcfca41e60e07b258c0943613783dd55.patch?full_index=1"
+    sha256 "120d313213c34bd3d48ae02baaa84dc5a0e80a88a6cae4de6a6164aefd6ff300"
+  end
+  patch do
+    url "https://github.com/nim-lang/Nim/commit/ce1761dff9e79d00bc012938ad6be37caa2edcfd.patch?full_index=1"
+    sha256 "06ecc37ab1c349a154cf05f1ca468ed0044e59e812d6401fc2a0f076717cbabc"
+  end
+  patch do
+    url "https://github.com/nim-lang/Nim/commit/cb802af44e3c684a8738684ebdd84df31aeabf09.patch?full_index=1"
+    sha256 "b9d5c030510018822c59714f26b933f822e462856f970ec918af6d4c6a9d285f"
+  end
+  patch do
+    url "https://github.com/nim-lang/Nim/commit/27fc4fedb5c1be6a4ec27f7d0d0c913a63f792b4.patch?full_index=1"
+    sha256 "f012298fe2ef8201fc303f8a7e91dcb10662f3382693ec899e0a505dd90872cc"
+  end
+  patch do
+    url "https://github.com/nim-lang/Nim/commit/0e3ac706156887ce143681da42b21874c2b20774.patch?full_index=1"
+    sha256 "625c837b002bfd492b60cecce812ecdd2d42bd4b3117526f6d3004661949ce90"
+  end
+  patch do
+    url "https://github.com/nim-lang/Nim/commit/07de39cde6341ae278b47d64f73dd9c823dd18c5.patch?full_index=1"
+    sha256 "33b5787281af6bcd4c30354de8ad49457a3360f3acfb000230b162aad114fe4c"
+  end
+
   def install
     if build.head?
       # this will clone https://github.com/nim-lang/csources_v1
@@ -65,18 +97,18 @@ class Nim < Formula
   end
 
   test do
-    (testpath/"hello.nim").write <<~EOS
+    (testpath/"hello.nim").write <<~NIM
       echo("hello")
-    EOS
+    NIM
     assert_equal "hello", shell_output("#{bin}/nim compile --verbosity:0 --run #{testpath}/hello.nim").chomp
 
-    (testpath/"hello.nimble").write <<~EOS
+    (testpath/"hello.nimble").write <<~NIM
       version = "0.1.0"
       author = "Author Name"
       description = "A test nimble package"
       license = "MIT"
       requires "nim >= 0.15.0"
-    EOS
+    NIM
     assert_equal "name: \"hello\"\n", shell_output("#{bin}/nimble dump").lines.first
   end
 end
