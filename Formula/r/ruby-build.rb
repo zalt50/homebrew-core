c570b846855447599da9a0fb908acd004ae62812
Mislav Marohnić <hi@mislav.net>
2026-01-14 03:05:45 +0100

    ruby-build 20260114
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/r/ruby-build.rb
+++ Formula/r/ruby-build.rb
@@ -1,8 +1,8 @@
 class RubyBuild < Formula
   desc "Install various Ruby versions and implementations"
   homepage "https://rbenv.org/man/ruby-build.1"
-  url "https://github.com/rbenv/ruby-build/archive/refs/tags/v20260113.tar.gz"
-  sha256 "12596ed62e6c80074de1a3902665515e8c20aef430910930210595bbfd530132"
+  url "https://github.com/rbenv/ruby-build/archive/refs/tags/v20260114.tar.gz"
+  sha256 "63476be6827d3c88c8e8f309707ba0775011a41fa70ab875d0c6fa5c47f77113"
   license "MIT"
   head "https://github.com/rbenv/ruby-build.git", branch: "master"
 
