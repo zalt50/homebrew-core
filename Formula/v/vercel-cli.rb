34443fe7cc6485ac79d0d6e863a36628c91a4b17
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-15 20:26:31 +0000

    vercel-cli 50.4.3

--- Formula/v/vercel-cli.rb
+++ Formula/v/vercel-cli.rb
@@ -1,8 +1,8 @@
 class VercelCli < Formula
   desc "Command-line interface for Vercel"
   homepage "https://vercel.com/home"
-  url "https://registry.npmjs.org/vercel/-/vercel-50.4.2.tgz"
-  sha256 "81a3df93d97a36d5154f2ed0d0799827515803db7eda8e89dc479607c8ff90a9"
+  url "https://registry.npmjs.org/vercel/-/vercel-50.4.3.tgz"
+  sha256 "e6a1b07aa0bf7d766d757a9d330375e9a981cef562996415826b6698d201617c"
   license "Apache-2.0"
 
   bottle do
