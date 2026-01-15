5428b6e6f26d508bb1242b25cfba10f56bfa7649
Bevan Kay <email@bevankay.me>
2026-01-15 11:28:39 +1100

    shiki 3.21.0 (new formula)

---
+++ Formula/s/shiki.rb
@@ -0,0 +1,24 @@
+class Shiki < Formula
+  desc "Beautiful yet powerful syntax highlighter"
+  homepage "https://shiki.style/"
+  url "https://registry.npmjs.org/@shikijs/cli/-/cli-3.21.0.tgz"
+  sha256 "a0550a23b3501889ed103761331b7d6da636abb062aff63e30bbf770ba56b77c"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/shiki --version")
+
+    (testpath/"test.txt").write <<~TXT
+      Hello, world!
+    TXT
+
+    assert_match "Hello, world!", shell_output("#{bin}/shiki #{testpath}/test.txt")
+  end
+end
