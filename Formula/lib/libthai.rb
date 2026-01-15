0a59ba7120e3c4de2f32c1a7a795fa467f744cb0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-15 16:49:56 +0000

    libthai: add 0.1.30 bottle.

--- Formula/lib/libthai.rb
+++ Formula/lib/libthai.rb
@@ -5,6 +5,15 @@ class Libthai < Formula
   sha256 "ddba8b53dfe584c3253766030218a88825488a51a7deef041d096e715af64bdd"
   license "LGPL-2.1-or-later"
 
+  bottle do
+    sha256 arm64_tahoe:   "683257ceaae0ebf522c07e2fd39b82863ff54d35ac8a985d9d62597bda8a05b3"
+    sha256 arm64_sequoia: "428726b804e7bb80faef7381ba27b0be1a0087e54746d0f355c0af7575220ac6"
+    sha256 arm64_sonoma:  "e969faf14de2c64bcd89e1e1abea14d12d8695eba54647782e2e841042b3209d"
+    sha256 sonoma:        "151b8c0772dce394bc4c1112d90b45999d18be1b0406abf322be74f5976cf63a"
+    sha256 arm64_linux:   "424e3a5b178a1eaf5cd0c9d56bfd3671c316cd88096e9297d407836c826d5296"
+    sha256 x86_64_linux:  "54551ab441971407573f2f97d085cd26ec3b61eda8fbf6d599ce2a41bfdb5f02"
+  end
+
   depends_on "pkgconf" => [:build, :test]
   depends_on "libdatrie"
 
