0fc2e31d9c310acff143216f4eb170f71938d89d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-16 17:02:30 +0000

    docker 29.1.5

--- Formula/d/docker.rb
+++ Formula/d/docker.rb
@@ -2,7 +2,7 @@ class Docker < Formula
   desc "Pack, ship and run any application as a lightweight container"
   homepage "https://www.docker.com/"
   url "https://github.com/docker/cli.git",
-      tag:      "v29.1.4",
+      tag:      "v29.1.5",
       revision: "0e6fee6c52f761dc79dc4bf712ea9fe4095c9bd2"
   license "Apache-2.0"
   head "https://github.com/docker/cli.git", branch: "master"
