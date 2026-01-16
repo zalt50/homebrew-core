4474b38870b16a06329bc33187b74d58e8a92126
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-16 11:40:09 +0000

    gitlogue: update 0.7.0 bottle.

--- Formula/g/gitlogue.rb
+++ Formula/g/gitlogue.rb
@@ -7,12 +7,12 @@ class Gitlogue < Formula
   head "https://github.com/unhappychoice/gitlogue.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "db4458dd42e5d0db1ed75f20117cb38ef5f8b54d6d49d32c663776eb0effd738"
-    sha256 cellar: :any,                 arm64_sequoia: "6393915dc453bd36e87a6cac6fee4c66253c85fd15581781a6e436b8e811f2d9"
-    sha256 cellar: :any,                 arm64_sonoma:  "e6a3ce8cc7c3ceebd7b17163d2b0494848910dabec70fe9e39aad44935e6522e"
-    sha256 cellar: :any,                 sonoma:        "6c1cff1dad195fd9d67e0313a7c24f6716fd733f3f2144f1353c2e5226ec308c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d408b8414b7c1175320b7af637a675cbc538f38747b717ad37121644458d8dba"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "927248a05f0707d6020de2dbf3a03107159a250d5b69be176a550c2d5f1fc5e8"
+    sha256 cellar: :any,                 arm64_tahoe:   "a8cdc8b35552f37e26cf041e08129933a9e589d27331ddd50f613cc80306defc"
+    sha256 cellar: :any,                 arm64_sequoia: "2e42f9ab6c6daee4635c25823921551be7ff2d529b5c89c36fff90f437c553e1"
+    sha256 cellar: :any,                 arm64_sonoma:  "ddf4f2a76b95914842c745837a128a3bce7394522e6ee762516b46ee7d2aab07"
+    sha256 cellar: :any,                 sonoma:        "b25ccf8d7fadfe0fb243acc527850a1e256885d2e3ea9ff4bfd0c88a85a03d15"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "0bf5f64d9f0fa52a8d5aefcccbff9fb0110cf9f7f3b7485e7827443241b9a368"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "967ae9fb8ee80a5f1014b059e03d2a0acdfea197cfcf344a66a6a12d152e9f74"
   end
 
   depends_on "pkgconf" => :build
