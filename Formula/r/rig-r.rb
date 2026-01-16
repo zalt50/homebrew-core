46495e805165171aa7ef1f4be2caa531c562d4f2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-16 13:54:16 +0000

    rig-r: add 0.7.1 bottle.

--- Formula/r/rig-r.rb
+++ Formula/r/rig-r.rb
@@ -5,6 +5,15 @@ class RigR < Formula
   sha256 "23f30bff14026141c82000b5e085f05410d30dace04ed383a6445981cebb3989"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4a1036df99991fe7a75a97f9597eacdb97831ec77154fec34c5d161b1db1f70c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7e4ba5e6c0c9355b7e9662fb85c6c0ce9bd2048fdca32ccee2c42205cec68202"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e47bc29145a28f48c26f72eb8769e15018a0426375255f4eb027b3cf27179f44"
+    sha256 cellar: :any_skip_relocation, sonoma:        "0b852f137a8505a9b1336e1a6b140fa43e52dde797024c00bdbc8a448225eccf"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "0da71bb0532251d9eb59a4ee896da94f778fd1ebf3d20c001364dfed0120ee81"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1e1618090f52d92e8cbfcc3524933798ef54db747bcb956fa6c5df3f9a5e74d6"
+  end
+
   depends_on "rust" => :build
 
   def install
