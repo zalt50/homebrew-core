30c36bb785892e703129686df309c1cb71565e4f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-16 16:57:11 +0000

    bitrise 2.36.3

--- Formula/b/bitrise.rb
+++ Formula/b/bitrise.rb
@@ -1,8 +1,8 @@
 class Bitrise < Formula
   desc "Command-line automation tool"
   homepage "https://github.com/bitrise-io/bitrise"
-  url "https://github.com/bitrise-io/bitrise/archive/refs/tags/v2.36.2.tar.gz"
-  sha256 "d30022e3136533b7cc7933dc252ed3967085b3d3924c16c41d95947b062dc046"
+  url "https://github.com/bitrise-io/bitrise/archive/refs/tags/v2.36.3.tar.gz"
+  sha256 "65351f86cf713cafa4a3290df65f8551d56c42ea3ead090a0b2bea1ee8f488ac"
   license "MIT"
   head "https://github.com/bitrise-io/bitrise.git", branch: "master"
 
