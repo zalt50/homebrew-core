3aca2ec1ae7548bfcdf02e54821c90facef75a01
Anton Melnikov <antonsm21@gmail.com>
2026-01-16 05:19:28 +0000

    securefs: update 2.0.0_5 bottle.

--- Formula/s/securefs.rb
+++ Formula/s/securefs.rb
@@ -13,8 +13,8 @@ class Securefs < Formula
   end
 
   bottle do
-    sha256 arm64_linux:  "7c57aaeec247543bfd572249eab3a320b49392e118f065cfcfb2d77b48c1d17d"
-    sha256 x86_64_linux: "482a50fec11c48fbcce7347101b1716172cb3cb2aefed7eb5704afa34f713f92"
+    sha256 arm64_linux:  "7e087fde5a7371287f70bba1e263ede84ef5b934794f30f644e81be044741648"
+    sha256 x86_64_linux: "f91050ed851418fffbac2f809c473e6a70ec61307f7614a97c3da981e118f0c7"
   end
 
   depends_on "cmake" => :build
