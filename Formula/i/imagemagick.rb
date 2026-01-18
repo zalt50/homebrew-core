f2400f22c2bc66f0e1ceeb7b737ad07457f7cf24
Mike McQuaid <mike@mikemcquaid.com>
2026-01-18 12:36:09 +0100

    imagemagick: move some formats to imagemagick-full.

--- Formula/i/imagemagick.rb
+++ Formula/i/imagemagick.rb
@@ -1,10 +1,10 @@
 class Imagemagick < Formula
-  desc "Tools and libraries to manipulate images in many formats"
+  desc "Tools and libraries to manipulate images in select formats"
   homepage "https://imagemagick.org/index.php"
   url "https://imagemagick.org/archive/releases/ImageMagick-7.1.2-12.tar.xz"
   sha256 "e22c5dc6cd3f8e708a2809483fd10f8e37438ef7831ec8d3a07951ccd70eceba"
   license "ImageMagick"
-  revision 1
+  revision 2
   head "https://github.com/ImageMagick/ImageMagick.git", branch: "main"
 
   livecheck do
@@ -22,23 +22,19 @@ class Imagemagick < Formula
   end
 
   depends_on "pkgconf" => :build
-  depends_on "cairo"
-  depends_on "fontconfig"
-  depends_on "freetype"
+
+  # Only add dependencies required for dependents in homebrew-core,
+  # recursive dependencies or INCREDIBLY widely used and light formats in the
+  # current year (2026).
+  # Add other dependencies to imagemagick-full formula or consider making
+  # formulae dependent on imagemagick-full.
+  depends_on "glib"
   depends_on "jpeg-turbo"
-  depends_on "jpeg-xl"
   depends_on "libheif"
-  depends_on "liblqr"
   depends_on "libpng"
-  depends_on "libraw"
-  depends_on "librsvg"
   depends_on "libtiff"
   depends_on "libtool"
-  depends_on "libultrahdr"
-  depends_on "libzip"
   depends_on "little-cms2"
-  depends_on "openexr"
-  depends_on "openjpeg"
   depends_on "webp"
   depends_on "xz"
 
@@ -47,26 +43,11 @@ class Imagemagick < Formula
   uses_from_macos "zlib"
 
   on_macos do
-    depends_on "gdk-pixbuf"
     depends_on "gettext"
-    depends_on "glib"
     depends_on "imath"
-    depends_on "libomp"
-  end
-
-  on_linux do
-    depends_on "glib"
-    depends_on "libx11"
-    depends_on "libxext"
   end
 
   skip_clean :la
-
-  # Patch to fix build with LibRaw 0.22+
-  patch do
-    url "https://gitlab.archlinux.org/archlinux/packaging/packages/imagemagick/-/raw/ca9b35f767e1c4a166847fbfe17c2d715aa80582/libraw-0.22.patch"
-    sha256 "baed7cbfb378734d32d277b6e13882ac541932ef67e6aa8867b185ffef12f986"
-  end
 
   def install
     # Avoid references to shim
@@ -80,33 +61,23 @@ class Imagemagick < Formula
       "--disable-opencl",
       "--enable-shared",
       "--enable-static",
-      "--with-freetype=yes",
-      "--with-rsvg=yes",
       "--with-gvc=no",
       "--with-modules",
-      "--with-openjp2",
-      "--with-openexr",
       "--with-webp=yes",
       "--with-heic=yes",
-      "--with-raw=yes",
-      "--with-uhdr=yes",
-      "--with-zip=yes",
+      "--with-raw=no",
       "--without-gslib",
-      "--with-gs-font-dir=#{HOMEBREW_PREFIX}/share/ghostscript/fonts",
       "--with-lqr",
       "--without-djvu",
       "--without-fftw",
       "--without-pango",
       "--without-wmf",
-      "--enable-openmp",
+      "--without-jxl",
+      "--without-openexr",
     ]
     if OS.mac?
       args += [
         "--without-x",
-        # Work around "checking for clang option to support OpenMP... unsupported"
-        "ac_cv_prog_c_openmp=-Xpreprocessor -fopenmp",
-        "ac_cv_prog_cxx_openmp=-Xpreprocessor -fopenmp",
-        "LDFLAGS=-lomp -lz",
       ]
     end
 
@@ -116,8 +87,7 @@ class Imagemagick < Formula
 
   def caveats
     <<~EOS
-      Ghostscript is not installed by default as a dependency.
-      If you need PS or PDF support, ImageMagick will still use the ghostscript formula if installed directly.
+      imagemagick-full includes additional tools and libraries that are not included in the regular imagemagick formula.
     EOS
   end
 
@@ -126,14 +96,8 @@ class Imagemagick < Formula
 
     # Check support for recommended features and delegates.
     features = shell_output("#{bin}/magick -version")
-    %w[Modules freetype heic jpeg png raw rsvg tiff].each do |feature|
+    %w[Modules heic jpeg png tiff].each do |feature|
       assert_match feature, features
-    end
-
-    # Check support for a few specific image formats, mostly to ensure LibRaw linked correctly.
-    formats = shell_output("#{bin}/magick -list format")
-    ["AVIF  HEIC      rw+", "ARW  DNG       r--", "DNG  DNG       r--"].each do |format|
-      assert_match format, formats
     end
   end
 end
