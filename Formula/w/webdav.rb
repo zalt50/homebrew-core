39cf315a5a11a9de022c63b28605fdb135565a6b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-18 09:45:31 +0000

    webdav: update 5.10.3 bottle.

--- Formula/w/webdav.rb
+++ Formula/w/webdav.rb
@@ -7,13 +7,12 @@ class Webdav < Formula
   head "https://github.com/hacdias/webdav.git", branch: "main"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ae1bd9347cac2636204fea1d640dae22494a7286d1803a9e471e563de6335462"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ae1bd9347cac2636204fea1d640dae22494a7286d1803a9e471e563de6335462"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ae1bd9347cac2636204fea1d640dae22494a7286d1803a9e471e563de6335462"
-    sha256 cellar: :any_skip_relocation, sonoma:        "878b1ef609d7a81c2f17e07bb286562abbb3e7c9f41a4e7bf66b690c1c6b6e93"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "1386ad2a86ab7e82433f14009f78bd1e0789b7a5ba81c4a38d0dddb2acca71c7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "aa211edf9a4f41a9d41549b1dc81fe61eee588f2bded6ed82b12a066985e4ad4"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "dfdf7f9167caeceffdab045bfcb15c6bff72fbbf47f36f1e725b918b6eb4fd95"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "dfdf7f9167caeceffdab045bfcb15c6bff72fbbf47f36f1e725b918b6eb4fd95"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "dfdf7f9167caeceffdab045bfcb15c6bff72fbbf47f36f1e725b918b6eb4fd95"
+    sha256 cellar: :any_skip_relocation, sonoma:        "b842733f6cdef3e1c23e5a920b67291d793898f94ee8e3d9a93f4f7e4bbefd53"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ef6f3361d40ec0e1dcf1068d5aa557dd150f3acf7a51389b24cee37a0a98a20f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "483b40512680c4a5bbccf4eb475065c20d4459dcfdba29dcb13f21b01baf7ded"
   end
 
   depends_on "go" => :build
