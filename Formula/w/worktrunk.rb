017c337299593b547124216ae480fa5b9beb77d2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-18 11:34:15 +0000

    worktrunk: add 0.15.2 bottle.

--- Formula/w/worktrunk.rb
+++ Formula/w/worktrunk.rb
@@ -6,6 +6,15 @@ class Worktrunk < Formula
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/max-sixty/worktrunk.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "181b490924e84ba09be244d7ce94de1a49d1671bff5a70a6b0185ef0c18a8261"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f56173043415bdc18a35e1dbb7511f6c1b1da95a77dd8b7963a2f43f6a09bddf"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "75c4922b866ba9b992a7fa380d80ffc123edd4e1f8ff567cca91f2a27c06db0f"
+    sha256 cellar: :any_skip_relocation, sonoma:        "971339e082df172a8e560ae711ca12d74852672cc00add5d15de76fcedfde5b5"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "79299653ea1ac2db3261d061521e715fec92ae67c87a2088612ad86580dbc870"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "eab35f18b346d493c8b932a5e903119bf0f6ce1519c9a0b9463b3c1bbe03f5a0"
+  end
+
   depends_on "rust" => :build
 
   def install
