cf65131a7462445b310834691ee26ed7a03ccd00
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-18 20:45:43 +0000

    imagemagick-full: add 7.1.2-12_2 bottle.

--- Formula/i/imagemagick-full.rb
+++ Formula/i/imagemagick-full.rb
@@ -12,6 +12,15 @@ class ImagemagickFull < Formula
     regex(/href=.*?ImageMagick[._-]v?(\d+(?:\.\d+)+-\d+)\.t/i)
   end
 
+  bottle do
+    sha256 arm64_tahoe:   "4e4faf644af0d4b684c83f8775dd95eb713ab85c81940c394c4c8acf3fe6510f"
+    sha256 arm64_sequoia: "6761b474f366449e080554f241f2d5aeb966cff5e95a38af94d9858710bb4885"
+    sha256 arm64_sonoma:  "af102b8cb6925b4791de85cb5e0078e45ecd1cbb510e97cf37af1fa88d49a061"
+    sha256 sonoma:        "90d1e6abdd1d5b03e783fcc36fd42fbf614fd42cbb35fe2a742e1171164f2991"
+    sha256 arm64_linux:   "0c40dbe723e02769624a8068638b98e39868c77e6542c01fc90b1811351b27df"
+    sha256 x86_64_linux:  "8bd7821eff6ebb2325cbad886ae6992728539c42de2e4710e29ae49c4a50625e"
+  end
+
   keg_only :versioned_formula
 
   depends_on "pkgconf" => :build
