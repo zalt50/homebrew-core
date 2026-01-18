5e9ded2a63fdde7f0eb94405ae2ec3f964418c8e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-18 10:14:02 +0000

    fakeroot: update 1.37.2 bottle.

--- Formula/f/fakeroot.rb
+++ Formula/f/fakeroot.rb
@@ -11,14 +11,12 @@ class Fakeroot < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "564fd8232d47a45c63da8ef9f7dae69e703529215d4a7c486c5c1e21a7065483"
-    sha256 cellar: :any,                 arm64_sequoia: "ab29a59c3530a716bfa3151bed5458ece1bb6b136dd2a316f54f88ff031e1f52"
-    sha256 cellar: :any,                 arm64_sonoma:  "1aadc43f4ce3f17a1a609750a139f412205a71b1abc068b06874e83e40fcd7f3"
-    sha256 cellar: :any,                 arm64_ventura: "1c11f5d0a36d1d27fb51c32f3989802f4c385796b56959da1472c3a97a42c687"
-    sha256 cellar: :any,                 sonoma:        "2c240e8fb51287a6e2304c2aca7dc0e0c0bf7103f72b3f36872abaa23d62c0d0"
-    sha256 cellar: :any,                 ventura:       "f67ebe96d1e957f934e9de9cb936671f7fef7780e6e12011828265ec2547ccb1"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "7f239b1a7a44fed02417a5230512a0a1f06d3c84bd27aff90db9c9a1a5704dc9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "41acec3bc608043a4a49fcca0008b90bbc45abc655d9b1c13b74577df13297a2"
+    sha256 cellar: :any,                 arm64_tahoe:   "eb496ad0f12203de3fb0ba287620f65883f1e90cf2f67dc53021a1dfdd448430"
+    sha256 cellar: :any,                 arm64_sequoia: "bff583cc0931e9a01bf287230b319bd68d8f8083881ca9fadeba02f2b61b45ee"
+    sha256 cellar: :any,                 arm64_sonoma:  "b463fc4605df2dd647baea44571170e058a9662f0df507ca6e2ed1a0b6e68593"
+    sha256 cellar: :any,                 sonoma:        "38c3bbe68895d203e84372ab3f1f5658fa160d8edc241e3ab01d19b37cd773ea"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "63d8636c69b2bad7bda249e7273d95079ea10ea6ccfadc654ba9282d114a9f84"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c24ea0f7754e65c3243a293aff65150a855485b760f42e7a230de0c4bf508681"
   end
 
   on_linux do
