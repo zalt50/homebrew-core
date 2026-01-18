2a6f82216f14acc78c3f0cf422216ed5f70cc30a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-18 17:31:44 +0000

    nak 0.17.9

--- Formula/n/nak.rb
+++ Formula/n/nak.rb
@@ -1,8 +1,8 @@
 class Nak < Formula
   desc "CLI for doing all things nostr"
   homepage "https://github.com/fiatjaf/nak"
-  url "https://github.com/fiatjaf/nak/archive/refs/tags/v0.17.4.tar.gz"
-  sha256 "b8fbd360984d9903a636cc7a0daa5ac123b2a31ead3424137fdcc31e2a36e71c"
+  url "https://github.com/fiatjaf/nak/archive/refs/tags/v0.17.9.tar.gz"
+  sha256 "09b1dbdf40483c8ac79410ede204c1c7f3d41c952c2b90230b4f6eca7a3ca239"
   license "Unlicense"
   head "https://github.com/fiatjaf/nak.git", branch: "master"
 
