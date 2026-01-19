10a52c996c90ce7451b4202b6bf68b472d4f8fb5
Branch Vincent <branchevincent@gmail.com>
2026-01-18 18:48:06 -0800

    forego: add go.mod, remove `no_autobump!`

--- Formula/f/forego.rb
+++ Formula/f/forego.rb
@@ -1,12 +1,19 @@
 class Forego < Formula
   desc "Foreman in Go for Procfile-based application management"
   homepage "https://github.com/ddollar/forego"
-  url "https://github.com/ddollar/forego/archive/refs/tags/20180216151118.tar.gz"
-  sha256 "23119550cc0e45191495823aebe28b42291db6de89932442326340042359b43d"
   license "Apache-2.0"
   head "https://github.com/ddollar/forego.git", branch: "master"
 
-  no_autobump! because: :requires_manual_review
+  stable do
+    url "https://github.com/ddollar/forego/archive/refs/tags/20180216151118.tar.gz"
+    sha256 "23119550cc0e45191495823aebe28b42291db6de89932442326340042359b43d"
+
+    # Add go.mod
+    patch do
+      url "https://github.com/ddollar/forego/commit/89fb456a167f59ace41e0e9294f4b7c01f76943e.patch?full_index=1"
+      sha256 "98274160eb0251af323df030f8f05369ab19b0116a2b87e58372974bae5c0524"
+    end
+  end
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_tahoe:    "c0ecd5f1aa248b2ebc4611679e8f8a34ca3330db2cc069bdab6a3ead41f0328d"
@@ -27,17 +34,12 @@ class Forego < Formula
   depends_on "go" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
-    ENV["GO111MODULE"] = "off"
-    (buildpath/"src/github.com/ddollar/forego").install buildpath.children
-    cd "src/github.com/ddollar/forego" do
-      ldflags = "-X main.Version=#{version} -X main.allowUpdate=false"
-      system "go", "build", *std_go_args(ldflags:)
-    end
+    ldflags = "-s -w -X main.Version=#{version} -X main.allowUpdate=false"
+    system "go", "build", *std_go_args(ldflags:)
   end
 
   test do
-    (testpath/"Procfile").write "web: echo \"it works!\""
+    (testpath/"Procfile").write "web: echo 'it works!'"
     assert_match "it works", shell_output("#{bin}/forego start")
   end
 end
