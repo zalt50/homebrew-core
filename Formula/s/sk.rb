4a2c8c9a8bc2f5c11b325ec5170e07b44c59decf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-19 12:30:15 +0000

    sk: update 1.0.1 bottle.

--- Formula/s/sk.rb
+++ Formula/s/sk.rb
@@ -12,14 +12,12 @@ class Sk < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8673c1f23e20a9046e5341e7f9bb2029966aad393adea9a92c839332e99a3807"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0e64e2ab2050344a8e178d635f300acb272a27b414efcce6dc06296b6414c84c"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ef46fab492395a38197dd582bb66e594b968ad7368598f96ba72568ed08b36f0"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "d93770fb5e79c73492e01038943ea22e440987d309aefe40f0b0379c68266843"
-    sha256 cellar: :any_skip_relocation, sonoma:        "385dc10fb64e302ccbb054ae5fed0d15dbebe8f203ac215fb865978d43d2d1f7"
-    sha256 cellar: :any_skip_relocation, ventura:       "d0eec0b3b53c89451193f3e81d44a70a2a1cd1a5895afd7c2e7523c0a5a74ca0"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "c2faf26cdaba0186091d6454bfb9904e5acb4a54c1c084fa3b9ba5293eb615aa"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2631947f06f94f9f1f0bce68d0684da6770f379ebe15d6a323b0fbcda8a492d2"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "fd5a27ae6d57b1d85976c2e2f0c48c0437b12a2df2616b6df60fdfdf11d5f7ee"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7edf37da605e4ec8b09263e043ee363392c25b451eceac9207b65dad309b6323"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "382d30e43c47be1e11b95b937c60dbfd2aecf78ce802a9f0f4e19c997de96e51"
+    sha256 cellar: :any_skip_relocation, sonoma:        "704ece0b133bffae268a13b25e379e01a4c1434cc433de766339d1b645933178"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ac4eb86af5e97ceee452e8a2699b105e9e8f19c0f18a89beac1512cd3a7b9d61"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "80fc9c82bc523f149e9ff77720caab906bd69080249a0eb21d807163c2921a11"
   end
 
   depends_on "rust" => :build
