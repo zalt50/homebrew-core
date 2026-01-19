3d8b2adb45a091f96ab2ad6f4fd3f9c554c24a81
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-19 14:53:23 +0000

    fresh-editor: update 0.1.81 bottle.

--- Formula/f/fresh-editor.rb
+++ Formula/f/fresh-editor.rb
@@ -6,12 +6,12 @@ class FreshEditor < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "499fc1b13b0149ec35463825ef3b137e552fbb87fc9d8b7113a253c7483480b1"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2d7ee50f0ec455a728624aba00c0154649c34e4baff11ab1df2326a88a09f8c0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5c7ae7d445c81aa36f24f9d615a60a8aab6defc59411df3db69113b92255497e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "d860fbf6aefc8b1e01f31a64f5a8128ae53eb4bc9e583954a95f1418125d92fe"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a81d04bf327104f3a7023ff20e801b2ffffbd50c33bceb06a2409a19773b2a52"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b15c337370482874ca65931da3e6e660058b57ed75db0a23d1b03632575dc8b2"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6acbfd9bbc7b918d2f17c417c721e378b6cfd2adf299129b84dddae82db24be7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "39a0b69d2a102201a5c1b16f9c3d8d95bf3ebfa00f9ea10070db5913effaaa9b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7b1acebd6168f01974b47b999241a73705e1f3046f8a24b68f92882f4f598a70"
+    sha256 cellar: :any_skip_relocation, sonoma:        "4ae0301310c6779215510838ee0330f2a03573ddf0bf54a18c8b9ca1bd955ba6"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "bbc75966616c4d967b6c0f38369fbdb1135ebc64c64232519b48d83c9d7d0d38"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "06df0dde844aec256a1d72be426bdd56492db94d3fdb164fa3f0bcf6c877191b"
   end
 
   depends_on "rust" => :build
