a35570d5620b22affbbfe9494eafd73062178579
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-19 06:17:59 +0000

    signalwire-client-c: add 2.0.3 bottle.

--- Formula/s/signalwire-client-c.rb
+++ Formula/s/signalwire-client-c.rb
@@ -5,6 +5,15 @@ class SignalwireClientC < Formula
   sha256 "4ce196a4bb886854dfcb9018c05b466484a19f71a50c3d5a990a88429e74163a"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "2aceb796bbefcfcee258125049f9e3921597fe650daed375bdbcdf83e6e90c61"
+    sha256 cellar: :any,                 arm64_sequoia: "27076754d1e4589727339146b931cc3f43d05dd99645e22239819d23f7db67b1"
+    sha256 cellar: :any,                 arm64_sonoma:  "725482ec8c26018912db30839afd8681b9be1bae23b7818e0a8250eba8f3edd6"
+    sha256 cellar: :any,                 sonoma:        "ee751cfa7b26fc1225f4c13211851bce4f92219c2ec70521b713c6f4788dc812"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "804d9d853a4686dbb9066b8e64374c365c959414adf49158dbfdd33a71e26211"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b054215f2df6b19d9767c779b338f0f122cd4833c048a3924729d03e8e426ecd"
+  end
+
   depends_on "cmake" => :build
   depends_on "pkgconf" => [:build, :test]
   depends_on "libks"
