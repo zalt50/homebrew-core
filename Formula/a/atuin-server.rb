963e7a720e99ca993ff98cfc2e7c189f0b716992
Ted Pearson <1477390+tedpearson@users.noreply.github.com>
2026-02-11 00:27:59 -0500

    atuin-server 18.12.0 (new formula)

---
+++ Formula/a/atuin-server.rb
@@ -0,0 +1,35 @@
+class AtuinServer < Formula
+  desc "Sync server for atuin - Improved shell history for zsh, bash, fish and nushell"
+  homepage "https://atuin.sh"
+  url "https://github.com/atuinsh/atuin/releases/download/v18.12.1/source.tar.gz"
+  sha256 "94b38e6031bad2409c176beae63580da35a1c3a1c129cc7c4c8f74f1e2965638"
+  license "MIT"
+  head "https://github.com/atuinsh/atuin.git", branch: "main"
+
+  depends_on "protobuf" => :build
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args(path: "crates/atuin-server")
+    pkgetc.install "crates/atuin-server/server.toml"
+  end
+
+  service do
+    run [opt_bin/"atuin-server", "start"]
+    environment_variables ATUIN_CONFIG_DIR: etc/"atuin-server"
+    keep_alive true
+    log_path var/"log/atuin-server.log"
+    error_log_path var/"log/atuin-server.log"
+  end
+
+  def caveats
+    <<~EOS
+      The configuration file is located at:
+        #{pkgetc}/server.toml
+    EOS
+  end
+
+  test do
+    assert_match "Atuin sync server", shell_output("#{bin}/atuin-server 2>&1", 2)
+  end
+end
