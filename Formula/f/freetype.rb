256ba0dc6eb2cd3062f169eb910c03def15ce3aa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-01 17:06:47 +0000

    freetype 2.14.2
    freetype: remove gnu-sed macos build dep
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/f/freetype.rb
+++ Formula/f/freetype.rb
@@ -1,11 +1,10 @@
 class Freetype < Formula
   desc "Software library to render fonts"
   homepage "https://www.freetype.org/"
-  url "https://downloads.sourceforge.net/project/freetype/freetype2/2.14.1/freetype-2.14.1.tar.xz"
-  mirror "https://download.savannah.gnu.org/releases/freetype/freetype-2.14.1.tar.xz"
-  sha256 "32427e8c471ac095853212a37aef816c60b42052d4d9e48230bab3bdf2936ccc"
+  url "https://downloads.sourceforge.net/project/freetype/freetype2/2.14.2/freetype-2.14.2.tar.xz"
+  mirror "https://download.savannah.gnu.org/releases/freetype/freetype-2.14.2.tar.xz"
+  sha256 "4b62dcab4c920a1a860369933221814362e699e26f55792516d671e6ff55b5e1"
   license "FTL"
-  revision 2
 
   livecheck do
     url :stable
@@ -26,18 +25,11 @@ class Freetype < Formula
 
   uses_from_macos "bzip2"
 
-  on_macos do
-    depends_on "gnu-sed" => :build
-  end
-
   on_linux do
     depends_on "zlib-ng-compat"
   end
 
   def install
-    # https://gitlab.freedesktop.org/freetype/freetype/-/issues/1358
-    ENV.prepend_path "PATH", Formula["gnu-sed"].opt_libexec/"gnubin" if OS.mac?
-
     # This file will be installed to bindir, so we want to avoid embedding the
     # absolute path to the pkg-config shim.
     inreplace "builds/unix/freetype-config.in", "%PKG_CONFIG%", "pkg-config"
