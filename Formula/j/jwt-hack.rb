cd8045610587ef3cce0c45be84ee9660b1b1dc01
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-02 14:30:45 +0000

    jwt-hack 2.5.0
    jwt-hack: capture verify stderr in test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/j/jwt-hack.rb
+++ Formula/j/jwt-hack.rb
@@ -1,8 +1,8 @@
 class JwtHack < Formula
   desc "JSON Web Token Hack Toolkit"
   homepage "https://github.com/hahwul/jwt-hack"
-  url "https://github.com/hahwul/jwt-hack/archive/refs/tags/v2.4.0.tar.gz"
-  sha256 "c167f595e5552dfbf5bd3fd79e2f061b1ad9d99790ffd036351172997a6de678"
+  url "https://github.com/hahwul/jwt-hack/archive/refs/tags/v2.5.0.tar.gz"
+  sha256 "85c6d8ade6f60ccafd12283c82ee1368874bc00af90f11fcad8f252baa197374"
   license "MIT"
   head "https://github.com/hahwul/jwt-hack.git", branch: "main"
 
@@ -34,6 +34,6 @@ class JwtHack < Formula
             "G4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTc0OTI4NzU5Nn0.6Z458qcvAJFTH9XCBgfmwgmqupsrefK8ItGYfyH0Ipc"
     assert_match "John", shell_output("#{bin}/jwt-hack decode #{token}")
     secret = "a-string-secret-at-least-256-bits-long"
-    assert_match "valid", shell_output("#{bin}/jwt-hack verify #{token} --secret #{secret}")
+    assert_match "valid", shell_output("#{bin}/jwt-hack verify #{token} --secret #{secret} 2>&1")
   end
 end
