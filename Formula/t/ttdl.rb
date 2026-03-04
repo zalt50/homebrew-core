c910e4ec5a464204102ab71af592b1c6838dd279
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-04 06:37:57 +0000

    ttdl: update 4.24.1 bottle.

--- Formula/t/ttdl.rb
+++ Formula/t/ttdl.rb
@@ -6,12 +6,12 @@ class Ttdl < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0cc35f58e9f612e1141097a9aee937677a610c00ec0202f84506a8ac9a164958"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ec9ecd0a17bbd5e32505b9a8b46114dd62a8d23369f6016cf13465e3eb8bd85e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "136a89bafc02210aa392284f9a4dd53ddd1dd070351202e52638b4040e275c71"
-    sha256 cellar: :any_skip_relocation, sonoma:        "18837af4ac3e24fdd150385acf8cacc3c09bc9659942444050ff6add5804edbf"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "368ed44f1b3d701f74a0e0670acf310cc2ca3b9dcb373d8ccab2735808adbb25"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5e4798b0692862883287cf77ca945d0dfecdba8aaec8ee59ca5466efe9ca2ff2"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "fa3c1cfff30a48e27cbb79a3aea1d637f8a56f22349f4ffa8d69611486dbb2ef"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "625bfe4204c555fd1b2fcd010214e2ceb447526fd5f4de67ca2a5d1c99655f26"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c2988a8def865b81da419dc3d4f7b53bce49d969e9d201da669ff6921aafd71a"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a67a7af1e8e53e09386f99be51a31df61783ea5c67c8d7f72b6e28f9c0b9b6c9"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1d78016b1ba5c9eb5cb8d3020d6b605d8258a09e639f1c10160aab02abd851c2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d14ecd93740c1b9f6cf37a496445ba2d189db0f4552097105de9f1f23734224e"
   end
 
   depends_on "rust" => :build
