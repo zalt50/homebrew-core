393f8235eba799679be8b8cbf5e47369f196c942
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-04 06:39:27 +0000

    ironclaw: update 0.14.0 bottle.

--- Formula/i/ironclaw.rb
+++ Formula/i/ironclaw.rb
@@ -7,12 +7,12 @@ class Ironclaw < Formula
   head "https://github.com/nearai/ironclaw.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "65e2605a2afd5199a403d61bd6d095b4bf78ef309420c4b6ec5f171c5c10d9a3"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a45a2dec67804852497847ab0e91b84424e9a4a7b66ec5816bbff408299dc60b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ebdb211ac56d09ec7c9890d660baa724d27b7dc199ea7e193a064b964d84c6b2"
-    sha256 cellar: :any_skip_relocation, sonoma:        "aecb1560d5d3816d85ac1361a94d09c7491b041e25faf2bf2c602ac43e9b69b3"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "388a7bd4755248db33ad5368e91415536135167ed1402337a5886ff3ad754ee7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7fbbb0a279b25072e2e89c6a4b078b524e239e70e6667077c337786476d5c847"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "143c4c13ea57c5a07c8a8d7699072f2797af8e22e7e808911e51df13792f13da"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d0cd9ee866ca752752cdbb61c93c95c769abde6eae4ae5886bbe4679bcaf31d7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "07f2b7c61696f42ffd8935d0716b49797511063f6713754ed4f449e933da67bb"
+    sha256 cellar: :any_skip_relocation, sonoma:        "20adf08ccc99cb397c8962ced86e0521902bb73bb95696af4b06c836a5d6778c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ba56cc8360f6511526351b2c3a7d332bd7c4c19c83f38450f0909f58d0467a2d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cb977cf5098ae97251df7121e9c1d91c10e13109402d9e04af9caf14a7804b40"
   end
 
   depends_on "pkgconf" => :build
