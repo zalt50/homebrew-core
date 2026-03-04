46b2c8f305f99cf917545c5490c4ef5b9350b938
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-04 02:56:25 +0000

    eksctl 0.224.0

--- Formula/e/eksctl.rb
+++ Formula/e/eksctl.rb
@@ -2,8 +2,8 @@ class Eksctl < Formula
   desc "Simple command-line tool for creating clusters on Amazon EKS"
   homepage "https://eksctl.io"
   url "https://github.com/eksctl-io/eksctl.git",
-      tag:      "0.223.0",
-      revision: "7a9410b8dbb499ec6cc5fc4f690d4bebbcc95289"
+      tag:      "0.224.0",
+      revision: "be36fd4e253a61d4c30a6d9f7a4e9148a48e5477"
   license "Apache-2.0"
   head "https://github.com/eksctl-io/eksctl.git", branch: "main"
 
