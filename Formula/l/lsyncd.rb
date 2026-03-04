d364a9ff868b16c55a1a498f701150dba00d049c
Michael Cho <michael@michaelcho.dev>
2026-03-04 01:01:00 -0500

    lsyncd: revision bump for `lua@5.4`

--- Formula/l/lsyncd.rb
+++ Formula/l/lsyncd.rb
@@ -4,6 +4,7 @@ class Lsyncd < Formula
   url "https://github.com/lsyncd/lsyncd/archive/refs/tags/release-2.3.1.tar.gz"
   sha256 "fc19a77b2258dc6dbb16a74f023de7cd62451c26984cedbec63e20ff22bcbdd8"
   license "GPL-2.0-or-later"
+  revision 1
 
   bottle do
     rebuild 2
@@ -16,7 +17,7 @@ class Lsyncd < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on "lua"
+  depends_on "lua@5.4"
 
   resource "xnu" do
     # From https://opensource.apple.com/releases/
