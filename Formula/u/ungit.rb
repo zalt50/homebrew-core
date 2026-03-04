0b33f9b5d5a07bd872327db010b524c5a5d3fa30
Michael Cho <michael@michaelcho.dev>
2026-03-03 21:13:05 -0500

    ungit 1.5.30

--- Formula/u/ungit.rb
+++ Formula/u/ungit.rb
@@ -1,8 +1,8 @@
 class Ungit < Formula
   desc "Easiest way to use Git. On any platform. Anywhere"
   homepage "https://github.com/FredrikNoren/ungit"
-  url "https://registry.npmjs.org/ungit/-/ungit-1.5.28.tgz"
-  sha256 "51f2e120f7b4ceb88ff19c7debf77877d50363f15df07d2df1235257387858af"
+  url "https://github.com/FredrikNoren/ungit/archive/refs/tags/v1.5.30.tar.gz"
+  sha256 "960b9e459edca37712303e42845fe90ca4a31744e854aa204c0e259b40e90315"
   license "MIT"
 
   bottle do
@@ -19,12 +19,8 @@ class Ungit < Formula
 
   test do
     port = free_port
-
-    fork do
-      exec bin/"ungit", "--no-launchBrowser", "--port=#{port}"
-    end
-    sleep 15
-
-    assert_includes shell_output("curl -s 127.0.0.1:#{port}/"), "<title>ungit</title>"
+    spawn bin/"ungit", "--no-launchBrowser", "--port=#{port}"
+    output = shell_output("curl --silent --retry 5 --retry-connrefused 127.0.0.1:#{port}/")
+    assert_match "<title>ungit</title>", output
   end
 end
