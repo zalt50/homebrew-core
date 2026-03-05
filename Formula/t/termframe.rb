ad89c01b5c93b6140c8fe1b22cbf655d86d62e38
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-05 20:52:35 +0000

    termframe 0.8.2

--- Formula/t/termframe.rb
+++ Formula/t/termframe.rb
@@ -1,8 +1,8 @@
 class Termframe < Formula
   desc "Terminal output SVG screenshot tool"
   homepage "https://github.com/pamburus/termframe"
-  url "https://github.com/pamburus/termframe/archive/refs/tags/v0.8.1.tar.gz"
-  sha256 "a355a4919974fa80c088adde8ecf56464bfdc8392cdf9f7ad9121f79f3350136"
+  url "https://github.com/pamburus/termframe/archive/refs/tags/v0.8.2.tar.gz"
+  sha256 "a3539aa9df7ccc803e32cc70b6fd93c791dc1b8e3e20c369705730d3a103c9a4"
   license "MIT"
   head "https://github.com/pamburus/termframe.git", branch: "main"
 
