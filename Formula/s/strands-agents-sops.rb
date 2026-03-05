9fec8de197da80596d3db48358ab34940037621e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-05 00:59:56 +0000

    strands-agents-sops: update 1.1.1 bottle.

--- Formula/s/strands-agents-sops.rb
+++ Formula/s/strands-agents-sops.rb
@@ -8,7 +8,7 @@ class StrandsAgentsSops < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "2d95dbf7246f6b394187da594b8a915f3d47d49e6e4d8be752badec2034daf25"
+    sha256 cellar: :any_skip_relocation, all: "f44f75acb7943451d6c0e5b002974aee94c7e747e541348ec6b0d668a3fa05cd"
   end
 
   depends_on "certifi" => :no_linkage
