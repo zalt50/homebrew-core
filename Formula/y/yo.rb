97c505574d86de3d03718b52067b7958cf9fb498
Anton Melnikov <antonsm21@gmail.com>
2026-03-05 12:03:34 +0000

    yo: add 7.0.0 bottle.

--- Formula/y/yo.rb
+++ Formula/y/yo.rb
@@ -6,6 +6,15 @@ class Yo < Formula
   license "BSD-2-Clause"
   head "https://github.com/yeoman/yo.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9486d31c4b44bf37322e4308d76ab31be862f46bdf73123731048c1ad4da5365"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9486d31c4b44bf37322e4308d76ab31be862f46bdf73123731048c1ad4da5365"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9486d31c4b44bf37322e4308d76ab31be862f46bdf73123731048c1ad4da5365"
+    sha256 cellar: :any_skip_relocation, sonoma:        "9486d31c4b44bf37322e4308d76ab31be862f46bdf73123731048c1ad4da5365"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "be53916a9e49b611ba57c7da357c203339e7cb605b8b5ec55dab608a46a463ce"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "be53916a9e49b611ba57c7da357c203339e7cb605b8b5ec55dab608a46a463ce"
+  end
+
   depends_on "node"
 
   def install
