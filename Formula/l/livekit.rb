db01f0dad2fec3ae075de53e62d973217c59233f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-05 20:33:40 +0000

    livekit 1.9.12

--- Formula/l/livekit.rb
+++ Formula/l/livekit.rb
@@ -1,8 +1,8 @@
 class Livekit < Formula
   desc "Scalable, high-performance WebRTC server"
   homepage "https://livekit.io"
-  url "https://github.com/livekit/livekit/archive/refs/tags/v1.9.11.tar.gz"
-  sha256 "a0f8042a63e895b33bc9c5d231969427c13201deb74dd10090a7655929a7d28a"
+  url "https://github.com/livekit/livekit/archive/refs/tags/v1.9.12.tar.gz"
+  sha256 "6e0d22cf782c2c91b731ec4a84f4ae7e63d59ea189bb41afc945fc124b753923"
   license "Apache-2.0"
   head "https://github.com/livekit/livekit.git", branch: "master"
 
