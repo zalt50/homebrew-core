eb5c4dd3d3d3c86c798d827b4cf23e6e943462bb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-05 12:52:10 +0000

    xonsh: update 0.22.6 bottle.

--- Formula/x/xonsh.rb
+++ Formula/x/xonsh.rb
@@ -10,12 +10,12 @@ class Xonsh < Formula
   head "https://github.com/xonsh/xonsh.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "26f5f3149fa2a6cf95a1b1eaec58ccb4900f6965c717da28ee920339763b8098"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6a953529d01040b2ea1edd10a0cff8d77a60212e4fea662c5d63cb50b121f2f2"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2702dbc09ae7ff99a43b0097c4ef2f760086c4dd4e5d8d0924b60027d04db481"
-    sha256 cellar: :any_skip_relocation, sonoma:        "fa7fbf110a2ba69ca1ef5b8369687104fa4ccdd4feec078b30d5c97b22c15ce1"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "12f83c157c5b5532fc7deefd635dac6a2b232ae204fcf5678645d8069accc69a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1edb63f5225bec04c9b48a0b087f7d6ba1f6b7a80cbcdec6e775111a728ab362"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7149c6bba10a7371c1f6acb3c7acf428fcec6b699884af79d4b24d36e68a2564"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "75e444048acacfefca535ad61c18316da64f37846d67bc66d93c7a7aecde033c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5349ff555ad93fdccae8f9c909376a52e98d46085b39d4ebfc955aca17636912"
+    sha256 cellar: :any_skip_relocation, sonoma:        "fde0d09bc990213507b4f071f77eea6a0b564cde0ba3c25e600fa6f46ebcbe46"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "b9cc24c5af25c8618b29dd59463965b6d4313108469ac90f4bd6f97e687e4a85"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "98cd6172706cb1e114c7fc889a7ef6e8dccff88b7b1cf9dbd1e34957927717f5"
   end
 
   depends_on "python@3.14"
