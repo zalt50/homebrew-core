9bee894529d53828623f97060736c4c56dcd6d20
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-06 20:19:12 +0000

    goenv 2.2.38

--- Formula/g/goenv.rb
+++ Formula/g/goenv.rb
@@ -1,8 +1,8 @@
 class Goenv < Formula
   desc "Go version management"
   homepage "https://github.com/go-nv/goenv"
-  url "https://github.com/go-nv/goenv/archive/refs/tags/2.2.37.tar.gz"
-  sha256 "fbb2d8fa31f41b9e4c660c1cab7a62bdc4d52666079136904c49a929b05c5e51"
+  url "https://github.com/go-nv/goenv/archive/refs/tags/2.2.38.tar.gz"
+  sha256 "ec22b8ca7f869f67e657fe6a2155e3fd1407a364429bdbaf04bb3daf33e12aeb"
   license "MIT"
   version_scheme 1
   head "https://github.com/go-nv/goenv.git", branch: "master"
