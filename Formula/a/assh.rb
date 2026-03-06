94bc3effd20fd4b144e7e1e86180c340e0431e4b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-06 22:58:11 +0000

    assh 2.17.1

--- Formula/a/assh.rb
+++ Formula/a/assh.rb
@@ -1,8 +1,8 @@
 class Assh < Formula
   desc "Advanced SSH config - Regex, aliases, gateways, includes and dynamic hosts"
   homepage "https://v1.manfred.life/assh/"
-  url "https://github.com/moul/assh/archive/refs/tags/v2.17.0.tar.gz"
-  sha256 "7640558e6ce57a2c90b628cedd2f639647e4845538e3aa261870098dfdf95445"
+  url "https://github.com/moul/assh/archive/refs/tags/v2.17.1.tar.gz"
+  sha256 "9571e3424dcfa5d52e32f3f7dd19995e3d1f30c9d80420e77d47681d4a201744"
   license "MIT"
   head "https://github.com/moul/assh.git", branch: "master"
 
