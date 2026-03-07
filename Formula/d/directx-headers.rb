12d664af80a19d91ad472ba58bca411802fd9baf
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2026-03-07 11:42:26 +0000

    directx-headers: add 1.619.1_1 bottle.

--- Formula/d/directx-headers.rb
+++ Formula/d/directx-headers.rb
@@ -6,6 +6,15 @@ class DirectxHeaders < Formula
   license "MIT"
   revision 1
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4d7a71f9e4416bf63af6b7ed1c2c7dca98f34c6b0e1917e8926a0ae35b01692b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5e1e0731354dba0d68e1f640c37340f20723cbded2bcbf4e6c16f399133c4791"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c56d31b978252ef14fd576b2dc8f38e15027f3e5f12454467e5e1ef297a544cc"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e283ac8535f3fb1c0ad04aff821ecac92f253d0c99e207b4f926ce2031326db4"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3bd9e83fbb2e810a14db26e48785dcfcc25fb541abf10c847635057c06b7c2a0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6f351534f4c948743491df1005f554a6518c89b8fd36b2b76d1c57c676356555"
+  end
+
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "mingw-w64" => :test
