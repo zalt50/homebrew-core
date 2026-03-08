8fe63158591475b107c7fc3d0804b978d4425c7d
Rui Chen <rui@chenrui.dev>
2026-03-07 20:56:30 -0500

    spicedb 1.49.2 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/s/spicedb.rb
@@ -0,0 +1,41 @@
+class Spicedb < Formula
+  desc "Open Source, Google Zanzibar-inspired database"
+  homepage "https://authzed.com/docs/spicedb/getting-started/discovering-spicedb"
+  url "https://github.com/authzed/spicedb/archive/refs/tags/v1.49.2.tar.gz"
+  sha256 "375343717e81ace4108aed7442a6a0827a5b404f00d66a8527f37ca6e610c029"
+  license "Apache-2.0"
+  head "https://github.com/authzed/spicedb.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X github.com/jzelinskie/cobrautil/v2.Version=#{version}"
+    system "go", "build", *std_go_args(ldflags:), "./cmd/spicedb"
+
+    generate_completions_from_executable(bin/"spicedb", shell_parameter_format: :cobra)
+    (man1/"spicedb.1").write Utils.safe_popen_read(bin/"spicedb", "man")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/spicedb version")
+
+    require "open3"
+
+    json = <<~JSON
+      {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "initialize",
+        "params": {
+          "rootUri": null,
+          "capabilities": {}
+        }
+      }
+    JSON
+
+    Open3.popen3(bin/"spicedb", "lsp") do |stdin, stdout|
+      stdin.write "Content-Length: #{json.size}\r\n\r\n#{json}"
+      assert_match(/^Content-Length: \d+/i, stdout.readline)
+    end
+  end
+end
