14e7512e912835eb30180092f80b87081c71b3df
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-08 23:57:06 +0000

    micasa: update 1.76.2 bottle.

--- Formula/m/micasa.rb
+++ Formula/m/micasa.rb
@@ -6,12 +6,12 @@ class Micasa < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "497129986bc5499abbfb40c52a0ea63e1c124f6c34ade7507e75e910082c8087"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "497129986bc5499abbfb40c52a0ea63e1c124f6c34ade7507e75e910082c8087"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "497129986bc5499abbfb40c52a0ea63e1c124f6c34ade7507e75e910082c8087"
-    sha256 cellar: :any_skip_relocation, sonoma:        "267ec0336189ff2459383bcc83134978145302768e63e787bf5723ef2d5edfc9"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ba1fd41dde0a488020aa72328e980ac3ea46e1717f33b774d828a49b4810a1b3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0964c96abc9d52301de7c5fdcf69c7165088081d2403fb92a59b302b5598887b"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "11cfa2ceb11540a54d1c36ddf269b8a6e56d4895b6352ace58c9732501905cc8"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "11cfa2ceb11540a54d1c36ddf269b8a6e56d4895b6352ace58c9732501905cc8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "11cfa2ceb11540a54d1c36ddf269b8a6e56d4895b6352ace58c9732501905cc8"
+    sha256 cellar: :any_skip_relocation, sonoma:        "98f11759f60f5c3a151f882093da58145108818e1f1fd8ba2ed9510b32132840"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "470f23f65c261df660a506f4d376e3df6ec6e469577f072c9f3cb778db734cab"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b2f3c06071acbda89c7595b027f7c378b3b1f929c11ba26a682470d54a27e0de"
   end
 
   depends_on "go" => :build
