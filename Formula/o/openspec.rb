c1538b9425e9de6d80270e8815b366c6d9841a67
JingBh <jingbohao@yeah.net>
2026-03-08 18:01:36 +0800

    openspec 1.2.0 (new formula)

---
+++ Formula/o/openspec.rb
@@ -0,0 +1,20 @@
+class Openspec < Formula
+  desc "Spec-driven development (SDD) for AI coding assistants"
+  homepage "https://openspec.dev/"
+  url "https://registry.npmjs.org/@fission-ai/openspec/-/openspec-1.2.0.tgz"
+  sha256 "2aceda94693f1db0b0d2ea3c750a2a418737eab30d026d1d066629945cde98ba"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    system bin/"openspec", "init", "--tools", "none"
+    assert_path_exists testpath/"openspec/changes"
+    assert_path_exists testpath/"openspec/specs"
+  end
+end
