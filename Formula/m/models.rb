a6898d429ff6944cf2ed1fdcb1f89fb03ac66c31
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-09 04:24:55 +0000

    models: add 0.10.0 bottle.

--- Formula/m/models.rb
+++ Formula/m/models.rb
@@ -6,6 +6,15 @@ class Models < Formula
   license "MIT"
   head "https://github.com/arimxyer/models.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "71ffbc0871a553e10cf0a40980c1288a70433ad44b431b851f35a38242846ba6"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "424b155e8670995e29918fdb9258e9c9f9262dfbfd4e8f11133e1eede7a90e5b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0b57065e374259e2c43402d6dab8e039ae9702eb74383752449ce05512510781"
+    sha256 cellar: :any_skip_relocation, sonoma:        "345133e8763d4285843c98e21c17bcddbdbd270d96ee854bb41741710f3241da"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "77ebab66a61d8aa3853e32867a693ffafd34d850d272ac891d2e8006b4498d9f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "add06a2015ed23081c0f8cd546ae8d1e80ecd27ae17a7b3fa2d7fad404bf5854"
+  end
+
   depends_on "rust" => :build
 
   def install
