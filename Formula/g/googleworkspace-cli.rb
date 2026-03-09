efbf42601b57d1e69f303449b614c7d563cb3ac4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-09 03:03:15 +0000

    googleworkspace-cli 0.8.1

--- Formula/g/googleworkspace-cli.rb
+++ Formula/g/googleworkspace-cli.rb
@@ -2,8 +2,8 @@ class GoogleworkspaceCli < Formula
   desc "CLI for Drive, Gmail, Calendar, Sheets, Docs, Chat, Admin, and more"
   homepage "https://developers.google.com/workspace"
   # We cannot install from the npm registry because it installs precompiled binaries
-  url "https://github.com/googleworkspace/cli/archive/refs/tags/v0.8.0.tar.gz"
-  sha256 "385feb75f45da21da4ed09f6a4d3785fbf6dca3c114ebd9f1bdc053a1afffdc5"
+  url "https://github.com/googleworkspace/cli/archive/refs/tags/v0.8.1.tar.gz"
+  sha256 "3da932c88b1824e38d1114729fb77eed47bf456c78637ea033c5acd8c8af643b"
   license "Apache-2.0"
 
   bottle do
