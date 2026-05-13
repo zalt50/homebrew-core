59abe570822fbeae4f742eceddc69f35f5f92ec3
Efstathios Chouliaris <Efstathios.Chouliaris@thomsonreuters.com>
2026-05-13 13:03:20 -0500

    goenv 3.1.1
    Update goenv to v3, a Go-based rewrite of the version manager.
    
    Add goenv@3 alias for the new major version.

--- Formula/g/goenv.rb
+++ Formula/g/goenv.rb
@@ -1,11 +1,12 @@
 class Goenv < Formula
   desc "Go version management"
   homepage "https://github.com/go-nv/goenv"
-  url "https://github.com/go-nv/goenv/archive/refs/tags/2.2.39.tar.gz"
-  sha256 "9c5571e731c1bbf4f6cc1d3606da1960ba56cf0ce1206c58a74e52e6df430e35"
+  url "https://github.com/go-nv/goenv/archive/refs/tags/3.1.1.tar.gz"
+  sha256 "8c107c1ec31cd544bbf59dc822676ef1966811a13e006aa35ba09546a74c4d9b"
   license "MIT"
   version_scheme 1
-  head "https://github.com/go-nv/goenv.git", branch: "master"
+  # TODO: Uncomment when default branch is changed from 'master' to 'main'
+  # head "https://github.com/go-nv/goenv.git", branch: "main"
 
   livecheck do
     url :stable
@@ -13,26 +14,37 @@ class Goenv < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "3cd517fd086597a2c1439187f5126ee613038ed5b84b291a100f10c1bcada012"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3cd517fd086597a2c1439187f5126ee613038ed5b84b291a100f10c1bcada012"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3cd517fd086597a2c1439187f5126ee613038ed5b84b291a100f10c1bcada012"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3cd517fd086597a2c1439187f5126ee613038ed5b84b291a100f10c1bcada012"
+    sha256 cellar: :any_skip_relocation, sonoma:        "3cd517fd086597a2c1439187f5126ee613038ed5b84b291a100f10c1bcada012"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3cd517fd086597a2c1439187f5126ee613038ed5b84b291a100f10c1bcada012"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3cd517fd086597a2c1439187f5126ee613038ed5b84b291a100f10c1bcada012"
   end
 
-  def install
-    inreplace_files = [
-      "libexec/goenv",
-      "plugins/go-build/install.sh",
-      "test/goenv.bats",
-      "test/test_helper.bash",
-      "plugins/go-build/test/test_helper.bash",
-    ]
-    inreplace inreplace_files, "/usr/local", HOMEBREW_PREFIX
+  depends_on "go" => :build
 
-    prefix.install Dir["*"]
-    %w[goenv-install goenv-uninstall go-build].each do |cmd|
-      bin.install_symlink "#{prefix}/plugins/go-build/bin/#{cmd}"
-    end
+  def install
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+      -X main.commit=#{tap&.user || "homebrew"}
+      -X main.buildTime=#{time.iso8601}
+    ]
+    system "go", "build", *std_go_args(ldflags:, output: bin/"goenv")
+  end
+
+  def caveats
+    <<~EOS
+      If you are upgrading from goenv v2, you may need to remove the stale shim:
+        rm -f "${GOENV_ROOT:-$HOME/.goenv}/shims/goenv"
+    EOS
   end
 
   test do
-    assert_match "Usage: goenv <command> [<args>]", shell_output("#{bin}/goenv help")
+    ENV["GOENV_ROOT"] = testpath/".goenv"
+
+    output = shell_output("#{bin}/goenv root")
+    assert_equal testpath/".goenv", Pathname(output.chomp)
   end
 end
