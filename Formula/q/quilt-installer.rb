6ccf7dfa2c4114047227d1441f92c141d74086b7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-15 07:04:48 +0000

    quilt-installer 0.13.1

--- Formula/q/quilt-installer.rb
+++ Formula/q/quilt-installer.rb
@@ -1,17 +1,13 @@
 class QuiltInstaller < Formula
   desc "Installer for Quilt for the vanilla launcher"
   homepage "https://quiltmc.org/"
-  url "https://maven.quiltmc.org/repository/release/org/quiltmc/quilt-installer/0.12.1/quilt-installer-0.12.1.jar"
-  sha256 "8b716edc692a2fa1fb78dbc2f432643be1bc6c867e5605f36f691f44257120ca"
+  url "https://maven.quiltmc.org/repository/release/org/quiltmc/quilt-installer/0.13.1/quilt-installer-0.13.1.jar"
+  sha256 "6e1bd14860f30e74a5827ba9dcc8c201a0c64f700421670c4bc7c436baca2747"
   license "Apache-2.0"
 
   livecheck do
     url "https://maven.quiltmc.org/repository/release/org/quiltmc/quilt-installer/maven-metadata.xml"
     regex(%r{<version>v?(\d+(?:\.\d+)+)</version>}i)
-  end
-
-  bottle do
-    sha256 cellar: :any_skip_relocation, all: "c2c3e3aa829bedf8617e2a6159dd32b8f80177f6ad1035e9a5fc51bef91330ab"
   end
 
   depends_on "openjdk"
