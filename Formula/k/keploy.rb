a309c6fcff1c878c6707ca4b1992f1ae4828081d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-18 09:31:29 +0000

    keploy 3.5.40

--- Formula/k/keploy.rb
+++ Formula/k/keploy.rb
@@ -1,8 +1,8 @@
 class Keploy < Formula
   desc "Testing Toolkit creates test-cases and data mocks from API calls, DB queries"
   homepage "https://keploy.io"
-  url "https://github.com/keploy/keploy/archive/refs/tags/v3.5.39.tar.gz"
-  sha256 "ac9dbc880146a98db9a92b397061d5302013cf26b091c9c3aa8f3c48b86a2962"
+  url "https://github.com/keploy/keploy/archive/refs/tags/v3.5.40.tar.gz"
+  sha256 "63b5341ded679ed3d160c9a2b43d025e805a251ed138b39782b0283e0c3a01ac"
   license "Apache-2.0"
   head "https://github.com/keploy/keploy.git", branch: "main"
 
