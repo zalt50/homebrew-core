5e2ec98c1da0f32af4d45552a7e6b6b11cd994da
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-18 21:45:47 +0000

    far2l-tty: add 2.8.0 bottle.

--- Formula/f/far2l-tty.rb
+++ Formula/f/far2l-tty.rb
@@ -10,6 +10,15 @@ class Far2lTty < Formula
     regex(/^v?_?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "088121534bab64a1ea3b5ae24d599b48554637c99581a04cf55cf3ee21180709"
+    sha256 cellar: :any,                 arm64_sequoia: "37bde471941fb73555cfd74a507817877a4747c23c87a6010a982625cd3751a5"
+    sha256 cellar: :any,                 arm64_sonoma:  "856e7218a5a8fe807b9d248402495f41f9a3d2b30084fe4d0e4705473915ca6b"
+    sha256 cellar: :any,                 sonoma:        "49ab5d7fc482aa75baaee02257c1a6c07fc1b5dfbd3e6cb435eaf51de5098b08"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "0a15d72ba2c10c49b9ff09893495ed73b8f4c4a64ab803359bc819646eddb2a4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "869a9e42952ddf4293495d448202df7a88bda9b91e6676981c9051b827c29e8f"
+  end
+
   depends_on "cmake" => :build
   depends_on "gperf" => :build
   depends_on "ninja" => :build
