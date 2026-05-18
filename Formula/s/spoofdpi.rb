ca3f0c26ad48f167e164ceca3d1b7a72ba3ca03a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-18 10:18:16 +0000

    spoofdpi: update 1.5.3 bottle.

--- Formula/s/spoofdpi.rb
+++ Formula/s/spoofdpi.rb
@@ -15,12 +15,12 @@ class Spoofdpi < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f30aadc2198a830c7afbcf3ae77cee7b1d793a47ed35da022646aae94dc8f94d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "92154552ed01f86e7826c03e5e719dddf274f47dd01be319be770204d67b7059"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "030dc6f6f158c4fa8fc538c4577ce25a328167804dc389d0ec666eaf65ab20e9"
-    sha256 cellar: :any_skip_relocation, sonoma:        "bf5021038e17b0a4a9d67f7b6377d7ff852187569cc79de7399b266b363ab5e6"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "41c04f77ace14af317a2fe5ed09c572e1f9818df474b8498b7591c779a8d0a98"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "92c03771d252a4124caa65a4bc68ee69cc27d91fb4b0414e8cb8845328c0f433"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2e631168212bca6fec6f132de41ba3339085432b9a34724b7eb3dbb6e7950122"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "59fe319df1427800638209c50a6f2cb78a8476e85c39868c74515c2ba064a9d0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fc77553af4edfe22b9791ce8e2e6586a1f894e7a3a647199157509581b148cfb"
+    sha256 cellar: :any_skip_relocation, sonoma:        "810d35048180d765431b7797892b55f6e6f039a9fb12187acaa97c722315483e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "8e1e998d069638b33eeb9c5de62d44844b8f83b43ca83a11b300da4f3edc16e5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8930ac9608dc25b6677ea0e2f17d09f1a939c30883eed01752f18a8517e4ae71"
   end
 
   depends_on "go" => :build
