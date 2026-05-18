958435b824f647f86dbbc37e6008465436a64151
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-18 11:58:54 +0000

    apfel 1.3.5

--- Formula/a/apfel.rb
+++ Formula/a/apfel.rb
@@ -1,8 +1,8 @@
 class Apfel < Formula
   desc "Apple Intelligence from the command-line, with OpenAi-compatible API server"
   homepage "https://apfel.franzai.com"
-  url "https://github.com/Arthur-Ficial/apfel/archive/refs/tags/v1.3.4.tar.gz"
-  sha256 "ec6253436da2b718e77caacd3b2a04d0529c3ea3dd87611dac65641eca8950fd"
+  url "https://github.com/Arthur-Ficial/apfel/archive/refs/tags/v1.3.5.tar.gz"
+  sha256 "0069cb125eee5faeef6810fe2d4b8bb95ba23a68a64c3e8ee5105fc94f69209b"
   license "MIT"
   head "https://github.com/Arthur-Ficial/apfel.git", branch: "main"
 
