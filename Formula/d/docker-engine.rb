3bd396d71c4143f211af1ff0c2c8832dd305bd89
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-18 17:48:24 +0000

    docker-engine 29.5.1

--- Formula/d/docker-engine.rb
+++ Formula/d/docker-engine.rb
@@ -2,8 +2,8 @@ class DockerEngine < Formula
   desc "Pack, ship and run any application as a lightweight container (Daemon)"
   homepage "https://www.docker.com/"
   url "https://github.com/moby/moby.git",
-      tag:      "docker-v29.5.0",
-      revision: "ff8d90ae98c160c3c8751412edbf95ec557217c5"
+      tag:      "docker-v29.5.1",
+      revision: "dd24a3adc1db4c762fb1b26b35c08ffd936f2d8f"
   license "Apache-2.0"
   head "https://github.com/moby/moby.git", branch: "master"
 
