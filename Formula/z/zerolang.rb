d14992563379877b07db907867114979d0af5cf9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-19 13:50:29 +0000

    zerolang: add 0.1.2 bottle.

--- Formula/z/zerolang.rb
+++ Formula/z/zerolang.rb
@@ -6,6 +6,15 @@ class Zerolang < Formula
   license "Apache-2.0"
   head "https://github.com/vercel-labs/zero.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9355c5915ebc91d1de391d1db9709630ad86ea5caf235002093165922b2bc324"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b165540f83ad2069b026452758db96f21f464c8f36c539ae12cc22380294d612"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c193cb33b43de50d2784e259260ffc4d0ae32f9b20bd1617d87b104afa7acc4a"
+    sha256 cellar: :any_skip_relocation, sonoma:        "b02fcd8a67537e487cdf33882da74f375e2dc37f34b8a95017ff615fa95c72fb"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c2affcdc77c77fb1521334c1a2595b042a02f6cc2a036e795bafc9dbc217c08d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2b07f908845ba1e3c05d14c98caf32419529eb32a6dd92e919df1d291029d564"
+  end
+
   def install
     system "make", "-C", "native/zero-c", "OUT=#{bin}/zero"
     rm bin/"zero.build"
