5e5fc5114f61edfe3b9ee88be03161ce12147edc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-19 19:07:07 +0000

    algolia: update 1.10.2 bottle.

--- Formula/a/algolia.rb
+++ Formula/a/algolia.rb
@@ -7,12 +7,12 @@ class Algolia < Formula
   head "https://github.com/algolia/cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9df882746a827a6fd5bf55bbcbdf5b9c185026480910194d54c999b947da64a3"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9df882746a827a6fd5bf55bbcbdf5b9c185026480910194d54c999b947da64a3"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9df882746a827a6fd5bf55bbcbdf5b9c185026480910194d54c999b947da64a3"
-    sha256 cellar: :any_skip_relocation, sonoma:        "1adeef4ecd1ff51176ef457ec8cbc72b6a7d1d30d3d1503dbc3004c8d6b5ac9d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "08a4bc724309c36e4816d872bae3c4b5a75d0073f0e07703ac822762ba17a7c6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7602e742c1e0397e24ee093f957c15d5d22cb809b094b81bfc8313aa7f791b2a"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a263bf087e2888e8329493b03ab7d25ccc4f8480c84ada85914d3245e01655fc"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a263bf087e2888e8329493b03ab7d25ccc4f8480c84ada85914d3245e01655fc"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a263bf087e2888e8329493b03ab7d25ccc4f8480c84ada85914d3245e01655fc"
+    sha256 cellar: :any_skip_relocation, sonoma:        "d30411c62976f403c93da05851f6e549bfbf5912178f3bd7c4aafda92d837e2e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c254755bc9e179068eeede67381127e2cd9025999644590969f52ba27b021359"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "60ac8b8de9598e0b27e8647ae06e4fc2246fbb0906b3ff5d93bc9fa798e350d1"
   end
 
   depends_on "go" => :build
