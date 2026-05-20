c646fe18beac84d9007a3f4a6001b47747a76a52
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-20 03:42:08 +0000

    ruby-build 20260520

--- Formula/r/ruby-build.rb
+++ Formula/r/ruby-build.rb
@@ -1,8 +1,8 @@
 class RubyBuild < Formula
   desc "Install various Ruby versions and implementations"
   homepage "https://rbenv.org/man/ruby-build.1"
-  url "https://github.com/rbenv/ruby-build/archive/refs/tags/v20260512.tar.gz"
-  sha256 "fc4aedf0f808a164c8a38a6315bb92047e01ab0152c488c270f3adb0b147a61c"
+  url "https://github.com/rbenv/ruby-build/archive/refs/tags/v20260520.tar.gz"
+  sha256 "5ff9f795c3c751eec29aaca25f98d371c1c52c3408d76d8aefb8cc2fcdc8f118"
   license "MIT"
   compatibility_version 1
   head "https://github.com/rbenv/ruby-build.git", branch: "master"
