699d3658c0fd64177e37c228b228025eb73a1720
Michael Cho <michael@michaelcho.dev>
2026-05-19 22:56:02 -0400

    etcd-cpp-apiv3: revision bump (protobuf 35.0)

--- Formula/e/etcd-cpp-apiv3.rb
+++ Formula/e/etcd-cpp-apiv3.rb
@@ -4,7 +4,7 @@ class EtcdCppApiv3 < Formula
   url "https://github.com/etcd-cpp-apiv3/etcd-cpp-apiv3/archive/refs/tags/v0.15.4.tar.gz"
   sha256 "4516ecfa420826088c187efd42dad249367ca94ea6cdfc24e3030c3cf47af7b4"
   license "BSD-3-Clause"
-  revision 45
+  revision 46
 
   bottle do
     sha256 cellar: :any, arm64_tahoe:   "bf69a21c97148bc0cf6b4f3df53d42b9cc38e572214a6c355fd93bddc8bd7112"
