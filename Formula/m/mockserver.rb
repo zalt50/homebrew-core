c62623ed0eaa81b7f2365a1025ed446952a0755a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-20 21:05:35 +0000

    mockserver 6.0.0

--- Formula/m/mockserver.rb
+++ Formula/m/mockserver.rb
@@ -1,8 +1,8 @@
 class Mockserver < Formula
   desc "Mock HTTP server and proxy"
   homepage "https://www.mock-server.com/"
-  url "https://search.maven.org/remotecontent?filepath=org/mock-server/mockserver-netty/5.15.0/mockserver-netty-5.15.0-brew-tar.tar"
-  sha256 "5f679d84e4254f32f4b129cdeead98028d59f523419c2e61ac13f3dc3e7418df"
+  url "https://search.maven.org/remotecontent?filepath=org/mock-server/mockserver-netty/6.0.0/mockserver-netty-6.0.0-brew-tar.tar"
+  sha256 "d6527928e2dce6b4681b7cd0df56c99a367e6e772498e0e57b1b9457a0744d26"
   license "Apache-2.0"
 
   livecheck do
