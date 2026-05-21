c690a2d7bf58b2f6938a0e3e2d90e19d07c7182c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-21 11:31:39 +0000

    grayskull: update 3.1.1_2 bottle.

--- Formula/g/grayskull.rb
+++ Formula/g/grayskull.rb
@@ -10,12 +10,12 @@ class Grayskull < Formula
   head "https://github.com/conda/grayskull.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b25a1679ab786e181839bbab0de93d7794e458a828c8e0e567bf044e0dac7222"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e215f2ece23da76481b64232cc7cdcdb20d5f00a666981b749a4bf0e9ab10ec3"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2dbd0742938392d0262a2580d50749b54f86ebda13f570d22aa3f0d7ea0942cf"
-    sha256 cellar: :any_skip_relocation, sonoma:        "07fd85e910f47240cef9966db356a4e3dce8f2c140583ca268daf601bb71c430"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3eeb416aec5c82309e3a5efcaf397e24c8fbc23aa004bc893588b79cc633fd88"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b50695354d8206b4cf6b7e32210ea8d8b29248ae1486eced399707ccb69e1d53"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f89acdcbc78a72cb528137d0cd36bcaf2f1be56cf9a95dadaa0b8203bdfce859"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9fd2de4d7881ead8e85dfe90a0884f7c2f3c653cf0876dd804838bd54f2ac558"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5046e7f4ff83d3b28f0c126105f11430db01f6393e8b82c8d44bb6a29db1ae81"
+    sha256 cellar: :any_skip_relocation, sonoma:        "4d54205c560aaa9d25d2d44f1927661cc2e6c2833af1142ef344190d0a053fd6"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3a8daf4e08623ab89a3dc0b61a3bb0701bdc414568f20b8b4f75ae85023df7f5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d38f34696b7fa5e5fc3bfa8344431bc7e457f1870760a7244305632bae92b89c"
   end
 
   depends_on "cmake" => :build
