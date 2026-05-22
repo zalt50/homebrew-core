2f68c072c7fc18d530fad902d36bb339b40d16fb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-22 20:24:11 +0000

    aom 3.14.1

--- Formula/a/aom.rb
+++ Formula/a/aom.rb
@@ -2,8 +2,8 @@ class Aom < Formula
   desc "Codec library for encoding and decoding AV1 video streams"
   homepage "https://aomedia.googlesource.com/aom"
   url "https://aomedia.googlesource.com/aom.git",
-      tag:      "v3.14.0",
-      revision: "047d8cf6168feafe1300eb6902000dd1a03d5549"
+      tag:      "v3.14.1",
+      revision: "03087864cf4bea6abb0d28f95cf7843511413d8f"
   license "BSD-2-Clause"
   head "https://aomedia.googlesource.com/aom.git", branch: "main"
 
