6eeee9f67dcad26a9cb7a3093ed0d20865c8a03c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-22 09:36:58 +0000

    summarize 0.16.1

--- Formula/s/summarize.rb
+++ Formula/s/summarize.rb
@@ -1,8 +1,8 @@
 class Summarize < Formula
   desc "Multi-modal AI tool to extract and summarize content"
   homepage "https://summarize.sh"
-  url "https://registry.npmjs.org/@steipete/summarize/-/summarize-0.15.2.tgz"
-  sha256 "b1706a747f8f44c31d23235d906d4e9a2583a8f3569ed7fd8902ab3cd38d53a9"
+  url "https://registry.npmjs.org/@steipete/summarize/-/summarize-0.16.1.tgz"
+  sha256 "138a32cbd5b4484ace8fd341554974585fcbc298760968218773924511a090c7"
   license "MIT"
   head "https://github.com/steipete/summarize.git", branch: "main"
 
