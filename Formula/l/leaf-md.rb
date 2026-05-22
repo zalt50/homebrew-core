db82a2ec1d771e0de5c873d2e1bce133f329d1a6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-22 14:00:40 +0000

    leaf-md: add 1.22.3 bottle.

--- Formula/l/leaf-md.rb
+++ Formula/l/leaf-md.rb
@@ -6,6 +6,15 @@ class LeafMd < Formula
   license "MIT"
   head "https://github.com/RivoLink/leaf.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "10aeb192489ad66899f3e441ec0a997b268445fbde4151866dd622381b747b08"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "22366ba78a121b163b1f2f6846291943fc91cb98fb3d5c99f49a293daa6c24bd"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7b4d1b1cc549857f10e4d1f8eb08372c8f3d5c8755b5f6d55c99f7fa5e9db323"
+    sha256 cellar: :any_skip_relocation, sonoma:        "45b3a403ce8c6af1963a81d7f552925634becb9f7416ac58d308c1f317de509a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "108005931b4f23e439eed5ad8a8a3e605768c00240b08d50c7b2607c633a3008"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7ef1134644373a1d811c027035195a39d1f160231160f99e0f18f887f267c22d"
+  end
+
   depends_on "rust" => :build
 
   def install
