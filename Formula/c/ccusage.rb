49c5317ef3e2987de6c95a3001c07116d4994da8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-22 12:30:27 +0000

    ccusage: update 20.0.3 bottle.

--- Formula/c/ccusage.rb
+++ Formula/c/ccusage.rb
@@ -6,12 +6,12 @@ class Ccusage < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0868ff3f9e8c54e12fc2ed87a9725208b0bfa7a34d532da7d6cbc98b3853b243"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8ccadf179710c1377015d3c8bc6414a4368ecdf47ea3cd305526bdd58e928ea2"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "527a1925ac2a97f7f9cffb094b9acc3e0d987cdbeadade9c00bd3b8f5c0efacf"
-    sha256 cellar: :any_skip_relocation, sonoma:        "81ecd2f3d7095c8c49670ec860f124493667a10f00cf30c12c4326de2a105ea6"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b1ec44b8448d22d1156cb16f26f94263d7d146725ef1ba7ae35824f9656a5930"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2351cc0a0973fd2a580da6e7fcc7c7224198da6b768887ed2fe65f41909fc37d"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "62ec0a47fc5ccc486ce6b27708b3c238f17b249c526e04e56a1591d36092742e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c1bc47ab097c15825fde1ae6a25b3015a2bb2585b05b7ee5feace920e7e0cef8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "44a727faa721d477e0538bef81c9a31c8e4fd57cdc22da4a7e64c906486c29d1"
+    sha256 cellar: :any_skip_relocation, sonoma:        "1780ff6c4bdace61cc4ed94f9c46a24258b23267730e74704ce324bdf613fd80"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "68d91fda2dee54ea03e097a04f2ed4df185e812461ca1eb0fff42fa2eebffd8e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4e72ac82f3fb690a8c1ab8e17ed962e58bb5b7d826917c7619028eeee6eb1a07"
   end
 
   depends_on "rust" => :build
