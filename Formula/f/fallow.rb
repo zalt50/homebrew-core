5ed1cbcd33f26156493852bfcacfc0aa2ce35292
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-22 18:21:30 +0000

    fallow: update 2.78.1 bottle.

--- Formula/f/fallow.rb
+++ Formula/f/fallow.rb
@@ -7,12 +7,12 @@ class Fallow < Formula
   head "https://github.com/fallow-rs/fallow.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5ec280284fd014959d0e0a43b14a6879ddaf7170bae7ee08ceb94abe5c384440"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2b0cb953cbc3c8a61187e5a4e6ae097fd2db6144bfdd629324d74a1e533dc9a3"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cc1ecdf1fcf0640132cc225757aa2c8c2191b4c9a35cda1dba22f07c8999921d"
-    sha256 cellar: :any_skip_relocation, sonoma:        "30bee513f626a6ead190fb5f6b987597b2aee8f7114069c79a76fcd18c4895f7"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8fda828b29ab8709dfc2af71a52cc52124d44048c72490a637923500a923cbe6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cf7711976f51719f19596a476c79828c351842b6c85da7fe265092951084d4d7"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "dd769a0041f9633c3c1945dc7c808bef2470449af6d2a82b968aed1eb015ea5d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "dadac3462dff0131013331ff1b5822669281c31f4e12b14a29efc2e919816fd4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "951ddd9998f6d9e10b64840193adc2e7e9605ea5a30b60122c02033478bc195d"
+    sha256 cellar: :any_skip_relocation, sonoma:        "cc119c4ff9a8302239f338fb7e3cb90e2827fa6ac739eac417ab88495f7b7dcd"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "95135ca5a1b127baf43ccef687bdb87e5b4c47e8ef530003017cd2606b1d3487"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b2b4e65e2e7b8bd7f15f0187d730c2f5ce803edf28ffe7ff4a5d8db15b59040c"
   end
 
   depends_on "rust" => :build
