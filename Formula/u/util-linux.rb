8a8b912036f7d145f06e94442da54fb3a60e726c
botantony <antonsm21@gmail.com>
2026-05-22 20:48:58 +0200

    util-linux 2.42.1

--- Formula/u/util-linux.rb
+++ Formula/u/util-linux.rb
@@ -1,8 +1,8 @@
 class UtilLinux < Formula
   desc "Collection of Linux utilities"
   homepage "https://github.com/util-linux/util-linux"
-  url "https://mirrors.edge.kernel.org/pub/linux/utils/util-linux/v2.42/util-linux-2.42.tar.xz"
-  sha256 "3452b260bbaa775d6e749ac3bb22111785003fc1f444970025c8da26dfa758e9"
+  url "https://mirrors.edge.kernel.org/pub/linux/utils/util-linux/v2.42/util-linux-2.42.1.tar.xz"
+  sha256 "82e9158eb12a9b0b569d84e1687fed9dd18fe89ccd8ef5ac3427218a7c0d7f7f"
   license all_of: [
     "BSD-3-Clause",
     "BSD-4-Clause-UC",
