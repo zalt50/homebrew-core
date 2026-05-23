743da8345fe8ddddba5e76684710be4c6f49a38a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-23 12:35:52 +0000

    util-linux: update 2.42.1 bottle.

--- Formula/u/util-linux.rb
+++ Formula/u/util-linux.rb
@@ -25,12 +25,12 @@ class UtilLinux < Formula
   end
 
   bottle do
-    sha256 arm64_tahoe:   "22c68cf329077a00f5c2eb9ea3030c305be6837e1a1dd8fc87da1329d57cef26"
-    sha256 arm64_sequoia: "a4100e6dc75510d4820b3fcbecf3f224a37a97299aafb2380a9bb29948b976ab"
-    sha256 arm64_sonoma:  "7145241dd73fc2d948dafdae4e4b8da48954dff12bca3bd277c33d64377897b9"
-    sha256 sonoma:        "9488d859b2612179a1085086d8df291f307c9c4473fec0a20718da8aa8f237b6"
-    sha256 arm64_linux:   "9237db9b3c08580b2e6d13c386b7fa282d80a4038861dab51276a993f196a5dd"
-    sha256 x86_64_linux:  "e97b24d7baa64ffc4a3a61aedd8daf41d3a3559be823910a9ca218dc24d62497"
+    sha256 arm64_tahoe:   "3d9b6c9ed9786e56475bdbf1181d8a59aca9d0dd7bd01ecf782b1d2220033f22"
+    sha256 arm64_sequoia: "26073d37214d23bc3da03a92a6250e0b7461c9c3e58b53017bd03958b019f0c0"
+    sha256 arm64_sonoma:  "cb76a523dcaa1bfe9b01288c582c83ee165a964978dd6163e4f27a651b7be58f"
+    sha256 sonoma:        "9797d1d36e0e2726b41cdc002ed51204b3032a5c69899205790b51bdc2b6d01d"
+    sha256 arm64_linux:   "fe7287412da2b1494ac30408eee4e509aa4d137e62b445da9843ce6ce8ca8d55"
+    sha256 x86_64_linux:  "700f5145a2588ddc99b51c16504c934e81e34418731e07fba13bfa2949bb6c6e"
   end
 
   keg_only :shadowed_by_macos, "macOS provides the uuid.h header"
