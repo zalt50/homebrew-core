bbbc8f967ebc832a46b098dfa19f281f261929b0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-23 14:19:58 +0000

    satellite-tracker: add 0.1.20 bottle.

--- Formula/s/satellite-tracker.rb
+++ Formula/s/satellite-tracker.rb
@@ -5,6 +5,15 @@ class SatelliteTracker < Formula
   sha256 "9a5ff9f12230b6821805a07a76e61420d52f0ed60ee4a5da2cc37917abdebebf"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0e3f1176e48ce806909cd6e32aeaea679fea6353a0bad61f97880c55bfc21f33"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "402effae1dd52f4ee6c28d921c55e17c5f3be92063146a49fbd08beaca7e3e81"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5708af5d305862da059606d4d33f00946351d617c2e0a683725e4422aac3dd32"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e02ec3ff9cf189291eae233f7b183b1812c52a38eb264d35da0880cbaf25b37c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "7dd5cacb400a734ffa52b7bb8a747ffcdbeaeedc7ce76666b9920ac462f27b19"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "48b732f5167d97ee80f570a12bf2e4fd618532c0599f93f94ee784ae50e58e55"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
 
