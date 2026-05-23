b42c7d4dc9f53a99474b82e34e0bfef97ad882c7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-23 20:40:36 +0000

    objfw 1.5.4

--- Formula/o/objfw.rb
+++ Formula/o/objfw.rb
@@ -1,8 +1,8 @@
 class Objfw < Formula
   desc "Portable, lightweight framework for the Objective-C language"
   homepage "https://objfw.nil.im/"
-  url "https://objfw.nil.im/downloads/objfw-1.5.3.tar.gz"
-  sha256 "30baa45a702efc99601d1cd4c2864f77128903a3aedd27aa05093acd56224147"
+  url "https://objfw.nil.im/downloads/objfw-1.5.4.tar.gz"
+  sha256 "7306dcf95bab8070efd1ff66791585b40c1826c109e280fb0db9a5d1cfb1384c"
   license "LGPL-3.0-only"
   head "https://git.nil.im/ObjFW/ObjFW.git", branch: "main"
 
