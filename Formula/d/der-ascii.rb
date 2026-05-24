abc1c6350cbf14038c565e3df57982be46911a04
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-24 17:34:16 +0000

    der-ascii: update 0.8.0 bottle.

--- Formula/d/der-ascii.rb
+++ Formula/d/der-ascii.rb
@@ -7,12 +7,12 @@ class DerAscii < Formula
   head "https://github.com/google/der-ascii.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d4a18fe39f98ab330d0be623e93e92fc7fc21663a21e8fedb2e0c4108e42acce"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d4a18fe39f98ab330d0be623e93e92fc7fc21663a21e8fedb2e0c4108e42acce"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d4a18fe39f98ab330d0be623e93e92fc7fc21663a21e8fedb2e0c4108e42acce"
-    sha256 cellar: :any_skip_relocation, sonoma:        "9f90e0345b09d95c169528d54c1910032ff8cda324a16fd80db4e9c5c8320dfd"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "841b6ec4e24ecd59cece4aef187231f63b7fe67e61534ed43cd9c3d1aa80055a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a7313f36585dbab7370d770765e0e8e974dfa6da731d44e0340c193307b83659"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6fa6de5038d534489c96633367decdb1dd2c1551858d6aab33217b94be3f5170"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6fa6de5038d534489c96633367decdb1dd2c1551858d6aab33217b94be3f5170"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6fa6de5038d534489c96633367decdb1dd2c1551858d6aab33217b94be3f5170"
+    sha256 cellar: :any_skip_relocation, sonoma:        "11f733dbc4b5c511ead29361e1e6d7461f719416def5044b24193eb5336faed5"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "14497d6a5b43b29de266da7c68f38339f3920299083113514afac90629e2c4e4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "66207c1fead032727e757ade6ffbb02c75c54e97fe39a9a2f4de582da442e60b"
   end
 
   depends_on "go" => :build
