3824644df187fee8e6f7e771628bf05a7ece70c4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-24 05:15:15 +0000

    youplot: update 0.5.0 bottle.

--- Formula/y/youplot.rb
+++ Formula/y/youplot.rb
@@ -6,13 +6,12 @@ class Youplot < Formula
   license "MIT"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_tahoe:   "eeb8d9717d60ae7b9dfcd78aacdc79015d4ca8295b7f97fe1edd475ae4792d91"
-    sha256 cellar: :any,                 arm64_sequoia: "b2dbf2a7fa893c9113ea3f231d61bde826361916aeab171ffcda8c19f62d7513"
-    sha256 cellar: :any,                 arm64_sonoma:  "895212146959ad158fd3ed9a352f23f044abe05cac8f08a2288dc969a7d02ca8"
-    sha256 cellar: :any,                 sonoma:        "ef0afa69f04a5b1b718d24ff46ebd7c285ff6d520d54c1f4369c0d4887b45e54"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "dedea3ca0dcb2c889d0f26e9fe2f06f9eb506db2457950066ca15cae852fcc60"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "823bd972bb4b954f066ef5503fdba09fa90b1c097143201817e06ff4e1be4724"
+    sha256 cellar: :any,                 arm64_tahoe:   "dc94ee1841953db68429b184f1ccf38608174114f6560ecf9469f72c1e95f7bf"
+    sha256 cellar: :any,                 arm64_sequoia: "9136892172c5aa99186e6dcf0fdef334339f4b0cb0c15efa580d28b1ac72c6b3"
+    sha256 cellar: :any,                 arm64_sonoma:  "fe8890b71a8dcd5ed49f2697b4d28b5ebce4ac8af848fc89163d6d7b61275305"
+    sha256 cellar: :any,                 sonoma:        "b4881454d9f25cc985533b8ac3bb4258fd55d93e79a4d06e332bd2c02a883d73"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "27d6f5919c287738dda830625f2e474d8b3902aaf6c5b96e2f87b4288cca59cd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d52bd59e045e634d0f5f5a476235989bf766615bcf6d76912aa4f34d7e237744"
   end
 
   depends_on "ruby"
