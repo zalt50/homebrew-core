5fed7df2dcdd5395c6f4eba5fcb81fdff4d0ae2c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-25 06:24:18 +0000

    cpp-httplib 0.46.0

--- Formula/c/cpp-httplib.rb
+++ Formula/c/cpp-httplib.rb
@@ -1,8 +1,8 @@
 class CppHttplib < Formula
   desc "C++ header-only HTTP/HTTPS server and client library"
   homepage "https://github.com/yhirose/cpp-httplib"
-  url "https://github.com/yhirose/cpp-httplib/archive/refs/tags/v0.45.1.tar.gz"
-  sha256 "0977d608bb77848044af71afe004554a0e577496dc0f4fb6da33d5380e7f5070"
+  url "https://github.com/yhirose/cpp-httplib/archive/refs/tags/v0.46.0.tar.gz"
+  sha256 "4b91c970132cd140586ba880beab099fd16f0dc48425a5a60c418422c04d9fc1"
   license "MIT"
 
   bottle do
