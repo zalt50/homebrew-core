f6c5a8f64727d52c06697e8ec270181e39955640
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-25 12:48:41 +0000

    terragrunt 1.0.6

--- Formula/t/terragrunt.rb
+++ Formula/t/terragrunt.rb
@@ -1,8 +1,8 @@
 class Terragrunt < Formula
   desc "Thin wrapper for Terraform e.g. for locking state"
   homepage "https://terragrunt.gruntwork.io/"
-  url "https://github.com/gruntwork-io/terragrunt/archive/refs/tags/v1.0.5.tar.gz"
-  sha256 "75013ef30d3ec05343876d3cd9deccc5191334013007a0c8a9be29b3b94b1be8"
+  url "https://github.com/gruntwork-io/terragrunt/archive/refs/tags/v1.0.6.tar.gz"
+  sha256 "ecd41d35d794b9d580797590c3dccb1d166812ab21abb87191b190f9c556c7a1"
   license "MIT"
 
   # Upstream creates releases that use a stable tag (e.g., `v1.2.3`) but are
@@ -30,7 +30,7 @@ class Terragrunt < Formula
   def install
     ldflags = %W[
       -s -w
-      -X github.com/gruntwork-io/go-commons/version.Version=#{version}
+      -X github.com/gruntwork-io/terragrunt/internal/version.Version=#{version}
     ]
     system "go", "build", *std_go_args(ldflags:)
   end
