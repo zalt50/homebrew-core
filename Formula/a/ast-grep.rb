e72cadc3edc43adc52999aa962ae9b49d8fc6717
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-25 22:02:32 +0000

    ast-grep: update 0.43.0 bottle.

--- Formula/a/ast-grep.rb
+++ Formula/a/ast-grep.rb
@@ -7,12 +7,12 @@ class AstGrep < Formula
   head "https://github.com/ast-grep/ast-grep.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5d0fe480ee1fb474b7eeb2bb62fb04a839de4384d613f5116a4428ee81c160b8"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "049a2cefb3568ef7c1727bd8d2de03b87619d4fd9f836420bea03dc25a07282d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4d0196fae8b95e4f1efdd1bac4f53cce311ef4c242c750138114db405de97208"
-    sha256 cellar: :any_skip_relocation, sonoma:        "0cbbca27ad6966a734644f9e7b5328436fb8be7a4eed38a11018eefc6c6fc093"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "285610c3aea721472cccaab9b68adc340b15bb72aef0177481994fd6b71ba6a4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e36514fcd295a97c5a3ff223a39f873aa847710ca00c82cf052a7b82e6cdda42"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a3b8f1e80564bbad94c72b307999bbbf876462f06004d0b31b98ef8c0977bc0b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fa7639de28b4becddd39f68d15f10b0dea2355d362ecfe295ca7ae5d4f293187"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4d302e0f236feed599b2a8bf7e32a97d33bfb6823a4dc5076f7b6f5861337dc1"
+    sha256 cellar: :any_skip_relocation, sonoma:        "7fa8c9c82228c329563b194deafeb024e5a4b320105fa95ab519f778f4032758"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "222f7b9deb03c3b45fcac87f830841260a60d676aaa70066ba8a5c35f55f5c49"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "197984baf50547ba6248d942393b52eef14f42893117f8453e3a85de33ccc750"
   end
 
   depends_on "rust" => :build
