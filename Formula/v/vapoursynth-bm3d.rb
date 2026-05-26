2ed78f26c18005ff1d72fb00fcaaa32f7b4de05b
Michael Cho <michael@michaelcho.dev>
2026-05-25 20:04:16 -0400

    vapoursynth-bm3d: revision bump (vapoursynth 76)

--- Formula/v/vapoursynth-bm3d.rb
+++ Formula/v/vapoursynth-bm3d.rb
@@ -4,6 +4,7 @@ class VapoursynthBm3d < Formula
   url "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-BM3D/archive/refs/tags/r10.1.tar.gz"
   sha256 "3a340c23f4d77559d7c766a2a14f4a1e408752a785958930eb4ca41e13392c85"
   license "MIT"
+  revision 1
   head "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-BM3D.git", branch: "master"
 
   bottle do
@@ -19,27 +20,21 @@ class VapoursynthBm3d < Formula
   depends_on "ninja" => :build
   depends_on "pkgconf" => :build
   depends_on "fftw"
+  depends_on "python@3.14"
   depends_on "vapoursynth"
 
-  def install
-    # Upstream build system wants to install directly into vapoursynth's libdir and does not respect
-    # prefix, but we want it in a Cellar location instead.
-    inreplace "meson.build" do |s|
-      s.gsub!(/^incdir = include_directories\(.*?^\)/m,
-              "incdir = include_directories('#{Formula["vapoursynth"].opt_include}/vapoursynth', 'include')")
-      s.gsub! "install_dir: py.get_install_dir() / 'vapoursynth/plugins'", "install_dir: '#{lib}/vapoursynth'"
-    end
+  def python3 = "python3.14"
 
-    system "meson", "setup", "build", *std_meson_args
+  def install
+    # Work around Homebrew's python prefix patch
+    args = %W[-Dpython.platlibdir=#{prefix/Language::Python.site_packages(python3)}]
+
+    system "meson", "setup", "build", *args, *std_meson_args
     system "meson", "compile", "-C", "build", "--verbose"
     system "meson", "install", "-C", "build"
   end
 
   test do
-    python = Formula["vapoursynth"].deps
-                                   .find { |d| d.name.match?(/^python@\d\.\d+$/) }
-                                   .to_formula
-                                   .opt_libexec/"bin/python"
-    system python, "-c", "from vapoursynth import core; core.bm3d"
+    system python3, "-c", "from vapoursynth import core; core.bm3d"
   end
 end
