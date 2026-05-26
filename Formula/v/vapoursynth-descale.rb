313d739f499f9b6002dcf077f44d276e9718f68b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-26 08:47:18 +0000

    vapoursynth-descale: update 8_1 bottle.

--- Formula/v/vapoursynth-descale.rb
+++ Formula/v/vapoursynth-descale.rb
@@ -8,12 +8,12 @@ class VapoursynthDescale < Formula
   head "https://github.com/Irrational-Encoding-Wizardry/descale.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "367adf1613ed0354d8a93588f765437e072bc9c2fcfa7cd8cbcef9291f38475d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9faad8e0a5ee9b78fbe86a7119fae7b316f96565f2ab72881f42301f5bcfa931"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4b77deed08ec1f7fa494acb387ea8af43a2a81afb2aee8431c6b30b27f5f63f3"
-    sha256 cellar: :any_skip_relocation, sonoma:        "46b58d656b8b7a374e1a759ca4e10e786d29356084308ed06d97f9919c56ce7d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "268756b29d7c04d9f28f6142462a51c77d6ab779b06011d12b42e6c71f18e279"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9b57118f0638ab218aadc619c10a0790d97676ee8f0fee8de3ae9e8daf027fc8"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6f9634605ee0f4847576315b231184694138b1acde8092705e3bb465b4bb3a06"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "baa8d37ca8fb30b658a49831fff0dd09b3fb74163f655b4fb742e55f619a216d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "99889b5ab38fbfccc54913f103d37fab1a32161e4fba2244b701e186a0760c53"
+    sha256 cellar: :any_skip_relocation, sonoma:        "b4ee93781befa25edce1bc69a12f772643b1369b5a0fecfbdb0dc70a3c779788"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "0733d5b06a7a860adb8ebd3a8f951fa1f87d7bd876bdde302334899c7ff91b72"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "449087686e60c7ee1d59cc82290c791c139d7210aea8e936f6a099b071282f71"
   end
 
   depends_on "meson" => :build
