e7dca0e66ef09ab0bd57afbf7e8bc06f6fd8984e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-26 06:54:48 +0000

    taze 19.14.1

--- Formula/t/taze.rb
+++ Formula/t/taze.rb
@@ -1,8 +1,8 @@
 class Taze < Formula
   desc "Modern cli tool that keeps your deps fresh"
   homepage "https://github.com/antfu-collective/taze"
-  url "https://registry.npmjs.org/taze/-/taze-19.13.0.tgz"
-  sha256 "c5257a05a68723df5adecac966f014a97e57cfa8ae058e2ed69e4b1bc9030092"
+  url "https://registry.npmjs.org/taze/-/taze-19.14.1.tgz"
+  sha256 "bdad9774757a08062d2ac3175e49cd3080cbb56b9e184fc495792d1693073588"
   license "MIT"
   head "https://github.com/antfu-collective/taze.git", branch: "main"
 
