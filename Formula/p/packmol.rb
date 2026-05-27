adb3f52106a3b4d4aa0a65dd9056845fbb32d53b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-27 15:25:56 +0000

    packmol 21.2.3

--- Formula/p/packmol.rb
+++ Formula/p/packmol.rb
@@ -1,8 +1,8 @@
 class Packmol < Formula
   desc "Packing optimization for molecular dynamics simulations"
   homepage "https://www.ime.unicamp.br/~martinez/packmol/"
-  url "https://github.com/m3g/packmol/archive/refs/tags/v21.2.1.tar.gz"
-  sha256 "6b8275be64e425fd578e7606ac22bb586108bc6b5c17d417873bd108928f830f"
+  url "https://github.com/m3g/packmol/archive/refs/tags/v21.2.3.tar.gz"
+  sha256 "b26af58d407b1c8d786aa6c2bb6bfa40d741aaa0424db51300fdd6fd66dd4813"
   license "MIT"
   head "https://github.com/m3g/packmol.git", branch: "master"
 
