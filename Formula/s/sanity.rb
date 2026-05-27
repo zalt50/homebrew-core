b81c98069839b62c42ce0a504e1f17e667a8abb0
Jonah Snider <jonah@jonahsnider.com>
2026-05-27 14:14:57 -0700

    sanity 5.27.0 (new formula)
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

---
+++ Formula/s/sanity.rb
@@ -0,0 +1,33 @@
+class Sanity < Formula
+  desc "Command-line interface for Sanity"
+  homepage "https://www.sanity.io/"
+  url "https://registry.npmjs.org/sanity/-/sanity-5.27.0.tgz"
+  sha256 "723b1da05828c9ed2e49bbfef14659ea31676a67ce747f9189ee61eec4ccd18e"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+
+    node_modules = libexec/"lib/node_modules/sanity/node_modules"
+    # Remove incompatible pre-built `bare-fs`/`bare-os`/`bare-url` binaries
+    os = OS.kernel_name.downcase
+    arch = Hardware::CPU.intel? ? "x64" : Hardware::CPU.arch.to_s
+    node_modules.glob("{bare-fs,bare-os,bare-url}/prebuilds/*")
+                .each { |dir| rm_r(dir) if dir.basename.to_s != "#{os}-#{arch}" }
+
+    deuniversalize_machos node_modules/"fsevents/fsevents.node" if OS.mac?
+  end
+
+  test do
+    ENV["HOME"] = testpath
+    ENV["CI"] = "1"
+    ENV.delete "SANITY_AUTH_TOKEN"
+
+    output = shell_output("#{bin}/sanity debug")
+    assert_match "Not logged in", output
+    assert_match "No project found", output
+  end
+end
