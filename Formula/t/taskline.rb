3902921adf2edf97ccac5043924863d11c46306f
Michael Cho <michael@michaelcho.dev>
2026-05-27 12:41:42 -0400

    taskline: create all bottle

--- Formula/t/taskline.rb
+++ Formula/t/taskline.rb
@@ -26,12 +26,12 @@ class Taskline < Formula
 
     clipboardy_fallbacks_dir = libexec/"lib/node_modules/@perryrh0dan/#{name}/node_modules/clipboardy/fallbacks"
     rm_r(clipboardy_fallbacks_dir) # remove pre-built binaries
-    if OS.linux?
-      linux_dir = clipboardy_fallbacks_dir/"linux"
-      linux_dir.mkpath
-      # Replace the vendored pre-built xsel with one we build ourselves
-      ln_sf (Formula["xsel"].opt_bin/"xsel").relative_path_from(linux_dir), linux_dir
-    end
+
+    linux_dir = clipboardy_fallbacks_dir/"linux"
+    linux_dir.mkpath
+    # Replace the vendored pre-built xsel with one we build ourselves.
+    # We add unused symlink on macOS to create an all bottle
+    ln_sf (Formula["xsel"].opt_bin/"xsel").relative_path_from(linux_dir), linux_dir
   end
 
   test do
