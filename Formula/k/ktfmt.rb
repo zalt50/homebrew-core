c1b19a57f0ff3bd0855ccb21932621cab9cad93b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-27 00:06:14 +0000

    ktfmt: update 0.63 bottle.

--- Formula/k/ktfmt.rb
+++ Formula/k/ktfmt.rb
@@ -6,12 +6,12 @@ class Ktfmt < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a78b1d6dd17daf32f24b39c56462fd9b64fe78e110ee03f5cce9d7e720eaf1b1"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a78b1d6dd17daf32f24b39c56462fd9b64fe78e110ee03f5cce9d7e720eaf1b1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a78b1d6dd17daf32f24b39c56462fd9b64fe78e110ee03f5cce9d7e720eaf1b1"
-    sha256 cellar: :any_skip_relocation, sonoma:        "a78b1d6dd17daf32f24b39c56462fd9b64fe78e110ee03f5cce9d7e720eaf1b1"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8730ebfede680e8298d21438c5bad4cf3a088d088b3e8adaf1275fec54a18a3a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8730ebfede680e8298d21438c5bad4cf3a088d088b3e8adaf1275fec54a18a3a"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "106bb88438645dd8c6637d63045e8939d234cef082b53bd0dd862f4d1fdd6e31"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "106bb88438645dd8c6637d63045e8939d234cef082b53bd0dd862f4d1fdd6e31"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "106bb88438645dd8c6637d63045e8939d234cef082b53bd0dd862f4d1fdd6e31"
+    sha256 cellar: :any_skip_relocation, sonoma:        "106bb88438645dd8c6637d63045e8939d234cef082b53bd0dd862f4d1fdd6e31"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ac48dd69361bd524166f0197bb1d2a30fa954c113c859f8ea329fe84252f830b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ac48dd69361bd524166f0197bb1d2a30fa954c113c859f8ea329fe84252f830b"
   end
 
   depends_on "gradle" => :build
