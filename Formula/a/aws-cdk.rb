90a1fea1ef6fdda9cb822e37c3f7f63deaadeba5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-28 17:54:30 +0000

    aws-cdk 2.1125.0

--- Formula/a/aws-cdk.rb
+++ Formula/a/aws-cdk.rb
@@ -1,8 +1,8 @@
 class AwsCdk < Formula
   desc "AWS Cloud Development Kit - framework for defining AWS infra as code"
   homepage "https://github.com/aws/aws-cdk"
-  url "https://registry.npmjs.org/aws-cdk/-/aws-cdk-2.1124.1.tgz"
-  sha256 "545695bf080aeff6fa776c4a2ae6eef857efbbd291fcb3579b7903a81d7fd5a6"
+  url "https://registry.npmjs.org/aws-cdk/-/aws-cdk-2.1125.0.tgz"
+  sha256 "8437eb72f5ac16a223d87a12f87374de4f24b7ab8a7a0bbb2dd1d467093ea836"
   license "Apache-2.0"
 
   bottle do
