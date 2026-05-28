7e088bce2870803bc732a02abd1b906faa3d8ebb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-28 12:56:08 +0000

    woodpecker-cli 3.15.0

--- Formula/w/woodpecker-cli.rb
+++ Formula/w/woodpecker-cli.rb
@@ -1,8 +1,8 @@
 class WoodpeckerCli < Formula
   desc "CLI client for the Woodpecker Continuous Integration server"
   homepage "https://woodpecker-ci.org/"
-  url "https://github.com/woodpecker-ci/woodpecker/archive/refs/tags/v3.14.1.tar.gz"
-  sha256 "f84115ea73513b9728570c90d85fef09ac07879232e2f1741f766a9368a2a954"
+  url "https://github.com/woodpecker-ci/woodpecker/archive/refs/tags/v3.15.0.tar.gz"
+  sha256 "14acf419b5a8b349fc73662ac272e238b8492477911ad27c95701f8efbbdc5c2"
   license "Apache-2.0"
   head "https://github.com/woodpecker-ci/woodpecker.git", branch: "main"
 
