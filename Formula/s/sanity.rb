3656b3a101fd6aeca6beacfbf49076009256c64d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-28 22:02:45 +0000

    sanity: add 5.27.0 bottle.

--- Formula/s/sanity.rb
+++ Formula/s/sanity.rb
@@ -5,6 +5,15 @@ class Sanity < Formula
   sha256 "723b1da05828c9ed2e49bbfef14659ea31676a67ce747f9189ee61eec4ccd18e"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "73aee46847c64e39567905bb9cf97bb7c9eac9da3ef8cdaa4b17c82b8345e8bb"
+    sha256 cellar: :any,                 arm64_sequoia: "4f76e67085d48ab11bdcfc5e54f2177401f928240eb549a0d4c2aa74af3a3a92"
+    sha256 cellar: :any,                 arm64_sonoma:  "4f76e67085d48ab11bdcfc5e54f2177401f928240eb549a0d4c2aa74af3a3a92"
+    sha256 cellar: :any,                 sonoma:        "128a115b00e2d40b8e8ba42be0648ee5268c6969eead5bb73ba9f00b2b3b5ad7"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "85783a2314d4fca6f9e82613b1730e9bd559ee0118e761674bebbe3e0e7a3de5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "29d0b66b58c09fc054bacb20cc83abb238e4f384b433afd109350697f49ce576"
+  end
+
   depends_on "node"
 
   def install
