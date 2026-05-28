d84990503be88208a3fbe80dc95397cf86437759
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-28 15:55:46 +0000

    subnetcalc 2.7.0
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/s/subnetcalc.rb
+++ Formula/s/subnetcalc.rb
@@ -1,8 +1,8 @@
 class Subnetcalc < Formula
   desc "IPv4/IPv6 subnet calculator"
   homepage "https://www.nntb.no/~dreibh/subnetcalc/index.html"
-  url "https://github.com/dreibh/subnetcalc/archive/refs/tags/subnetcalc-2.6.6.tar.gz"
-  sha256 "5f33894fac3420d0d906ba7d4e3f7bcfee831707deec08a43d6f2397e30ac486"
+  url "https://github.com/dreibh/subnetcalc/archive/refs/tags/subnetcalc-2.7.0.tar.gz"
+  sha256 "193a5b182642952eecc8aee162324b27b227fd57527b6f71297d428a664674e1"
   license "GPL-3.0-or-later"
   head "https://github.com/dreibh/subnetcalc.git", branch: "master"
 
@@ -17,6 +17,8 @@ class Subnetcalc < Formula
 
   depends_on "cmake" => :build
   depends_on "gettext" => :build
+  depends_on "libidn2"
+  depends_on "libmaxminddb"
 
   on_macos do
     depends_on "gettext"
@@ -37,14 +39,14 @@ class Subnetcalc < Formula
       Broadcast      = not needed on Point-to-Point links
       Wildcard Mask  = 0.0.0.0
       Hex. Address   = 01010101
-      Hosts Bits     = 0
+      Host Bits      = 0
       Max. Hosts     = 1   (2^0 - 0)
       Host Range     = { 1.1.1.1 - 1.1.1.1 }
       Properties     = \
 
          - 1.1.1.1 is a HOST address in 1.1.1.1/32
          - Class A
-      Performing reverse DNS lookup ...\r\e[KDNS Hostname   = one.one.one.one
+      DNS Hostname   = one.one.one.one
     EOS
     assert_equal expected, shell_output("#{bin}/subnetcalc 1.1.1.1/32")
   end
