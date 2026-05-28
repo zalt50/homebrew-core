88583cdfe8c576e8bb56b365476537b1e14d0440
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-28 02:49:34 +0000

    chroma 2.26.0

--- Formula/c/chroma.rb
+++ Formula/c/chroma.rb
@@ -1,8 +1,8 @@
 class Chroma < Formula
   desc "General purpose syntax highlighter in pure Go"
   homepage "https://github.com/alecthomas/chroma"
-  url "https://github.com/alecthomas/chroma/archive/refs/tags/v2.25.0.tar.gz"
-  sha256 "5bae5544762e9c58d1a6eb7b04248576859d866855561e82226020a8dda606a7"
+  url "https://github.com/alecthomas/chroma/archive/refs/tags/v2.26.0.tar.gz"
+  sha256 "1945cbc31165917490ffb400a51aa90b9c726daf0b485369938c963ecbdc565b"
   license "MIT"
   head "https://github.com/alecthomas/chroma.git", branch: "master"
 
