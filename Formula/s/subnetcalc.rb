461e591d37eb75fe54986dd4017857e0d198d31d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-28 21:45:01 +0000

    subnetcalc: update 2.7.0 bottle.

--- Formula/s/subnetcalc.rb
+++ Formula/s/subnetcalc.rb
@@ -7,12 +7,12 @@ class Subnetcalc < Formula
   head "https://github.com/dreibh/subnetcalc.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "4d898fb3ede780ca246054d1675db73ea9f1e959c589cc11b4fb950504db6325"
-    sha256 cellar: :any,                 arm64_sequoia: "e010fe936044545ca967b333b8aa8b070b4cd0bb1f62397e653b5b33bed1b101"
-    sha256 cellar: :any,                 arm64_sonoma:  "76790a61513fbea2c785817d20f3e291dedc4498715d66ec2ab31503cc8a5507"
-    sha256 cellar: :any,                 sonoma:        "7ea1c64b4a61e8bef1d52603169419784eeea83c168c1582c1b74c42c6db80d9"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a1f0c5a7bf7a46c48de87ac1fdc1ae974c8a20468e142f3f97436fa210852666"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ab319c8808024515852af04a7384ff64d16b1668fd34004eb5a4bdb4aaf52248"
+    sha256 cellar: :any,                 arm64_tahoe:   "3076ab0ce037ec2d338a34957f5493381249ebdecf5b0e1c2d677b86199d886b"
+    sha256 cellar: :any,                 arm64_sequoia: "37843dde6199ff2baf60ca1f432cad8c770e6b5479a193184c57a417353dcac5"
+    sha256 cellar: :any,                 arm64_sonoma:  "66d0045104b990b56bae7af2f3c64a7de19397b0c3f73aa26896e071491728da"
+    sha256 cellar: :any,                 sonoma:        "818ec1e44412b726cf100ba48f90849f14d690a0af606cda7c2706cbf2b83ab5"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "b24c92e3f69f966589be34907e6741901b618883a3ba4cf2675ebb6910333051"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e8f46fcaf47c5359d30d61bfcbe70a63ae9afd65edf6e79012fef9f8cc531bfe"
   end
 
   depends_on "cmake" => :build
