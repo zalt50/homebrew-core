028d311bfefb607c61775bf9827537a5057a804e
Michael Cho <michael@michaelcho.dev>
2026-05-27 20:57:44 -0400

    letta-code: remove prebuilts

--- Formula/l/letta-code.rb
+++ Formula/l/letta-code.rb
@@ -14,18 +14,54 @@ class LettaCode < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "3acaea3c7e5b138f67c417ebc7c6928e798a0c4b80bc521ecc775807147c7516"
   end
 
+  depends_on "pkgconf" => :build
+  depends_on "glib"
   depends_on "node"
+  depends_on "ripgrep"
+  depends_on "vips"
+
+  on_macos do
+    depends_on "gettext"
+  end
+
+  resource "node-gyp" do
+    url "https://registry.npmjs.org/node-gyp/-/node-gyp-12.3.0.tgz"
+    sha256 "d209963f2b21fd5f6fad1f6341897a98fc8fd53025da36b319b92ebd497f6379"
+
+    livecheck do
+      url :url
+    end
+  end
 
   def install
     system "npm", "install", *std_npm_args
     bin.install_symlink libexec.glob("bin/*")
 
-    # Remove incompatible pre-built binaries
-    os = OS.kernel_name.downcase
-    arch = Hardware::CPU.intel? ? "x64" : Hardware::CPU.arch.to_s
+    # Remove ripgrep pre-built binaries
     node_modules = libexec/"lib/node_modules/@letta-ai/letta-code/node_modules"
-    (node_modules/"node-pty/prebuilds").glob("*").each do |dir|
-      rm_r(dir) if dir.basename.to_s != "#{os}-#{arch}"
+    rm_r(node_modules.glob("@vscode/ripgrep-*"))
+    rm_r(node_modules/"@vscode/ripgrep") # keeping separate from previous rm_r to fail if missing
+
+    # Replace node-pty pre-built binaries
+    cd node_modules/"node-pty" do
+      rm_r(["prebuilds", "third_party"])
+      system "npm", "run", "install"
+    end
+
+    # Replace sharp pre-built binaries
+    rm_r(node_modules.glob("@img/sharp-*"))
+    resource("node-gyp").stage do
+      system "npm", "install", *std_npm_args(prefix: buildpath/"node-gyp")
+      ENV.append_path "NODE_PATH", buildpath/"node-gyp/lib/node_modules"
+    end
+    cd node_modules/"sharp" do
+      ENV["SHARP_FORCE_GLOBAL_LIBVIPS"] = "1"
+      system "npm", "run", "build"
+      rm_r("src/build/Release/obj.target")
+
+      # help letta.js find source-built sharp
+      sharp = Pathname.pwd.glob("src/build/Release/sharp-*.node").first
+      (node_modules/"@img"/sharp.basename(".node")).install_symlink sharp => "sharp.node"
     end
   end
 
