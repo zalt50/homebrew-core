56488527c8b872debdda8c41cba85c94f097c86b
botantony <antonsm21@gmail.com>
2026-05-29 16:29:35 +0200

    open-babel 3.2.0
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Formula/o/open-babel.rb
+++ Formula/o/open-babel.rb
@@ -1,20 +1,10 @@
 class OpenBabel < Formula
   desc "Chemical toolbox"
   homepage "https://github.com/openbabel/openbabel"
+  url "https://github.com/openbabel/openbabel/archive/refs/tags/openbabel-3-2-0.tar.gz"
+  sha256 "9aadf9f01b3d0ff15d49fcd28d7d76b923218d70bf10f99ea4cc466607f4c7e2"
   license "GPL-2.0-only"
-  revision 5
   head "https://github.com/openbabel/openbabel.git", branch: "master"
-
-  stable do
-    url "https://github.com/openbabel/openbabel/releases/download/openbabel-3-1-1/openbabel-3.1.1-source.tar.bz2"
-    sha256 "a6ec8381d59ea32a4b241c8b1fbd799acb52be94ab64cdbd72506fb4e2270e68"
-
-    # Backport support for configuring PYTHON_INSTDIR to avoid Setuptools
-    patch do
-      url "https://github.com/openbabel/openbabel/commit/f7910915c904a18ac1bdc209b2dc9deeb92f7db3.patch?full_index=1"
-      sha256 "f100bb9bffb82b318624933ddc0027eeee8546bf4d6deda5067ecbd1ebd138ea"
-    end
-  end
 
   bottle do
     rebuild 1
@@ -57,22 +47,6 @@ class OpenBabel < Formula
       -DPYTHON_EXECUTABLE=#{which(python3)}
       -DPYTHON_INSTDIR=#{prefix/Language::Python.site_packages(python3)}
     ]
-
-    if build.stable?
-      # Workaround to build with CMake 4
-      args << "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
-      inreplace "CMakeLists.txt", "cmake_policy(SET CMP0042 OLD)",
-                                  "cmake_policy(SET CMP0042 NEW)"
-
-      # Workaround to build with eigen 5.0.0
-      # Issue ref: https://github.com/openbabel/openbabel/issues/2839
-      args += %W[-DEIGEN3_FOUND=ON -DEIGEN3_INCLUDE_DIR=#{Formula["eigen"].opt_include}/eigen3]
-      inreplace "CMakeLists.txt", "set (CMAKE_CXX_STANDARD 11)", "set (CMAKE_CXX_STANDARD 14)"
-      rm "cmake/modules/FindEigen3.cmake"
-
-      # Fix to error: ‘clock’ was not declared in this scope on Linux
-      inreplace "include/openbabel/obutil.h", "#include <math.h>", "#include <ctime>\n\\0"
-    end
 
     system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
     system "cmake", "--build", "build"
