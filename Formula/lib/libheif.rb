bcff6b0bea7b2dd519ba65b90aa6bf4f0481f898
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-29 18:24:32 +0000

    libheif 1.23.0

--- Formula/lib/libheif.rb
+++ Formula/lib/libheif.rb
@@ -1,8 +1,8 @@
 class Libheif < Formula
   desc "ISO/IEC 23008-12:2017 HEIF file format decoder and encoder"
   homepage "https://www.libde265.org/"
-  url "https://github.com/strukturag/libheif/releases/download/v1.22.2/libheif-1.22.2.tar.gz"
-  sha256 "eea48e4841f83fbe51d029337ffd2d14512d0203015dad40b90213d872958af3"
+  url "https://github.com/strukturag/libheif/releases/download/v1.23.0/libheif-1.23.0.tar.gz"
+  sha256 "4c9182b18897617182eed12ab5eb9f9d855b3aa3a736d6bdb31abc034ec7d393"
   license "LGPL-3.0-or-later"
   compatibility_version 1
 
