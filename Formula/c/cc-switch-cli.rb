a6a85b7f5666036586cfd738a031df3edc4849da
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-29 13:31:28 +0000

    cc-switch-cli: add 5.7.0 bottle.

--- Formula/c/cc-switch-cli.rb
+++ Formula/c/cc-switch-cli.rb
@@ -6,6 +6,15 @@ class CcSwitchCli < Formula
   license "MIT"
   head "https://github.com/SaladDay/cc-switch-cli.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5514ba905951dcc0223a8e4bbc3cd0ace67041b3735924f7dc4e1aff302a2c79"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f35650ad278558d2c1e81e66112ec1369b1cdfb7bdcacb0bd1576e99a1115655"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8de193a93bcb26544b44bebfe73d6df6c310db8a3bb6570fb395866a7b459f9a"
+    sha256 cellar: :any_skip_relocation, sonoma:        "2034c32915c5d3eba875d0fb0aa85992542872068376ee3e5ef5a4438f2d0196"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "81b8d6d3b4a5ab23322ca984657106fe7900d54132c626cd82ad0e948cde6aeb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4ce97802d81c8f56007b491acbd2a099cce1a79046c2f499464b83ae101fefc5"
+  end
+
   depends_on "rust" => :build
 
   def install
