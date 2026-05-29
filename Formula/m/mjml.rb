c46dd90f80fb721fd9e4aa95713c6a5eb05512b0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-29 11:49:05 +0000

    mjml: update 5.3.0 bottle.

--- Formula/m/mjml.rb
+++ Formula/m/mjml.rb
@@ -6,12 +6,7 @@ class Mjml < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8e3f21cbf9b4c1222081c6e2d5560bb8c9cdec32b7b6a38814908807e7f98e05"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ac25931ac32af203b7bc370932e829c2190763a58a1fc9e595407b7d85678652"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ac25931ac32af203b7bc370932e829c2190763a58a1fc9e595407b7d85678652"
-    sha256 cellar: :any_skip_relocation, sonoma:        "f5e1180502aaf7acddafc0c1b99efb489e1e4760f045545d53389eab583a0773"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "9483fcbdf9c823d7df38007564cf43fdd9760194896dc8c7d0735353cc15acc9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9483fcbdf9c823d7df38007564cf43fdd9760194896dc8c7d0735353cc15acc9"
+    sha256 cellar: :any_skip_relocation, all: "cd2dfc0545a9ee0ff33b375c6e0259534182bfdb75c37ae5e0305fbb950fce60"
   end
 
   depends_on "node"
