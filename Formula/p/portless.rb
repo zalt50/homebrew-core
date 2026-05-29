fe1dfde29ade7ad8f8eda35950a2f1c0c32e8819
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-29 00:11:57 +0000

    portless 0.13.1

--- Formula/p/portless.rb
+++ Formula/p/portless.rb
@@ -1,8 +1,8 @@
 class Portless < Formula
   desc "Replace port numbers with stable, named local URLs for humans and agents"
   homepage "https://port1355.dev"
-  url "https://registry.npmjs.org/portless/-/portless-0.13.0.tgz"
-  sha256 "f4dba587b6cc4c117aa069d6b6de917622776492ae98782729fd922a403ea268"
+  url "https://registry.npmjs.org/portless/-/portless-0.13.1.tgz"
+  sha256 "b76a950d9b634408bd3f9e2529cf8c4f0990e906e8ef78b1e1630a9495f92a0b"
   license "Apache-2.0"
 
   bottle do
