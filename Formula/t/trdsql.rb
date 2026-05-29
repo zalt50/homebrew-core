583f3c7371ed2804d9a9c32847a4ff28883b5e59
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-29 07:17:29 +0000

    trdsql 1.2.3

--- Formula/t/trdsql.rb
+++ Formula/t/trdsql.rb
@@ -1,8 +1,8 @@
 class Trdsql < Formula
   desc "CLI tool that can execute SQL queries on CSV, LTSV, JSON, YAML and TBLN"
   homepage "https://github.com/noborus/trdsql"
-  url "https://github.com/noborus/trdsql/archive/refs/tags/v1.2.1.tar.gz"
-  sha256 "58a4b5987e80ed313bf878b070b4d94768fae658f2cf56f353284d762937a84a"
+  url "https://github.com/noborus/trdsql/archive/refs/tags/v1.2.3.tar.gz"
+  sha256 "23a80d1c7cf44f458440ad9b6ffc5dc9305e98f191fbce102f47bf6be2c4fb17"
   license "MIT"
   head "https://github.com/noborus/trdsql.git", branch: "master"
 
