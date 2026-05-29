6f4ee7378988a21b0a3b7c62c79ad7ced32b4544
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-29 15:47:20 +0000

    cozypkg: add 1.4.1 bottle.

--- Formula/c/cozypkg.rb
+++ Formula/c/cozypkg.rb
@@ -6,6 +6,15 @@ class Cozypkg < Formula
   license "Apache-2.0"
   head "https://github.com/cozystack/cozystack.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5badc63774459a2a0bd7add2398fd150f67807e8636206a43f932c80001e95bf"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c5b156912150d7dfe96b1bf7fea29b1259db30c2ebd9e495ba1bb00fe34d564c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2027dc32839d718f49f3d7943412351c540f8cccd9a016b44ecd54fc439508b9"
+    sha256 cellar: :any_skip_relocation, sonoma:        "503df95236611600b253c12d9a222b0a19ff4bf1a01bc1bb5441a0b20309c8a7"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "00aa6d3ccf46fa1b489b363e06cb5325c08edd30a75faa7227c6cc47107f4f77"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1375bc110b1fb87fbfbeca3989b871c765bce1ba8664ff6419229d1767f6ae58"
+  end
+
   depends_on "go" => :build
 
   def install
