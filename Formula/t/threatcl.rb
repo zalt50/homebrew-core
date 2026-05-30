4c45f6f0f2f5a54accd34e024e3904a1b67ff52c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-30 09:00:34 +0000

    threatcl: update 0.4.12 bottle.

--- Formula/t/threatcl.rb
+++ Formula/t/threatcl.rb
@@ -7,12 +7,12 @@ class Threatcl < Formula
   head "https://github.com/threatcl/threatcl.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "aaf7c0fb2038fe21de69fe2cbc7d3cd129acfcfb8911b6341ac3931cfc4609e1"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "aaf7c0fb2038fe21de69fe2cbc7d3cd129acfcfb8911b6341ac3931cfc4609e1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "aaf7c0fb2038fe21de69fe2cbc7d3cd129acfcfb8911b6341ac3931cfc4609e1"
-    sha256 cellar: :any_skip_relocation, sonoma:        "9ca21f41c2db96cbffb8a8e26f0d2ce25bc1a32c6381c4f9971ba9e843b0b8df"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b2fabde00ce9171eaa3916f97c3d05e108932d981d287ef354440d5570d13b3d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ea3fc07411e23e66a11aadeee1b63d41de6dd4caeb1dcd0f816b4242031ef4c5"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ecb516dbffc6da13dbd08c91449238b90c1696fb2142d2a87241432bdf94e538"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ecb516dbffc6da13dbd08c91449238b90c1696fb2142d2a87241432bdf94e538"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ecb516dbffc6da13dbd08c91449238b90c1696fb2142d2a87241432bdf94e538"
+    sha256 cellar: :any_skip_relocation, sonoma:        "b57eb7fc28f1a16825500cfb2bb9c7b0ecd745e2d22981d3edf9379b23863286"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "7267266ef9eea8b17d9027a129a0dd286b7eaf665aae248b994f094b0c6d7b0a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c0b4c6c2428a967fbcd863e72884e6fc7fdb038af3a89ece3c3f15775b68711e"
   end
 
   depends_on "go" => :build
