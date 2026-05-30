2ce5ef6fefec86f65fe4c2b69e59299b14076665
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-30 16:22:42 +0000

    quien: add 0.10.0 bottle.

--- Formula/q/quien.rb
+++ Formula/q/quien.rb
@@ -5,6 +5,15 @@ class Quien < Formula
   sha256 "35bb500f7929ccfc22091a201c35c0e9e660ccd3da383f8b42a4e95530333fbe"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3d460b3269973669cdb01e7013323efaa191e78977f4756c1b4c213961b261ac"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3d460b3269973669cdb01e7013323efaa191e78977f4756c1b4c213961b261ac"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3d460b3269973669cdb01e7013323efaa191e78977f4756c1b4c213961b261ac"
+    sha256 cellar: :any_skip_relocation, sonoma:        "384c69e6a6f3bd6de62f40f6bb9d87bb4522ef1058a4983fedce5e29f6137726"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "108c32294d38248ce3ce4cde2f6327fa2d41c1cdc66b526a438aedd41460d1da"
+    sha256 cellar: :any,                 x86_64_linux:  "813de163d1a8b41f55aa4f2aea52422f57a78ba5a38398640f3c5fa51813459f"
+  end
+
   depends_on "go" => :build
 
   def install
