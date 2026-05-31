6c4f1f5ff4555869764d8ea7c1ebe84e21a76190
Kayla Washburn-Love <kayla@tree.camp>
2026-05-30 22:51:23 -0600

    k0sctl 0.30.1 (new formula)
    k0sctl: use tarball
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    k0sctl: fix tarball version and completions
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/k/k0sctl.rb
@@ -0,0 +1,34 @@
+class K0sctl < Formula
+  desc "Bootstrapping and management tool for k0s clusters"
+  homepage "https://github.com/k0sproject/k0sctl"
+  url "https://github.com/k0sproject/k0sctl/archive/refs/tags/v0.30.1.tar.gz"
+  sha256 "5eb2d2d5702dca0619ccd92646f58eba55347b069fde51ce06845a96a176471c"
+  license "Apache-2.0"
+  head "https://github.com/k0sproject/k0sctl.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    inreplace "version/version.go", "Version = versioninfo.Version", "Version = \"v#{version}\"" if build.stable?
+
+    ldflags = %W[
+      -s -w
+      -X github.com/k0sproject/k0sctl/version.Environment=production
+      -X github.com/carlmjohnson/versioninfo.Revision=#{tap.user}
+      -X github.com/carlmjohnson/versioninfo.Version=v#{version}
+    ]
+    system "go", "build", *std_go_args(ldflags:)
+
+    generate_completions_from_executable(bin/"k0sctl", "completion", "--shell")
+  end
+
+  test do
+    assert_match "version: v#{version}", shell_output("#{bin}/k0sctl version")
+
+    output = shell_output("#{bin}/k0sctl init")
+    assert_match "apiVersion: k0sctl.k0sproject.io/v1beta1", output
+
+    output = shell_output("#{bin}/k0sctl init --cluster-name brew-test")
+    assert_match "name: brew-test", output
+  end
+end
