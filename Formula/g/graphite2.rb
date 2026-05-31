c9d2c6b9af240878f657ea9f94dcb15ad4734065
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-31 23:32:13 +0000

    graphite2 1.3.15
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/g/graphite2.rb
+++ Formula/g/graphite2.rb
@@ -1,8 +1,8 @@
 class Graphite2 < Formula
   desc "Smart font renderer for non-Roman scripts"
   homepage "https://graphite.sil.org/"
-  url "https://github.com/silnrsi/graphite/releases/download/1.3.14/graphite2-1.3.14.tgz"
-  sha256 "f99d1c13aa5fa296898a181dff9b82fb25f6cc0933dbaa7a475d8109bd54209d"
+  url "https://github.com/silnrsi/graphite/releases/download/1.3.15/graphite2-1.3.15.tgz"
+  sha256 "c6bc8b4252724665297f7cad0c55897285c673f9b8e6db3522ace833593fe0b1"
   license any_of: ["GPL-2.0-or-later", "LGPL-2.1-or-later", "MPL-1.1+"]
   head "https://github.com/silnrsi/graphite.git", branch: "master"
 
@@ -23,19 +23,6 @@ class Graphite2 < Formula
   end
 
   def install
-    # CMake: Raised required version to 3.5
-    cmake_policy_files = %w[CMakeLists.txt src/CMakeLists.txt]
-    cmake_files = cmake_policy_files + %w[
-      gr2fonttest
-      tests/bittwiddling
-      tests/json
-      tests/sparsetest
-      tests/utftest
-    ].map { |f| "#{f}/CMakeLists.txt" }
-
-    inreplace cmake_files, "CMAKE_MINIMUM_REQUIRED(VERSION 2.8.0 FATAL_ERROR)", "CMAKE_MINIMUM_REQUIRED(VERSION 3.5)"
-    inreplace cmake_policy_files, "cmake_policy(SET CMP0012 NEW)", ""
-
     args = %W[
       -DCMAKE_INSTALL_RPATH=#{rpath}
     ]
