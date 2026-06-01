a61dd355b97d451ded049310817293b23c60e4f4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-01 07:04:07 +0000

    pgrok 1.6.0

--- Formula/p/pgrok.rb
+++ Formula/p/pgrok.rb
@@ -1,8 +1,8 @@
 class Pgrok < Formula
   desc "Poor man's ngrok, multi-tenant HTTP/TCP reverse tunnel solution"
   homepage "https://github.com/pgrok/pgrok"
-  url "https://github.com/pgrok/pgrok/archive/refs/tags/v1.5.0.tar.gz"
-  sha256 "e0dac3df791e9727c850a18874c7888544b548345b27a1a94914fcfc10f05e9c"
+  url "https://github.com/pgrok/pgrok/archive/refs/tags/v1.6.0.tar.gz"
+  sha256 "98599e07212151639e1acc5817f3d142dba6d533fbbee5c1d3f87d9588e2002d"
   license "MIT"
 
   livecheck do
