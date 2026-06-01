74f7ea7337a6db9c628d824c0a27ff09a8348b5e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-01 13:28:42 +0000

    frpc: update 0.69.1 bottle.

--- Formula/f/frpc.rb
+++ Formula/f/frpc.rb
@@ -7,12 +7,12 @@ class Frpc < Formula
   head "https://github.com/fatedier/frp.git", branch: "dev"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "daa7c0b542d64052fe1ef20793c2b85a2367cecde6b82591c920c2b217a32e4e"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "daa7c0b542d64052fe1ef20793c2b85a2367cecde6b82591c920c2b217a32e4e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "daa7c0b542d64052fe1ef20793c2b85a2367cecde6b82591c920c2b217a32e4e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "cbb9c662db1dd395acca7e5310f653e93b6c099feed114d293c362e7b4fcbced"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "257261456f8d068ba0d4e104a8e43e088ae80fbcac05696a22979093719004b4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "18df22ac3b3516bbeaf1a4912c243deb9539d7820c54bc2c623156bfda5f992a"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a8064f4bcb1eb546fad61c08d6f1a5e7ed8ede0e60fd6cee068abd9c0bd81281"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a8064f4bcb1eb546fad61c08d6f1a5e7ed8ede0e60fd6cee068abd9c0bd81281"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a8064f4bcb1eb546fad61c08d6f1a5e7ed8ede0e60fd6cee068abd9c0bd81281"
+    sha256 cellar: :any_skip_relocation, sonoma:        "66db0f1d8faf08cf0e7f546b0942d7ffe85a0ddcb6c83ca86b061fdea69ccbc7"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "db0262c1196eaadbd820771a1d4b933e425f35243db0bade7d334e9ada65523a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "13d4d04d42a1f97e0fc504a8e7fc0eba35c66a4a3dabcd5f404287ace60bc236"
   end
 
   depends_on "go" => :build
