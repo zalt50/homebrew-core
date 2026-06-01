6256bc06497c981e8278d539aa6ee956676e68ae
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-01 06:25:59 +0000

    act 0.2.89

--- Formula/a/act.rb
+++ Formula/a/act.rb
@@ -1,8 +1,8 @@
 class Act < Formula
   desc "Run your GitHub Actions locally"
   homepage "https://github.com/nektos/act"
-  url "https://github.com/nektos/act/archive/refs/tags/v0.2.88.tar.gz"
-  sha256 "a70c187ee403b53ead14b53296671528787734980b99d6deba029191c69ab131"
+  url "https://github.com/nektos/act/archive/refs/tags/v0.2.89.tar.gz"
+  sha256 "649cd5b91cad870871d2283fb3ad95c8fa1d5ced7a1db8d7b346d1a7dcd3ec71"
   license "MIT"
   head "https://github.com/nektos/act.git", branch: "master"
 
