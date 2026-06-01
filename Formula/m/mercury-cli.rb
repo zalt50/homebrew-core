929d33a7c79c7da60eb98828001ccef385277975
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-01 19:34:44 +0000

    mercury-cli: update 0.9.2 bottle.

--- Formula/m/mercury-cli.rb
+++ Formula/m/mercury-cli.rb
@@ -6,12 +6,12 @@ class MercuryCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f991a32eb64d998beaff9b52899e48c8c804e144f93ff77788c704869723b658"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f991a32eb64d998beaff9b52899e48c8c804e144f93ff77788c704869723b658"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f991a32eb64d998beaff9b52899e48c8c804e144f93ff77788c704869723b658"
-    sha256 cellar: :any_skip_relocation, sonoma:        "a1f04adceb2e453962161c8c534b7f2a6aaecb8eee295c4bdf76128ea7a404a0"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "9f3212c3d3c36aca09dc86eff9f86b0be1e7f8a6ca05046f58bcf4b1a3bf96c3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e3c771b1f1216cd94d30099323cdd0b20a05419ced620fbdcda6baaa78132ea7"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c10e7ec2490824a2c73a8261de2feeddc8be1448466859c5f8740aeb3121b0bd"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c10e7ec2490824a2c73a8261de2feeddc8be1448466859c5f8740aeb3121b0bd"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c10e7ec2490824a2c73a8261de2feeddc8be1448466859c5f8740aeb3121b0bd"
+    sha256 cellar: :any_skip_relocation, sonoma:        "376fa1d6cd002e8c706a53b64136f8dcdf0ff4e5b8ad958d4fa244c4648238dd"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c80670f450f497c6117b4ce7d52b32fb85a0b3b69bc6569a97331b30bd76c99d"
+    sha256 cellar: :any,                 x86_64_linux:  "0eea02c83a516309b3ee0205c868bdacba50ca974f68bb108c8bf422275e0624"
   end
 
   depends_on "go" => :build
