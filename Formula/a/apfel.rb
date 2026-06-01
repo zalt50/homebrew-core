14b28508370e53b978faf3950c2585a36249d2a7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-01 17:26:30 +0000

    apfel: update 1.5.0 bottle.

--- Formula/a/apfel.rb
+++ Formula/a/apfel.rb
@@ -7,7 +7,7 @@ class Apfel < Formula
   head "https://github.com/Arthur-Ficial/apfel.git", branch: "main"
 
   bottle do
-    sha256 arm64_tahoe: "cd633408995cf286f10ba511739d2821997df1f43f1abd3ebae5b61c7adce0e8"
+    sha256 arm64_tahoe: "2de12d4f64d43264edec4df5bd29bbcead7920276c0f6895b7f9779d9092c190"
   end
 
   depends_on xcode: ["26.4", :build]
