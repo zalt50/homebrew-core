bbb5bcf42ffb644d5265e10c50274a42c74e9c65
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-02 04:26:27 +0000

    webkitgtk: update 2.52.4 bottle.

--- Formula/w/webkitgtk.rb
+++ Formula/w/webkitgtk.rb
@@ -11,8 +11,8 @@ class Webkitgtk < Formula
   end
 
   bottle do
-    sha256 arm64_linux:  "6b58e9d66ef40f21045160438479eae88d320db458f647e6932db2438e11d202"
-    sha256 x86_64_linux: "60c486132493d2dde50c75eaaf8d896502d55daea08dc2df4179f06ae624153c"
+    sha256 arm64_linux:  "cf5b4e8dfddeac150803cbf12523f9787c0eec9767ba36cce97f9cf8f8a175be"
+    sha256 x86_64_linux: "73fb4d2c20b752d92f9a7c74c467ab482e765fe1d84e7116c9f2e222660f07ee"
   end
 
   depends_on "cmake" => :build
