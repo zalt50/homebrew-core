057ac4b30ad08d2aab7f1f29f14eb4e97b5480dc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-02 02:53:17 +0000

    dalfox 3.0.1

--- Formula/d/dalfox.rb
+++ Formula/d/dalfox.rb
@@ -1,8 +1,8 @@
 class Dalfox < Formula
   desc "XSS scanner and utility focused on automation"
   homepage "https://dalfox.hahwul.com"
-  url "https://github.com/hahwul/dalfox/archive/refs/tags/v3.0.0.tar.gz"
-  sha256 "277b98f2d2f75380292d8888d0f3e88d87b0a35dcfb510f3be5a17cb4d3a4186"
+  url "https://github.com/hahwul/dalfox/archive/refs/tags/v3.0.1.tar.gz"
+  sha256 "ffd6c0ebb576c80150a0abfeb1830f84e8254ff2089e4617bb1748a91754e590"
   license "MIT"
   head "https://github.com/hahwul/dalfox.git", branch: "main"
 
@@ -25,7 +25,7 @@ class Dalfox < Formula
     assert_match version.to_s, shell_output("#{bin}/dalfox -V 2>&1")
 
     url = "https://pentest-ground.com:4280/vulnerabilities/xss_r/"
-    output = shell_output("#{bin}/dalfox scan \"#{url}\" 2>&1", 1)
+    output = shell_output("#{bin}/dalfox scan \"#{url}\" 2>&1")
     assert_match "scan completed", output
   end
 end
