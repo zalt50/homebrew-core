37ea58c58ba9c085ab5fbbd2a0ced0dd2d95d4d2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-03 09:23:34 +0000

    bnd 7.3.0

--- Formula/b/bnd.rb
+++ Formula/b/bnd.rb
@@ -1,8 +1,8 @@
 class Bnd < Formula
   desc "Swiss Army Knife for OSGi bundles"
   homepage "https://bnd.bndtools.org/"
-  url "https://search.maven.org/remotecontent?filepath=biz/aQute/bnd/biz.aQute.bnd/7.2.3/biz.aQute.bnd-7.2.3.jar"
-  sha256 "0122ea026c24285fdff48c5f523bc83750956bb920c12615c497f3d1c175f08b"
+  url "https://search.maven.org/remotecontent?filepath=biz/aQute/bnd/biz.aQute.bnd/7.3.0/biz.aQute.bnd-7.3.0.jar"
+  sha256 "b41c8b09f9f0d8a076c0ea496f393a5043eded15cafdb30541b44c45db9146af"
   license any_of: ["Apache-2.0", "EPL-2.0"]
 
   livecheck do
