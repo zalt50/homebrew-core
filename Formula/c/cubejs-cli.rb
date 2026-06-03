51608454d1426d8c3ead6bafbfe60c4243ac590a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-03 18:42:37 +0000

    cubejs-cli: update 1.6.53 bottle.

--- Formula/c/cubejs-cli.rb
+++ Formula/c/cubejs-cli.rb
@@ -6,12 +6,12 @@ class CubejsCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "73a52ed57e7777721a6cdc1bf325b20b97a5f9d97b422273895e3d7076d51b3c"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7983babcb4536a52df5fd3f922d75242131b09e2adb640d51341fe87530d6580"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7983babcb4536a52df5fd3f922d75242131b09e2adb640d51341fe87530d6580"
-    sha256 cellar: :any_skip_relocation, sonoma:        "e500c5ed6ec423796a502377cea9059bed78964221ae282d21cbdf828c6fa5d9"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d716a8473a71c60420afe270f507abf70b72dfe08cf6a159762932df222383c3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d716a8473a71c60420afe270f507abf70b72dfe08cf6a159762932df222383c3"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d44971ea4969929b8c33f2a33c1ca38ee5f1613dc36d79dae675beb5b91d6f23"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "346ee067fe04a1b19ccf7b2c434f7c0a280656fb408a8278f8584900e98cc9d7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "346ee067fe04a1b19ccf7b2c434f7c0a280656fb408a8278f8584900e98cc9d7"
+    sha256 cellar: :any_skip_relocation, sonoma:        "0c8f6377e45d63290a4a7b960d90cff916e609be6d36fce16ca15daae4c69112"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f28ffa455357ba1267057753557c87d824fbdbc6517208b0c960425799543919"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f28ffa455357ba1267057753557c87d824fbdbc6517208b0c960425799543919"
   end
 
   depends_on "node"
