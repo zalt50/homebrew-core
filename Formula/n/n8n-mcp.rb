7aa91738bab0982cf7b300fc780fb23d9d10ac4e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-03 13:25:05 +0000

    n8n-mcp: update 2.56.1 bottle.

--- Formula/n/n8n-mcp.rb
+++ Formula/n/n8n-mcp.rb
@@ -6,7 +6,7 @@ class N8nMcp < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "6f4513dfad6539312811acae9f3065840ad454858da1f24736777b880ee15be7"
+    sha256 cellar: :any_skip_relocation, all: "b2020e0f031506c39c59ff19b45adc2b03b92ad93589374c19dbe4614a1690d6"
   end
 
   depends_on "node"
