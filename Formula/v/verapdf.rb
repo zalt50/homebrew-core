9274bd2b771a600e9946468cd9ba462af4737005
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-03 13:18:28 +0000

    verapdf 1.30.2

--- Formula/v/verapdf.rb
+++ Formula/v/verapdf.rb
@@ -1,8 +1,8 @@
 class Verapdf < Formula
   desc "Open-source industry-supported PDF/A validation"
   homepage "https://verapdf.org/home/"
-  url "https://github.com/veraPDF/veraPDF-apps/archive/refs/tags/v1.30.1.tar.gz"
-  sha256 "5efc41110eba2f5fb9ff13511fcf058cbecd9ee1c8e9f6fe66b8806aaac32c37"
+  url "https://github.com/veraPDF/veraPDF-apps/archive/refs/tags/v1.30.2.tar.gz"
+  sha256 "77dfc85544b83784ba4a9d2807bdb3f194b0c22d83904ecf222054ddf10a5347"
   license any_of: ["GPL-3.0-or-later", "MPL-2.0"]
   head "https://github.com/veraPDF/veraPDF-apps.git", branch: "integration"
 
