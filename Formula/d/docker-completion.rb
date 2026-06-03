77fb1233faa2c7dc3a913e95ce518daddbf16bce
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-03 20:55:32 +0000

    docker-completion 29.5.3

--- Formula/d/docker-completion.rb
+++ Formula/d/docker-completion.rb
@@ -1,8 +1,8 @@
 class DockerCompletion < Formula
   desc "Bash, Zsh and Fish completion for Docker"
   homepage "https://www.docker.com/"
-  url "https://github.com/docker/cli/archive/refs/tags/v29.5.2.tar.gz"
-  sha256 "e5cf80d9e0b6630bb2e97decf664ad5f0c732b296b7c47030044c3a7287bbddb"
+  url "https://github.com/docker/cli/archive/refs/tags/v29.5.3.tar.gz"
+  sha256 "7608d82f33ce0ebbbed5bf8f6997319375c30a46d325496f3e8974c9a0c8ce6b"
   license "Apache-2.0"
   compatibility_version 1
   head "https://github.com/docker/cli.git", branch: "master"
