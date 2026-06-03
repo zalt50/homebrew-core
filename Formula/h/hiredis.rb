43634b65c1c06d0e29b89df835925e4e51e3eb14
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-03 15:57:53 +0000

    hiredis 1.4.0

--- Formula/h/hiredis.rb
+++ Formula/h/hiredis.rb
@@ -1,10 +1,10 @@
 class Hiredis < Formula
   desc "Minimalistic client for Redis"
   homepage "https://github.com/redis/hiredis"
-  url "https://github.com/redis/hiredis/archive/refs/tags/v1.3.0.tar.gz"
-  sha256 "25cee4500f359cf5cad3b51ed62059aadfc0939b05150c1f19c7e2829123631c"
+  url "https://github.com/redis/hiredis/archive/refs/tags/v1.4.0.tar.gz"
+  sha256 "5fa6e719e59cd4f8ae435c52a18ac4035d135251f9ee54e7a045bccf59107ed8"
   license "BSD-3-Clause"
-  compatibility_version 1
+  compatibility_version 2
   head "https://github.com/redis/hiredis.git", branch: "master"
 
   bottle do
