18ecd1e7283092de2abb3817b2179362b134f8fd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-04 23:03:49 +0000

    vim-classic: add 8.3.0 bottle.

--- Formula/v/vim-classic.rb
+++ Formula/v/vim-classic.rb
@@ -5,6 +5,15 @@ class VimClassic < Formula
   sha256 "6e1c97c8269e9354bbc474f0efa7e1e0b23fcdb6075067474d731a9bfac6e8ef"
   license "Vim"
 
+  bottle do
+    sha256               arm64_tahoe:   "7bce18cd76b55002dddd04c9eb9ac47fdfc1a9c92147854455165959bb15ab53"
+    sha256               arm64_sequoia: "bd0739551806f358d2dfdebb5a168fd2f9759c8219c72bc305e323b4147e2225"
+    sha256               arm64_sonoma:  "b67e80a92a1c36f06c3d05f3f004b46b9883d2239ead12c1f877e5ce7d347c8d"
+    sha256 cellar: :any, sonoma:        "ef1d55aac7aa8a441e56f1fc58f9a7aa18153c59f792cd862c38b979066e69bb"
+    sha256               arm64_linux:   "e5f40a11cd33362be0cbe6eac7fa377b9a509b351c085a596548c25adccb7e50"
+    sha256               x86_64_linux:  "8842d6b9aaf6401bdaac782ffff54b17a56819998d4b3559b9249878ac9c07f2"
+  end
+
   # Homebrew Ruby installs (4.x) currently cause a linker error with vim-classic,
   # so we don't try building in Ruby interpreter support.
   depends_on "gettext" => :build
