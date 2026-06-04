e68d6f515a72b061eb0c20a375691cc3b7c2264e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-04 09:37:34 +0000

    filebrowser: update 2.63.11 bottle.

--- Formula/f/filebrowser.rb
+++ Formula/f/filebrowser.rb
@@ -7,12 +7,12 @@ class Filebrowser < Formula
   head "https://github.com/filebrowser/filebrowser.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "38a5456fde41efc8311159d575aee525a1ce2aec5f68283f62bbabcf6e33cf92"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7a7f4cf84bd52cd9296942db15ed49261f6ec7ec443c050cbe600d0dbc2b551e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a14bda7ae5079687932d9c98457ed6b55c8d50a8cbf79a534f958178f69e66b5"
-    sha256 cellar: :any_skip_relocation, sonoma:        "ad4adfcda9844e439d80ac2d7423cf511b51ecb7227917c33eb5b76e0c5a981f"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "90583aa8242bd2d4e39c1a767c5239f9d956cd8ee8b22ee36c0ce23599a8d72f"
-    sha256 cellar: :any,                 x86_64_linux:  "1bec04592bfe262c0627819644b15c825a75880e64995e379d47f81469eb8a56"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "91fc88dbf8e5c8aecf1f1cf982811eedfa1f7a9d802a6a7057721bddfa8ac94b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e77fd6b593ecddbf748daa7a5504aba369ebd1d0af085b852c973b8cf61df838"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a0ace5458bb2c764efd47d29e9eb89e7b59f99120766ea6b1eec0ecb6d5b124b"
+    sha256 cellar: :any_skip_relocation, sonoma:        "34bf57f4bbedf39066a4ea93e40149677bdce7cc3afe3abea024ec61fbb28d4b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a8ef91a6881c39bb748466091cee93b143503117b5887ef8f9a1e9327b62c3db"
+    sha256 cellar: :any,                 x86_64_linux:  "acf3582b3e0e31961c36c2df2742a3e6b7215012c94636b8bd0fe389c5eb49d9"
   end
 
   depends_on "go" => :build
