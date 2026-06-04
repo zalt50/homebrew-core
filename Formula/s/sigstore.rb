5880b4115b5241865edad82762c15ff707c4bd25
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-04 17:27:02 +0000

    sigstore: update 4.3.0 bottle.

--- Formula/s/sigstore.rb
+++ Formula/s/sigstore.rb
@@ -9,12 +9,12 @@ class Sigstore < Formula
   head "https://github.com/sigstore/sigstore-python.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "a38bd1a6ccec26500709fc5c7cb2830b10a58980ac6bc17ea25c696cbce5a3a7"
-    sha256 cellar: :any, arm64_sequoia: "36ee929d7166c0f3ce2fe4f7f1b7f88b35e64cca4442a016b65a38c62c31fde0"
-    sha256 cellar: :any, arm64_sonoma:  "27b48b21ea9904d6034224704f9d46659a171463404360c25b13b19fbcf12242"
-    sha256 cellar: :any, sonoma:        "b9f98c3c7d53c809e6c44ea282cdfb2afbf2c15237f7f0c12ad39f1e92765f88"
-    sha256 cellar: :any, arm64_linux:   "da59577a032d927a43f28ac4fb6d86402973a01d604e9378f45406f3409d3a64"
-    sha256 cellar: :any, x86_64_linux:  "dcabbeaf60f1beb47148eeea7dea4e32cbfd7295bd77f5a69ab0570976659868"
+    sha256 cellar: :any, arm64_tahoe:   "2c0d8ef7fe6e8dfe60f8fd09e6ee6cd29d0a2e6e8673f2f8f313b5db6a07978f"
+    sha256 cellar: :any, arm64_sequoia: "91812308c0666b81be26be63ebbc47583c6f36bee290494fe52bcdfdd355ad42"
+    sha256 cellar: :any, arm64_sonoma:  "c46b81f8420051a512d26da531ab76d6d89016fe897dae777bc4908fa37d3384"
+    sha256 cellar: :any, sonoma:        "af5cc3e31483db531938869018a940eede925801e57c7fdaf8fb77f40cbdea41"
+    sha256 cellar: :any, arm64_linux:   "f2890d19baa1e3c4c726af2f3a80c43e7718a7dfa6efc0eedd1885b094efcc66"
+    sha256 cellar: :any, x86_64_linux:  "5992a2c69374f3409f0619d4f69db5b6241b198df656f581f4bdcac728633e75"
   end
 
   depends_on "pkgconf" => :build # for rfc3161-client
