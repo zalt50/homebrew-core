a0f75b2e16ec4d574e0a5a92848c63f7f68f5269
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-04 22:31:19 +0000

    harsh: update 0.14.5 bottle.

--- Formula/h/harsh.rb
+++ Formula/h/harsh.rb
@@ -7,12 +7,12 @@ class Harsh < Formula
   head "https://github.com/wakatara/harsh.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "722b25444ab490ee72bcbffacdb4241e213563fc8f3ee23f6d18fba1de9b2c53"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "722b25444ab490ee72bcbffacdb4241e213563fc8f3ee23f6d18fba1de9b2c53"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "722b25444ab490ee72bcbffacdb4241e213563fc8f3ee23f6d18fba1de9b2c53"
-    sha256 cellar: :any_skip_relocation, sonoma:        "6771e155e8bf2bebd401872271fb49ad0811ea843330274673aca2e70f5c309b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "867fe201539c71557a9d6beb72c53b52c04ef17898c4b641cd10d49fc06734a4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c4d26fc4007bc869952ecd34763eacf719dff9483c53f4560fc79e515cb7a906"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "719d5448ca9017772ec460c7f625bea82fd5a907375bd58ad2a07b3b7396e18a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "719d5448ca9017772ec460c7f625bea82fd5a907375bd58ad2a07b3b7396e18a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "719d5448ca9017772ec460c7f625bea82fd5a907375bd58ad2a07b3b7396e18a"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a2a01c719ab02ec0ca66eaacda19efe97697960ba881be85b15d622b01b3caa7"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "b4706f0b4f15463c04498a90168bb19c973e03b73e65d1d10391a9d3985bb33e"
+    sha256 cellar: :any,                 x86_64_linux:  "418289bd491051962c37e1325da682c8b128845341518fdf4335a0e8d3b43110"
   end
 
   depends_on "go" => :build
