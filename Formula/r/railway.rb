5ae62597b52f00dcb3238ffac1ccc16fbbc25270
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-04 17:16:29 +0000

    railway: update 5.0.0 bottle.

--- Formula/r/railway.rb
+++ Formula/r/railway.rb
@@ -7,12 +7,12 @@ class Railway < Formula
   head "https://github.com/railwayapp/cli.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e70ca625f7731a51c166e3a88667726731c35cd25f9a9277048b68edf6991a27"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "110541505255946bb79e1425958bda4d3a6eb0e0a7ef250a33f0688f8167686d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1821ddd1d2a4b623582914f41f8361526488eb940cd33a9664b9f4e413688064"
-    sha256 cellar: :any_skip_relocation, sonoma:        "12c2545db06e6ef35593be799a6f9b4cce92a6eeb6efa8680beb5933cc154376"
-    sha256 cellar: :any,                 arm64_linux:   "4333c9eee3723a645817f0ed4d6cec379fba2c5643d0cc96c415b1936ad161c6"
-    sha256 cellar: :any,                 x86_64_linux:  "5d67f2ff763144ea9df23c608b43560fb1d8c6b7296822ef1b8b7c512889a252"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "221b1088e7c3b2254dbfa97319aa8276b4a290254186ed7315caf1a0f6da07f4"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "483725079d4bbd55e27323adf9a5460e55cac426e5506d8f713a8b0ff5044449"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "954cc50cd432377c1762248bcdfce8820ce941e6a50ed669aa75b363897a91ba"
+    sha256 cellar: :any_skip_relocation, sonoma:        "69dee4e26913a2525f691499bf0045dd57e3eb6fc835010f07ce51330b7803b9"
+    sha256 cellar: :any,                 arm64_linux:   "2df59e97e1f6b0574e884897b1b467bd8a4c9c34e09b7a8f06aaf8b8444e01d5"
+    sha256 cellar: :any,                 x86_64_linux:  "71122bafc1f76ce6cd75fa8caf031c643f72fe5c10d161c29d871455fe2944d9"
   end
 
   depends_on "rust" => :build
