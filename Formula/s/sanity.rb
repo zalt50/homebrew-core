6de2e3655887e168dad280c7e87155768d01d940
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-04 19:31:26 +0000

    sanity: update 6.7.2 bottle.

--- Formula/s/sanity.rb
+++ Formula/s/sanity.rb
@@ -6,12 +6,12 @@ class Sanity < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "ea7512d4cd8a237c053d6c39a2cf597c60d6587f06426a9390934186516bd087"
-    sha256 cellar: :any, arm64_sequoia: "a5299bdc99172dd27d106dabc13c5f5f6f0e882fc376460fec35952217e8af71"
-    sha256 cellar: :any, arm64_sonoma:  "a5299bdc99172dd27d106dabc13c5f5f6f0e882fc376460fec35952217e8af71"
-    sha256 cellar: :any, sonoma:        "5ad164961c4d0c3f9bd787190437976738db8ef28464082316fb86f72ef2a4f7"
-    sha256 cellar: :any, arm64_linux:   "655c88e71328eb970351d37178667db2eff4a2e2dfae6499835b24c7dac0ab79"
-    sha256 cellar: :any, x86_64_linux:  "1577806cc326fb887e259e3972a21753dc29124cd02c49eeb80df4d4af6bbb0b"
+    sha256 cellar: :any, arm64_tahoe:   "5839215f3f470a72152f48ab027e78b4fbea81d003cec7ce8ac92831d6d9545e"
+    sha256 cellar: :any, arm64_sequoia: "7de3190495ecb002eb25795547552b5e1daf3c475ac7f8ffc3b61a69b1e2ad9f"
+    sha256 cellar: :any, arm64_sonoma:  "7de3190495ecb002eb25795547552b5e1daf3c475ac7f8ffc3b61a69b1e2ad9f"
+    sha256 cellar: :any, sonoma:        "bf4a3b50c7713a0288bfa1943ad1d0a648e5a9fd6f5e7bb45af1fcae33b022f9"
+    sha256 cellar: :any, arm64_linux:   "0065b8751040f5a11975bb37c7f05b1c8ddd36e378b956e5e4ff75b7dcbd5b30"
+    sha256 cellar: :any, x86_64_linux:  "1c97702cedcdb1775600f6a9d2236d3c5eb78cafaad05efd4b5ce6dc95e60eb5"
   end
 
   depends_on "node"
