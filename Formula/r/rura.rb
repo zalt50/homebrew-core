72c0648c052b64be91aac0d44666923963c024eb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-05 08:00:40 +0000

    rura: update 1.4.0 bottle.

--- Formula/r/rura.rb
+++ Formula/r/rura.rb
@@ -6,12 +6,12 @@ class Rura < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "fc270818a4eb2ec98936224475852be5aaac2177edc12f10f21a0228ec8e5129"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1e48e4105ced6a9ee7aa8c55a3be7212a72610dd856257ffb6aa7bf96e59d28d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "840e7112a466bd0104d84bb340dbfdec591bbb226aa27b236b7b0a8a8401c5eb"
-    sha256 cellar: :any_skip_relocation, sonoma:        "3190c9be710065cdcbd336664bf6a0756e530cbc8991841e790dc937d73cc997"
-    sha256 cellar: :any,                 arm64_linux:   "a88f4694db9aef6f9e7b52923673acfc490033faace669c9a5f9bd2aea841f78"
-    sha256 cellar: :any,                 x86_64_linux:  "27b2ec5f5c149a0baf8aabd70de382d3b12e105689b6bb2e383365e00f7408b3"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f78694abc69f739ab72ed448c73400a2ebb17876724eeae5ee62c209ee319a98"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c9d53031e8429506a407889e1a3f28590124184c8504825c1c850fa5c6678409"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f1e018612b98799b4fbac76f92636eec7e7174b7f0a84b051bf6fe42de276639"
+    sha256 cellar: :any_skip_relocation, sonoma:        "bf819fb22fb8b79a128101bc5cc166269a8a7a9ec493c6d35691dcfa2964bf96"
+    sha256 cellar: :any,                 arm64_linux:   "0b056c6aa982eec17d9b181b3d0674eb8a7d5e204bc6eb7384cccc295802c7f3"
+    sha256 cellar: :any,                 x86_64_linux:  "3548006b0582cede1b42d1eaa2eca375c42232429d94c4792a6b3013683aea4c"
   end
 
   depends_on "rust" => :build
