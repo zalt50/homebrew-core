8cbaa9746068b15c4ed1e8e05e1e3b3ea9c8f8d5
Anton Melnikov <antonsm21@gmail.com>
2026-06-05 11:35:22 +0000

    renovate 43.213.0

--- Formula/r/renovate.rb
+++ Formula/r/renovate.rb
@@ -1,8 +1,8 @@
 class Renovate < Formula
   desc "Automated dependency updates. Flexible so you don't need to be"
   homepage "https://github.com/renovatebot/renovate"
-  url "https://registry.npmjs.org/renovate/-/renovate-43.212.0.tgz"
-  sha256 "7a56c1bd91a95fdd6b9980d697eb294ad741a42d40a6d56feea615e67a45579c"
+  url "https://registry.npmjs.org/renovate/-/renovate-43.213.0.tgz"
+  sha256 "f02ee82b5a954eaf1ca67674b83229b07e9685453eaab7faa7997d4397657cf4"
   license "AGPL-3.0-only"
 
   # livecheck needs to surface multiple versions for version throttling but
