14ddebd48c25f2885d96fd7e2eb19a0c0ea57b6d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-05 16:22:23 +0000

    rumdl: update 0.2.8 bottle.

--- Formula/r/rumdl.rb
+++ Formula/r/rumdl.rb
@@ -11,12 +11,12 @@ class Rumdl < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4f6114fe018c3d1f191feec8704222160d926736d507b8c05d1fd1cdb677d710"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "83774f117df94320b20b769d975327e4fddf8111b35fe36d4de94c0af3106353"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6930db68d965bd925836f0f69b0c96139f489f9cfb235a8c05b18bcf652a1b68"
-    sha256 cellar: :any_skip_relocation, sonoma:        "fa92fb3bdbf12456ddd3266e95c0a3b91e97de5d9cb5510900936d8e76607b46"
-    sha256 cellar: :any,                 arm64_linux:   "f3ab0a9b5c91890ed8dfba140eb5fc5607c5252830be7d416c9c431136cd3386"
-    sha256 cellar: :any,                 x86_64_linux:  "a252633ce5939cd5729cf713e3a900d29c8dc908ae65cc6cf8ccac21b55ba71a"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2d1f65b3bdaeffd757d3e7ad04bf3d61330e817b28fbeb85811be05ef82c8349"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6766a5c16049ba6f69003d9b3e7177f3cc0acd5dac6be74bef5925293d8fcf8d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4bc8a44f77c295e1e5ef5c45878eed2e5fe55530a8876ec051ab366a4e7d7a4a"
+    sha256 cellar: :any_skip_relocation, sonoma:        "92e60d3666de3a2788ebe55d57783db7800917251fd85cb7d0132161306a9466"
+    sha256 cellar: :any,                 arm64_linux:   "2c987676ae3b1bf7bc3551c1838339310fc4040f255eac70338084bd86ceba42"
+    sha256 cellar: :any,                 x86_64_linux:  "a31f012cbca04015097fdd8fa27eb8d481a4b5127d34a9c204aa4ef7b6fcf003"
   end
 
   depends_on "rust" => :build
