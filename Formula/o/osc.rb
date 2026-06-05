d3f2f617a2ed66e2f5e6195a075ebfb743878e86
Anton Melnikov <antonsm21@gmail.com>
2026-06-05 11:29:22 +0000

    osc 1.27.1

--- Formula/o/osc.rb
+++ Formula/o/osc.rb
@@ -3,8 +3,8 @@ class Osc < Formula
 
   desc "Command-line interface to work with an Open Build Service"
   homepage "https://openbuildservice.org"
-  url "https://files.pythonhosted.org/packages/eb/2d/af6a0c3a51f0533a79ff1de4b1772f49eae828cfe9d31f3d094b7683126d/osc-1.27.0.tar.gz"
-  sha256 "879db8bbf49ca28fbb78018a3b1d44b266b83ebf6430550075ba2ac3c5b9e6c7"
+  url "https://files.pythonhosted.org/packages/d4/78/5fc9cc5ab36b0136cc54e727aed100ff3f1bed27503f375749dd037d4537/osc-1.27.1.tar.gz"
+  sha256 "ad8f7ec2b8be6001a47a2685552e81e83dfc2abb342d353b87309860fb473934"
   license "GPL-2.0-or-later"
   head "https://github.com/openSUSE/osc.git", branch: "master"
 
