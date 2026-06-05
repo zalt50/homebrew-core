25d8432850916d9771cecf7f039febfab7706a45
Anton Melnikov <antonsm21@gmail.com>
2026-06-05 11:27:05 +0000

    nom 3.3.2

--- Formula/n/nom.rb
+++ Formula/n/nom.rb
@@ -1,8 +1,8 @@
 class Nom < Formula
   desc "RSS reader for the terminal"
   homepage "https://github.com/guyfedwards/nom"
-  url "https://github.com/guyfedwards/nom/archive/refs/tags/v3.3.1.tar.gz"
-  sha256 "84d5732d6725af5536ef07827427a14932de4a69d6bd6e0d3e7dbbea6c356bf1"
+  url "https://github.com/guyfedwards/nom/archive/refs/tags/v3.3.2.tar.gz"
+  sha256 "b462d343e81f1382f38a9bd829be38f6b89ed2457f7bdbac2f8849078b9b094d"
   license "GPL-3.0-only"
   head "https://github.com/guyfedwards/nom.git", branch: "master"
 
