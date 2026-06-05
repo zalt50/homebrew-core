9816c5ec6149ef2b83ed4fc82d1dbf26412db10a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-05 18:22:32 +0000

    infracost: update 2.2.6 bottle.

--- Formula/i/infracost.rb
+++ Formula/i/infracost.rb
@@ -12,12 +12,12 @@ class Infracost < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e02ba691b874604fcc3b9542dc1e0c4471401591bd8f7348681cde652b824e83"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e02ba691b874604fcc3b9542dc1e0c4471401591bd8f7348681cde652b824e83"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e02ba691b874604fcc3b9542dc1e0c4471401591bd8f7348681cde652b824e83"
-    sha256 cellar: :any_skip_relocation, sonoma:        "a6bef01f880485ecaeefb52fc12a8d118949ca932717e5ab001b31021ef21cb8"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b522f9d7e68658cc2eb4eeac8790548d07d556a1000e313fd715c88f9be9a322"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7dd72e6162135c80867b05056415e043a6eca20206518db7da4d9cc244a71903"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "92ef0080c6be1d27d5fc0faf307b8dd1767b111f271b82a30c1721f112ed5755"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "92ef0080c6be1d27d5fc0faf307b8dd1767b111f271b82a30c1721f112ed5755"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "92ef0080c6be1d27d5fc0faf307b8dd1767b111f271b82a30c1721f112ed5755"
+    sha256 cellar: :any_skip_relocation, sonoma:        "7bb7f5d7141a7d461b7c4bf3dac8a16157bdd2c2619f778a4cd7da243abbbc89"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "6217aa08f65ea440e96373d0345c28caac3a1c72d68318d3ed060096d31aaa41"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "46de66732cadff19314d1f1d402166347991e2b1867fa44a56a491f8a5f4ba15"
   end
 
   depends_on "go" => :build
