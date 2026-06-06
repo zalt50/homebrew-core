b31cd96f6a83b6b3f02145276a16fc03db103a20
Anton Melnikov <antonsm21@gmail.com>
2026-06-06 19:50:12 +0000

    cargo-shear 1.13.1

--- Formula/c/cargo-shear.rb
+++ Formula/c/cargo-shear.rb
@@ -1,8 +1,8 @@
 class CargoShear < Formula
   desc "Detect and remove unused dependencies from `Cargo.toml` in Rust projects"
   homepage "https://github.com/Boshen/cargo-shear"
-  url "https://github.com/Boshen/cargo-shear/archive/refs/tags/v1.13.0.tar.gz"
-  sha256 "f8245a9eabd0ea457334d7638f902ce0e40f93dc91dd1255bf43d52b7e3c675f"
+  url "https://github.com/Boshen/cargo-shear/archive/refs/tags/v1.13.1.tar.gz"
+  sha256 "3da17c403b45d4a4c1f0547d8f1ee459465510c636f22a59190e449c69fc3352"
   license "MIT"
   head "https://github.com/Boshen/cargo-shear.git", branch: "main"
 
