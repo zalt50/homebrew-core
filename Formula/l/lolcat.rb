12f9a9a54837659ce182590513aa9ced1dafb8ce
Michael Cho <michael@michaelcho.dev>
2026-06-06 10:21:06 -0400

    lolcat: add resources to lock versions
    Also use brew Ruby

--- Formula/l/lolcat.rb
+++ Formula/l/lolcat.rb
@@ -22,12 +22,33 @@ class Lolcat < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "8110115e78f3bcb1aca5f3aee73cb81726e59a1293236a780ce99af733a8f524"
   end
 
-  uses_from_macos "ruby"
+  depends_on "ruby"
+
+  # List with `gem install --explain lolcat -v #{version}`
+  resource "manpages" do
+    url "https://rubygems.org/gems/manpages-0.6.1.gem"
+    sha256 "cdbad16823c8510c15a93d4cdbd46e7b4290aff8b10f3d4b70caa8e62c8de686"
+  end
+
+  resource "optimist" do
+    url "https://rubygems.org/gems/optimist-3.0.1.gem"
+    sha256 "336b753676d6117cad9301fac7e91dab4228f747d4e7179891ad3a163c64e2ed"
+  end
+
+  resource "paint" do
+    url "https://rubygems.org/gems/paint-2.3.0.gem"
+    sha256 "327d623e4038619d5bd99ae5db07973859cd78400c7f0329eea283cef8e83be5"
+  end
 
   def install
     ENV["GEM_HOME"] = libexec
+
+    resources.each do |r|
+      system "gem", "install", r.cached_download, "--ignore-dependencies", "--no-document", "--install-dir", libexec
+    end
     system "gem", "build", "lolcat.gemspec"
-    system "gem", "install", "lolcat-#{version}.gem"
+    system "gem", "install", "--ignore-dependencies", "lolcat-#{version}.gem"
+
     bin.install libexec/"bin/lolcat"
     bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
     man6.install "man/lolcat.6"
