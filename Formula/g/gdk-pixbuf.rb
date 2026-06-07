8d94f30340743c97a0e1b990cd7ea5a66553c77a
Michael Cho <michael@michaelcho.dev>
2026-06-07 19:34:57 -0400

    gdk-pixbuf: build with HOMEBREW_PREFIX paths
    This uses a staging directory via DESTDIR mechanism which allows us to
    avoid inreplace patching and to switch to post_install_steps

--- Formula/g/gdk-pixbuf.rb
+++ Formula/g/gdk-pixbuf.rb
@@ -4,6 +4,7 @@ class GdkPixbuf < Formula
   url "https://download.gnome.org/sources/gdk-pixbuf/2.44/gdk-pixbuf-2.44.6.tar.xz"
   sha256 "140c2d0b899fcf853ee92b26373c9dc228dbcde0820a4246693f4328a27466fa"
   license "LGPL-2.1-or-later"
+  revision 1
   compatibility_version 1
 
   bottle do
@@ -34,24 +35,13 @@ class GdkPixbuf < Formula
     depends_on "shared-mime-info"
   end
 
-  # gdk-pixbuf has an internal version number separate from the overall
-  # version number that specifies the location of its module and cache
-  # files, this will need to be updated if that internal version number
-  # is ever changed (as evidenced by the location no longer existing)
-  def gdk_so_ver
-    "2.0"
-  end
-
-  def gdk_module_ver
-    "2.10.0"
-  end
-
   def install
-    inreplace "gdk-pixbuf/meson.build",
-              "-DGDK_PIXBUF_LIBDIR=\"@0@\"'.format(gdk_pixbuf_libdir)",
-              "-DGDK_PIXBUF_LIBDIR=\"@0@\"'.format('#{HOMEBREW_PREFIX}/lib')"
+    # Use HOMEBREW_PREFIX to find modules installed by dependents without
+    # needing environment variables or inreplaces. In order to support this,
+    # we need install into a staging directory.
+    meson_args = std_meson_args.map { |s| s.sub prefix, HOMEBREW_PREFIX }
+    ENV["DESTDIR"] = buildpath/"stage"
 
-    ENV["DESTDIR"] = "/"
     system "meson", "setup", "build", "-Drelocatable=false",
                                       "-Dnative_windows_loaders=false",
                                       "-Dtests=false",
@@ -64,26 +54,14 @@ class GdkPixbuf < Formula
                                       "-Dothers=enabled",
                                       "-Dintrospection=enabled",
                                       "-Dglycin=disabled",
-                                      *std_meson_args
+                                      *meson_args
     system "meson", "compile", "-C", "build", "--verbose"
     system "meson", "install", "-C", "build"
-
-    # Other packages should use the top-level modules directory
-    # rather than dumping their files into the gdk-pixbuf keg.
-    inreplace lib/"pkgconfig/gdk-pixbuf-#{gdk_so_ver}.pc" do |s|
-      s.change_make_var! "prefix", HOMEBREW_PREFIX
-    end
+    prefix.install Pathname(File.join("stage", HOMEBREW_PREFIX)).children
   end
 
-  # The directory that loaders.cache gets linked into, also has the "loaders"
-  # directory that is scanned by gdk-pixbuf-query-loaders in the first place
-  def module_dir
-    "#{HOMEBREW_PREFIX}/lib/gdk-pixbuf-#{gdk_so_ver}/#{gdk_module_ver}"
-  end
-
-  def post_install
-    ENV["GDK_PIXBUF_MODULEDIR"] = "#{module_dir}/loaders"
-    system bin/"gdk-pixbuf-query-loaders", "--update-cache"
+  post_install_steps do
+    gdk_pixbuf_query_loaders
   end
 
   test do
@@ -95,8 +73,17 @@ class GdkPixbuf < Formula
         return 0;
       }
     C
-    flags = shell_output("pkgconf --cflags --libs gdk-pixbuf-#{gdk_so_ver}").chomp.split
+
+    gdk_pixbuf_pc = lib.glob("pkgconfig/gdk-pixbuf-*.pc").first.basename(".pc")
+    flags = shell_output("pkgconf --cflags --libs #{gdk_pixbuf_pc}").chomp.split
     system ENV.cc, "test.c", "-o", "test", *flags
     system "./test"
+
+    # Check that HOMEBREW_PREFIX paths are used
+    gdk_pixbuf_cache_file = shell_output("pkgconf --variable=gdk_pixbuf_cache_file #{gdk_pixbuf_pc}").chomp
+    loaders = shell_output(bin/"gdk-pixbuf-query-loaders")
+    assert_match "#{HOMEBREW_PREFIX}/lib/", gdk_pixbuf_cache_file
+    assert_match "LoaderDir = #{HOMEBREW_PREFIX}/lib/gdk-pixbuf-", loaders
+    refute_match prefix.realpath.to_s, loaders
   end
 end
