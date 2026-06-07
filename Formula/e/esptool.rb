876622318990ad912aa3a8ea3745e24bcad8e19c
Thierry Moisan <thierry.moisan@gmail.com>
2026-06-07 10:54:29 -0400

    esptool: add shell completions

--- Formula/e/esptool.rb
+++ Formula/e/esptool.rb
@@ -90,6 +90,7 @@ class Esptool < Formula
 
   def install
     virtualenv_install_with_resources
+    generate_completions_from_executable(bin/"esptool", shell_parameter_format: :click)
   end
 
   test do
