56f3288414b1145ea559ee872eea5b4c483aec8c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-07 14:51:57 +0000

    wtype: add 0.4 bottle.

--- Formula/w/wtype.rb
+++ Formula/w/wtype.rb
@@ -5,6 +5,11 @@ class Wtype < Formula
   sha256 "da91786d828b6a6e29b884bc510473939eda052658ebef87d7bdeafa6a8746f9"
   license "MIT"
 
+  bottle do
+    sha256 arm64_linux:  "67c56b2dd69bc9e41cb2f1d31fae8421bf589e1c094cc16bd975dc743fc61c00"
+    sha256 x86_64_linux: "fb8b560329a38b9ffce961dbedbbfa9cd687d6e07f104604754da4328035ed11"
+  end
+
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkgconf" => :build
