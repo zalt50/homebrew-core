e4f004a87caa1115d9d307ea0a01b07a4e410d78
Michael Cho <michael@michaelcho.dev>
2026-06-06 22:39:12 -0400

    mdless: add resources to lock versions

--- Formula/m/mdless.rb
+++ Formula/m/mdless.rb
@@ -14,13 +14,52 @@ class Mdless < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "18f8a5c0d2ae98e656ae881c66a8276d5ef014f51070826eb37f0227f75bb7f4"
   end
 
-  # Requires Ruby >= 2.7
   depends_on "ruby"
+
+  # List with `gem install --explain mdless --platform ruby -v #{version}`
+  resource "tty-which" do
+    url "https://rubygems.org/gems/tty-which-0.5.0.gem"
+    sha256 "5824055f0d6744c97e7c4426544f01d519c40d1806ef2ef47d9854477993f466"
+  end
+
+  resource "tty-cursor" do
+    url "https://rubygems.org/gems/tty-cursor-0.7.1.gem"
+    sha256 "79534185e6a777888d88628b14b6a1fdf5154a603f285f80b1753e1908e0bf48"
+  end
+
+  resource "tty-spinner" do
+    url "https://rubygems.org/gems/tty-spinner-0.9.3.gem"
+    sha256 "0e036f047b4ffb61f2aa45f5a770ec00b4d04130531558a94bfc5b192b570542"
+  end
+
+  resource "tty-screen" do
+    url "https://rubygems.org/gems/tty-screen-0.8.2.gem"
+    sha256 "c090652115beae764336c28802d633f204fb84da93c6a968aa5d8e319e819b50"
+  end
+
+  resource "rouge" do
+    url "https://rubygems.org/gems/rouge-4.7.0.gem"
+    sha256 "dba5896715c0325c362e895460a6d350803dbf6427454f49a47500f3193ea739"
+  end
+
+  resource "redcarpet" do
+    url "https://rubygems.org/gems/redcarpet-3.6.1.gem"
+    sha256 "d444910e6aa55480c6bcdc0cdb057626e8a32c054c29e793fa642ba2f155f445"
+  end
+
+  resource "logger" do
+    url "https://rubygems.org/gems/logger-1.7.0.gem"
+    sha256 "196edec7cc44b66cfb40f9755ce11b392f21f7967696af15d274dde7edff0203"
+  end
 
   def install
     ENV["GEM_HOME"] = libexec
+
+    resources.each do |r|
+      system "gem", "install", r.cached_download, "--ignore-dependencies", "--no-document", "--install-dir", libexec
+    end
     system "gem", "build", "#{name}.gemspec"
-    system "gem", "install", "#{name}-#{version}.gem"
+    system "gem", "install", "--ignore-dependencies", "#{name}-#{version}.gem"
     bin.install libexec/"bin/#{name}"
     bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
   end
