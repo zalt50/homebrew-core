b65618fc4f887a65eedbcc7443bccc5fadea891b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-07 09:27:19 +0000

    pocketbase 0.39.2

--- Formula/p/pocketbase.rb
+++ Formula/p/pocketbase.rb
@@ -1,8 +1,8 @@
 class Pocketbase < Formula
   desc "Open source backend for your next project in 1 file"
   homepage "https://pocketbase.io/"
-  url "https://github.com/pocketbase/pocketbase/archive/refs/tags/v0.39.1.tar.gz"
-  sha256 "7757b4222df46a292a5eece0a705c2c1f5b6e05681f0f9ddcb13d5ff06b2b5f1"
+  url "https://github.com/pocketbase/pocketbase/archive/refs/tags/v0.39.2.tar.gz"
+  sha256 "7d275ea218f058282deb662d0c0f0dd0a6fd460199294262c469f4e0e42ba843"
   license "MIT"
 
   bottle do
