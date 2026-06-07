c7410a6ea5e397c5d188747dfa58faf09f2382c9
Michael Cho <michael@michaelcho.dev>
2026-06-07 01:32:24 -0400

    libgweather: use compile_gsettings_schemas

--- Formula/lib/libgweather.rb
+++ Formula/lib/libgweather.rb
@@ -63,8 +63,8 @@ class Libgweather < Formula
     system "meson", "install", "-C", "build"
   end
 
-  def post_install
-    system "#{Formula["glib"].opt_bin}/glib-compile-schemas", "#{HOMEBREW_PREFIX}/share/glib-2.0/schemas"
+  post_install_steps do
+    compile_gsettings_schemas
   end
 
   test do
