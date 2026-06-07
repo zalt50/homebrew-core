15c942f6a35c8d5df806e2586578b48d15b939d3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-07 21:46:28 +0000

    libpqxx: update 8.0.1 bottle.

--- Formula/lib/libpqxx.rb
+++ Formula/lib/libpqxx.rb
@@ -7,12 +7,12 @@ class Libpqxx < Formula
   compatibility_version 2
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "8c37900dc12a476a4d4c1d6027e28f5bcbd48dd0c13d0d5eef9130365db04f09"
-    sha256 cellar: :any,                 arm64_sequoia: "a167b4c5b35c0e9226475349c2255eb13f471843a7bf2ab5749845ae0cb17b1a"
-    sha256 cellar: :any,                 arm64_sonoma:  "5d6010ea63e22a52c3a57772b928c3db42258e03371d0fadcb342540d5e9f7eb"
-    sha256 cellar: :any,                 sonoma:        "04c3c2f21bfd8c568c4de68c41c6110ec3bd96fbff44a22eec8c1b0cc1c41bbc"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "7e2d5fa38a95abfaee1581de92d57b84b059364ad4d20908aaf53295467d6a76"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1d837edceff2ce790992504d93fc7c46fbb1856ed6ccc747c78a5ccc8261be89"
+    sha256 cellar: :any, arm64_tahoe:   "74bd7251500d9ac5f524882e2be5b8cbe87f5e0b487d0166ddc46359fcaf2543"
+    sha256 cellar: :any, arm64_sequoia: "bd1693c778322338101c88fdcc06e2b46ae3beb1492b80ab19507231a2701ffa"
+    sha256 cellar: :any, arm64_sonoma:  "92bc90091b0e175626488aa9efea6cd65635633bb6ee6b9fc3fa4d8a9702f340"
+    sha256 cellar: :any, sonoma:        "0ebf76a99e37fdc0d41565ab8a5e5b820c7c68e412d6874d937fdd1f8647f7d5"
+    sha256 cellar: :any, arm64_linux:   "0451b37525134aabc4b82880f09feccf9136bb5d77ffee435d96e08a3097dc03"
+    sha256 cellar: :any, x86_64_linux:  "b2359b444e05795b3eba91929f48df79b3e259bb26dcbe20a180df25424f5660"
   end
 
   depends_on "cmake" => :build
