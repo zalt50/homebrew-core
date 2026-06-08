b5827a35437a747a169fb97d09dab09c1e11c9e4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-08 08:40:44 +0000

    sem-cli: update 0.8.0 bottle.

--- Formula/s/sem-cli.rb
+++ Formula/s/sem-cli.rb
@@ -7,12 +7,12 @@ class SemCli < Formula
   head "https://github.com/Ataraxy-Labs/sem.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "441e1f60a26e53d3a368c199f90abd456ec83e4e66eedb29a7122c4437d217ae"
-    sha256 cellar: :any, arm64_sequoia: "1136d4d8c0eeae9206c3fe3c168720ed7ce8d24ec628902956ba9a8567f611c7"
-    sha256 cellar: :any, arm64_sonoma:  "54fe092da31038c41748ca6f48a8fc1df9ae722be654a23825382bf76200ef75"
-    sha256 cellar: :any, sonoma:        "adb2361157ab865a3f56aacf44ab2e7b0c10ed6d110de2f8474044e46fcd5cbf"
-    sha256 cellar: :any, arm64_linux:   "4092e06dad2cd6aef4dd9a788ff6df9cfee30888ca530f24291278f514b6915b"
-    sha256 cellar: :any, x86_64_linux:  "5123d75dd3cc42b2df6f3ef2b72d16ac1a7ec0f16621a8f91a15d48def3aa3d4"
+    sha256 cellar: :any, arm64_tahoe:   "b581e6b179657b97c118ae981ce8a9e7fa41345bd5817ebe779332c89a95d123"
+    sha256 cellar: :any, arm64_sequoia: "8d617f9ec0d5b4f9db80a46cd90240720e85108a2752a040b48a31298507bdc6"
+    sha256 cellar: :any, arm64_sonoma:  "d4a918306b755bb37eb5c7374b641e00e2470cb8d1bfe28c3247a83a2e84d5ce"
+    sha256 cellar: :any, sonoma:        "35d997b256705c798d113713f98f0f34d037c5d6dd88349c67010e7cf6873cfe"
+    sha256 cellar: :any, arm64_linux:   "e746e215af4409f02289fc7bbccb5d72a0129dae5c345eb0031beb618f771b9b"
+    sha256 cellar: :any, x86_64_linux:  "2a8921254ccab785998eeb454185ca01989919656551885932613a93b52075cf"
   end
 
   depends_on "pkgconf" => :build
