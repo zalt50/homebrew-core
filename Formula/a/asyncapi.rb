4a192cd85da4698a4b674d797af6a90d1867a34e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-08 00:43:56 +0000

    asyncapi: update 6.0.2 bottle.

--- Formula/a/asyncapi.rb
+++ Formula/a/asyncapi.rb
@@ -7,12 +7,12 @@ class Asyncapi < Formula
   version_scheme 1
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "a115bad3e03498e458e7d28b74dbc810ba8170a185a8c0b37de63c1bf0f1fdf3"
-    sha256 cellar: :any, arm64_sequoia: "5e954a5f2631a5b73e7f17568ac7803ee9fb6df40ef755d2157b35d83c95ea27"
-    sha256 cellar: :any, arm64_sonoma:  "5e954a5f2631a5b73e7f17568ac7803ee9fb6df40ef755d2157b35d83c95ea27"
-    sha256 cellar: :any, sonoma:        "502f8f010aefa31dd53e0a21187b03a3ecb15db2357230e4727fa462d06c7943"
-    sha256 cellar: :any, arm64_linux:   "5abc424bacc89217c4c1e6ec8556db7e4b373d7d6ecafd0fcf7569fcfb8df25d"
-    sha256 cellar: :any, x86_64_linux:  "624fb2b7051106f37f4bb7ffd3d49e861e55ddaa3e8bb6c9a6e8161d1c8a9fcc"
+    sha256 cellar: :any, arm64_tahoe:   "cba3743eb9834bf40e238097d40f5b78972e7e0bc057f097a9227a9c09128b9a"
+    sha256 cellar: :any, arm64_sequoia: "14a8493220875fdbfe09991fc0439df95a0df61ea0829759f8b26772018af3b8"
+    sha256 cellar: :any, arm64_sonoma:  "14a8493220875fdbfe09991fc0439df95a0df61ea0829759f8b26772018af3b8"
+    sha256 cellar: :any, sonoma:        "f7bbe2a92d7b4991feb047276694c1bec6b863380d46beb8868f3b64d631d871"
+    sha256 cellar: :any, arm64_linux:   "77ee2374e17b18baaf325dfc841d7e6e9a02e6977e9d33ad07b849a0fc2cc1dd"
+    sha256 cellar: :any, x86_64_linux:  "9149c8c6629dc5f6019056a8ca416c3a95de0fa3e772d73703f3d4f5824ad582"
   end
 
   depends_on "node"
