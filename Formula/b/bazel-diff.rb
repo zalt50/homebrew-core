92920575595c08312c37b21b6ff90c6051685d4f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-08 21:10:21 +0000

    bazel-diff: update 26.0.0 bottle.

--- Formula/b/bazel-diff.rb
+++ Formula/b/bazel-diff.rb
@@ -6,12 +6,12 @@ class BazelDiff < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "caa19bb4f81e7271cda0f5a72ee51dcbc7183cf10c6f3daa388529daa0d74d31"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "caa19bb4f81e7271cda0f5a72ee51dcbc7183cf10c6f3daa388529daa0d74d31"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "caa19bb4f81e7271cda0f5a72ee51dcbc7183cf10c6f3daa388529daa0d74d31"
-    sha256 cellar: :any_skip_relocation, sonoma:        "caa19bb4f81e7271cda0f5a72ee51dcbc7183cf10c6f3daa388529daa0d74d31"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "950c98e57f00808f08a6c134a5d9f576b083d0fd12a60bec2c45d2632aaff58e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "950c98e57f00808f08a6c134a5d9f576b083d0fd12a60bec2c45d2632aaff58e"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "66afccb21bf3290816ed965a8faee82d046201980380e880cf762a3ecd552e62"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "66afccb21bf3290816ed965a8faee82d046201980380e880cf762a3ecd552e62"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "66afccb21bf3290816ed965a8faee82d046201980380e880cf762a3ecd552e62"
+    sha256 cellar: :any_skip_relocation, sonoma:        "66afccb21bf3290816ed965a8faee82d046201980380e880cf762a3ecd552e62"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "65976addaf0e68b83ad3c70a5e980003997d6dd6e8d49d6586caaf163382a3fb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "65976addaf0e68b83ad3c70a5e980003997d6dd6e8d49d6586caaf163382a3fb"
   end
 
   depends_on "bazel" => [:build, :test]
