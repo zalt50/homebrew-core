792668a672ef5cf93ec91536e49d1924b04aeb8e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-08 02:52:50 +0000

    file-formula 5.48

--- Formula/f/file-formula.rb
+++ Formula/f/file-formula.rb
@@ -2,8 +2,8 @@
 class FileFormula < Formula
   desc "Utility to determine file types"
   homepage "https://darwinsys.com/file/"
-  url "https://astron.com/pub/file/file-5.47.tar.gz"
-  sha256 "45672fec165cb4cc1358a2d76b5d57d22876dcb97ab169427ac385cbe1d5597a"
+  url "https://astron.com/pub/file/file-5.48.tar.gz"
+  sha256 "ed14656883b23a364b4057c05595d93252da9bc473d30106519519d0da141283"
   license "BSD-2-Clause-Darwin"
   head "https://github.com/file/file.git", branch: "master"
 
@@ -25,14 +25,11 @@ class FileFormula < Formula
 
   depends_on "libmagic"
 
-  patch :DATA
-
   def install
     ENV.prepend "LDFLAGS", "-L#{Formula["libmagic"].opt_lib} -lmagic"
 
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
-    system "make", "install-exec"
+    system "./configure", *std_configure_args
+    system "make", "install-exec", "file_DEPENDENCIES=", "file_LDADD=$(LDADD) -lm"
     system "make", "-C", "doc", "install-man1"
     rm_r lib
   end
@@ -41,26 +38,3 @@ class FileFormula < Formula
     system bin/"file", test_fixtures("test.mp3")
   end
 end
-
-__END__
-diff --git a/src/Makefile.in b/src/Makefile.in
-index 155034b..0cc9f4d 100644
---- a/src/Makefile.in
-+++ b/src/Makefile.in
-@@ -151,7 +151,6 @@ libmagic_la_LINK = $(LIBTOOL) $(AM_V_lt) --tag=CC $(AM_LIBTOOLFLAGS) \
- 	$(libmagic_la_LDFLAGS) $(LDFLAGS) -o $@
- am_file_OBJECTS = file.$(OBJEXT) seccomp.$(OBJEXT)
- file_OBJECTS = $(am_file_OBJECTS)
--file_DEPENDENCIES = libmagic.la
- AM_V_P = $(am__v_P_@AM_V@)
- am__v_P_ = $(am__v_P_@AM_DEFAULT_V@)
- am__v_P_0 = false
-@@ -372,7 +371,7 @@ libmagic_la_LDFLAGS = -no-undefined -version-info 1:0:0
- @MINGW_TRUE@MINGWLIBS = -lgnurx -lshlwapi
- libmagic_la_LIBADD = -lm $(LTLIBOBJS) $(MINGWLIBS)
- file_SOURCES = file.c seccomp.c
--file_LDADD = libmagic.la -lm
-+file_LDADD = $(LDADD) -lm
- CLEANFILES = magic.h
- EXTRA_DIST = magic.h.in cdf.mk BNF memtest.c
- HDR = $(top_srcdir)/src/magic.h.in
