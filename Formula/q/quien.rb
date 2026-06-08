0200fd9e9845d87c428f81ab5a880fcf68cc44cc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-08 02:27:41 +0000

    quien: update 0.11.0 bottle.

--- Formula/q/quien.rb
+++ Formula/q/quien.rb
@@ -6,12 +6,12 @@ class Quien < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3d460b3269973669cdb01e7013323efaa191e78977f4756c1b4c213961b261ac"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3d460b3269973669cdb01e7013323efaa191e78977f4756c1b4c213961b261ac"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3d460b3269973669cdb01e7013323efaa191e78977f4756c1b4c213961b261ac"
-    sha256 cellar: :any_skip_relocation, sonoma:        "384c69e6a6f3bd6de62f40f6bb9d87bb4522ef1058a4983fedce5e29f6137726"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "108c32294d38248ce3ce4cde2f6327fa2d41c1cdc66b526a438aedd41460d1da"
-    sha256 cellar: :any,                 x86_64_linux:  "813de163d1a8b41f55aa4f2aea52422f57a78ba5a38398640f3c5fa51813459f"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a588450373ecb684c9ae39d5d3307a7d56bd04f5be3d00be62ffb344e76ad7ff"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a588450373ecb684c9ae39d5d3307a7d56bd04f5be3d00be62ffb344e76ad7ff"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a588450373ecb684c9ae39d5d3307a7d56bd04f5be3d00be62ffb344e76ad7ff"
+    sha256 cellar: :any_skip_relocation, sonoma:        "4bf312f5e5276d247f914eef464415abf06bdf0e712bc7ba55dba02284484fbe"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "9aef471b4f6097cbbb26735667c6499c629a31dfa338084ded3e168cca66ae4b"
+    sha256 cellar: :any,                 x86_64_linux:  "bccb2c992d641f6d2ff618d1ac71129d0dd85968ce5713644166da23389cf2f1"
   end
 
   depends_on "go" => :build
