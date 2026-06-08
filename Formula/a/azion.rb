0010f81c0fc6ed0aea90b98b2f73922d438775e5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-08 18:58:41 +0000

    azion: update 4.22.1 bottle.

--- Formula/a/azion.rb
+++ Formula/a/azion.rb
@@ -7,12 +7,12 @@ class Azion < Formula
   head "https://github.com/aziontech/azion.git", branch: "dev"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "61d478763e42a398801a83870ea0904605304fb669ed2605a8907fa383cbde70"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "61d478763e42a398801a83870ea0904605304fb669ed2605a8907fa383cbde70"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "61d478763e42a398801a83870ea0904605304fb669ed2605a8907fa383cbde70"
-    sha256 cellar: :any_skip_relocation, sonoma:        "d408a28b47fe3ba16952c7666151d770cb9fd0f6745afe5ef72be64f4683a988"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "64b6e0f5a02fcb0fd2620f4e2f5b7f779057beb40f142641ad26d7f24966f410"
-    sha256 cellar: :any,                 x86_64_linux:  "7f267e26a751ce7799b5871690f339ac5f7d6e10978b9ef8061293761964152f"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8bbb6120c2b153896ad0616b5f6f7ab05eb5885156ba4105161bdb224cd04bd8"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8bbb6120c2b153896ad0616b5f6f7ab05eb5885156ba4105161bdb224cd04bd8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8bbb6120c2b153896ad0616b5f6f7ab05eb5885156ba4105161bdb224cd04bd8"
+    sha256 cellar: :any_skip_relocation, sonoma:        "37f73bd3e1e6a547b2a9a7cff295941ce7433c02a1085752094f04204548efef"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "08c5ddd9d54139f6dcf2afa18d41be8b591c652208bc9ebf576217c9f794230d"
+    sha256 cellar: :any,                 x86_64_linux:  "1b31ed6e5a36e36ce020e0d688f145b58f8140a13e9084463d65f8221aadf1a8"
   end
 
   depends_on "go" => :build
