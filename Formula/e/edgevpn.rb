cb0530ea81486b6bb90c1247ead3ab49348a3034
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-09 09:26:07 +0000

    edgevpn: update 0.35.2 bottle.

--- Formula/e/edgevpn.rb
+++ Formula/e/edgevpn.rb
@@ -7,12 +7,12 @@ class Edgevpn < Formula
   head "https://github.com/mudler/edgevpn.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "76007fba59ff05de837f064dd68cfd063798f4a59998382936627b245eabd7d6"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "76007fba59ff05de837f064dd68cfd063798f4a59998382936627b245eabd7d6"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "76007fba59ff05de837f064dd68cfd063798f4a59998382936627b245eabd7d6"
-    sha256 cellar: :any_skip_relocation, sonoma:        "6292326c92b8e579250cc26c5b8a72fa9d74f5a0a42f617d33c3a8e4933b78b1"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f58b999c46e294f0bb042920e0426d851beeb480904b93e22e2a3f43aeab323f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "288086b33bf2fc193a9d61ec1c69373f754221385d6c27895444ae704104b5fd"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "631037e02c05af4c059381430031b778718a9ed26bd61285da5cb8e232825754"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "631037e02c05af4c059381430031b778718a9ed26bd61285da5cb8e232825754"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "631037e02c05af4c059381430031b778718a9ed26bd61285da5cb8e232825754"
+    sha256 cellar: :any_skip_relocation, sonoma:        "81ee6548be40edff124dd587eca0f4ebc1cfa6364f5c9402ea9cc11a695fa552"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "44ad9803b894902fd0f5807e7342191b5a0b46a23e70d1845d845fd46279f8a8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3a91fcc4faccc05aa7ae5d0f601138d84317e49c1230ffb5a7cc7b7a0d40e107"
   end
 
   depends_on "go" => :build
