270c8b3f6feb3ca029b9dc25d5f5670fe5faa021
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-09 17:33:38 +0000

    aws-vault 7.12.1

--- Formula/a/aws-vault.rb
+++ Formula/a/aws-vault.rb
@@ -1,8 +1,8 @@
 class AwsVault < Formula
   desc "Securely store and access AWS credentials in development environments"
   homepage "https://github.com/ByteNess/aws-vault"
-  url "https://github.com/ByteNess/aws-vault/archive/refs/tags/v7.12.0.tar.gz"
-  sha256 "dc6e255c9590b378a322fc669dbc2a369e6f85a14dadd73813afd0027e55b6e2"
+  url "https://github.com/ByteNess/aws-vault/archive/refs/tags/v7.12.1.tar.gz"
+  sha256 "ff60781aaf5378aa4600c4b9989c6d3e3cbfa0454124f291c8b0dcea8f0c3c91"
   license "MIT"
   head "https://github.com/ByteNess/aws-vault.git", branch: "main"
 
