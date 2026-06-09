9e5e0ba77cef931f7d2126f3f5c44ee1d8f1cb08
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-09 05:03:14 +0000

    pitchfork: update 2.13.1 bottle.

--- Formula/p/pitchfork.rb
+++ Formula/p/pitchfork.rb
@@ -7,12 +7,12 @@ class Pitchfork < Formula
   head "https://github.com/jdx/pitchfork.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a75694e98f6c745d9f1642fbec99940316f96a12e9a4c830c462f092756e9f50"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9df396944aab1314c5beac44346dc9db6bbbf2bd212aadbcdc104a3cb0ad2ddf"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "382dd9236e7bd4987456fafbb0937c2c7e70335172973c91c0c3fb2267a87ff3"
-    sha256 cellar: :any_skip_relocation, sonoma:        "7f61afa701f61f2ec851892801586c048916a0dd86697605f80d8a761c939cbb"
-    sha256 cellar: :any,                 arm64_linux:   "8a0b8cd6df59dfa676bd55e3a5bc0de585ea26e5f6d2a6adbb4d1c306e333684"
-    sha256 cellar: :any,                 x86_64_linux:  "973ddbfecf30066c25913d69628f984b0c2efd65521ccb19e7aadf5ab13d7314"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "fd9cc0b6b591574a4c377ab4e3cb9f9a3f1a6ce423c0ca2cd103ceb5d4934379"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "304006d97a50a9858f5fd870eedd77ab2e3317183e2562bedf9667b48770b337"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "efbbcb676f1aa4d41f5ec12e65a27120dc7d9ee2e59613564b900d5285026554"
+    sha256 cellar: :any_skip_relocation, sonoma:        "6c2d674ac3cd2b4a5ce53034a2b0d43a20cb466ca346245d6cd06b7c7064f561"
+    sha256 cellar: :any,                 arm64_linux:   "d0eae816ceed9426111ce03a0f8687d3969d97427994063117b412a9035b337e"
+    sha256 cellar: :any,                 x86_64_linux:  "b6c78bf07f54e38d0632442cd4635dcb2f2524bcbc4de657ca1ae3e590e13aed"
   end
 
   depends_on "rust" => :build
