3f25bca52a166ef976dc7aa6a09a746d337bb89a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-10 04:45:54 +0000

    officecli: add 1.0.106 bottle.

--- Formula/o/officecli.rb
+++ Formula/o/officecli.rb
@@ -5,6 +5,15 @@ class Officecli < Formula
   sha256 "430442f6416421ab6a0937e3e1406d627890e4a009b8cdf81bce0e2978d47ae8"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6154e7eef23337102638e516db0250174fc23ceb48e0a596d2442741b8ad1dd6"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b6565f95be3418b90214880f068dac9b3c4a9fc8189b194337b54a15b666ec7f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b0f130a21c25c880e0b1324896e7f49c818a51c439fb5aca592686dfd4a65e8b"
+    sha256 cellar: :any_skip_relocation, sonoma:        "d01b0d69545f53131e8e158f89352bc88f5003425b7fee559143635051a80209"
+    sha256 cellar: :any,                 arm64_linux:   "deef4e85f72ec79f708a92557eb0393badfbc6ae6c27ea4dbe7378459d7c8933"
+    sha256 cellar: :any,                 x86_64_linux:  "a65a6b4be4b053d6ed670ed851f31b7a3018ae1eaecf1d6d393068f7687f1adf"
+  end
+
   depends_on "dotnet"
 
   def install
