a043b670bb65d0545025835ffdc11f75a8c94ad8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-10 17:57:17 +0000

    deark 1.7.3

--- Formula/d/deark.rb
+++ Formula/d/deark.rb
@@ -1,8 +1,8 @@
 class Deark < Formula
   desc "File conversion utility for older formats"
   homepage "https://entropymine.com/deark/"
-  url "https://entropymine.com/deark/releases/deark-1.7.2.tar.gz"
-  sha256 "e2163169b18781425e87566abf21a235513cef457a4bd27c14c304cf9a872971"
+  url "https://entropymine.com/deark/releases/deark-1.7.3.tar.gz"
+  sha256 "84f9e0134830389e4ab12c89dc09cf42a3f885ec551254e9fdc22952858add11"
   license "MIT"
 
   livecheck do
@@ -17,6 +17,12 @@ class Deark < Formula
     sha256 cellar: :any_skip_relocation, sonoma:        "124d7842655076287bfb363bdd3cc0c7c97c7275125588ae9f44ab8a324c3262"
     sha256 cellar: :any_skip_relocation, arm64_linux:   "e4caec2711f3f0f3e3d9339a4efead42bf0c10a05d4a6986c466f976a877d119"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "0dafff439bf4b70a04558225a992ff78f4da434d4dd6b7d6ced1da488341d1e6"
+  end
+
+  # Backport for macOS workaround
+  patch do
+    url "https://github.com/jsummers/deark/commit/d9ad63f9331f7d804c4978a644272810e200eb4c.patch?full_index=1"
+    sha256 "0b08bfdd88bd0ff99208c6eb96eb72fc91f421c633a55bfc2ec960c5ee74a1cf"
   end
 
   def install
