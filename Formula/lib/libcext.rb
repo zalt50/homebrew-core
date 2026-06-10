04f17d72e2c1067e4e692b9e8358b3f738c43484
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-10 10:05:23 +0000

    libcext: add 1.3.2 bottle.

--- Formula/lib/libcext.rb
+++ Formula/lib/libcext.rb
@@ -10,6 +10,15 @@ class Libcext < Formula
     regex(/href=.*?cext[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
+  bottle do
+    sha256 cellar: :any, arm64_tahoe:   "29e252f3c56b2d68da1aa5da66f4551bebd87e53d6db8449fc25059c022dddf7"
+    sha256 cellar: :any, arm64_sequoia: "f18f3718af6c5b721c033f913bb2c5692ce09c92e33479c728a59f825c902dec"
+    sha256 cellar: :any, arm64_sonoma:  "13fab2ed8c3c4c5589ec52a94ad4eae20de0c10d7365e1804f04f3790284e9ce"
+    sha256 cellar: :any, sonoma:        "9791a79885d754c78fd120a01072164ccb1d7f82d14b1a45c93449e497ace04e"
+    sha256 cellar: :any, arm64_linux:   "27dab44f698fa0473bd0a1333023942141fcadeb2b21e5b2dc4a2cadae4c0884"
+    sha256 cellar: :any, x86_64_linux:  "97189a77ae9c48a742fc50a75e3607b7a3b95f1e8c9bed299ef17a399f29c707"
+  end
+
   def install
     system "./configure", *std_configure_args
     system "make", "install"
