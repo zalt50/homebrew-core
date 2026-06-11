c5b0dc69794382e77e389e2a9c6b69b2c9bbbd5b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-11 01:39:05 +0000

    zot: add 0.2.28 bottle.

--- Formula/z/zot.rb
+++ Formula/z/zot.rb
@@ -6,6 +6,15 @@ class Zot < Formula
   license "MIT"
   head "https://github.com/patriceckhart/zot.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "be6fa43c5491a6f8b8f61aa6133924f704b41611a04a0e7dbf3734ef9e4acd85"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "be6fa43c5491a6f8b8f61aa6133924f704b41611a04a0e7dbf3734ef9e4acd85"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "be6fa43c5491a6f8b8f61aa6133924f704b41611a04a0e7dbf3734ef9e4acd85"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e3606d82b10c1717f17269d270b8605adabbd7d6d646b79938f2eeba12be2a57"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "24044ee928f06f8274373d67f0c61492c360e39532aecc1828d10b7aa9970ce1"
+    sha256 cellar: :any,                 x86_64_linux:  "388ff22adcdc5dcbabab8556336f6a1a8c7b4060c10d93b8a21c49c71df3252f"
+  end
+
   depends_on "go" => :build
 
   def install
