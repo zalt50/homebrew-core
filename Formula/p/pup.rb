988aabb31e62dac8060f9410e88ba7b9ea760fa2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-11 01:39:13 +0000

    pup: add 1.0.0 bottle.

--- Formula/p/pup.rb
+++ Formula/p/pup.rb
@@ -6,6 +6,15 @@ class Pup < Formula
   license "Apache-2.0"
   head "https://github.com/DataDog/pup.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2735b7507d9ee44f57f8c2e8fe0be6090a67c1a0e026582d86b4168cde9a7924"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c1198a2e46219fc4c27524ee8323718f9af106aa9a8c253c4846cb4971715136"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fd8eaf28c9d214992f7535e3ddd0a974597623d2834bb38c1ae1ab0654d2fa6e"
+    sha256 cellar: :any_skip_relocation, sonoma:        "13d771b10ad1a1a2ae5d7f44ca30c74b65443465a464fba98c250ed22625b9a7"
+    sha256 cellar: :any,                 arm64_linux:   "9199f62771bff8ce7dbe0c02f08d2ab74cd92f9e9f1dac6e0cecb03232a56d65"
+    sha256 cellar: :any,                 x86_64_linux:  "c5a52975e68237ca147511ab1702b2f15a4f279509b4f3423430b28317b70e99"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
 
