a1a87f03d49783f4e64a943da48e9acd21fc5559
Rui Chen <rui@chenrui.dev>
2026-06-10 21:00:57 -0400

    zot 0.2.28 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/z/zot.rb
@@ -0,0 +1,20 @@
+class Zot < Formula
+  desc "Lightweight coding agent harness written in Go"
+  homepage "https://www.zot.sh/"
+  url "https://github.com/patriceckhart/zot/archive/refs/tags/v0.2.28.tar.gz"
+  sha256 "1b12d9c84f8a60d3b42bad05b885929b305aa25b425abcf572d9dbbb6a6c9ceb"
+  license "MIT"
+  head "https://github.com/patriceckhart/zot.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X main.version=#{version}"
+    system "go", "build", *std_go_args(ldflags:), "./cmd/zot"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/zot --version")
+    assert_match "zot: no credential for anthropic", shell_output("#{bin}/zot rpc 2>&1", 1)
+  end
+end
