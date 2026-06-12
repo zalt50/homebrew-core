7600e13c0ed8efe5fcb7378fa50939c7523019c6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-12 23:36:12 +0000

    heroku 11.5.0

--- Formula/h/heroku.rb
+++ Formula/h/heroku.rb
@@ -1,8 +1,8 @@
 class Heroku < Formula
   desc "CLI for Heroku"
   homepage "https://www.npmjs.com/package/heroku/"
-  url "https://registry.npmjs.org/heroku/-/heroku-11.4.0.tgz"
-  sha256 "fc8b6d466a90bef772b629398145a128ec8e3cd49f14c9da9e0ab485355c9e51"
+  url "https://registry.npmjs.org/heroku/-/heroku-11.5.0.tgz"
+  sha256 "9cb8847462256e60b0e480867b00665ac50ea6ffa31fd21b71e215a369a620cb"
   license "ISC"
 
   bottle do
