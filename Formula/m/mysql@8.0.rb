94e385e9a2d27d108a315b4977bcae109627e320
botantony <antonsm21@gmail.com>
2026-06-12 15:15:52 +0200

    mysql@8.0: revision bump (protobuf 35.1)

--- Formula/m/mysql@8.0.rb
+++ Formula/m/mysql@8.0.rb
@@ -6,7 +6,7 @@ class MysqlAT80 < Formula
   url "https://cdn.mysql.com/Downloads/MySQL-8.0/mysql-boost-8.0.46.tar.gz"
   sha256 "dff4332ee7f8f37fc0516c66763600a22a81c8192c743c477b6484206e314f2f"
   license "GPL-2.0-only" => { with: "Universal-FOSS-exception-1.0" }
-  revision 1
+  revision 2
 
   livecheck do
     url "https://dev.mysql.com/downloads/mysql/8.0.html?tpl=files&os=src&version=8.0"
