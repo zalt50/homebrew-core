0ee06de894cf55139d0132c80592ebe52a655271
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-12 23:16:30 +0000

    cargo-generate 0.23.12

--- Formula/c/cargo-generate.rb
+++ Formula/c/cargo-generate.rb
@@ -1,8 +1,8 @@
 class CargoGenerate < Formula
   desc "Use pre-existing git repositories as templates"
   homepage "https://github.com/cargo-generate/cargo-generate"
-  url "https://github.com/cargo-generate/cargo-generate/archive/refs/tags/v0.23.11.tar.gz"
-  sha256 "586981962590a77589998cded424e08d22361f63e351c2396c71df1cda5d0a67"
+  url "https://github.com/cargo-generate/cargo-generate/archive/refs/tags/v0.23.12.tar.gz"
+  sha256 "de3aac69feb0085bda238344521c2289e757d2211c5950ab8af8ae826908ad0c"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/cargo-generate/cargo-generate.git", branch: "main"
 
