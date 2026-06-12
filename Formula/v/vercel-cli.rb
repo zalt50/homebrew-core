3425ccf2911010c91b47018d8b7698c7793c6051
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-12 21:23:43 +0000

    vercel-cli 54.12.2

--- Formula/v/vercel-cli.rb
+++ Formula/v/vercel-cli.rb
@@ -1,8 +1,8 @@
 class VercelCli < Formula
   desc "Command-line interface for Vercel"
   homepage "https://vercel.com/home"
-  url "https://registry.npmjs.org/vercel/-/vercel-54.11.1.tgz"
-  sha256 "3f87a84a2e246f562ba9f65051edb5177ec66433ede3e88a0c3308725647093f"
+  url "https://registry.npmjs.org/vercel/-/vercel-54.12.2.tgz"
+  sha256 "a2579daa10508398aa4458acb3aba0c176242821813fbfc7542a9d00a2e2f070"
   license "Apache-2.0"
 
   bottle do
@@ -26,6 +26,13 @@ class VercelCli < Formula
     rm_r node_modules/"sandbox/dist/pty-server-linux-x86_64"
 
     deuniversalize_machos node_modules/"fsevents/fsevents.node" if OS.mac?
+
+    (node_modules/"@vercel/go/bin").glob("**/proxy-*").each do |f|
+      next if OS.linux? && f.arch == Hardware::CPU.arch
+
+      rm f
+    end
+
     bin.install_symlink libexec.glob("bin/*")
   end
 
