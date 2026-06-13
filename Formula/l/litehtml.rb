a237507b5ead01414b156417ee640c72a3631896
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-13 23:36:39 +0000

    litehtml 0.10

--- Formula/l/litehtml.rb
+++ Formula/l/litehtml.rb
@@ -1,8 +1,8 @@
 class Litehtml < Formula
   desc "Fast and lightweight HTML/CSS rendering engine"
   homepage "http://www.litehtml.com/"
-  url "https://github.com/litehtml/litehtml/archive/refs/tags/v0.9.tar.gz"
-  sha256 "ef957307da15b1258a70961942840bcf54225a8d75315dcbc156186eba35b1a7"
+  url "https://github.com/litehtml/litehtml/archive/refs/tags/v0.10.tar.gz"
+  sha256 "7700eced92847d34ad9846b138cf195a9c974b519be70de58797880ae9da649e"
   license "BSD-3-Clause"
   compatibility_version 1
 
@@ -38,18 +38,20 @@ class Litehtml < Formula
       #include <litehtml.h>
 
       int main(void) {
-        litehtml::css_element_selector selector;
-        selector.parse("[attribute=value]");
-        assert(selector.m_tag == litehtml::star_id);
-        assert(selector.m_attrs.size() == 1);
-        assert(selector.m_attrs[0].type == litehtml::select_equal);
-        assert(selector.m_attrs[0].name == litehtml::_id("attribute"));
-        assert(selector.m_attrs[0].val == "value");
+        litehtml::css_selector selector;
+        assert(selector.parse("[attribute=value]", litehtml::no_quirks_mode));
+        const litehtml::css_element_selector &el = selector.m_right;
+        assert(el.m_tag == litehtml::star_id);
+        assert(el.m_attrs.size() == 1);
+        assert(el.m_attrs[0].type == litehtml::select_attr);
+        assert(el.m_attrs[0].matcher == litehtml::attribute_equals);
+        assert(el.m_attrs[0].name == litehtml::_id("attribute"));
+        assert(el.m_attrs[0].value == "value");
         return 0;
       }
     CPP
 
-    system ENV.cxx, "-std=c++11", "test.cpp", "-o", "test", "-I#{include}/litehtml", "-L#{lib}", "-llitehtml"
+    system ENV.cxx, "-std=c++17", "test.cpp", "-o", "test", "-I#{include}/litehtml", "-L#{lib}", "-llitehtml"
     system "./test"
   end
 end
