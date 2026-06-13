638617fc311ba5b5aeb4d39c87918d9d6bceb89d
Stefan Löffler <st.loeffler@gmail.com>
2026-06-13 13:32:50 +0200

    poppler-qt6 26.06.0 (new formula)
    * poppler-qt6 26.06.0 (new formula)
      Modelled after the deprecated poppler-qt5
    * Apply suggestions from code review
    
    Closes #287794.
    
    Co-authored-by: Anton Melnikov <antonsm21@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/p/poppler-qt6.rb
@@ -0,0 +1,90 @@
+class PopplerQt6 < Formula
+  desc "PDF rendering library (based on the xpdf-3.0 code base)"
+  homepage "https://poppler.freedesktop.org/"
+  url "https://poppler.freedesktop.org/poppler-26.06.0.tar.xz"
+  sha256 "4cb4e5a3dc8cb5eec751c8a23c8ba19f61f96dedc0cd07d2aee6b0c8e2cf6ba4"
+  license any_of: ["GPL-2.0-only", "GPL-3.0-only"] # see README-XPDF
+  compatibility_version 1
+  head "https://gitlab.freedesktop.org/poppler/poppler.git", branch: "master"
+
+  livecheck do
+    formula "poppler"
+  end
+
+  keg_only "it conflicts with poppler"
+
+  depends_on "cmake" => :build
+  depends_on "gettext" => :build
+  depends_on "gobject-introspection" => :build
+  depends_on "pkgconf" => :build
+
+  depends_on "cairo"
+  depends_on "fontconfig"
+  depends_on "freetype"
+  depends_on "glib"
+  depends_on "gpgmepp"
+  depends_on "jpeg-turbo"
+  depends_on "libpng"
+  depends_on "libtiff"
+  depends_on "little-cms2"
+  depends_on "nspr"
+  depends_on "nss"
+  depends_on "openjpeg"
+  depends_on "qtbase"
+
+  uses_from_macos "gperf" => :build
+  uses_from_macos "curl"
+
+  on_macos do
+    depends_on "gettext"
+    depends_on "gpgme"
+  end
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
+
+  resource "font-data" do
+    url "https://poppler.freedesktop.org/poppler-data-0.4.12.tar.gz"
+    sha256 "c835b640a40ce357e1b83666aabd95edffa24ddddd49b8daff63adb851cdab74"
+
+    livecheck do
+      url "https://poppler.freedesktop.org/"
+      regex(/href=.*?poppler-data[._-]v?(\d+(?:\.\d+)+)\.t/i)
+    end
+  end
+
+  def install
+    args = %W[
+      -DBUILD_GTK_TESTS=OFF
+      -DENABLE_BOOST=OFF
+      -DENABLE_GLIB=ON
+      -DENABLE_QT5=OFF
+      -DENABLE_QT6=ON
+      -DENABLE_UNSTABLE_API_ABI_HEADERS=ON
+      -DWITH_GObjectIntrospection=ON
+      -DCMAKE_INSTALL_RPATH=#{rpath}
+    ]
+
+    system "cmake", "-S", ".", "-B", "build_shared", *args, *std_cmake_args
+    system "cmake", "--build", "build_shared"
+    system "cmake", "--install", "build_shared"
+
+    system "cmake", "-S", ".", "-B", "build_static", *args, *std_cmake_args, "-DBUILD_SHARED_LIBS=OFF"
+    system "cmake", "--build", "build_static"
+    lib.install "build_static/libpoppler.a"
+    lib.install "build_static/cpp/libpoppler-cpp.a"
+    lib.install "build_static/glib/libpoppler-glib.a"
+
+    # Add extra metafiles for licensing information
+    prefix.install "COPYING3", "README-XPDF"
+
+    resource("font-data").stage do
+      system "make", "install", "prefix=#{prefix}"
+    end
+  end
+
+  test do
+    system bin/"pdfinfo", test_fixtures("test.pdf")
+  end
+end
