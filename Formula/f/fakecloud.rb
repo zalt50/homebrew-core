fd8e099e9e8b69429927f62f044608500848a40b
Rui Chen <rui@chenrui.dev>
2026-06-14 19:50:25 -0400

    fakecloud 0.17.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/f/fakecloud.rb
@@ -0,0 +1,40 @@
+class Fakecloud < Formula
+  desc "Free, open-source local AWS cloud emulator for integration testing"
+  homepage "https://fakecloud.dev/"
+  url "https://github.com/faiscadev/fakecloud/archive/refs/tags/v0.17.0.tar.gz"
+  sha256 "3fd6e838d92df4ddaedbd79f8bf7d6aa677c1f7fb1e51c4ce02185061d57133b"
+  license "AGPL-3.0-or-later"
+  head "https://github.com/faiscadev/fakecloud.git", branch: "main"
+
+  depends_on "pkgconf" => :build
+  depends_on "rust" => :build
+
+  on_linux do
+    depends_on "openssl@3"
+    depends_on "zlib-ng-compat"
+  end
+
+  def install
+    system "cargo", "install", *std_cargo_args(path: "crates/fakecloud-server")
+  end
+
+  service do
+    run [opt_bin/"fakecloud"]
+    keep_alive true
+  end
+
+  test do
+    port = free_port
+
+    assert_match version.to_s, shell_output("#{bin}/fakecloud --version")
+
+    pid = spawn bin/"fakecloud", "--addr", "127.0.0.1:#{port}"
+    sleep 3
+
+    output = shell_output("curl -s http://127.0.0.1:#{port}/_fakecloud/health 2>&1")
+    assert_match "ok", output.downcase
+  ensure
+    Process.kill("TERM", pid)
+    Process.wait(pid)
+  end
+end
