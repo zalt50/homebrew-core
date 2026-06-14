73d78d01fba808dfae348d530ada0defb2a8a54e
Rui Chen <rui@chenrui.dev>
2026-06-14 19:35:11 -0400

    gnhf 0.1.41 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/g/gnhf.rb
@@ -0,0 +1,21 @@
+class Gnhf < Formula
+  desc "Autonomous agent orchestrator for long-running coding tasks"
+  homepage "https://github.com/kunchenguid/gnhf"
+  url "https://registry.npmjs.org/gnhf/-/gnhf-0.1.41.tgz"
+  sha256 "2eba212fac15dd36e8147ce6cade319d6d32f0813e76f1aa745c77e4072e026f"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/gnhf --version")
+
+    output = shell_output("#{bin}/gnhf --current-branch 2>&1", 1)
+    assert_match "gnhf: This command must be run inside a Git repository", output
+  end
+end
