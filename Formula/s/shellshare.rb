49a043786f223d858bd749994dcdde99c18165f2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-14 05:59:19 +0000

    shellshare: update 3.4.0 bottle.

--- Formula/s/shellshare.rb
+++ Formula/s/shellshare.rb
@@ -7,12 +7,12 @@ class Shellshare < Formula
   head "https://github.com/vitorbaptista/shellshare.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a75120585874d126689d8dcc50415013565a05f9799e60841ca7c866562ff767"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7127478bbceeabeede943aadd7ec56d527ec4c181eb2575b4d4e2b73023d0d22"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7379c9be2667634f519714d767526841d810266dbcdde6929c8ff0da484da273"
-    sha256 cellar: :any_skip_relocation, sonoma:        "2041ec7a35883314d0725f679e0e3415ffb8bc7ed533f33f888222d67661a27d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "dde954a53f74d89b49b405d25aa2279426605cad5bad5ecf3db0d38b494a7c64"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b268e373ed7c1086a794db27afc947f3ace5580dfc203f1c6b938a6147ce3481"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "07cf8063e892c8e7d5182964bfb69e7f8018ec96860da38d5c2a8e026e2a7faf"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e8c5b375dc28d83ef52146c9dcdd5924ed32173c5a9063fb7d5eafb17f526012"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "01849ff8b7dc458956a429fce49179c3251ede0205cd68230296c4bdf6d4794a"
+    sha256 cellar: :any_skip_relocation, sonoma:        "6e191e6690c9416867a805f6680c4dfa658f28ae9b2d3d64221e16872239426e"
+    sha256 cellar: :any,                 arm64_linux:   "85ac44fc9b671ab639009758c9f0273f6b057b54c1a2ee014ae6584e7e55983e"
+    sha256 cellar: :any,                 x86_64_linux:  "4c712582c5083e580b031dad3c9d9b947ead124a5d71e39f95bff604b04118e0"
   end
 
   depends_on "rust" => :build
