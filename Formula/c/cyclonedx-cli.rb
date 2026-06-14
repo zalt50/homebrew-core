0ce18df89b22b1ef907cfb905ed38ff6fe2dc65b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-14 23:05:05 +0000

    cyclonedx-cli: add 0.32.0 bottle.

--- Formula/c/cyclonedx-cli.rb
+++ Formula/c/cyclonedx-cli.rb
@@ -5,6 +5,15 @@ class CyclonedxCli < Formula
   sha256 "9ba4bcb4c315b28fbf5e461511ff94c5b8088a4696b018368694bd235341d3cc"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c0daaaa9a506c7c8970c278ef62670c8e6b7b7116d9793cdaff95a403fcfac89"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "38eb945d4f7aaaf592775b77bb3028c3deaab1ffc0c764d0a896cf313c5ac67d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f642bb4ad9f9a4a5cdefdd4bc071dbf9883e06a9b302323876b5e419ec3d1e17"
+    sha256 cellar: :any_skip_relocation, sonoma:        "233cae284ad5950e23da9ed3751a682790e591dd0b60c07063b23a14abf17fe1"
+    sha256 cellar: :any,                 arm64_linux:   "6034df8e976469768fbacaab95eaff55c6c729e423d9d42eb26e30d28a20367f"
+    sha256 cellar: :any,                 x86_64_linux:  "f812684f055c3cd6a2a38950ec57ed8735ee9c662b2dd435e0417bbcba2613ff"
+  end
+
   depends_on "dotnet"
 
   def install
