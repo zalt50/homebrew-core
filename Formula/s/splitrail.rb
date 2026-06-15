0bcea4b9e2ae169a6485d5cc0affe076683c5bd0
Rui Chen <rui@chenrui.dev>
2026-06-15 04:43:31 +0000

    splitrail: add 3.5.4 bottle.

--- Formula/s/splitrail.rb
+++ Formula/s/splitrail.rb
@@ -6,6 +6,15 @@ class Splitrail < Formula
   license "MIT"
   head "https://github.com/Piebald-AI/splitrail.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "131580489224907e20fba10ac66a284213c4a598944b1fa039e8ffb615aa4c27"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "07264b8f9cd7ef23f49ff9b46310bf0cb104ea496f9adaf41376cca4de038cac"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bc27a90cf27a484f9ed22f563f7fd30280a9a7207ba369d8f598b8ce99e92a11"
+    sha256 cellar: :any_skip_relocation, sonoma:        "185fd83137a41fff50582bfdf4ead346eb321578c3c3296b40dcd95f45821843"
+    sha256 cellar: :any,                 arm64_linux:   "d04489ea5514fdbb3e7fc083b88dc032aca9d00356b2cab61e816d899461fba2"
+    sha256 cellar: :any,                 x86_64_linux:  "b7b38124f43df509b14bb333b50e3a00042ec309f0002f09e981790445306d1c"
+  end
+
   depends_on "rust" => :build
 
   def install
