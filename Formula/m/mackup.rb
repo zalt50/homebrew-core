ce8dfc359f93c385492342ca1dffdf2c4a8141fb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-15 16:15:07 +0000

    mackup 0.10.4

--- Formula/m/mackup.rb
+++ Formula/m/mackup.rb
@@ -3,8 +3,8 @@ class Mackup < Formula
 
   desc "Keep your Mac's application settings in sync"
   homepage "https://github.com/lra/mackup"
-  url "https://files.pythonhosted.org/packages/58/d6/dd5e60719e8f75117ea0462f690c0bfffee9483f2438cf0408255900e36c/mackup-0.10.3.tar.gz"
-  sha256 "f0166a3b2bd9d01e51d0287d9129a533939919c4eaee3f55fa018aab2bafd3ca"
+  url "https://files.pythonhosted.org/packages/e3/24/d21c072a8f3c363b0957aab83ec8964c48080c7b08d5b3870792310ce035/mackup-0.10.4.tar.gz"
+  sha256 "7f93b262137521f08ffc2f71df15736572e996915093ae3dd7cd9f65903d1455"
   license "GPL-3.0-or-later"
   head "https://github.com/lra/mackup.git", branch: "master"
 
