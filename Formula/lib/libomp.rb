f3425a773018cef97a9717333a4b6c2a645738de
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-16 16:04:55 +0000

    libomp 22.1.8

--- Formula/lib/libomp.rb
+++ Formula/lib/libomp.rb
@@ -1,8 +1,8 @@
 class Libomp < Formula
   desc "LLVM's OpenMP runtime library"
   homepage "https://openmp.llvm.org/"
-  url "https://github.com/llvm/llvm-project/releases/download/llvmorg-22.1.7/llvm-project-22.1.7.src.tar.xz"
-  sha256 "5cc4a3f12bba50b6bdfb4b61bdc852117a0ff2517807c3902fc13267fb93562e"
+  url "https://github.com/llvm/llvm-project/releases/download/llvmorg-22.1.8/llvm-project-22.1.8.src.tar.xz"
+  sha256 "922f1817a0df7b1489272d18134ee0087a8b068828f87ac63b9861b1a9965888"
   license "MIT"
   compatibility_version 1
 
