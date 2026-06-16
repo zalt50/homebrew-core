27355beed40c809dde71a21a43568a09aef08b6a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-16 11:34:40 +0000

    buffa: add 0.7.1 bottle.

--- Formula/b/buffa.rb
+++ Formula/b/buffa.rb
@@ -6,6 +6,15 @@ class Buffa < Formula
   license "Apache-2.0"
   head "https://github.com/anthropics/buffa.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5b794d2b90ac426a78c214511c566ee851cdef4b5fdb6faabf90818d0d1d77ad"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d8303a73d81f7cf80c93e629df895adfac6ca16f22af5ed9aadae29eeaa1b8b8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4c49d96cdb75c0eeedcc7019c42ad63fadd45c64e9831e3bb1ea50510e2c8f34"
+    sha256 cellar: :any_skip_relocation, sonoma:        "368fccd711f1e356929dfc06cdcbc3a75590661c76f4f76bd9513ceb8928d25b"
+    sha256 cellar: :any,                 arm64_linux:   "bdfec1cabc75e02626d2d24e9857141d436ba6960aa3e7343eb12c51f17b1aaa"
+    sha256 cellar: :any,                 x86_64_linux:  "12b7f83ee1e20ab9e75e55c30f9a0240c65d8dbc96fda10933589372c4b7ca9b"
+  end
+
   depends_on "rust" => :build
   depends_on "protobuf"
 
