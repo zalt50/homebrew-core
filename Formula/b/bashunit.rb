bc4f800bdc025cee55b3d87e50debfe1a81ce4af
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-16 20:45:51 +0000

    bashunit 0.40.0

--- Formula/b/bashunit.rb
+++ Formula/b/bashunit.rb
@@ -1,8 +1,8 @@
 class Bashunit < Formula
   desc "Simple testing library for bash scripts"
   homepage "https://bashunit.typeddevs.com"
-  url "https://github.com/TypedDevs/bashunit/releases/download/0.39.1/bashunit"
-  sha256 "0cb0153ebcf7198371051332b8e8fce36c47b514eef9bb362148f404837c2e82"
+  url "https://github.com/TypedDevs/bashunit/releases/download/0.40.0/bashunit"
+  sha256 "0ee0474803b6e88e7dfa4f4c2486ea8f8e53fd8324134a9fe604ec3df8b5e72c"
   license "MIT"
 
   bottle do
