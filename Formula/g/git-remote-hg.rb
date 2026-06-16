be8366acee1a50b4e2d69de80c42c2cc7f3a41bd
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2026-06-16 13:19:15 +0900

    git-remote-hg: add patch for mercurial

--- Formula/g/git-remote-hg.rb
+++ Formula/g/git-remote-hg.rb
@@ -1,6 +1,5 @@
 class GitRemoteHg < Formula
   include Language::Python::Shebang
-  include Language::Python::Virtualenv
 
   desc "Transparent bidirectional bridge between Git and Mercurial"
   homepage "https://github.com/felipec/git-remote-hg"
@@ -20,23 +19,24 @@ class GitRemoteHg < Formula
   end
 
   depends_on "asciidoctor" => :build
+  depends_on "mercurial"
   depends_on "python@3.14"
 
   conflicts_with "git-cinnabar", because: "both install `git-remote-hg` binaries"
 
-  # TODO: Switch back to formula after Mercurial 7.2+ is supported
-  # Currently fails with "mercurial.error.BundleUnknownFeatureError: b'changegroup'"
-  resource "mercurial" do
-    url "https://www.mercurial-scm.org/release/mercurial-7.1.2.tar.gz"
-    sha256 "ce27b9a4767cf2ea496b51468bae512fa6a6eaf0891e49f8961dc694b4dc81ca"
+  # Workaround for Mercurial 7.2+
+  # PR ref: https://github.com/felipec/git-remote-hg/pull/100
+  patch do
+    url "https://github.com/felipec/git-remote-hg/commit/bad0a3e5e5dd8352b3ea67d6efa8584ebde5311e.patch?full_index=1"
+    sha256 "9a9e1f052571b48747d1c14bd5f09dcf4778b64bb2673ecc9987f46d03537105"
+  end
+  patch do
+    url "https://github.com/felipec/git-remote-hg/commit/e7ac1caffaf7518d17e158d41f35fe1e0ba057b8.patch?full_index=1"
+    sha256 "6177d0e7800e8553ba4976c7746772f3179617a674b229905114228f1ce0c94b"
   end
 
   def install
-    venv = virtualenv_create(libexec, "python3.14")
-    venv.pip_install resource("mercurial")
-    venv_python = venv.root/"bin/python"
-
-    rewrite_shebang python_shebang_rewrite_info(venv_python), "git-remote-hg"
+    rewrite_shebang detected_python_shebang, "git-remote-hg"
     system "make", "install", "prefix=#{prefix}"
 
     ENV["XML_CATALOG_FILES"] = etc/"xml/catalog"
