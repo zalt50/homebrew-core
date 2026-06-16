24ee881a9a5b0d086a0621a99d9dee7dd3488dc0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-16 22:52:02 +0000

    git-machete: update 3.43.0 bottle.

--- Formula/g/git-machete.rb
+++ Formula/g/git-machete.rb
@@ -8,7 +8,7 @@ class GitMachete < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "19e0f184e8f4edcef49b62c5720f628d34bcb7ec5f5acab5b3840090ad1bfa22"
+    sha256 cellar: :any_skip_relocation, all: "456d2d17b8cb28979fece95631c36be7690ad2369c03bc199ae4a8d404060944"
   end
 
   depends_on "python@3.14"
