a502d20cfcf494fff7eff06235aab57969a5e43c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-16 04:27:00 +0000

    gogcli: update 0.28.0 bottle.

--- Formula/g/gogcli.rb
+++ Formula/g/gogcli.rb
@@ -7,12 +7,12 @@ class Gogcli < Formula
   head "https://github.com/steipete/gogcli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3dbac191186ce6c6e7e4d1a649e2e438684f13a5878b23a75b5c6fc7f91c80f7"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ece9a2c681fad38187b0417c904b80d23583879155e260e78f332930281eb54a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ee7fd025cde5661b813814e8329303ad3f0b044c49c39adfed6a7f35a08fb98a"
-    sha256 cellar: :any_skip_relocation, sonoma:        "cfd99084538b1cca89f2b12c99c69a48d25c6b6a45c2f2361d52a68596d6a7a0"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "52de0a939305b55f1f6d25a2cc42f81a8ae9820c4986f6642a3c7d32fa56845b"
-    sha256 cellar: :any,                 x86_64_linux:  "1130c627965e49b0e62c0ce655bbb40d1b725c4758ee575ac72e0734a8d8776b"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "64cb81ac568519def159dc7c3bf646005df795f8b6ee0fbbe033d2a46e5c9aa0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2fbf03ff24f885fd9cb548454bf34c4b3f26ee613c533deecc6d054ce829fb7e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "eeb84652924f9a761bedafd32c2fed79badcdecc53c56401e5c0ab9745893a8d"
+    sha256 cellar: :any_skip_relocation, sonoma:        "21ae5eac299423adc11257b588c54dce7783ead0f677e197f16c30e388caf2b3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "0fd5633a2773b51e6663df1ff2d34ad85efd3eb9b2596cf8f8befa817645d6f4"
+    sha256 cellar: :any,                 x86_64_linux:  "af6b9a4c7f6eed9ec1c9bde30c517be8bbb4bd70c8dba5c9f0b2595c763e1416"
   end
 
   depends_on "go" => :build
