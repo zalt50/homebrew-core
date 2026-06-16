552c4c5c2b408afe00eecd201ff0eb5358fe3c79
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-16 21:39:11 +0000

    arkade: update 0.11.100 bottle.

--- Formula/a/arkade.rb
+++ Formula/a/arkade.rb
@@ -12,12 +12,12 @@ class Arkade < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e919976d772ac329895c9a87364b71661f193569078924348c8787e7b2ff6744"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e919976d772ac329895c9a87364b71661f193569078924348c8787e7b2ff6744"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e919976d772ac329895c9a87364b71661f193569078924348c8787e7b2ff6744"
-    sha256 cellar: :any_skip_relocation, sonoma:        "586034f33b6e70b4835396e46ab0e85d9269684771c3c333958cbd5564f3c68a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "fb8aabff9de7815cedb1921f0289baa0a99c3a07ef0b7a84132b062e058b58de"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3c8ef8ce0823c6071a7419b41a0478d67f405245defd781173cd7e526a8a8f57"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0208a419ee065ce3989598c42bf1173704449075468f91c8471106ec77996b64"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0208a419ee065ce3989598c42bf1173704449075468f91c8471106ec77996b64"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0208a419ee065ce3989598c42bf1173704449075468f91c8471106ec77996b64"
+    sha256 cellar: :any_skip_relocation, sonoma:        "ff54de15ee9ba5c8689533a4d37de0fb5d7a415e6c9cae0118a9b27f64b95ffe"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c294ce0bdfb531519eea2a5cadb72d2be83be625eaddf0741bff9c4ce8c70df2"
+    sha256 cellar: :any,                 x86_64_linux:  "3fdfb8e1ffc5eab2a73f089256843d998cb6363163b5468927ffd3dc3baa3e51"
   end
 
   depends_on "go" => :build
