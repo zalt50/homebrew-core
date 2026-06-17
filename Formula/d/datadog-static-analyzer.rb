5e82164ebf06b3266a78ee74205119a4142d9657
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-17 14:52:45 +0000

    datadog-static-analyzer 0.8.7

--- Formula/d/datadog-static-analyzer.rb
+++ Formula/d/datadog-static-analyzer.rb
@@ -1,8 +1,8 @@
 class DatadogStaticAnalyzer < Formula
   desc "Static analysis tool for code quality and security"
   homepage "https://docs.datadoghq.com/security/code_security/static_analysis/"
-  url "https://github.com/DataDog/datadog-static-analyzer/archive/refs/tags/0.8.6.tar.gz"
-  sha256 "e5b0d9190240d5e2eb72a37438ab5a7c1a0e6107d465dc368b389ee288262926"
+  url "https://github.com/DataDog/datadog-static-analyzer/archive/refs/tags/0.8.7.tar.gz"
+  sha256 "ed324328d19c710a5c8d03a04e5ee13dd821765205afd75cf5320440f72d9921"
   license "Apache-2.0"
   head "https://github.com/DataDog/datadog-static-analyzer.git", branch: "main"
 
