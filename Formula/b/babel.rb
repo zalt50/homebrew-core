283c10feba11d8939e9bfe82fe1c7135f37216d1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-17 14:44:55 +0000

    babel 8.0.0

--- Formula/b/babel.rb
+++ Formula/b/babel.rb
@@ -1,8 +1,8 @@
 class Babel < Formula
   desc "Compiler for writing next generation JavaScript"
   homepage "https://babeljs.io/"
-  url "https://registry.npmjs.org/@babel/cli/-/cli-7.29.7.tgz"
-  sha256 "7620855acd400b22c1972c23a5a60a9d3340c52b059df7f26ebaa50316d9f49d"
+  url "https://registry.npmjs.org/@babel/cli/-/cli-8.0.0.tgz"
+  sha256 "aa9a8ed43f3092864cc21b53f569061d4159910e742c6cd51c40e7b5b1f924fe"
   license "MIT"
 
   bottle do
