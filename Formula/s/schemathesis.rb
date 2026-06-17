683c67047492f52c2dc19154821386c65ed849cc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-17 20:01:43 +0000

    schemathesis: update 4.21.8 bottle.

--- Formula/s/schemathesis.rb
+++ Formula/s/schemathesis.rb
@@ -8,12 +8,12 @@ class Schemathesis < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "93bba3d8654c56e158ac76072c51dbba49dbb0688134c34f5d7892f4186b5bb1"
-    sha256 cellar: :any, arm64_sequoia: "8dea8fba9531f96d9bb4882f695c82fa20703ab3b90ee8615f75e6caf025a6fa"
-    sha256 cellar: :any, arm64_sonoma:  "e69cd0f26582bc2db4b963020f019b52afa80f707ebfaf7478813f9fb7feca1f"
-    sha256 cellar: :any, sonoma:        "08260cd3a1d1856077103bb9619e52327c3f8a793970d2f0d2013682c4b09870"
-    sha256 cellar: :any, arm64_linux:   "f442bd62b86e8fd613053160b134d31c758647a5557b44d049d144daf75f1201"
-    sha256 cellar: :any, x86_64_linux:  "2499d924e8e855f422606e54f9a6378c3973752ba46fe12bfadb31b9181253ba"
+    sha256 cellar: :any, arm64_tahoe:   "e281f51469569206b98c1b025973a3887a6199d13117cea7fad8e8fa4ca0e95f"
+    sha256 cellar: :any, arm64_sequoia: "c5c8dfa8e3b3350eb5e9bfe57d62ca17c58425b0b8dc45e84b21edcde34f1dd9"
+    sha256 cellar: :any, arm64_sonoma:  "b3c7984319d09718a51125c463a97d77a0300b36bd1221d7eb126984b3377a4d"
+    sha256 cellar: :any, sonoma:        "39d22df4bf239e8af2c83b2fc608e6430797db970f679dfe8a961df518849bda"
+    sha256 cellar: :any, arm64_linux:   "04c4aa11b081fca3178b858404c10bc943e99d49874261ebbbb60c5c7d59043a"
+    sha256 cellar: :any, x86_64_linux:  "25c5efc047df98aa0121ea20b58e3792ca8870fa08d38a4688de021f1e4522cc"
   end
 
   depends_on "rust" => :build # for jsonschema-rs
