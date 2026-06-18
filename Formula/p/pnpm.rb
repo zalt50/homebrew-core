2fad0351948a5f982b302b0c3169b20529e52776
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-18 12:42:24 +0000

    pnpm 11.8.0

--- Formula/p/pnpm.rb
+++ Formula/p/pnpm.rb
@@ -1,8 +1,8 @@
 class Pnpm < Formula
   desc "Fast, disk space efficient package manager"
   homepage "https://pnpm.io/"
-  url "https://registry.npmjs.org/pnpm/-/pnpm-11.7.0.tgz"
-  sha256 "deafa7ec98a1218b6a047289b92fbe2395c1e22d3495bb711653013218ee15ee"
+  url "https://registry.npmjs.org/pnpm/-/pnpm-11.8.0.tgz"
+  sha256 "1e963a5c4ca5168550ba03fc4ee8d873a772b072b7fce63b48fff27d720e2e98"
   license "MIT"
   compatibility_version 1
 
