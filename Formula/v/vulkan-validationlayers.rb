5472e57ed33a5a6a6119cf5b8c1d31cbcddee282
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-18 18:35:47 +0000

    vulkan-validationlayers 1.4.350.1

--- Formula/v/vulkan-validationlayers.rb
+++ Formula/v/vulkan-validationlayers.rb
@@ -1,8 +1,8 @@
 class VulkanValidationlayers < Formula
   desc "Vulkan layers that enable developers to verify correct use of the Vulkan API"
   homepage "https://github.com/KhronosGroup/Vulkan-ValidationLayers"
-  url "https://github.com/KhronosGroup/Vulkan-ValidationLayers/archive/refs/tags/vulkan-sdk-1.4.350.0.tar.gz"
-  sha256 "4905ae2d2424cccdd88554b78f6f53e3469da17433c3923d049a167ea674f50e"
+  url "https://github.com/KhronosGroup/Vulkan-ValidationLayers/archive/refs/tags/vulkan-sdk-1.4.350.1.tar.gz"
+  sha256 "a299313781987946b6b26553d9f3da34126ebaea6e1bf805beb402d510d3b300"
   license "Apache-2.0"
   head "https://github.com/KhronosGroup/Vulkan-ValidationLayers.git", branch: "main"
 
