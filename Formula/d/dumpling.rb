acf11e3be16a57714ccf0c313acfc0d9acdb71e2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-18 12:12:36 +0000

    dumpling 26.3.4

--- Formula/d/dumpling.rb
+++ Formula/d/dumpling.rb
@@ -1,8 +1,8 @@
 class Dumpling < Formula
   desc "Creating SQL dump from a MySQL-compatible database"
   homepage "https://github.com/pingcap/tidb"
-  url "https://github.com/pingcap/tidb/archive/refs/tags/v26.3.3.tar.gz"
-  sha256 "28e7e5721589e6948c5adb31f429b75a17ff73e464c8b4c77af40d5705ee42dd"
+  url "https://github.com/pingcap/tidb/archive/refs/tags/v26.3.4.tar.gz"
+  sha256 "373e8f8b55e99734e60655d2289fb4214a0f686d09eb38f100f5b0c5e401ae75"
   license "Apache-2.0"
   head "https://github.com/pingcap/tidb.git", branch: "master"
 
