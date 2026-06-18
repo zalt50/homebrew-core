1d4eee0b1004216a1c31a48f2b7492027919960a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-18 19:10:39 +0000

    kdash: update 2.0.1 bottle.

--- Formula/k/kdash.rb
+++ Formula/k/kdash.rb
@@ -7,12 +7,12 @@ class Kdash < Formula
   head "https://github.com/kdash-rs/kdash.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "bdd8ab76b752910ca43b0857d1e011493f0688d1f17b72c713a61b3d7e713383"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "baa1194af51dc85294bcae8ce7584fe4521257d42f7eee8a0d28834f50515a6b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5cd56a7bd3234cc7f6f283d10a5e0ab34be054a2cf1bf9ed0ae5bf1bbce6ae37"
-    sha256 cellar: :any_skip_relocation, sonoma:        "3416672ea21eb8c398d1cff8455b98043296f9b852fbea40369156a7cadbe270"
-    sha256 cellar: :any,                 arm64_linux:   "a99ee490dec131fca847a5129fa3bd5d53ae04c5a53a85129b16643d940d1b36"
-    sha256 cellar: :any,                 x86_64_linux:  "9be007dabbb68a50b3747bed3b636095bbb28990546a5f2ff741232ad1efe265"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3e6cfd24486c1fee5a2438521f2f60f4649754602a5fd85f79e5f9389806046a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "aa876f24dd7c7c525976f8831cdf203e73b8f6048f625f7475cbd24301c304e2"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "15a76ea3e08cb0d8e6c3734076a4009a2f2872c7989c895c0a7644a8ba5d9f4f"
+    sha256 cellar: :any_skip_relocation, sonoma:        "bdfc6ad50c7f2ea413077018a950927739af5773d53391d5b044aae91a5d4014"
+    sha256 cellar: :any,                 arm64_linux:   "ec9335ac1f6f64e230ba2e0b8ff8ee6dd80d61eed229de34062b25a7079932c5"
+    sha256 cellar: :any,                 x86_64_linux:  "d0592850c0f3bf641b4b073e68ad26dcf08eacc82bc6da3773419b3ab9b920ef"
   end
 
   depends_on "rust" => :build
