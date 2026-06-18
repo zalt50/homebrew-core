e0c3a3c4895bc34b9229c29d8de3e5bcd9b3fb9a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-18 07:35:21 +0000

    yorkie 0.7.12

--- Formula/y/yorkie.rb
+++ Formula/y/yorkie.rb
@@ -1,8 +1,8 @@
 class Yorkie < Formula
   desc "Document store for collaborative applications"
   homepage "https://yorkie.dev/"
-  url "https://github.com/yorkie-team/yorkie/archive/refs/tags/v0.7.11.tar.gz"
-  sha256 "f4060762df17bd3a3a0c09c60301adfe7fed9aae062ff97890905aa696f13732"
+  url "https://github.com/yorkie-team/yorkie/archive/refs/tags/v0.7.12.tar.gz"
+  sha256 "893eadb97b8742abbb5126d4d76718dc7da363f33d704ad830423ed66aaf5b84"
   license "Apache-2.0"
   head "https://github.com/yorkie-team/yorkie.git", branch: "main"
 
