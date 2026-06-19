c92e221d9c806c6a78c0c8cd00abc0358bcf5ee5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-19 15:25:30 +0000

    unuran: add 1.11.0 bottle.

--- Formula/u/unuran.rb
+++ Formula/u/unuran.rb
@@ -6,6 +6,15 @@ class Unuran < Formula
   license "GPL-2.0-or-later"
   head "https://github.com/unuran/unuran.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any, arm64_tahoe:   "cc688fd0bb04c726a41b12d0328533ff271128d42a1b8651950f89e8a1ea0406"
+    sha256 cellar: :any, arm64_sequoia: "9e411035f79de2f3dd58892bf08d2a25d28c20addbefd6a222668d0015ba5f29"
+    sha256 cellar: :any, arm64_sonoma:  "7b22444f06bce13d65e9ff6b7bad05dbe1d9b3d7c77191e5dfb4939c2fb53461"
+    sha256 cellar: :any, sonoma:        "ebfde1d5ba197b0d7bec27c917b72265ef6f59609daaf8a187d7a780c030e203"
+    sha256 cellar: :any, arm64_linux:   "369a27ae88f6f61a2dc0bee167f46b246a58b7df062e80783439bf2beff51b11"
+    sha256 cellar: :any, x86_64_linux:  "30722e8980d7a9bba0f24166944fb992aae982b08c3dba912b5f996d829890b7"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
