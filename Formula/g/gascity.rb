58f8ee9bb114fd0cdb1a0a229417a30ea3954532
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-19 02:57:24 +0000

    gascity 1.3.1

--- Formula/g/gascity.rb
+++ Formula/g/gascity.rb
@@ -1,8 +1,8 @@
 class Gascity < Formula
   desc "Orchestration-builder SDK for multi-agent coding workflows"
   homepage "https://github.com/gastownhall/gascity"
-  url "https://github.com/gastownhall/gascity/archive/refs/tags/v1.2.1.tar.gz"
-  sha256 "e35d375d404b434d46961cd2fe0f16618808381f8f1a2ae17b84890151da2916"
+  url "https://github.com/gastownhall/gascity/archive/refs/tags/v1.3.1.tar.gz"
+  sha256 "ec728d55175dacb5fa24e20c3f4fa3198c0414f748a16ce68dc19f83092d35fe"
   license "MIT"
   head "https://github.com/gastownhall/gascity.git", branch: "main"
 
@@ -18,6 +18,7 @@ class Gascity < Formula
   depends_on "go" => :build
   depends_on "beads"
   depends_on "dolt"
+  depends_on "icu4c@78"
   depends_on "jq"
   depends_on "tmux"
 
