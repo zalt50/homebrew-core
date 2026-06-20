55f608b202cc23b5335d87c9b9cfaaf966a45f26
Michael Cho <michael@michaelcho.dev>
2026-06-20 18:30:33 -0400

    gemmi: use `zlib-ng-compat`

--- Formula/g/gemmi.rb
+++ Formula/g/gemmi.rb
@@ -16,7 +16,9 @@ class Gemmi < Formula
 
   depends_on "cmake" => :build
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args
