2042fdacf2de8ab5de36edb56e7f9ef04e622063
Michael Cho <michael@michaelcho.dev>
2026-06-20 18:29:42 -0400

    oarfish: use `zlib-ng-compat`

--- Formula/o/oarfish.rb
+++ Formula/o/oarfish.rb
@@ -17,7 +17,10 @@ class Oarfish < Formula
   depends_on "rust" => :build
 
   uses_from_macos "bzip2"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "cargo", "install", *std_cargo_args
