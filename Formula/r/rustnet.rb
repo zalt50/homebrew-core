8b292682eac4985f84e59275e9788359aad4ab50
Michael Cho <michael@michaelcho.dev>
2026-06-20 18:32:12 -0400

    rustnet: use `zlib-ng-compat`

--- Formula/r/rustnet.rb
+++ Formula/r/rustnet.rb
@@ -18,12 +18,12 @@ class Rustnet < Formula
   depends_on "rust" => :build
 
   uses_from_macos "libpcap"
-  uses_from_macos "zlib"
 
   on_linux do
     depends_on "llvm" => :build
     depends_on "pkgconf" => :build
     depends_on "elfutils"
+    depends_on "zlib-ng-compat"
   end
 
   def install
