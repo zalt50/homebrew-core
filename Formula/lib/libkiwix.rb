c7083077181ea1e1a7047eaceb12e7e7b2bcff12
Michael Cho <michael@michaelcho.dev>
2026-06-20 18:30:04 -0400

    libkiwix: use `zlib-ng-compat`

--- Formula/lib/libkiwix.rb
+++ Formula/lib/libkiwix.rb
@@ -26,7 +26,10 @@ class Libkiwix < Formula
 
   uses_from_macos "python" => :build
   uses_from_macos "curl"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   # TODO: separate as a new formula once upstream release a new tag
   resource "mustache" do
