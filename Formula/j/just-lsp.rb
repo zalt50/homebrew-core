0c9c543c02da774cee54fb8756054211800ff91a
Caleb Xu <calebcenter@live.com>
2026-06-20 09:13:53 -0400

    just-lsp 0.4.7 (new formula)

---
+++ Formula/j/just-lsp.rb
@@ -0,0 +1,40 @@
+class JustLsp < Formula
+  desc "Language server for just"
+  homepage "https://github.com/terror/just-lsp"
+  url "https://github.com/terror/just-lsp/archive/refs/tags/0.4.7.tar.gz"
+  sha256 "ec32c00edc76d3c0c1721fe1b227c9b86b586f9d5d19bfb8e1c5159104f6f6b2"
+  license "CC0-1.0"
+  head "https://github.com/terror/just-lsp.git", branch: "master"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/just-lsp --version")
+
+    require "open3"
+
+    json = <<~JSON
+      {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "initialize",
+        "params": {
+          "processId": 88075,
+          "rootUri": null,
+          "capabilities": {},
+          "trace": "verbose",
+          "workspaceFolders": null
+        }
+      }
+    JSON
+
+    Open3.popen3(bin/"just-lsp") do |stdin, stdout|
+      stdin.write "Content-Length: #{json.size}\r\n\r\n#{json}"
+      assert_match(/^Content-Length: \d+/i, stdout.readline)
+    end
+  end
+end
