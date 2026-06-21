beea833f7eaa7454edc3781ebafbe54b8bf59b89
Patrick Linnane <patrick@linnane.io>
2026-06-21 15:18:25 -0700

    zbar: revision bump (imagemagick 7.1.2-26)

--- Formula/z/zbar.rb
+++ Formula/z/zbar.rb
@@ -4,7 +4,7 @@ class Zbar < Formula
   url "https://linuxtv.org/downloads/zbar/zbar-0.23.93.tar.bz2"
   sha256 "83be8f85fc7c288fd91f98d52fc55db7eedbddcf10a83d9221d7034636683fa0"
   license "LGPL-2.1-only"
-  revision 2
+  revision 3
 
   livecheck do
     url :homepage
@@ -54,6 +54,10 @@ class Zbar < Formula
 
   def install
     ENV["XML_CATALOG_FILES"] = etc/"xml/catalog"
+
+    # zbar uses gettext but upstream only links libintl on Windows, and the
+    # newer macOS linker no longer resolves it transitively. Link it explicitly.
+    ENV.append "LDFLAGS", "-lintl" if OS.mac?
 
     system "autoreconf", "--force", "--install", "--verbose" if build.head?
 
