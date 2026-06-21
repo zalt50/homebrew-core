23d00ae6e0add1db9f42d829bea2d7e3c64be329
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-21 03:38:11 +0000

    superseedr 1.0.10

--- Formula/s/superseedr.rb
+++ Formula/s/superseedr.rb
@@ -1,8 +1,8 @@
 class Superseedr < Formula
   desc "BitTorrent Client in your Terminal"
   homepage "https://github.com/Jagalite/superseedr"
-  url "https://github.com/Jagalite/superseedr/archive/refs/tags/v1.0.9.tar.gz"
-  sha256 "63b79fbfba6ebff9227ab25e9e4916f1c6c58e0f07779f971856153a06b79dd4"
+  url "https://github.com/Jagalite/superseedr/archive/refs/tags/v1.0.10.tar.gz"
+  sha256 "23b846bce0cd92f0f94dd893b8b4c6ebc227680d532bea6a33d4d9d72c153911"
   license "GPL-3.0-or-later"
   head "https://github.com/Jagalite/superseedr.git", branch: "main"
 
