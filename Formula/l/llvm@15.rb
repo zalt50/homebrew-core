f8306d8f74e34132d4130bf3211c247eeac8e6e5
Mike McQuaid <mike@mikemcquaid.com>
2026-06-21 20:04:07 +0100

    llvm@15: use path helpers.
    Avoid loading formula objects when stable paths are enough.

--- Formula/l/llvm@15.rb
+++ Formula/l/llvm@15.rb
@@ -146,7 +146,7 @@ class LlvmAT15 < Formula
       builtins_cmake_args += clt_sdk_support_flags
     else
       args << "-DFFI_INCLUDE_DIR=#{Formula["libffi"].opt_include}"
-      args << "-DFFI_LIBRARY_DIR=#{Formula["libffi"].opt_lib}"
+      args << "-DFFI_LIBRARY_DIR=#{formula_opt_lib("libffi")}"
 
       # Disable `libxml2` which isn't very useful.
       args << "-DLLVM_ENABLE_LIBXML2=OFF"
