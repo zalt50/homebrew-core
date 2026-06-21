d3bd45e7e58044f0a03aa95dfb9a0899086d1817
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-21 09:34:26 +0000

    nuxi 3.36.0

--- Formula/n/nuxi.rb
+++ Formula/n/nuxi.rb
@@ -1,8 +1,8 @@
 class Nuxi < Formula
   desc "Nuxt CLI (nuxi) for creating and managing Nuxt projects"
   homepage "https://github.com/nuxt/cli"
-  url "https://registry.npmjs.org/nuxi/-/nuxi-3.35.2.tgz"
-  sha256 "4a54db7308b9e81786c357d87a99945740f919b61b195681a4b643623ffc5b67"
+  url "https://registry.npmjs.org/nuxi/-/nuxi-3.36.0.tgz"
+  sha256 "a95383c466a75df45c04fb9ccb388108f779242299386322cc4d80d724fce99d"
   license "MIT"
   head "https://github.com/nuxt/cli.git", branch: "main"
 
