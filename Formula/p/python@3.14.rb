02699262b810fd0d38b6e184828ed49d3e57ab51
Michael Cho <michael@michaelcho.dev>
2026-06-21 22:41:17 -0400

    python@3.14: remove unzip

--- Formula/p/python@3.14.rb
+++ Formula/p/python@3.14.rb
@@ -34,7 +34,6 @@ class PythonAT314 < Formula
   uses_from_macos "libedit"
   uses_from_macos "libffi"
   uses_from_macos "ncurses"
-  uses_from_macos "unzip"
 
   on_linux do
     depends_on "zlib-ng-compat"
