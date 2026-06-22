17648df7fd94817d5a9fd966f1f79ca28dc52c99
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-22 06:47:36 +0000

    dbxcli 3.4.0

--- Formula/d/dbxcli.rb
+++ Formula/d/dbxcli.rb
@@ -1,8 +1,8 @@
 class Dbxcli < Formula
   desc "Command-line tool for Dropbox users and team admins"
   homepage "https://github.com/dropbox/dbxcli"
-  url "https://github.com/dropbox/dbxcli/archive/refs/tags/v3.3.3.tar.gz"
-  sha256 "be0187b703ef726b21ace33212a9b9f743502e74a8149d2f356fda650408c1a7"
+  url "https://github.com/dropbox/dbxcli/archive/refs/tags/v3.4.0.tar.gz"
+  sha256 "6c80185757f2d80d11a83e03ee265c841f2e98244f92cf670da059ffbc7dd37b"
   license "Apache-2.0"
   head "https://github.com/dropbox/dbxcli.git", branch: "master"
 
