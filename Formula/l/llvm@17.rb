1ce8aa3440ec0d6d90cca84ad44a348418f10daa
Michael Cho <michael@michaelcho.dev>
2026-06-22 15:47:06 -0400

    llvm@17: replace deprecated sdk_path_if_needed with sdk_path

--- Formula/l/llvm@17.rb
+++ Formula/l/llvm@17.rb
@@ -111,7 +111,7 @@ class LlvmAT17 < Formula
     builtins_cmake_args = []
 
     if OS.mac?
-      macos_sdk = MacOS.sdk_path_if_needed
+      macos_sdk = MacOS.sdk_path
       args << "-DFFI_INCLUDE_DIR=#{macos_sdk}/usr/include/ffi"
       args << "-DFFI_LIBRARY_DIR=#{macos_sdk}/usr/lib"
 
