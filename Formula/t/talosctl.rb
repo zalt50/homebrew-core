bc90aade3d88cb97b0ded8aa12f9efb6f472d738
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-22 17:51:18 +0000

    talosctl: update 1.13.5 bottle.

--- Formula/t/talosctl.rb
+++ Formula/t/talosctl.rb
@@ -7,12 +7,12 @@ class Talosctl < Formula
   head "https://github.com/siderolabs/talos.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "14807fc90ac5b4a94f3e72c2f34deb9ed651941eaa983a695c82d90bbd94c0d6"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f02ff35718d03f04bc55f07390cd5714778e7d89632abac69b06dc8be9033260"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2b28dc7543c21114085432700f4a5623ba897ba13897f80f630b2d9b6dd91a46"
-    sha256 cellar: :any_skip_relocation, sonoma:        "6fc4ad16249cebca3b17d143a67bef8f9dc569339a375576b9af7fe79cc30207"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8c9c0b55a77c1d8d5df5806b8546041cd6a903322264e2d57789162d0673766d"
-    sha256 cellar: :any,                 x86_64_linux:  "5f7705820010c86760f501d124718463dd026319f60d83a8ae38e8281bf6cc1c"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3ef6347b31c99a164738b0746fffc1ce3d039187aa7ec1168569f6036f2fa628"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c0d0c5248f737a6af1c15e447198ddc8273ff37520ada9333b5c0bd5266aaddd"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "81ed1badbb0543b359b18cd34023c521979548331b494ccafc0cf58365c342f6"
+    sha256 cellar: :any_skip_relocation, sonoma:        "846ef45fac530bd539b3f4a402fb7316fe49d22d1f126e52ff450deec47d8950"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "573731ee3691271ea12b8bfc61619ea1136ed0ba6dacf916b69342c980e9d6e4"
+    sha256 cellar: :any,                 x86_64_linux:  "70beee69846b05e5c494094a1429e0361c3698bc30d906fd365927e96ebe40f9"
   end
 
   depends_on "go" => :build
