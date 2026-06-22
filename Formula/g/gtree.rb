d977bfcdeb5ea5f2db4ce9f7b61dc443772cf035
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-22 09:08:16 +0000

    gtree: update 1.14.3 bottle.

--- Formula/g/gtree.rb
+++ Formula/g/gtree.rb
@@ -7,12 +7,12 @@ class Gtree < Formula
   head "https://github.com/ddddddO/gtree.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8cd7402fedfb69e3c835bad509a75727ce00b3c2ee00006721c0a120a3cfd4e9"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8cd7402fedfb69e3c835bad509a75727ce00b3c2ee00006721c0a120a3cfd4e9"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8cd7402fedfb69e3c835bad509a75727ce00b3c2ee00006721c0a120a3cfd4e9"
-    sha256 cellar: :any_skip_relocation, sonoma:        "b1cfff0208e2433302889b43bf896579091f3e5349f0580c2424ac4bd79fc957"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ed02a78395e545b6655e9984ecef676ebe1c6f8f9d017393e83201952801cd37"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "67d126d5f294064f14dcd02dd9fb842199d2e09ddeb58d2b4cb60c8e18c8313f"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b827aee48c23aab9b24a30afaa5561d0827440f617056c1e03134f4b6802dc23"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b827aee48c23aab9b24a30afaa5561d0827440f617056c1e03134f4b6802dc23"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b827aee48c23aab9b24a30afaa5561d0827440f617056c1e03134f4b6802dc23"
+    sha256 cellar: :any_skip_relocation, sonoma:        "55ea46a9234883abf886214d15c61ae26be11a74ed4dcecd9d00d147e7544a44"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "0452c2c9c150ca087261db80ae184ac80e7a701ada5ac3907bddaba4fd4179d7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9d650a96e271973690a1b4f15564c5fc787b9fa22f3a8d3dea6ec4e9998d4c29"
   end
 
   depends_on "go" => :build
