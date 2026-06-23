5e3b45b56cb8041ca7477b9f3f6db5668175b045
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-23 21:04:20 +0000

    lacework-cli 2.14.1

--- Formula/l/lacework-cli.rb
+++ Formula/l/lacework-cli.rb
@@ -2,8 +2,8 @@ class LaceworkCli < Formula
   desc "CLI for managing Lacework"
   homepage "https://docs.lacework.com/cli"
   url "https://github.com/lacework/go-sdk.git",
-      tag:      "v2.14.0",
-      revision: "cdcf9c86b83836f23f5ca0b915c5d6373eafd524"
+      tag:      "v2.14.1",
+      revision: "8091de30979a5f6c6dd5485c483597219d9d4200"
   license "Apache-2.0"
   head "https://github.com/lacework/go-sdk.git", branch: "main"
 
