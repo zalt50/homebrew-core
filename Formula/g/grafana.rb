6920869d97a5c41664c755b2e1f6898fabe4f127
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-23 12:11:37 +0000

    grafana 13.1.0

--- Formula/g/grafana.rb
+++ Formula/g/grafana.rb
@@ -1,8 +1,8 @@
 class Grafana < Formula
   desc "Gorgeous metric visualizations and dashboards for timeseries databases"
   homepage "https://grafana.com"
-  url "https://github.com/grafana/grafana/archive/refs/tags/v13.0.2.tar.gz"
-  sha256 "3f6ccffda94137c9679d0993312a83a76815406efd3df7dabbb8f99467e7e8c6"
+  url "https://github.com/grafana/grafana/archive/refs/tags/v13.1.0.tar.gz"
+  sha256 "3fdc33bce23123b5da795f985df22f047aafe62c4915bb5567fd374e5570418c"
   license "AGPL-3.0-only"
   head "https://github.com/grafana/grafana.git", branch: "main"
 
