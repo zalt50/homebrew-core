1e4277f832dacbb21825da89df6bc5d0f4bb2bdc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-23 10:14:32 +0000

    openturns: add 1.27 bottle.

--- Formula/o/openturns.rb
+++ Formula/o/openturns.rb
@@ -6,6 +6,15 @@ class Openturns < Formula
   license "LGPL-3.0-or-later"
   head "https://github.com/openturns/openturns.git", branch: "master"
 
+  bottle do
+    sha256 arm64_tahoe:   "514290aa010de74a1d37399c96ac1dedd0b454f908703a2e6ac4d2a52e459ca5"
+    sha256 arm64_sequoia: "ae9d55444d0943d38a2a3042e190eb01f3bc31a542268b8d38931627170ed772"
+    sha256 arm64_sonoma:  "3fd5beea88a176ca6adbc932a0404493d326adde45aa8b8e9cf1bee873555f17"
+    sha256 sonoma:        "8bafa88f02605a59cea5a842c84dfccbe90d78e85bd82f01dcd90eca267f6bae"
+    sha256 arm64_linux:   "7c528051d44e738eff1f2e139cda7229ada842baf45a57d493c7e0673716be8d"
+    sha256 x86_64_linux:  "4123a31093a55a07f41d155cb350d3fb8ab3fc72c66e708491800b6b84d05916"
+  end
+
   depends_on "cmake" => :build
   depends_on "boost"
   depends_on "cminpack"
