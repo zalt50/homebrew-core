f31192eff45c7f88ce65568765e6b3ad02de90c1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-23 12:51:16 +0000

    tanka 0.37.4

--- Formula/t/tanka.rb
+++ Formula/t/tanka.rb
@@ -1,8 +1,8 @@
 class Tanka < Formula
   desc "Flexible, reusable and concise configuration for Kubernetes using Jsonnet"
   homepage "https://tanka.dev"
-  url "https://github.com/grafana/tanka/archive/refs/tags/v0.37.3.tar.gz"
-  sha256 "ef2a0d390097fee64cd1e37b11f886b7abd4634a2b5ae90618449ee38ddba2d0"
+  url "https://github.com/grafana/tanka/archive/refs/tags/v0.37.4.tar.gz"
+  sha256 "979de90bac45cf0f87e6b03b2a9a588648524f712da41906e32faa27d6fcbbb4"
   license "Apache-2.0"
   head "https://github.com/grafana/tanka.git", branch: "main"
 
