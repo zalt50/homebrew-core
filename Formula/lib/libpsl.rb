deefee80b235a73a3cb000074c1383c6cf6d31bb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-24 22:28:16 +0000

    libpsl: update 0.22.0 bottle.

--- Formula/lib/libpsl.rb
+++ Formula/lib/libpsl.rb
@@ -8,13 +8,12 @@ class Libpsl < Formula
   compatibility_version 1
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any, arm64_tahoe:   "612197fd840036fc2c946c3550d814b63d4ac5e29eba01bd889d6c2c4047e147"
-    sha256 cellar: :any, arm64_sequoia: "2baca54e155686829dbf5482311731c8f7978c7f5fbcf02a1f3826bf60f0d116"
-    sha256 cellar: :any, arm64_sonoma:  "0b4d77a6438667c54b645fb6314bcc8150ed0a79bfac3493dbec98a6d2569076"
-    sha256 cellar: :any, sonoma:        "df41fd194645b9d521a70cedd0cc1e5736ca7ad2d8002ffdaada282981b21fe5"
-    sha256 cellar: :any, arm64_linux:   "68e82e2e15d8512e8a1ca94d0598dc68c61dc0e53a8f7f60679594867984211e"
-    sha256 cellar: :any, x86_64_linux:  "b98808e409ada829e347489c6ed40a94d96f1f980688216b66e30d0c9c6c0b69"
+    sha256 cellar: :any, arm64_tahoe:   "3c4819817efda046038fe4dcf4d82c218c5ef48a9a147f2ed6e24a30703d8c1c"
+    sha256 cellar: :any, arm64_sequoia: "247ca6f370f62e262875a552da94d16623ec8d099ef18a4c32a7de738b17a875"
+    sha256 cellar: :any, arm64_sonoma:  "c4bd88dfa97f5f1bc9c42a9f14e11cd5ed5255c353474902d4dd877b8d5c522b"
+    sha256 cellar: :any, sonoma:        "84565757d4d0c734752c7fd48b22717b116918637fa5a0a491995edb6dff2bd6"
+    sha256 cellar: :any, arm64_linux:   "8a6259680220230609b6a8893662778becb3a4823f4002de273a518f8dcc2145"
+    sha256 cellar: :any, x86_64_linux:  "7e2a10eff5f58dd812693f98ad804b4ff3992966e9d64d01d0637fc95a43e9e0"
   end
 
   depends_on "pkgconf" => :build
