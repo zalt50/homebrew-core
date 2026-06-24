dad31f534821f850349a42c4a2ea4e81dddf973c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-24 15:44:29 +0000

    scw 2.58.3

--- Formula/s/scw.rb
+++ Formula/s/scw.rb
@@ -1,8 +1,8 @@
 class Scw < Formula
   desc "Command-line Interface for Scaleway"
   homepage "https://www.scaleway.com/en/cli/"
-  url "https://github.com/scaleway/scaleway-cli/archive/refs/tags/v2.57.0.tar.gz"
-  sha256 "39f00f21c7a4fd50ecba6c2cd1198ccefc6fc7bd556d9d6271d78a5c98a0a316"
+  url "https://github.com/scaleway/scaleway-cli/archive/refs/tags/v2.58.3.tar.gz"
+  sha256 "1a9b2533a573309794276c8a862683a360408a35256a0da125d7f94c78a105c8"
   license "Apache-2.0"
 
   livecheck do
