89f9ce5c13e85691ca44b328fb957748b62aa73b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-24 03:26:24 +0000

    node@24 24.18.0

--- Formula/n/node@24.rb
+++ Formula/n/node@24.rb
@@ -1,8 +1,8 @@
 class NodeAT24 < Formula
   desc "Open-source, cross-platform JavaScript runtime environment"
   homepage "https://nodejs.org/"
-  url "https://nodejs.org/dist/v24.17.0/node-v24.17.0.tar.xz"
-  sha256 "a7ab562ed2369a29c68b72fa00e3103bcdfe37063dff799c6acc8e404e275fcd"
+  url "https://nodejs.org/dist/v24.18.0/node-v24.18.0.tar.xz"
+  sha256 "e94afde24db08e0c564ee7110a2d5aab51ee0059382c9fd8233c54eec47b28f9"
   license "MIT"
   compatibility_version 1
 
