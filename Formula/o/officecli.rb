152de468ebf672455622c3e299a203963fbc9f3e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-24 13:46:07 +0000

    officecli: update 1.0.120 bottle.

--- Formula/o/officecli.rb
+++ Formula/o/officecli.rb
@@ -6,12 +6,12 @@ class Officecli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "afdebd4ac3c8e812561672c25e1dd0fcb9fe7701afb66149e25ad3803b07934f"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fe39654dff0c91241d148ee0d9a6e8c58daf65d65d71873a476c28888387203a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d68657ebacc488329246d875acea352333ce52b05cf0594617899f9d17ceed96"
-    sha256 cellar: :any_skip_relocation, sonoma:        "b96de46107154fcf3ac8403e911153c2d4966b89bf177d1b1270f4a4bd91d8e5"
-    sha256 cellar: :any,                 arm64_linux:   "d678403bc461fc7dc68f484604d497a560a918ff2ecda7e7b3004442c3140253"
-    sha256 cellar: :any,                 x86_64_linux:  "679f2b5773be14b826622611c7d9e83f766530b3fcb4f9d456e094013a6df0f1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "baa21936dfd798c45d39f06f24ed431bdec635e98736edda00a0175ece983efb"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "04a654e31537df5d811187cfc61142a6c8e205720d513ac3c91bbe59de9f3113"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f4c1ddce38186b5c94e908c32ab0f1bad9e29b5721b11cb9dd94612d3914f95a"
+    sha256 cellar: :any_skip_relocation, sonoma:        "1e77092427eecbd6eb813f72a05e4136c3610ed889b9fffe731f35522609db8b"
+    sha256 cellar: :any,                 arm64_linux:   "7a729ff0470b25d7b17e6ce66b242c016b59ded751ce435cc9b945e263a6dfa2"
+    sha256 cellar: :any,                 x86_64_linux:  "bc5cf3153796953efc88770cef2524fb74e0de69341c59a510324b3e626a8523"
   end
 
   depends_on "dotnet"
