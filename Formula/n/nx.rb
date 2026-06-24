016a5789ab75f7bae018d88c183a4bde52c5da1e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-24 18:03:22 +0000

    nx 23.0.1

--- Formula/n/nx.rb
+++ Formula/n/nx.rb
@@ -1,8 +1,8 @@
 class Nx < Formula
   desc "Smart, Fast and Extensible Build System"
   homepage "https://nx.dev"
-  url "https://registry.npmjs.org/nx/-/nx-23.0.0.tgz"
-  sha256 "86f2229624923fd1c0f97822cf86da133f8ae57feca070847ac50eb5c12bb251"
+  url "https://registry.npmjs.org/nx/-/nx-23.0.1.tgz"
+  sha256 "0af6934f8ef6cb26c19509e9757e417371ec1223bbe49ab96f96f1c0c65db7ef"
   license "MIT"
   version_scheme 1
 
