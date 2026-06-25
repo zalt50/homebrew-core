073aeff4f378501baa5807bfae495fb150465c38
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-25 07:59:23 +0000

    apache-opennlp: update 2.5.10 bottle.

--- Formula/a/apache-opennlp.rb
+++ Formula/a/apache-opennlp.rb
@@ -7,7 +7,7 @@ class ApacheOpennlp < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "f9c439771c5d41e0bcdf840d8253abde31ae7680fe429e1d3d7139af540599bc"
+    sha256 cellar: :any_skip_relocation, all: "6f8fc0741fa2ea27fa203041bf1013bc6c36c988c9994840ce9220337e4b1cf9"
   end
 
   depends_on "openjdk"
