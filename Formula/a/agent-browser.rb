f2d1f8be75ea942dc67411dfe66d34277e119237
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-25 21:10:23 +0000

    agent-browser: update 0.31.0 bottle.

--- Formula/a/agent-browser.rb
+++ Formula/a/agent-browser.rb
@@ -6,12 +6,12 @@ class AgentBrowser < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "86ec847037b8a321346c79dd9a84a070b7af6152356f82cb46262449613d6c2d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9269e232f353ece8991b4546672b008c4ad51ece154909fe46c277908b17654f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "022223ec5cd158bc6d2401c755d1da9268e29d0921604ea520d40146e2bda3e1"
-    sha256 cellar: :any_skip_relocation, sonoma:        "79c656189f35b06aac441d587cbd8ac227e682d2ee14479daece4a826213af76"
-    sha256 cellar: :any,                 arm64_linux:   "3651cbb5546931dde508339ae2ada01d2123130ccdcb09fff2eb6eb4328b5b51"
-    sha256 cellar: :any,                 x86_64_linux:  "2815fd88e5183560c365d9671260c5da3d519ce8aaff874ca1f786180841881e"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f51cfe641004950b0c51b102e585344fa7f656d007c93f9f488e520848d67c8d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "805261fbe284092b4692cc65683a9e0cd7103a2886cdd22c5c6941cd846a5854"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "32e30e0bc9566318b6f69d490b35f5fe837e5c8b49e55b6ba3230d3405c51747"
+    sha256 cellar: :any_skip_relocation, sonoma:        "744bf817c3af55b88aa85e4cd02fd3bb8917040c209fede2269ed6c422b38fd3"
+    sha256 cellar: :any,                 arm64_linux:   "d553fe025fa3fc4e727dcb9c649d91e0420b00df7d1337803ef2605278747291"
+    sha256 cellar: :any,                 x86_64_linux:  "d8b5810e0e170d023c2436f0892edd5f7d29aa92c684305c4955e082e7941967"
   end
 
   depends_on "rust" => :build
