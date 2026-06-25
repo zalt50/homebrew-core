562905b4ee8a70004ea871a70e0282b7c9b8cc2d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-25 11:05:51 +0000

    zuban: update 0.9.0 bottle.

--- Formula/z/zuban.rb
+++ Formula/z/zuban.rb
@@ -7,12 +7,13 @@ class Zuban < Formula
   head "https://github.com/zubanls/zuban.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "05465d53000c90a78f1ea145d73625c2767fb5c98c0c1ccb1493408a2ce893cb"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cd53af540c586296f1f23bd223a78ed179f89475d114fd30b170e2c5569b54bf"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "05309ef5565bada1d4d274c43ea154a4c77b2b01f22bb4d9c6bb4132b784ada3"
-    sha256 cellar: :any_skip_relocation, sonoma:        "9afcffa21d4c98caf070b74c79181d8802c241c5f45179c7fd7b597b14b0b205"
-    sha256 cellar: :any,                 arm64_linux:   "3355062c89f5bf3864a7f35b2e5ae5310713df57d92be5b7817dfa47f94ec176"
-    sha256 cellar: :any,                 x86_64_linux:  "6cc03bf8927bd507aee4d0630237c0bab3fa5538f4ca2c850c901fa2fcc8723a"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d52b14df80b383c4176ae3eab6688b6bbb3259f82785c6e7732da7e7e5ee9e41"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9cb4f53f2f6fdd39ac9c845484debacdf0bc4ff8e6e5f8e1fcae7e98cc8fe032"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c8030e8b4d0ec98c8097bda64355e9268c4eaa5323d84b74cbb3dc4a87898203"
+    sha256 cellar: :any_skip_relocation, sonoma:        "1ee6d951b83ba9ee4132d2115178d7d46cc45771770f82069293397e8d57b684"
+    sha256 cellar: :any,                 arm64_linux:   "8b051562316bb052b1d717349f150c503449273f703a5df7bb8960f10fb700fb"
+    sha256 cellar: :any,                 x86_64_linux:  "fc5caf9cc60adb5889df153d5eaafe62c98fc3743a7c90d020e05ab1140583b4"
   end
 
   depends_on "rust" => :build
