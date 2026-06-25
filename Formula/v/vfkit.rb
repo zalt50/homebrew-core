780318011da1b633e7b4e11c70a2ed9eac42a4a3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-25 15:15:05 +0000

    vfkit: update 0.6.3 bottle.

--- Formula/v/vfkit.rb
+++ Formula/v/vfkit.rb
@@ -7,10 +7,11 @@ class Vfkit < Formula
   head "https://github.com/crc-org/vfkit.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3953a8b58decd41c83de7686212202184e575c3d45bfad7002a7033549612bab"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6fecae760b337abd886f5e0c414aa6ac4be681231f55d50696b9abd485915cd4"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "343cd8954cebd64b329487f0ab292d213247eef5026e431cc2e50bb4eb16257c"
-    sha256 cellar: :any_skip_relocation, sonoma:        "ee7116feaeeb5feea19c876d8a5f157bf646af00f8dc2d1033965ff5a8c50bfc"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f2b457cf9a83db240fd8cbf8da42696559ec98dcb0df8bae65e10d9639c67a2a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fbe1419e8154b9ffb4372c29e91570cb3f7551570c558615da7ceec62ca894ae"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0cc27e2bbf85e2d829c23dad01b56673bf992851ce1c29c701210a8c19f81cc5"
+    sha256 cellar: :any_skip_relocation, sonoma:        "14299743d19df1a3b719e471dff77e67c864db71e0b6b043f42a90b33ca81b60"
   end
 
   depends_on "go" => :build
