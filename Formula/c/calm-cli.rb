d7aadd2c9cc8dbb93ff1f16721e94959e19f7871
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-25 11:47:04 +0000

    calm-cli 1.47.0

--- Formula/c/calm-cli.rb
+++ Formula/c/calm-cli.rb
@@ -1,8 +1,8 @@
 class CalmCli < Formula
   desc "CLI allows you to interact with the Common Architecture Language Model (CALM)"
   homepage "https://github.com/finos/architecture-as-code/tree/main/cli"
-  url "https://registry.npmjs.org/@finos/calm-cli/-/calm-cli-1.46.0.tgz"
-  sha256 "37df4b2f020b22f4d3617ee8c93c8a2398356c0b6e5e0e128c944ed5e6a54fe0"
+  url "https://registry.npmjs.org/@finos/calm-cli/-/calm-cli-1.47.0.tgz"
+  sha256 "34689ddfef4f290f8f8b65942109792b787cbe871b11f0340748637f7bba8305"
   license "Apache-2.0"
 
   bottle do
