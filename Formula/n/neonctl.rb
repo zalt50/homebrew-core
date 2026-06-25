f4b01b5abc0e44efed606874feef23b210505358
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-25 19:08:57 +0000

    neonctl: update 2.27.1 bottle.

--- Formula/n/neonctl.rb
+++ Formula/n/neonctl.rb
@@ -6,12 +6,12 @@ class Neonctl < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a1804a338e8ba6953230c3441d58eb085bdc6587793f85d439fd50111e1edef1"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a1804a338e8ba6953230c3441d58eb085bdc6587793f85d439fd50111e1edef1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a1804a338e8ba6953230c3441d58eb085bdc6587793f85d439fd50111e1edef1"
-    sha256 cellar: :any_skip_relocation, sonoma:        "2a3144118642f71d55c2b8fa84e8cf402429098021e14b9ad81a3a1719efd6f9"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "dd1ba84ae21c92acbe58553c0bae3e0d1041e2a85a29b895d1184759089f4109"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6556410c8355f29094fb5cea20c0fcb76273f58bcc90c78da7c174c0f3bdd273"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "835d63b685a5f14a19e4d709eeae18b9865b7e6cb567a9eca38c1072db3cae6f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "835d63b685a5f14a19e4d709eeae18b9865b7e6cb567a9eca38c1072db3cae6f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "835d63b685a5f14a19e4d709eeae18b9865b7e6cb567a9eca38c1072db3cae6f"
+    sha256 cellar: :any_skip_relocation, sonoma:        "864932089a564d9cad2922ecc008c972f7021662357f46b2abf45a72fdb574c8"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a0c3df99d043b0bd7df2c7a9977b41ec78a83202fce661b5211c3b00937ce604"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c9c4fc87f969d1eacb326d8a78392ad992b40be1969a80719a01a29bc8726893"
   end
 
   depends_on "node"
