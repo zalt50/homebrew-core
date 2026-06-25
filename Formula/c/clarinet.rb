fdf9b3a9558b92031ed8ec83c51a590a64b6c31d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-25 17:37:21 +0000

    clarinet 3.21.0

--- Formula/c/clarinet.rb
+++ Formula/c/clarinet.rb
@@ -1,8 +1,8 @@
 class Clarinet < Formula
   desc "Command-line tool and runtime for the Clarity smart contract language"
   homepage "https://stackslabs.com/"
-  url "https://github.com/stx-labs/clarinet/archive/refs/tags/v3.20.0.tar.gz"
-  sha256 "6a649db0c593b0d079cdfaa8eb777070c0d59685e24b41f5ca15529cc6ab7cbd"
+  url "https://github.com/stx-labs/clarinet/archive/refs/tags/v3.21.0.tar.gz"
+  sha256 "9e401f06f321c263375f939621075aced16c1115773ed0ec6edff70a26a1f122"
   license "GPL-3.0-only"
   version_scheme 1
   head "https://github.com/stx-labs/clarinet.git", branch: "main"
