3888808930bf2a62d7f87fb65b089cb5c2e726d6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-25 20:42:09 +0000

    flyctl 0.4.61

--- Formula/f/flyctl.rb
+++ Formula/f/flyctl.rb
@@ -2,8 +2,8 @@ class Flyctl < Formula
   desc "Command-line tools for fly.io services"
   homepage "https://fly.io"
   url "https://github.com/superfly/flyctl.git",
-      tag:      "v0.4.60",
-      revision: "34b600fecde7ffb577ee0e6daeec203247bebcd2"
+      tag:      "v0.4.61",
+      revision: "1ac1f449b981403ceede52477289e3f6f548cfca"
   license "Apache-2.0"
   head "https://github.com/superfly/flyctl.git", branch: "master"
 
