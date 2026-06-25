1bfa527a63a366b45f68e0dfcb6cf63058556ba3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-25 18:34:40 +0000

    renovate 43.243.0

--- Formula/r/renovate.rb
+++ Formula/r/renovate.rb
@@ -1,8 +1,8 @@
 class Renovate < Formula
   desc "Automated dependency updates. Flexible so you don't need to be"
   homepage "https://github.com/renovatebot/renovate"
-  url "https://registry.npmjs.org/renovate/-/renovate-43.241.0.tgz"
-  sha256 "09d258ec47fb73e30fe92ca0fcbd0e24134d4ed0e407c6689c98f4dc0e0a6a65"
+  url "https://registry.npmjs.org/renovate/-/renovate-43.243.0.tgz"
+  sha256 "93a422d5dd2f5abfa200ba8bb31cc5bb74a8076c065b9fce7edcc15dc2f76c62"
   license "AGPL-3.0-only"
 
   # livecheck needs to surface multiple versions for version throttling but
