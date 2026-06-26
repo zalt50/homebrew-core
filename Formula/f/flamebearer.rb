e7f70b542534f5a364d4862757bc9663ed9f1212
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-26 14:55:17 +0000

    flamebearer 2.1.0

--- Formula/f/flamebearer.rb
+++ Formula/f/flamebearer.rb
@@ -1,8 +1,8 @@
 class Flamebearer < Formula
   desc "Blazing fast flame graph tool for V8 and Node"
   homepage "https://github.com/mapbox/flamebearer"
-  url "https://registry.npmjs.org/flamebearer/-/flamebearer-1.1.3.tgz"
-  sha256 "e787b71204f546f79360fd103197bc7b68fb07dbe2de3a3632a3923428e2f5f1"
+  url "https://registry.npmjs.org/flamebearer/-/flamebearer-2.1.0.tgz"
+  sha256 "35ebda2a2a30e954f7732098f67d215b7ccadf5a0672f631ed8b7e0277c6c624"
   license "ISC"
 
   bottle do
@@ -18,16 +18,14 @@ class Flamebearer < Formula
   end
 
   test do
-    (testpath/"app.js").write "console.log('hello');"
-    system Formula["node"].bin/"node", "--prof", testpath/"app.js"
-    logs = testpath.glob("isolate*.log")
+    (testpath/"app.js").write <<~JS
+      let x = 0;
+      for (let i = 0; i < 5e6; i++) x += Math.sqrt(i);
+      console.log(x);
+    JS
+    system Formula["node"].bin/"node", "--cpu-prof", "--cpu-prof-dir=#{testpath}", testpath/"app.js"
+    profile = testpath.glob("*.cpuprofile").first
 
-    assert_match "Processed V8 log",
-      pipe_output(
-        bin/"flamebearer",
-        shell_output("#{Formula["node"].bin}/node --prof-process --preprocess -j #{logs.join(" ")}"),
-      )
-
-    assert_path_exists testpath/"flamegraph.html"
+    assert_match(/samples:\s+\d+/, shell_output("#{bin}/flamebearer #{profile}"))
   end
 end
