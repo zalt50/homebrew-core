b680979bf149b623d0374af53ec57f78e93e15cc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-26 05:56:05 +0000

    elixir: update 1.20.2 bottle.

--- Formula/e/elixir.rb
+++ Formula/e/elixir.rb
@@ -13,12 +13,12 @@ class Elixir < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "689920df4f1dd9b1b44e256499abd178aa7e132118eb23bb982301b3db9251ac"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "54a6bb2575d376ee06da20c14d4379e9a6273e2750c49c383c4460da60b2542c"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a882e18eb4132d47f7d96d934c358027fd60d58df01a8c18796d2b64093362c2"
-    sha256 cellar: :any_skip_relocation, sonoma:        "4096f17dd4a47a23806da39cbd0288c800d58f8ab19df964dd0e1b3c29d8b02e"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "5b4a86f48c68d98b1509d2025425bf45d2b2bfb63064205d4301fb6398e906e5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c315926e28272cea56ff675f835cb095390ff69f384f5030fd949454be3fe49f"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d73669ada0da76a46cd1f6cb803a2d9a2bdd6984973b92b71de62a904da9c57e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b2998c4caa20ee100a5326257c1ef179038ead0c63a27db75f0cdb5ddeea1d66"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e5adba195b1fc670f08fd2a3014d367178ae20cce0dc0d40675074ea432bf2db"
+    sha256 cellar: :any_skip_relocation, sonoma:        "676fdd0abc7cb992f143239ae844693fa92d3504bd76d96f8af815f4997b6225"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "46998dd0611cef4b28d5c79f982bb370b456b73b01ff39cf4bc823cca57d2b2d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3ef588ecd35dcb724fe1d055fe02f11534c3ec4386a37367ce18278fd98737a8"
   end
 
   depends_on "erlang"
