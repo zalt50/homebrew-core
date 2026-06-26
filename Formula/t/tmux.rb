28d2384566bc80031dc1cfe94a31088fe9e9c7bf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-26 18:35:07 +0000

    tmux 3.7

--- Formula/t/tmux.rb
+++ Formula/t/tmux.rb
@@ -1,8 +1,8 @@
 class Tmux < Formula
   desc "Terminal multiplexer"
   homepage "https://tmux.github.io/"
-  url "https://github.com/tmux/tmux/releases/download/3.6b/tmux-3.6b.tar.gz"
-  sha256 "390759d25fdba016887ec982b808927e637070fd7d03a8021f8ef3102b9ae3c7"
+  url "https://github.com/tmux/tmux/releases/download/3.7/tmux-3.7.tar.gz"
+  sha256 "2344f191501b8a73eb71dd6c5fd5dcf8c765f5066f34ab46f04b3013dc7bc1a5"
   license "ISC"
   compatibility_version 1
 
