15c8f9188d8aab35fa4ee2fa6f0caf381608dfb9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-26 21:30:08 +0000

    versitygw 1.6.0

--- Formula/v/versitygw.rb
+++ Formula/v/versitygw.rb
@@ -1,8 +1,8 @@
 class Versitygw < Formula
   desc "Versity S3 Gateway"
   homepage "https://www.versity.com/products/versitygw/"
-  url "https://github.com/versity/versitygw/archive/refs/tags/v1.5.0.tar.gz"
-  sha256 "6a667e38d78a08effdf0b7c3c3e4b9929233b333c8a53c16b62eded7d43eeedc"
+  url "https://github.com/versity/versitygw/archive/refs/tags/v1.6.0.tar.gz"
+  sha256 "6621fe4c1b3a644ac37ef923078be570aa71292396b879db67549b29ed4d0616"
   license "Apache-2.0"
   head "https://github.com/versity/versitygw.git", branch: "main"
 
