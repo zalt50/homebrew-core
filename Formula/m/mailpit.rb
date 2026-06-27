ae1290153332fbb8af966e2b46f9d58c86ccd32f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-27 11:47:40 +0000

    mailpit 1.30.3

--- Formula/m/mailpit.rb
+++ Formula/m/mailpit.rb
@@ -1,8 +1,8 @@
 class Mailpit < Formula
   desc "Web and API based SMTP testing"
   homepage "https://mailpit.axllent.org/"
-  url "https://github.com/axllent/mailpit/archive/refs/tags/v1.30.2.tar.gz"
-  sha256 "239f044997dcb6ec27ed1b85b5ca3bba9d5996d66dad67014c3f4aa75549269b"
+  url "https://github.com/axllent/mailpit/archive/refs/tags/v1.30.3.tar.gz"
+  sha256 "1e6a94e92b5e6ebbdaed7ec2dd280b7d200185697d723044c5a9fc239fcdb6d8"
   license "MIT"
 
   bottle do
