289e1f381cd0b7e2e61fcf44d48bccfd705f6822
Loki Astari <Loki.Astari@gmail.com>
2026-06-27 00:46:25 -0700

    thors-anvil 11.0.4

--- Formula/t/thors-anvil.rb
+++ Formula/t/thors-anvil.rb
@@ -2,8 +2,8 @@ class ThorsAnvil < Formula
   desc "Set of modern C++20 libraries for writing interactive Web-Services"
   homepage "https://github.com/Loki-Astari/ThorsAnvil"
   url "https://github.com/Loki-Astari/ThorsAnvil.git",
-      tag:      "11.0.3",
-      revision: "8cd544b4d6e78635e91f6509cbf9e0341911c5c6"
+      tag:      "11.0.4",
+      revision: "40f7fc76ea17b80bcc170cf7caf5a7ae9c000a09"
   license "MIT"
 
   bottle do
