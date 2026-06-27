f817b4d75a69efa25b575cd1615988634156b446
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-27 19:03:34 +0000

    ghorg: update 1.11.12 bottle.

--- Formula/g/ghorg.rb
+++ Formula/g/ghorg.rb
@@ -7,12 +7,12 @@ class Ghorg < Formula
   head "https://github.com/gabrie30/ghorg.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b8ede1256526f9b7bbdf4b914de96ea944c75174608333df52270b0cda0618c5"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b8ede1256526f9b7bbdf4b914de96ea944c75174608333df52270b0cda0618c5"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b8ede1256526f9b7bbdf4b914de96ea944c75174608333df52270b0cda0618c5"
-    sha256 cellar: :any_skip_relocation, sonoma:        "92ad2c66d7530401e8dc28b469963c9d64a86a4b118bc0e8a47aec1a7588bc77"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "767fa919a31a01628ada3adc4cb84937f43173b823939ac1c9aab899ddac6ee6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "85e44f9db27e124ebd6545f44b9dc285c5029896981b0669a7e7bdfe14263818"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "fcda2b9387140065e392cf8edd445b7344a4151407421925252dcb6d6f36fd69"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fcda2b9387140065e392cf8edd445b7344a4151407421925252dcb6d6f36fd69"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fcda2b9387140065e392cf8edd445b7344a4151407421925252dcb6d6f36fd69"
+    sha256 cellar: :any_skip_relocation, sonoma:        "381919554ead0b742c56c72a82a6bff05c4989e759c2c5f9f7c4dee5d617756a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "205abe7f17808bb058ce605e9018f9a373e364da53b88e4f1aaac31ac5dbb56b"
+    sha256 cellar: :any,                 x86_64_linux:  "a2961964a77c217d9af51c99ab87839aea4237a44767766d8d5d0a65de363b8d"
   end
 
   depends_on "go" => :build
