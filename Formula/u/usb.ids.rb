7cab54215829814b11eef93eaf0ea1ba1cd6c085
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-27 03:43:36 +0000

    usb.ids 2026.06.26

--- Formula/u/usb.ids.rb
+++ Formula/u/usb.ids.rb
@@ -1,8 +1,8 @@
 class UsbIds < Formula
   desc "Repository of vendor, device, subsystem and device class IDs used in USB devices"
   homepage "http://www.linux-usb.org/usb-ids.html"
-  url "https://deb.debian.org/debian/pool/main/u/usb.ids/usb.ids_2025.12.13.orig.tar.xz"
-  sha256 "492edc99d85a25dca388930c304e85543ef5913b63b5f2fdfa2c213d77709784"
+  url "https://deb.debian.org/debian/pool/main/u/usb.ids/usb.ids_2026.06.26.orig.tar.xz"
+  sha256 "bbd6b4c8d9fff1e2602551b6a73b8ca0475d1aed93a1807d680d02556c49f8e3"
   license any_of: ["GPL-2.0-or-later", "BSD-3-Clause"]
   compatibility_version 1
 
