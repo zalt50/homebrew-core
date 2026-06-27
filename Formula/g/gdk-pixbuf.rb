fbcdc944e9c4e3ff92459c836e09a39d91f6a2bc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-27 14:30:02 +0000

    gdk-pixbuf 2.44.7

--- Formula/g/gdk-pixbuf.rb
+++ Formula/g/gdk-pixbuf.rb
@@ -1,10 +1,9 @@
 class GdkPixbuf < Formula
   desc "Toolkit for image loading and pixel buffer manipulation"
   homepage "https://gtk.org"
-  url "https://download.gnome.org/sources/gdk-pixbuf/2.44/gdk-pixbuf-2.44.6.tar.xz"
-  sha256 "140c2d0b899fcf853ee92b26373c9dc228dbcde0820a4246693f4328a27466fa"
+  url "https://download.gnome.org/sources/gdk-pixbuf/2.44/gdk-pixbuf-2.44.7.tar.xz"
+  sha256 "172f80e3626ec31520a970400f1a3694e04718f6c2cd2885f75250fb5a6995a4"
   license "LGPL-2.1-or-later"
-  revision 1
   compatibility_version 1
 
   bottle do
