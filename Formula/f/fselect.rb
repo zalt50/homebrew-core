3b32a3183305ba06c08d61882c9a4edae1dbfc2a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-28 11:41:19 +0000

    fselect 0.10.2

--- Formula/f/fselect.rb
+++ Formula/f/fselect.rb
@@ -1,8 +1,8 @@
 class Fselect < Formula
   desc "Find files with SQL-like queries"
   homepage "https://fselect.rocks"
-  url "https://github.com/jhspetersson/fselect/archive/refs/tags/0.10.1.tar.gz"
-  sha256 "20425a3ab36fd46b24bb280a3c13187e0b33ab2e68734d8f51b07710af25581f"
+  url "https://github.com/jhspetersson/fselect/archive/refs/tags/0.10.2.tar.gz"
+  sha256 "3a51225c41ff0fa460abdbf30cb7434769454cbcba8d717ad24a5c8fa005485f"
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
