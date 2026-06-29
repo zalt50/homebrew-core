f5608aa4f76cfac32cbc3229502fcccb9884651a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-29 09:51:05 +0000

    nerdctl 2.3.4

--- Formula/n/nerdctl.rb
+++ Formula/n/nerdctl.rb
@@ -1,8 +1,8 @@
 class Nerdctl < Formula
   desc "ContaiNERD CTL - Docker-compatible CLI for containerd"
   homepage "https://github.com/containerd/nerdctl"
-  url "https://github.com/containerd/nerdctl/archive/refs/tags/v2.3.3.tar.gz"
-  sha256 "eacca139faba3182cc6be633f146e02f3744c5dbec2cf93d6b089d7e63621614"
+  url "https://github.com/containerd/nerdctl/archive/refs/tags/v2.3.4.tar.gz"
+  sha256 "d3262200794152a858247ee95dd3b5366e1fdeda0577908cef2e76cd71c5cc38"
   license "Apache-2.0"
   head "https://github.com/containerd/nerdctl.git", branch: "main"
 
