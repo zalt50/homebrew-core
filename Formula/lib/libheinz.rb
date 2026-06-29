1ca411efb0a80c30e56c43c3423fe6cd9005a8ec
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-29 09:43:50 +0000

    libheinz 4.1.0

--- Formula/lib/libheinz.rb
+++ Formula/lib/libheinz.rb
@@ -1,8 +1,8 @@
 class Libheinz < Formula
   desc "C++ base library of Heinz Maier-Leibnitz Zentrum"
   homepage "https://jugit.fz-juelich.de/mlz/libheinz"
-  url "https://jugit.fz-juelich.de/mlz/libheinz/-/archive/v4.0.0/libheinz-v4.0.0.tar.bz2"
-  sha256 "cc78e7701a70bc4e476f8968d1c15030b26126da578e099994724fb7f1a3a5fa"
+  url "https://jugit.fz-juelich.de/mlz/libheinz/-/archive/v4.1.0/libheinz-v4.1.0.tar.bz2"
+  sha256 "c22ae3d26e6fa34bce14424ad335cf4189df15a0419650e60b4138a914245e4f"
   license "0BSD"
 
   bottle do
