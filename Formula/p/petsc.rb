086ee187e80a51355ac75be22bc11d88aee08746
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-29 23:47:15 +0000

    petsc 3.25.3

--- Formula/p/petsc.rb
+++ Formula/p/petsc.rb
@@ -1,10 +1,10 @@
 class Petsc < Formula
   desc "Portable, Extensible Toolkit for Scientific Computation (real)"
   homepage "https://petsc.org/"
-  url "https://web.cels.anl.gov/projects/petsc/download/release-snapshots/petsc-3.24.6.tar.gz"
-  sha256 "d6ad14652996b0e0d3da51068eec902118057f275de867e8cf258ffd64d90a7d"
+  url "https://web.cels.anl.gov/projects/petsc/download/release-snapshots/petsc-3.25.3.tar.gz"
+  sha256 "95ce60df2c7f9c5044d6a544c41e996a512557f91df1a60bdb690b332904ebb5"
   license "BSD-2-Clause"
-  compatibility_version 1
+  compatibility_version 2
 
   livecheck do
     url "https://web.cels.anl.gov/projects/petsc/download/release-snapshots/"
