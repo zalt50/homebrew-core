8d970e9c3ccc005ae7f67ec05bcaabac7edb4e60
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-29 06:59:38 +0000

    rust-analyzer 2026-06-29

--- Formula/r/rust-analyzer.rb
+++ Formula/r/rust-analyzer.rb
@@ -2,8 +2,8 @@ class RustAnalyzer < Formula
   desc "Experimental Rust compiler front-end for IDEs"
   homepage "https://rust-analyzer.github.io/"
   url "https://github.com/rust-lang/rust-analyzer.git",
-      tag:      "2026-06-22",
-      revision: "69ccffdb5b3570c6c14c5780bf2e8836f2209d90"
+      tag:      "2026-06-29",
+      revision: "972c4e7bee140acd27e26b3e04673bfd05302f89"
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
