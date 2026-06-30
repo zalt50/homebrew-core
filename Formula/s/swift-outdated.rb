74f983cc262a49dcefa1d0de2f9b7ed5792895bb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-30 15:23:44 +0000

    swift-outdated 0.15.0

--- Formula/s/swift-outdated.rb
+++ Formula/s/swift-outdated.rb
@@ -1,8 +1,8 @@
 class SwiftOutdated < Formula
   desc "Check for outdated Swift package manager dependencies"
   homepage "https://github.com/kiliankoe/swift-outdated"
-  url "https://github.com/kiliankoe/swift-outdated/archive/refs/tags/0.14.0.tar.gz"
-  sha256 "15551cf280b6262a573f4818d641aa8c526a1558efb2b40f4fc20e7805481a90"
+  url "https://github.com/kiliankoe/swift-outdated/archive/refs/tags/0.15.0.tar.gz"
+  sha256 "1b44f8be65f80dbdf6de561955c046cfec761eb7bb8ca49415d4a4c0885540b3"
   license "MIT"
   head "https://github.com/kiliankoe/swift-outdated.git", branch: "main"
 
