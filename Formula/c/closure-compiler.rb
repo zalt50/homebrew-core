0a1750553c135aba55457dcbee22f617a6f7758f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-30 23:19:12 +0000

    closure-compiler 20260629

--- Formula/c/closure-compiler.rb
+++ Formula/c/closure-compiler.rb
@@ -1,8 +1,8 @@
 class ClosureCompiler < Formula
   desc "JavaScript optimizing compiler"
   homepage "https://developers.google.com/closure/compiler"
-  url "https://search.maven.org/remotecontent?filepath=com/google/javascript/closure-compiler/v20260628/closure-compiler-v20260628.jar"
-  sha256 "c88778fa954eea68a1705e1682078d0da553cea2894d96f16061a8083ec691a4"
+  url "https://search.maven.org/remotecontent?filepath=com/google/javascript/closure-compiler/v20260629/closure-compiler-v20260629.jar"
+  sha256 "e231dbbdec34739b7fddee3d1a060b5653bd31ba30ca213610d44e1ba7573d71"
   license "Apache-2.0"
 
   livecheck do
