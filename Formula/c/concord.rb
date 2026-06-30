8c11dea9aeef8739cf6bc619b879bbee80ceae64
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-30 15:23:28 +0000

    concord: update 2.2.10 bottle.

--- Formula/c/concord.rb
+++ Formula/c/concord.rb
@@ -6,12 +6,12 @@ class Concord < Formula
   license "GPL-3.0-only"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5c02fd76764896749f1fbd8421e92c41e89aa15ed9f853735a843930d7aefd28"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "05fca502fd56eba509bd70d1e4865295bce3cc2cac3585b88a1b3c13323672af"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8ed3db239d8110aa3087ffca175d8cac38fe967921f34345388fd434195be47d"
-    sha256 cellar: :any_skip_relocation, sonoma:        "bbe6a2a0541f4c142c0b4fa7258c56831f9f0edfbac368555d9303c0c357f98c"
-    sha256 cellar: :any,                 arm64_linux:   "36003e001bbc677f8af91ec269815b54996dc3be9620ecf4b127e97b995aa9a8"
-    sha256 cellar: :any,                 x86_64_linux:  "c53df1dc30e35e868a1f8b1acc4e79f8b05f12d62ec9de8e9fa33e3315bbaf0c"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "702e5beae5ad562ae59976da6e1946955fb551944f6e4b63ca8afff6b9099393"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ffd6afd78e76dcde54064e4e3a04cc45294d1341b662e1471cf524d569035419"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a2cc72af62cfe3a36b012d12f80d7f1cd1fe9d2775dab4d62e48dcfa32b34319"
+    sha256 cellar: :any_skip_relocation, sonoma:        "36e003f6c79dd31d86acc0275e118053affd8126ce25b1fc87893bc3c2847c4b"
+    sha256 cellar: :any,                 arm64_linux:   "4107743ccd2b69e4f9879c465a548845b1cbfacbd9e1fc463f0e69b1b5367169"
+    sha256 cellar: :any,                 x86_64_linux:  "93094c68777a8e294c2098b03b7b9c4a740869cd7d179fe4384806a39a747d22"
   end
 
   depends_on "pkgconf" => :build
