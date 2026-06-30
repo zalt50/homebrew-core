9d01b79af34aa0b3e2b33dd55b0796e932018c77
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-30 15:31:16 +0000

    webpack 5.108.2

--- Formula/w/webpack.rb
+++ Formula/w/webpack.rb
@@ -3,8 +3,8 @@ require "json"
 class Webpack < Formula
   desc "Bundler for JavaScript and friends"
   homepage "https://webpack.js.org/"
-  url "https://registry.npmjs.org/webpack/-/webpack-5.108.1.tgz"
-  sha256 "2a37258ef2d49be353864ee1893ca145986496fe910ed18ef6338258c6958f12"
+  url "https://registry.npmjs.org/webpack/-/webpack-5.108.2.tgz"
+  sha256 "469c9145a3c748dda34604508e5cef530f110c6a736ecca94df5a85df2523abc"
   license "MIT"
   head "https://github.com/webpack/webpack.git", branch: "main"
 
