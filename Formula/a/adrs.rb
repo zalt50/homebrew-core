c5a8942a28d2ad279493734a2b029924a1bbeaf8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-01 03:07:02 +0000

    adrs: add 0.8.0 bottle.

--- Formula/a/adrs.rb
+++ Formula/a/adrs.rb
@@ -6,6 +6,15 @@ class Adrs < Formula
   license any_of: ["MIT", "Apache-2.0"]
   head "https://github.com/joshrotenberg/adrs.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "25a8d71cdc87d5d3792f88d6344b4a38fab55382b8c8100dd3aca8bce438c881"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "af435878d082dba204060157a50ac6a641dfbb9c716736b852d4083d8606869e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7add0f6863ddd48b4e4e5005ea83c7fcfde18514226aab7f524deed3b1df2096"
+    sha256 cellar: :any_skip_relocation, sonoma:        "b6cee37fb358e7389f884497a52ef755baa1d2203c364e0def0530c9d0df2e23"
+    sha256 cellar: :any,                 arm64_linux:   "44f77564216bfbb72de0ab368ff934257a53a5d80dded67befc2ceee11978324"
+    sha256 cellar: :any,                 x86_64_linux:  "e9ac2898367b53e304f8ad1fb59d07fda48af45a13284680b00b79697857a386"
+  end
+
   depends_on "rust" => :build
 
   def install
