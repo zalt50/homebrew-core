b5a2419b15c6a94e4ab7dee265a1c6ac9a3d625d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-01 10:16:30 +0000

    jackett: update 0.24.2151 bottle.

--- Formula/j/jackett.rb
+++ Formula/j/jackett.rb
@@ -7,12 +7,12 @@ class Jackett < Formula
   head "https://github.com/Jackett/Jackett.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "700692fb4e4a12000a5f61d48b7d5d7a5da43ccec896068e20c2781804b27446"
-    sha256 cellar: :any, arm64_sequoia: "092f2e71136914a1af358edeb777529743c6c190c5d2f71a967aa8c14fca2972"
-    sha256 cellar: :any, arm64_sonoma:  "21a9edc176356379b0266b862e624f1e880c568c3d615036ac863a8ab0981bec"
-    sha256 cellar: :any, sonoma:        "30a223a18b8e56dc90804f3f377f32eb13d85da0bf557a9a6b7ea28e39e4efd4"
-    sha256 cellar: :any, arm64_linux:   "145d359391a52d057ffceab3a920ebb2a662cebfa0e72670278f3d46f947c634"
-    sha256 cellar: :any, x86_64_linux:  "d8e16374ccfacd3cf0fef753dd5d75bea0a25b95b3a93cb9302bbafa15198873"
+    sha256 cellar: :any, arm64_tahoe:   "28e64dce22d57d401c925bcbc8857b1244afd0a98d8bfa0035778bf8e0795a84"
+    sha256 cellar: :any, arm64_sequoia: "286d58bb8e8f0f94a1552c81552d3e6d92c6d95e9791be7179baef63ecce4f5c"
+    sha256 cellar: :any, arm64_sonoma:  "c5da8c8fd86e9c81ecb9f70e76bcb183d9b1c0c5decb846930722866b92a4363"
+    sha256 cellar: :any, sonoma:        "28692bbedb18b3bee1f0ea9fd03d37675c2132314c8ac8a7687f518447d2c757"
+    sha256 cellar: :any, arm64_linux:   "b07a4f18c0f5175e8941b1e2f8d7de710c4a33873fc6dc9f0f8f090a787b1b54"
+    sha256 cellar: :any, x86_64_linux:  "86563ce5a404b433f29eea9f81ccd0f5256c07053f3840bea807a6ee9e928955"
   end
 
   # Aligned to .NET dependency. Can remove if updated to latest .NET
