d8f181f7b0129978208cc2782eeae24ea5ac300f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-01 03:31:02 +0000

    zinit 3.15.0

--- Formula/z/zinit.rb
+++ Formula/z/zinit.rb
@@ -1,8 +1,8 @@
 class Zinit < Formula
   desc "Flexible and fast Zsh plugin manager"
   homepage "https://zdharma-continuum.github.io/zinit/wiki/"
-  url "https://github.com/zdharma-continuum/zinit/archive/refs/tags/v3.14.0.tar.gz"
-  sha256 "4707baaad983d2ea911b4c2fddde9e7876593b3dc969a5efd9d387c9e3d03bb3"
+  url "https://github.com/zdharma-continuum/zinit/archive/refs/tags/v3.15.0.tar.gz"
+  sha256 "41e2861d81da3cacc7aacdae6a34daad22ed44be7695ba868cbe0bdae281d69f"
   license "MIT"
   head "https://github.com/zdharma-continuum/zinit.git", branch: "main"
 
