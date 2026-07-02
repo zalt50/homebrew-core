fa299fe57db8f5d21519ae78267987b16f7f5438
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-02 23:35:40 +0000

    gh 2.96.0

--- Formula/g/gh.rb
+++ Formula/g/gh.rb
@@ -1,8 +1,8 @@
 class Gh < Formula
   desc "GitHub command-line tool"
   homepage "https://cli.github.com/"
-  url "https://github.com/cli/cli/archive/refs/tags/v2.95.0.tar.gz"
-  sha256 "b6a1c88cbd15f49f60a68d210a117a60b349bcb0d028a0dca0ef2d9dc92bd028"
+  url "https://github.com/cli/cli/archive/refs/tags/v2.96.0.tar.gz"
+  sha256 "8d80d0aeccea7bec8024f8c30365bbfa76852901f2b2cb0afb7ab2cbf6d317c2"
   license "MIT"
   compatibility_version 1
   head "https://github.com/cli/cli.git", branch: "trunk"
