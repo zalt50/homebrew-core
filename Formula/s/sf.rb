01f9a3fef1e36f8e6618524e06ac22519ede67ce
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-02 18:08:13 +0000

    sf 2.141.6

--- Formula/s/sf.rb
+++ Formula/s/sf.rb
@@ -1,8 +1,8 @@
 class Sf < Formula
   desc "Command-line toolkit for Salesforce development"
   homepage "https://developer.salesforce.com/tools/salesforcecli"
-  url "https://registry.npmjs.org/@salesforce/cli/-/cli-2.140.6.tgz"
-  sha256 "f007804673c76cd9bbd733482393a927ca222879f1220f40c0fa04bbd72810ea"
+  url "https://registry.npmjs.org/@salesforce/cli/-/cli-2.141.6.tgz"
+  sha256 "cf867c7217697d9d36db5a95fb64c2b93cc56e0d9119a8c0554105ac82e78bc2"
   license "BSD-3-Clause"
 
   livecheck do
