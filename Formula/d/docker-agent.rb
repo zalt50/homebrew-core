2aef112443c66307aa4653ec404248b5a511751f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-02 16:39:04 +0000

    docker-agent: update 1.96.0 bottle.

--- Formula/d/docker-agent.rb
+++ Formula/d/docker-agent.rb
@@ -7,12 +7,12 @@ class DockerAgent < Formula
   head "https://github.com/docker/docker-agent.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "624d9b3c0338e0d0cd2fc3b90c92988085ca21dbc5448d3bb600bfa7c26ed6aa"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e504be61fcba9cf8cda983996c20ecc70e4554d16d9eb910322883c9e026457f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "756a8276b0bdbe754a2be42ba4074ff481331e469bf6937872c1727d8b0d6b6f"
-    sha256 cellar: :any_skip_relocation, sonoma:        "de3ab86b64d35390003f2de2b2c476dadeaaf3cc7c32fac849e84244e73499cf"
-    sha256 cellar: :any,                 arm64_linux:   "70a9563b772ac620ff7eedebb003472290cb70af1066cda6a2116ffdceff16dd"
-    sha256 cellar: :any,                 x86_64_linux:  "c980bc58e8fe569a270b7fdf67376e4dd961d100828ee1ba397486b35e3ebb05"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "597410e2a8d0d1ff819678e1aee694f611da63c7b16d2e4fdfbdfcd69fac92e5"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "59449bc34dd6732017b29b558f43def55493045fa991c51ddf94fbee9b8f90b4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ce654e6516394f6ac6535b693673750a643174e20c9e4a2983dd20c4ffc34215"
+    sha256 cellar: :any_skip_relocation, sonoma:        "4dda033848d10c54db7e4beb1b728c1f70fa6f42148c8fe9aea4a0b77b8273a5"
+    sha256 cellar: :any,                 arm64_linux:   "c7696f4ae8ba6f1edab456b20481642281b53ad1b9300e235be8c6309c0497e8"
+    sha256 cellar: :any,                 x86_64_linux:  "15dab904968965b4acc98a607accdd70722ed7bfc3c02fd2b04404fd45385d03"
   end
 
   depends_on "go" => :build
