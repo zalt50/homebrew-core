86444f64799b6b72005cc07af8d5479628335d87
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-03 15:51:02 +0000

    tofu-ls: update 0.5.2 bottle.

--- Formula/t/tofu-ls.rb
+++ Formula/t/tofu-ls.rb
@@ -12,12 +12,12 @@ class TofuLs < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5d3e4c9469b7d123a08479b4e469d7cb86ba6fd749cbe1dbfc54f1aafeecc2b8"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5d3e4c9469b7d123a08479b4e469d7cb86ba6fd749cbe1dbfc54f1aafeecc2b8"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5d3e4c9469b7d123a08479b4e469d7cb86ba6fd749cbe1dbfc54f1aafeecc2b8"
-    sha256 cellar: :any_skip_relocation, sonoma:        "aa4a272b66e223f9e7ccbd8f85029671f491c4ffe5ff723ebfb25a14c8168c74"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "0137f76821028e640f1a222c3eddd14d66ff97cbbe8b02599d56b4f4b065b8a8"
-    sha256 cellar: :any,                 x86_64_linux:  "a67cce204d57752e8f0ccf840e9b146f3171172ba9f3ff916683cdf71ec1710a"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6ffdb4125761b437a7fe5ba99c0f1b18e49e640210a41fa9b2deb7bbb1874587"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6ffdb4125761b437a7fe5ba99c0f1b18e49e640210a41fa9b2deb7bbb1874587"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6ffdb4125761b437a7fe5ba99c0f1b18e49e640210a41fa9b2deb7bbb1874587"
+    sha256 cellar: :any_skip_relocation, sonoma:        "75d1fa044bfc2ccea9027d5121366c70db92e9f6fbda29b6ccec2fbc2aa8a5c5"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "b5667421aa5a060b9ea16db9f286cf9d5330536ac0c0257a7bf12ecf48b05d7c"
+    sha256 cellar: :any,                 x86_64_linux:  "4f1ee0943fd00c465384b6d3625f3cdd5c96c158748e6617a8d17ec71439693e"
   end
 
   depends_on "go" => :build
