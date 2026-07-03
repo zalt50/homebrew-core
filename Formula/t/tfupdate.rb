64cc67e326af0da587b5fd2104e9c3acd58e5b28
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-03 04:12:50 +0000

    tfupdate: update 0.10.0 bottle.

--- Formula/t/tfupdate.rb
+++ Formula/t/tfupdate.rb
@@ -7,12 +7,12 @@ class Tfupdate < Formula
   head "https://github.com/minamijoyo/tfupdate.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "54e137ab80021f4b6670263861576db2ece07d6005cfb971161395124b9dfe75"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "54e137ab80021f4b6670263861576db2ece07d6005cfb971161395124b9dfe75"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "54e137ab80021f4b6670263861576db2ece07d6005cfb971161395124b9dfe75"
-    sha256 cellar: :any_skip_relocation, sonoma:        "04f3264e317c2c9f427afc7767fccf79adba8144fd8fba4073afba2f2e24f90a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "c42528ba18879ab1e4f979ccf25a1ce066f8db25b01813273d7e1b12ed6c868b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3df17d25feb77ddbf81f811f40664c60091d6345c6b951e49c15befeaa4422b5"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "272a3ea75cbbcb0eb2bf07db01e2c252fa0d882f53fd1177da9d051e4d42dfe7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "272a3ea75cbbcb0eb2bf07db01e2c252fa0d882f53fd1177da9d051e4d42dfe7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "272a3ea75cbbcb0eb2bf07db01e2c252fa0d882f53fd1177da9d051e4d42dfe7"
+    sha256 cellar: :any_skip_relocation, sonoma:        "f656e6eb88b5cc7addcbc0f0860161040ce6b822eb743abcd32b1960b60d3655"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "2ced5adf6d96a9db07d2c1b99eff7847356d6f60e5162fcf3c9cfc92e0df517c"
+    sha256 cellar: :any,                 x86_64_linux:  "1799ee47013e5027b1858d49a746ee39264cb53fa5c40643584876adf4173031"
   end
 
   depends_on "go" => :build
