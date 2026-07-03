19a5d2dda10ba2a4beb91e5af3675add229f9f10
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-03 03:16:31 +0000

    lanraragi 0.9.80

--- Formula/l/lanraragi.rb
+++ Formula/l/lanraragi.rb
@@ -1,10 +1,9 @@
 class Lanraragi < Formula
   desc "Web application for archival and reading of manga/doujinshi"
   homepage "https://lrr.tvc-16.science"
-  url "https://github.com/Difegue/LANraragi/archive/refs/tags/v.0.9.71.tar.gz"
-  sha256 "4dab46dddd2c227bd0428eef4318cad673fca1e2a1420eee1fa2110043827408"
+  url "https://github.com/Difegue/LANraragi/archive/refs/tags/v.0.9.80.tar.gz"
+  sha256 "25c36e844054cf750d13de7f67d0124b225da540b877b88becb657db0aeff287"
   license "MIT"
-  revision 1
   head "https://github.com/Difegue/LANraragi.git", branch: "dev"
 
   bottle do
