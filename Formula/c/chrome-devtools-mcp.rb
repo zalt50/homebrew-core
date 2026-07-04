351b8fbb2df7dda9790a0c2c4c5daf8e5b71b9d6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-04 14:25:12 +0000

    chrome-devtools-mcp 1.5.0

--- Formula/c/chrome-devtools-mcp.rb
+++ Formula/c/chrome-devtools-mcp.rb
@@ -1,8 +1,8 @@
 class ChromeDevtoolsMcp < Formula
   desc "Chrome DevTools for coding agents"
   homepage "https://github.com/chromedevtools/chrome-devtools-mcp"
-  url "https://registry.npmjs.org/chrome-devtools-mcp/-/chrome-devtools-mcp-1.4.0.tgz"
-  sha256 "d2d44d9a73da4992082c1f1685fbc4a1369417670d4a9948d30ecbef41419999"
+  url "https://registry.npmjs.org/chrome-devtools-mcp/-/chrome-devtools-mcp-1.5.0.tgz"
+  sha256 "065788d2755517488f9f362b5eef4316607d138348c23641fe170f85203c5457"
   license "Apache-2.0"
 
   bottle do
