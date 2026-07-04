ea35c41fef2827d8d83a391cfcdb7ace9ecec9bd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-04 14:44:48 +0000

    chrome-devtools-mcp: update 1.5.0 bottle.

--- Formula/c/chrome-devtools-mcp.rb
+++ Formula/c/chrome-devtools-mcp.rb
@@ -6,7 +6,7 @@ class ChromeDevtoolsMcp < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "8cd2174c514d8c00936d58a0277351f33922951ef95063162acb48c21f1b7f96"
+    sha256 cellar: :any_skip_relocation, all: "70575dc9d53d0b14b901238440182562adc72eb5d510175fe8c550f69615ea8f"
   end
 
   depends_on "node"
