fdc84dc23ca0ffe7674df35befb0d35cf01b3952
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-05 17:56:15 +0000

    vapoursynth-bestsource 19

--- Formula/v/vapoursynth-bestsource.rb
+++ Formula/v/vapoursynth-bestsource.rb
@@ -1,9 +1,8 @@
 class VapoursynthBestsource < Formula
   desc "Audio/video source and FFmpeg wrapper"
   homepage "https://github.com/vapoursynth/bestsource"
-  url "https://github.com/vapoursynth/bestsource.git",
-      tag:      "R18",
-      revision: "68f857839f0d78f9261edc875b77adc26821fbe1"
+  url "https://github.com/vapoursynth/bestsource/archive/refs/tags/R19.tar.gz"
+  sha256 "bbd391dd2725ae68994e0627f4a57b484d37b6e8d150276f07122e89fb6cda5d"
   license "MIT"
   head "https://github.com/vapoursynth/bestsource.git", branch: "master"
 
@@ -21,6 +20,7 @@ class VapoursynthBestsource < Formula
     sha256               x86_64_linux:  "cb5ae4c2ad0c0b435b2e4591f2407b76fc1472a0423bea9f7d833ee496bae4a0"
   end
 
+  depends_on "avisynthplus" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkgconf" => :build
@@ -29,17 +29,31 @@ class VapoursynthBestsource < Formula
   depends_on "vapoursynth"
   depends_on "xxhash"
 
+  resource "libp2p" do
+    url "https://github.com/sekrit-twc/libp2p/archive/869fa993041f9f3af7d9ac8b10158920c6ddce66.tar.gz"
+    sha256 "cacef2683a19a8b288cf567544b507f7f06dd66160db566bf3349e5ee0b73d90"
+  end
+
+  deny_network_access!
+
   def python3 = "python3.14"
 
   def install
     ENV.runtime_cpu_detection if Hardware::CPU.intel?
 
+    resource("libp2p").stage("subprojects/libp2p")
+    (buildpath/"subprojects/libp2p").install "subprojects/packagefiles/libp2p/meson.build"
+
+    # upstream expects a subproject, but we can build with our avisynthplus instead
+    avisynth_pc = formula_opt_lib("avisynthplus")/"pkgconfig/avisynth.pc"
+    (buildpath/"pkgconfig").install_symlink avisynth_pc => "avisynthplus.pc"
+    ENV.append_path "PKG_CONFIG_PATH", buildpath/"pkgconfig"
+
     # Work around Homebrew's python prefix patch
     args = %W[-Dpython.platlibdir=#{prefix/Language::Python.site_packages(python3)}]
 
-    args << "--force-fallback-for=avisynthplus,libp2p" if build.head?
-
-    system "meson", "setup", "build", *args, *std_meson_args
+    # Using nodownload as nofallback with `--force-fallback-for` can download HEAD git repos
+    system "meson", "setup", "build", *args, *std_meson_args, "--wrap-mode=nodownload"
     system "meson", "compile", "-C", "build", "--verbose"
     system "meson", "install", "-C", "build"
   end
