1e216e7ad52a568692af38658d65bf83f35e0097
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-05 03:37:18 +0000

    algernon: update 1.17.10 bottle.

--- Formula/a/algernon.rb
+++ Formula/a/algernon.rb
@@ -13,12 +13,12 @@ class Algernon < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "82bf51039681cae7b732e91e05a90287f231e52bed1ea5f3bb2500fab4ef8901"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "82bf51039681cae7b732e91e05a90287f231e52bed1ea5f3bb2500fab4ef8901"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "82bf51039681cae7b732e91e05a90287f231e52bed1ea5f3bb2500fab4ef8901"
-    sha256 cellar: :any_skip_relocation, sonoma:        "a637a28aadf3afe682c997dfc3672c0d980d7752c3185aa12631ada02ac6ce18"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ce1729ae56c53fb27a6dc95ce52a6944216a27adc533ed38ef64bac921b214d6"
-    sha256 cellar: :any,                 x86_64_linux:  "f58b176d399d64ad4e2adb87fa0a75009a89a64c1ceefd2e92155efdaf030994"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c621cd706b6e625233e3aed1cc86844d44d8e8a877c30c1dd121af228977e7c0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c621cd706b6e625233e3aed1cc86844d44d8e8a877c30c1dd121af228977e7c0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c621cd706b6e625233e3aed1cc86844d44d8e8a877c30c1dd121af228977e7c0"
+    sha256 cellar: :any_skip_relocation, sonoma:        "d2f046b292231b90c945d6d443a5d5aebf7684c514b69d3078c2a31ab929e788"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "6b56edd3136bda26f7f8bc9235d37c6e9895baecd34b46fcd7a49ac536d5d890"
+    sha256 cellar: :any,                 x86_64_linux:  "1bf07cf4d35fbb67c870b69ce143eec4bb515946135c1b4ae1c8bf4d769deff2"
   end
 
   depends_on "go" => :build
