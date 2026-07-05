48bdf31af41b2c21aeaa7ef6e373bdf02e95e12e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-05 14:35:48 +0000

    zero: add 0.1.0 bottle.

--- Formula/z/zero.rb
+++ Formula/z/zero.rb
@@ -6,6 +6,15 @@ class Zero < Formula
   license "MIT"
   head "https://github.com/Gitlawb/zero.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "dd977cffd79355eee27f5c032ca5b6b145e511b687c9f6a89249f9af4aef58cc"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "dd977cffd79355eee27f5c032ca5b6b145e511b687c9f6a89249f9af4aef58cc"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "dd977cffd79355eee27f5c032ca5b6b145e511b687c9f6a89249f9af4aef58cc"
+    sha256 cellar: :any_skip_relocation, sonoma:        "8c12f58c2c548569204666e1e16938cc3ae85d217ee86858e7269940ea762025"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "7ba3f438d6c9ae4ae7e06a00373e5240b42e59a30e21813a16d216f281f6bd1a"
+    sha256 cellar: :any,                 x86_64_linux:  "0d5bad4f986feeb2db4d38bf3517776f6579be7c747a1f324a6c40daa9434deb"
+  end
+
   depends_on "go" => :build
 
   def install
