b4028c14c816cf805901a43674c11291615e3739
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-05 23:48:35 +0000

    oh-my-posh: update 29.20.1 bottle.

--- Formula/o/oh-my-posh.rb
+++ Formula/o/oh-my-posh.rb
@@ -15,12 +15,12 @@ class OhMyPosh < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d376210edb1c8a65eac90caa4fcfb22ff7abf907bbf27394f173ef09284653ca"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "54507985b8ac80315a725e922ee423b55e4937c8b18e3e9cf280b27c0b453db8"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1336af90528b39651e8746bb449091a5fd2adf916b317215b64025c430fce8b9"
-    sha256 cellar: :any_skip_relocation, sonoma:        "fd95e6a55122b962c2fb5d005f398b0fba3d4222dd0e352e07e4a3c223ab7be0"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a485ca1ad0323fe00416a4f6941036d5400ddb398e6ce52e8ba71139cf581389"
-    sha256 cellar: :any,                 x86_64_linux:  "01a07dda363f688cf251409dc079dbe08a96bd8fc8a9590cbf1958603fdaacb8"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4311813f7f4226539a207f40c9590771f1d4a419e6b6e413403c058cc53d6c7c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b72ec936bdc93a3c52e37df1f7d1eeaa952109aa1dcf4e16f144e7a67142a732"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d3b14612d1608a57ee1ac877ba2d5fc5a775e0ea922e96b26094526f693a411c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "83fb23f7dfe9ac1f2bb7493806002d1448c78233ae96e7062415241bb667c073"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1895da49911f5dfb00bfce241089252817744d3eb692b604dedcc5219eb63407"
+    sha256 cellar: :any,                 x86_64_linux:  "993c5287a8d1bdc05e53297b07eaf6691b53cbb9462565cf69b3288e401f23eb"
   end
 
   depends_on "go" => :build
