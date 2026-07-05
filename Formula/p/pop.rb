85871a057368ef18279f400242ccbe78570a7b97
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-05 18:52:31 +0000

    pop: update 0.3.0 bottle.

--- Formula/p/pop.rb
+++ Formula/p/pop.rb
@@ -6,12 +6,12 @@ class Pop < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ad6867f02fbd078db70ca3e9d0ad9a1cf898cfc5902c856974881f98be4b73b5"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ad6867f02fbd078db70ca3e9d0ad9a1cf898cfc5902c856974881f98be4b73b5"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ad6867f02fbd078db70ca3e9d0ad9a1cf898cfc5902c856974881f98be4b73b5"
-    sha256 cellar: :any_skip_relocation, sonoma:        "7d93db3b51ad72d659059e42b9f601f859315e024198b345c9c09c4118c1bb9c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "475f0a5e99e18179d93990a2ff2357921874264e85f9be96c5d4962c9da116d7"
-    sha256 cellar: :any,                 x86_64_linux:  "1dc73545affea029e10ca4dc13a56fcefef922b2b4c5dcb3cdf11741bcd15580"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cc8ec1da3efb44e70714d7bad03473f0292b5ce736f8b9c0b9343afd3b5cfe44"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1d83a780dd2ddcaf2ff3888f6a2ad5236c87dc7850bf9b7a0acb0337465ae203"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9e3f092967e61d33f452c3501b9c35c04e2272c1eb6105c69b1271f5ff19400a"
+    sha256 cellar: :any_skip_relocation, sonoma:        "52083121e6d4264bde585e0844039e9a8c6d0bea4c8f9c92a21d6b5d2f7ad350"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "767ef62db135a2419bd3d719bc5e5b668a87e7ccdc18de7a35e66beabbf42ad2"
+    sha256 cellar: :any,                 x86_64_linux:  "cd7b586c2ac4ca0835197aac98bf14b604a58ef1b4c7ef29811101f2ea87efb4"
   end
 
   depends_on "go" => :build
