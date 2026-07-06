718ca0960e40129f8c61d6ea9fe545591dfb9813
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-06 15:12:59 +0000

    cimg: update 4.0.2 bottle.

--- Formula/c/cimg.rb
+++ Formula/c/cimg.rb
@@ -11,7 +11,7 @@ class Cimg < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "931e57725d73e55c7257f61602768fbccebd280fba5ba4ea24e9762b1e401618"
+    sha256 cellar: :any_skip_relocation, all: "ab4a5de713d20a3acaf257072e7202e92b79f856211db4a7bd356b8850f81d16"
   end
 
   def install
