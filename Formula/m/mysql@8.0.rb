b6ccc6df8f16ad14699492cad20ddb53f4287285
Michael Cho <michael@michaelcho.dev>
2026-07-06 23:51:56 -0400

    mysql@8.0: replace postinstall message with caveat

--- Formula/m/mysql@8.0.rb
+++ Formula/m/mysql@8.0.rb
@@ -138,17 +138,11 @@ class MysqlAT80 < Formula
     init_data_dir "mysql", using: :mysql_initialize
   end
 
-  def post_install
-    if (my_cnf = ["/etc/my.cnf", "/etc/mysql/my.cnf"].find { |x| File.exist? x })
-      opoo <<~EOS
-        A "#{my_cnf}" from another install may interfere with a Homebrew-built
-        server starting up correctly.
-      EOS
-    end
-  end
-
   def caveats
     <<~EOS
+      A "/etc/my.cnf" or "/etc/mysql/my.cnf" from another install may interfere
+      with a Homebrew-built server starting up correctly.
+
       We've installed your MySQL database without a root password. To secure it run:
           mysql_secure_installation
 
