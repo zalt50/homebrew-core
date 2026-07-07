6890e2c3d79dc43d3bb0aeccdf61dd1fec319092
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-07 02:33:34 +0000

    tmuxai: update 2.3.1 bottle.

--- Formula/t/tmuxai.rb
+++ Formula/t/tmuxai.rb
@@ -6,12 +6,12 @@ class Tmuxai < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "eb9253da71b8ae6fdb9aba4e43248d24b8a979a68ef82c3021ed35b05e002948"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "eb9253da71b8ae6fdb9aba4e43248d24b8a979a68ef82c3021ed35b05e002948"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "eb9253da71b8ae6fdb9aba4e43248d24b8a979a68ef82c3021ed35b05e002948"
-    sha256 cellar: :any_skip_relocation, sonoma:        "f22fb71c776cb6c6d9cc8e070e61794a1084bec4193b6a87a816524febc1be32"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "293a33de8e58f38a695edb8271f38f97e945cd9373d89dbdbe0ff9a0ec11f91b"
-    sha256 cellar: :any,                 x86_64_linux:  "38b5316876f61e36ab5aaa2c924724883ab2703e495389461f6bd4dada8a0650"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ba6970fe914485b01129d6616002006e283a8ea11c83d075d571935b72c334aa"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ba6970fe914485b01129d6616002006e283a8ea11c83d075d571935b72c334aa"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ba6970fe914485b01129d6616002006e283a8ea11c83d075d571935b72c334aa"
+    sha256 cellar: :any_skip_relocation, sonoma:        "ffaa2841e0ac7d4de8494659505e0ce7af9ab6635f0b1b29e1af9b3d075b970e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "dc887ae39907b63ad85b1074631cc6e0c4de75f18e42190ffae15dd201d0a6aa"
+    sha256 cellar: :any,                 x86_64_linux:  "c192c379b3328c7e59b94cc9926490e9f2963ce705870bca55694c4ee914b65a"
   end
 
   depends_on "go" => :build
