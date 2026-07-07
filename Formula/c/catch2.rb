ec2fd2caecc2825087c52a1ea307619b829c7b0f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-07 20:33:50 +0000

    catch2 3.15.2

--- Formula/c/catch2.rb
+++ Formula/c/catch2.rb
@@ -1,8 +1,8 @@
 class Catch2 < Formula
   desc "Modern, C++-native, test framework"
   homepage "https://github.com/catchorg/Catch2"
-  url "https://github.com/catchorg/Catch2/archive/refs/tags/v3.15.1.tar.gz"
-  sha256 "be23a52b85cf04cd9587612147a10b023d59ed9757fa1843cc99e615d6c0893c"
+  url "https://github.com/catchorg/Catch2/archive/refs/tags/v3.15.2.tar.gz"
+  sha256 "acfae120892c2b67a74142d36d060c0caa96f1c3aaa8aabd96e19961163d0420"
   license "BSL-1.0"
   head "https://github.com/catchorg/Catch2.git", branch: "devel"
 
