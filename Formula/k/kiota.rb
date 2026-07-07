8534454997515f140f30fb5a5ae6c3d204656ec2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-07 02:25:15 +0000

    kiota: update 1.33.0 bottle.

--- Formula/k/kiota.rb
+++ Formula/k/kiota.rb
@@ -7,12 +7,12 @@ class Kiota < Formula
   head "https://github.com/microsoft/kiota.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b4139a63b84f05fc7072b3e6f5230d29464d89c6641df1993b6f524c80ba5260"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ea15809948684b9f97521f5e628343038e2b326555ed95b7b1ce0ea2f40440cd"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ea7a9635bf6192000b6a4ccf04c0fb46d932634d6d3b3b2bd631beb6cf2b760d"
-    sha256 cellar: :any_skip_relocation, sonoma:        "b8a51533849a10787cdddf28839a5a170f164dfc2aab23254dbfebcda70d08e2"
-    sha256 cellar: :any,                 arm64_linux:   "94f78d614d5fc0f2aad9c8b8259435b4c8e648a9533817e56525deac17265378"
-    sha256 cellar: :any,                 x86_64_linux:  "13f651d154db18b6ac5acea1b44d05aea42987333b0b575065f0cff31b2bc3db"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "df4226b0b4daf6dbdc85cb33c818b1a0f6df17558fe655961d7464bac14745f7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "92f5ce19aeeb9e6d4054826ddc84a88dc75b64e6ded2dfe0af1969a1b9bc511d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c0e50d452973202a2f1184f369764e030e23f769e578cdcc32004747dbcfcedd"
+    sha256 cellar: :any_skip_relocation, sonoma:        "82db3bb1be7e083ee7ba44edffce107e0173c4950e34c351c760df2539818d10"
+    sha256 cellar: :any,                 arm64_linux:   "1d9afd05231adba3d01cd7ec18d47b03ddac37653429e74d733fa154db39283d"
+    sha256 cellar: :any,                 x86_64_linux:  "690b71360219447f7e6af67835e332b89f16f11266d9b9fe2abfbaa59c1e41ba"
   end
 
   depends_on "dotnet"
