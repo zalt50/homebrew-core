231aac8115bb4b653a41970bb0ad9a8575fb361f
botantony <antonsm21@gmail.com>
2026-07-08 01:08:12 +0300

    spek: revision bump (wxwidgets 3.3.3)
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Formula/s/spek.rb
+++ Formula/s/spek.rb
@@ -4,7 +4,7 @@ class Spek < Formula
   url "https://github.com/alexkay/spek/releases/download/v0.8.5/spek-0.8.5.tar.xz"
   sha256 "1bccf85a14a01af8f2f30476cbad004e8bf6031f500e562bbe5bbd1e5eb16c59"
   license "GPL-3.0-or-later"
-  revision 6
+  revision 7
 
   bottle do
     sha256 cellar: :any,                 arm64_tahoe:   "35bf9a140676a8e6205c91c540decde724994782b3159cfae796d354e976e20f"
