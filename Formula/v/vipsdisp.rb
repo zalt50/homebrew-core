00683ace4402f4fdf77e1cffa6e2b61eab3d57fe
Michael Cho <michael@michaelcho.dev>
2026-07-06 22:42:57 -0400

    vipsdisp: use post_install_steps

--- Formula/v/vipsdisp.rb
+++ Formula/v/vipsdisp.rb
@@ -40,9 +40,9 @@ class Vipsdisp < Formula
     system "meson", "install", "-C", "build"
   end
 
-  def post_install
-    system "#{formula_opt_bin("glib")}/glib-compile-schemas", "#{HOMEBREW_PREFIX}/share/glib-2.0/schemas"
-    system "#{formula_opt_bin("gtk4")}/gtk4-update-icon-cache", "-f", "-t", "#{HOMEBREW_PREFIX}/share/icons/hicolor"
+  post_install_steps do
+    compile_gsettings_schemas
+    gtk_update_icon_cache
   end
 
   test do
