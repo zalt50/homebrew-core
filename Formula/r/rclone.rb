c452e03fa2ca2789d6a137014a5103f9798a036c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-08 18:09:18 +0000

    rclone 1.74.4

--- Formula/r/rclone.rb
+++ Formula/r/rclone.rb
@@ -1,8 +1,8 @@
 class Rclone < Formula
   desc "Rsync for cloud storage"
   homepage "https://rclone.org/"
-  url "https://github.com/rclone/rclone/archive/refs/tags/v1.74.3.tar.gz"
-  sha256 "3ba8bc7fb216f8f0307357ac67842467f453050468d5751e9269954819148568"
+  url "https://github.com/rclone/rclone/archive/refs/tags/v1.74.4.tar.gz"
+  sha256 "b8279a31a5249e4aecf04acff744ace4a2e3a169e4539a24aa67a9994f645d3b"
   license "MIT"
   compatibility_version 1
   head "https://github.com/rclone/rclone.git", branch: "master"
