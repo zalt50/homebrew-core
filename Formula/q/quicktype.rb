ce689c6af083f13784041eecb2e096dea67f6a04
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-08 15:53:25 +0000

    quicktype 23.3.22

--- Formula/q/quicktype.rb
+++ Formula/q/quicktype.rb
@@ -1,8 +1,8 @@
 class Quicktype < Formula
   desc "Generate types and converters from JSON, Schema, and GraphQL"
   homepage "https://app.quicktype.io"
-  url "https://registry.npmjs.org/quicktype/-/quicktype-23.3.21.tgz"
-  sha256 "9e61d2e05f0ebbe4ce52dea2ee3b700611ef24fb835e8b560b620ebb6cef5df7"
+  url "https://registry.npmjs.org/quicktype/-/quicktype-23.3.22.tgz"
+  sha256 "be157311f93b295f26c3f7d2da459dc3d55b1cf7ffd75a13cca917566b95b136"
   license "Apache-2.0"
 
   bottle do
