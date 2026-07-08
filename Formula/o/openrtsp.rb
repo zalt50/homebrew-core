ee2364fab5d939daa4f27b28130e94cfad391ade
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-08 03:15:33 +0000

    openrtsp 2026.07.08

--- Formula/o/openrtsp.rb
+++ Formula/o/openrtsp.rb
@@ -1,11 +1,11 @@
 class Openrtsp < Formula
   desc "Command-line RTSP client"
   homepage "http://www.live555.com/openRTSP"
-  url "https://download.live555.com/live.2026.07.06.tar.gz"
-  mirror "https://download.videolan.org/pub/videolan/testing/contrib/live555/live.2026.07.06.tar.gz"
-  mirror "https://pkg.freebsd.org/ports-distfiles/live.2026.07.06.tar.gz"
+  url "https://download.live555.com/live.2026.07.08.tar.gz"
+  mirror "https://download.videolan.org/pub/videolan/testing/contrib/live555/live.2026.07.08.tar.gz"
+  mirror "https://pkg.freebsd.org/ports-distfiles/live.2026.07.08.tar.gz"
   # Keep a mirror as upstream tarballs are removed after each version
-  sha256 "1f2cfd356a4baf6539bd76110b67e5058fd99d44ee983d6fff73e4756ecf817e"
+  sha256 "2e56491e6b5359a3f2cde89a693c01fb312cc279b6da1362194e4cb6c7cdfa35"
   license "LGPL-3.0-or-later"
 
   livecheck do
