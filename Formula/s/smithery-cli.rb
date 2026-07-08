4669b887a6f01318565bd2571168486805420a19
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-08 18:21:24 +0000

    smithery-cli: add 4.11.1 bottle.

--- Formula/s/smithery-cli.rb
+++ Formula/s/smithery-cli.rb
@@ -5,6 +5,10 @@ class SmitheryCli < Formula
   sha256 "2d3a60fbbd9c2db4ffe80e806afa2075620a3b18b4a80cb26eb48501d76dfd3e"
   license "AGPL-3.0-or-later"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "7953d52a012f6401ce602007ddf769460798fc32feb2e35a60ca9c64e897ee27"
+  end
+
   depends_on "node"
 
   def install
