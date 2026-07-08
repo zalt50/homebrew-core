a524f363a0814539a736e05add4e6ff4f8153782
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-08 13:58:35 +0000

    salmon: add 2.3.1 bottle.

--- Formula/s/salmon.rb
+++ Formula/s/salmon.rb
@@ -6,6 +6,15 @@ class Salmon < Formula
   license "BSD-3-Clause"
   head "https://github.com/COMBINE-lab/salmon.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "21e77fb856e48124a68c7c8b6ea554923075608e8e1c38397d77ab7823bb5719"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "86c6cecc11a546fda3770b36eb07518f2a142991df5fa2c15facd2912a6abc1f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "495e7b6a2584afa35eed6653bc9d47e40cab6c8ccd0245a4bae97d1085f5f371"
+    sha256 cellar: :any_skip_relocation, sonoma:        "77333f3d635113404bea6eb1fe01a2913e0d0cca289554c345e29a600f1ddceb"
+    sha256 cellar: :any,                 arm64_linux:   "2665a819fb2547c3c772ef606336af60625372fcb74dedb913c5cca3e0dca1c4"
+    sha256 cellar: :any,                 x86_64_linux:  "cb441d70f05dc4bb1e7c44455be2828d5eda3af632d11edb373a755ae37ad5aa"
+  end
+
   depends_on "rust" => :build
 
   def install
