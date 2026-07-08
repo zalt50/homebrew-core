6916b66e31b2c67ba39cba68191abddf031577cc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-08 06:19:18 +0000

    netlify-cli 26.2.0

--- Formula/n/netlify-cli.rb
+++ Formula/n/netlify-cli.rb
@@ -1,8 +1,8 @@
 class NetlifyCli < Formula
   desc "Netlify command-line tool"
   homepage "https://www.netlify.com/docs/cli"
-  url "https://registry.npmjs.org/netlify-cli/-/netlify-cli-26.1.0.tgz"
-  sha256 "ce8d2c284b20e2bd1f054ad3652fd9e098d32911c90da48c2b695eadfced39ff"
+  url "https://registry.npmjs.org/netlify-cli/-/netlify-cli-26.2.0.tgz"
+  sha256 "741d46f0f18df96a8d3ee27614f51bfa529d2f9937a1122976358e54e40f6747"
   license "MIT"
 
   bottle do
