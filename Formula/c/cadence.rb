7b095401736b1c0a7d4187284dabe866baed84e0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-09 20:31:04 +0000

    cadence 1.10.5

--- Formula/c/cadence.rb
+++ Formula/c/cadence.rb
@@ -1,8 +1,8 @@
 class Cadence < Formula
   desc "Resource-oriented smart contract programming language"
   homepage "https://cadence-lang.org/"
-  url "https://github.com/onflow/cadence/archive/refs/tags/v1.10.4.tar.gz"
-  sha256 "1676685d0041d37afa3f98bd09766a6e65b67c05f8544e1b142f7878bcf999d7"
+  url "https://github.com/onflow/cadence/archive/refs/tags/v1.10.5.tar.gz"
+  sha256 "e161d0bc823887fb43b7104162637d22c8d35659093f51ffec23b74879f80fae"
   license "Apache-2.0"
   head "https://github.com/onflow/cadence.git", branch: "master"
 
