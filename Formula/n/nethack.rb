62189b12031780d601e3aa61dbf9df53ed844f71
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-09 22:03:26 +0000

    nethack: update 5.0.0_1 bottle.

--- Formula/n/nethack.rb
+++ Formula/n/nethack.rb
@@ -37,13 +37,12 @@ class Nethack < Formula
   no_autobump! because: :incompatible_version_format
 
   bottle do
-    rebuild 1
-    sha256 arm64_tahoe:   "64c1e5e3881ad027b0863dec469eb70bb17c96d2682b0ca111c472e27ea685ef"
-    sha256 arm64_sequoia: "cbe525013079e0b7036e3eb1ce549b738fe9a973e9d49a6209f2be4eaaad9f71"
-    sha256 arm64_sonoma:  "ae526f9bb987bd3fd402f6eba3bb9ff12fe17d32db301e9649ece36c3e14bfc0"
-    sha256 sonoma:        "dabec95206e3a906c55552727a6bbd346daa31ba7b8a9e7310ccf7ff915456c3"
-    sha256 arm64_linux:   "27674cebca8e1f2cdaafe515b2562f4fe5e363d56b24915db408b7d59278101b"
-    sha256 x86_64_linux:  "23c92a6f3d645059c8624d380fd8c2a31d995d902029a9589614a24db22b6a44"
+    sha256 arm64_tahoe:   "4ed420ea146cea8d1db4e8db8b21471a5058ece3e714696f754765365264f115"
+    sha256 arm64_sequoia: "6c778031601539eaa003d5350ef383311832157e313c75b2f95ce2e726e924cf"
+    sha256 arm64_sonoma:  "62417badde3fbbd385613140aefd6f72271cf24397cdc2f713cfcc1d96cfb8b1"
+    sha256 sonoma:        "e66b26d59fdb22fe8c8b5516cfb56771ebbcd4893e37c99326b3a073c8c06b9d"
+    sha256 arm64_linux:   "7eed963a9502ddabe4314e4c06afdc60453b5e7d4d9172605946081ea888a15c"
+    sha256 x86_64_linux:  "a7e677736166fa625a926028c41097d40535b6dd8ac19dd6ad27aabf027ee623"
   end
 
   depends_on "groff" => :build
