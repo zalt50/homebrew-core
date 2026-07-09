5130e0a1a7d12705c6673b359f97e66c7de80dfa
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2026-07-09 23:55:42 +0900

    btfs: revision bump (libtorrent-rasterbar 2.1.0)

--- Formula/b/btfs.rb
+++ Formula/b/btfs.rb
@@ -4,6 +4,7 @@ class Btfs < Formula
   url "https://github.com/johang/btfs/archive/refs/tags/v3.1.tar.gz"
   sha256 "c363f04149f97baf1c5e10ac90677b8309724f2042ab045a45041cfb7b44649b"
   license "GPL-3.0-only"
+  revision 1
   head "https://github.com/johang/btfs.git", branch: "master"
 
   bottle do
@@ -20,8 +21,15 @@ class Btfs < Formula
   depends_on :linux # on macOS, requires closed-source macFUSE
   depends_on "openssl@3"
 
+  # Build against libtorrent-rasterbar 2.1 (ABI 2)
+  patch do
+    url "https://github.com/johang/btfs/commit/9502625df2d1c0a7e70da2e75de81078de95318b.patch?full_index=1"
+    sha256 "7f5c6974d4902b7cda3876e22e24c44a0a9ac933b16ff4b97fdbd39adbbbe7f8"
+    type :unofficial
+    resolves "https://github.com/johang/btfs/pull/109"
+  end
+
   def install
-    ENV.cxx11
     system "autoreconf", "--force", "--install", "--verbose"
     system "./configure", "--disable-silent-rules", *std_configure_args
     system "make", "install"
