8a47be3a5274525bdd92454f1911d3c977e8a486
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-09 22:31:05 +0000

    rawtoaces: add 2.1.1 bottle.

--- Formula/r/rawtoaces.rb
+++ Formula/r/rawtoaces.rb
@@ -5,6 +5,15 @@ class Rawtoaces < Formula
   sha256 "69e846978935ee2fb9751f3604f71d212b8db4aed40d64660a9d68cd3c8e7ac1"
   license "Apache-2.0"
 
+  bottle do
+    sha256 arm64_tahoe:   "d7b38645cb3f73a2639b0795478e69de32c713503b5fd507f05e2373aa1c4e0f"
+    sha256 arm64_sequoia: "e6954930c57d107f10e997e9bb3c83de9b717cffa31284d9f4791549b9f2b0b5"
+    sha256 arm64_sonoma:  "63211d091edf57ab78e703bb416ba42abfa9b2baf270ec34d83acfa89a413055"
+    sha256 sonoma:        "9059be7ff2157c5d11762bfdb1099e420ae59481e4403a23c42cd5204ce5a6a7"
+    sha256 arm64_linux:   "9f32bd9670be5be8c94efadd6c78339c50eb88fa7faec894fe8703edcf56c218"
+    sha256 x86_64_linux:  "0c277ca3cb3f63e5e531674b69997470712cf96ba33a00b08367366c356bba45"
+  end
+
   depends_on "cmake" => :build
   depends_on "nlohmann-json" => :build
   depends_on "pkgconf" => :build
