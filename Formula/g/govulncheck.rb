6384cb9106897ad2d127cb8c3a08435b7cfde0f6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-09 19:22:48 +0000

    govulncheck: update 1.6.0 bottle.

--- Formula/g/govulncheck.rb
+++ Formula/g/govulncheck.rb
@@ -7,12 +7,12 @@ class Govulncheck < Formula
   head "https://github.com/golang/vuln.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ac1cd789fe007590e13925c718966934b8a4fd16e38a00298bb6e2a6a4172b5d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ac1cd789fe007590e13925c718966934b8a4fd16e38a00298bb6e2a6a4172b5d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ac1cd789fe007590e13925c718966934b8a4fd16e38a00298bb6e2a6a4172b5d"
-    sha256 cellar: :any_skip_relocation, sonoma:        "280ad2be788ebf90c7f4602aa363bad0a47d54f511dfb6ceaa38e08a374c9b94"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "097815854d5a3a482375431b7cd7974d6a970524eb912cb4fe3b508c44624e41"
-    sha256 cellar: :any,                 x86_64_linux:  "4e8bfb5665ab423ad36253e640a9082bd18663046fb3043b66848b3bc9037c98"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3ce3ef2c7ee9471bf980f13386092cb8654dc18a92ef496a4b09f56f276f152f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3ce3ef2c7ee9471bf980f13386092cb8654dc18a92ef496a4b09f56f276f152f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3ce3ef2c7ee9471bf980f13386092cb8654dc18a92ef496a4b09f56f276f152f"
+    sha256 cellar: :any_skip_relocation, sonoma:        "74716a77803293ddb2ed331dbf010e38882a575d3b2871c34e81081907498389"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e8183b30f602d11264f94398633401078cf97a8c85b5db197667b2d54d823282"
+    sha256 cellar: :any,                 x86_64_linux:  "7cd2a3110967c2c555d05862b2e878b6bb16f729baa1a4651b55929a3be57972"
   end
 
   depends_on "go" => [:build, :test]
