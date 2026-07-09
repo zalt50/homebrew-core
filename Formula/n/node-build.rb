54bf7802afc30a0bc3adf3d52e46f065cc7daebe
botantony <antonsm21@gmail.com>
2026-07-09 14:57:29 +0300

    node-build 5.4.45

--- Formula/n/node-build.rb
+++ Formula/n/node-build.rb
@@ -1,8 +1,8 @@
 class NodeBuild < Formula
   desc "Install NodeJS versions"
   homepage "https://github.com/nodenv/node-build"
-  url "https://github.com/nodenv/node-build/archive/refs/tags/v5.4.44.tar.gz"
-  sha256 "a595d329ff1c6226c94f41688475eed8660e5b283f92f17ff20f158eb3fd58af"
+  url "https://github.com/nodenv/node-build/archive/refs/tags/v5.4.45.tar.gz"
+  sha256 "00d28b09c74363c3cd7432ac2a3ff53d0710444893df09cf7b64b90b0074725a"
   license "MIT"
   compatibility_version 1
   head "https://github.com/nodenv/node-build.git", branch: "main"
