321faf25a0f5d127c8fb9609f0480b7baa9b4093
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-10 15:24:22 +0000

    mx 7.83.3

--- Formula/m/mx.rb
+++ Formula/m/mx.rb
@@ -1,8 +1,8 @@
 class Mx < Formula
   desc "Command-line tool used for the development of Graal projects"
   homepage "https://github.com/graalvm/mx"
-  url "https://github.com/graalvm/mx/archive/refs/tags/7.83.2.tar.gz"
-  sha256 "ebea9e3a6e1d035b00b58a140f8c5786190774e771d10355a35652ea742d2c82"
+  url "https://github.com/graalvm/mx/archive/refs/tags/7.83.3.tar.gz"
+  sha256 "9eebffcc39b84c92202f67cfd7cfe58b622b154c43cf7b09a0b2ba142271b2f5"
   license "GPL-2.0-only"
 
   livecheck do
