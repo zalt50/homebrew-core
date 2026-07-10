f25d035fb482a70fd6c3a8ab6d3db275eeec9253
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-10 02:53:13 +0000

    depot 2.101.70
    * depot 2.101.70
    * depot: update test
    
    Closes #292309.
    
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/d/depot.rb
+++ Formula/d/depot.rb
@@ -1,8 +1,8 @@
 class Depot < Formula
   desc "Build your Docker images in the cloud"
   homepage "https://depot.dev/"
-  url "https://github.com/depot/cli/archive/refs/tags/v2.101.69.tar.gz"
-  sha256 "e39ce4aea211fcbe329283c12e3fbb79f2a5dd477fe791fe8e5582797c874bb0"
+  url "https://github.com/depot/cli/archive/refs/tags/v2.101.70.tar.gz"
+  sha256 "9e5bb232409486412176003f17d5fd35e8af71d1a3ca85c66bdc3ba8306be52a"
   license "MIT"
   head "https://github.com/depot/cli.git", branch: "main"
 
@@ -40,6 +40,6 @@ class Depot < Formula
   test do
     assert_match version.to_s, shell_output("#{bin}/depot --version")
     output = shell_output("#{bin}/depot list builds 2>&1", 1)
-    assert_match "Error: unknown project ID", output
+    assert_match "unknown project ID", output
   end
 end
