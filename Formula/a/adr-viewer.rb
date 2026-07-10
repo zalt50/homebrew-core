ec2e8ee3b2ac07a62d9d0b8ecf092c28ead1179d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-10 10:17:21 +0000

    adr-viewer: update 1.4.0_6 bottle.

--- Formula/a/adr-viewer.rb
+++ Formula/a/adr-viewer.rb
@@ -9,12 +9,12 @@ class AdrViewer < Formula
   revision 6
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8b12cb08799dcf6fb78b0bf55eb9601f6ddb14be34a69422e74f9c4377b6e6c5"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d5ebeb6ec7d5ef62ce1fbd71fd64e5cae43c0308200fb6607b67da929df7d452"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0e2c1f99a51f6e897fd58f048c90df94a1ce1cf03a6c6e61cc5fb15710b67602"
-    sha256 cellar: :any_skip_relocation, sonoma:        "48cfd793dcde485c4e19cdc8ca5488ea30282829ec888f9dd83fdf562ab84e47"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "410d4f2c715a9d2a1afe0522ecd8b249c2c8a607a86d2f946051fe6d5aba1923"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b267fa080e8d5b1750951fa2ae1b18c11687fa2af57dd9a5bc7032d2e71bdc69"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "152ad9b0751d066f89191be23d6292cdfd9dd892d64074f509697329e550757d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c8429f4d6db6d40ad61add9e0bf1033735b4d2cda769e528708a024c45e65d2e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f585a716bffc66ff9fe6e96aa692e05485fd70bcc590326cb6be40c3ac6059a7"
+    sha256 cellar: :any_skip_relocation, sonoma:        "2ab4cf5120f5ad48331844075457672fba7acc9b49ce5f4299f1c6739967100c"
+    sha256 cellar: :any,                 arm64_linux:   "69d8faa722ff32fff16a0b2fd6d9c875bf1251535dfb6874f86e13c43c3a5537"
+    sha256 cellar: :any,                 x86_64_linux:  "5893cba966f2ba67a474d57f204bb69c484306333171f06db047641219738236"
   end
 
   depends_on "python@3.14"
