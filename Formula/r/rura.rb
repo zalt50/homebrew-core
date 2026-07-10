5f4056af5f8e8149022969c58cd78ba1b87aef3c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-10 19:47:17 +0000

    rura 1.9.0

--- Formula/r/rura.rb
+++ Formula/r/rura.rb
@@ -1,8 +1,8 @@
 class Rura < Formula
   desc "Interactive TUI scratchpad for building shell pipelines"
   homepage "https://github.com/tlipinski/rura"
-  url "https://github.com/tlipinski/rura/archive/refs/tags/v1.8.0.tar.gz"
-  sha256 "6716b12debfb9970f82d098fb573ff011b9eab879e0c7b908527bcb4d83bad59"
+  url "https://github.com/tlipinski/rura/archive/refs/tags/v1.9.0.tar.gz"
+  sha256 "53f7ee8e24cfce1bcf1c7c51239db7f493a696f7b94e6d2519016599c560b025"
   license "MIT"
 
   bottle do
