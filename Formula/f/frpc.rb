6fffe28d58567cc28a40ed901a72e979f24076bb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-11 14:28:39 +0000

    frpc 0.70.0

--- Formula/f/frpc.rb
+++ Formula/f/frpc.rb
@@ -1,8 +1,8 @@
 class Frpc < Formula
   desc "Client app of fast reverse proxy to expose a local server to the internet"
   homepage "https://github.com/fatedier/frp"
-  url "https://github.com/fatedier/frp/archive/refs/tags/v0.69.1.tar.gz"
-  sha256 "79a62c1071ddb947e95146ad7b1cb8b25f182fed548a4a8a68d5fca06b37502c"
+  url "https://github.com/fatedier/frp/archive/refs/tags/v0.70.0.tar.gz"
+  sha256 "661554e336407880c444bf148b904f62f49c9442c4d71f3a2b0840dfbb12d678"
   license "Apache-2.0"
   head "https://github.com/fatedier/frp.git", branch: "dev"
 
