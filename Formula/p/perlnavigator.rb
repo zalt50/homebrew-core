63ec84a94f1c8df8aec671be9d66b4cbc46337d9
Tahmid Khan <60913202+ta7mid@users.noreply.github.com>
2026-07-11 15:26:44 +0600

    perlnavigator 0.8.20 (new formula)

---
+++ Formula/p/perlnavigator.rb
@@ -0,0 +1,30 @@
+class Perlnavigator < Formula
+  desc "Perl language server"
+  homepage "https://github.com/bscan/PerlNavigator"
+  url "https://registry.npmjs.org/perlnavigator-server/-/perlnavigator-server-0.8.20.tgz"
+  sha256 "2e50dead7e169f865902ef6e447761705f9ef07b89df0bc5b754ff3d484112db"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    require "open3"
+
+    request = {
+      jsonrpc: "2.0",
+      id:      1,
+      method:  "initialize",
+      params:  { rootUri: nil, capabilities: {} },
+    }.to_json
+
+    Open3.popen3(bin/"perlnavigator", "--stdio") do |stdin, stdout|
+      stdin.write "Content-Length: #{request.bytesize}\r\n\r\n#{request}"
+      assert_match(/^Content-Length: \d+/i, stdout.readline)
+    end
+  end
+end
