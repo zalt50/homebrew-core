31628ad6df41d555d4f9d5a08638a552a0e9e710
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-11 12:14:24 +0000

    prek: update 0.4.9 bottle.

--- Formula/p/prek.rb
+++ Formula/p/prek.rb
@@ -7,12 +7,12 @@ class Prek < Formula
   head "https://github.com/j178/prek.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "742ed48cc787357cc2ca623d0df5e752379c3ac0aec63e7a59352bc758d330d2"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9c96e5aa17506d78a1aecd86ceffddee62fbe2ad8b46acb9090958d3f80bbc47"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8a34261872eb5ec3f4e8ed5e761c3904633d906230991775daa9b76db2e04e83"
-    sha256 cellar: :any_skip_relocation, sonoma:        "0f00af4e2d404c7b88d2fffccdae95ed2f66bf74ce5da5263894dbaa2d6dd0cb"
-    sha256 cellar: :any,                 arm64_linux:   "368dd551ebd28176ee190fbf695a80022b18307918576584813adc608fdfa35c"
-    sha256 cellar: :any,                 x86_64_linux:  "26512688ac8b51619b271a243ec06465cd952cbdb2ffacc88a2715c053ea6a04"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "38e094c8586b0f69a0f39e48d0b817b9a5b259b395f8cb860d6c167ffc773f73"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cf983b4efe1c63c9ac7c30f0190f4ca5fda5633a15a3528c5966011ce082b011"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4c380255d1471669d2ec43f2eda3ed2ce49d15af0c6899bd94159be119ae6ec6"
+    sha256 cellar: :any_skip_relocation, sonoma:        "93f65c62351fbb7bbdde9a9cf86b0848f6bfa28eb27799369718a8175b9413cb"
+    sha256 cellar: :any,                 arm64_linux:   "a0d8d72af8d9415febf3159fdcd3bc9b356248937062cb10e61de51b243b86fb"
+    sha256 cellar: :any,                 x86_64_linux:  "5b091284e215ebf0e4102dd3b293dc9667f35c26e032d97781cb23dbf8322ac4"
   end
 
   depends_on "rust" => :build
