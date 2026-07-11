69ba504125a21afee230310af7a37be762284aff
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-11 18:00:29 +0000

    goimapnotify: add 2.5.6 bottle.

--- Formula/g/goimapnotify.rb
+++ Formula/g/goimapnotify.rb
@@ -6,6 +6,15 @@ class Goimapnotify < Formula
   license "GPL-3.0-or-later"
   head "https://gitlab.com/shackra/goimapnotify.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f95781ffbd87f18943ee5e89a71a16f54abf0369714bd772b7a1d82402d99dbb"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f95781ffbd87f18943ee5e89a71a16f54abf0369714bd772b7a1d82402d99dbb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f95781ffbd87f18943ee5e89a71a16f54abf0369714bd772b7a1d82402d99dbb"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a719ca0b44aae13f4f5565da2debd8bd0cad746c24414faed90d5b80331ac950"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "de795319b932c7e4f5dbb7fb83ac04999120d7a9ab1465b7ac4c0675e85720b0"
+    sha256 cellar: :any,                 x86_64_linux:  "3b02ecd13d04763ebebacf077fad9e0fb3bee56d8f1c776a500429ae36ea1545"
+  end
+
   depends_on "go" => :build
 
   def install
