019e20e91029e70706731c9a1e6d44dcc194941f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-11 17:43:06 +0000

    diffoscope: update 324 bottle.

--- Formula/d/diffoscope.rb
+++ Formula/d/diffoscope.rb
@@ -8,12 +8,12 @@ class Diffoscope < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c304a76cc97b40aae3598a2ad399c48a2cee28128ee793f1a7e2c3b2da9a5211"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c304a76cc97b40aae3598a2ad399c48a2cee28128ee793f1a7e2c3b2da9a5211"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c304a76cc97b40aae3598a2ad399c48a2cee28128ee793f1a7e2c3b2da9a5211"
-    sha256 cellar: :any_skip_relocation, sonoma:        "79b37d40d506531020978d6da98863b0b10c3af19b8582da8e64d80981bf150c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "df1bd65d168e37e0651f68eb9697130c0300b352c68e08be9ba2814a054294ad"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "df1bd65d168e37e0651f68eb9697130c0300b352c68e08be9ba2814a054294ad"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ef55173ab656a7dfb08fa72d0a012a17ae163099532967176316de7b4b9e7bd7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ef55173ab656a7dfb08fa72d0a012a17ae163099532967176316de7b4b9e7bd7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ef55173ab656a7dfb08fa72d0a012a17ae163099532967176316de7b4b9e7bd7"
+    sha256 cellar: :any_skip_relocation, sonoma:        "341c8b4e83b98b59b674643ffadaa9153f98b615b55d61014947186e3daa578c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e211882a3554e89d988a36761b496fab330bcb0d6c67c865cf6fdf8efb09426b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e211882a3554e89d988a36761b496fab330bcb0d6c67c865cf6fdf8efb09426b"
   end
 
   depends_on "libarchive"
