dc021712da6b104f76f080bacc39e348b037bcc7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-11 21:39:05 +0000

    steamguard-cli: update 0.18.3 bottle.

--- Formula/s/steamguard-cli.rb
+++ Formula/s/steamguard-cli.rb
@@ -7,12 +7,12 @@ class SteamguardCli < Formula
   head "https://github.com/dyc3/steamguard-cli.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c3051b5915ad5d2eeadd416e10d845fa8d0afb9888e90e88dbc6a5c2c07b16bb"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "283a860a41c2678b489b929b39a53a50a6685ffde51ae5cdec479d0cb5bebe9e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "78f666b5117aa58c6b6022abde9a89d3c01fb3bff78fb05dfa5e9761962d7763"
-    sha256 cellar: :any_skip_relocation, sonoma:        "312fe142210e875c62813885cde814427deec87603bc478ab8a5403b20d0a249"
-    sha256 cellar: :any,                 arm64_linux:   "cd5bbd9459185a9cbb7149ce8335df26fb726af3c9a222a784ef8cd1b732f6fe"
-    sha256 cellar: :any,                 x86_64_linux:  "1f3f82041feece9dff813ca14ee73da512d507e2e8723f655bfd822e0337b401"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c843e3bbd3379b3ea11594ecdd83901b13006aa6c8541fbd20f0ac31c176851a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0af453aa27b4122832cb2cf2132ebcfb56691d2dfab6ae5054bd90fecec6b40a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "934a35c092e88d3be14d128cb95e913ff55c483c0e50e9e55fbf62a88be54174"
+    sha256 cellar: :any_skip_relocation, sonoma:        "0a57713b5f7eb04cea229a94c6b39a541923fab9ee0abb94b1b4815bf5bd9f47"
+    sha256 cellar: :any,                 arm64_linux:   "9da602e8519a0f48c5715b9f89882ef11286e8641cea637da705e1585d1d8dfa"
+    sha256 cellar: :any,                 x86_64_linux:  "d15bd2e2a3a9b3c87486ea478e9e78aafe7c76606dbaf9a4af304bbbd9e477d8"
   end
 
   depends_on "rust" => :build
