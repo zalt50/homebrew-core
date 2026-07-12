7016713413c87f1fcf43409f893daf4964ae518b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-12 00:17:38 +0000

    jsrepo: update 3.8.1 bottle.

--- Formula/j/jsrepo.rb
+++ Formula/j/jsrepo.rb
@@ -6,12 +6,12 @@ class Jsrepo < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "7a94ab660d761e17f36e0a682c7722d925e7e59f57d7dd65b0ba8ec50d98cc6d"
-    sha256 cellar: :any,                 arm64_sequoia: "1da67b31ebda86db8b4edb872f2582064fa423f975ed54ed369e6c5ea3edb401"
-    sha256 cellar: :any,                 arm64_sonoma:  "1da67b31ebda86db8b4edb872f2582064fa423f975ed54ed369e6c5ea3edb401"
-    sha256 cellar: :any,                 sonoma:        "0d9be6230acf1ff1b784e85add44aafc807069a4882a74fee56db3442a9fc17e"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "402efb13e7ba3d1db23a4af5dc7673f2254387c7074e398fd5947869a4c1dfd0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "133d7b665d25c5e29db6bb356f0059d1fad864d91982e962e3c6c9af64ddb642"
+    sha256 cellar: :any,                 arm64_tahoe:   "60738222814804c43a0a3331d69615b2d01105bbd169bc1e0c729be72d4716a9"
+    sha256 cellar: :any,                 arm64_sequoia: "f5930ad2a456251b7b1e8c29403e2d05ad13f397cfca338a49c453d27af10741"
+    sha256 cellar: :any,                 arm64_sonoma:  "f5930ad2a456251b7b1e8c29403e2d05ad13f397cfca338a49c453d27af10741"
+    sha256 cellar: :any,                 sonoma:        "80cb4ca3fefd204c3cec450725dacce894ee17356bfe7af930c35a8237709e9f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "9f3bb2af1fd0dc7efc12c69d52f5ed4dec8ae9fb3e835f556b543f9e703976c9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "80102f13fe2ed38139d2a6f557851ab3d30f94886ea2e34e09d05335754c68f2"
   end
 
   depends_on "node"
