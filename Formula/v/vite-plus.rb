1f433cf37858c523ed6a2e62f3b61bb7a47f54a3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-12 00:26:50 +0000

    vite-plus: update 0.2.4 bottle.

--- Formula/v/vite-plus.rb
+++ Formula/v/vite-plus.rb
@@ -7,12 +7,12 @@ class VitePlus < Formula
   head "https://github.com/voidzero-dev/vite-plus.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "8665d09f2c8d972a472448884820b9355e41d1aaf7f137586262841cb9552fee"
-    sha256 cellar: :any, arm64_sequoia: "b6614e403237661780af51b8672df57617afb04b408723f2854f91d703b9eb59"
-    sha256 cellar: :any, arm64_sonoma:  "d60043a4a7937aee0ef96bcbc8b47355b08751ef602467c5798914995af1da92"
-    sha256 cellar: :any, sonoma:        "ec02d73c4f1e12a78b8e425d8208d5ab47a12443bc491ec27460da21c7a494ab"
-    sha256               arm64_linux:   "f96f5d1ff290ece17662ea4f0ae4a7978326225ffc38409f0212409c54f5fb12"
-    sha256               x86_64_linux:  "a64d17555235833cce623249a229ed52885855892276d5c73416d3c0caf2861b"
+    sha256 cellar: :any, arm64_tahoe:   "fd53e30679dd750bf1636cfc4ac8c43d66e3f586c104cd27f8a12d0b9a03567d"
+    sha256 cellar: :any, arm64_sequoia: "137415c142dd99705b8528e28e7a57f103078ff5fd78d6e12a0640663ecbf41b"
+    sha256 cellar: :any, arm64_sonoma:  "d34b1e5894fd32ede8cebbf22381fdd941837f51bfba00e0a29ca6d3267c7841"
+    sha256 cellar: :any, sonoma:        "d65e14d4a1a6d036c6e11d420acbec0c6b95cf48e2098021510eb91a1cab3d7e"
+    sha256               arm64_linux:   "3fab748e0f0789eb85e5901a6d76f3b7c9a2b84b50658f556321b0b73380ce12"
+    sha256               x86_64_linux:  "8b4e7bb3bba7ef6ff268ea57281eff4b5daffa4f56019c3697b04c737c424a6f"
   end
 
   depends_on "cmake" => :build
