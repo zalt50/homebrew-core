b7262c531672afb6627211ee8e1fccd7438f0b10
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-13 17:38:56 +0000

    fizz 2026.07.13.00

--- Formula/f/fizz.rb
+++ Formula/f/fizz.rb
@@ -1,8 +1,8 @@
 class Fizz < Formula
   desc "C++14 implementation of the TLS-1.3 standard"
   homepage "https://github.com/facebookincubator/fizz"
-  url "https://github.com/facebookincubator/fizz/archive/refs/tags/v2026.07.06.00.tar.gz"
-  sha256 "05b890bbfa7824bd66bb4edcd64bcfb86eb0351d2931ef46679275d78cf79622"
+  url "https://github.com/facebookincubator/fizz/archive/refs/tags/v2026.07.13.00.tar.gz"
+  sha256 "19ac460edd73f0fab68a637fcf55672c1e270c5596684dd726d72e9fadae7e12"
   license "BSD-3-Clause"
   compatibility_version 1
   head "https://github.com/facebookincubator/fizz.git", branch: "main"
