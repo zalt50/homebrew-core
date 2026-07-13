05759ee73beeea62321c046ce6d1ea7048943145
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-13 05:20:37 +0000

    supermodel: update 0.3a-20260712-git-fef2b38 bottle.

--- Formula/s/supermodel.rb
+++ Formula/s/supermodel.rb
@@ -8,13 +8,12 @@ class Supermodel < Formula
   head "https://github.com/trzy/Supermodel.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any, arm64_tahoe:   "8caefa29784e10bec7b0d5c2b56224c335fd17cb108c30a71ad0484839d4f361"
-    sha256 cellar: :any, arm64_sequoia: "30a0c3e5fcb08ea841e4e02532cfa3502ed76b1a09fc763fd78693b4456cb033"
-    sha256 cellar: :any, arm64_sonoma:  "4fccf349cd37905cde410dd78c971284bdd8dfc9057ac7446dcc9ac521adf17c"
-    sha256 cellar: :any, sonoma:        "bad3083a9e53bc18a86cc1b25f8930b28bcc8defccc5c254347ec15b5c76ffd6"
-    sha256 cellar: :any, arm64_linux:   "7942ee2e3317ff8f134b3a102644782dc1ec17f2720110727355728a38f80e9d"
-    sha256 cellar: :any, x86_64_linux:  "d743cefb87a3c9a5c3c0b27a161e31925a7353666752c65caaecf96f72c7ad49"
+    sha256 cellar: :any, arm64_tahoe:   "a5e421c80ffe138c793556b9dad8ca976fd1d81486dfc959286a10066db9cea8"
+    sha256 cellar: :any, arm64_sequoia: "e00faf7ccd57bd5fe182c2aae74356a9750620c2651322a44e3eb68a7f5cea0d"
+    sha256 cellar: :any, arm64_sonoma:  "e23579edefa3dd6d1a5e8f88561203f15b399205001cb9c1cda4ef77e70a46dd"
+    sha256 cellar: :any, sonoma:        "7a3c0f6824aae404196523aa60c07179c291614b25e03989b7ff4ca1c7f47351"
+    sha256 cellar: :any, arm64_linux:   "18fe8b43894670d490dda20c96ac0ff53e8ab14fbd960a1e449b11f8591d5948"
+    sha256 cellar: :any, x86_64_linux:  "c0306fec6b1a9ada44bbfea7afbdb4120d59e04ab236c93e49017f89d91883ae"
   end
 
   depends_on "sdl2-compat"
