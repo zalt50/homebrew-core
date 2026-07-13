f7fcfea405e63f5415a1dcad62d5193a2e6a5eb1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-13 02:36:58 +0000

    cython: update 3.2.8 bottle.

--- Formula/c/cython.rb
+++ Formula/c/cython.rb
@@ -9,12 +9,13 @@ class Cython < Formula
   head "https://github.com/cython/cython.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "be70819a19262dac44cf976ab33523a774cf17cd87835ebb79fd7aa725ec625e"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6441816b31243f0a22b5bdb9205ab38dc26d108cbf2450c22ab470ec781e5967"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "643f1cfb0695386195931e8203d7585400083cc8a954c83d3b7b06113e45e450"
-    sha256 cellar: :any_skip_relocation, sonoma:        "955748e5cac120bf4a5390b3d6c2b1f7f3f9402d2cf39eb7af397f5252463f36"
-    sha256 cellar: :any,                 arm64_linux:   "80f95bf2ca4a8fe962ccd937ff248987f4e70f413e79d6c4f7173e7011399796"
-    sha256 cellar: :any,                 x86_64_linux:  "da644e004b46422958b2fcc2fbcce737d0f6c3193895e73ebe3a3ed565615d65"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "dee3caa30fd60b55f5e48e5980e34d504a819282445c13618c77a5ac12e4074f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4cecf57851f1a107f05be70108eaaae75d35e6975225a50ece15825f3d3afa90"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8f96e8610eaf2b33349af4d0e6cd4b0e00b833f646fd0b64c3cc04152303526d"
+    sha256 cellar: :any_skip_relocation, sonoma:        "c4c903f7a5f57346add8b09bab97ceb1a4becc0e14bd30010cdabc0b4dc1839d"
+    sha256 cellar: :any,                 arm64_linux:   "162d1860c8f19d1b0f05b43988aaf9a4ee48cafab1a883d3c9d93c3dd9e1eedb"
+    sha256 cellar: :any,                 x86_64_linux:  "0a2bf44dfb82d24457b02eaafa2f8aa4e4bfaff818c0aae24f87342095cb0311"
   end
 
   depends_on "python@3.14"
