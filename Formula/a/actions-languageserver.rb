06fa94e59a0026b1bf132d5f5fe29a4119806f6b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-13 20:11:24 +0000

    actions-languageserver 0.3.59

--- Formula/a/actions-languageserver.rb
+++ Formula/a/actions-languageserver.rb
@@ -1,8 +1,8 @@
 class ActionsLanguageserver < Formula
   desc "Language server for GitHub Actions YAML files"
   homepage "https://github.com/actions/languageservices/tree/main/languageserver"
-  url "https://github.com/actions/languageservices/archive/refs/tags/release-v0.3.58.tar.gz"
-  sha256 "83d24888f9b328aaf84a382f1fff718968df4255dca1ec097765131ae993d558"
+  url "https://github.com/actions/languageservices/archive/refs/tags/release-v0.3.59.tar.gz"
+  sha256 "fc212c5d43a153920328708fff7cee18a849f4c5220f4150878796224c664938"
   license "MIT"
 
   bottle do
