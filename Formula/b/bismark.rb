1e56e6c70c8ea5c6be3233e08ede6e354de10ba9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-13 05:08:09 +0000

    bismark: add 3.0.0 bottle.

--- Formula/b/bismark.rb
+++ Formula/b/bismark.rb
@@ -6,6 +6,15 @@ class Bismark < Formula
   license "GPL-3.0-only"
   head "https://github.com/FelixKrueger/Bismark.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "518b6eab8123951f73fa55bca7e44a5b2999236150b8242b5ebf0add60e24ca5"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "10af470234099771470b3985d418dceabd3162b18d935e5852b6223940bfb7ce"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c5836eea519b2d4da5fa8f0bb2dc7ed9ac67174bcafd9ece56d315cc22ae717c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "d5a1a3df7c383ad62b3ae2ae43d482766aaf9eacee98c8a9542608648bdaf43c"
+    sha256 cellar: :any,                 arm64_linux:   "afd805509f3fa7645e3a8fcbd7fce042253a6574b33386ea4936c62be7f12cac"
+    sha256 cellar: :any,                 x86_64_linux:  "6762ba9438e00ee612a3dc7481f086df6fff564026caf54f83776192e0efc394"
+  end
+
   depends_on "rust" => :build
   depends_on "bowtie2"
   depends_on "minimap2"
