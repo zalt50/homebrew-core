da7b4785534801f9094fcac6b6a50cbb877d38f8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-14 11:34:22 +0000

    hk 1.51.0

--- Formula/h/hk.rb
+++ Formula/h/hk.rb
@@ -3,8 +3,8 @@ class Hk < Formula
   homepage "https://hk.jdx.dev"
   # pull from git tag to get submodules
   url "https://github.com/jdx/hk.git",
-      tag:      "v1.50.0",
-      revision: "e9cc0984a1a8d639519e76ab6e329effcce42144"
+      tag:      "v1.51.0",
+      revision: "8e26af2d614f09e2c803d4ef9f699b126a188028"
   license "MIT"
   head "https://github.com/jdx/hk.git", branch: "main"
 
