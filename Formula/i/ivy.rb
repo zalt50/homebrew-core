b25e546ee9399f0e15127bb984c9fe12d6bb6890
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-15 20:53:58 +0000

    ivy: update 2.6.0 bottle.

--- Formula/i/ivy.rb
+++ Formula/i/ivy.rb
@@ -7,7 +7,7 @@ class Ivy < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "c3132187b5761ef4cf40ef487beb49fa5e3d7c963628854a4d44d10dd1741065"
+    sha256 cellar: :any_skip_relocation, all: "bf45fa90dd574a280c73748a8add44d6c3d1bcf11281d5459bda8d75df60531f"
   end
 
   depends_on "openjdk"
