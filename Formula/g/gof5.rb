46e459ea151418ed977b9c93e4656ccf192d0231
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-15 09:49:29 +0000

    gof5: add 0.1.5 bottle.

--- Formula/g/gof5.rb
+++ Formula/g/gof5.rb
@@ -6,6 +6,15 @@ class Gof5 < Formula
   license "Apache-2.0"
   head "https://github.com/kayrus/gof5.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0ee93bcf3d09a81771d580217796c75dbf33e234f202c5a010eb61522cabef99"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0ee93bcf3d09a81771d580217796c75dbf33e234f202c5a010eb61522cabef99"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0ee93bcf3d09a81771d580217796c75dbf33e234f202c5a010eb61522cabef99"
+    sha256 cellar: :any_skip_relocation, sonoma:        "4e94f9c1fa6e77ae7321a5464b93d22636fef18fd533b0638e574cc5e9cffde9"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d9829dfab109bf82fb320df186c444a8424c9935e588dd5bf59d7e8bcd747266"
+    sha256 cellar: :any,                 x86_64_linux:  "f5524a1ccc6b2245c2d819d80a8b376ca15bfecce20e0bc22dd1384ff1167841"
+  end
+
   depends_on "go" => :build
 
   def install
