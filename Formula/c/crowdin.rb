510fea000e08a7693118692cb18225d69b28f170
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-15 14:33:59 +0000

    crowdin 4.14.4

--- Formula/c/crowdin.rb
+++ Formula/c/crowdin.rb
@@ -1,8 +1,8 @@
 class Crowdin < Formula
   desc "Command-line tool that allows to manage your resources with crowdin.com"
   homepage "https://support.crowdin.com/cli-tool/"
-  url "https://github.com/crowdin/crowdin-cli/releases/download/4.14.3/crowdin-cli.zip"
-  sha256 "26d8f559c2a4245d2da1adbfc4afdc474af8fa5cf33059284271ba8c215200ce"
+  url "https://github.com/crowdin/crowdin-cli/releases/download/4.14.4/crowdin-cli.zip"
+  sha256 "74744580063e9d666ae3a5690b8b5cfc0d9047e7fbdf2a67d5a3314c8f60d45d"
   license "MIT"
 
   livecheck do
