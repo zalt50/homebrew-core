4430f614ffe4e63996989ae28c90864920b841d4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-15 20:35:22 +0000

    marmot 0.10.0

--- Formula/m/marmot.rb
+++ Formula/m/marmot.rb
@@ -1,8 +1,8 @@
 class Marmot < Formula
   desc "Open-source data catalog exposing metadata to AI agents"
   homepage "https://marmotdata.io"
-  url "https://github.com/marmotdata/marmot/archive/refs/tags/v0.9.0.tar.gz"
-  sha256 "6e6f85402045105220f9c2d91aa3d953c46a3093e530b2d55be8879e00b68c31"
+  url "https://github.com/marmotdata/marmot/archive/refs/tags/v0.10.0.tar.gz"
+  sha256 "9c06cc0d0f460cb7910ebf8de45c18b242caeccf32c7c3f55b1fb7416c515526"
   license "MIT"
   head "https://github.com/marmotdata/marmot.git", branch: "main"
 
@@ -23,7 +23,7 @@ class Marmot < Formula
   depends_on "go" => :build
 
   def install
-    ENV["CGO_ENABLED"] = "1"
+    ENV["CGO_ENABLED"] = "0"
     ldflags = %W[
       -s -w
       -X github.com/marmotdata/marmot/internal/cmd.Version=#{version}
@@ -32,7 +32,7 @@ class Marmot < Formula
   end
 
   test do
-    assert_match "marmot v#{version}", shell_output("#{bin}/marmot version")
+    assert_match version.to_s, shell_output("#{bin}/marmot version")
     assert_match "MARMOT_SERVER_ENCRYPTION_KEY", shell_output("#{bin}/marmot generate-encryption-key")
   end
 end
