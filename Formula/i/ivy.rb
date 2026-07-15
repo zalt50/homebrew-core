3f8ea01deb84b5f57a51142411bf0cc94b3a0a4f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-15 17:40:18 +0000

    ivy 2.6.0

--- Formula/i/ivy.rb
+++ Formula/i/ivy.rb
@@ -1,9 +1,9 @@
 class Ivy < Formula
   desc "Agile dependency manager"
   homepage "https://ant.apache.org/ivy/"
-  url "https://www.apache.org/dyn/closer.lua?path=ant/ivy/2.5.3/apache-ivy-2.5.3-bin.tar.gz"
-  mirror "https://archive.apache.org/dist/ant/ivy/2.5.3/apache-ivy-2.5.3-bin.tar.gz"
-  sha256 "3d41e45021b84089e37329ede433e3ca20943cb1be0235390b6ddf4a919a85af"
+  url "https://www.apache.org/dyn/closer.lua?path=ant/ivy/2.6.0/apache-ivy-2.6.0-bin.tar.gz"
+  mirror "https://archive.apache.org/dist/ant/ivy/2.6.0/apache-ivy-2.6.0-bin.tar.gz"
+  sha256 "3ada1eaadfaddf5347f1de8303d283dc535f4d8bddd7bee7b32e397c8327cb1f"
   license "Apache-2.0"
 
   bottle do
