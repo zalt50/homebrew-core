ad2f23139d6b0508a98343cd443b23df30b4f71d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-15 15:34:00 +0000

    flyline: add 1.3.0 bottle.

--- Formula/f/flyline.rb
+++ Formula/f/flyline.rb
@@ -5,6 +5,15 @@ class Flyline < Formula
   sha256 "cf6984e075286050f03e2b1550290db367cfb249b48d63d2d8e9d5d840e5ae8c"
   license any_of: ["GPL-3.0-only", "MIT"]
 
+  bottle do
+    sha256 cellar: :any, arm64_tahoe:   "d5f7ee032ee2d5cc044f74a6b4c0fa1ae188d2f154cd94906b5264f21d6dc4d1"
+    sha256 cellar: :any, arm64_sequoia: "5862c3e09a4b983ad5de8d999d08bfc95ecd489d0cf6c1a721d6b3dcc69876f8"
+    sha256 cellar: :any, arm64_sonoma:  "eed1435b29a10ec6808ab721c75e553e229d4a2df0e0a8294b2114c3ad0e67b2"
+    sha256 cellar: :any, sonoma:        "cecad69181aeda455ab7a63fbe78a93814abc6ae57354941f73375bdb837aa3a"
+    sha256 cellar: :any, arm64_linux:   "8e40c3eb4df3cfab959545df9759389187e4ea27cdca86c349c30397d8f1d614"
+    sha256 cellar: :any, x86_64_linux:  "74098069c2c4b3837db7cdfa5ddcc49f714c2ac3c37e7c56b702a5e08ed97cc6"
+  end
+
   depends_on "rust" => :build
   depends_on "bash" => :test
 
