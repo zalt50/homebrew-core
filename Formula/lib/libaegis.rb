7e0b34781852270a61abec1f2f0df564667abfbf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-15 20:53:11 +0000

    libaegis: update 0.10.3 bottle.

--- Formula/lib/libaegis.rb
+++ Formula/lib/libaegis.rb
@@ -6,12 +6,12 @@ class Libaegis < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "315bd70f2198e4989fdc6c0913bef679c76344fedf97ad39f2c1e7cbab362d5d"
-    sha256 cellar: :any, arm64_sequoia: "0c4addbd07bd7ef67ce5eb200be561bc939dd98cc49483e48b2940fd5790b372"
-    sha256 cellar: :any, arm64_sonoma:  "4b9f7c790ad7cd4436c24632cf7160ac0216ad17a2cb48bf12ae71eeb6c03e4a"
-    sha256 cellar: :any, sonoma:        "9898529d66e1ec68fce6253a46d9674141c1392e0a31ff6e62aad1b2b3e8c1b1"
-    sha256 cellar: :any, arm64_linux:   "2587e84b24305d094bec03eadbc8bf1e9111c74014664d5e5e19ebd1a72cbb7e"
-    sha256 cellar: :any, x86_64_linux:  "6c82de9044770af5161e00797bd04601a9c05d1eb564efa8c0ffac5af137920f"
+    sha256 cellar: :any, arm64_tahoe:   "9072fddecb6b077000edf40a561759d3aa53377f0e4ca0784044302740c4e299"
+    sha256 cellar: :any, arm64_sequoia: "ec09589bd139feb28aafa06ddfc3b9d966c472d79c7932926c7a3d957e0ad6a2"
+    sha256 cellar: :any, arm64_sonoma:  "82095481991eaa985c7a6f3fb2e368bd7fe942731e0fa5fa9e96877c2f84ae57"
+    sha256 cellar: :any, sonoma:        "79ef35267e08bb4e1a716371da5bb35701bb1c586e00820f75a88eaa74847a8d"
+    sha256 cellar: :any, arm64_linux:   "167e84042e9fee90f3c6ddc23211724f35f839a506cd62c715af3d639958bb86"
+    sha256 cellar: :any, x86_64_linux:  "74ccde5fcd7cdb3bf5ea1f06b79398fc0633935e8e3365e78402280091e41038"
   end
 
   depends_on "cmake" => :build
