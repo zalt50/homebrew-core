341ea38e353789899beb7a0ae4b6c0b86019adcd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-16 04:48:15 +0000

    hebcal: update 5.15.0 bottle.

--- Formula/h/hebcal.rb
+++ Formula/h/hebcal.rb
@@ -7,12 +7,12 @@ class Hebcal < Formula
   head "https://github.com/hebcal/hebcal.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "209a4013d8eaa838d014ca0edd41dcd7641c2ecc47009519dab3d6bee8e7c84f"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "209a4013d8eaa838d014ca0edd41dcd7641c2ecc47009519dab3d6bee8e7c84f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "209a4013d8eaa838d014ca0edd41dcd7641c2ecc47009519dab3d6bee8e7c84f"
-    sha256 cellar: :any_skip_relocation, sonoma:        "54ac453d24dac878dc1455e5f98329994859fbd59173d12d47a3d6230a5dbc5d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "aaacdfe4f2bf2c1864e54cef8ca469e5b2966462148d936da06a7b36ac03140e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4aa20e4937803d2794230e8713d0bcdac621c1257f43a91428611c7e75adf673"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c42a164b98c5a5efb697a90c0803d4d4360a2e5475613cdd5456ee5cd9a2a253"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c42a164b98c5a5efb697a90c0803d4d4360a2e5475613cdd5456ee5cd9a2a253"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c42a164b98c5a5efb697a90c0803d4d4360a2e5475613cdd5456ee5cd9a2a253"
+    sha256 cellar: :any_skip_relocation, sonoma:        "d267feeea5667151aee5720e32128208439d03a321ccabfd2a9487ee29825f55"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1993074fb1a23f36f0ed6f1a429f3de3252f7b134f2590357b0d4c7f3c8cadce"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bd4bf4f37c1b11cf7640e976ac5631d2a861cfdf737f3422ad2818635df2a761"
   end
 
   depends_on "go" => :build
