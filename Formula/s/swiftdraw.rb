fc23033bf36fc729eed8f5a502ed8b40caf1ff62
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-16 03:31:55 +0000

    swiftdraw 0.28.1

--- Formula/s/swiftdraw.rb
+++ Formula/s/swiftdraw.rb
@@ -1,8 +1,8 @@
 class Swiftdraw < Formula
   desc "Convert SVG into PDF, PNG, JPEG or SF Symbol"
   homepage "https://github.com/swhitty/SwiftDraw"
-  url "https://github.com/swhitty/SwiftDraw/archive/refs/tags/0.28.0.tar.gz"
-  sha256 "ac660bc69f2c25154e67b9e65f8b54327860ad51b194ef59c74a7142fb6099f9"
+  url "https://github.com/swhitty/SwiftDraw/archive/refs/tags/0.28.1.tar.gz"
+  sha256 "1f882249a98b4c22fdd13095f9c237680eba9c53c24bedbfb007e468d1665ed1"
   license "Zlib"
   head "https://github.com/swhitty/SwiftDraw.git", branch: "main"
 
