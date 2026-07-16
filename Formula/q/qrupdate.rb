7dec5b236e9ec511debd4d3d6161ca3e63f91d9a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-16 09:07:29 +0000

    qrupdate 1.2.0

--- Formula/q/qrupdate.rb
+++ Formula/q/qrupdate.rb
@@ -1,8 +1,8 @@
 class Qrupdate < Formula
   desc "Fast updates of QR and Cholesky decompositions"
   homepage "https://gitlab.mpi-magdeburg.mpg.de/koehlerm/qrupdate-ng"
-  url "https://gitlab.mpi-magdeburg.mpg.de/koehlerm/qrupdate-ng/-/archive/v1.1.5/qrupdate-ng-v1.1.5.tar.bz2"
-  sha256 "d81523077586fec9dc26585c82a6c9c109f912f29d5ec9097822faf56297c4d2"
+  url "https://gitlab.mpi-magdeburg.mpg.de/koehlerm/qrupdate-ng/-/archive/v1.2.0/qrupdate-ng-v1.2.0.tar.bz2"
+  sha256 "d3bea4ceafd7b1641ca74c50b74060aeacd7a3cb9ff4159a92aad9c262c57666"
   license "GPL-3.0-or-later"
 
   bottle do
@@ -18,23 +18,17 @@ class Qrupdate < Formula
   depends_on "gcc" # for gfortran
   depends_on "openblas"
 
-  # Backport fix for CMake 4
-  patch do
-    url "https://gitlab.mpi-magdeburg.mpg.de/koehlerm/qrupdate-ng/-/commit/5ae0333225130c1ec377e9cf10a60ad7c86c058d.diff"
-    sha256 "2396f89a55a12e65120358502a450a6752dfa6ca4fc3f00462a0510909eb33db"
-    type :backport
-  end
-
   def install
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
-    pkgshare.install "test/tch1dn.f", "test/utils.f"
+    pkgshare.install "test/tch1dn.f90", "test/utils.f90"
   end
 
   test do
-    system "gfortran", "-o", "test", pkgshare/"tch1dn.f", pkgshare/"utils.f",
+    system "gfortran", "-o", "test", pkgshare/"tch1dn.f90", pkgshare/"utils.f90",
                        "-fallow-argument-mismatch",
+                       "-I#{include}/qrupdate",
                        "-L#{lib}", "-lqrupdate",
                        "-L#{formula_opt_lib("openblas")}", "-lopenblas"
     assert_match "PASSED   4     FAILED   0", shell_output("./test")
