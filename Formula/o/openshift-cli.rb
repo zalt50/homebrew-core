e8d77125f906b320368671648731d151e78728b7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-16 20:39:37 +0000

    openshift-cli 4.22.4

--- Formula/o/openshift-cli.rb
+++ Formula/o/openshift-cli.rb
@@ -1,7 +1,7 @@
 class OpenshiftCli < Formula
   desc "OpenShift command-line interface tools"
   homepage "https://www.openshift.com/"
-  url "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.22.3/openshift-client-src.tar.gz"
+  url "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.22.4/openshift-client-src.tar.gz"
   # This project employs synchronized versioning so the sha256 may not change on version bumps
   sha256 "ccdc948c24ff7a48b59f19cbccf26a342d2156eaef864f26a336af803eb6dfe9"
   license "Apache-2.0"
