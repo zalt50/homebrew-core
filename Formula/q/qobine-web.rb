15895f7f5fb01e4819ea62f5b77219a2f439f67a
Sofus Addington <sofus@addington.dk>
2026-07-17 10:32:20 +0200

    qobine-web 2026-07-15 (new formula)

---
+++ Formula/q/qobine-web.rb
@@ -0,0 +1,25 @@
+class QobineWeb < Formula
+  desc "Server and web based player for Qobuz"
+  homepage "https://github.com/SofusA/qobine"
+  url "https://github.com/sofusA/qobine/archive/refs/tags/v2026-07-15.tar.gz"
+  sha256 "1e2f5c0353dc51609f3a4ff0feb1fc02c9da17646b1f5357e5ea6c05fd39180f"
+  license "GPL-3.0-only"
+  head "https://github.com/sofusa/qobine.git", branch: "main"
+
+  depends_on "pkgconf" => :build
+  depends_on "rust" => :build
+
+  on_linux do
+    depends_on "alsa-lib"
+    depends_on "openssl@4"
+  end
+
+  def install
+    system "cargo", "install", *std_cargo_args(path: "web-module")
+  end
+
+  test do
+    _, stdout, = Open3.popen2("#{bin}/qobine-web login")
+    assert_match "Login to Qobuz in browser...", stdout.gets("\n")
+  end
+end
