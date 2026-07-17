4807fa6a8f797462570cc78ea70d3b670d13189f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-17 03:51:13 +0000

    neonctl: update 2.33.1 bottle.

--- Formula/n/neonctl.rb
+++ Formula/n/neonctl.rb
@@ -6,7 +6,7 @@ class Neonctl < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "f53820e5bc08cd3155a1d0675c869d0b756a41bd4c5b457dabc12222826171eb"
+    sha256 cellar: :any_skip_relocation, all: "b3dc011fe8b9c3e24d6b0887e2952bac55be0b599926a60a85e488343fe1562d"
   end
 
   depends_on "esbuild" # replaces the bundled copy
