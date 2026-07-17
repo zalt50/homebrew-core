8f64e5070313234f0d00c8dc7667b631dfc85d06
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-17 11:05:21 +0000

    gcx: add 0.4.4 bottle.

--- Formula/g/gcx.rb
+++ Formula/g/gcx.rb
@@ -6,6 +6,15 @@ class Gcx < Formula
   license "Apache-2.0"
   head "https://github.com/grafana/gcx.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a6a4ea68759f8a706375a758856217ec6efa499e88d30efa002eb21323f80500"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6fad35f4f28c1fae5b656e4153f7a46de0f2c7446937d8cb4667e3d559935b8b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1f87a39e77b53ac02bdb20c92c116855c2a888ad3b9c3fe8b1487e86122c7abe"
+    sha256 cellar: :any_skip_relocation, sonoma:        "4ac063e90a451044d902ae493f6fd5acb1a2190a9027af73c6ba3b7a5fd38af1"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "0122710ecff21489d0e07f07deaa16de4c0c149a4cfa8ad492b17e281877fc93"
+    sha256 cellar: :any,                 x86_64_linux:  "bfc63f51be2eeb1d5d8877de28fba3a73ef26424e28a3e7c6077fddad385edfc"
+  end
+
   depends_on "go" => :build
 
   def install
