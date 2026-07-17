c446bbc88c0958c3b92456dd849b9a1bfafa016d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-17 14:48:51 +0000

    octl 0.0.30

--- Formula/o/octl.rb
+++ Formula/o/octl.rb
@@ -1,8 +1,8 @@
 class Octl < Formula
   desc "Modern CLI for Outscale"
   homepage "https://github.com/outscale/octl"
-  url "https://github.com/outscale/octl/archive/refs/tags/v0.0.29.tar.gz"
-  sha256 "69489868eba43b5451c8e13834007ad0a1c65e3c3413217ad8d14cc400761343"
+  url "https://github.com/outscale/octl/archive/refs/tags/v0.0.30.tar.gz"
+  sha256 "3b7428a07d73785cd8eb9e633d349a9c215db50e848c72fbeacc38df76200bd0"
   license "BSD-3-Clause"
   head "https://github.com/outscale/octl.git", branch: "main"
 
