d9a7890455c70221d29981e00adc1a91606a1a24
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-17 02:47:09 +0000

    cminpack 1.3.13

--- Formula/c/cminpack.rb
+++ Formula/c/cminpack.rb
@@ -1,8 +1,8 @@
 class Cminpack < Formula
   desc "Solves nonlinear equations and nonlinear least squares problems"
   homepage "http://devernay.free.fr/hacks/cminpack/cminpack.html"
-  url "https://github.com/devernay/cminpack/archive/refs/tags/v1.3.11.tar.gz"
-  sha256 "45675fac0a721a1c7600a91a9842fe1ab313069db163538f2923eaeddb0f46de"
+  url "https://github.com/devernay/cminpack/archive/refs/tags/v1.3.13.tar.gz"
+  sha256 "cf0d6cc654f8c63bb65979056ea5bcda1046768b1dfe83ceda504924d8331167"
   license "Minpack"
   head "https://github.com/devernay/cminpack.git", branch: "master"
 
@@ -24,6 +24,7 @@ class Cminpack < Formula
       -DBUILD_SHARED_LIBS=ON
       -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON
       -DCMINPACK_LIB_INSTALL_DIR=lib
+      -DBUILD_EXAMPLES=OFF
     ]
 
     system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
