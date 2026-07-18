cf291672be3115c0a5f24514e77c3e27e36b530a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-18 15:42:21 +0000

    utiluti: add 1.5 bottle.

--- Formula/u/utiluti.rb
+++ Formula/u/utiluti.rb
@@ -6,6 +6,13 @@ class Utiluti < Formula
   license "Apache-2.0"
   head "https://github.com/scriptingosx/utiluti.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "442eeea49c01e4615962ef30340a5bcc949906223c39e157b21b5160d5573c82"
+    sha256 cellar: :any,                 arm64_sequoia: "2b4bbc74f26387db21f628d23ebb880a0b5a794b18a15b886a63d8c5aff502f3"
+    sha256 cellar: :any,                 arm64_sonoma:  "5cad6055463553d8f64c68a1f78500835dded2a21395b5830be7c2c156ca5e0b"
+    sha256 cellar: :any,                 sonoma:        "6b3f20399145a7efafcd4756fc4efa4848afbb1c5aed2ff690715f8974d43a32"
+  end
+
   depends_on :macos
   uses_from_macos "swift" => :build, since: :tahoe # Swift 6.2
 
