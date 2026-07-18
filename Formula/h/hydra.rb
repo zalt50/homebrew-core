6a4392b1ca25a144e49807070c68da3e6042e73f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-18 11:01:18 +0000

    hydra: update 9.7 bottle.

--- Formula/h/hydra.rb
+++ Formula/h/hydra.rb
@@ -7,14 +7,12 @@ class Hydra < Formula
   head "https://github.com/vanhauser-thc/thc-hydra.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "1da116ec3e9e0c199532be9dab820da943fd90882a26c4299e5784783b680af2"
-    sha256 cellar: :any,                 arm64_sequoia: "59462731671d23a67a26c61731967a0997cf97be55b47241bb7bc8773315d5b7"
-    sha256 cellar: :any,                 arm64_sonoma:  "1587d19830a37949d7997933c8c2e0aabaef9d13ff054e588ae2c0461fd1b15b"
-    sha256 cellar: :any,                 arm64_ventura: "e9ec5262f77f8f099f7d1fcf0a4c35d8cc66f71af7dd9629f4b662dfb4bbe125"
-    sha256 cellar: :any,                 sonoma:        "32ae8946b3891da2b56df93f7de828243be8498ddbca4e4a20b3864c9f052dd4"
-    sha256 cellar: :any,                 ventura:       "7ce1e368623e1a98fa4594879774604d871b56eedea4127c4bdafb59369e1e8c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "564ffb41b6e522fd8a13fc3ae634aa888144c66216198a96dd81d61ee2ba6205"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e062f5c8583970cd8fff70e7f4ae8ead1b2514c8e4d0fac47422990aa21940ae"
+    sha256 cellar: :any, arm64_tahoe:   "a12d39fc54c39d3ce32ed87e5628c318cb35598e53b9452f31d946d31bb51566"
+    sha256 cellar: :any, arm64_sequoia: "39027bba058948d1f26a768b5aa462bd9e72ea329e7b8722aac7e3365ed1e723"
+    sha256 cellar: :any, arm64_sonoma:  "370b0703a0221729a6da9bea96f1fc8c31b39365359e193f03177f32d974aac4"
+    sha256 cellar: :any, sonoma:        "4adc5e6a609e89cdec94e302c956b4e3eb5f05ae8b23654083342e34ca771e1f"
+    sha256 cellar: :any, arm64_linux:   "6d030ea28a0c59bf8453b3084f5c3f641f0798b50cae2e24a8dc425aa73299e1"
+    sha256 cellar: :any, x86_64_linux:  "081e245a7a3a7d46acbcbff41c7f4df66f1ab85f503b7136db5d6e49864ffc19"
   end
 
   depends_on "pkgconf" => :build
