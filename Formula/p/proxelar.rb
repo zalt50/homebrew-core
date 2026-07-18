ad430e073f455c70447953761de7714179af5479
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-18 11:50:43 +0000

    proxelar 0.5.0

--- Formula/p/proxelar.rb
+++ Formula/p/proxelar.rb
@@ -1,8 +1,8 @@
 class Proxelar < Formula
   desc "Man-in-the-Middle proxy for HTTP/HTTPS traffic"
   homepage "https://proxelar.micheletti.io"
-  url "https://github.com/emanuele-em/proxelar/archive/refs/tags/v0.4.7.tar.gz"
-  sha256 "592edd0211e594de5b4af0c456db7e72fcbd1290f6b552b84a2cc80e994acfb7"
+  url "https://github.com/emanuele-em/proxelar/archive/refs/tags/v0.5.0.tar.gz"
+  sha256 "2675e32a3e9a21b7ad077687ac4e2282e8318fb756b29e0796eed2605418e0ac"
   license "MIT"
 
   bottle do
