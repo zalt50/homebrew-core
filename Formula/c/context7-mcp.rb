3c71361f26c6b0367b4b56337a99b1a50cad943f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-18 12:19:55 +0000

    context7-mcp: update 3.2.4 bottle.

--- Formula/c/context7-mcp.rb
+++ Formula/c/context7-mcp.rb
@@ -6,7 +6,7 @@ class Context7Mcp < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "5beffa6b12a0d80556d1ecb05b4403cea5b821381ab6b495ab4fe3e4c0ad32d8"
+    sha256 cellar: :any_skip_relocation, all: "2dab033099bea7fde00731e59b7c876cb6f2ba2140b65b4061cb059787e00495"
   end
 
   depends_on "node"
