d7f644601653e555626c1dc2d4b9943c23a27966
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-19 11:38:48 +0000

    dbx-cli: update 0.4.34 bottle.

--- Formula/d/dbx-cli.rb
+++ Formula/d/dbx-cli.rb
@@ -6,12 +6,12 @@ class DbxCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256               arm64_tahoe:   "072bbff3bc8585a5e9d7b4fabff6cf16cdc0e6ea1c99eb618be4cde8cf669647"
-    sha256               arm64_sequoia: "3c4c896e2966a1e220e9d9e6d347a9c5609e4d08a2fbe0db55dec816d01f3165"
-    sha256               arm64_sonoma:  "1888fff5a38ad61524ba062a60698e9babd37c3870f97157aa383c7e181917a6"
-    sha256               sonoma:        "56cb1df7d1c1413878ae52bd1fec38e894923f5f0af956e42f4bd6f04be4b285"
-    sha256 cellar: :any, arm64_linux:   "dba0fe606b891e2ac501faa6a036422e684d8e499984eac66479bc771f25ad08"
-    sha256 cellar: :any, x86_64_linux:  "ad82e2dadfc73d7a17b77870d0202971bedbc07dc2c3e74acdc38c3842213c7c"
+    sha256               arm64_tahoe:   "58070b0f57511d444262d478895dde9cd44eab06d984a4cb4a43b852e4665559"
+    sha256               arm64_sequoia: "c128a006825a31d8a953d92a62feb4b2eeed0c7fdc678a2d31f3c8a11e0067fc"
+    sha256               arm64_sonoma:  "407eb1932ac183caf15718726391eceee924057c31eb7107c3545fe305868f0c"
+    sha256               sonoma:        "9ee5f23a73d818c17171c34c0289a37ca4a58a8b1cf52e138c1814b8ea10de73"
+    sha256 cellar: :any, arm64_linux:   "ce6530f7536d632e576d80e99ae5293b74943c202903e1d4e0906ff716b68309"
+    sha256 cellar: :any, x86_64_linux:  "d95b6eb6b46fb84649e15c66e594f795edd341b05a25f7a1b8a4eae675fe4c1c"
   end
 
   depends_on "node"
