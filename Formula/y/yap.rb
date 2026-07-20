976a3247afbd14e951972f2c792b49b5ec25730c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-20 09:18:55 +0000

    yap 1.2.1

--- Formula/y/yap.rb
+++ Formula/y/yap.rb
@@ -1,8 +1,8 @@
 class Yap < Formula
   desc "On-device audio transcription using Speech.framework"
   homepage "https://github.com/finnvoor/yap"
-  url "https://github.com/finnvoor/yap/archive/refs/tags/1.2.0.tar.gz"
-  sha256 "bbaeba13b2c9cd73e89564d97e6fc85ab05273648e81d6dd61ca79bd7f453295"
+  url "https://github.com/finnvoor/yap/archive/refs/tags/1.2.1.tar.gz"
+  sha256 "0ee7ea1bd8b724acdf95575baa832365dd843caab81f9e57b33a28a5712921eb"
   license "CC0-1.0"
   head "https://github.com/finnvoor/yap.git", branch: "main"
 
