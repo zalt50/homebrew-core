73d55e81615c23a1dfe76e1e801cf8fbefb64d7c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-20 18:47:36 +0000

    stripe-cli: update 1.44.0 bottle.

--- Formula/s/stripe-cli.rb
+++ Formula/s/stripe-cli.rb
@@ -6,12 +6,12 @@ class StripeCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "393ed9c9ecf0d993d2fe90c07d191d3feb5223973cb9b8aa780ca406544ed2c0"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "393ed9c9ecf0d993d2fe90c07d191d3feb5223973cb9b8aa780ca406544ed2c0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "393ed9c9ecf0d993d2fe90c07d191d3feb5223973cb9b8aa780ca406544ed2c0"
-    sha256 cellar: :any_skip_relocation, sonoma:        "eb4995b964178c62b8ab82e1e2aac86b2b8126ef5dff046b8ea2b4c9ad447f0f"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d5d5b7ba7ae04c75879d43a05e05ca13420f11737f1d18616ec5c31916262f3f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f3b2bf4496de5796add8600a4a56fcc1f6e9c11a43a2c1054c4e1b8434a5cd65"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "836179c77e3a6a3da48f4c83d463b777f2aaa2fa06b4247973fb060cf5173324"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "836179c77e3a6a3da48f4c83d463b777f2aaa2fa06b4247973fb060cf5173324"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "836179c77e3a6a3da48f4c83d463b777f2aaa2fa06b4247973fb060cf5173324"
+    sha256 cellar: :any_skip_relocation, sonoma:        "2b01da6a52db516251958668c178778c65153c4012926032896d53c3636076d3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "b76e61871ccbcccdb1923a1dcdbf57073644faab5ba072af4e874e012a506d6c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3765f66c3d2644778d6264fd5ba1206b918de07b1576fe79016e2530d029bfd8"
   end
 
   depends_on "go" => :build
