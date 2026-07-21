ce9044161e60edf9ca8f9c614cc109e50703a509
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-21 11:53:12 +0000

    vercel: add 56.3.2 bottle.

--- Formula/v/vercel.rb
+++ Formula/v/vercel.rb
@@ -5,6 +5,15 @@ class Vercel < Formula
   sha256 "d25fc8c4f2f2a41b05221f3a0b45f6409d5f2b3b62837dd320d4d522296b070c"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "4b9cbd63093428102d1955b0ec2fbd07d32a183dc03b940b305bb7aad7f96f96"
+    sha256 cellar: :any,                 arm64_sequoia: "4b9cbd63093428102d1955b0ec2fbd07d32a183dc03b940b305bb7aad7f96f96"
+    sha256 cellar: :any,                 arm64_sonoma:  "4b9cbd63093428102d1955b0ec2fbd07d32a183dc03b940b305bb7aad7f96f96"
+    sha256 cellar: :any,                 sonoma:        "befe99fe5d9327ba1c461645c8c33ab74194b28a1d7e67266de572efefd0c33d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "556f24a3f426cef67a127bcb792cb411bd69fd20dd2d93aa035220025fa278e4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7a8bdfd91a9af1256445264b97a18cf51cb9839539e8d9ae08cc10da98154088"
+  end
+
   depends_on "node"
 
   def install
