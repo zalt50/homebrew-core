7b7146728648541fe89e22c9ea849532bf98e208
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-21 20:35:17 +0000

    apptainer: update 1.5.3 bottle.

--- Formula/a/apptainer.rb
+++ Formula/a/apptainer.rb
@@ -7,8 +7,8 @@ class Apptainer < Formula
   head "https://github.com/apptainer/apptainer.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any, arm64_linux:  "d61943bcce4da33880dfc1dbf9b9c13b3e3aa38c52ffb8782f3993d40982533d"
-    sha256 cellar: :any, x86_64_linux: "bad9358f6bdf33b8d71cb4ec22e7e91040eb5faaa2f3c806d8dd269ecc79ea6d"
+    sha256 cellar: :any, arm64_linux:  "01fd8bad65282e4318436fa892dd79b825e4cc11564078b96e4bd71b82dfeebe"
+    sha256 cellar: :any, x86_64_linux: "a475188da8cf418e3fa1afed2dedfdd178f18b29195a5c0912b3f983002b7e59"
   end
 
   # No relocation, the localstatedir to find configs etc is compiled into the program
