b94bf85f6046b138a725728119ec0299486a943d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-21 14:37:59 +0000

    dropbear 2026.93

--- Formula/d/dropbear.rb
+++ Formula/d/dropbear.rb
@@ -1,8 +1,8 @@
 class Dropbear < Formula
   desc "Small SSH server/client for POSIX-based system"
   homepage "https://matt.ucc.asn.au/dropbear/dropbear.html"
-  url "https://matt.ucc.asn.au/dropbear/releases/dropbear-2026.92.tar.bz2"
-  sha256 "91dcb5234de8dea68dd82c55411c9fc986b457ab58372a780ee8a870419c2f7e"
+  url "https://matt.ucc.asn.au/dropbear/releases/dropbear-2026.93.tar.bz2"
+  sha256 "310a6087952897c182efbe16088fa0c4d07c467e850a22699472137278fabf09"
   license "MIT"
 
   livecheck do
