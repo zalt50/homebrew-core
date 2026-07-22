72db37946344a04e1a57b6039f2532099e2b4536
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-22 17:58:24 +0000

    virtiofsd: add 1.14.0 bottle.

--- Formula/v/virtiofsd.rb
+++ Formula/v/virtiofsd.rb
@@ -5,6 +5,11 @@ class Virtiofsd < Formula
   sha256 "0646c2cd5a733dd411afb49be8eff7f0a10bb4a286f75b822961e07ea6a654af"
   license all_of: ["Apache-2.0", "BSD-3-Clause"]
 
+  bottle do
+    sha256 cellar: :any, arm64_linux:  "af1aab9fc68a2784268e0ecb254f90dbfdb9800a7df0b92b46b13dd45f6fda26"
+    sha256 cellar: :any, x86_64_linux: "8bd01117ad94d525b5ca73fba37f4bad0d4d2c04569a4d0a469aef5b14428291"
+  end
+
   depends_on "rust" => :build
   depends_on "libcap-ng"
   depends_on "libseccomp"
