8ee52e3eb300bb92fe32254e8f4f219756ad1b86
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-22 09:11:37 +0000

    nvc 1.22.0

--- Formula/n/nvc.rb
+++ Formula/n/nvc.rb
@@ -1,8 +1,8 @@
 class Nvc < Formula
   desc "VHDL compiler and simulator"
   homepage "https://www.nickg.me.uk/nvc/"
-  url "https://github.com/nickg/nvc/releases/download/r1.21.1/nvc-1.21.1.tar.gz"
-  sha256 "0481436a52b791cf33e412090c94ffc59bee6d8a6f1a350cf7d00883f155458f"
+  url "https://github.com/nickg/nvc/releases/download/r1.22.0/nvc-1.22.0.tar.gz"
+  sha256 "bd648d236145da472be91fe8ef2b201958ff6f65a75b92c8c97f23ac1a2c980c"
   license "GPL-3.0-or-later"
 
   bottle do
