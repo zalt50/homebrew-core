3835ed4706f8be0674c3f2784527566e7b617487
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-24 23:20:38 +0000

    cloud-sql-proxy 2.24.1

--- Formula/c/cloud-sql-proxy.rb
+++ Formula/c/cloud-sql-proxy.rb
@@ -1,8 +1,8 @@
 class CloudSqlProxy < Formula
   desc "Utility for connecting securely to your Cloud SQL instances"
   homepage "https://github.com/GoogleCloudPlatform/cloud-sql-proxy"
-  url "https://github.com/GoogleCloudPlatform/cloud-sql-proxy/archive/refs/tags/v2.24.0.tar.gz"
-  sha256 "302d36cc853fb98c89e37b7646b99525d1660bc9129231e3a0daf8695f3162f2"
+  url "https://github.com/GoogleCloudPlatform/cloud-sql-proxy/archive/refs/tags/v2.24.1.tar.gz"
+  sha256 "bef13d34896d3e250cb04edc3fd01a4b1c68934e2edd2cd6826a82c69a27b427"
   license "Apache-2.0"
   head "https://github.com/GoogleCloudPlatform/cloud-sql-proxy.git", branch: "main"
 
