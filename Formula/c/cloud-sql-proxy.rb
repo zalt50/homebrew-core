204b02337b5d7fd5292e7ff58c06bbdfac5cb739
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-24 23:41:52 +0000

    cloud-sql-proxy: update 2.24.1 bottle.

--- Formula/c/cloud-sql-proxy.rb
+++ Formula/c/cloud-sql-proxy.rb
@@ -7,12 +7,12 @@ class CloudSqlProxy < Formula
   head "https://github.com/GoogleCloudPlatform/cloud-sql-proxy.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "bcfd9dc483ce02d193da9b797f7db2187ff244a20589f9ea793c92044d513dac"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5ec62761c4a8ceb8ecc574c6aedd220a21981c88aa8ee6d4e4e8cf4c03224bfb"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e8c27ed244f188734c34e16c7a96cb6c58d3f92cabd2063f69113e2a6cf3484c"
-    sha256 cellar: :any_skip_relocation, sonoma:        "17b93498a498e7d4c168e3920c35be0fa7bf55f26a9b2e7dca3db916aa850cd5"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "aa5fb5d3b3a53526ae7329a10bf0e49fd8e3288185d98ba4cc500e644b8bb7f6"
-    sha256 cellar: :any,                 x86_64_linux:  "1e12305f1cf2f5776bcee80e7ca1035f970ba7ce7d1d029d180dbc84dd4627c3"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "dad8a0005498b32d69fd85e00468a1681e633e43115c4dadbc88f47023d3999b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "28e43555313ea4d35cb47a4b8110ce41a4fcb9a22fcec786a10c1d4a18911a68"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "45691e832f9a6c3f1c4540835faa9ff1e8bd92d32f09228c6ac0b3b1d49ab252"
+    sha256 cellar: :any_skip_relocation, sonoma:        "c4cff0f00c939532589cb10431b993d9ee072905f1429c8cda52ba564499d180"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "95cc70def5c89baa2655f5b0d1c659d8b8941b0f0c2fe68b8f7709e19a942644"
+    sha256 cellar: :any,                 x86_64_linux:  "685369d3a6d1532aeb3bff2f4965e640c8b53d4dddd0f555fbf7dfd2919c2e48"
   end
 
   depends_on "go" => :build
