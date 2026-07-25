5c9b998159c8d99e460a0667766eaed2c108537f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-25 10:53:22 +0000

    zot: update 0.3.7 bottle.

--- Formula/z/zot.rb
+++ Formula/z/zot.rb
@@ -7,12 +7,12 @@ class Zot < Formula
   head "https://github.com/patriceckhart/zot.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "094b9a9fa02a5a86ee1b069f46f3fab97765b8155ae1500347e05f392ce0aa58"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "094b9a9fa02a5a86ee1b069f46f3fab97765b8155ae1500347e05f392ce0aa58"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "094b9a9fa02a5a86ee1b069f46f3fab97765b8155ae1500347e05f392ce0aa58"
-    sha256 cellar: :any_skip_relocation, sonoma:        "1480fc8e7bd6418ab02da48c6c56b24cafa2529034e13c3dfde66cbdc686a577"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ec4ad2852e93c1a7a8dc64f4e713b1be7ee13677e8fbfa9823ced11fe6786832"
-    sha256 cellar: :any,                 x86_64_linux:  "83eb9cc6ee2cfcc6d4c4c1f2260e1838281f8e397eeca0c510172033021b7517"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "86a2ad13bc74f1d7070450927326745d3d297b4ed4ab5cde54bf4ed6995f1e51"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "86a2ad13bc74f1d7070450927326745d3d297b4ed4ab5cde54bf4ed6995f1e51"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "86a2ad13bc74f1d7070450927326745d3d297b4ed4ab5cde54bf4ed6995f1e51"
+    sha256 cellar: :any_skip_relocation, sonoma:        "48e09889f5bc07d0f4c144d3e07fcaf1bca1282e7b2a58617c889f4dde2757ec"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c1d1c39973ca1e8f06263f509d906dd64c487015ebf4ad3280ac81251ccf7536"
+    sha256 cellar: :any,                 x86_64_linux:  "be34fd8c50dca5b3a2bd9ad75a86d5fd46866eecd1cfea4af427a867ae25f470"
   end
 
   depends_on "go" => :build
