c9852043b3c1e1af1b45c2313583d66adcd15f05
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-26 20:20:45 +0000

    fakecloud 0.44.4

--- Formula/f/fakecloud.rb
+++ Formula/f/fakecloud.rb
@@ -1,8 +1,8 @@
 class Fakecloud < Formula
   desc "Free, open-source local AWS cloud emulator for integration testing"
   homepage "https://fakecloud.dev/"
-  url "https://github.com/faiscadev/fakecloud/archive/refs/tags/v0.44.3.tar.gz"
-  sha256 "924bd87a6ca33494aca84ea395d18c71bcbb5319fc879ce169c83b24cc6c6443"
+  url "https://github.com/faiscadev/fakecloud/archive/refs/tags/v0.44.4.tar.gz"
+  sha256 "52f946ba3f5db91608e503f077a0cc63be60a7a3f7d70d183f51c7f1e443694c"
   license "AGPL-3.0-or-later"
   head "https://github.com/faiscadev/fakecloud.git", branch: "main"
 
