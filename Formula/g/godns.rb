997f66656117c98c06f92d7d3bfaf3ca33103137
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-26 03:07:42 +0000

    godns 3.4.3

--- Formula/g/godns.rb
+++ Formula/g/godns.rb
@@ -1,8 +1,8 @@
 class Godns < Formula
   desc "Dynamic DNS client with multiple providers support"
   homepage "https://github.com/TimothyYe/godns"
-  url "https://github.com/TimothyYe/godns/archive/refs/tags/v3.4.2.tar.gz"
-  sha256 "0edb5b0c82da603a930c04e6745ec3d0ef9b7412ff983e4f4c86c7e28aeffe08"
+  url "https://github.com/TimothyYe/godns/archive/refs/tags/v3.4.3.tar.gz"
+  sha256 "0a38ffd19b39371d9e28970679d74b4c04f8476a2f19ea8966a0b9767248f63e"
   license "Apache-2.0"
   head "https://github.com/TimothyYe/godns.git", branch: "master"
 
@@ -18,8 +18,8 @@ class Godns < Formula
   depends_on "go" => :build
 
   resource "web" do
-    url "https://github.com/TimothyYe/godns/releases/download/v3.4.2/godns-web-v3.4.2.zip"
-    sha256 "fba1e397785b71a27ad3ca08d3f6205aef294dd4bd72baad5c019ac038377c95"
+    url "https://github.com/TimothyYe/godns/releases/download/v3.4.3/godns-web-v3.4.3.zip"
+    sha256 "4e845347cf580e8c25350423c8c28003bc2ab3c978a80eb8c336c9861e70d0a6"
 
     livecheck do
       formula :parent
