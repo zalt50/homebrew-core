a737bda91e8938627367aa9308e767017ea5dda3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-26 03:56:13 +0000

    node-sass: update 1.102.0 bottle.

--- Formula/n/node-sass.rb
+++ Formula/n/node-sass.rb
@@ -6,12 +6,12 @@ class NodeSass < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2db356aa0e177010a4b6907308f529458adcf7a7fd9d6d49c1414af511c1179c"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2db356aa0e177010a4b6907308f529458adcf7a7fd9d6d49c1414af511c1179c"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2db356aa0e177010a4b6907308f529458adcf7a7fd9d6d49c1414af511c1179c"
-    sha256 cellar: :any_skip_relocation, sonoma:        "015283836271579becc75fb2c9292275c46b211afa0dd08cfd7fbc62c125aff3"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "0319beef26936187f4041710a825ad046d40228a29df04dcecd27fd024712e5a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "24a8f6a88a3c323587e4335b9692c7d01ee01b15e23789aed0597f99d41789a7"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e602c302fd7ea0021bb5820946cb3b2413bf0b9e97c09e49af91c6f227ac8799"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e602c302fd7ea0021bb5820946cb3b2413bf0b9e97c09e49af91c6f227ac8799"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e602c302fd7ea0021bb5820946cb3b2413bf0b9e97c09e49af91c6f227ac8799"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e18510a4292f751851461906c756c60812a3db289bd969a5abee5b49d4b20e69"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3cc4b04255fb1ddf7a7821bf5cb92378f63a9d736b9b77a9eb19cef8b422c234"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5c0d60f4a5841651d52ee9317b0764db949fc845d6bc5376d6a87aded83484ae"
   end
 
   depends_on "node"
