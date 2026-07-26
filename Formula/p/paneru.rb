011e571d257fb446e7eff1d1603ff413566b5906
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-26 15:05:47 +0000

    paneru 0.4.4

--- Formula/p/paneru.rb
+++ Formula/p/paneru.rb
@@ -1,8 +1,8 @@
 class Paneru < Formula
   desc "Sliding, tiling window manager for MacOS"
   homepage "https://github.com/karinushka/paneru"
-  url "https://github.com/karinushka/paneru/archive/refs/tags/v0.4.3.tar.gz"
-  sha256 "5d6bb92c549a1e608e2d54a44dd828651ab29cc0e51f948bf94b14bdc1781c70"
+  url "https://github.com/karinushka/paneru/archive/refs/tags/v0.4.4.tar.gz"
+  sha256 "e087a421a41f50959446f856fa2e70fb3b57942a78c425d83f78d9759a295694"
   license "MIT"
   head "https://github.com/karinushka/paneru.git", branch: "main"
 
