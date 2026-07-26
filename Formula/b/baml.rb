738eba16061757193746fdc5df86a982bd5a8327
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-26 02:23:16 +0000

    baml: add 0.2.2 bottle.

--- Formula/b/baml.rb
+++ Formula/b/baml.rb
@@ -11,6 +11,15 @@ class Baml < Formula
     regex(/^baml-wrapper[._-]v?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "de69b38f725b0e3fb07c6b9fc3815d4aa828093bf20b0dd3595817afa40fc623"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "94053fe3f793b16bfe0546365633bc68dee8ba56a69e208164899e1ca811d3fb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "94ab7c29be1a21d70d4dedb21488c69bf2609a688887b916609d470cfcdc7264"
+    sha256 cellar: :any_skip_relocation, sonoma:        "f8d6196b0abe2960dbccbedc26444e10da90aef4c57cb6ae5671cf7a3a0ff216"
+    sha256 cellar: :any,                 arm64_linux:   "b6f2fe530bf503f8f9efe7ec385681d2dd6efcf5b52408f02e77ba148b7fd1eb"
+    sha256 cellar: :any,                 x86_64_linux:  "a9438a264f761ed8ebab75008a93cdda79e2d4f1a1921e2d094e4342c4024c85"
+  end
+
   depends_on "cmake" => :build
   depends_on "rust" => :build
 
