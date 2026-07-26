66b556da465b311f3de8cbef3de08a5ccb1f5861
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-26 03:26:16 +0000

    neonctl 2.36.1

--- Formula/n/neonctl.rb
+++ Formula/n/neonctl.rb
@@ -1,8 +1,8 @@
 class Neonctl < Formula
   desc "Neon CLI tool"
   homepage "https://neon.tech/docs/reference/neon-cli"
-  url "https://registry.npmjs.org/neonctl/-/neonctl-2.36.0.tgz"
-  sha256 "e768b7bee592f74c7d00e81241aebf7c662c302d52ede7970d69080b1d7e1ad6"
+  url "https://registry.npmjs.org/neonctl/-/neonctl-2.36.1.tgz"
+  sha256 "86830f521ce0763ed4f53cdb7922ba3bfc90f8cae77ae19be78e6f68a2b32cbe"
   license "Apache-2.0"
 
   bottle do
