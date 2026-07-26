8f84781905971df0365a3e70312edd2dc649f529
Johan Walles <johan.walles@gmail.com>
2026-07-26 17:56:05 +0000

    moor 2.15.2

--- Formula/m/moor.rb
+++ Formula/m/moor.rb
@@ -1,8 +1,8 @@
 class Moor < Formula
   desc "Nice to use pager for humans"
   homepage "https://github.com/walles/moor"
-  url "https://github.com/walles/moor/archive/refs/tags/v2.15.1.tar.gz"
-  sha256 "efef9649f11aae825423381513a44b28840a009bf928edd436068cab0fdb5551"
+  url "https://github.com/walles/moor/archive/refs/tags/v2.15.2.tar.gz"
+  sha256 "1e9b26de52fcc9434e54c2ea4c5ee9f0554d52e2fb5795a13c0b8b943a950651"
   license "BSD-2-Clause"
   head "https://github.com/walles/moor.git", branch: "master"
 
