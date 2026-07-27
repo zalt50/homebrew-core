24a44899ccf12fa94516fcbae109a48035a754e4
Michael Cho <michael@michaelcho.dev>
2026-07-27 10:42:21 -0400

    ckan: migrate from Mono to .NET
    This avoids installing the pre-built .exe.
    
    Also stop recommending the deprecated ckan-app. Can be restored if
    upstream app meets Gatekeeper requirement.

--- Formula/c/ckan.rb
+++ Formula/c/ckan.rb
@@ -1,8 +1,8 @@
 class Ckan < Formula
   desc "Comprehensive Kerbal Archive Network"
   homepage "https://github.com/KSP-CKAN/CKAN/"
-  url "https://github.com/KSP-CKAN/CKAN/releases/download/v1.36.4/ckan.exe"
-  sha256 "7748082cdfcf2dbb5f502261b18afc85003193894c60427d04b0ac7a8dfef7de"
+  url "https://github.com/KSP-CKAN/CKAN/archive/refs/tags/v1.36.4.tar.gz"
+  sha256 "5b7d4257ccd760b809cd75a6b2b7bfb2fcf42c8bd492d6c1b61b04e7649b2aaf"
   license "MIT"
 
   livecheck do
@@ -10,27 +10,42 @@ class Ckan < Formula
     strategy :github_latest
   end
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, all: "c0cf6076bb24c81f0b6c26fa2f14e1c0ef2b341190185e9c69a10cafd1ae813f"
-  end
-
-  depends_on "mono"
+  depends_on "dotnet"
 
   def install
-    (libexec/"bin").install "ckan.exe"
-    (bin/"ckan").write <<~SHELL
-      #!/bin/sh
-      exec mono "#{libexec}/bin/ckan.exe" "$@"
-    SHELL
+    ENV["DOTNET_CLI_TELEMETRY_OPTOUT"] = "1"
+
+    dotnet = Formula["dotnet"]
+    args = %W[
+      --configuration Release
+      --framework net#{dotnet.version.major_minor}
+      --output #{libexec}
+      --no-self-contained
+      --use-current-runtime
+      -p:AppHostRelativeDotNet=#{dotnet.opt_libexec.relative_path_from(libexec)}
+    ]
+
+    system "dotnet", "publish", "Cmdline/CKAN-cmdline.csproj", *args
+    bin.install_symlink libexec/"CKAN-CmdLine" => "ckan"
   end
 
   def caveats
-    <<~EOS
-      To use the CKAN GUI, install the ckan-app cask.
-    EOS
+    on_macos do
+      <<~EOS
+        If upgrading from a prior Mono-based install, you may want to migrate your
+        appdata in "$HOME/.local/share/CKAN" to "$HOME/Library/Application Support/CKAN"
+      EOS
+    end
   end
 
   test do
+    # On macOS .NET uses NSApplicationSupportDirectory which does not support $HOME.
+    # Thus CKAN is expected to fail within sandboxed test when run for first time.
+    if OS.mac?
+      assert_match "Library/Application Support/CKAN' is denied.", pipe_output("#{bin}/ckan version 2>&1")
+      return
+    end
+
     assert_match version.to_s, shell_output("#{bin}/ckan version")
 
     output = shell_output("#{bin}/ckan update", 1)
