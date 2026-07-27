c093ac4234a150b80f319c8c9794bee05f7b6102
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2026-07-27 12:44:57 +0900

    csound: add pulseaudio dependency
    csound links libpulse directly, pulled into the build environment as an
    indirect dependency via fluid-synth. The revision bump also covers
    libwebsockets 5.0.0.

--- Formula/c/csound.rb
+++ Formula/c/csound.rb
@@ -2,7 +2,7 @@ class Csound < Formula
   desc "Sound and music computing system"
   homepage "https://csound.com"
   license "LGPL-2.1-or-later"
-  revision 14
+  revision 15
   head "https://github.com/csound/csound.git", branch: "develop"
 
   # Remove `stable` block when patches are no longer needed
@@ -78,6 +78,7 @@ class Csound < Formula
   on_linux do
     depends_on "alsa-lib"
     depends_on "libx11"
+    depends_on "pulseaudio"
     depends_on "zlib-ng-compat"
   end
 
