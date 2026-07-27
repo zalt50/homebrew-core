ef3a3e156b3c32ad625ff1471193d0ec4e4b7019
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-27 18:21:36 +0000

    rattler-build 0.71.0

--- Formula/r/rattler-build.rb
+++ Formula/r/rattler-build.rb
@@ -1,8 +1,8 @@
 class RattlerBuild < Formula
   desc "Universal conda package builder"
   homepage "https://rattler.build"
-  url "https://github.com/prefix-dev/rattler-build/archive/refs/tags/v0.70.1.tar.gz"
-  sha256 "b037a68c7e87e36d3268bbd4a4c527f893805a77394eedb48927ac0647d8760c"
+  url "https://github.com/prefix-dev/rattler-build/archive/refs/tags/v0.71.0.tar.gz"
+  sha256 "a41c8820f38eddbf01fe65b2c5fcd0ca08fafb4bcf491f017f22c5f5eb9f6f46"
   license "BSD-3-Clause"
   head "https://github.com/prefix-dev/rattler-build.git", branch: "main"
 
