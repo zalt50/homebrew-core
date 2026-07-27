eb968b05a1c9b138ba8a7c0a6c8093ad62297cbc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-27 15:12:37 +0000

    just-lsp 0.5.1

--- Formula/j/just-lsp.rb
+++ Formula/j/just-lsp.rb
@@ -1,8 +1,8 @@
 class JustLsp < Formula
   desc "Language server for just"
   homepage "https://github.com/terror/just-lsp"
-  url "https://github.com/terror/just-lsp/archive/refs/tags/0.5.0.tar.gz"
-  sha256 "ceb3a2ec7c8d0c1cae4dd8e542303892e679b70a85393dc99766654e77207df2"
+  url "https://github.com/terror/just-lsp/archive/refs/tags/0.5.1.tar.gz"
+  sha256 "cdcd87b0307dbb1a77f8fb2755dfdfe487138cf98f235762fb035831ccf34e37"
   license "CC0-1.0"
   head "https://github.com/terror/just-lsp.git", branch: "master"
 
