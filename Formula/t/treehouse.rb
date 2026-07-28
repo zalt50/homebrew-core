d9933d7b24aa3d6424385bcb513d70a2f3f901e5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-28 12:01:49 +0000

    treehouse: add 2.1.0 bottle.

--- Formula/t/treehouse.rb
+++ Formula/t/treehouse.rb
@@ -5,6 +5,15 @@ class Treehouse < Formula
   sha256 "d68626da446824a88437ee7bc0f7fdd10eb507a8a0317bb210bc106ed85f480e"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "17587b11a56729717ddab23d932502d628effd2632efc92a56037a554284bc95"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cb0c6caab22784a70c19951718077b44728cbc8643635238bdf7ffe6a8677cb9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "dd24dd008c6a67b991bdeacae0aef704594012977478fe51fb9b6a133d42e96a"
+    sha256 cellar: :any_skip_relocation, sonoma:        "36f03e30e6f0e916bce3afa8e57567480e29cbe487ec64fa49eb04b07d8d93ce"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "077a4e5db5cb2e5421dd31e6ea46db5483075b074885c7e06fb055aa6ef82e7c"
+    sha256 cellar: :any,                 x86_64_linux:  "d2f6af6b64cba4034d4b5cd52d6fe7f2a668dcaf1dc59faafa1a4f00cdefc78b"
+  end
+
   depends_on "go" => :build
 
   def install
