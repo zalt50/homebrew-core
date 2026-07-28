a1445e75efe700b932a80614e915add4d5f3230c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-28 12:00:20 +0000

    aicommit2: update 2.9.0 bottle.

--- Formula/a/aicommit2.rb
+++ Formula/a/aicommit2.rb
@@ -6,7 +6,7 @@ class Aicommit2 < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "a110382c105dbdd45b9e7e3e32b6e55f80ef75026b31b2a36ea053dca918926f"
+    sha256 cellar: :any_skip_relocation, all: "871584df715a6cb238217c9e4f7cb4fafdffb8d51ff51ea98698be5ae400bd52"
   end
 
   depends_on "node"
