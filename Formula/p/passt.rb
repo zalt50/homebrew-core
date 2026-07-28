0b68732892d8dedcc60dfd796040f6e60ea358ce
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-28 18:08:45 +0000

    passt 2026_07_28.f8df3f1

--- Formula/p/passt.rb
+++ Formula/p/passt.rb
@@ -1,9 +1,9 @@
 class Passt < Formula
   desc "User-mode networking daemons for virtual machines and namespaces"
   homepage "https://passt.top/passt/about/"
-  url "https://passt.top/passt/snapshot/passt-2026_07_16.090d739.tar.xz"
-  version "2026_07_16.090d739"
-  sha256 "e7ce7a7964826292ccc4d5cdd4402d54268f0eca352b9f06cb73a40e637af52e"
+  url "https://passt.top/passt/snapshot/passt-2026_07_28.f8df3f1.tar.xz"
+  version "2026_07_28.f8df3f1"
+  sha256 "fcfeb5fbdf775bcc48edc1d5eac8a6d57bc333f8e67b714149376d36061416f0"
   license all_of: ["GPL-2.0-or-later", "BSD-3-Clause"]
   head "git://passt.top/passt", branch: "master"
 
@@ -31,7 +31,7 @@ class Passt < Formula
     begin
       # Just check failure as unable to use pasta or passt on unprivileged Docker
       output = shell_output("#{bin}/pasta --pid #{pidfile} 2>&1", 1)
-      assert_match "Failed to set up tap device in namespace", output
+      assert_match "Couldn't create user namespace", output
     ensure
       if pidfile.exist? && (pid = pidfile.read.to_i).positive?
         Process.kill("TERM", pid)
