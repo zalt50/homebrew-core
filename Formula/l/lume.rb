1b94b367345353100a675bdd4963a83257d3684d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-28 23:30:48 +0000

    lume 0.5.0

--- Formula/l/lume.rb
+++ Formula/l/lume.rb
@@ -1,8 +1,8 @@
 class Lume < Formula
   desc "Create and manage Apple Silicon-native virtual machines"
   homepage "https://cua.ai"
-  url "https://github.com/trycua/cua/archive/refs/tags/lume-v0.4.0.tar.gz"
-  sha256 "5f7cb6d8789d5df77d907dec00e0de8e5758456fb8197470686d39252d2002bc"
+  url "https://github.com/trycua/cua/archive/refs/tags/lume-v0.5.0.tar.gz"
+  sha256 "7667e9853f05e15a4c9bcb7c802b9c6f01ea5b13e91aa11afd4d35f59d97d969"
   license "MIT"
   head "https://github.com/trycua/cua.git", branch: "main"
 
