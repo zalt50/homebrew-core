248a7fccc33ef302d6af748b2525f3f8934c05d3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-28 12:20:58 +0000

    calm-cli: update 1.50.1 bottle.

--- Formula/c/calm-cli.rb
+++ Formula/c/calm-cli.rb
@@ -6,12 +6,12 @@ class CalmCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8e506b71425add295ded7a81dec87f9c9acce3c25c611dc59a6a0e5d9f39e82b"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8e506b71425add295ded7a81dec87f9c9acce3c25c611dc59a6a0e5d9f39e82b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8e506b71425add295ded7a81dec87f9c9acce3c25c611dc59a6a0e5d9f39e82b"
-    sha256 cellar: :any_skip_relocation, sonoma:        "c8b772f389f02fdf53dd1d3e2fe4bb011b16d0d658cee24490ad14a46615f256"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "cff168ab9e9c5536a31ef8e0314f84eab006b4d912ddc59acffbb04a26a6328e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "765e275e4c18224a7522a88c5c651d4fb8153c6acd7aeeb58ca3d7bbc63a560b"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "97d93e540e655093e367dc5ce352a93746fd62d6944a3ed637466d13b6682a09"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "97d93e540e655093e367dc5ce352a93746fd62d6944a3ed637466d13b6682a09"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "97d93e540e655093e367dc5ce352a93746fd62d6944a3ed637466d13b6682a09"
+    sha256 cellar: :any_skip_relocation, sonoma:        "388d7895da62684d60d7aec0dc3cb635bea34074cd9f9d68c43185addb3f369c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f73121e5c28e9d772caa5411b6e638475cafd5d5d59901712e89dfb5fd5a571e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "464d075039658f97a55e6192c4ef4159a3f400b42a20c0bc1baadcc3dc719ad3"
   end
 
   depends_on "node"
