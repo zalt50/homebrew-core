0e57b88c3f77b8d1dbf7612285db323b32787ab1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-28 03:48:21 +0000

    zns: update 0.6.0 bottle.

--- Formula/z/zns.rb
+++ Formula/z/zns.rb
@@ -7,14 +7,12 @@ class Zns < Formula
   head "https://github.com/znscli/zns.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7cc41244bcfbf7df964ebcb5ac7171abbeee3e9935107ed37e008cfc1ec1103b"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7cc41244bcfbf7df964ebcb5ac7171abbeee3e9935107ed37e008cfc1ec1103b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7cc41244bcfbf7df964ebcb5ac7171abbeee3e9935107ed37e008cfc1ec1103b"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "7cc41244bcfbf7df964ebcb5ac7171abbeee3e9935107ed37e008cfc1ec1103b"
-    sha256 cellar: :any_skip_relocation, sonoma:        "039f992e91d42aab450a339cd8ab8426f7f3873d423b06fac2a14a13ff9cd882"
-    sha256 cellar: :any_skip_relocation, ventura:       "039f992e91d42aab450a339cd8ab8426f7f3873d423b06fac2a14a13ff9cd882"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8739ac54fb0abed262d78c9bbbe2dfed5af8bb48539439a6ffb9a400ef8a4713"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7497871ad502dca1485cce5d0e5654bd2e989080dd6914ba2efe880df81a707f"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8610f17339cf2a4b507db3de681f008e90d7b05931d5c80fc84e0c7564385e9d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8610f17339cf2a4b507db3de681f008e90d7b05931d5c80fc84e0c7564385e9d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8610f17339cf2a4b507db3de681f008e90d7b05931d5c80fc84e0c7564385e9d"
+    sha256 cellar: :any_skip_relocation, sonoma:        "038b4beebadb645f7bf3b89d208fb90ae191ac34e8cae2708a1dd60107137959"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f1e0666fe60c823f0ae70e36ece6b0db4cc836679207e697634d64eb9167bd1b"
+    sha256 cellar: :any,                 x86_64_linux:  "1c2f347241913ebc20f406d114cbaab4f020186d5dee7abea4960c2cac521825"
   end
 
   depends_on "go" => :build
