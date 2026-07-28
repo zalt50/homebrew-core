a0eeef6ebac2daaeba63b1c74d447b5a4af999e7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-28 20:08:55 +0000

    kops: update 1.36.1 bottle.

--- Formula/k/kops.rb
+++ Formula/k/kops.rb
@@ -12,12 +12,12 @@ class Kops < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "fbdda10847046641edb68dc942443f4ca1f9cfabd3783b12eda0d30651b9ce87"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "76799720f6980f21d5c17de971eab4bd038993241a47941b4f02287cdef3d431"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a55110c00f2c81ca4ca44be6d22511d9ee69d7da1407de89339394116753fb62"
-    sha256 cellar: :any_skip_relocation, sonoma:        "16feea6167f6b7af6f6031e2de0d3b1216e566d9e3c27d28edc6c40a6f32faa6"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8b963f86d75a2bf67c15a21bce175ef697a9e32dd391f50d4991409c854d3601"
-    sha256 cellar: :any,                 x86_64_linux:  "86ac0698e2a45061ed22bba9e9f1676b241a3624f7c531b2929606a62b95dd18"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "861f50f2c9ece016b9feb5738568cf6ff81ba13d55e37dedcd01c25c4cffcbf6"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1d525e9911eed79890c8046f8f7dd692ee067323efce7ea590e583a3a99230cc"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "989b303460043026f2cd07d30a029acd766745be011ad0d3ea5e13ef11aec5ae"
+    sha256 cellar: :any_skip_relocation, sonoma:        "283bead8f72ae30ce201183af05ed580837cd4163b7d8f757ec193b605b49335"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "66cc66cc561e85dd2bfc101d0ad39ecadcaa4a362b9a3f4bc7d9c439b16f220b"
+    sha256 cellar: :any,                 x86_64_linux:  "8a963fd455f69b2929779e27f0c92699ad88a16b0c6b3d60c9da257a40657220"
   end
 
   depends_on "go" => :build
