b76d659750a94e43d213b90e20796d9c09169fe0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-28 02:49:39 +0000

    doltgres 0.57.1

--- Formula/d/doltgres.rb
+++ Formula/d/doltgres.rb
@@ -1,8 +1,8 @@
 class Doltgres < Formula
   desc "Dolt for Postgres"
   homepage "https://github.com/dolthub/doltgresql"
-  url "https://github.com/dolthub/doltgresql/archive/refs/tags/v0.57.0.tar.gz"
-  sha256 "7b7a74280d108c8fdbbb074f348a358ad7170d329b025f8a0b62065dcdb2915c"
+  url "https://github.com/dolthub/doltgresql/archive/refs/tags/v0.57.1.tar.gz"
+  sha256 "b6079caa695a7c5e9784b7b98f3c775c8b622677d8302796914f110e9f1cd855"
   license "Apache-2.0"
   head "https://github.com/dolthub/doltgresql.git", branch: "main"
 
