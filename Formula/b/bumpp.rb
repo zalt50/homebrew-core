3f61d2fe9bbad1802d1b1d6a1e7de3a6de454422
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-29 15:09:47 +0000

    bumpp: update 12.1.0 bottle.

--- Formula/b/bumpp.rb
+++ Formula/b/bumpp.rb
@@ -6,7 +6,7 @@ class Bumpp < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "83087780310bee0e82d8ab6dd47c670b56668f7689b76e7da18cd100a76de146"
+    sha256 cellar: :any_skip_relocation, all: "9af3df2880a9b047642c41b05f3f8de6401461508adc63ee4dc6c6d5eb612688"
   end
 
   depends_on "node"
