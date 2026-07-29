99c30cb5ad3de4e5b64d07bf4a25d719304531a1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-29 11:38:37 +0000

    clickhouse-sql-parser 0.5.3

--- Formula/c/clickhouse-sql-parser.rb
+++ Formula/c/clickhouse-sql-parser.rb
@@ -1,8 +1,8 @@
 class ClickhouseSqlParser < Formula
   desc "Writing clickhouse sql parser in pure Go"
   homepage "https://github.com/AfterShip/clickhouse-sql-parser"
-  url "https://github.com/AfterShip/clickhouse-sql-parser/archive/refs/tags/v0.5.2.tar.gz"
-  sha256 "bc1eca7da4ba60426a5434df69d353aec543fe1e69486f0d3cbec26ca820b266"
+  url "https://github.com/AfterShip/clickhouse-sql-parser/archive/refs/tags/v0.5.3.tar.gz"
+  sha256 "c6939ccdfb7437414427dca959ffca51067d5eeca7d05501cea6ddd6d6fb6302"
   license "MIT"
   head "https://github.com/AfterShip/clickhouse-sql-parser.git", branch: "master"
 
