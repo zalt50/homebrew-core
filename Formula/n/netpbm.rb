fdb7fa70cec0a1cdb677302df5660940f9c846dc
botantony <antonsm21@gmail.com>
2026-07-29 18:35:54 +0200

    netpbm 11.02.27
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Formula/n/netpbm.rb
+++ Formula/n/netpbm.rb
@@ -3,8 +3,8 @@ class Netpbm < Formula
   homepage "https://netpbm.sourceforge.net/"
   # Maintainers: Look at https://sourceforge.net/p/netpbm/code/HEAD/tree/
   # for stable versions and matching revisions.
-  url "https://svn.code.sf.net/p/netpbm/code/stable", revision: "5175"
-  version "11.02.23"
+  url "https://svn.code.sf.net/p/netpbm/code/stable", revision: "5280"
+  version "11.02.27"
   license "GPL-3.0-or-later"
   version_scheme 1
   compatibility_version 1
@@ -46,8 +46,8 @@ class Netpbm < Formula
   resource "html" do
     # Rolling release, latest revision also documents previous software versions
     # NOTE: Keep "revision" and "version" in sync
-    url "https://svn.code.sf.net/p/netpbm/code/userguide", revision: "5177"
-    version "5177"
+    url "https://svn.code.sf.net/p/netpbm/code/userguide", revision: "5275"
+    version "5275"
 
     livecheck do
       url "https://sourceforge.net/p/netpbm/code/HEAD/log/?path=/userguide"
