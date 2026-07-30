3d56c1a0ecc8f37d657092c76dc01da706f41cc1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-30 07:01:59 +0000

    todoist-cli 3.0.6

--- Formula/t/todoist-cli.rb
+++ Formula/t/todoist-cli.rb
@@ -1,8 +1,8 @@
 class TodoistCli < Formula
   desc "Official command-line interface for Todoist"
   homepage "https://github.com/Doist/todoist-cli"
-  url "https://registry.npmjs.org/@doist/todoist-cli/-/todoist-cli-3.0.5.tgz"
-  sha256 "05ffc41e063904a12647f5b8ac8cbc573d62e79d8fe4e121ab2599ee5894b718"
+  url "https://registry.npmjs.org/@doist/todoist-cli/-/todoist-cli-3.0.6.tgz"
+  sha256 "bd049743482513c16d579d7660619f8e0c9b5298a166185ed5c6b674625379a9"
   license "MIT"
 
   bottle do
