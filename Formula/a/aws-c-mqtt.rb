e7cd9f44b8dcb671b5e3117145d938af4d5f09d8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-30 23:13:28 +0000

    aws-c-mqtt 0.16.1

--- Formula/a/aws-c-mqtt.rb
+++ Formula/a/aws-c-mqtt.rb
@@ -1,8 +1,8 @@
 class AwsCMqtt < Formula
   desc "C99 implementation of the MQTT 3.1.1 specification"
   homepage "https://github.com/awslabs/aws-c-mqtt"
-  url "https://github.com/awslabs/aws-c-mqtt/archive/refs/tags/v0.16.0.tar.gz"
-  sha256 "9bc044a9c2f0d80c384ae6a6907c8817e0b40f673f75c4615c83b20f83140374"
+  url "https://github.com/awslabs/aws-c-mqtt/archive/refs/tags/v0.16.1.tar.gz"
+  sha256 "48fd84e6ff51fdce5cdc4250593d7b0f10db91f8592737c0fe69e0177ee48144"
   license "Apache-2.0"
   compatibility_version 1
 
