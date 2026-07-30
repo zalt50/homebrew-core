9092b30aca583a1437756aad994b0df5eea0a108
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-30 19:48:18 +0000

    git-pkgs-forge: add 0.7.0 bottle.

--- Formula/g/git-pkgs-forge.rb
+++ Formula/g/git-pkgs-forge.rb
@@ -6,6 +6,15 @@ class GitPkgsForge < Formula
   license "MIT"
   head "https://github.com/git-pkgs/forge.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "430cf7c160735215672e98a9e88abac07c7cd04476ba7006f1b85925add48e67"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "430cf7c160735215672e98a9e88abac07c7cd04476ba7006f1b85925add48e67"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "430cf7c160735215672e98a9e88abac07c7cd04476ba7006f1b85925add48e67"
+    sha256 cellar: :any_skip_relocation, sonoma:        "2a1589f532f699a59b60b23c28a1ff6ae637e0dc3c5b7884439836f057ba3659"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ece6c47433bf10ca61ed14d38aaa4645263fee4ff5dd7ea3dd3548c2400a85bc"
+    sha256 cellar: :any,                 x86_64_linux:  "1a0a8dfb2e89586f0970e57b213000894acce5d4602b1dd9136410c4bbf7f5fb"
+  end
+
   depends_on "go" => :build
 
   def install
