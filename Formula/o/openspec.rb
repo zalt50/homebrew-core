68af9749743e06c5d576cf29050b386353a77143
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-30 03:56:01 +0000

    openspec: update 1.7.0 bottle.

--- Formula/o/openspec.rb
+++ Formula/o/openspec.rb
@@ -6,12 +6,12 @@ class Openspec < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a89332a86193cdd004b2513803b03a15c5acdf6d66abeecf0f05f860496652bb"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a89332a86193cdd004b2513803b03a15c5acdf6d66abeecf0f05f860496652bb"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a89332a86193cdd004b2513803b03a15c5acdf6d66abeecf0f05f860496652bb"
-    sha256 cellar: :any_skip_relocation, sonoma:        "78fb999014b3b3abd013838013bf5d38779ba6a0fbbed4b4dd853e6f4bbe4505"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "78fb999014b3b3abd013838013bf5d38779ba6a0fbbed4b4dd853e6f4bbe4505"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "78fb999014b3b3abd013838013bf5d38779ba6a0fbbed4b4dd853e6f4bbe4505"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "686317b17d6e8027b2db878dffe5f5ce3457ef24f4f52c70d73e78996ce125ad"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "686317b17d6e8027b2db878dffe5f5ce3457ef24f4f52c70d73e78996ce125ad"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "686317b17d6e8027b2db878dffe5f5ce3457ef24f4f52c70d73e78996ce125ad"
+    sha256 cellar: :any_skip_relocation, sonoma:        "f6f967c8a40145f198972f4bcca9b77b594d1fcc319cde56b814b92efb6cc293"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f6f967c8a40145f198972f4bcca9b77b594d1fcc319cde56b814b92efb6cc293"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f6f967c8a40145f198972f4bcca9b77b594d1fcc319cde56b814b92efb6cc293"
   end
 
   depends_on "node"
