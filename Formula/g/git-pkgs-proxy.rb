c53b55eb57f093ebf90c1e4d2c6d857b7ae5ff84
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-30 20:22:00 +0000

    git-pkgs-proxy: add 0.6.0 bottle.

--- Formula/g/git-pkgs-proxy.rb
+++ Formula/g/git-pkgs-proxy.rb
@@ -6,6 +6,15 @@ class GitPkgsProxy < Formula
   license "GPL-3.0-or-later"
   head "https://github.com/git-pkgs/proxy.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "22a0dded1e64c0ceefe1a244d742e0fbb2a0f301c112812a2fa822fcfcff9d64"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "86723094bbee6daab05889f44f8fa53c2f268680142cc50c01a10320bbcb3a89"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6b43892dc87637576608defb2893360d0e6762979370d4df802af98e6df01163"
+    sha256 cellar: :any_skip_relocation, sonoma:        "8b4c7dc7580ab5c2750474f185f69d25b988b5a652cc1f7924a53fb30e6e744f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c96d11996a79e752ae4c6388ac200adc6e5d9d219fe2c51970affbce6f1809c8"
+    sha256 cellar: :any,                 x86_64_linux:  "c7606230aac01946b6050056a90bace59542e49f6a7bd44874e5d748e15aef24"
+  end
+
   depends_on "go" => :build
 
   def install
