cb0686941717e3a20f9c2d7d897785a8e98af500
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-30 12:27:14 +0000

    linux-headers@5.15 5.15.213

--- Formula/l/linux-headers@5.15.rb
+++ Formula/l/linux-headers@5.15.rb
@@ -1,8 +1,8 @@
 class LinuxHeadersAT515 < Formula
   desc "Header files of the Linux kernel"
   homepage "https://kernel.org/"
-  url "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.212.tar.gz"
-  sha256 "27aa53f0a605389d6fe6f36b23d3b4d4f661392303f3713c85cf0cf17b9099c5"
+  url "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.213.tar.gz"
+  sha256 "11726e81114b6cd50399f1d2e189856c7359439ee12ce7fcfa6f77267b8d1e3c"
   license "GPL-2.0-only" => { with: "Linux-syscall-note" }
   compatibility_version 1
 
