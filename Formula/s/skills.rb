6b4bbad9d8d8d2d4c408a486c1438a2435ff6218
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-31 00:22:54 +0000

    skills 1.5.21

--- Formula/s/skills.rb
+++ Formula/s/skills.rb
@@ -1,8 +1,8 @@
 class Skills < Formula
   desc "Open agent skills ecosystem"
   homepage "https://skills.sh"
-  url "https://registry.npmjs.org/skills/-/skills-1.5.20.tgz"
-  sha256 "359417194b4049eed6b5a80a3c3f57b82c75d48962d791a0efdf41b85cf52990"
+  url "https://registry.npmjs.org/skills/-/skills-1.5.21.tgz"
+  sha256 "10a7008ca357242001d765eba31b232de7b0897d9796b21fa0a76ab17729f9b3"
   license "MIT"
 
   bottle do
