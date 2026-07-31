8694d81cc1bbcf479bead44e21bf963dbdcf5e7d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-31 21:07:31 +0000

    uv 0.12.1

--- Formula/u/uv.rb
+++ Formula/u/uv.rb
@@ -1,8 +1,8 @@
 class Uv < Formula
   desc "Extremely fast Python package installer and resolver, written in Rust"
   homepage "https://docs.astral.sh/uv/"
-  url "https://github.com/astral-sh/uv/archive/refs/tags/0.12.0.tar.gz"
-  sha256 "17c768f651d4e09e83a25b781a74924694838f222a1f3676c4450073161c859a"
+  url "https://github.com/astral-sh/uv/archive/refs/tags/0.12.1.tar.gz"
+  sha256 "4e87a6c0e5feddb55e080ba2e8b7ecc1379361471d724071b0c0e0ba5df72981"
   license any_of: ["Apache-2.0", "MIT"]
   compatibility_version 1
   head "https://github.com/astral-sh/uv.git", branch: "main"
