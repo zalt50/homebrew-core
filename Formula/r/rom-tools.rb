ed7b2aa62013fa15c75e9ded8e030f39ef967fd2
botantony <antonsm21@gmail.com>
2026-07-31 14:21:50 +0200

    rom-tools 0.289
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Formula/r/rom-tools.rb
+++ Formula/r/rom-tools.rb
@@ -1,9 +1,9 @@
 class RomTools < Formula
   desc "Tools for Multiple Arcade Machine Emulator"
   homepage "https://www.mamedev.org/"
-  url "https://github.com/mamedev/mame/archive/refs/tags/mame0288.tar.gz"
-  version "0.288"
-  sha256 "244d916eb3fb8bcd71f2ac51ae71ab6af8cf99869ea7b85d7efc7339ea56c563"
+  url "https://github.com/mamedev/mame/archive/refs/tags/mame0289.tar.gz"
+  version "0.289"
+  sha256 "0929cc749afabcef892900e10dd90bd8b05f94a7dde8f367ac6a5d2082589f84"
   license "GPL-2.0-or-later"
   head "https://github.com/mamedev/mame.git", branch: "master"
 
