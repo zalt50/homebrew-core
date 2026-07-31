73ad6e3a57edb397ff2e1e0cc7f7693962ecca75
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-31 11:52:21 +0000

    dua-cli 2.40.0

--- Formula/d/dua-cli.rb
+++ Formula/d/dua-cli.rb
@@ -1,8 +1,8 @@
 class DuaCli < Formula
   desc "View disk space usage and delete unwanted data, fast"
   homepage "https://lib.rs/crates/dua-cli"
-  url "https://github.com/Byron/dua-cli/archive/refs/tags/v2.39.1.tar.gz"
-  sha256 "dfa2918a5d21cdfa355d324f3d5ac92bb0d1445ef813218ea2d2ea3a36a819b4"
+  url "https://github.com/Byron/dua-cli/archive/refs/tags/v2.40.0.tar.gz"
+  sha256 "3e64699945bd7fd6ca6c6c0262618e0bf432c291b1bec2d7cb11ffb4df3f4792"
   license "MIT"
 
   bottle do
