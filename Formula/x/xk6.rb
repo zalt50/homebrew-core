7ce1f2bb76f38d6fbe40ee1ed1e188f297851b84
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-31 13:09:25 +0000

    xk6 1.4.8

--- Formula/x/xk6.rb
+++ Formula/x/xk6.rb
@@ -1,8 +1,8 @@
 class Xk6 < Formula
   desc "Build k6 with extensions"
   homepage "https://k6.io"
-  url "https://github.com/grafana/xk6/archive/refs/tags/v1.4.7.tar.gz"
-  sha256 "01704695d60d5ce7d8d51531a9bb13aeb293509aba4c4097e409a0bb03ef38df"
+  url "https://github.com/grafana/xk6/archive/refs/tags/v1.4.8.tar.gz"
+  sha256 "faaa6de8f3f03b32bfb947f367216aaaad950307e7b45cf4fba90df5f2c08f5b"
   license "Apache-2.0"
   head "https://github.com/grafana/xk6.git", branch: "master"
 
