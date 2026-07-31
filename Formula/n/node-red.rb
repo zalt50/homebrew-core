0c93508f6f27738164879e9aeb0399c5ca1b9bcf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-31 23:58:15 +0000

    node-red 5.0.4

--- Formula/n/node-red.rb
+++ Formula/n/node-red.rb
@@ -1,8 +1,8 @@
 class NodeRed < Formula
   desc "Low-code programming for event-driven applications"
   homepage "https://nodered.org/"
-  url "https://registry.npmjs.org/node-red/-/node-red-5.0.2.tgz"
-  sha256 "255bac831730505fc34ed6379afc209096508c360d218150b725428b88d49fb0"
+  url "https://registry.npmjs.org/node-red/-/node-red-5.0.4.tgz"
+  sha256 "1acc0ed42023c9170100e2d5596b9db012d0de11f5457f52bed343b67eefbe4a"
   license "Apache-2.0"
 
   bottle do
