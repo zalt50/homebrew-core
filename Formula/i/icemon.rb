2db0f0488d7c44d805e09265959cfb9cdd5840d8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-31 15:09:10 +0000

    icemon 3.4

--- Formula/i/icemon.rb
+++ Formula/i/icemon.rb
@@ -1,10 +1,9 @@
 class Icemon < Formula
   desc "Icecream GUI Monitor"
   homepage "https://kfunk.org/tag/icemon/"
-  url "https://github.com/icecc/icemon/archive/refs/tags/v3.3.tar.gz"
-  sha256 "3caf14731313c99967f6e4e11ff261b061e4e3d0c7ef7565e89b12e0307814ca"
+  url "https://github.com/icecc/icemon/archive/refs/tags/v3.4.tar.gz"
+  sha256 "75f79aa21e0524e384c9041b558d3b55ad5e2ce9a8c851d6c7ca94ce0bfe7e9a"
   license "GPL-2.0-or-later"
-  revision 1
 
   bottle do
     rebuild 3
@@ -34,23 +33,7 @@ class Icemon < Formula
     depends_on "libcap-ng"
   end
 
-  # Backport fix for CMake 4
-  patch do
-    url "https://github.com/icecc/icemon/commit/b07bf3eb0c28ac5cd527d3ab675d2273d1866b48.patch?full_index=1"
-    sha256 "015098bad42e0b020dfefa2fbc8287fa1eb054576ab642b85818ac36cd0755de"
-  end
-
-  # Backport support for Qt 6
-  patch do
-    url "https://github.com/icecc/icemon/commit/d0969453c7d4467e22dcff0f218b31e81136afbe.patch?full_index=1"
-    sha256 "ea808f5daba80a6c92c45f661a53c67742df513cfee430fe724819daab8d551a"
-  end
-
   def install
-    # Workaround for std::unary_function usage
-    # Issue ref: https://github.com/icecc/icemon/issues/80
-    ENV.append "CXXFLAGS", "-D_LIBCPP_ENABLE_CXX17_REMOVED_UNARY_BINARY_FUNCTION" if ENV.compiler == :clang
-
     args = ["-DECM_DIR=#{Formula["extra-cmake-modules"].opt_share}/ECM/cmake"]
     system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
     system "cmake", "--build", "build"
