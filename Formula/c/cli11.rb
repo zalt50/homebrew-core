ae8a284dbb5b2ee8664553a9972ecde0cc0909ea
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-31 14:41:48 +0000

    cli11 2.7.1

--- Formula/c/cli11.rb
+++ Formula/c/cli11.rb
@@ -1,8 +1,8 @@
 class Cli11 < Formula
   desc "Simple and intuitive command-line parser for C++11"
   homepage "https://cliutils.github.io/CLI11/book/"
-  url "https://github.com/CLIUtils/CLI11/archive/refs/tags/v2.7.0.tar.gz"
-  sha256 "7828464fabc29d361e1ccb2d8721e6e00e73ff5c5fad5135408a8c236508398a"
+  url "https://github.com/CLIUtils/CLI11/archive/refs/tags/v2.7.1.tar.gz"
+  sha256 "c02abe5c5fed9b9c9192bff3c74d2c0d19755253f9ca9e3c44484c1cf5ce6568"
   license "BSD-3-Clause"
   head "https://github.com/CLIUtils/CLI11.git", branch: "main"
 
