2005f3dfecbc75de2e38446603c3d8c1bc9dc420
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-01 15:06:36 +0000

    oh-my-reddit: add 0.1.5 bottle.

--- Formula/o/oh-my-reddit.rb
+++ Formula/o/oh-my-reddit.rb
@@ -5,6 +5,15 @@ class OhMyReddit < Formula
   sha256 "623db2b5489557f9b44cff6038b83ffecd8b35fdbd83766a78b52327a7f46ce6"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4d84be470ad928d3440bfc4c27821328e6bdda8828de6b5b4eb649a26bbaf3ce"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "515d42c879fd0c0596fb8e456e7f757bfdcfb6fd834d0cc483a7fca22d1d2b21"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ce393fc27f603b8a3bab6df418e5f87f7e3217fb2625264daa70c978bc541f08"
+    sha256 cellar: :any_skip_relocation, sonoma:        "9af1d06c5ef0e69dd9fbe42b5a395e587c0bd10a8746ac127bec88983233862f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "48ce6c0d9e9338fba5ddc2a297603dfcc9f5da0833aa780a3018b184c759fdb9"
+    sha256 cellar: :any,                 x86_64_linux:  "0e358e168121424957b6f6afea1c72ba038d2c729fa2d3ee73e992051ed1823d"
+  end
+
   depends_on "go" => :build
 
   def install
