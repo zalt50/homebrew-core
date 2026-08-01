82de72ab21a259e75174eaef3a40c917ed692468
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-01 17:49:04 +0000

    baguette: update 0.1.87 bottle.

--- Formula/b/baguette.rb
+++ Formula/b/baguette.rb
@@ -7,7 +7,7 @@ class Baguette < Formula
   head "https://github.com/tddworks/baguette.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe: "df16776739373d30995d495b605bfd287420636792c11e0422932f689fb841ff"
+    sha256 cellar: :any, arm64_tahoe: "647f6afd13767710e0c897c3ca0aa461063188382d6e01b186516f017aec8b76"
   end
 
   depends_on xcode: ["26.0", :build]
