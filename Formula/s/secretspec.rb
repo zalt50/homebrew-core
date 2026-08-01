a18435d8da33cf7edf5a491a70da1f2e18d5f6e2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-01 18:17:50 +0000

    secretspec 0.17.1

--- Formula/s/secretspec.rb
+++ Formula/s/secretspec.rb
@@ -1,8 +1,8 @@
 class Secretspec < Formula
   desc "Declarative secrets management tool"
   homepage "https://secretspec.dev"
-  url "https://github.com/cachix/secretspec/archive/refs/tags/v0.17.0.tar.gz"
-  sha256 "eca629033bd3e86f6c746f11e2cc0d7a0cb73ec7806bd49e6cfec0f8cd131c16"
+  url "https://github.com/cachix/secretspec/archive/refs/tags/v0.17.1.tar.gz"
+  sha256 "2701b24fc2aaea6e888500df5c9f1cb2ec0d1b84aa3773ee8046754b9c86787a"
   license "Apache-2.0"
   head "https://github.com/cachix/secretspec.git", branch: "main"
 
