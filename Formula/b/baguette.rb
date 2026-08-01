fc1566a7a070c6cf853013facef2877a2c8c84f6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-01 20:14:47 +0000

    baguette 0.1.88

--- Formula/b/baguette.rb
+++ Formula/b/baguette.rb
@@ -1,8 +1,8 @@
 class Baguette < Formula
   desc "Headless iOS Simulator manager and host-side input injection for iOS 26"
   homepage "https://tddworks.github.io/baguette/"
-  url "https://github.com/tddworks/baguette/archive/refs/tags/v0.1.87.tar.gz"
-  sha256 "2de6f1212e3be311073b2a4003aaa387d0b8b75555d199ef95aed1b2eaa0a2a0"
+  url "https://github.com/tddworks/baguette/archive/refs/tags/v0.1.88.tar.gz"
+  sha256 "efb8908fe5846032f749659022e14d46173be48f5225e2e531e73b6b30808e4b"
   license "Apache-2.0"
   head "https://github.com/tddworks/baguette.git", branch: "main"
 
