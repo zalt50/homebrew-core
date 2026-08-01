a09b93e4937de730b8c49ac237f1b63f8f6be388
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-01 06:41:12 +0000

    groovy: update 5.0.8 bottle.

--- Formula/g/groovy.rb
+++ Formula/g/groovy.rb
@@ -11,7 +11,7 @@ class Groovy < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "f19d943b190effa8bef15443b009b639564a91b55ee72eef5844857fc70c9cc9"
+    sha256 cellar: :any_skip_relocation, all: "fe82327e2bafae9e378727e55237899ce02667361f4c455d43acfb8a615abf8e"
   end
 
   depends_on "openjdk"
