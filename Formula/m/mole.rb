923b41a7cd86fbce39bead096db23ecb0746c3c0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-02 14:12:00 +0000

    mole: update 1.49.1 bottle.

--- Formula/m/mole.rb
+++ Formula/m/mole.rb
@@ -15,10 +15,10 @@ class Mole < Formula
   no_autobump! because: :bumped_by_upstream
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9ea405ab3384d34c8c6ef8489997aab4f696b378125c80044eac8306b1bafc8b"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d22f7de4fdd620143705f10cbf7e2f6d0497b686de0b9051f3d4c29a1ce7e735"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "851aed3cb232a0a1dbee3b4843123bc49f81abae7a8af4cc764f4dbc926fc1c8"
-    sha256 cellar: :any_skip_relocation, sonoma:        "754bf652ce3784bf5d72b490cc26b72eb72c564233add60a0b2d901243ede4b1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e94db500f8ec7db4dc0f2d9f6e963847d8a657f7a459cff832a446ac9b18ecdf"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "08ffc55ff3b0c59676b336a1773cf709132260bb55569942676592ff9dbf0b6e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d09ac08a199d79adac6aae88a76843a7a6428daacf11f9fe85501423a4eb1287"
+    sha256 cellar: :any_skip_relocation, sonoma:        "4a89036e8c37f27c2aa71b5a90479751989104376a4fc06e6f49b56fc238be8c"
   end
 
   depends_on "go" => :build
