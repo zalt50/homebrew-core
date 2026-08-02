f3430d18b75386c7d5f6d02806873bf11dbf0d69
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-02 18:10:39 +0000

    tock: update 2.0.4 bottle.

--- Formula/t/tock.rb
+++ Formula/t/tock.rb
@@ -7,12 +7,12 @@ class Tock < Formula
   head "https://github.com/kriuchkov/tock.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "339b1df88fa899397156ecbfff34e1a98d052a0240b7361cb3c8cff70dc2bf37"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a235080d499ef1ce9ced62e598e547b4f63914e608d46ba1559c4931b5b9b6fd"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d93c1d2d401a35b45e9cc53a2c1211ba312b2e5b67e645d00b4e6d814d05b8cf"
-    sha256 cellar: :any_skip_relocation, sonoma:        "ef008a0798ea3d784ec98371d054754389361225fbcd69b4f3c23fb45b705d02"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "dfb5f0915706dd32a5841fc1d1bf26b030b6ddd69041a0436afb554f53cd4ec2"
-    sha256 cellar: :any,                 x86_64_linux:  "808575d03cb90b9a1711b2de2d667c9645dfdfcffe42e7a85afee1138c4f4b2e"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "12ea4fa14be27932b2f90965d99865981a5d4d5c1dde1dc22be930a95efec741"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "594816977fa33fa0f293b9452c2d01584c09636146e111c4e0ecdc1b62e8e218"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5fa226da949604b5d85552515f881f3fc6ef2accb769179397052d5d13187fc7"
+    sha256 cellar: :any_skip_relocation, sonoma:        "d6481b0c70b33c9a09806908f35c18a7c933921b15df61ff9a56db791600b4c4"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "458995628753f7e262e001853134268b2d5a31c86f03ffd8841e40b75c34c9b3"
+    sha256 cellar: :any,                 x86_64_linux:  "3c97ff8695bac0581d418e43d0905d45b70960f5e9600dcb1ebe8d2239ba2785"
   end
 
   depends_on "go" => :build
