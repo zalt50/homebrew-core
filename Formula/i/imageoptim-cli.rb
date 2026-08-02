6ff0cfdd0971e68f0b06bdb1d335c14f6e03438c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-02 11:56:04 +0000

    imageoptim-cli 4.0.0

--- Formula/i/imageoptim-cli.rb
+++ Formula/i/imageoptim-cli.rb
@@ -1,8 +1,8 @@
 class ImageoptimCli < Formula
   desc "CLI for ImageOptim, ImageAlpha and JPEGmini"
   homepage "https://jamiemason.github.io/ImageOptim-CLI/"
-  url "https://github.com/JamieMason/ImageOptim-CLI/archive/refs/tags/3.1.9.tar.gz"
-  sha256 "35aee4c380d332355d9f17c97396e626eea6a2e83f9777cc9171f699e2887b33"
+  url "https://github.com/JamieMason/ImageOptim-CLI/archive/refs/tags/4.0.0.tar.gz"
+  sha256 "29a6d28984273eb70ab8be03ea028a5b7285b051d442598e41bd77306ead8f52"
   license "MIT"
   head "https://github.com/JamieMason/ImageOptim-CLI.git", branch: "main"
 
@@ -16,21 +16,17 @@ class ImageoptimCli < Formula
     sha256 cellar: :any_skip_relocation, all: "a773e296348e3eefcf67f3b5101d88df39de5a2cf3746ddc67bb14542620c105"
   end
 
-  depends_on "yarn" => :build
+  depends_on "rust" => :build
   depends_on :macos
-  depends_on "node"
 
   def install
-    # Adjust package.json's bin and add missing shebang to avoid bundling node
-    inreplace "src/imageoptim.ts", "import chalk from 'chalk'", "#!/usr/bin/env node\n\nimport chalk from 'chalk'"
-    system "npm", "pkg", "set", "bin.imageoptim=dist/imageoptim.js"
-    system "yarn", "install"
-    system "npm", "run", "build:ts"
-    system "npm", "install", *std_npm_args
-    bin.install_symlink libexec.glob("bin/*")
+    system "cargo", "install", *std_cargo_args
   end
 
   test do
-    assert_equal version.to_s, shell_output("#{bin}/imageoptim -V").chomp
+    assert_match version.to_s, shell_output("#{bin}/imageoptim -V")
+
+    cp test_fixtures("test.png"), testpath/"test.png"
+    assert_match "test.png", shell_output("#{bin}/imageoptim --dry-run --no-color test.png")
   end
 end
