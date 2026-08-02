5a25b11c3484287e08d62c7685bfd85ecf8c25ad
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-02 12:51:17 +0000

    pv-migrate: update 3.6.0 bottle.

--- Formula/p/pv-migrate.rb
+++ Formula/p/pv-migrate.rb
@@ -12,12 +12,12 @@ class PvMigrate < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "589185d9e27ab2a2ace05d316155a3e7fa8fad2d1d478054b6c69f1fcf17d80d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "51f26866b7fb7c5896a279f975ed419a50ccd4757ecfaa8f66d87a79dc74eb68"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c43c9146d4aef96bd853573d2ac182b61f691b15da7d3c02cf2c3e4b42656e54"
-    sha256 cellar: :any_skip_relocation, sonoma:        "1792243fef3afe8355b46648cdbd087c673451ef6d2e3d0609688386de300324"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "55b07db0b03f916584924ff8dd227685164bf20946cfacb2483159f4567587c2"
-    sha256 cellar: :any,                 x86_64_linux:  "3e9e0e3769a41c2ef511bd5315086137a984af78e1ebaca123dc21fb8f2d705a"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a7aee03994dbfd423291743c311f1221fd487e3cac58fae4d0974773ecf598a2"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e40dcf449eda9466eeeb23cad0a15152dc12313928b77a40491ac6798f11ba35"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "698c6f46cf7bbdddcc8c6a1384792f31f3b82945e2b0abbd1a287357edecdbf3"
+    sha256 cellar: :any_skip_relocation, sonoma:        "ee6dc14f04241daf4a8b1ec0fb552b6a20f4489dd57b7843c5efb195068f5ff1"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c594a875618486488fce75fbcb9b2ef6f201240119dc512aaed8ea86d2d1dce9"
+    sha256 cellar: :any,                 x86_64_linux:  "d6a8e387ba5ec4c96d45fadf694378bb1670a58fa952c13c175f9b83a40688d6"
   end
 
   depends_on "go" => :build
