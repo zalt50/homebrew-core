a83f976fb94f3eff55e0f3aeba608612342e3761
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-02 17:25:29 +0000

    go-librespot 0.8.0

--- Formula/g/go-librespot.rb
+++ Formula/g/go-librespot.rb
@@ -1,8 +1,8 @@
 class GoLibrespot < Formula
   desc "Spotify client"
   homepage "https://github.com/devgianlu/go-librespot"
-  url "https://github.com/devgianlu/go-librespot/archive/refs/tags/v0.7.4.tar.gz"
-  sha256 "9bf982c9afb8ec04bceb12a084addf5dd35ed601225bf3bfb6382a67e086b9c7"
+  url "https://github.com/devgianlu/go-librespot/archive/refs/tags/v0.8.0.tar.gz"
+  sha256 "444e32986bf61a2b011bb672793d0d2ebcf07ccdcefab8dc8b99c618bd4ad6bc"
   license "GPL-3.0-only"
   head "https://github.com/devgianlu/go-librespot.git", branch: "master"
 
