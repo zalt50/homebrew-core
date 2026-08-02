1cea8ad366f4b03db83eb547c43a63316823627d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-02 06:13:01 +0000

    codanna: update 0.13.0 bottle.

--- Formula/c/codanna.rb
+++ Formula/c/codanna.rb
@@ -6,12 +6,12 @@ class Codanna < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "577b3566ea44062b4ba4cbaa9688fde73a86874156c7dece4f0bf8e3155f47a9"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "18064f6e4c167610e0750edda2b6f05dce29b9f115048fe71a8909e2fb14962d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "facd8951835a92c58444f4ee8134a4e7f16f040019a5cdef836c5ceb9964840f"
-    sha256 cellar: :any_skip_relocation, sonoma:        "a696dfb91bc3d1d98479e22d50a9eefae19acf054373b5a46d072254822b9c11"
-    sha256 cellar: :any,                 arm64_linux:   "828010c8e9c738b4ad6056212d11a6bd42f221898d6e3c0858c05be391e388f6"
-    sha256 cellar: :any,                 x86_64_linux:  "7e8a693ece04430e7e490a564f8aa0c5bc1a172d3027b499f506517cbb4eb5d0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "18d1ffd1dc403af1392eab7782d71fae43317f1dc6b9892faa9e00f76ca6fa47"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d856bac0d07ea5bacd21aeba0a3d4b6b5b2bc00c3d2482d5ff91c59903bab43e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "eaee6b99417be0a542cc1da10e2c1886b3ab315acf5064a9da957e0bac2e728c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "d02433150da16403ba80df0f148f329d78f475c76b6bddad001cd9069388f4ba"
+    sha256 cellar: :any,                 arm64_linux:   "ffacf80b1b2b977f0d0a0c115d675f8f7bc4a34f3addc0c933bcce0e2d413af5"
+    sha256 cellar: :any,                 x86_64_linux:  "3c93eb715a66c2eb2cb7fa1529531830d066d7c989a434e041f790704fab7fc7"
   end
 
   depends_on "pkgconf" => :build
