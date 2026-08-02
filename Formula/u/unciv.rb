40246b8b7100789c4c070d866718225788b2c7e1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-02 09:42:22 +0000

    unciv: update 4.21.5 bottle.

--- Formula/u/unciv.rb
+++ Formula/u/unciv.rb
@@ -13,7 +13,7 @@ class Unciv < Formula
   no_autobump! because: :bumped_by_upstream
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "8a06abc98d42cd2cd4aef0718d35d45da8187e87a4e2a07c81dca1e9eec3b68a"
+    sha256 cellar: :any_skip_relocation, all: "cf4e0935908f4c468e9b68037d7fc6c205b559481112a38714364a8167eaa762"
   end
 
   depends_on "openjdk"
