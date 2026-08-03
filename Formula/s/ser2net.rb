af8a1cd2dfcabe6ac95ba14188b4c2d42b22ae4d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-03 18:32:01 +0000

    ser2net: update 4.6.8 bottle.

--- Formula/s/ser2net.rb
+++ Formula/s/ser2net.rb
@@ -11,12 +11,12 @@ class Ser2net < Formula
   end
 
   bottle do
-    sha256               arm64_tahoe:   "826a5c4d76bc78574fbf69dc9fba7c15bcd24efc78a2db3749389b3ba25a662e"
-    sha256               arm64_sequoia: "b2a4a4a84f7dd3011ba5a9c7ac1817bf725f847241b951edc7baa0fb26949fac"
-    sha256               arm64_sonoma:  "905a3d6cb27b9ee2eb89bde470232112695067f626fb5d623c9fdc93f76ed8c3"
-    sha256 cellar: :any, sonoma:        "956a9630a71d18386912c344979746f842c234c05919cd8959a00c70a7c31677"
-    sha256               arm64_linux:   "a6a11cc4e843769d9c58aff84407bbf482443fb8bfcad51f209d178bd9f38847"
-    sha256               x86_64_linux:  "e151fb33658fdea88474c879275d6b4fa26caf8f97a12600a00b4ce3fcb71807"
+    sha256               arm64_tahoe:   "3c214f961d732a82ac62fe977dde82c7912de54d826b1e23990fc035572f4e02"
+    sha256               arm64_sequoia: "fd7227618e0430537da799a8e4dae00cd08c880aba27dfaf727f5df16d6faf39"
+    sha256               arm64_sonoma:  "9850a09ea9910b84cb9879ab4b7df9d77ca1e76b4315a7d863ea6ea8c303cf02"
+    sha256 cellar: :any, sonoma:        "ba8e0b026ac4be7f8e6db552172841afda4d5f4d14e75bc513b42d5ac97dd883"
+    sha256               arm64_linux:   "bbe0fa0064edb0b44ecec3d77ef667880961eadd37b10183f0f7b3d309b3622b"
+    sha256               x86_64_linux:  "29357f49e5e690898ace616231506a8dbfa9972d2f60385d67767f2170a371cc"
   end
 
   depends_on "gensio"
