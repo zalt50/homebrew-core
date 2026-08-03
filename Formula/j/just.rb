bac6ee6b5dbdc027f282929a2d613d64afd2e04a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-03 20:49:47 +0000

    just 1.58.0

--- Formula/j/just.rb
+++ Formula/j/just.rb
@@ -1,8 +1,8 @@
 class Just < Formula
   desc "Handy way to save and run project-specific commands"
   homepage "https://just.systems"
-  url "https://github.com/casey/just/archive/refs/tags/1.57.0.tar.gz"
-  sha256 "905c556aad3c0a4b0376db98b706a9aa3485fcf50a30377d50737bf20f3792cb"
+  url "https://github.com/casey/just/archive/refs/tags/1.58.0.tar.gz"
+  sha256 "c8a36e6e9397f2fdfcb0cc246fcdb790b52a784f3c8cabc0d8baeb031852a148"
   license "CC0-1.0"
   head "https://github.com/casey/just.git", branch: "master"
 
