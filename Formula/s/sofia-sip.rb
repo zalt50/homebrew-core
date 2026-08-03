1112a9f448a0b5721c9c8ced5720e16209b13e12
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-03 21:18:15 +0000

    sofia-sip 1.13.18

--- Formula/s/sofia-sip.rb
+++ Formula/s/sofia-sip.rb
@@ -1,8 +1,8 @@
 class SofiaSip < Formula
   desc "SIP User-Agent library"
   homepage "https://sofia-sip.sourceforge.net/"
-  url "https://github.com/freeswitch/sofia-sip/archive/refs/tags/v1.13.17.tar.gz"
-  sha256 "daca3d961b6aa2974ad5d3be69ed011726c3e4d511b2a0d4cb6d878821a2de7a"
+  url "https://github.com/freeswitch/sofia-sip/archive/refs/tags/v1.13.18.tar.gz"
+  sha256 "d2ad4e64753a7c9843b766b8de8081d9c1d7acfaeb53c12b3aed7fdb9235766c"
   license "LGPL-2.1-or-later"
 
   bottle do
