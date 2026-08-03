a3c7bf26dbe8a9112b8da54d0d2ca6bd92404518
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-03 21:05:40 +0000

    delve: update 1.27.1 bottle.

--- Formula/d/delve.rb
+++ Formula/d/delve.rb
@@ -7,12 +7,12 @@ class Delve < Formula
   head "https://github.com/go-delve/delve.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5ac70d93a74e61aeff4697f5aa6f2828faaf7a7d92cbf22e731662f754b3f436"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5ac70d93a74e61aeff4697f5aa6f2828faaf7a7d92cbf22e731662f754b3f436"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5ac70d93a74e61aeff4697f5aa6f2828faaf7a7d92cbf22e731662f754b3f436"
-    sha256 cellar: :any_skip_relocation, sonoma:        "ecf83f7874c1cfe9be3319a323bdbe9665f1971d9a2ed9bb3b6e848f6e2f942e"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "4bea0d8a418386d30225006b82bb0fbab87da17b3ea74a7472fc7312b4d52a25"
-    sha256 cellar: :any,                 x86_64_linux:  "a4d24fbdef8dccf6b987ac9173abec27f467d30357fc438ba0117ab0971e4a10"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1ac0e5294e2e25e71d7a2ed927ed042817bef29a6a141d57a5fcd8acfc62ce2b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1ac0e5294e2e25e71d7a2ed927ed042817bef29a6a141d57a5fcd8acfc62ce2b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1ac0e5294e2e25e71d7a2ed927ed042817bef29a6a141d57a5fcd8acfc62ce2b"
+    sha256 cellar: :any_skip_relocation, sonoma:        "1cc967a17c14b5195fd1f20e06c0d018ecf309c140a47c8289036f770fb2375f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "def0eee6dd0963c8648068f8c8464b9dbc2507a3930fa62591da8383e1bf204e"
+    sha256 cellar: :any,                 x86_64_linux:  "eff00cdfcf9dcb6967dc8781ed7674cc52be1c4384d28536de533659aba704ff"
   end
 
   depends_on "go" => :build
