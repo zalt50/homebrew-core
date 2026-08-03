7ec202ab85ea348f0d25d5199996ae72c09564d4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-03 23:11:05 +0000

    arcadedb 26.8.1

--- Formula/a/arcadedb.rb
+++ Formula/a/arcadedb.rb
@@ -1,8 +1,8 @@
 class Arcadedb < Formula
   desc "Multi-Model DBMS: Graph, Document, Key/Value, Search, Time Series, Vector"
   homepage "https://arcadedb.com"
-  url "https://github.com/ArcadeData/arcadedb/releases/download/26.7.3/arcadedb-26.7.3.tar.gz"
-  sha256 "eec0cd2a669f8050d81d3d91fa53b12eee0a6a682c307cd484508f7d3b08305f"
+  url "https://github.com/ArcadeData/arcadedb/releases/download/26.8.1/arcadedb-26.8.1.tar.gz"
+  sha256 "e9ef74e7bc2a2ca4bf924c20d1bac9076a636aeba9e97c25e8d85e948dcd3ccb"
   license "Apache-2.0"
 
   livecheck do
