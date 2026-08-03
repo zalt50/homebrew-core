a83555574549ec151805af60fb7a1652fe9538c6
Nicolas Karolak <nicolas@karolak.fr>
2026-08-03 11:06:31 +0200

    lfk 0.15.16 (new formula)

---
+++ Formula/l/lfk.rb
@@ -0,0 +1,23 @@
+class Lfk < Formula
+  desc "Terminal user interface for navigating and managing Kubernetes clusters"
+  homepage "https://github.com/janosmiko/lfk"
+  url "https://github.com/janosmiko/lfk/archive/refs/tags/v0.15.16.tar.gz"
+  sha256 "16345459778efad67dd2e9ec8b60b88ecf99d5b7077b89835a9b484729cf7943"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+    ldflags = %W[
+      -X github.com/janosmiko/lfk/internal/version.Version=#{version}
+      -X github.com/janosmiko/lfk/internal/version.BuildDate=#{Time.now.utc.iso8601}
+    ]
+    system "go", "build", *std_go_args(ldflags:)
+  end
+
+  test do
+    # This program is TUI-only
+    assert_match version.to_s, shell_output("#{bin}/lfk version")
+  end
+end
