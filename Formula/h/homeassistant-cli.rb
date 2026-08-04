03bd4b479599424dcdf20b2f7844ff72b6cffbd8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-04 11:29:14 +0000

    homeassistant-cli: update 1.0.0_4 bottle.

--- Formula/h/homeassistant-cli.rb
+++ Formula/h/homeassistant-cli.rb
@@ -10,14 +10,14 @@ class HomeassistantCli < Formula
   head "https://github.com/home-assistant-ecosystem/home-assistant-cli.git", branch: "dev"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e83b6a51205da4d01b25de3621cea3ab51f6d7fdd34519fa1895e5a34419282d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3a62807577210775303f254c6ce933665704fc7c7d7a4dcc266b2d13bce79391"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "000cec53fcef067041128f446a91ae9c0b52e6ae3a042513f638966a49009172"
-    sha256 cellar: :any_skip_relocation, tahoe:         "3c1eeb8ccf4db685e1554fa31497c6686ab46d2862ed8eb5fdc2e6d6dfde7415"
-    sha256 cellar: :any_skip_relocation, sequoia:       "b09dbd334dcb2fdafedc62af2d51221838e6ab94b34f62134a5b86338a6b4c99"
-    sha256 cellar: :any_skip_relocation, sonoma:        "45713ca2bf7b79e022c6ff40a5fd974a85d18e7f3aaf19e1b38bef6f46099115"
-    sha256 cellar: :any,                 arm64_linux:   "08a424d63e821e287098832430ce4dfe69ec83ab97142c67e9561cc8750095d7"
-    sha256 cellar: :any,                 x86_64_linux:  "e234053e55ebfb9b65c2929ee496e749fc34358902116b53be4cbcb147098b1c"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "03b8f9b918726ad04d1f72a7eaa474bbd1170df624a71b002b40e806bae9e1f6"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f301cb8e3bd79bd5a9efe9bb7c10202ccfd1898af10896c481595cc5b7e5e725"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a18a40147d84557a0c66e94d9add77b80e8ed949b31cdbc442d163c26adaa746"
+    sha256 cellar: :any_skip_relocation, tahoe:         "52d4ba3a9e867beef12d2dbb67de41b0610e07f8fa0bd51fbd2f8e87665e47d7"
+    sha256 cellar: :any_skip_relocation, sequoia:       "f2acd718174cec2846ed015f7e09e9fbf8f16a6ee11f101e580c0768260d482c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "f5c68dc543ca54d23ed7d6a5c108220da04016ffc90ef04db66d9c3ea668787d"
+    sha256 cellar: :any,                 arm64_linux:   "a61359a9d222e02902813fae079f2f67013e7f71d200108bd7b5ab92861c4ec4"
+    sha256 cellar: :any,                 x86_64_linux:  "c208fc42f0f6afb5b04b060eaf7ae7d4429a21ca9ff5370359013a164b65d58f"
   end
 
   depends_on "certifi" => :no_linkage
