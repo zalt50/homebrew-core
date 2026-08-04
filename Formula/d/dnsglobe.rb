3f3561c179fd042ae8dd080ed1aa0396b6cb5fad
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-04 07:04:35 +0000

    dnsglobe: add 0.4.0 bottle.

--- Formula/d/dnsglobe.rb
+++ Formula/d/dnsglobe.rb
@@ -6,6 +6,15 @@ class Dnsglobe < Formula
   license "MIT"
   head "https://github.com/514-labs/dnsglobe.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5796861b7a196f634812edd09acc13aae0ca8aa56a306deabd00379aed5d0526"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "735add78828594599af16d44d3bc6ed9e4ce07f7471030c679473fe8692893ae"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d04c09ba360db088b094acd6539ce1189d703f1202f4732065984afbd37b8866"
+    sha256 cellar: :any_skip_relocation, sonoma:        "7d2f105424d64eddaa316bfee2cca4304a6f75b678f099dee3618859c7329b59"
+    sha256 cellar: :any,                 arm64_linux:   "d5e04bd675c8182592d4ed6c928ac10606e2a318b86b38fea8568d006472b507"
+    sha256 cellar: :any,                 x86_64_linux:  "5a6c58049d2f7bf3c9ade5815be961472415222095b62006a7da2912e8494adc"
+  end
+
   depends_on "rust" => :build
 
   def install
