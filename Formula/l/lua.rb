a8fc7b87d5cea9d63d7c720667b428c8766435d8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-04 03:17:36 +0000

    lua 5.5.1

--- Formula/l/lua.rb
+++ Formula/l/lua.rb
@@ -1,8 +1,8 @@
 class Lua < Formula
   desc "Powerful, lightweight programming language"
   homepage "https://www.lua.org/"
-  url "https://www.lua.org/ftp/lua-5.5.0.tar.gz"
-  sha256 "57ccc32bbbd005cab75bcc52444052535af691789dba2b9016d5c50640d68b3d"
+  url "https://www.lua.org/ftp/lua-5.5.1.tar.gz"
+  sha256 "1c4b4068d67061f2a2231ad2b5422e77acea1487ea9890f6320af614f4373dce"
   license "MIT"
   compatibility_version 1
 
