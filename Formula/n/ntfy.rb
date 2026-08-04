3985751de7b69f6a61afea015d42118914560be8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-04 06:21:55 +0000

    ntfy 2.27.0

--- Formula/n/ntfy.rb
+++ Formula/n/ntfy.rb
@@ -1,8 +1,8 @@
 class Ntfy < Formula
   desc "Send push notifications to your phone or desktop via PUT/POST"
   homepage "https://ntfy.sh/"
-  url "https://github.com/binwiederhier/ntfy/archive/refs/tags/v2.26.3.tar.gz"
-  sha256 "6a1378a383133ae500bbe0658eca06a536cafd5c46c83cb1412e6dc4bf49cd70"
+  url "https://github.com/binwiederhier/ntfy/archive/refs/tags/v2.27.0.tar.gz"
+  sha256 "1f46463acc177479860861cca8d7ec7da92244eab7ea42d20a89c17055d6641c"
   license any_of: ["Apache-2.0", "GPL-2.0-only"]
   head "https://github.com/binwiederhier/ntfy.git", branch: "main"
 
