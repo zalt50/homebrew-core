3085b9573df124195f596268586e6111737a2e03
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-05 03:49:40 +0000

    ldcli: update 3.9.0 bottle.

--- Formula/l/ldcli.rb
+++ Formula/l/ldcli.rb
@@ -7,12 +7,12 @@ class Ldcli < Formula
   head "https://github.com/launchdarkly/ldcli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "72c90aa26cc40295df091bd52a7365f35684391a220e8e37d9071a9604971549"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f826e92aef9ca4b77dfb1ae07c01623d0084a890934f082972580e30e38cb582"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cec75e05ff6ee4af07ed1503549df99e8b97e0225c80a704cf6eb4a346aea2ed"
-    sha256 cellar: :any_skip_relocation, sonoma:        "0cca02109b4223c18bd424765b14a2ed1aaca34108fe6c414bbd25795e6b3f3f"
-    sha256 cellar: :any,                 arm64_linux:   "7be6bcd016317cad68f29e3c98a9657bd8c3396b58507994803f754ff31281a9"
-    sha256 cellar: :any,                 x86_64_linux:  "3351e6f8c1c2c8047ff258857fb941e72c6e015c4bab8ac1378146c3fa7cf60e"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "afdfbe652ed433a983ee89a70660f5808fda00c34605b9f22591cb4dd1601b28"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bd898fc30eeaab5a340c08e3b0c39e6436e7762f9a1323781ef3fe2d8200894a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a09a7a1a2e618067758078ecbe826b52dcf88cf8d45f5c0be72f28ac82cf5d10"
+    sha256 cellar: :any_skip_relocation, sonoma:        "54ac77fa9a68de32ffd3b809bf555c647cfc3ccf965827fe8c4b00f219368991"
+    sha256 cellar: :any,                 arm64_linux:   "082c547eb900507b9dddb0b57e33f377726051351a86ee7bb6496381e86492cc"
+    sha256 cellar: :any,                 x86_64_linux:  "d82cd3bf9e7faf9406a7ce22a2087d2589dfd3bc18e8c0ed7690f4f08aa37d0e"
   end
 
   depends_on "go" => :build
