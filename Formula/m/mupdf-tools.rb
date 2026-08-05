15a13ebd59919e9e995dad7000e5429216437539
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-05 03:50:50 +0000

    mupdf-tools: update 1.28.2 bottle.

--- Formula/m/mupdf-tools.rb
+++ Formula/m/mupdf-tools.rb
@@ -11,12 +11,12 @@ class MupdfTools < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "73967fbcff0300ef7a5d7ad08714845849d3a18ba9cb3ecca922daf8d8abe527"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7832b33da3edb6bbf998cad4c99a7b03c48bba9378dec39e275566af05b93012"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d2e939f2ba09aed67157517f1e8c588a4425a0ef51799e8d14ac86a914daffc8"
-    sha256 cellar: :any_skip_relocation, sonoma:        "79f247a81f6c5a6e0aee2df55612d5b66dfee6096c56b4c792145770e35c01c7"
-    sha256 cellar: :any,                 arm64_linux:   "f3b96be412032412152f3b01654de869fd50b7d9cb17eec80af943c1bcd4fd81"
-    sha256 cellar: :any,                 x86_64_linux:  "57da6feb6abda4424711bac79c265652f9843d14a47f9959c44a1b7fd3c05c2e"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "69a127f018a1b13c41d186f5653e0fa2dc87f306d2bda9532d0f9636659cb77d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "618b71ee0afb6f580c63269c5173e6bef7e080cc2fcfa1def95092c986ea388c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "61d4ec722053dccb75ae71561bd5cce6d107a11bcdf2954f059abcf60f5978bd"
+    sha256 cellar: :any_skip_relocation, sonoma:        "bba5e444e832028823db5f4df2a4dfecc5de5e820da8a5fcb2d99145fe6a1b07"
+    sha256 cellar: :any,                 arm64_linux:   "110e2fc500ce56ea960b3dd3323ce322dbf82de9d867d187b05438141af32328"
+    sha256 cellar: :any,                 x86_64_linux:  "3c92a26441c0b7bad81ccd6e7f62f2d9c1ccfeb00c7c38f5ff448228c6f8f6e0"
   end
 
   conflicts_with "mupdf", because: "mupdf and mupdf-tools install the same binaries"
