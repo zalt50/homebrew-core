ea9f02e94778535399a76791821abeb4d132b58a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-05 10:39:50 +0000

    renovate: update 44.12.0 bottle.

--- Formula/r/renovate.rb
+++ Formula/r/renovate.rb
@@ -18,7 +18,7 @@ class Renovate < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "642202ed9e8b4a98264626590c2b32d59b13913b3224d15446e795200c37fb36"
+    sha256 cellar: :any_skip_relocation, all: "1a5e449a70325af56015cacb6ec958fcf491c7a91659550e1dbb13a2beb9fe6c"
   end
 
   depends_on "node@24"
