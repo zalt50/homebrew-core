f4d3d005eae677385c42fcf572d183d521f8397b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-05 19:37:15 +0000

    web-ext: update 10.6.0 bottle.

--- Formula/w/web-ext.rb
+++ Formula/w/web-ext.rb
@@ -6,7 +6,7 @@ class WebExt < Formula
   license "MPL-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "a41120c10c9372cb7c405d5066a15c8e0cf8e4c189bb5f5100c88d1ad7142e59"
+    sha256 cellar: :any_skip_relocation, all: "ca160932c7caac54082402a76e0b593714f47cfa66eb119cfe6e6462a161dae5"
   end
 
   depends_on "node"
