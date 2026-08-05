d5d0693c52ae1007a17fddec68d3c52ad7efd553
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-05 17:23:48 +0000

    circleci 1.0.47091
    Update to the new 1.x series for the CircleCI CLI.
    
    - Fix build tags.
    - Generate completions for all shells now supported.
    - Man page now supported.
    - Telemetry opt out env var changed to standard one.
    - Version is outputted slightly differently.
    - There's no self update functionality any more, so remove the check.

--- Formula/c/circleci.rb
+++ Formula/c/circleci.rb
@@ -1,10 +1,10 @@
 class Circleci < Formula
-  desc "Enables you to reproduce the CircleCI environment locally"
-  homepage "https://circleci.com/docs/guides/toolkit/local-cli/"
+  desc "Official command-line tool for CircleCI"
+  homepage "https://cli.circleci.com"
   # Updates should be pushed no more frequently than once per week.
   url "https://github.com/CircleCI-Public/circleci-cli.git",
-      tag:      "v0.1.38646",
-      revision: "f96353c6bb0085274f4061376e7c3ad719f265bc"
+      tag:      "v1.0.47091",
+      revision: "66186e495abd408f2e9f0022d27b64793bfb783b"
   license "MIT"
   head "https://github.com/CircleCI-Public/circleci-cli.git", branch: "main"
 
@@ -25,28 +25,19 @@ class Circleci < Formula
   depends_on "go" => :build
 
   def install
-    ldflags = %W[
-      -X github.com/CircleCI-Public/circleci-cli/version.packageManager=#{tap.user.downcase}
-      -X github.com/CircleCI-Public/circleci-cli/version.Version=#{version}
-      -X github.com/CircleCI-Public/circleci-cli/version.Commit=#{Utils.git_short_head}
-      -X github.com/CircleCI-Public/circleci-cli/telemetry.SegmentEndpoint=https://api.segment.io
-    ]
-    system "go", "build", *std_go_args(ldflags:)
+    ldflags = "-X main.version=#{version}"
+    system "go", "build", *std_go_args(ldflags:), "./cmd/circleci"
 
-    generate_completions_from_executable(bin/"circleci", "--skip-update-check", "completion",
-                                        shells: [:bash, :zsh])
+    generate_completions_from_executable(bin/"circleci", "completion")
+    system bin/"circleci", "man", "--output", man1/"circleci.1"
   end
 
   test do
-    ENV["CIRCLECI_CLI_TELEMETRY_OPTOUT"] = "1"
+    ENV["DO_NOT_TRACK"] = "1"
     # assert basic script execution
-    assert_match(/#{version}\+.{7}/, shell_output("#{bin}/circleci version").strip)
+    assert_match(/^circleci #{version} \(\h{12}\)$/, shell_output("#{bin}/circleci version").strip)
     (testpath/".circleci.yml").write("{version: 2.1}")
     output = shell_output("#{bin}/circleci config pack #{testpath}/.circleci.yml")
     assert_match "version: 2.1", output
-    # assert update is not included in output of help meaning it was not included in the build
-    assert_match(/update.+This command is unavailable on your platform/, shell_output("#{bin}/circleci help 2>&1"))
-    assert_match "update is not available because this tool was installed using homebrew.",
-      shell_output("#{bin}/circleci update")
   end
 end
