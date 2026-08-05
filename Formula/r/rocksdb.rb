ad4e6571c1fc29e1a1a3a597af3731f970b79ec2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-05 00:11:42 +0000

    rocksdb 11.8.0

--- Formula/r/rocksdb.rb
+++ Formula/r/rocksdb.rb
@@ -1,8 +1,8 @@
 class Rocksdb < Formula
   desc "Embeddable, persistent key-value store for fast storage"
   homepage "https://rocksdb.org/"
-  url "https://github.com/facebook/rocksdb/archive/refs/tags/v11.1.2.tar.gz"
-  sha256 "3aba946031d27734eeea68ced7dd6f46629ba23a7b49945eb29aea68d1595311"
+  url "https://github.com/facebook/rocksdb/archive/refs/tags/v11.8.0.tar.gz"
+  sha256 "86434e099fc27a24bf0ebd98114a8dcac55dba60a0ba291faf700f3bd84f47aa"
   license any_of: ["GPL-2.0-only", "Apache-2.0"]
   compatibility_version 1
   head "https://github.com/facebook/rocksdb.git", branch: "main"
