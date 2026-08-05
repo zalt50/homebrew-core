6a4852b5cb75c791be786a22e41e635e6e37b707
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-05 09:28:30 +0000

    delly: add 2.3.1 bottle.

--- Formula/d/delly.rb
+++ Formula/d/delly.rb
@@ -6,6 +6,15 @@ class Delly < Formula
   license "BSD-3-Clause"
   head "https://github.com/dellytools/delly.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any, arm64_tahoe:   "4d305d4bafc55fb97920c21950004f8e40ca4f43ab73cdbd4028e7594ed8ff03"
+    sha256 cellar: :any, arm64_sequoia: "63fc590a4872b683b417e33e9031d4f74377513563809e598018a3b431939da3"
+    sha256 cellar: :any, arm64_sonoma:  "5179b7118b48e17f4a2d02a0138a6ee450ee6382dcb65bdd50517901a59be6dd"
+    sha256 cellar: :any, sonoma:        "834c274742d7ec449cf8a1bdd2c9f1d64312d83cc25beca0d1cee94db55e5f2f"
+    sha256 cellar: :any, arm64_linux:   "0671ac324c98977b4270ef18c30ae460fad728f4677a65f006ada28e5327a530"
+    sha256 cellar: :any, x86_64_linux:  "de3341eb6d9b7b958fd4c635a7984e546aa85ef4753496798792e3923c8c41c0"
+  end
+
   depends_on "boost"
   depends_on "htslib"
   depends_on "xz"
