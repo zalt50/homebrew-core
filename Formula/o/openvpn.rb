c29aaa7b8b5d6e9fc00f10f9120d1015ccdd78cf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-06 15:41:19 +0000

    openvpn 2.7.6

--- Formula/o/openvpn.rb
+++ Formula/o/openvpn.rb
@@ -1,9 +1,9 @@
 class Openvpn < Formula
   desc "SSL/TLS VPN implementing OSI layer 2 or 3 secure network extension"
   homepage "https://openvpn.net/community/"
-  url "https://swupdate.openvpn.net/community/releases/openvpn-2.7.5.tar.gz"
-  mirror "https://build.openvpn.net/downloads/releases/openvpn-2.7.5.tar.gz"
-  sha256 "c6864b3c7d4e059c7d6ce22d1b5fa646c8b379a06af872eeb9792b6083a44ac4"
+  url "https://swupdate.openvpn.net/community/releases/openvpn-2.7.6.tar.gz"
+  mirror "https://build.openvpn.net/downloads/releases/openvpn-2.7.6.tar.gz"
+  sha256 "10e24a9385f23cc38cc5cf448f3ca0769f939bc4cbecc4f4647d7e006e52db74"
   license "GPL-2.0-only" => { with: "openvpn-openssl-exception" }
 
   livecheck do
