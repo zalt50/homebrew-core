edd2ffbc28770dc886f26ceb01217c5dfb2e982b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-06 06:15:40 +0000

    jfrog-cli 2.118.0

--- Formula/j/jfrog-cli.rb
+++ Formula/j/jfrog-cli.rb
@@ -1,8 +1,8 @@
 class JfrogCli < Formula
   desc "Command-line interface for JFrog products"
   homepage "https://docs.jfrog.com/integrations/docs/jfrog-cli"
-  url "https://github.com/jfrog/jfrog-cli/archive/refs/tags/v2.117.0.tar.gz"
-  sha256 "f90d930aed97ac6b3b2357a5984cf0deafe6980ee335ec5b2984f5b3a26c7eec"
+  url "https://github.com/jfrog/jfrog-cli/archive/refs/tags/v2.118.0.tar.gz"
+  sha256 "c29dd05a4714c10a36e9e6ea394235ebcad774c2eab4bb39565ff10716ecd0e9"
   license "Apache-2.0"
   head "https://github.com/jfrog/jfrog-cli.git", branch: "master"
 
