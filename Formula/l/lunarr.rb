0933b6fbfdbe112a6ecf249b94fed23b34e4c695
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-06 14:09:04 +0000

    lunarr: update 0.9.2 bottle.

--- Formula/l/lunarr.rb
+++ Formula/l/lunarr.rb
@@ -7,12 +7,12 @@ class Lunarr < Formula
   head "https://github.com/lunarr-app/lunarr-go.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "d0a5fb8854913128e9d856a75f1d9da732bf524bc1c1cdf13c3e961a42f4283b"
-    sha256 cellar: :any,                 arm64_sequoia: "28b39545813899ae0ffe2f4acbb74372f0c2ffc57bbd24f7d15e7e60bc40dc91"
-    sha256 cellar: :any,                 arm64_sonoma:  "b5ce20419b1e40e6c5e3c451b4192cac7b1d989d705fd4c48c87ee60f464642a"
-    sha256 cellar: :any,                 sonoma:        "30b53055df963ab0fa1f8de5ea69c5cb7fc18a2e4b1715f570a40d4badd7925a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "9a6514fd5d48f6ddfd2b899e3dc912f74b3e048560438733b6e77fe947cac83c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "69ce6b4bee4ea707eadde7af50faf9f1eb8a5d96df5423ab34e1d0c3cfbba582"
+    sha256 cellar: :any,                 arm64_tahoe:   "16c6f8ca6abbb7f9625d5635560bfc727e369f331228efe7ae29b8a4297b074a"
+    sha256 cellar: :any,                 arm64_sequoia: "a7f0060fb14642b172ba669c4b2e5327193a6b5801355161f5eda00f53d4ded3"
+    sha256 cellar: :any,                 arm64_sonoma:  "8e7220f2ba18c5f687f6fa13da8a5633f7f933744f4b2be4691ecaa5dddbeb60"
+    sha256 cellar: :any,                 sonoma:        "f9b029d86e77c98755429375b357c0a1e32b4264718fc26669c58475edaf7e36"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "0aaa8e641dfb4e84136122d1035f1e1f44280d5942889f891e2b97256d87718f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f0718562e716556ced713d31acc3095b0a2c9453dc7b27885830d8a45aa01bba"
   end
 
   depends_on "ffmpeg"
