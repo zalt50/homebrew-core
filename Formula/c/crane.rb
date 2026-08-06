8c97f097c2e7f2fb67ce4e94af8865bf26ee2afd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-06 03:45:51 +0000

    crane: update 0.21.9 bottle.

--- Formula/c/crane.rb
+++ Formula/c/crane.rb
@@ -7,12 +7,12 @@ class Crane < Formula
   head "https://github.com/google/go-containerregistry.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "21e9302d89eaee2226468f347ecc2f2b75a8e108701738c564f7c2ca9bafce7f"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "21e9302d89eaee2226468f347ecc2f2b75a8e108701738c564f7c2ca9bafce7f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "21e9302d89eaee2226468f347ecc2f2b75a8e108701738c564f7c2ca9bafce7f"
-    sha256 cellar: :any_skip_relocation, sonoma:        "a499285047cb22eb174fbb7319d4c32610e12fc6a96e14a9f95582dc3ca40ed3"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "63f1e4e3bae4942d883126b39c139177a15fc11429748620bcd9dffddaa667f1"
-    sha256 cellar: :any,                 x86_64_linux:  "85ca40fef1b22974e1dd1bb5e6f00ecb31063d853bf2e39d40f0856a3e51876b"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "da9debb8436aab46fea78f6853e4dc46d3cc5f7fa3ee24fba78a20a332e09dca"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "da9debb8436aab46fea78f6853e4dc46d3cc5f7fa3ee24fba78a20a332e09dca"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "da9debb8436aab46fea78f6853e4dc46d3cc5f7fa3ee24fba78a20a332e09dca"
+    sha256 cellar: :any_skip_relocation, sonoma:        "d2f6eeebed24749d9dfc177269456cc05db1d597d1f1f9bb8baa5fd2256c6b8b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f65aec042a82b024d3b42bd81237e692b747a1afa700186cad7185670c15ee53"
+    sha256 cellar: :any,                 x86_64_linux:  "4c172801cabf797c370af6a7f733f03ed5aa8945f98fbf4fb43ffaae3d21f790"
   end
 
   depends_on "go" => :build
