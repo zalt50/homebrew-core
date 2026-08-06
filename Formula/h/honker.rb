7d4ad1c15372eed55bf9205349194e568a7b922b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-06 08:21:24 +0000

    honker: add 0.4.0 bottle.

--- Formula/h/honker.rb
+++ Formula/h/honker.rb
@@ -5,6 +5,15 @@ class Honker < Formula
   sha256 "fde7ef3e6cc439573683730d7bb5f598d804bfc169e0e9c8488e5e59ff762148"
   license any_of: ["Apache-2.0", "MIT"]
 
+  bottle do
+    sha256 cellar: :any, arm64_tahoe:   "43bbb0659318bb2ebc904b95ddfca94d316afa5c48943e94f971577121e21456"
+    sha256 cellar: :any, arm64_sequoia: "5c76772088e6ee172ac9d0d7a804a889111fc77b5f8bd65c4e77e51f07253845"
+    sha256 cellar: :any, arm64_sonoma:  "561d1622c5e10b8e54aad3dd4dc4ebf98fd25d5bd9a527e4dab8f09ce3ff0da0"
+    sha256 cellar: :any, sonoma:        "21feef5f4255d26a640f1cb9b98df975d8b4b745bd9a2a72a0a94faee521de42"
+    sha256 cellar: :any, arm64_linux:   "0ff5a67aed26923e25204bcca3f58e9bccb12ca7478a118beb020a5c08b5a604"
+    sha256 cellar: :any, x86_64_linux:  "a2ec37d52091c6b9d5c73ee2d2118bd3d138dab3f76f405bb8dcb2b783b9be52"
+  end
+
   depends_on "rust" => :build
   depends_on "sqlite" # macOS sqlite can't load extensions
 
