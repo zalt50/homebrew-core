f1367336df908f2c47f4e8fa3395bcd06b5aa84a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-06 10:07:39 +0000

    newrelic-cli: update 0.113.4 bottle.

--- Formula/n/newrelic-cli.rb
+++ Formula/n/newrelic-cli.rb
@@ -12,12 +12,12 @@ class NewrelicCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b1956cfb7680c4a8207e45d7cfb00e58663331fc5e29064985e5a7c2dcb98b30"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9630d8f9fd0d01f3257c073c3092fbbd3dcd2debcafb0f41c188092bb9896da4"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a7bab566b4067a78110b2a0503fdfe2d9f41e365a7ff7e600fa63bc95eaa6103"
-    sha256 cellar: :any_skip_relocation, sonoma:        "934446d406f3b1f389de316cc6d94417c0c1075f4adbcd17950de0d6cde9858f"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "868a7d0af47fb7395f7d131f1e3a1a021e99fe1c779531a9ce4941b36aa40662"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "31f5b4bd609be35ce924b5f6303c5f0ee90228a24fbd7421f746fe8c259850f9"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0cc0d6b4a9d29fe46b4df26cd89c8a6d70eed9a85e14523f39f38b6b5036711c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "33644c63f4c836ea12b229f21344663bc911233a0daba49d405c61559bc2e9cb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "dbc2b6f142e4ceaf75267a509b59d9354eaeacceef56c51c65da624805fecfb7"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e69d9a464d107d7ff6ddf82692b523c2a503df5f406a83b04a23da02816d2426"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "33a71441ed3f42f0032f4f5b400189be81d2025f4b0268c8a0c9fb5711c2cebf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e95f5726422c06f30a7739bc2e0dff368aff4712d136231a746fc4c767ffa823"
   end
 
   depends_on "go" => :build
