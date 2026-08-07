572951b2d513d3e0c17c9c0966146f0ad8103cfa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-07 09:41:52 +0000

    modal: update 1.5.3_1 bottle.

--- Formula/m/modal.rb
+++ Formula/m/modal.rb
@@ -9,12 +9,12 @@ class Modal < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "33ae71591efb2a6d620b65d5eea1cd9963e4a5b4ea76f6cbf2b160a580d5a748"
-    sha256 cellar: :any, arm64_sequoia: "62ff8a8b401c5692300d783cfaf25afd65e1f0ec1c2ceb969669e930d3e5917d"
-    sha256 cellar: :any, arm64_sonoma:  "1e1af094ccfab4e781a3ba3b7e0323b26af7930baa0ffff5b6c28126456f72dc"
-    sha256 cellar: :any, sonoma:        "59330e86571b6a1597337a42f8ce7ebb245730ed5220374a540f119ad04f98b9"
-    sha256 cellar: :any, arm64_linux:   "017b9ffea2b25a55847b8dd59d901a44afa97f8a0ad28122b048e0a8d246e1d3"
-    sha256 cellar: :any, x86_64_linux:  "99524f8d4992c30281a5f83563d595f68acfd56709a97418aadeb9383a326d07"
+    sha256 cellar: :any, arm64_tahoe:   "74d2930636c902c8de66a12e14a48a8655df5b96ba27d65e51b97f24d9e64a88"
+    sha256 cellar: :any, arm64_sequoia: "9f9b3625c526a627673c1057eeb18394821239732fa7d6d898d5c61fbcd9230d"
+    sha256 cellar: :any, arm64_sonoma:  "2a7f72d08d149b32e4ca34be2adcec4867f385852a5c6024904031067232e264"
+    sha256 cellar: :any, sonoma:        "255eb53bb1b7d934114571b91dfd0eefa4332f5c8b46c4f0ec32c2045b482db0"
+    sha256 cellar: :any, arm64_linux:   "fb9c3fb4e19e23b577dcf44d49603076c4df123be8205ab147b3491e54eed26c"
+    sha256 cellar: :any, x86_64_linux:  "6670b2265df217dec6615c64d6ba5c223ef6fd0cbaa1bf51b5382c9077dd767d"
   end
 
   depends_on "rust" => :build # for `cbor2`
