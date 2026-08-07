9572664ffa8412f8c8817d42a0731aef1d1b46ac
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-07 15:01:39 +0000

    monitoring-plugins 3.0.3

--- Formula/m/monitoring-plugins.rb
+++ Formula/m/monitoring-plugins.rb
@@ -1,8 +1,8 @@
 class MonitoringPlugins < Formula
   desc "Plugins for nagios compatible monitoring systems"
   homepage "https://www.monitoring-plugins.org"
-  url "https://www.monitoring-plugins.org/download/monitoring-plugins-2.4.0.tar.gz"
-  sha256 "e5dfd4ad8fde0a40da50aab3aff6d9a27020b8f283e332bc4da6ef9914f4028c"
+  url "https://www.monitoring-plugins.org/download/monitoring-plugins-3.0.3.tar.gz"
+  sha256 "a1df32ef4791defd5418907b54be1549c81598fd02e339c4595d2d26107b3280"
   license "GPL-3.0-or-later"
 
   livecheck do
@@ -23,6 +23,7 @@ class MonitoringPlugins < Formula
     sha256               x86_64_linux:   "27b58371aa6e210a9510c9fd0b64ed2f0923425b336eeda2b5c9eb0fd08509ac"
   end
 
+  depends_on "net-snmp"
   depends_on "openssl@3"
 
   on_macos do
@@ -35,6 +36,22 @@ class MonitoringPlugins < Formula
 
   conflicts_with "nagios-plugins", because: "both install their plugins to the same folder"
 
+  # Fix check_snmp build at the site upstream missed when renaming `USE_OPENSSL`
+  patch do
+    url "https://github.com/monitoring-plugins/monitoring-plugins/commit/b80f5de71eeafd809203da2690aec24d99b381e3.patch?full_index=1"
+    sha256 "7450acc000dccf52bcf9be2cd267e0ffc2a89a575eed52bb1a64978339603dbf"
+    type :unofficial
+    resolves "https://github.com/monitoring-plugins/monitoring-plugins/pull/2319"
+  end
+
+  # Fix check_snmp build against net-snmp without the legacy `DEFAULT_SNMP_VERSION` alias
+  patch do
+    url "https://github.com/monitoring-plugins/monitoring-plugins/commit/717a74ecedad30973a83b3eca1823dbe284c6de5.patch?full_index=1"
+    sha256 "e965307e728ee0289be0bcf776f92d4ff241b5373adf773e2299673f72005b3e"
+    type :unofficial
+    resolves "https://github.com/monitoring-plugins/monitoring-plugins/pull/2319"
+  end
+
   def install
     # workaround for Xcode 14.3
     ENV.append "CFLAGS", "-Wno-implicit-function-declaration" if DevelopmentTools.clang_build_version >= 1403
@@ -42,6 +59,7 @@ class MonitoringPlugins < Formula
     args = %W[
       --libexecdir=#{libexec}/sbin
       --with-openssl=#{formula_opt_prefix("openssl@3")}
+      --with-netsnmpconfig-command=#{formula_opt_bin("net-snmp")}/net-snmp-config
     ]
 
     system "./configure", *args, *std_configure_args
