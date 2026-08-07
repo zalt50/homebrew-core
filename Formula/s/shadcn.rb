aa9be134f5cb66f862006599f1e43a265487d622
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-07 13:39:32 +0000

    shadcn: update 4.16.2 bottle.

--- Formula/s/shadcn.rb
+++ Formula/s/shadcn.rb
@@ -6,12 +6,12 @@ class Shadcn < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0ecf9b62c898740fca55c59819e8459d276c76dbe3945d651048c7b2ae323aa0"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0ecf9b62c898740fca55c59819e8459d276c76dbe3945d651048c7b2ae323aa0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0ecf9b62c898740fca55c59819e8459d276c76dbe3945d651048c7b2ae323aa0"
-    sha256 cellar: :any_skip_relocation, sonoma:        "1bd34542e724aacd029530c4a2850ad2cc9dc306549e8461da6980bb1f76a0cf"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "51f6b82dbe3527942c8b7e40888511faccc4473af044e20ac30ccfb245f531bd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "51f6b82dbe3527942c8b7e40888511faccc4473af044e20ac30ccfb245f531bd"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6cb21d50decc9ab07afd659a2e4a6332e211fc9d80354330645a09ee4ca61e12"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6cb21d50decc9ab07afd659a2e4a6332e211fc9d80354330645a09ee4ca61e12"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6cb21d50decc9ab07afd659a2e4a6332e211fc9d80354330645a09ee4ca61e12"
+    sha256 cellar: :any_skip_relocation, sonoma:        "1df591191b8d5f37129bb3b72a48771570bdff7cd5c9bdd71d66c1379c6481a4"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f3b69b635971e27569ecb7dd152bdb025079d419664d55e2bc522e7184724063"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f3b69b635971e27569ecb7dd152bdb025079d419664d55e2bc522e7184724063"
   end
 
   depends_on "node"
