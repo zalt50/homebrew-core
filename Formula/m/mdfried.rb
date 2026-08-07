e4ef6fd582b44292717e2c1923bd5717dda13d31
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-07 12:50:55 +0000

    mdfried: update 0.22.5 bottle.

--- Formula/m/mdfried.rb
+++ Formula/m/mdfried.rb
@@ -12,12 +12,12 @@ class Mdfried < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "6399f76a432e9ecad93170ffad5dd7603859f6734706c9fdc76be11fdeeaef20"
-    sha256 cellar: :any, arm64_sequoia: "99af53a18e42dd0d71d110f9e97501fb2ef288bbbe434da8895a299d13bb5738"
-    sha256 cellar: :any, arm64_sonoma:  "9cf3ed1af6f520f27195f7ea071189ae386800449e9d3887c0bb9a18abc8d549"
-    sha256 cellar: :any, sonoma:        "5b9757b44742cd000e36be5f03038cf57e9cb4fcb011d5757ddba0c9c751b6dc"
-    sha256 cellar: :any, arm64_linux:   "61b1bd5db30766cdcbc0f7e6ca435651a2d070e64a91319770950af478ccb8cc"
-    sha256 cellar: :any, x86_64_linux:  "60019137d62b1d005939cc6d2185f3bfefa7a7f73be3d3a4d09e69294ceb3934"
+    sha256 cellar: :any, arm64_tahoe:   "c2db802211a4d6f9a76bb599951aaadbae197003f626e5f1e9036049ef2676f1"
+    sha256 cellar: :any, arm64_sequoia: "9c4e5ddc51ac46a8b8914f1cc9a780808723c03fc3107e40362c85b11898da37"
+    sha256 cellar: :any, arm64_sonoma:  "b212375ad181005a6e3f4339332120cfd30ee368ab70dc667d5dbb82933683d1"
+    sha256 cellar: :any, sonoma:        "fbdfe32caf1b703b5e3e3dde8cc6e88f9752c2aae4d55c1ac1fad1270dec56cf"
+    sha256 cellar: :any, arm64_linux:   "ec8147d82bc0077ae71d6053e9613d6da763b875335ed864c9a132db5dce8253"
+    sha256 cellar: :any, x86_64_linux:  "798de72d8f2233249b4d9277359e2becf9f49132020da1bcb33b3b82051de5b4"
   end
 
   depends_on "pkgconf" => :build
