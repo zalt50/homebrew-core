c1847425fb66d2c575e357012e3cbf2c842b97dc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-07 06:26:49 +0000

    osv-scanner 2.5.0

--- Formula/o/osv-scanner.rb
+++ Formula/o/osv-scanner.rb
@@ -1,8 +1,8 @@
 class OsvScanner < Formula
   desc "Vulnerability scanner which uses the OSV database"
   homepage "https://google.github.io/osv-scanner/"
-  url "https://github.com/google/osv-scanner/archive/refs/tags/v2.4.0.tar.gz"
-  sha256 "407ceeb62b2a1f3fb8260bf527f2d61d1079f440e23c8c94a9796791d7edd565"
+  url "https://github.com/google/osv-scanner/archive/refs/tags/v2.5.0.tar.gz"
+  sha256 "466d25bf7ab38a9f12d3d15ab0b94432c45849ac91580caa8475bb2ae41faa93"
   license "Apache-2.0"
   head "https://github.com/google/osv-scanner.git", branch: "main"
 
