f02677f3ca075bfefd8431da255703591ebf3818
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-08 17:49:35 +0000

    style-dictionary 5.5.1

--- Formula/s/style-dictionary.rb
+++ Formula/s/style-dictionary.rb
@@ -1,8 +1,8 @@
 class StyleDictionary < Formula
   desc "Build system for creating cross-platform styles"
   homepage "https://styledictionary.com"
-  url "https://registry.npmjs.org/style-dictionary/-/style-dictionary-5.5.0.tgz"
-  sha256 "cd2c00277fb8690ec986b76814ee8df15069e298af1007a7f0dd822d0be75134"
+  url "https://registry.npmjs.org/style-dictionary/-/style-dictionary-5.5.1.tgz"
+  sha256 "90a46f042a1b24301037b31667d0652841bda3bc29c31994339f1e59270169c9"
   license "Apache-2.0"
 
   bottle do
