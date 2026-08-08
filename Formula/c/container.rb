19765d9f53f0facffe35836bcd5032db36769544
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-08 02:39:39 +0000

    container 1.2.2

--- Formula/c/container.rb
+++ Formula/c/container.rb
@@ -1,8 +1,8 @@
 class Container < Formula
   desc "Create and run Linux containers using lightweight virtual machines"
   homepage "https://apple.github.io/container/documentation/"
-  url "https://github.com/apple/container/archive/refs/tags/1.2.1.tar.gz"
-  sha256 "0cbc4820cacff200a733fc7e994cf6ccf9d605691e30713e6bd3600d3b6cf07c"
+  url "https://github.com/apple/container/archive/refs/tags/1.2.2.tar.gz"
+  sha256 "61841d675a6542179aaa1b48ec00dc2ff8b888a58d9ec0d87fa279b30b9b5ced"
   license "Apache-2.0"
   head "https://github.com/apple/container.git", branch: "main"
 
