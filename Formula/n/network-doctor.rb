b17132247a3da24d3f4a4aada53b800a7fde003e
Adrian Ho <the.gromgit@gmail.com>
2026-08-08 12:58:24 +0800

    network-doctor 1.10.7 (new formula)

---
+++ Formula/n/network-doctor.rb
@@ -0,0 +1,19 @@
+class NetworkDoctor < Formula
+  desc "Network troubleshooting TUI"
+  homepage "https://github.com/heymaikol/network-doctor/"
+  url "https://github.com/heymaikol/network-doctor/archive/refs/tags/v1.10.7.tar.gz"
+  sha256 "4488e38a6cc5874c689f6f06d0de74b4a91a192b025fd96e3bfcb82db27fee6b"
+  license "GPL-3.0-only"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-X main.version=#{version}", output: bin/"netdoc")
+  end
+
+  test do
+    output = JSON.parse shell_output("#{bin}/netdoc -json")
+    assert_equal version.to_s, output["version"]
+    assert_equal true, output["checks"].any? { |hash| hash["id"] == "iface" && hash["status"] == "PASS" }
+  end
+end
