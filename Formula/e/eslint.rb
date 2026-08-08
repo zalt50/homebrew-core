cf0b0369559ed253c25d43c790f80cf14f5f88be
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-08 23:38:33 +0000

    eslint: update 10.8.1 bottle.

--- Formula/e/eslint.rb
+++ Formula/e/eslint.rb
@@ -7,7 +7,7 @@ class Eslint < Formula
   head "https://github.com/eslint/eslint.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "eec20feeb379e0c31e527dc20ac0db522805858ba02a95fa8717128b186f258b"
+    sha256 cellar: :any_skip_relocation, all: "f4818942c2c59086b517722a8ed8b9c61ad63ecf641b17f33670149b994c092e"
   end
 
   depends_on "node"
