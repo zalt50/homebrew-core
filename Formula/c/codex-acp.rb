d6178157e5efa7a48bc7a5c5a26f5a43c9d94d39
Dawid Dziurla <dawidd0811@gmail.com>
2026-08-09 10:24:04 +0200

    codex-acp 1.1.14

--- Formula/c/codex-acp.rb
+++ Formula/c/codex-acp.rb
@@ -1,10 +1,10 @@
 class CodexAcp < Formula
-  desc "Use Codex from ACP-compatible clients such as Zed!"
-  homepage "https://github.com/zed-industries/codex-acp"
-  url "https://github.com/zed-industries/codex-acp/archive/refs/tags/v0.16.0.tar.gz"
-  sha256 "08765b8cfbc9218fc7192348065da22820657dba040f31119de13ba6a20be7e2"
+  desc "ACP server that exposes Codex CLI functionality for ACP-compatible clients"
+  homepage "https://github.com/agentclientprotocol/codex-acp"
+  url "https://registry.npmjs.org/@agentclientprotocol/codex-acp/-/codex-acp-1.1.14.tgz"
+  sha256 "674e4e939ee373e42bf7b1eece51c42cb7a8dd5b564523eea1b1fa8bbdfbce03"
   license "Apache-2.0"
-  head "https://github.com/zed-industries/codex-acp.git", branch: "main"
+  head "https://github.com/agentclientprotocol/codex-acp.git", branch: "main"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_tahoe:   "df2b45deb6d78341b196a8ee4085d85c33f47e18adde859ba17d142584ff8b18"
@@ -15,44 +15,26 @@ class CodexAcp < Formula
     sha256 cellar: :any,                 x86_64_linux:  "ea5f8f4915a88f34b11f2c286eaa6267d0a23d0e1354dbb42450a4ee110b9442"
   end
 
-  depends_on "pkgconf" => :build
-  depends_on "rust" => :build
-
-  uses_from_macos "bzip2"
-
-  on_linux do
-    depends_on "libcap" => :build
-    depends_on "openssl@4"
-    depends_on "zlib-ng-compat"
-  end
+  depends_on "node"
 
   def install
-    ENV["OPENSSL_DIR"] = formula_opt_prefix("openssl@4") if OS.linux?
-    system "cargo", "install", *std_cargo_args
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+    rm libexec.glob("lib/node_modules/**/codex-resources/zsh/bin/zsh") if OS.linux?
   end
 
   test do
-    require "open3"
-    require "timeout"
-
     json = <<~JSON
       {"jsonrpc":"2.0","id":1,"method":"initialize","params":{"protocolVersion":1}}
     JSON
 
-    Open3.popen3(bin/"codex-acp") do |stdin, stdout, _stderr, wait_thr|
-      stdin.write(json)
-      stdin.close
-
-      line = Timeout.timeout(15) { stdout.gets }
-      assert_match "\"protocolVersion\":1", line
-    ensure
-      if wait_thr&.alive?
-        begin
-          Process.kill("TERM", wait_thr.pid)
-        rescue Errno::ESRCH
-          # Process already exited between alive? check and kill.
-        end
-      end
+    Open3.popen3(bin/"codex-acp") do |stdin, stdout, _e, w|
+      stdin.write json
+      sleep 3
+      output = stdout.readline
+      assert_match("\"protocolVersion\":1", output)
+      assert_match("\"agentInfo\":{\"name\":\"@agentclientprotocol/codex-acp\"", output)
+      Process.kill("KILL", w.pid)
     end
   end
 end
