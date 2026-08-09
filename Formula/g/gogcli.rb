415d9fd7a36ecb19a282f7e99c89b2f6cc06c7a3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-09 08:22:37 +0000

    gogcli 0.35.0

--- Formula/g/gogcli.rb
+++ Formula/g/gogcli.rb
@@ -1,8 +1,8 @@
 class Gogcli < Formula
   desc "Google Suite CLI"
   homepage "https://gogcli.sh"
-  url "https://github.com/openclaw/gogcli/archive/refs/tags/v0.34.2.tar.gz"
-  sha256 "ce00cedfaec8e83592880d2127c9aff1344ce61b5299c12e5e6ce75dbe879e89"
+  url "https://github.com/openclaw/gogcli/archive/refs/tags/v0.35.0.tar.gz"
+  sha256 "d43e9333b93dfbc6673ff6d1349f49b2b947877403ac4d9dd6a1bcaaa2a6a780"
   license "MIT"
   head "https://github.com/openclaw/gogcli.git", branch: "main"
 
