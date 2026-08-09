c727aec93cab1acb0c8cadccb3f3079fed195203
童武 <tongwu.zl@alibaba-inc.com>
2026-08-09 18:03:25 +0800

    open-code-review: add shell completions
    Generate shell completions from the existing `ocr completion` subcommand so they are available after `brew install`.

--- Formula/o/open-code-review.rb
+++ Formula/o/open-code-review.rb
@@ -20,6 +20,7 @@ class OpenCodeReview < Formula
   def install
     ldflags = "-X main.Version=#{version}"
     system "go", "build", *std_go_args(ldflags:, output: bin/"ocr"), "./cmd/opencodereview"
+    generate_completions_from_executable(bin/"ocr", shell_parameter_format: :cobra)
   end
 
   test do
