92f8ed0a8b9cdb940075abfe97391040c3f0ea22
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-09 17:33:43 +0000

    openkermit: add 11.0.505 bottle.

--- Formula/o/openkermit.rb
+++ Formula/o/openkermit.rb
@@ -10,6 +10,15 @@ class Openkermit < Formula
     strategy :github_latest
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9bb49244a3197278ad616a1db7bc66d6265b83df118069eac598db528e8cf0ac"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0dfeaf19da1b723218fcb3755388148defd26f5d75de504eab8daae70a37d1f4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e23842bab8d10800310dd946ecf9647cc02d13ec40527eaea46241e08731b8fa"
+    sha256 cellar: :any_skip_relocation, sonoma:        "b22b75855c0f19b7d532f7e010cf355cbea2e99bc58b2139d30416c7e785d7ad"
+    sha256 cellar: :any,                 arm64_linux:   "b41d0c8ec805ecdb8e4a884157c3b2a40c7f8b1444d0a75e9c6d8359458757ae"
+    sha256 cellar: :any,                 x86_64_linux:  "1263fff09db1be9d264a60e4cbcbd329896362f08c274016964b291bcc0d78bb"
+  end
+
   uses_from_macos "libxcrypt"
   uses_from_macos "ncurses"
 
