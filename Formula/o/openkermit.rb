c880f4333b810200bfb54aeb0438f3ce67fc7215
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-09 21:00:48 +0000

    openkermit 11.0.507

--- Formula/o/openkermit.rb
+++ Formula/o/openkermit.rb
@@ -1,8 +1,8 @@
 class Openkermit < Formula
   desc "Scriptable network and serial communication for UNIX and VMS"
   homepage "https://www.openkermit.org/"
-  url "https://github.com/openkermit/ckermit/archive/refs/tags/v11.0.505.tar.gz"
-  sha256 "c4cbbae6fd83e3aad318afdae72a993af9c5e73221e18d18f9b7a6937f3735c2"
+  url "https://github.com/openkermit/ckermit/archive/refs/tags/v11.0.507.tar.gz"
+  sha256 "45070b3fb0f9eda87e8a3b9126b110aed8fe4f561bf803e4fd856dbc367d9b0a"
   license "BSD-3-Clause"
 
   livecheck do
@@ -35,7 +35,9 @@ class Openkermit < Formula
   end
 
   test do
-    system "#{bin}/kermit", "-C", "set host /network-type:pseudoterminal \"kermit -x\", get /bin/sh, bye, quit"
+    # /confirm:off keeps this headless.
+    system "#{bin}/kermit", "-C",
+           "set host /network-type:pseudoterminal \"kermit -x\", get /confirm:off /bin/sh, bye, quit"
     assert_path_exists "sh"
   end
 end
