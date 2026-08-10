2f73b3f559c705c53f58771b20403744b8bb20d5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-10 10:12:41 +0000

    swift-section: update 0.15.0 bottle.

--- Formula/s/swift-section.rb
+++ Formula/s/swift-section.rb
@@ -12,8 +12,8 @@ class SwiftSection < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "673afa8880e3916622a40fcb6207a36a9712d38d6cba9ac02de481d1ee31e01c"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "285488b1186d33ed7689f36fdffcb304727c7996cc66f2e3b353ca9666b262b8"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "86599914f0703f4935208886dcfceba16bc6e7f594242abd7d29122120f10fde"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c473067289ca368f98ede27726af98a22a8f43728d27d4af5f2d57a03b08acee"
   end
 
   # The Package.swift file requires Swift 5.10 or later.
