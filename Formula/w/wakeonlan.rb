10885cd2f9071ab31fd18c42417b296c6f39c240
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-10 00:21:12 +0000

    wakeonlan 0.50

--- Formula/w/wakeonlan.rb
+++ Formula/w/wakeonlan.rb
@@ -1,8 +1,8 @@
 class Wakeonlan < Formula
   desc "Sends magic packets to wake up network-devices"
   homepage "https://github.com/jpoliv/wakeonlan"
-  url "https://github.com/jpoliv/wakeonlan/archive/refs/tags/v0.42.tar.gz"
-  sha256 "4f533f109f7f4294f6452b73227e2ce4d2aa81091cf6ae1f4fa2f87bad04a031"
+  url "https://github.com/jpoliv/wakeonlan/archive/refs/tags/v0.50.tar.gz"
+  sha256 "cbbf9d75db0cc0b8deb9d43ae0b0a320864bc6f00e032771f11a926b0aa2463f"
   license "Artistic-1.0-Perl"
 
   bottle do
