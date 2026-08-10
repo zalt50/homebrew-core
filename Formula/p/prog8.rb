a85ab2f0548ce03b06e40de20d4561b0100d11cb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-10 21:06:01 +0000

    prog8 12.3.1

--- Formula/p/prog8.rb
+++ Formula/p/prog8.rb
@@ -1,8 +1,8 @@
 class Prog8 < Formula
   desc "Compiled programming language targeting the 8-bit 6502 CPU family"
   homepage "https://prog8.readthedocs.io"
-  url "https://github.com/irmen/prog8/archive/refs/tags/v12.3.tar.gz"
-  sha256 "40d86ee9c4d77d3b36151f979221d7ed06a106f16fa2602e5bdae8d815844e34"
+  url "https://github.com/irmen/prog8/archive/refs/tags/v12.3.1.tar.gz"
+  sha256 "f73de9784579da94733f218e6ced5377d7450b409dce61e38194c21be4973523"
   license "GPL-3.0-only"
 
   livecheck do
