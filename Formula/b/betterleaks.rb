e2e8bcdb6e8196c131bf7c0d7b9d5e16f9744d97
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-10 17:24:56 +0000

    betterleaks: update 1.7.4 bottle.

--- Formula/b/betterleaks.rb
+++ Formula/b/betterleaks.rb
@@ -7,12 +7,12 @@ class Betterleaks < Formula
   head "https://github.com/betterleaks/betterleaks.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1fac761950752d436a9c55970a9cb55f3b4b6d36f54eda3222df0af4e16889c9"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1fac761950752d436a9c55970a9cb55f3b4b6d36f54eda3222df0af4e16889c9"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1fac761950752d436a9c55970a9cb55f3b4b6d36f54eda3222df0af4e16889c9"
-    sha256 cellar: :any_skip_relocation, sonoma:        "ae8b669138955cb78905a57d6e0c66f79d5a44d52d125bbe0513d76464567659"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "fca229b4dba591e59264f9a80b6acd185ef27eefbef01656f36dc1aa0d934fff"
-    sha256 cellar: :any,                 x86_64_linux:  "f1974e63d221e02490e40bcf200e8166a152114b85e8f5cac608c44958743637"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8422b101f66a31f7cc869e8a3186669620ba5736b9e63380a4136c6fc5e2b832"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8422b101f66a31f7cc869e8a3186669620ba5736b9e63380a4136c6fc5e2b832"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8422b101f66a31f7cc869e8a3186669620ba5736b9e63380a4136c6fc5e2b832"
+    sha256 cellar: :any_skip_relocation, sonoma:        "dd4de6b6bd8cc0138b5fb0945e99e9c9a285682af02c7d9f176ddbd2588dbbfc"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c7a1beef4906d711f1050e1b3f0539088ca296a97be73d09dab57caa0ea55613"
+    sha256 cellar: :any,                 x86_64_linux:  "4486bfd0838558810efc5c8d863f2a63a362c498006b985c41a6d87a707d554b"
   end
 
   depends_on "go" => :build
