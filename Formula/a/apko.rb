d1eb5991858736165f6792d22e850970427ceee9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-10 02:34:11 +0000

    apko 1.2.36

--- Formula/a/apko.rb
+++ Formula/a/apko.rb
@@ -1,8 +1,8 @@
 class Apko < Formula
   desc "Build OCI images from APK packages directly without Dockerfile"
   homepage "https://github.com/chainguard-dev/apko"
-  url "https://github.com/chainguard-dev/apko/archive/refs/tags/v1.2.35.tar.gz"
-  sha256 "5dda8fc9b54e34edf6822876960c03971b3b8fd2c2f1b391ddc350b5066b8556"
+  url "https://github.com/chainguard-dev/apko/archive/refs/tags/v1.2.36.tar.gz"
+  sha256 "3c811a8a8086d6bd86be2f2dd770dcb27cc974b448698a23a54747262db8de89"
   license "Apache-2.0"
   head "https://github.com/chainguard-dev/apko.git", branch: "main"
 
