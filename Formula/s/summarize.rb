67fee522bacbd8744b431223a37384015fb6290e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-10 06:56:44 +0000

    summarize: update 0.21.9 bottle.

--- Formula/s/summarize.rb
+++ Formula/s/summarize.rb
@@ -7,7 +7,7 @@ class Summarize < Formula
   head "https://github.com/steipete/summarize.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "a7859f7f152dc783e8576054531f923e588760838d3a255a87f5a2c7c78d8c1c"
+    sha256 cellar: :any_skip_relocation, all: "3d17eae02daf9ec1313513d03b2880d8b027ceef48c079317d20b962bb1a8991"
   end
 
   depends_on "ffmpeg"
