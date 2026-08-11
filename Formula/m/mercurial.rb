38d1940f90a9252fc41499a7b2e53a93659c925e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-11 14:55:29 +0000

    mercurial 7.2.4

--- Formula/m/mercurial.rb
+++ Formula/m/mercurial.rb
@@ -3,8 +3,8 @@
 class Mercurial < Formula
   desc "Scalable distributed version control system"
   homepage "https://mercurial-scm.org/"
-  url "https://www.mercurial-scm.org/release/mercurial-7.2.3.tar.gz"
-  sha256 "1cb74ca95da021666b5a94a9bdc62f16255a6452f5a84cc2ae71bda8c7dbe36b"
+  url "https://www.mercurial-scm.org/release/mercurial-7.2.4.tar.gz"
+  sha256 "85839e0f39e6cb893a88932aa36ef661759f3c5c5de4551ad26bd9df53cb71a2"
   license "GPL-2.0-or-later"
   compatibility_version 1
 
