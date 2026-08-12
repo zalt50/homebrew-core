3d5517510d7920cd22e7ad48103a5c64ffcc2849
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-12 22:13:52 +0000

    sf: update 2.147.7 bottle.

--- Formula/s/sf.rb
+++ Formula/s/sf.rb
@@ -13,7 +13,7 @@ class Sf < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "b5d6b62eee251ba24f9985c8ebc2b4c5e31b5246f0af011abcefb2823e978f3f"
+    sha256 cellar: :any_skip_relocation, all: "f2479d93e9c811c4cb9603edb8e4eda1fe02bed55e748997c1531c05417e175a"
   end
 
   depends_on "node"
