3fe2e362139c4a30676279b9fce5d6f390b3e819
Michael Cho <michael@michaelcho.dev>
2026-08-12 13:35:16 -0400

    pagmo: revision bump (boost 1.92.0)

--- Formula/p/pagmo.rb
+++ Formula/p/pagmo.rb
@@ -4,7 +4,7 @@ class Pagmo < Formula
   url "https://github.com/esa/pagmo2/archive/refs/tags/v2.19.1.tar.gz"
   sha256 "ecc180e669fa6bbece959429ac7d92439e89e1fd1c523aa72b11b6c82e414a1d"
   license any_of: ["LGPL-3.0-or-later", "GPL-3.0-or-later"]
-  revision 7
+  revision 8
 
   bottle do
     sha256 cellar: :any,                 arm64_tahoe:   "d5474b826c3a4d2e4e0a557f8cae80452cf73a24e9ba1b6f375c1d9e78f18f77"
