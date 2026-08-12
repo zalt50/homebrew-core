07c3f61917fdb092b7bb2da394cc78247fa7f464
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-12 19:00:07 +0000

    weave: update 0.5.0 bottle.

--- Formula/w/weave.rb
+++ Formula/w/weave.rb
@@ -7,12 +7,12 @@ class Weave < Formula
   head "https://github.com/Ataraxy-Labs/weave.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "eb079cf08f69a75c497116a47d3819a5f281321484a9c319e440d72871a4b691"
-    sha256 cellar: :any, arm64_sequoia: "abf4ba376ddea9c2ffabd2d7d85a76a9d6a28ed67e2ca47377c9bc9c211322a2"
-    sha256 cellar: :any, arm64_sonoma:  "939a7a987580f91d21c2f5f8e35cba4a3a3773e6a408a407efd3b692f97ed930"
-    sha256 cellar: :any, sonoma:        "4b9143760b2dbd574d05fc61ce794f052e799677a522a470fd96cdb99ecafb24"
-    sha256 cellar: :any, arm64_linux:   "885faf28efaef1b93f8eb623b863553b7ddae4fda1b47326dcf78614e3967a81"
-    sha256 cellar: :any, x86_64_linux:  "d2da3f1e3a12401c147af121d879eddfce1064d7b8f1fc49d05000b98b75e1c2"
+    sha256 cellar: :any, arm64_tahoe:   "3a72b4c30cb252d8a6c0a4b21d938fa600e5ef11794a9eccc850d3a035ab359b"
+    sha256 cellar: :any, arm64_sequoia: "bd5453ddafcb455a18944a70e2abd02ba0ece97a90ea287bfa2d09f9c4fee66b"
+    sha256 cellar: :any, arm64_sonoma:  "1a59a2f47a47829576cc3c8b8e266b140777dc4b8a121d79837bed13d999975e"
+    sha256 cellar: :any, sonoma:        "b07bd47a6d3ef5aa0828f9f933917f72062e9ca1aaa84614893df5aab72ed776"
+    sha256 cellar: :any, arm64_linux:   "50e5073d1daebaeb751cf53d2b7ae24008ba067602321304373767c49bce9b98"
+    sha256 cellar: :any, x86_64_linux:  "66a0ba14f9eb300f0b6e69b0e3147e4038109991bbba69203dd3bc533fcdb2f2"
   end
 
   depends_on "pkgconf" => :build
