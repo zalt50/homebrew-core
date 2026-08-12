c2b3961c7b83c18b5a63cbb3e8fdb1b6ba038fa0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-12 03:45:51 +0000

    pup: update 1.10.7 bottle.

--- Formula/p/pup.rb
+++ Formula/p/pup.rb
@@ -7,12 +7,12 @@ class Pup < Formula
   head "https://github.com/DataDog/pup.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "254e44deb2277f6580f61b670e7ec411389682ac1785844801b344c2d98b1daa"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "208bece35434eaa2a4ae76ecb1d8df32f0f02d7eaf95b710c09201e86dcbb5cc"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9db2b92df403b74c63152a77d1079167d159ac4c9f0ced86e8ac95ae606f274d"
-    sha256 cellar: :any_skip_relocation, sonoma:        "6c0c00d1fe482331325e3d36ae038c199d5a72c8892fd3c78d539733f469ccc7"
-    sha256 cellar: :any,                 arm64_linux:   "88750d14b184514b301d9046689b0a86cb6aa6994d36d5e6edb516721e3433ce"
-    sha256 cellar: :any,                 x86_64_linux:  "1d298ca45bd0b892c5d4135ff3c1d111f49955caa692c16e74669704f8ecbce2"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "be12988fa6b9d4464ff6c2a217aee5f4e986bbc099e1b99cdde5740ee2db0364"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f89abc55538aebfd8a85d844b10fc10ab4c1d8452645c5c31384748c7ec2ddb6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cb5a8d333ff6418dbfe2da2031bcb8f86f3ca9057e885905471af20718fbc917"
+    sha256 cellar: :any_skip_relocation, sonoma:        "4de1c8c12fb986540960fe9dbb71454c1ea497c76aba749f4e29070343723eb0"
+    sha256 cellar: :any,                 arm64_linux:   "b2681da52e6058cd94c57ecbc18b00ccfbbcd0ade7e3ac354459e499efce7db8"
+    sha256 cellar: :any,                 x86_64_linux:  "b7e1e4a91f7b9b5f058fe6af929a2c2ab1794988a6c895cdb0130a2831741db6"
   end
 
   depends_on "pkgconf" => :build
