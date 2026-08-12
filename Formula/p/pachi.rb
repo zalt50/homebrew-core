2a3cd52b1e004addd84db315430412df8d9308b9
Michael Cho <michael@michaelcho.dev>
2026-08-12 13:35:16 -0400

    pachi: revision bump (boost 1.92.0)

--- Formula/p/pachi.rb
+++ Formula/p/pachi.rb
@@ -7,7 +7,7 @@ class Pachi < Formula
     "GPL-2.0-only",
     "BSD-2-Clause", # `caffe`
   ]
-  revision 2
+  revision 3
   head "https://github.com/pasky/pachi.git", branch: "master"
 
   bottle do
