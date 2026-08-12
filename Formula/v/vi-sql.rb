c023a0b2c264c467e131891182067c3184b49c58
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-12 13:58:20 +0000

    vi-sql: add 0.2.0 bottle.

--- Formula/v/vi-sql.rb
+++ Formula/v/vi-sql.rb
@@ -5,6 +5,15 @@ class ViSql < Formula
   sha256 "99321f8db75ab0f6932e59b5a5ffd871a99b654649ea56f8246986cf31e9c62e"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e80279f457a615e09512754fe1ababc6ffbd09a28c4cc0aecb49e945938b85c7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e80279f457a615e09512754fe1ababc6ffbd09a28c4cc0aecb49e945938b85c7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e80279f457a615e09512754fe1ababc6ffbd09a28c4cc0aecb49e945938b85c7"
+    sha256 cellar: :any_skip_relocation, sonoma:        "07a4e2fa244382f040bdc59e9971ea0ab382bbca1fc6bc2ec9dc72400afb0513"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "116f6f54d83a48bf00ff90449d55a9d3022516c17d985553ddff194ee88716b9"
+    sha256 cellar: :any,                 x86_64_linux:  "9970f7fdd18ef426edc387b2e51df4123104498a913ae495e83b58b73969752d"
+  end
+
   depends_on "go" => :build
 
   uses_from_macos "sqlite" => :test
