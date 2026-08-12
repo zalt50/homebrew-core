79fb5cd318bf5d752916a868c6774d0bbb09e646
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-12 03:18:54 +0000

    cvs-fast-export: update 2.4 bottle.

--- Formula/c/cvs-fast-export.rb
+++ Formula/c/cvs-fast-export.rb
@@ -16,12 +16,12 @@ class CvsFastExport < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "38155b919b59eca4b641ea04db7d0a91f99f77f1b7e1b916351dc553f1aaf71a"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "38155b919b59eca4b641ea04db7d0a91f99f77f1b7e1b916351dc553f1aaf71a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "38155b919b59eca4b641ea04db7d0a91f99f77f1b7e1b916351dc553f1aaf71a"
-    sha256 cellar: :any_skip_relocation, sonoma:        "919151cd27299bc8c9bc6863cc6afb75fb2c6199b13cc4e3e4ccb5aaafb835a3"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d15a192efd4f10816e1ead37775cbaa351ee109c040d77afb9a788cb8c345ac2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8fe7b73fa4870f0da2a002fb5c4fdc9da1edc7a48b65adb0c52fc6351cf656ab"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "51f93ab4538297329f06cd29df096e82f99da9e0ebf1e015c39a4f43a8e6283d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "51f93ab4538297329f06cd29df096e82f99da9e0ebf1e015c39a4f43a8e6283d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "51f93ab4538297329f06cd29df096e82f99da9e0ebf1e015c39a4f43a8e6283d"
+    sha256 cellar: :any_skip_relocation, sonoma:        "463565950ba70ded0f236c4566b700cdd2e84d87944b6b92ac500c9cf8f462e5"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "38b64a99f9bfd2196f7a611fcaba4f7ac2d7f329b6ca284c1c97ff3ff5616763"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fe9331d9d4216cc28b7b3c9d36f9c8fdd5fe8045971680b53851f9c5d5a54f86"
   end
 
   depends_on "asciidoctor" => :build
