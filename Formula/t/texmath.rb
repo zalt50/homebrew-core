9d03ffa5395c582b79aef37e18bc58b3cf7ce995
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-12 03:19:06 +0000

    texmath: update 0.13.2.1 bottle.

--- Formula/t/texmath.rb
+++ Formula/t/texmath.rb
@@ -7,12 +7,12 @@ class Texmath < Formula
   head "https://github.com/jgm/texmath.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "8738f8f2723298443452b033ea5f09be8f227ed573ab068fb8efccda15d3ea62"
-    sha256 cellar: :any, arm64_sequoia: "55076a06aa19174a76ff4bd18ed22ba259ea500847e5ba1f76878b49c5949d24"
-    sha256 cellar: :any, arm64_sonoma:  "f0da3b65ece8ebfecf5e92e8393ef8c7635c2ff8f2005875595536b673bddc2e"
-    sha256 cellar: :any, sonoma:        "96b910a84e879e39e44df06dbd6dfc4fe506865e342239ba843c2cd60fe1e31d"
-    sha256 cellar: :any, arm64_linux:   "0aa8b3ae54dcf2b35405ed7d66e9d6ca17d15b82625e8982926c175b9e65d738"
-    sha256 cellar: :any, x86_64_linux:  "f5e8084b99f63d9fbe4663e42679cbfe2427917a638ca6dad40c98c6d1caf886"
+    sha256 cellar: :any, arm64_tahoe:   "c6481101e4e7e2c2aed5799373882c8218a80b43e843c82dfa93b96ae5725ca3"
+    sha256 cellar: :any, arm64_sequoia: "434077aef261750db743e0d23d913bb09c26cb208186bd0271bf160a31220f5f"
+    sha256 cellar: :any, arm64_sonoma:  "53c7589f26ff7ebb6df422ad381398f8397508d6f582b5b890635e8e9e3d7068"
+    sha256 cellar: :any, sonoma:        "172067c8f5efe01fad2a12571cc2025db5e0a6e56a6cbd2c7dd7af9b9c75e6f0"
+    sha256 cellar: :any, arm64_linux:   "9664ecfba44a45e4fb6cbbd930df3bc047ffb1516c32200f55a390885d8adb56"
+    sha256 cellar: :any, x86_64_linux:  "2f1d4f321d044cba4e4d569e33bb976c3309ab7154797aadd7276929b5415de3"
   end
 
   depends_on "cabal-install" => :build
