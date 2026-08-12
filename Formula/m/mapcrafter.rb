05f8fd3d4429a1e5a4256e8c3da24b2f4bdd88a0
Michael Cho <michael@michaelcho.dev>
2026-08-12 13:35:14 -0400

    mapcrafter: revision bump (boost 1.92.0)

--- Formula/m/mapcrafter.rb
+++ Formula/m/mapcrafter.rb
@@ -4,7 +4,7 @@ class Mapcrafter < Formula
   url "https://github.com/mapcrafter/mapcrafter/archive/refs/tags/v.2.4.tar.gz"
   sha256 "f3b698d34c02c2da0c4d2b7f4e251bcba058d0d1e4479c0418eeba264d1c8dae"
   license "GPL-3.0-or-later"
-  revision 16
+  revision 17
 
   bottle do
     sha256 cellar: :any,                 arm64_tahoe:   "44a442066454eea4959b0e1f8f25e2c1623760b204daa6a7a8e544a371012148"
