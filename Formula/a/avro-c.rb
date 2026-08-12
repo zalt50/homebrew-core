12164a486f21784e9d43830aa1c71c2a7f8cdd2c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-12 20:09:27 +0000

    avro-c 1.12.2

--- Formula/a/avro-c.rb
+++ Formula/a/avro-c.rb
@@ -1,9 +1,9 @@
 class AvroC < Formula
   desc "Data serialization system"
   homepage "https://avro.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=avro/avro-1.12.1/c/avro-c-1.12.1.tar.gz"
-  mirror "https://archive.apache.org/dist/avro/avro-1.12.1/c/avro-c-1.12.1.tar.gz"
-  sha256 "b64e31b94719499549622aa92f1d96d1742967ced261a0931b63be3bbe907f2c"
+  url "https://www.apache.org/dyn/closer.lua?path=avro/avro-1.12.2/c/avro-c-1.12.2.tar.gz"
+  mirror "https://archive.apache.org/dist/avro/avro-1.12.2/c/avro-c-1.12.2.tar.gz"
+  sha256 "ccc85c5a967ca647fe0961ddeedf286cde00aacf87834d8ca552a0165ae4aa6d"
   license "Apache-2.0"
 
   bottle do
