52fbd53dbe837f7b39d2c11b45e7b503da2f54da
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-13 20:16:52 +0000

    garble: update 0.17.0_1 bottle.

--- Formula/g/garble.rb
+++ Formula/g/garble.rb
@@ -8,12 +8,12 @@ class Garble < Formula
   head "https://github.com/burrowers/garble.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e1a7e4c1c62f5856e61b9d14cbc65a0470f2a1db63abc9daf769ad9de993c9e4"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e1a7e4c1c62f5856e61b9d14cbc65a0470f2a1db63abc9daf769ad9de993c9e4"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e1a7e4c1c62f5856e61b9d14cbc65a0470f2a1db63abc9daf769ad9de993c9e4"
-    sha256 cellar: :any_skip_relocation, sonoma:        "ec94f7e1177c6fc10fe16e9081e19d1fb66a8a7acc3d212daf49ff3dc4ad0834"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "2cbf574da6fd4fcdafd18da8f53355264d69bda288495fc5d2bbae1e66ed97eb"
-    sha256 cellar: :any,                 x86_64_linux:  "4ecf63a71b1484005ebbf108776759963c348ae107ce8bdf933c09b4fa151f7c"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "79aa754be7df29d8c317bcc67be29a3fd03c4563ebec971429a8d7052e61635f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "79aa754be7df29d8c317bcc67be29a3fd03c4563ebec971429a8d7052e61635f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "79aa754be7df29d8c317bcc67be29a3fd03c4563ebec971429a8d7052e61635f"
+    sha256 cellar: :any_skip_relocation, sonoma:        "fc9b42154a3e9b913d58ffebf0665950a287e7fe2e144dcb12f895cc553879b6"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "403c8939b0cdb4ff980c366452ecb83f020c95264a51c64461adc14a1db91a26"
+    sha256 cellar: :any,                 x86_64_linux:  "4c540fa76667760aa0637acdf2ff9915ca6d51cf46bf9e0ec413a1401962a2a0"
   end
 
   depends_on "go" => [:build, :test]
