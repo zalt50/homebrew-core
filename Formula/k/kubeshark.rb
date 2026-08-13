155c85a04db78a50da1ae1ddf10b56067917b635
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-13 22:14:31 +0000

    kubeshark: update 53.4.0 bottle.

--- Formula/k/kubeshark.rb
+++ Formula/k/kubeshark.rb
@@ -15,12 +15,12 @@ class Kubeshark < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f02106e34c8fb8c9d2e09d7f04f0728b6b90fc87dcdf8d065c9d9e3f1d541119"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2d0bd9475f33975c12760d15d5cae5dc5d5be1aa98a63de09f147153ef85135e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7f04761f585f8c66e06f56f8d2b8f60462eaf65c05d2fda9ca023bb41f2f11d6"
-    sha256 cellar: :any_skip_relocation, sonoma:        "524c883d22388e6788b8e24b33e2c5d37e163c5d26bc4c4cc27935d19c8b8b10"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "81a88a8b827d612f3d5e95414af783abd396e09e75f4bd02a476ff1fe24a4023"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "660034a79fba1d1adf5abb1286a5798a24ed6a1371a4ed529c45e3705a29dbd9"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "568b47b6b22c5f03918ac03c726203807cf410b36880e5aecaa42afc0d8aff58"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b661e644aca12f69a5ede60eaf55f106a289ae5454604737a093987888891ffc"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bfdaf6152ff45d0ed935c3e96965920c2162bf8713ba255ba1c39aef00774cd3"
+    sha256 cellar: :any_skip_relocation, sonoma:        "eed570dd1d0f97c8ffea452ffc5c94190e348ea989b71b17ad23dfcd959e942f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f14ec9e9141b4fe59adce36d3ecdc107cd8860e0bb877512da72a189427e791d"
+    sha256 cellar: :any,                 x86_64_linux:  "a1a8f3dacd3a56f85ddc287c8f58ea97369e3e8c042a40f5304bf822abf8646b"
   end
 
   depends_on "go" => :build
