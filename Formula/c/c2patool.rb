136a32618afb951cd16b599838dec23c211a3e01
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-13 23:10:22 +0000

    c2patool 0.27.15

--- Formula/c/c2patool.rb
+++ Formula/c/c2patool.rb
@@ -1,8 +1,8 @@
 class C2patool < Formula
   desc "CLI for working with C2PA manifests and media assets"
   homepage "https://contentauthenticity.org"
-  url "https://github.com/contentauth/c2pa-rs/archive/refs/tags/c2patool-v0.27.14.tar.gz"
-  sha256 "e19c9a93503abaa3283994d01412fb2a82841e8404c709b413da611a82a144e6"
+  url "https://github.com/contentauth/c2pa-rs/archive/refs/tags/c2patool-v0.27.15.tar.gz"
+  sha256 "0a02eca6f34b46db6ce499705a68ab0da0feb172f5c6600bca2daa872c9c494b"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/contentauth/c2pa-rs.git", branch: "main"
 
