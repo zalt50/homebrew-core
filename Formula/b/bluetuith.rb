0c029747948279948e6d567dd4eb110733e62a81
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-13 07:28:49 +0000

    bluetuith: add 0.2.7 bottle.

--- Formula/b/bluetuith.rb
+++ Formula/b/bluetuith.rb
@@ -5,6 +5,11 @@ class Bluetuith < Formula
   sha256 "9586383c1703dd4e12e81f5f68e5144481aed8fb0526ee046dc3a80558d0f0dc"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_linux:  "15579f6700793d2fc4c27a173cd839c994790a7a6e592f6a7ea9ed1fa9c03f93"
+    sha256 cellar: :any,                 x86_64_linux: "53b21734919392d56bea9aa92cb1f546148e17176949445c56431564d49fc093"
+  end
+
   depends_on "go" => :build
   depends_on :linux
 
