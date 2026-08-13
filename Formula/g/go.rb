c86ca0458453283704dbefad2cb8423341cd9cc3
Štefan Baebler <319826+stefanb@users.noreply.github.com>
2026-08-13 19:16:24 +0200

    go 1.26.6

--- Formula/g/go.rb
+++ Formula/g/go.rb
@@ -1,11 +1,11 @@
 class Go < Formula
   desc "Open source programming language to build simple/reliable/efficient software"
   homepage "https://go.dev/"
-  url "https://go.dev/dl/go1.26.5.src.tar.gz"
-  mirror "https://fossies.org/linux/misc/go1.26.5.src.tar.gz"
-  sha256 "495be4bc87176ac567392e5b4116abd98466d33d7b49d41e764ccc6976b2dc42"
+  url "https://go.dev/dl/go1.26.6.src.tar.gz"
+  mirror "https://fossies.org/linux/misc/go1.26.6.src.tar.gz"
+  sha256 "a0721c54c688901448d77ad9b3ec7ea7c474730755ff891382e92ecb93ff2cb1"
   license "BSD-3-Clause"
-  compatibility_version 7
+  compatibility_version 8
   head "https://go.googlesource.com/go.git", branch: "master"
 
   livecheck do
