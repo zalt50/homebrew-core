fd41d28473c3e6673142fb9fe3a406852a296b08
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-13 08:22:40 +0000

    crun 1.29.1

--- Formula/c/crun.rb
+++ Formula/c/crun.rb
@@ -1,8 +1,8 @@
 class Crun < Formula
   desc "Fast and lightweight fully featured OCI runtime and C library"
   homepage "https://github.com/containers/crun"
-  url "https://github.com/containers/crun/releases/download/1.29/crun-1.29.tar.zst"
-  sha256 "d4846ce8407cd78ea322bc7daa8a75f7cbd2648a22eca4e388b84b87510b8352"
+  url "https://github.com/containers/crun/releases/download/1.29.1/crun-1.29.1.tar.zst"
+  sha256 "0fc095f629b6bba010b0cfa8b25ca1f769455e8891e06eb62c829ddc92ea2cad"
   license any_of: ["GPL-2.0-or-later", "LGPL-2.1-or-later"]
 
   bottle do
