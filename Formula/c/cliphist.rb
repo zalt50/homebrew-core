12389c1b51758fdf7bd29d7839f73dc54993d284
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-13 07:50:44 +0000

    cliphist: add 0.7.0 bottle.

--- Formula/c/cliphist.rb
+++ Formula/c/cliphist.rb
@@ -5,6 +5,11 @@ class Cliphist < Formula
   sha256 "8d7dc7b4495e5812b5613274c250ba5d3900933d78888ce7921c01247f191cc8"
   license "GPL-3.0-only"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_linux:  "0d341b62780cd1e692c0de243c81c94c99e42efbc86a6c1708f93d86b33e57a7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "b8468051b27d2b1b30ffa0f20208d14c70b21aa708709c6e6ea50385320e386e"
+  end
+
   depends_on "go" => :build
   depends_on :linux
 
