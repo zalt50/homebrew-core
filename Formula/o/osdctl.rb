c01eb7585a85431a2d40c85b3d0ea3b603192a71
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-14 17:42:37 +0000

    osdctl 0.63.0

--- Formula/o/osdctl.rb
+++ Formula/o/osdctl.rb
@@ -1,8 +1,8 @@
 class Osdctl < Formula
   desc "CLI tool for managed OpenShift clusters"
   homepage "https://github.com/openshift/osdctl"
-  url "https://github.com/openshift/osdctl/archive/refs/tags/v0.62.0.tar.gz"
-  sha256 "0e0543ef9760f827dedc91605c64e484c68e387a86e89cb579ebfa263d687ea2"
+  url "https://github.com/openshift/osdctl/archive/refs/tags/v0.63.0.tar.gz"
+  sha256 "1be4fb3e62e54c10977da04e308c29e7794c76f67f45186f0c9f4ea790efeb06"
   license "Apache-2.0"
   head "https://github.com/openshift/osdctl.git", branch: "master"
 
