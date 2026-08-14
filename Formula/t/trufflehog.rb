e62d025cd6ea2a835329377e7f7e1ee3e8ed512f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-14 22:50:47 +0000

    trufflehog: update 3.97.0 bottle.

--- Formula/t/trufflehog.rb
+++ Formula/t/trufflehog.rb
@@ -8,12 +8,12 @@ class Trufflehog < Formula
   head "https://github.com/trufflesecurity/trufflehog.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8366ccdc64485ac4015c5ccc3037467c5d10b9490c21f84d660a12fe93fd80a5"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cfa0e1af9a80bd2714b45d07d7c568f3f59d9f38c9ea734f8ebb01f54ec1221e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "69604e263535e5e5ff11dfb2bb5292f46019300674a3b45c71e56e3ad07b0158"
-    sha256 cellar: :any_skip_relocation, sonoma:        "826a8d594edce05765312b8ee945a8e1fdcd018fd915fb9f203b7866be7b81ac"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "5136148379628a548985ce8c9b6097adbd5debec24ef28125be117392593a79e"
-    sha256 cellar: :any,                 x86_64_linux:  "e3317c7258b4bc1e148b2a3deadc59277a57acf9abe664fa3bfae2529a523200"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d56767957f63392c5187e66637ff195296b3e2b9e7c4de4f3bbc886356e9a80c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b5b68b261fac32b8157b1e93a3549c333968bc9c2deab441b7619d384f669d51"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "75f8387f2ca69029da2c0caced7f929a57b1be0be4a0aef0616e5bb432ec2e92"
+    sha256 cellar: :any_skip_relocation, sonoma:        "c706c053bfc2114b639b99003c9c203e9def037f6908e8b9007d7aa5ec456321"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "32ad748b5f56244817f289180e54c2498f0b294417aae27daafc1aba342b96b2"
+    sha256 cellar: :any,                 x86_64_linux:  "7d1b186b0b8449a7b6ed234a7bb15d04b8e53013c1ed1c8926a9d1c3eaf1c8f6"
   end
 
   depends_on "go" => :build
