8c90d16565371b8698d8e1fac606a03333f5dce7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-14 23:04:06 +0000

    depot 2.102.1

--- Formula/d/depot.rb
+++ Formula/d/depot.rb
@@ -1,8 +1,8 @@
 class Depot < Formula
   desc "Build your Docker images in the cloud"
   homepage "https://depot.dev/"
-  url "https://github.com/depot/cli/archive/refs/tags/v2.102.0.tar.gz"
-  sha256 "1f59a283bc6577934b86d5a435690d46f236a5124a5af564a06fa2a907c58624"
+  url "https://github.com/depot/cli/archive/refs/tags/v2.102.1.tar.gz"
+  sha256 "05939796b2f76f26b66c5ad8ef3ccbbbd3a7490969bab339c818adc46ddb0193"
   license "MIT"
   head "https://github.com/depot/cli.git", branch: "main"
 
