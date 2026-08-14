e7ece6f27b19abad878154b41fc61e903ba6342b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-14 05:02:13 +0000

    frizbee: update 0.1.11 bottle.

--- Formula/f/frizbee.rb
+++ Formula/f/frizbee.rb
@@ -7,12 +7,12 @@ class Frizbee < Formula
   head "https://github.com/stacklok/frizbee.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "07c65e819a36178726e14a5c1dfd614c54c3471cf1a88e2d4e885c2022520cf6"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "07c65e819a36178726e14a5c1dfd614c54c3471cf1a88e2d4e885c2022520cf6"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "07c65e819a36178726e14a5c1dfd614c54c3471cf1a88e2d4e885c2022520cf6"
-    sha256 cellar: :any_skip_relocation, sonoma:        "7a768bb4744e281e1c04147c9e858d71a736f0ac25fb30f7d04b567accb36f6b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "faed5b12b9b9520de98d4e29626443aaa7dbe7fa94f04fe64918943cd0e2d988"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d4bbec3f991158720335c1566a0e43d9741d0f1ef1205c9aefca50aaffa46267"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "fe319cd42db4d53c25d774f399fd6ef42fbe367dffff73e4e2f32a7e4a09002a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fe319cd42db4d53c25d774f399fd6ef42fbe367dffff73e4e2f32a7e4a09002a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fe319cd42db4d53c25d774f399fd6ef42fbe367dffff73e4e2f32a7e4a09002a"
+    sha256 cellar: :any_skip_relocation, sonoma:        "cd1f4b851bfb272311ec20fd5d701674adc080dc2e3807651075b7e9b371a83a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "825d40ddf19f5fce0a95d2ee8a267e9f9a993153d7576f96cfa88b3e0cffa433"
+    sha256 cellar: :any,                 x86_64_linux:  "c7fe6b480b477f96fad69320c94a6a6eccec9a835504b3f1613f1ff9b19d6d53"
   end
 
   depends_on "go" => :build
