c8d14c82e0228eec3d8e96ccd63ee4c0434eb211
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-14 15:07:48 +0000

    solhint 6.2.4

--- Formula/s/solhint.rb
+++ Formula/s/solhint.rb
@@ -1,8 +1,8 @@
 class Solhint < Formula
   desc "Linter for Solidity code"
   homepage "https://protofire.github.io/solhint/"
-  url "https://registry.npmjs.org/solhint/-/solhint-6.2.3.tgz"
-  sha256 "76057380fb59f84cc8282c21da25fa718303bb6f5468cb75ac85d607fc4bfbc3"
+  url "https://registry.npmjs.org/solhint/-/solhint-6.2.4.tgz"
+  sha256 "c3060df2c802f344bfcdde6d5181aae393baa16f0fa5e29022a0168fc4d5baf6"
   license "MIT"
 
   bottle do
