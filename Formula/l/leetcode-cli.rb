616a91abd1b3020b8d9b702198b6d1147deb6804
Thierry Moisan <thierry.moisan@gmail.com>
2026-08-16 11:14:21 -0400

    leetcode-cli: migrate to `openssl@4`

--- Formula/l/leetcode-cli.rb
+++ Formula/l/leetcode-cli.rb
@@ -17,13 +17,13 @@ class LeetcodeCli < Formula
 
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
-  depends_on "openssl@3"
+  depends_on "openssl@4"
 
   uses_from_macos "sqlite"
 
   def install
     # Ensure that the `openssl` crate picks up the intended library.
-    ENV["OPENSSL_DIR"] = formula_opt_prefix("openssl@3")
+    ENV["OPENSSL_DIR"] = formula_opt_prefix("openssl@4")
 
     system "cargo", "install", *std_cargo_args
 
