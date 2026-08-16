7510b67bc8f5d14b4b4317edb7d7fa37e3996913
Thierry Moisan <thierry.moisan@gmail.com>
2026-08-16 16:11:13 -0400

    sprocket: migrate to `openssl@4`

--- Formula/s/sprocket.rb
+++ Formula/s/sprocket.rb
@@ -22,7 +22,7 @@ class Sprocket < Formula
 
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
-  depends_on "openssl@3"
+  depends_on "openssl@4"
 
   on_linux do
     depends_on "zlib-ng-compat"
