0bcfb8433a293e5e7c3ce804b8494c143779ab48
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-17 23:38:00 +0000

    dblab: update 0.48.1 bottle.

--- Formula/d/dblab.rb
+++ Formula/d/dblab.rb
@@ -7,12 +7,12 @@ class Dblab < Formula
   head "https://github.com/danvergara/dblab.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2232e75ad9321a2710e817ab9f42000a33ccbc24aa58a9e54795e3612effb4bb"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2232e75ad9321a2710e817ab9f42000a33ccbc24aa58a9e54795e3612effb4bb"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2232e75ad9321a2710e817ab9f42000a33ccbc24aa58a9e54795e3612effb4bb"
-    sha256 cellar: :any_skip_relocation, sonoma:        "33a1d7131de40b88fb774d15c959c3c1718214ca7b35f76b86924b7f1c772737"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "c45e09628860b75b3270223862afc43fa9c17cb7a1d76b8d18ee7c466b2af5be"
-    sha256 cellar: :any,                 x86_64_linux:  "89c08c68bd623b3cab9bf7d370d6b63d75822aa0bcce8cbbc9640a74fb794dae"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a130f8371a8daea38b74b256c4359831d1aba09436f2e8893f020e4b492ba31f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a130f8371a8daea38b74b256c4359831d1aba09436f2e8893f020e4b492ba31f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a130f8371a8daea38b74b256c4359831d1aba09436f2e8893f020e4b492ba31f"
+    sha256 cellar: :any_skip_relocation, sonoma:        "aa359e10264106f7aed99e67e9486c5c778dcea4555e797b9c81bd085751a31a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "94160069c4e2244dd46fa8ab22697e53c70680eb63a0c236840b11ef7dd42d9a"
+    sha256 cellar: :any,                 x86_64_linux:  "713691d15780c86db0b1b680890cf80ab1622342be02c44e8e7dfd3cd6b01ed9"
   end
 
   depends_on "go" => :build
