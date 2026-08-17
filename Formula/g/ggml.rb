ffbd99a17d3fac2122a2cb7a03da0569eb9441f5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-17 08:30:37 +0000

    ggml 0.20.1

--- Formula/g/ggml.rb
+++ Formula/g/ggml.rb
@@ -1,8 +1,8 @@
 class Ggml < Formula
   desc "Tensor library for machine learning"
   homepage "https://github.com/ggml-org/ggml"
-  url "https://github.com/ggml-org/ggml/archive/refs/tags/v0.20.0.tar.gz"
-  sha256 "85bdb8c38cf9e3074177713e34add52e57c1d310239de864e627d429dea3b51a"
+  url "https://github.com/ggml-org/ggml/archive/refs/tags/v0.20.1.tar.gz"
+  sha256 "8ca0b14551211156bade44999be2d31de2b240cbb7bad6d7a01bad3dbc8e1fff"
   license "MIT"
   compatibility_version 1
   head "https://github.com/ggml-org/ggml.git", branch: "master"
@@ -42,7 +42,7 @@ class Ggml < Formula
 
   # Lengthy test so not worth installing. Shorter examples/tests haven't been ported to new DL backend
   resource "test-backend-ops.cpp" do
-    url "https://raw.githubusercontent.com/ggml-org/ggml/refs/tags/v0.20.0/tests/test-backend-ops.cpp"
+    url "https://raw.githubusercontent.com/ggml-org/ggml/refs/tags/v0.20.1/tests/test-backend-ops.cpp"
     sha256 "e3575b45f3595d964e743e224551ed9cde3abd0cd51249b7efa72a7c8393f294"
 
     livecheck do
