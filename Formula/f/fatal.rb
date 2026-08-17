c87a616291a78180f1bb98d2824fd81305a623d5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-17 17:19:45 +0000

    fatal 2026.08.17.00

--- Formula/f/fatal.rb
+++ Formula/f/fatal.rb
@@ -1,8 +1,8 @@
 class Fatal < Formula
   desc "Facebook Template Library"
   homepage "https://www.facebook.com/groups/libfatal/"
-  url "https://github.com/facebook/fatal/archive/refs/tags/v2026.08.10.00.tar.gz"
-  sha256 "9187dc3360c0bd0cd61cb40507e5293580defbef4a62db52c5b95d6be16cff7c"
+  url "https://github.com/facebook/fatal/archive/refs/tags/v2026.08.17.00.tar.gz"
+  sha256 "01070f48a3f67d6a61231ca1717699b1ebc965b403c21758de4fe804f724a67e"
   license "BSD-3-Clause"
   head "https://github.com/facebook/fatal.git", branch: "main"
 
