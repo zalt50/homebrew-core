55da7978f0cdd2263df741521e5e16b326c1e0c8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-17 06:18:45 +0000

    oh-my-agent: update 12.3.1 bottle.

--- Formula/o/oh-my-agent.rb
+++ Formula/o/oh-my-agent.rb
@@ -6,12 +6,12 @@ class OhMyAgent < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "6d56499815247bd77fee48e1c42e2e114aab850bf8f74b382a50c8f5ff02c08e"
-    sha256 cellar: :any, arm64_sequoia: "676fd3cfaae2eb8ba7ae38a298750618fa6ec2ee5dcd9846820d38ffccd14890"
-    sha256 cellar: :any, arm64_sonoma:  "264f949aefef15b276007e884ce033b551add4bc36bd1695e7ab51e326b467a9"
-    sha256 cellar: :any, sonoma:        "ffd1952e9a9de5d4a2855444eb77d083e949f01d44247a9994ecc960aa10f0a0"
-    sha256 cellar: :any, arm64_linux:   "7d6548e50adf23b20622fa624e27a8fe2baf88ab8f9b128677198d108f63ee0d"
-    sha256 cellar: :any, x86_64_linux:  "562e47491d066fbea2a0715ee94c3a1ce6add6d11329c8027b2f4ed519759334"
+    sha256 cellar: :any, arm64_tahoe:   "efd9b413b79305556d5394744b08c9944d906d3c566588e78ed6068d0196d2e0"
+    sha256 cellar: :any, arm64_sequoia: "25c64e4bdcdad33bf903a148c9b142e3f85335ef33e14f35da5bb8074a75b134"
+    sha256 cellar: :any, arm64_sonoma:  "8dfe0b16b12b38358679d9af168f3c444f9c968c11d2262d3bb39a2a22ffb69a"
+    sha256 cellar: :any, sonoma:        "a37db2ffe950331496c14558c7acb62210103aed5c007728bf131967c76d0ce2"
+    sha256 cellar: :any, arm64_linux:   "959b1461a4037666664cf829ad7858b788e7404cd2c5b2c237f4acdc208081b8"
+    sha256 cellar: :any, x86_64_linux:  "7d2274555f8a09ca6af043feb33fd2c01daef398d15d6e4cacb597ab4cecff6a"
   end
 
   depends_on "node"
