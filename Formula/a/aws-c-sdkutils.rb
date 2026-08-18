97599eec9b5fbabcd31428cd14d4382edb4f1c83
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-18 03:24:39 +0000

    aws-c-sdkutils: update 0.2.10 bottle.

--- Formula/a/aws-c-sdkutils.rb
+++ Formula/a/aws-c-sdkutils.rb
@@ -7,12 +7,12 @@ class AwsCSdkutils < Formula
   compatibility_version 1
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "12e53ddcf6420f0109756010843908712e88c22638e200c5cbc0322b73891de9"
-    sha256 cellar: :any, arm64_sequoia: "ce7275fd31e8a75be22660782c7e75237daf036788f1983625d29a49be0034df"
-    sha256 cellar: :any, arm64_sonoma:  "96c5882e17e8ce44a8b2f39f4b7a94e81611dae6a7e4ca143dda5b6ea24fded0"
-    sha256 cellar: :any, sonoma:        "e9b98246467a6c9870698c7f16f706a40fd9e47684beabcf157cd9516c79141e"
-    sha256 cellar: :any, arm64_linux:   "5d61ac825b379449a3628ef6072510cd8b7eb8b8ce31064b02ba53525428b764"
-    sha256 cellar: :any, x86_64_linux:  "ac72abc24f0664ee82f5083d8a118ebea739b9954ad02a3d97b60a6fc268fd10"
+    sha256 cellar: :any, arm64_tahoe:   "fb0e452e0d5fd368ebc13c604a4acda9bc81c905c66062d37a8c501b8122489c"
+    sha256 cellar: :any, arm64_sequoia: "3473ca97c45b3386668aa8d5b3b3b24afc4bc8fa7a1c30f04195040e573f8400"
+    sha256 cellar: :any, arm64_sonoma:  "24f208f492d6a867711415024a0f9552073342af72ffaf081635736a2ac803fc"
+    sha256 cellar: :any, sonoma:        "9d906a80ed37da4082ffb6b6a2995498d395759749fbdf019ef3c8623b735018"
+    sha256 cellar: :any, arm64_linux:   "b1b6f25747719c5ddaf03b61f933915574547844b6ea94e4613e81f6ab00cb1b"
+    sha256 cellar: :any, x86_64_linux:  "c78a6f96439c9f0f2a8c51f2ec2e10cfe044c237a21d3caf06f71ccc893ae264"
   end
 
   depends_on "cmake" => :build
