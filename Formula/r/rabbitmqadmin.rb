2f218ce8d051577fd786515860e5c7a21123da1d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-18 03:23:16 +0000

    rabbitmqadmin 2.33.0

--- Formula/r/rabbitmqadmin.rb
+++ Formula/r/rabbitmqadmin.rb
@@ -1,8 +1,8 @@
 class Rabbitmqadmin < Formula
   desc "Command-line tool for RabbitMQ that uses the HTTP API"
   homepage "https://www.rabbitmq.com/docs/management-cli"
-  url "https://github.com/rabbitmq/rabbitmqadmin-ng/archive/refs/tags/v2.32.0.tar.gz"
-  sha256 "6236cf711256cc20b26a0ec2f3f48d33b4d0ee920e456d93fe801f3447db2f93"
+  url "https://github.com/rabbitmq/rabbitmqadmin-ng/archive/refs/tags/v2.33.0.tar.gz"
+  sha256 "0eea0ef610383d4270a1aed8f2ca39a218d212406fae5ec2df9bf08bb81a00de"
   license "Apache-2.0"
 
   bottle do
