24ce12609892133d7eda25361aa60649e8cafb0b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-18 06:20:45 +0000

    croc: update 11.1.2 bottle.

--- Formula/c/croc.rb
+++ Formula/c/croc.rb
@@ -7,12 +7,12 @@ class Croc < Formula
   head "https://github.com/schollz/croc.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b6be6014c9d5cb2ec3c9b4e8fd3ecbeafe5b9af87af66662989426c0389e3de5"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b6be6014c9d5cb2ec3c9b4e8fd3ecbeafe5b9af87af66662989426c0389e3de5"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b6be6014c9d5cb2ec3c9b4e8fd3ecbeafe5b9af87af66662989426c0389e3de5"
-    sha256 cellar: :any_skip_relocation, sonoma:        "6d2911b362de1e0cc913e78e9eebb71f2d6ff0234907f78678fc64abc8686a0a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "c8a29cf907fc532ab9149594e36a9669c8fe15171f468cb99a2e5afbe2bffec1"
-    sha256 cellar: :any,                 x86_64_linux:  "8146a0fdc0c807c4942cecf6c5a85caec69e861612e97b70b2af16750ee099b3"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8a53525f1d3594a7e6a2b8e77d0291649d60a971addcddb93df425be107a55a1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8a53525f1d3594a7e6a2b8e77d0291649d60a971addcddb93df425be107a55a1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8a53525f1d3594a7e6a2b8e77d0291649d60a971addcddb93df425be107a55a1"
+    sha256 cellar: :any_skip_relocation, sonoma:        "9655057791daaa95bad71d39e1754a5f8a1d9f19bfe7e079e89a89405b62f8f9"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "11e10108478ef7296f529a07c6de7e090a82d0841e56663a2bf4b380f9dcab32"
+    sha256 cellar: :any,                 x86_64_linux:  "745e2e9219c2ced2e667f3ca654c4fbed5733b654c094972e9fa48cb517f9394"
   end
 
   depends_on "go" => :build
