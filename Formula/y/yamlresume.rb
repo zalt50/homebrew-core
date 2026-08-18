03158dbcee14e3c11fec5ce33769d0d8c81e40dd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-18 06:48:53 +0000

    yamlresume: update 0.14.3 bottle.

--- Formula/y/yamlresume.rb
+++ Formula/y/yamlresume.rb
@@ -6,12 +6,12 @@ class Yamlresume < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9ecd220e872ec2e1d2247888da861dcf09b08be4f700c0f9d0d42622db912ff7"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1d87d465f708dba9dc2055c65ee1f62e48c63fdc0fc7f45cc96ec0ed9ae4d4f5"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7102ff690a35046e8e4d47d38bd8ba602ad89c605d06ca6176d7ab9fe8d45711"
-    sha256 cellar: :any_skip_relocation, sonoma:        "8eb572eff5541a165804e4b57699e5189b107f0c78105f8f55f42282c4bfddeb"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3addfc0375f2acf4612cc3cb4c895269b9bd208498611f2aa66a050ee3f356dd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3addfc0375f2acf4612cc3cb4c895269b9bd208498611f2aa66a050ee3f356dd"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "45aba94dd84cc32193d3ef10d858ac1cd6f0c7d2eaef9ecb17df2c43e867cb0b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a75540581600f4b66b154ff604f7f917dd903681e758a90253b1d556e1205fc1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "77a4ddebc99f653f1e1742ef8a5c973600624fb0c5d71024b39e07c88805cc60"
+    sha256 cellar: :any_skip_relocation, sonoma:        "d4122228d888c10a75903a5a6fc5221cb35f5e7074bfefab6ca11322af40cc32"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d099fbd2eec45568964d999ba30197db93907c1eacf61fb2a04571be9a2e53c1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d099fbd2eec45568964d999ba30197db93907c1eacf61fb2a04571be9a2e53c1"
   end
 
   depends_on "node"
