f9b23615384c8d9e59234b161c578863352f5e4b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-18 09:59:06 +0000

    tomcat@9: update 9.0.121 bottle.

--- Formula/t/tomcat@9.rb
+++ Formula/t/tomcat@9.rb
@@ -11,7 +11,7 @@ class TomcatAT9 < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "a9f5f846cfc1b99591a1dda57ed4e4590d8fcf6997bffd4549318a8f4ac7296e"
+    sha256 cellar: :any_skip_relocation, all: "11158ffc76373ebe6f4621f0e6184b6dd3157a559578045d9dad5ec7288eb300"
   end
 
   keg_only :versioned_formula
