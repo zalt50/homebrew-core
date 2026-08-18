7865dee28f5599ee2b4b9bfa85d79fc10371556f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-18 22:54:28 +0000

    pythran: update 0.19.0 bottle.

--- Formula/p/pythran.rb
+++ Formula/p/pythran.rb
@@ -9,12 +9,12 @@ class Pythran < Formula
   head "https://github.com/serge-sans-paille/pythran.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7d6f54c0238dc14c132d08adfb73ccfd2e4da315eb7a29ee20c5ff526a50bfc1"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7d6f54c0238dc14c132d08adfb73ccfd2e4da315eb7a29ee20c5ff526a50bfc1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7d6f54c0238dc14c132d08adfb73ccfd2e4da315eb7a29ee20c5ff526a50bfc1"
-    sha256 cellar: :any_skip_relocation, sonoma:        "ef2730ba9293e4b7b56aa9c4968e1aa5affd28e8a8407b4558a87da371b0caf6"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "7d6f54c0238dc14c132d08adfb73ccfd2e4da315eb7a29ee20c5ff526a50bfc1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7d6f54c0238dc14c132d08adfb73ccfd2e4da315eb7a29ee20c5ff526a50bfc1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "67ac72ad94168e18d5817877538317fbd52468bdf24a69112cd9118c25865d57"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "67ac72ad94168e18d5817877538317fbd52468bdf24a69112cd9118c25865d57"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "67ac72ad94168e18d5817877538317fbd52468bdf24a69112cd9118c25865d57"
+    sha256 cellar: :any_skip_relocation, sonoma:        "d5a6ed5fc7e32827a8c770d06a3492fc4d1714e88b4ade02a47d306ab04c2288"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "67ac72ad94168e18d5817877538317fbd52468bdf24a69112cd9118c25865d57"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "67ac72ad94168e18d5817877538317fbd52468bdf24a69112cd9118c25865d57"
   end
 
   depends_on "gcc" => :test
