0afef2d488dbd793f60692207134f35b0c3a220f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-18 13:11:58 +0000

    xcsift: update 1.4.1 bottle.

--- Formula/x/xcsift.rb
+++ Formula/x/xcsift.rb
@@ -7,12 +7,12 @@ class Xcsift < Formula
   head "https://github.com/ldomaradzki/xcsift.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "060225624cb0fcedd7e96cbbf5347dbb51649f6485f2ac8934814bc9570d4b7d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1853292e531cfa4e74be3975f04875fc5dc074d65b6d638eca79576c85c79096"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3275e8d066bbfd75c62033bd30001912f49c07968fa4d811b415340ddda5f0ba"
-    sha256 cellar: :any_skip_relocation, sonoma:        "2dc241c56b2558ff7ed02e684e8dc3c8d2762f2483c39fb3018f1bbbec63d113"
-    sha256 cellar: :any,                 arm64_linux:   "d5fd083b69ab076637a17c9777c34c1f30822fe5f6ccd4541e62eaeb3d4ad2da"
-    sha256 cellar: :any,                 x86_64_linux:  "10ef978d91010e4bf857a2fe94f89f7e020193420c74592f84c118b5f1f9458c"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f945a1d3b2f2e12e03d2c912661ded368efc932b1e05f086d9400a289606084e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1eefee55a3a0af81dcf66a33860281944a983d0ab3c78901b0c7a330703f0781"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9dfc64aff2e326dc6dd322bad2035136f64f8396d435ca5b496bd1d7fc0fcd95"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e2868d92ef2824f53c042e814af3a46a9a3f0582e4881dcc46c3388967f5360c"
+    sha256 cellar: :any,                 arm64_linux:   "1660602cfb35dc1fb574aef68669e847a85dfb98d06864c963de86fb6c70ca5f"
+    sha256 cellar: :any,                 x86_64_linux:  "a8d4c46f0eee175a97df739745d41c364c8ebff7babae3b6d1e2c3af0d1d7cff"
   end
 
   uses_from_macos "swift" => :build, since: :sonoma
