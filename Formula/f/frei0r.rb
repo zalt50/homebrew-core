9ecdefe11426a73bce18a70656846645bb513d28
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-18 15:20:32 +0000

    frei0r: update 3.4.0 bottle.

--- Formula/f/frei0r.rb
+++ Formula/f/frei0r.rb
@@ -7,12 +7,12 @@ class Frei0r < Formula
   compatibility_version 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c2706c42afd45857c29cc0fb7d708485974f2847926d8bd5ce7f124bbc5dbecc"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1cb5327841ef628a79c0f61e80bfb7642f6c3463d6a554ed6d7531b6547961f0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "dfbb5f11a01fc128c6682ae7745d793e0cba8f4750a453e95658118ec827e97e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "20f8ad866f1cc1c63348a7cf80d321a9727eb078e79538b72a7f1e9ba90a46ab"
-    sha256 cellar: :any,                 arm64_linux:   "b99d78dbeebbbfe344310bc32898fcd3358bd95ed71b9e3f23fb609fc2fcfb1c"
-    sha256 cellar: :any,                 x86_64_linux:  "d78c0812be35354b4a07e876d52501e6d1cf389ca5eeb0ed14a1fcac673865e6"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7e92a75da32424fe3f5559a650511a8bc80872332134fa95cbb36ae45cb4f9b5"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d5fe10a29c1a70472753eb7169863e9058996b14d381e10dcfc4d412afa276f0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "120d6cf5cec9c072941dee9a1f42c87577907b043559de3c754871ed237c7d25"
+    sha256 cellar: :any_skip_relocation, sonoma:        "545afaa3f3729c905d8246efe771cefac0f01dc3a5928b29aef1e944d68f16bb"
+    sha256 cellar: :any,                 arm64_linux:   "901e32a46dac71d5879455dc2cba6f82d6b6eea5376ce624a824c379027ea7fc"
+    sha256 cellar: :any,                 x86_64_linux:  "3fc99b2ccc1d88ec6bcbc285a161c98c8e57f1d9fe8a3e219fe970690eab255e"
   end
 
   depends_on "cmake" => :build
