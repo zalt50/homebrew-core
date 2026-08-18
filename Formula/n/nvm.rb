8c397608824625042560cea33a33b5ddf697cfb0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-18 08:57:13 +0000

    nvm 0.40.7

--- Formula/n/nvm.rb
+++ Formula/n/nvm.rb
@@ -1,8 +1,8 @@
 class Nvm < Formula
   desc "Manage multiple Node.js versions"
   homepage "https://github.com/nvm-sh/nvm"
-  url "https://github.com/nvm-sh/nvm/archive/refs/tags/v0.40.6.tar.gz"
-  sha256 "17302cad7feedb1ad33ba738f93d2176a90970724f22de119603624fcbdec1a2"
+  url "https://github.com/nvm-sh/nvm/archive/refs/tags/v0.40.7.tar.gz"
+  sha256 "d2fb84dba9914b02cd69b97df35dfca8695b8f22df6128667034d85b69b52d57"
   license "MIT"
   head "https://github.com/nvm-sh/nvm.git", branch: "master"
 
