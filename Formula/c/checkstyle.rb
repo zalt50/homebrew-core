ef654f503b865723e039a8370dcfbae885a70a6a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-18 14:10:04 +0000

    checkstyle 14.0.0

--- Formula/c/checkstyle.rb
+++ Formula/c/checkstyle.rb
@@ -1,8 +1,8 @@
 class Checkstyle < Formula
   desc "Check Java source against a coding standard"
   homepage "https://checkstyle.sourceforge.io/"
-  url "https://github.com/checkstyle/checkstyle/releases/download/checkstyle-13.11.0/checkstyle-13.11.0-all.jar"
-  sha256 "0ccb817d7cff230fa8d318ab9dac69b7fe8d8bf91d3a2b95d4ad0653f54ea5eb"
+  url "https://github.com/checkstyle/checkstyle/releases/download/checkstyle-14.0.0/checkstyle-14.0.0-all.jar"
+  sha256 "f3845df27df6a03e0b533fcc10bd5e4beab495b7a0d21c36b43ef243f6bf678f"
   license "LGPL-2.1-or-later"
 
   livecheck do
