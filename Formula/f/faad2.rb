61126b6b91c94fa601ad46110edf41ab96c0cdc9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-19 08:16:21 +0000

    faad2 2.11.3

--- Formula/f/faad2.rb
+++ Formula/f/faad2.rb
@@ -1,8 +1,8 @@
 class Faad2 < Formula
   desc "ISO AAC audio decoder"
   homepage "https://sourceforge.net/projects/faac/"
-  url "https://github.com/knik0/faad2/archive/refs/tags/2.11.2.tar.gz"
-  sha256 "3fcbd305e4abd34768c62050e18ca0986f7d9c5eca343fb98275418013065c0e"
+  url "https://github.com/knik0/faad2/archive/refs/tags/2.11.3.tar.gz"
+  sha256 "860ab62087e336c1844a70e33196c1790b525fb9a9e7b6ac4fab1a1a4e4d5ce8"
   license "GPL-2.0-or-later"
 
   bottle do
