478d5f7dc7b5b46709d312946688968312b41bf4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-19 20:48:37 +0000

    n8n-mcp 2.70.4

--- Formula/n/n8n-mcp.rb
+++ Formula/n/n8n-mcp.rb
@@ -1,8 +1,8 @@
 class N8nMcp < Formula
   desc "MCP for Claude Desktop, Claude Code, Windsurf, Cursor to build n8n workflows"
   homepage "https://www.n8n-mcp.com/"
-  url "https://registry.npmjs.org/n8n-mcp/-/n8n-mcp-2.70.1.tgz"
-  sha256 "d87605407760396fb0e98f739780abe196b2a2a744e6147b9c9aa3559fee573b"
+  url "https://registry.npmjs.org/n8n-mcp/-/n8n-mcp-2.70.4.tgz"
+  sha256 "c3ba2210b2fc00d24528d2d53a93c2e56e7c09b3ad833552e9a6acf05eb68d6e"
   license "MIT"
 
   bottle do
