97ee282928294b4b792fdc989f9377ca513b283d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-19 20:31:57 +0000

    chainsaw: update 2.16.4 bottle.

--- Formula/c/chainsaw.rb
+++ Formula/c/chainsaw.rb
@@ -7,12 +7,12 @@ class Chainsaw < Formula
   head "https://github.com/WithSecureLabs/chainsaw.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c526ddb6d1ec07f260275385ced0c998521d372b4a5a914d65c02d7fb6547346"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "754eafab7f23d27e2b218b8728d21e4855a49e77fbcf6b3db3d7231bfde9c377"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5fa830e74180c6d4f0a8870556364a1c784d0d833c15dc57cc5b72c5f317f986"
-    sha256 cellar: :any_skip_relocation, sonoma:        "7adee20d497acb24c6b732efd1301eb68ef92e45634e04bf908801327d09ab4e"
-    sha256 cellar: :any,                 arm64_linux:   "9d49eb88e5604875b2ef3f0ddbd68a6fe5d1a5a1fc9f72e96577a4498861166d"
-    sha256 cellar: :any,                 x86_64_linux:  "14a2814d49cd537bdfc32ac5121ed9e1fc832bf817a141f9e2457e40fb54005c"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8d9991a41f7b2151f07d6e9c602a826e1c8ba829a97094ee7f8d97af026810bd"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "59b48bace2e94800af8e4c3cad83876ad4b1416ff1d7864e01604e5182357f1a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6c5315932ed761a7478ed35a7e18a7ba7603addb48b75d98264ae6caa259560f"
+    sha256 cellar: :any_skip_relocation, sonoma:        "0f5040932f95559a7d8486c68aeda42a807d4e4647e1c21aca591fe275ef71dd"
+    sha256 cellar: :any,                 arm64_linux:   "efabf46fc5a2c508e2291b3f84bd78704ca44a71255d9796fd7a264af61476ce"
+    sha256 cellar: :any,                 x86_64_linux:  "f79481fb7c635c60b874c4b8da509bd2381cce352b46cb6100bd80dfc655cb57"
   end
 
   depends_on "rust" => :build
