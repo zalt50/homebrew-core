edd059104fcfed68173eb3a2cecea8d72ef7775c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-19 08:49:29 +0000

    recc: update 1.4.17_1 bottle.

--- Formula/r/recc.rb
+++ Formula/r/recc.rb
@@ -8,12 +8,12 @@ class Recc < Formula
   head "https://gitlab.com/BuildGrid/buildbox/buildbox.git", branch: "master"
 
   bottle do
-    sha256 arm64_tahoe:   "5ca6310657dc883d296da0d0d0def977cc66cfcffb6ce0887286af2d78c3c6ce"
-    sha256 arm64_sequoia: "6903273ee7d2296493269700582ca2932cb1dee589357af5184921da2e5466cb"
-    sha256 arm64_sonoma:  "cb31a8bf3143439fbfb92a64993aeeebc30fd0a28fff57b42a44c5f8f2c2b2b9"
-    sha256 sonoma:        "b0c68ceafc023489c3aff985deb7c445b5fe78e59a266fa9e06197bcf665cd6b"
-    sha256 arm64_linux:   "dbf9bec5d5aa5b9b8672f20347e5e8d52bf235d24dc509dc3dd4af505f4965a9"
-    sha256 x86_64_linux:  "2f32d5cd2dc0e2017ce2712b13d0e09534c36474944a7d64e9e0324fbdbc574d"
+    sha256 arm64_tahoe:   "aac815d7ddc7b1d025c89d89dfc53d2c087cbcb13907060f19b51f0c30c51b29"
+    sha256 arm64_sequoia: "c4b1e8a5040bf4a1276e2267f163875857ef30e2011330d2d6f41d79d9024d17"
+    sha256 arm64_sonoma:  "899e62ddc152d6f27a1086907b1be57f0d69c7932ddd336fc40db866b2e01b67"
+    sha256 sonoma:        "70ae701a9742c037bcf8e83cab93c0b9262b330a19931e685f57f48860a0174d"
+    sha256 arm64_linux:   "926ab82c3ae54b2ee082e0396338c5312cc8d0f30238760520ad547ef6fa736e"
+    sha256 x86_64_linux:  "98de9e9349e6d3f334e384520472df6a73eeab04c6c7ca6a3a026aebb7e9308e"
   end
 
   depends_on "cmake" => :build
