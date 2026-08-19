cf6393f263eb71b5cf4932a1bf7fb0f03f3cd4f5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-19 17:32:26 +0000

    gradle 9.7.1

--- Formula/g/gradle.rb
+++ Formula/g/gradle.rb
@@ -1,8 +1,8 @@
 class Gradle < Formula
   desc "Open-source build automation tool based on the Groovy and Kotlin DSL"
   homepage "https://www.gradle.org/"
-  url "https://services.gradle.org/distributions/gradle-9.7.0-all.zip"
-  sha256 "a9ecb5ac5c2ca40691e6527724d11d0b43b8c0a52825b77c09899f2a72d2d2bf"
+  url "https://services.gradle.org/distributions/gradle-9.7.1-all.zip"
+  sha256 "92c1a136d76b5017732a66d2e0a648ebff00dd3687d8bff0d0047a1bd904fdf2"
   license "Apache-2.0"
 
   livecheck do
