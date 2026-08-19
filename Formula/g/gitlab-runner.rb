fec6815fb4f0e37ca95f88ec489554aa0e9f0f25
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-19 11:15:59 +0000

    gitlab-runner 19.3.0

--- Formula/g/gitlab-runner.rb
+++ Formula/g/gitlab-runner.rb
@@ -2,8 +2,8 @@ class GitlabRunner < Formula
   desc "Official GitLab CI runner"
   homepage "https://gitlab.com/gitlab-org/gitlab-runner"
   url "https://gitlab.com/gitlab-org/gitlab-runner.git",
-      tag:      "v19.2.2",
-      revision: "343288f1214ac954d32f1c60ae3855f2d7b0afe6"
+      tag:      "v19.3.0",
+      revision: "9cbf0074ddb0e2f79d79f44406cafebf09117b64"
   license "MIT"
   head "https://gitlab.com/gitlab-org/gitlab-runner.git", branch: "main"
 
