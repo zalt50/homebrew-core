acefb95b4c27fd09cd2a1d1c8da7657c3ec02aa4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-19 14:23:11 +0000

    erofs-utils 1.9.4

--- Formula/e/erofs-utils.rb
+++ Formula/e/erofs-utils.rb
@@ -1,8 +1,8 @@
 class ErofsUtils < Formula
   desc "Utilities for Enhanced Read-Only File System"
   homepage "https://git.kernel.org/pub/scm/linux/kernel/git/xiang/erofs-utils.git"
-  url "https://git.kernel.org/pub/scm/linux/kernel/git/xiang/erofs-utils.git/snapshot/erofs-utils-1.9.3.tar.gz"
-  sha256 "17bfa54f4d370838c61081fce44022815a0366e282d777389589184414d5adc5"
+  url "https://git.kernel.org/pub/scm/linux/kernel/git/xiang/erofs-utils.git/snapshot/erofs-utils-1.9.4.tar.gz"
+  sha256 "7d135aa2550326a5acf20f53c518aea5a8900015ce50700044e40f818c31dd80"
   license "GPL-2.0-or-later"
   head "https://git.kernel.org/pub/scm/linux/kernel/git/xiang/erofs-utils.git", branch: "master"
 
