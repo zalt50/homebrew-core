a3e4415fc543fe9f7a1a426fcd1656ca42ed663e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-20 10:35:28 +0000

    varlock: update 1.17.0 bottle.

--- Formula/v/varlock.rb
+++ Formula/v/varlock.rb
@@ -6,12 +6,12 @@ class Varlock < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3cc035f786605edd92a9a100d10b40235fa5c31eeaf14ed5c8a7be6bbfe62902"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3cc035f786605edd92a9a100d10b40235fa5c31eeaf14ed5c8a7be6bbfe62902"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3cc035f786605edd92a9a100d10b40235fa5c31eeaf14ed5c8a7be6bbfe62902"
-    sha256 cellar: :any_skip_relocation, sonoma:        "fbb7883ace427f2ceef7777166123ec3f9b3e22c7484150f924eaebc760ba939"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8398382664f26ef61c765cb1d5475a3d1acf739a9975d6efb7180a24e5524457"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "83f5ea5090b581d82dd9792c4a04bc06fa9faefa03a0e900eea07946950c9c96"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "668846def45fb1687162e50b16dd2c9e256050eff8bf9c6d1ba2cdc3874c9f6a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "668846def45fb1687162e50b16dd2c9e256050eff8bf9c6d1ba2cdc3874c9f6a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "668846def45fb1687162e50b16dd2c9e256050eff8bf9c6d1ba2cdc3874c9f6a"
+    sha256 cellar: :any_skip_relocation, sonoma:        "433f7fd4ccaba9afa992f778b7dd5ebaf48e4f0c5c35f0e6b70434a463d76d01"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "9d25bf3d1f6728a968d6df7c84bbcd08566d40bd3c06742800f03650271adab1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8d1f2d50115c457b067044eabbc0161de4d4830821bcdfdd7deff37a73cb5855"
   end
 
   depends_on "node"
