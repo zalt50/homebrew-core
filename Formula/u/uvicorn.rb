b2ba47ce0b353aa32d27733a62e522575fa9ec88
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-20 09:58:33 +0000

    uvicorn: update 0.52.4 bottle.

--- Formula/u/uvicorn.rb
+++ Formula/u/uvicorn.rb
@@ -9,12 +9,12 @@ class Uvicorn < Formula
   head "https://github.com/Kludex/uvicorn.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "e8681059776088b3051741215c6b8400f6a2aaa4da55c94c59a8a03465b723ce"
-    sha256 cellar: :any, arm64_sequoia: "a92bb81e3da87376ccf97edbf8659a849cd9b673b7d7c3e9ce0288aa7fd802df"
-    sha256 cellar: :any, arm64_sonoma:  "fb3900bbb3657c6b1873c727ec6a50e6756dc30bb02982ad6cb98cb7d688ad6a"
-    sha256 cellar: :any, sonoma:        "f82042e2e5be4b63595e63c70e92bb3161e17898e20d53f544e9e592edc3732a"
-    sha256 cellar: :any, arm64_linux:   "28cca3b563a8b192a86a0cef635153c472f82c4d2441f300c343358a7609aa36"
-    sha256 cellar: :any, x86_64_linux:  "3938216f57536df765f78c1b21488c67b532ce114e9c33deeadaed9f3c9835bf"
+    sha256 cellar: :any, arm64_tahoe:   "570d14dabf66471f18963c1a09949b6985399631fd8252be08ad2facea7d0384"
+    sha256 cellar: :any, arm64_sequoia: "241e83dba13dfabd967ba0295c0b822c484b3f9389cf47c4e98f5e4999997ea9"
+    sha256 cellar: :any, arm64_sonoma:  "86a4c496c3e37377526110d99849e49a2d212cf7a864ce9bc5d247666a33a4aa"
+    sha256 cellar: :any, sonoma:        "528dfb22681dc52e72ce6f363a7ff9d50252c0eba1b6a1699f1e51a16db1aaf0"
+    sha256 cellar: :any, arm64_linux:   "94994ef15e6fcf65504ccb2bbd46d2662adad887c87984eda273d6e0579e3b26"
+    sha256 cellar: :any, x86_64_linux:  "5a6cf5901201b6b8b0cbfad7fae20e5578b9fb2ed5186d8e9027b61d7833012e"
   end
 
   depends_on "rust" => :build
