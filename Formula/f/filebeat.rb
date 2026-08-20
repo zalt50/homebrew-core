def2207ff2090c0e566a28899df10044513c2aaf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-20 17:25:39 +0000

    filebeat 9.5.2

--- Formula/f/filebeat.rb
+++ Formula/f/filebeat.rb
@@ -2,8 +2,8 @@ class Filebeat < Formula
   desc "File harvester to ship log files to Elasticsearch or Logstash"
   homepage "https://www.elastic.co/products/beats/filebeat"
   url "https://github.com/elastic/beats.git",
-      tag:      "v9.5.1",
-      revision: "53197d5422766e985c2aa0f56607750d34d0e912"
+      tag:      "v9.5.2",
+      revision: "8f4fe1e5dec067a139dce33d3af88c24b58c3660"
   # Outside of the "x-pack" folder, source code in a given file is licensed
   # under the Apache License Version 2.0
   license "Apache-2.0"
