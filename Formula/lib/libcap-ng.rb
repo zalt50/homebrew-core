0cc142209351a60502873d7b3eae5a1551c3a2d4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-20 17:49:18 +0000

    libcap-ng 0.9.4

--- Formula/lib/libcap-ng.rb
+++ Formula/lib/libcap-ng.rb
@@ -1,8 +1,8 @@
 class LibcapNg < Formula
   desc "Library for Linux that makes using posix capabilities easy"
   homepage "https://people.redhat.com/sgrubb/libcap-ng/"
-  url "https://github.com/stevegrubb/libcap-ng/archive/refs/tags/v0.9.3.tar.gz"
-  sha256 "fe11ebbb55904763b3532f19069f13ec319042634620180a03bd4653d301563e"
+  url "https://github.com/stevegrubb/libcap-ng/archive/refs/tags/v0.9.4.tar.gz"
+  sha256 "49c19c669bb06d5c0a398e813034240571b78b316faf1a9609c2cc3c284b55ba"
   license all_of: ["LGPL-2.1-or-later", "GPL-2.0-or-later"]
   head "https://github.com/stevegrubb/libcap-ng.git", branch: "master"
 
