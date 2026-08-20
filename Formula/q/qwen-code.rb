82f4314c89370c8db78adfd4dadec49877a223a8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-20 03:12:05 +0000

    qwen-code 0.21.14

--- Formula/q/qwen-code.rb
+++ Formula/q/qwen-code.rb
@@ -1,8 +1,8 @@
 class QwenCode < Formula
   desc "AI-powered command-line workflow tool for developers"
   homepage "https://github.com/QwenLM/qwen-code"
-  url "https://registry.npmjs.org/@qwen-code/qwen-code/-/qwen-code-0.21.13.tgz"
-  sha256 "b0bfd51d89c21ddbe214c568a7afb93ebc14b1dcf79967ac84bffdc01cb1ec53"
+  url "https://registry.npmjs.org/@qwen-code/qwen-code/-/qwen-code-0.21.14.tgz"
+  sha256 "ea865c120c3d73474f44e27fdc0bffc3ed21eb39403918def1c1a917fc1bc737"
   license "Apache-2.0"
 
   bottle do
