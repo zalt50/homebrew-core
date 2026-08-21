1cf4daf13cbdcf7285e6f4966095c07b5042ebe3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-21 16:17:06 +0000

    supabase-mcp-server: update 0.11.0 bottle.

--- Formula/s/supabase-mcp-server.rb
+++ Formula/s/supabase-mcp-server.rb
@@ -6,7 +6,7 @@ class SupabaseMcpServer < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "e92052c83724d8ea1b63823b8ddfc35475dfbeafc2410b04418f1f15c5d3a9ef"
+    sha256 cellar: :any_skip_relocation, all: "d2e7f7497ce6d15f10b84e3df5216fcf245443e431ce3c9dc5ad139ebe0b7c9f"
   end
 
   depends_on "node"
