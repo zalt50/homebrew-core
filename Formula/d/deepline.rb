de3c405ac2da1599305a62a3aa41adb23098e15f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-21 05:19:32 +0000

    deepline 0.3.0

--- Formula/d/deepline.rb
+++ Formula/d/deepline.rb
@@ -1,8 +1,8 @@
 class Deepline < Formula
   desc "CLI for Deepline data enrichment and durable plays"
   homepage "https://code.deepline.com"
-  url "https://registry.npmjs.org/deepline/-/deepline-0.2.60.tgz"
-  sha256 "741b67ab8795a1147f7c56612f0e2f60125acfec44f22a34fe6ffb74ee36fbdd"
+  url "https://registry.npmjs.org/deepline/-/deepline-0.3.0.tgz"
+  sha256 "4c8c382631a64f819cfe4f1e69f4739f72e1903bfd8a4d236c28feb30a45a721"
   license "MIT"
 
   livecheck do
