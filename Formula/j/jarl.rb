ba34fa9a2e98d063f94dc8d1f3c45c963e53a097
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-21 21:51:26 +0000

    jarl: add 0.5.0 bottle.

--- Formula/j/jarl.rb
+++ Formula/j/jarl.rb
@@ -5,6 +5,15 @@ class Jarl < Formula
   sha256 "7b1fd11adc3924fa71f3a4202a2a4a87f1c8d62944160adedba65eb8f01d1cda"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "45fc95c331aecc9e9f67b7aa447aa1789abad939906779f3d58bb51daf25bc9d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "00e0b769f6aff9bfbd8ce678d007c033d2da67ea640a63a3a846aa4291bdd509"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e3a65db005fd59432a89600b1c4a7c20c6cc21830085978546e898b43bc69faf"
+    sha256 cellar: :any_skip_relocation, sonoma:        "2953143e10d3c181beaca7f6af7d07c1345725e2bb68b56bba9e5270af500017"
+    sha256 cellar: :any,                 arm64_linux:   "1ba5b56ee9e7b427d08df9d30aa0da1d15831127551e0ff8e1225c386161f28c"
+    sha256 cellar: :any,                 x86_64_linux:  "5af2d4141c8130f3bbaec1b7edd1c4b48b02329adc390454a07fc340f43f2158"
+  end
+
   depends_on "rust" => :build
 
   def install
