9679df8a4c27685b54a73898c6441bd6f895d25c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-21 10:31:32 +0000

    scdl: update 3.0.8 bottle.

--- Formula/s/scdl.rb
+++ Formula/s/scdl.rb
@@ -9,12 +9,12 @@ class Scdl < Formula
   head "https://github.com/scdl-org/scdl.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5b9ed1397df2beb3cb80452d8c792eefbf1e08eb59f2e5db8b1f5f9838de0878"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2e5df23d2f3203badf11c9e9ae19658e47e2ab126d37a097df8ad175252c39e8"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "798fa763dc4f81575a42df6bad3f464784d1fbc29a5b4ff716733684000c10da"
-    sha256 cellar: :any_skip_relocation, sonoma:        "fe7bdd354b4a5619fc566bb0eb4d8783d2bcd2906912b355fea43eb08aa52a8b"
-    sha256 cellar: :any,                 arm64_linux:   "65cfcc4258c8a2170a149eafac2bf3d4890d48d3b8de5d201323d2dad80f7f1a"
-    sha256 cellar: :any,                 x86_64_linux:  "47e644da0806bf659dde4d41482e0e4f41fe23a18922776122d59e38b62ee823"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c4a3860381ed36b96d658f199a451fdb4136e1c7dfe237b1ccb21ce3474769de"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "25898cbf916c920405c7c238b37fd8ee5bcfd075fa56821ee28d5faa92c12be8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6c3fadd0f18db7addf267d6c4e1cf9b245bd528e7aee4336fc5b2d36af0971b6"
+    sha256 cellar: :any_skip_relocation, sonoma:        "b025e73968a52d5d1d8f0f271088e93f1cf8de98cca8d2215543ee6bf8db6520"
+    sha256 cellar: :any,                 arm64_linux:   "e15730a2076d5502a4474e46cc99dcd8700509e445163ab654e50f0dd6f923a6"
+    sha256 cellar: :any,                 x86_64_linux:  "27c5cbe3a5166e365eaed80d923d53fb3dad49a32c8660ab3cd4ca1a0ad3f633"
   end
 
   depends_on "certifi" => :no_linkage
