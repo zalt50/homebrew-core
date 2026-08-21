60c4839a1251d3eb15f7d55ce698fc07c088a5aa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-21 21:33:16 +0000

    staticcheck 2026.2.1

--- Formula/s/staticcheck.rb
+++ Formula/s/staticcheck.rb
@@ -1,10 +1,9 @@
 class Staticcheck < Formula
   desc "State of the art linter for the Go programming language"
   homepage "https://staticcheck.dev/"
-  url "https://github.com/dominikh/go-tools/archive/refs/tags/2026.2.tar.gz"
-  sha256 "72fa00a4bef32ab52aa3ca916e70108ca021ef3c35dda555350c0b670c432033"
+  url "https://github.com/dominikh/go-tools/archive/refs/tags/2026.2.1.tar.gz"
+  sha256 "8d807cd909f4481d6777f7707e5ae75dcc399e14d68ff14a3c814731826e0dfc"
   license "MIT"
-  revision 1
   head "https://github.com/dominikh/go-tools.git", branch: "master"
 
   bottle do
