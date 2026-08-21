6f5e767fbe2e0cda9bfa234d181ec966b7446ea1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-21 11:07:28 +0000

    concord 2.5.12

--- Formula/c/concord.rb
+++ Formula/c/concord.rb
@@ -1,8 +1,8 @@
 class Concord < Formula
   desc "Terminal user interface client for Discord"
   homepage "https://github.com/chojs23/concord"
-  url "https://github.com/chojs23/concord/archive/refs/tags/v2.5.11.tar.gz"
-  sha256 "d88817bffb89b7d27f77743ed000a46cda04f134e602a1ccdbff795fc15018b2"
+  url "https://github.com/chojs23/concord/archive/refs/tags/v2.5.12.tar.gz"
+  sha256 "6df8da04e80a97d222febf35a585756673251b58f464e520ccc8edfb4da85fc5"
   license "GPL-3.0-only"
 
   bottle do
