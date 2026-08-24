dbb805f56d5d9d078be642a4ea41b96bfef118ed
Michael Cho <michael@michaelcho.dev>
2026-08-23 23:10:20 -0400

    percona-server 9.7.1-1

--- Formula/p/percona-server.rb
+++ Formula/p/percona-server.rb
@@ -1,10 +1,9 @@
 class PerconaServer < Formula
   desc "Drop-in MySQL replacement"
   homepage "https://www.percona.com"
-  url "https://downloads.percona.com/downloads/Percona-Server-8.4/Percona-Server-8.4.11-11/source/tarball/percona-server-8.4.11-11.tar.gz"
-  sha256 "2fb90e235c25183d73c972cba481a32ea2d90cefca0669fe0786defc2acdfa18"
+  url "https://downloads.percona.com/downloads/Percona-Server-9.7/Percona-Server-9.7.1-1/source/tarball/percona-server-9.7.1-1.tar.gz"
+  sha256 "cfa835f66b415a46e64420d515096281f42a7bcf189bda0f6c434ea5a55d63ee"
   license "BSD-3-Clause"
-  revision 3
 
   livecheck do
     url "https://www.percona.com/wp-admin/admin-ajax.php", post_form: {
@@ -60,6 +59,13 @@ class PerconaServer < Formula
     cause "Requires GCC 10 or newer"
   end
 
+  # Backport commit from MySQL to fix build on newer Clang
+  patch do
+    url "https://github.com/mysql/mysql-server/commit/b006e3af4b6b1b6f7fdf7b91a00c6293c4f292b1.patch?full_index=1"
+    sha256 "e99e7e63d8581cbfb513a2dd43f36f8da0e3c1bf26e512156847c1036280adf3"
+    type :backport
+  end
+
   # Patch out check for Homebrew `boost`.
   # This should not be necessary when building inside `brew`.
   # https://github.com/Homebrew/homebrew-test-bot/pull/820
@@ -81,6 +87,9 @@ class PerconaServer < Formula
 
     # Find Homebrew OpenLDAP instead of the macOS framework
     inreplace "cmake/ldap.cmake", "NAMES ldap_r ldap", "NAMES ldap"
+
+    # `pthread_self` is only pulled in by a `HAVE_SCHED_GETCPU`-guarded include
+    inreplace "storage/rocksdb/ib_ut0counter.h", "#include <cstdint>", "#include <cstdint>\n#include <pthread.h>"
 
     # Disable ABI checking
     inreplace "cmake/abi_check.cmake", "RUN_ABI_CHECK 1", "RUN_ABI_CHECK 0" if OS.linux?
@@ -111,6 +120,8 @@ class PerconaServer < Formula
       -DWITH_SSL=system
       -DWITH_ZLIB=system
       -DWITH_ZSTD=system
+      -DWITH_MYSQL_SERVER_TELEMETRY=OFF
+      -DWITH_MYSQL_CLIENT_TELEMETRY=OFF
       -DWITH_UNIT_TESTS=OFF
       -DROCKSDB_BUILD_ARCH=#{ENV.effective_arch}
       -DALLOW_NO_ARMV81A_CRYPTO=ON
@@ -118,12 +129,6 @@ class PerconaServer < Formula
     ]
     args << "-DROCKSDB_DISABLE_AVX2=ON" if build.bottle?
     args << "-DWITH_KERBEROS=system" unless OS.mac?
-
-    # Workaround for
-    #  error: a template argument list is expected after a name prefixed by the template keyword
-    #   84 |     return Archive_derived_type::template get_size(std::forward<Type>(arg));
-    #      |                                           ^
-    ENV.append_to_cflags "-Wno-missing-template-arg-list-after-template-kw" if OS.mac?
 
     system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
     system "cmake", "--build", "build"
