4a431972260a3ffd8e51087a754a7bc2ed5aba52
Sean Molenaar <smillerdev@me.com>
2026-09-02 15:17:24 +0200

    libnghttp3: add network sandboxing

--- Formula/lib/libnghttp3.rb
+++ Formula/lib/libnghttp3.rb
@@ -20,6 +20,8 @@ class Libnghttp3 < Formula
 
   depends_on "cmake" => :build
 
+  deny_network_access!
+
   def install
     system "cmake", "-S", ".", "-B", "build", "-DENABLE_LIB_ONLY=1", *std_cmake_args
     system "cmake", "--build", "build"
