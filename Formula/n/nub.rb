22a5380e8d3053cdbf0fdc511f2c2631bfa87648
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-13 23:18:43 +0000

    nub 0.9.2

--- Formula/n/nub.rb
+++ Formula/n/nub.rb
@@ -1,8 +1,8 @@
 class Nub < Formula
   desc "Fast TypeScript runtime and package manager that augments Node"
   homepage "https://nubjs.com"
-  url "https://github.com/nubjs/nub/archive/refs/tags/v0.9.1.tar.gz"
-  sha256 "e42827f0ad1b416670c30c584ed5a065fd1ffa620e35a8b8b60fa817c58d5511"
+  url "https://github.com/nubjs/nub/archive/refs/tags/v0.9.2.tar.gz"
+  sha256 "76cdad3d6a047aba688330b95b926aecc6b725fe5c966014144c37505881307d"
   license "MIT"
 
   livecheck do
