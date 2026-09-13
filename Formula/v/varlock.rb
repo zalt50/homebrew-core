2956dbc282a7dbf9ed9c573d622b27bab8a896fb
FX Coudert <1980544+fxcoudert@users.noreply.github.com>
2026-09-13 16:32:04 +0000

    varlock: update 1.18.0 bottle.

--- Formula/v/varlock.rb
+++ Formula/v/varlock.rb
@@ -6,11 +6,12 @@ class Varlock < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "fdc8df4cecf38415482d46543544d8bc78d0b01c786f03cce9003876a64ab405"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fdc8df4cecf38415482d46543544d8bc78d0b01c786f03cce9003876a64ab405"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fdc8df4cecf38415482d46543544d8bc78d0b01c786f03cce9003876a64ab405"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "960e61e71e811a70731e371a3f5d2da60d673c6a99be821578226dd859c7aa76"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b2fa87c297224df67949847349ffde234826464c48dd95c6cd9dd689f014212e"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "fdc8df4cecf38415482d46543544d8bc78d0b01c786f03cce9003876a64ab405"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "fdc8df4cecf38415482d46543544d8bc78d0b01c786f03cce9003876a64ab405"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "fdc8df4cecf38415482d46543544d8bc78d0b01c786f03cce9003876a64ab405"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:      "fdc8df4cecf38415482d46543544d8bc78d0b01c786f03cce9003876a64ab405"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "960e61e71e811a70731e371a3f5d2da60d673c6a99be821578226dd859c7aa76"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:      "b2fa87c297224df67949847349ffde234826464c48dd95c6cd9dd689f014212e"
   end
 
   depends_on "node"
