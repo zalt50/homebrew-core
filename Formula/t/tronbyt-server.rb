cf49af10e10330716bfa13cdbec95018a153f5a7
Patrick Linnane <patrick@linnane.io>
2026-09-13 21:20:04 +0000

    tronbyt-server: update 2.4.0 bottle.

--- Formula/t/tronbyt-server.rb
+++ Formula/t/tronbyt-server.rb
@@ -7,12 +7,11 @@ class TronbytServer < Formula
   head "https://github.com/tronbyt/server.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any, arm64_golden_gate: "fa5ba0bc77b1a0575017af120c001dcff39ef88d882e309e210aca4d43cfa651"
-    sha256 cellar: :any, arm64_tahoe:       "79e0348ccb309df2f684703d26ebb351300caf0f1cdf7d80eec5731abccec965"
-    sha256 cellar: :any, arm64_sequoia:     "99505bea7cbe2ab5c9256932c98a91ab7437822aca6c6a801bfd92280ebab291"
-    sha256 cellar: :any, arm64_sonoma:      "fe642e4f784ab191f92ee2ba243a50e0fd14eb793ab8dc2e97f7847de2c81b0f"
-    sha256 cellar: :any, arm64_linux:       "f227f68eb0097256a22da43d1baea023d389f350c9f34bc4230d888902b3899f"
-    sha256 cellar: :any, x86_64_linux:      "435a99082df57715997c38413f0ba6deeaf8a24d8e494ff9316ba05327a9f13a"
+    sha256 cellar: :any, arm64_golden_gate: "310db534d8153086696920651cb1f80f4ad5789157df4ceb706209e774332c64"
+    sha256 cellar: :any, arm64_tahoe:       "dce1523443b59664a4401f1e36ad29cae37e56b447d588ffaada142b969d4efa"
+    sha256 cellar: :any, arm64_sequoia:     "8fb285a26ba69b9ca358945b1e3e08966964151ae232df193a608cede9162dce"
+    sha256 cellar: :any, arm64_linux:       "16178ef81f33c75913a389dc479ea9a5454d6c1808b96c5fb3efbb71bbfa8792"
+    sha256 cellar: :any, x86_64_linux:      "c5e85dc2b4c0bc763fd9fee15524b11fe48f1d1d85a43310e35eabba83a075ee"
   end
 
   depends_on "go" => :build
