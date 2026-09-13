30b86efcb3bb870d9d7756497fb1cd808aa6b99c
Patrick Linnane <patrick@linnane.io>
2026-09-13 19:44:41 +0000

    mycli: update 2.24.0 bottle.

--- Formula/m/mycli.rb
+++ Formula/m/mycli.rb
@@ -8,12 +8,11 @@ class Mycli < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any, arm64_golden_gate: "ba4566311cc2a92bee5198c10403f27535afb013a051fbc789c11063dbed9dc5"
-    sha256 cellar: :any, arm64_tahoe:       "4208afd5047aef177e9e1dc217ea885a4f93ca656a38979c6ec6a03f73a282c4"
-    sha256 cellar: :any, arm64_sequoia:     "153e479c047f8ca5790098842cc08263d7422c561bcf21ebe460627ce81f77f0"
-    sha256 cellar: :any, arm64_sonoma:      "f3cbf2c7208baacf0effaefdc250c5ce5bdab105c8381ca6f5caede5be3de51d"
-    sha256 cellar: :any, arm64_linux:       "991d09063339bc18c22375c468182d71f4e5dbd16e7c6e23a02596b55bc31059"
-    sha256 cellar: :any, x86_64_linux:      "824cb24adbaa7c0a6a2863037e49743c2e618a9fba9258977ed09386c9e10ee7"
+    sha256 cellar: :any, arm64_golden_gate: "5253d33aac2664b09e261301f5f37442ab0c56628291fae98f81c1a93b5797ca"
+    sha256 cellar: :any, arm64_tahoe:       "a210c9ddfef9a0485ed77dc47959422e62287aa28ba584ffa8da66222e772ca0"
+    sha256 cellar: :any, arm64_sequoia:     "bb76c4fb009456908d3ec1f7af7073d1b739440f4ab4033490814e5dc9d86afa"
+    sha256 cellar: :any, arm64_linux:       "eb63c2916f4816569da1c07663a409e18c27a9e3c2099867d654e6bbbf85b0f4"
+    sha256 cellar: :any, x86_64_linux:      "ca12635b38007d515dab3b990ae4b9dfdc1bee99cf4a4caa9457a303d5a6fdd1"
   end
 
   depends_on "rust" => :build # for jiter, polars, vl-convert
