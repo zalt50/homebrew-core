a953ad4c67e050a4211115a83baa6a2d79668162
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-13 23:11:47 +0000

    dcd: update 0.18.9 bottle.

--- Formula/d/dcd.rb
+++ Formula/d/dcd.rb
@@ -8,11 +8,11 @@ class Dcd < Formula
   head "https://github.com/dlang-community/dcd.git", branch: "master"
 
   bottle do
-    sha256               arm64_golden_gate: "2bc4fcdd140936c1780a00f7d79efa45e4f17c7eecb334cb934dd6d0c068f2a0"
-    sha256               arm64_tahoe:       "def9ae718fd56434aa61e338bd136ea87969ad7d0882dd6d77e8638d5e71a5a8"
-    sha256               arm64_sequoia:     "a83999ecff188e405b7678439ef3b56ec70e301a7cf3a6be97ddd6644576a4d0"
-    sha256 cellar: :any, arm64_linux:       "b944200f3819d9c58316de234dca817d9c8cb7167385d470fd40da7c17f8b979"
-    sha256 cellar: :any, x86_64_linux:      "d0a03c980a417cef92a99257081e9179d9712f74a70fab9dff0538bff3c94e76"
+    sha256               arm64_golden_gate: "f4d3fa9d331b5132691ce8755cd5e73169b9acc864a37986948921bd9403cb53"
+    sha256               arm64_tahoe:       "8ce5f300194818cd68b575497d82942fbe79a5535521dab05203bca31acb447f"
+    sha256               arm64_sequoia:     "19a77bbff0f98a247f9c3974c372dbf7879c8994caa2e78aabf50245209f0556"
+    sha256 cellar: :any, arm64_linux:       "a64e9c8185f0fd9075ba21479095662d4daf8547c66501c855556ea4eb10407a"
+    sha256 cellar: :any, x86_64_linux:      "2b81a1b0270d36822eb446d830b266b240732723049fa0c3ddc495a49b458c7e"
   end
 
   depends_on "ldc" => :build
