f7feb83bbc6cc73877028a9ddb5ff94fb700b076
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-13 13:59:54 +0000

    dysk 3.7.0

--- Formula/d/dysk.rb
+++ Formula/d/dysk.rb
@@ -1,8 +1,8 @@
 class Dysk < Formula
   desc "Linux utility to get information on filesystems, like df but better"
   homepage "https://dystroy.org/dysk/"
-  url "https://github.com/Canop/dysk/archive/refs/tags/v3.6.1.tar.gz"
-  sha256 "a712ce25f8c5867a29b699e5e22fed58b8884fb7ca112f01845c7a99b111dce5"
+  url "https://github.com/Canop/dysk/archive/refs/tags/v3.7.0.tar.gz"
+  sha256 "6c53a413f9c79855824116483ebcad7b6cd3cd6d37bbf39698d37e5013b27454"
   license "MIT"
   head "https://github.com/Canop/dysk.git", branch: "main"
 
