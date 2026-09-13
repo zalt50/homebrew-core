2a20ef0c92e79cdc67c55cf1868715e6b701e9ad
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-13 20:57:53 +0000

    treemd 0.8.2

--- Formula/t/treemd.rb
+++ Formula/t/treemd.rb
@@ -1,8 +1,8 @@
 class Treemd < Formula
   desc "TUI and CLI dual pane markdown viewer"
   homepage "https://github.com/epistates/treemd"
-  url "https://github.com/Epistates/treemd/archive/refs/tags/v0.8.1.tar.gz"
-  sha256 "aa89c2c48c5741f8c3d8c52dc7dd544b6d96f73d54dc612cfd9d3f22f6eb1289"
+  url "https://github.com/Epistates/treemd/archive/refs/tags/v0.8.2.tar.gz"
+  sha256 "28a857b266ef9ae5591b914260b058328fd0dbaf1c04e7617891e6ccbac64000"
   license "MIT"
   head "https://github.com/epistates/treemd.git", branch: "main"
 
