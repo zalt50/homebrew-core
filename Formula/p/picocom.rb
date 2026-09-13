9616a55b56bca4e6b2eb62c7260817fe9d6904bf
tfx2001 <tfx2001@outlook.com>
2026-09-13 16:39:49 +0800

    picocom 2024-07
    - Switch from GitHub to GitLab, where Wolfram Sang maintains the project
    - Bump the version to 2024-07
    - Add go-md2man as a build dependency and run `make doc` because the
      source archive no longer includes a prebuilt man page
    - Remove the arbitrary baud-rate patch, which is already included
      upstream [1]
    
    [1] https://gitlab.com/wsakernel/picocom/-/commit/f806bf28266cccdb75ba89d754de8d8fa64c6127
    
    Signed-off-by: Fengwei Tan <tfx2001@outlook.com>

--- Formula/p/picocom.rb
+++ Formula/p/picocom.rb
@@ -1,10 +1,9 @@
 class Picocom < Formula
   desc "Minimal dumb-terminal emulation program"
-  homepage "https://github.com/npat-efault/picocom"
-  url "https://github.com/npat-efault/picocom/archive/refs/tags/3.1.tar.gz"
-  sha256 "e6761ca932ffc6d09bd6b11ff018bdaf70b287ce518b3282d29e0270e88420bb"
+  homepage "https://gitlab.com/wsakernel/picocom"
+  url "https://gitlab.com/wsakernel/picocom/-/archive/2024-07/picocom-2024-07.tar.gz"
+  sha256 "4379de2ec591a5848123f37ccdbc7fbeee6dd3520ef1ce4119d84202fc268a17"
   license "GPL-2.0-or-later"
-  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_golden_gate: "a0abd50d8e84a919f7b024ddd32311d77a9a2830ee1f481b87e5db4758cf651f"
@@ -22,18 +21,10 @@ class Picocom < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:      "c765edf56732a7dd0631c12068fd17ba7800c98ceb24f98fa65990e7fa13dadc"
   end
 
-  # Upstream picocom supports arbitrary baud-rate settings on macOS out of the
-  # box, but only applies that to i386 and x86_64 systems. With the advent of
-  # arm64 macs, it is now necessary to expand that support.
-  patch do
-    url "https://github.com/npat-efault/picocom/commit/f806bf28266cccdb75ba89d754de8d8fa64c6127.patch?full_index=1"
-    sha256 "b1a29265d5b5e0e7e7f8f3194b818802de8c7d18e80525bc43cbb896a6def590"
-    type :unofficial
-    resolves "https://github.com/npat-efault/picocom/pull/129"
-  end
+  depends_on "go-md2man" => :build
 
   def install
-    system "make"
+    system "make", "all", "doc"
     bin.install "picocom"
     man1.install "picocom.1"
   end
