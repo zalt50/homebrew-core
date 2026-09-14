d4564912932213cea03603b96000811b4c2e749b
Xavier Roche <roche@httrack.com>
2026-09-14 08:15:51 +0200

    httrack: keep Linux desktop files

--- Formula/h/httrack.rb
+++ Formula/h/httrack.rb
@@ -25,8 +25,8 @@ class Httrack < Formula
 
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make", "install"
-    # Don't need Gnome integration
-    rm_r(Dir["#{share}/{applications,pixmaps}"])
+    # Gnome integration is inert on macOS, but Linux desktops use it
+    rm_r(Dir["#{share}/{applications,pixmaps}"]) if OS.mac?
   end
 
   test do
