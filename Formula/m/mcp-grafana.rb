2615ed56e78fafe15973abdfa734aec48411f562
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-14 18:05:38 +0000

    mcp-grafana 1.4.2

--- Formula/m/mcp-grafana.rb
+++ Formula/m/mcp-grafana.rb
@@ -1,8 +1,8 @@
 class McpGrafana < Formula
   desc "MCP server for Grafana"
   homepage "https://github.com/grafana/mcp-grafana"
-  url "https://github.com/grafana/mcp-grafana/archive/refs/tags/v1.4.1.tar.gz"
-  sha256 "1cc5dec0d45a7bd111f3a4f56795a3645a9cafbaf8c5ceeabc3e7f97022803f0"
+  url "https://github.com/grafana/mcp-grafana/archive/refs/tags/v1.4.2.tar.gz"
+  sha256 "4fd3452b8c1fe940ec01481d1acb3e41aa9cb2c2c805e4fdb8a2f2f0e0673677"
   license "Apache-2.0"
   head "https://github.com/grafana/mcp-grafana.git", branch: "main"
 
