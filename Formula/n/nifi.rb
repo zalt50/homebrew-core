6b097459bf3655e0839a5c7fb81cb46656838665
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-14 02:41:46 +0000

    nifi 2.12.0

--- Formula/n/nifi.rb
+++ Formula/n/nifi.rb
@@ -1,9 +1,9 @@
 class Nifi < Formula
   desc "Easy to use, powerful, and reliable system to process and distribute data"
   homepage "https://nifi.apache.org"
-  url "https://www.apache.org/dyn/closer.lua?path=/nifi/2.11.0/nifi-2.11.0-bin.zip"
-  mirror "https://archive.apache.org/dist/nifi/2.11.0/nifi-2.11.0-bin.zip"
-  sha256 "e549acad7e320416b12cb3a902884d0d5458cecd602d1ffd3f26d79081d7f512"
+  url "https://www.apache.org/dyn/closer.lua?path=/nifi/2.12.0/nifi-2.12.0-bin.zip"
+  mirror "https://archive.apache.org/dist/nifi/2.12.0/nifi-2.12.0-bin.zip"
+  sha256 "46482ddb2a5869a9ef005d7b19a463739695b1a7a8a646030aa3eadc427a61a3"
   license "Apache-2.0"
 
   bottle do
