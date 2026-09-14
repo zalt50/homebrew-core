b50eef3504a6a630aceb36a5207e8f335ba10a57
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-14 21:47:55 +0000

    vsce: update 4.0.0 bottle.

--- Formula/v/vsce.rb
+++ Formula/v/vsce.rb
@@ -14,13 +14,11 @@ class Vsce < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "de4c0e4a69c4bfbdfde112148f14a989cbfac2ded805c4d8d3220e83fbb88872"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "fc33e023eccb572d9611b5c41b1bf54cd80d4ffd80ffc43efb787fb22a1e91ea"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "fc33e023eccb572d9611b5c41b1bf54cd80d4ffd80ffc43efb787fb22a1e91ea"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:      "fc33e023eccb572d9611b5c41b1bf54cd80d4ffd80ffc43efb787fb22a1e91ea"
-    sha256 cellar: :any_skip_relocation, sonoma:            "e7f575023bb4fd3c5bc0fff3399a025753395a7773227ea2a0641c816c173e81"
-    sha256 cellar: :any,                 arm64_linux:       "277b790509802b9c5db6784a4ab6cf071698abce1fc88d3c3ffd5723cb632975"
-    sha256 cellar: :any,                 x86_64_linux:      "71b2b6bd8f2b78ef985778bd2a2feb27197ae538cdd513329a8e72655bc76e88"
+    sha256 cellar: :any, arm64_golden_gate: "3014eac759d264b1fc34e412e6718076a1ce4dac906025ab38fb16c5c2a28e85"
+    sha256 cellar: :any, arm64_tahoe:       "3014eac759d264b1fc34e412e6718076a1ce4dac906025ab38fb16c5c2a28e85"
+    sha256 cellar: :any, arm64_sequoia:     "3014eac759d264b1fc34e412e6718076a1ce4dac906025ab38fb16c5c2a28e85"
+    sha256 cellar: :any, arm64_linux:       "5d995d17d7aafbaa3da0938fc40eda6c40ddb7827f661921f2a7668cdef1d638"
+    sha256 cellar: :any, x86_64_linux:      "01756330f11062c042c9be0fbf0860d4590591b9ad5921d0565288d122c1bdb8"
   end
 
   depends_on "pkgconf" => :build
