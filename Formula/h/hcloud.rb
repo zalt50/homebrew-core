c08f42e4a831d0a1719755b6719e09ebc4674c74
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-14 18:06:25 +0000

    hcloud: update 1.68.0 bottle.

--- Formula/h/hcloud.rb
+++ Formula/h/hcloud.rb
@@ -7,13 +7,11 @@ class Hcloud < Formula
   head "https://github.com/hetznercloud/cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "1e9eee8b00731e34af1c9b2afc85f7344ad48cbda9849fe30e58226d8725ff38"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "5eb8da390af6ae7079579a487ba7564ff32ff471d919c1c7ae47e853a1dc66cc"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "0856455c3d999394def2b96d11fe7a16c0ef937b500c1e75cc874d2bb2df4f7a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:      "6f460b5dbbc75b9b0d9485bbc11a299cd2b28b5da18e981546ec9bbf440e43f5"
-    sha256 cellar: :any_skip_relocation, sonoma:            "cd40ccf960de97bd692d7ca2d523bd91a7f0355062bb53b3b857ab25bd5e700c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:       "2496bfd05d4074b443938d78423fb679f3ef860ce9b746a766d723410ccc5dde"
-    sha256 cellar: :any,                 x86_64_linux:      "728679684522e708c67612c51d2f363d02c87338a2da5321a510520beb1ea8f1"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "558e11c88e186f4c593cc7f98cd24642b72a752ea3223c5435ec8ced33524cc6"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "75450aff47678cad63d94b691860b3bc3b89004370147b201d062aefc9177afa"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "ab992aaf9702cb0d426f16e76bd3357b1089753f6b69980182113cf04add34b2"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "caa610997abbff7390111da380f10a307380582d276515781683fe9c7bb1935a"
+    sha256 cellar: :any,                 x86_64_linux:      "6136605bb522d853821b5420e8daa84b222ca697a34b649bbdc108c74d77f858"
   end
 
   depends_on "go" => :build
