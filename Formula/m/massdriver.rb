2506dd4525aa3e5b58c85152a7e5d19c434ea795
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-14 05:17:36 +0000

    massdriver 2.4.0

--- Formula/m/massdriver.rb
+++ Formula/m/massdriver.rb
@@ -1,8 +1,8 @@
 class Massdriver < Formula
   desc "Manage applications and infrastructure on Massdriver Cloud"
   homepage "https://www.massdriver.cloud/"
-  url "https://github.com/massdriver-cloud/mass/archive/refs/tags/2.3.0.tar.gz"
-  sha256 "7d8d2eea1145697256591312db5d712af198b4a1877568bff1c298bc7dc57ae5"
+  url "https://github.com/massdriver-cloud/mass/archive/refs/tags/2.4.0.tar.gz"
+  sha256 "a19a3709b39401eaf236323383aae1de1641e06f3d09b7a51b4c262a2ee2a4e6"
   license "Apache-2.0"
   head "https://github.com/massdriver-cloud/mass.git", branch: "main"
 
