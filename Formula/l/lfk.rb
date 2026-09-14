8a1fe82c4f1a5a2541443d00e77d804fde814f0c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-14 20:25:28 +0000

    lfk 0.18.14

--- Formula/l/lfk.rb
+++ Formula/l/lfk.rb
@@ -1,8 +1,8 @@
 class Lfk < Formula
   desc "Terminal user interface for navigating and managing Kubernetes clusters"
   homepage "https://github.com/janosmiko/lfk"
-  url "https://github.com/janosmiko/lfk/archive/refs/tags/v0.18.13.tar.gz"
-  sha256 "29c7e67cfcec97b8ea46a524da60cb058dd25f6a5446e25c622580872e5df01b"
+  url "https://github.com/janosmiko/lfk/archive/refs/tags/v0.18.14.tar.gz"
+  sha256 "8dd840720d04f9ce43f297330397346aea09b437b46175a1ef4859415ccce1b6"
   license "Apache-2.0"
 
   bottle do
