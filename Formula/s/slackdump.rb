cea1b5940d3a9972c7ee2530d586146aef3732f3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-14 05:47:26 +0000

    slackdump 4.4.5

--- Formula/s/slackdump.rb
+++ Formula/s/slackdump.rb
@@ -1,8 +1,8 @@
 class Slackdump < Formula
   desc "Export Slack data without admin privileges"
   homepage "https://github.com/rusq/slackdump"
-  url "https://github.com/rusq/slackdump/archive/refs/tags/v4.4.4.tar.gz"
-  sha256 "14edec13c1b462574ff06dfe548294b76031049a75b4e709ea29411dacb19b4c"
+  url "https://github.com/rusq/slackdump/archive/refs/tags/v4.4.5.tar.gz"
+  sha256 "6c910bb508c0789e65082adc3f8d1930cbb9cb2c27054fbf5e91d2b4a4a0ada0"
   license "AGPL-3.0-only"
   head "https://github.com/rusq/slackdump.git", branch: "master"
 
