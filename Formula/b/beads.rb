ba752da84d76a1884e6ceb4ca7f3c40812d7be50
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-15 19:53:55 +0000

    beads 1.3.0

--- Formula/b/beads.rb
+++ Formula/b/beads.rb
@@ -1,8 +1,8 @@
 class Beads < Formula
   desc "Memory upgrade for your coding agent"
   homepage "https://github.com/gastownhall/beads"
-  url "https://github.com/gastownhall/beads/archive/refs/tags/v1.2.2.tar.gz"
-  sha256 "892b8b641d1f9eb3fa9f0cddf704f3f41aea0da872e546fe623ddec30b2ea9cf"
+  url "https://github.com/gastownhall/beads/archive/refs/tags/v1.3.0.tar.gz"
+  sha256 "849f8b3c8d4e80d170ddef052b732baceb6f28c378edb106dcb097245367849c"
   license "MIT"
   compatibility_version 1
   head "https://github.com/gastownhall/beads.git", branch: "main"
