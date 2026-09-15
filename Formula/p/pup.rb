ba09023a2ce9f9b72047d43d4a0347fddf08e3ff
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-15 02:33:00 +0000

    pup 1.20.0

--- Formula/p/pup.rb
+++ Formula/p/pup.rb
@@ -1,8 +1,8 @@
 class Pup < Formula
   desc "CLI companion with 200+ commands across 33+ Datadog products"
   homepage "https://www.datadoghq.com"
-  url "https://github.com/DataDog/pup/releases/download/v1.19.1/pup_1.19.1_source.tar.gz"
-  sha256 "d6ac2587e0bfccb0967d505122bb3c92f0b688c21fdf23d68066c9d2922fd82c"
+  url "https://github.com/DataDog/pup/releases/download/v1.20.0/pup_1.20.0_source.tar.gz"
+  sha256 "7e7e1f2c3640457132e8f75e74d822a9dd97dc96774ca433af314d9aaca82562"
   license "Apache-2.0"
   head "https://github.com/DataDog/pup.git", branch: "main"
 
