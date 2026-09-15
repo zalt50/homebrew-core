50114fc2cf012d20ee0f79f4be3123949cc40088
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-15 02:58:54 +0000

    uv 0.12.14
    uv: add jemalloc dependency for Linux
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Formula/u/uv.rb
+++ Formula/u/uv.rb
@@ -1,8 +1,8 @@
 class Uv < Formula
   desc "Extremely fast Python package installer and resolver, written in Rust"
   homepage "https://docs.astral.sh/uv/"
-  url "https://github.com/astral-sh/uv/archive/refs/tags/0.12.13.tar.gz"
-  sha256 "6fed06d051682d521335fb26cd422bc652e5e15b87ea87ab13353b89d865fe40"
+  url "https://github.com/astral-sh/uv/archive/refs/tags/0.12.14.tar.gz"
+  sha256 "cf886aac4e27257bfb8863694a377fa3781f179e84b1eee55038b0a0f83ab7a9"
   license any_of: ["Apache-2.0", "MIT"]
   compatibility_version 1
   head "https://github.com/astral-sh/uv.git", branch: "main"
@@ -22,6 +22,10 @@ class Uv < Formula
   uses_from_macos "python" => :test
   uses_from_macos "bzip2"
   uses_from_macos "xz"
+
+  on_linux do
+    depends_on "jemalloc"
+  end
 
   # downloads wheels in test
   allow_network_access! :test
