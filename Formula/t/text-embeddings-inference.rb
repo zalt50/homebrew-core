22be5c9e04933bdd41ad7d498d54cd3ca70ee3cd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-15 13:14:57 +0000

    text-embeddings-inference: update 1.9.4 bottle.

--- Formula/t/text-embeddings-inference.rb
+++ Formula/t/text-embeddings-inference.rb
@@ -6,13 +6,11 @@ class TextEmbeddingsInference < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "3255ddce907b8e88d55c66f1d0ecdf548de3e969b1b54be87d49a071a35b05e4"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "882a2c6914cfa360291086c732b3317d60d125094afae0a10f29c16b878d197c"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "b3598491762db367951dbc74957b075a53700d021b891e593e8a4a37eb7c5488"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:      "141c6b7d4dffc4b5b89c703fcdaf93d855d85a89e81b52abc4805f5284111971"
-    sha256 cellar: :any_skip_relocation, sonoma:            "e91fe44b42fe195c9e78a832f9c39ab85c8b324a06e9ce0057de9f8150039538"
-    sha256 cellar: :any_skip_relocation, arm64_linux:       "f4b101ad37123e4c99697960e4880ce4f53779b6d5d30c5c079ace55c5f7c369"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:      "b54223fcde1cc35c7328489fe2e680d00fdd81469598ce55fbd8ae3b91a4fb77"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "9c3675efe828bfea3923b9f03da240ad9f242115930d272e4334107252dafca9"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "43588f288a133c9147ebffbdb0a6b8f630ca2653fd5f002757ed6c99839c2d18"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "1a25a69f9536564a236ec9f1e1d0d3ae438de36b8297e60a2307778315957ffb"
+    sha256 cellar: :any,                 arm64_linux:       "b3aaa3d02771808fa93c6038859146a61e4f96a27ddc50508151cf9337d3b1aa"
+    sha256 cellar: :any,                 x86_64_linux:      "898c31b43c95d845d2ac00aad83b2889d65f28fa1d94eae5760b6d032da84c97"
   end
 
   depends_on "pkgconf" => :build
