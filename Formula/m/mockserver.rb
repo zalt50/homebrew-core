fde38949c17c407adf8a37afaf3b922d332c20f3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-15 11:54:25 +0000

    mockserver: update 8.0.0 bottle.

--- Formula/m/mockserver.rb
+++ Formula/m/mockserver.rb
@@ -11,7 +11,7 @@ class Mockserver < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "303ae0aa63202c5033fb8ac77f8a7b008dca94d787229eeb23d5a91542493377"
+    sha256 cellar: :any_skip_relocation, all: "20c9fd25fc8524d5ffc01ac663f2dad07c347fb0d781a0fd8fda3c9f113793c4"
   end
 
   depends_on "openjdk"
