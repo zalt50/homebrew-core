4a1a4d45fd1a136c9ea9132f28a6fd0e33f23599
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-15 18:04:42 +0000

    skylighting 0.15

--- Formula/s/skylighting.rb
+++ Formula/s/skylighting.rb
@@ -1,8 +1,8 @@
 class Skylighting < Formula
   desc "Flexible syntax highlighter using KDE XML syntax descriptions"
   homepage "https://github.com/jgm/skylighting"
-  url "https://hackage.haskell.org/package/skylighting-0.14.7/skylighting-0.14.7.tar.gz"
-  sha256 "05df6bce0aba5af7da7b618e1891cbe02833f6810b2405e96c254e9ff741001f"
+  url "https://hackage.haskell.org/package/skylighting-0.15/skylighting-0.15.tar.gz"
+  sha256 "2929c28a042453ee67785201dda234308ef19e068fb474cc1db69dd5f67c4dab"
   license "GPL-2.0-only"
 
   bottle do
