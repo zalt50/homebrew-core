d643308ffdc4b8d1eece3352dc1223a3aa8fcf38
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-15 04:56:38 +0000

    cc-switch-cli 5.10.5

--- Formula/c/cc-switch-cli.rb
+++ Formula/c/cc-switch-cli.rb
@@ -1,8 +1,8 @@
 class CcSwitchCli < Formula
   desc "All-in-one assistant tool for Claude Code, Codex, Gemini, OpenCode and OpenClaw"
   homepage "https://github.com/SaladDay/cc-switch-cli"
-  url "https://github.com/SaladDay/cc-switch-cli/archive/refs/tags/v5.10.4.tar.gz"
-  sha256 "cb10c2742b5552bb4de4cf58663afdf8d79e96e05ea68b5533489a6ba0583dcb"
+  url "https://github.com/SaladDay/cc-switch-cli/archive/refs/tags/v5.10.5.tar.gz"
+  sha256 "995bb09b38534659301d94ac675b7c7e2e860e3bf0ce41d5fc430c76b7b61c06"
   license "MIT"
   head "https://github.com/SaladDay/cc-switch-cli.git", branch: "main"
 
