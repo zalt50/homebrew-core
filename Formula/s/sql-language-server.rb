9ce639c35ecc4babbd851fb04a0fd5fea86d4734
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-15 16:06:47 +0000

    sql-language-server: update 1.7.1_1 bottle.

--- Formula/s/sql-language-server.rb
+++ Formula/s/sql-language-server.rb
@@ -7,12 +7,12 @@ class SqlLanguageServer < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cf25118bc997d51a285ac4cbad9ceb6ca1e45ded96ef52c11834a30bf935d5e0"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1250a7af843e78b2398cdbb217e9625b1f77ecdb695cdc74a173ee377c3f6f25"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8191b922c90af4dbe2e3fce69ddf818643bd12c8ced97e454a7c0bd28e6458ea"
-    sha256 cellar: :any_skip_relocation, sonoma:        "c24bb22b07a5ba4ce0a6ee838fc5976c7bc50a7d51880f9694ad421fe5faaeb3"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "00eeaa0a119c6512fea47329da49f21c6c04f6d7832eac78d46e0f74f3339d7a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "07b1cdb55cc9429d8f2d34f30bfe5103c93efce6752d4f76cf35053616af74c0"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "90f006d732780a385cdebbdf4c195f5e66a35f43c976552a95ee8a5f155ccbbf"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "90f006d732780a385cdebbdf4c195f5e66a35f43c976552a95ee8a5f155ccbbf"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "90f006d732780a385cdebbdf4c195f5e66a35f43c976552a95ee8a5f155ccbbf"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "c99d2ded8d6c79b2e93fd2ea3d65ce7d2a61d8c248f70dc86db6dfe5f2692193"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:      "c99d2ded8d6c79b2e93fd2ea3d65ce7d2a61d8c248f70dc86db6dfe5f2692193"
   end
 
   depends_on "node"
