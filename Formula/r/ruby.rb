d13ffbbb5bdad6023d6153587889b76f08e3f06a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-15 05:39:54 +0000

    ruby 4.0.7

--- Formula/r/ruby.rb
+++ Formula/r/ruby.rb
@@ -2,20 +2,19 @@ class Ruby < Formula
   desc "Powerful, clean, object-oriented scripting language"
   homepage "https://www.ruby-lang.org/"
   license "Ruby"
-  revision 1
   compatibility_version 1
 
   stable do
     # TODO: enable default_user_install when updating to Ruby 4.1
-    url "https://cache.ruby-lang.org/pub/ruby/4.0/ruby-4.0.6.tar.gz"
-    sha256 "837d299e8f7ddf2be31a229a7a7e019d354979825117989acb3b32b1a9be262a"
+    url "https://cache.ruby-lang.org/pub/ruby/4.0/ruby-4.0.7.tar.gz"
+    sha256 "911ace20f90d068ca0e4dda6d0e4f0f81e52e52f2dd4f4004c721e253412e82d"
 
     # Should be updated only when Ruby is updated (if an update is available).
     # The exception is Rubygem security fixes, which mandate updating this
     # formula & the versioned equivalents and bumping the revisions.
     resource "rubygems" do
-      url "https://rubygems.org/rubygems/rubygems-4.0.16.tgz"
-      sha256 "ea9c669526af82874f8f33f69bea1b6ddd99283756e598227a9a890035a5a06a"
+      url "https://rubygems.org/rubygems/rubygems-4.0.20.tgz"
+      sha256 "177c816f9952419dbfd1f56ef93e4a9d1fdc60d885c6ca0787283485b0c05408"
 
       livecheck do
         url "https://rubygems.org/pages/download"
