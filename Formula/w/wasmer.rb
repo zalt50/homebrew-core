1d7274b4eb3f01eaca7ff6403e806967159a5414
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-15 21:22:40 +0000

    wasmer 7.4.2

--- Formula/w/wasmer.rb
+++ Formula/w/wasmer.rb
@@ -2,8 +2,8 @@ class Wasmer < Formula
   desc "Universal WebAssembly Runtime"
   homepage "https://wasmer.io"
   url "https://github.com/wasmerio/wasmer.git",
-    tag:      "v7.4.1",
-    revision: "df29aa22c6159a147f992375f5670edf77f8d6a0"
+    tag:      "v7.4.2",
+    revision: "7a48a071c7682a409d148cf37dc8da58322a123d"
   license "MIT"
   head "https://github.com/wasmerio/wasmer.git", branch: "main"
 
