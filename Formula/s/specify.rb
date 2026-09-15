3a745fab13e9001e6ef2778e039fa234611f0ce3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-15 14:58:05 +0000

    specify 1.0.7

--- Formula/s/specify.rb
+++ Formula/s/specify.rb
@@ -3,8 +3,8 @@ class Specify < Formula
 
   desc "Toolkit to help you get started with Spec-Driven Development"
   homepage "https://github.github.com/spec-kit/"
-  url "https://github.com/github/spec-kit/archive/refs/tags/v1.0.6.tar.gz"
-  sha256 "31c5db62974a943b68304ef881c35788e0a165d2d26e2bbb5a7aba6fef4b8045"
+  url "https://github.com/github/spec-kit/archive/refs/tags/v1.0.7.tar.gz"
+  sha256 "6957b08053a315211644a4a52fc41c4062e3f6306db0ea32a159c8f5c1a404c0"
   license "MIT"
 
   bottle do
