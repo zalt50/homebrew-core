180c7edb46c1d6d1f5a75c0f4aaae8d9d40b04a6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-15 23:03:41 +0000

    eas-cli 24.4.0

--- Formula/e/eas-cli.rb
+++ Formula/e/eas-cli.rb
@@ -1,8 +1,8 @@
 class EasCli < Formula
   desc "Command-line tool for working with Expo Application Services"
   homepage "https://docs.expo.dev/eas/"
-  url "https://registry.npmjs.org/eas-cli/-/eas-cli-24.3.0.tgz"
-  sha256 "ab2c8f48015f15b6a6595901cd2ababd54e985f9dd792a5d5bedcc325743200c"
+  url "https://registry.npmjs.org/eas-cli/-/eas-cli-24.4.0.tgz"
+  sha256 "80fae7f11eee400cfc5cd8fdeb948fc59917759b12b95cab0b3deddc872404e2"
   license "MIT"
 
   bottle do
