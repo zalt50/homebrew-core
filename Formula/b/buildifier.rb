a66a0e9b13bbca7a4e885d4cf93996f27f3730b0
Štefan Baebler <319826+stefanb@users.noreply.github.com>
2026-09-15 14:02:22 +0000

    buildifier 10.0.1

--- Formula/b/buildifier.rb
+++ Formula/b/buildifier.rb
@@ -1,8 +1,8 @@
 class Buildifier < Formula
   desc "Format bazel BUILD files with a standard convention"
   homepage "https://github.com/bazelbuild/buildtools"
-  url "https://github.com/bazelbuild/buildtools/archive/refs/tags/v8.5.1.tar.gz"
-  sha256 "f3b800e9f6ca60bdef3709440f393348f7c18a29f30814288a7326285c80aab9"
+  url "https://github.com/bazelbuild/buildtools/archive/refs/tags/v10.0.1.tar.gz"
+  sha256 "7914e09ee966e7498c4a0c365590f555c741c24b1dee022f60a2284036c2653a"
   license "Apache-2.0"
   head "https://github.com/bazelbuild/buildtools.git", branch: "main"
 
