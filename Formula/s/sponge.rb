99cf48543e3de50fdbdcac0b10890794f621fe8b
Michael Cho <michael@michaelcho.dev>
2026-09-14 21:52:52 -0400

    sponge: use Debian source recommended by homepage

--- Formula/s/sponge.rb
+++ Formula/s/sponge.rb
@@ -1,8 +1,8 @@
 class Sponge < Formula
   desc "Soak up standard input and write to a file"
   homepage "https://joeyh.name/code/moreutils/"
-  url "https://git.joeyh.name/index.cgi/moreutils.git/snapshot/moreutils-0.70.tar.gz"
-  sha256 "f2bf46d410ba567cc8d01507e94916994e48742722e690dc498fab59f5250132"
+  url "https://deb.debian.org/debian/pool/main/m/moreutils/moreutils_0.70.orig.tar.xz"
+  sha256 "a844c5e3360a73d12c0a5624750ecc1969d64afea2e84925328f137576e2eb55"
   license "GPL-2.0-only"
   head "https://git.joeyh.name/git/moreutils.git", branch: "master"
 
