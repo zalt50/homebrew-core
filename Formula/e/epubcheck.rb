453e531850d092cafd9565c363bd73b91672adb3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-15 11:00:42 +0000

    epubcheck 5.4.0

--- Formula/e/epubcheck.rb
+++ Formula/e/epubcheck.rb
@@ -1,8 +1,8 @@
 class Epubcheck < Formula
   desc "Validate EPUB files, version 2.0 and later"
   homepage "https://www.w3.org/publishing/epubcheck/"
-  url "https://github.com/w3c/epubcheck/releases/download/v5.3.0/epubcheck-5.3.0.zip"
-  sha256 "6c07e68584b2e2ce2f89fe06e1246dfead3eb36b46b340e7d93524f29dcff6c5"
+  url "https://github.com/w3c/epubcheck/releases/download/v5.4.0/epubcheck-5.4.0.zip"
+  sha256 "33350c61038e71dfb3d45a76aed04bf5481e6d5500cb780f6e98db8bbd15a28c"
   license "BSD-3-Clause"
 
   bottle do
