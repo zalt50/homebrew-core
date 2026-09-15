407ca6ec3b3cd4098454754a0c67713a05da8be1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-15 19:52:54 +0000

    aws-lc 5.9.0

--- Formula/a/aws-lc.rb
+++ Formula/a/aws-lc.rb
@@ -1,8 +1,8 @@
 class AwsLc < Formula
   desc "General-purpose cryptographic library"
   homepage "https://github.com/aws/aws-lc"
-  url "https://github.com/aws/aws-lc/archive/refs/tags/v5.8.0.tar.gz"
-  sha256 "04d9aa258641265099a3b1fbd37a21866e942943e16619087bf79c0b6e08f64e"
+  url "https://github.com/aws/aws-lc/archive/refs/tags/v5.9.0.tar.gz"
+  sha256 "f7b7b1a85bcc30496b98173257d69cc2990ae85d380b642ebe0b8cb251876d4f"
   license all_of: ["Apache-2.0", "ISC", "OpenSSL", "MIT", "BSD-3-Clause"]
 
   livecheck do
