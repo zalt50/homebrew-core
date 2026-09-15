ccaf5ccec73e9c3449881a08d01e0c07ab8c4a56
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-15 11:25:58 +0000

    packetq 1.7.4

--- Formula/p/packetq.rb
+++ Formula/p/packetq.rb
@@ -1,8 +1,8 @@
 class Packetq < Formula
   desc "SQL-like frontend to PCAP files"
   homepage "https://www.dns-oarc.net/tools/packetq"
-  url "https://www.dns-oarc.net/files/packetq/packetq-1.7.3.tar.gz"
-  sha256 "faa9a3700bf6010347fbfa595b7777d32059a77abbb027f6e070b419369d7718"
+  url "https://www.dns-oarc.net/files/packetq/packetq-1.7.4.tar.gz"
+  sha256 "9e2e72a37db9449aa0cae997b35ba7b80e4c1a88fc646e2299fd823769824929"
   license "GPL-3.0-or-later"
 
   livecheck do
