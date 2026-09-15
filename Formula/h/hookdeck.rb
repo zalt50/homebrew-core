6c97a51d42d6e0038093c07a18169cb2ff40835f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-15 11:08:58 +0000

    hookdeck 2.6.0

--- Formula/h/hookdeck.rb
+++ Formula/h/hookdeck.rb
@@ -1,8 +1,8 @@
 class Hookdeck < Formula
   desc "Forward webhook events from Hookdeck to a local server"
   homepage "https://hookdeck.com"
-  url "https://github.com/hookdeck/hookdeck-cli/archive/refs/tags/v2.5.0.tar.gz"
-  sha256 "16b421f3af652ebbea24e445815a750cae51584bc8bd069c2ffaad718b69076c"
+  url "https://github.com/hookdeck/hookdeck-cli/archive/refs/tags/v2.6.0.tar.gz"
+  sha256 "141af8ff0bdb357310cb8662e5107a3b9928a4a44ce361a1928d65947d5e7383"
   license "Apache-2.0"
   head "https://github.com/hookdeck/hookdeck-cli.git", branch: "main"
 
