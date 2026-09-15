51c9a9cceec1779dcdb5ddd8fa5236c7bcc744a5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-15 17:12:11 +0000

    gup 1.9.3

--- Formula/g/gup.rb
+++ Formula/g/gup.rb
@@ -1,8 +1,8 @@
 class Gup < Formula
   desc "Update binaries installed by go install"
   homepage "https://github.com/nao1215/gup"
-  url "https://github.com/nao1215/gup/archive/refs/tags/v1.9.2.tar.gz"
-  sha256 "0fd240e3e1794a1109914327f8707aeffa4ad7f758aad8bd4c0ea340add2cc4b"
+  url "https://github.com/nao1215/gup/archive/refs/tags/v1.9.3.tar.gz"
+  sha256 "8ad29a1dff6c0d78da82199709bc913ce9a7bcc8a8057166e0f6368e4d50c19f"
   license "Apache-2.0"
   head "https://github.com/nao1215/gup.git", branch: "main"
 
