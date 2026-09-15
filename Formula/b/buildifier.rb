4e1729f634072e36942d248b4acfd51392350bf0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-15 14:19:06 +0000

    buildifier: update 10.0.1 bottle.

--- Formula/b/buildifier.rb
+++ Formula/b/buildifier.rb
@@ -7,13 +7,11 @@ class Buildifier < Formula
   head "https://github.com/bazelbuild/buildtools.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "40e74095c993f35604dfc623f99570c7c1468983eda31ee4a7d80bff8513bb8b"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "2e4e70597b1f5e45ce815ad0612fd3a93e046502a1e1e38116b7632a54bcf50d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "2e4e70597b1f5e45ce815ad0612fd3a93e046502a1e1e38116b7632a54bcf50d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:      "2e4e70597b1f5e45ce815ad0612fd3a93e046502a1e1e38116b7632a54bcf50d"
-    sha256 cellar: :any_skip_relocation, sonoma:            "1f5af17e2918b7cb2eebdd8f7965115716a837fc398182d4bd9dc123ff1bd0cd"
-    sha256 cellar: :any_skip_relocation, arm64_linux:       "e6aa077cbc9cbdefc7e8bc774b9d1b9322b4dac24f035da678f3bd42a84690ad"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:      "7db8cf0a58bbad28267d15f4d8219de4098115afd38180586e4491d555c72eea"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "e4e471f61c74d0a8a61117434d5a738d82f83032f0c930da4b5d2d771db39cf1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "e4e471f61c74d0a8a61117434d5a738d82f83032f0c930da4b5d2d771db39cf1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "e4e471f61c74d0a8a61117434d5a738d82f83032f0c930da4b5d2d771db39cf1"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "6dfdcbd0a11a667b54c93554b937d6fd30dce26ff27098eff0cadfa6d18e2857"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:      "3a1879d2fe115560db96dd40e87cf017c7cdaf5f8fa1aff35ca69019118d01df"
   end
 
   depends_on "go" => :build
