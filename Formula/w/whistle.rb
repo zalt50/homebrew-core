026079e7280997ebccb945cf2a23ff4ff4c6842e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-15 15:06:58 +0000

    whistle 2.10.10

--- Formula/w/whistle.rb
+++ Formula/w/whistle.rb
@@ -1,8 +1,8 @@
 class Whistle < Formula
   desc "HTTP, HTTP2, HTTPS, Websocket debugging proxy"
   homepage "https://wproxy.org/"
-  url "https://registry.npmjs.org/whistle/-/whistle-2.10.9.tgz"
-  sha256 "1f15b5691ce97271fe4363d8c9e3d3eb413504158321fca00a8cbec40f22254e"
+  url "https://registry.npmjs.org/whistle/-/whistle-2.10.10.tgz"
+  sha256 "f04b0744547cffc03aa18efb56949db3bdbfc787466b7773246fd7d5aa347def"
   license "MIT"
 
   bottle do
