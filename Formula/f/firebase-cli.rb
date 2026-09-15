62243804dd1a0026649067d4c11520ac5205e034
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-15 23:21:49 +0000

    firebase-cli: update 15.30.1 bottle.

--- Formula/f/firebase-cli.rb
+++ Formula/f/firebase-cli.rb
@@ -6,11 +6,11 @@ class FirebaseCli < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_golden_gate: "82e2ab63bc2f5199c5e2e387d6d0fc5dfb9027b5c4e8570bdab2198f7b9cdf81"
-    sha256 cellar: :any, arm64_tahoe:       "82e2ab63bc2f5199c5e2e387d6d0fc5dfb9027b5c4e8570bdab2198f7b9cdf81"
-    sha256 cellar: :any, arm64_sequoia:     "82e2ab63bc2f5199c5e2e387d6d0fc5dfb9027b5c4e8570bdab2198f7b9cdf81"
-    sha256 cellar: :any, arm64_linux:       "2dc0beae76a87c2f32f06e7e6e5c220cde4fe1872d171589defdf21a75e9742d"
-    sha256 cellar: :any, x86_64_linux:      "9d4516b869e1c6b31f4e417893c1c2a776ff6d2870aab83fa06dce7034bf6817"
+    sha256 cellar: :any, arm64_golden_gate: "08be83aefe21d60de3523ddf8e2369545cec4631cf44b38d9380180e3fa1f87e"
+    sha256 cellar: :any, arm64_tahoe:       "08be83aefe21d60de3523ddf8e2369545cec4631cf44b38d9380180e3fa1f87e"
+    sha256 cellar: :any, arm64_sequoia:     "08be83aefe21d60de3523ddf8e2369545cec4631cf44b38d9380180e3fa1f87e"
+    sha256 cellar: :any, arm64_linux:       "1088e6771e0742fd2c6a0546c738ab021ebccafd8a3cff6d1220ec11c486c698"
+    sha256 cellar: :any, x86_64_linux:      "53979cf34559bc78a4b0869ae57bc44b8dd07ce7734be01bca63b55f68664836"
   end
 
   depends_on "node"
