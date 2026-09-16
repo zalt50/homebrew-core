ffc13d22ecc2977e827681893faa99234cf9777c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-16 13:54:39 +0000

    caesiumclt 1.5.0

--- Formula/c/caesiumclt.rb
+++ Formula/c/caesiumclt.rb
@@ -1,8 +1,8 @@
 class Caesiumclt < Formula
   desc "Fast and efficient lossy and/or lossless image compression tool"
   homepage "https://github.com/Lymphatus/caesium-clt"
-  url "https://github.com/Lymphatus/caesium-clt/archive/refs/tags/v1.4.0.tar.gz"
-  sha256 "3dc4bc062536fcaa368d044aea873afd695feeabd9310d6118a7d45b2f0ad52e"
+  url "https://github.com/Lymphatus/caesium-clt/archive/refs/tags/v1.5.0.tar.gz"
+  sha256 "c170c4d1ab1179b3757ec39f00c08250b121195e07f013fcbf9f3952db333a05"
   license "Apache-2.0"
 
   bottle do
