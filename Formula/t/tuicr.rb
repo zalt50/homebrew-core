979bfe502dc7ed4ee799b1cec5e99488033b75eb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-16 00:05:53 +0000

    tuicr 0.26.0

--- Formula/t/tuicr.rb
+++ Formula/t/tuicr.rb
@@ -1,8 +1,8 @@
 class Tuicr < Formula
   desc "Code review TUI with vim keybindings"
   homepage "https://tuicr.dev/"
-  url "https://github.com/agavra/tuicr/archive/refs/tags/v0.25.0.tar.gz"
-  sha256 "e7553c629d89c3fae2845a21bddf365cc542e0d2f2eed01e2fb5ad7017bd81fc"
+  url "https://github.com/agavra/tuicr/archive/refs/tags/v0.26.0.tar.gz"
+  sha256 "2ab1e5989b3f5a8b4a1b82734f69da367b45a68cd033edee02131fbf0642a802"
   license "MIT"
   head "https://github.com/agavra/tuicr.git", branch: "main"
 
