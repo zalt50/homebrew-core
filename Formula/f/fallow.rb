8b097e708279dbf4255fea7c7538873d73991051
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-16 02:08:00 +0000

    fallow 3.26.0

--- Formula/f/fallow.rb
+++ Formula/f/fallow.rb
@@ -1,8 +1,8 @@
 class Fallow < Formula
   desc "Codebase intelligence for TypeScript and JavaScript"
   homepage "https://docs.fallow.tools"
-  url "https://github.com/fallow-rs/fallow/archive/refs/tags/v3.25.0.tar.gz"
-  sha256 "b9b106320cda4481e4c047103a5c32e19669f4338307fd8daba20b4ce3173d52"
+  url "https://github.com/fallow-rs/fallow/archive/refs/tags/v3.26.0.tar.gz"
+  sha256 "5919a72ffd0b674fcaa665e24f84e4a13dbc622aabe4e97a835fc5c752b9617c"
   license "MIT"
   head "https://github.com/fallow-rs/fallow.git", branch: "main"
 
