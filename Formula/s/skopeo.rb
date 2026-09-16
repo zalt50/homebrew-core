c74925469dcc1d0d06ff7245e401b2be8a666e03
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-16 14:50:38 +0000

    skopeo 1.24.1

--- Formula/s/skopeo.rb
+++ Formula/s/skopeo.rb
@@ -1,8 +1,8 @@
 class Skopeo < Formula
   desc "Work with remote images registries"
   homepage "https://github.com/podman-container-tools/skopeo"
-  url "https://github.com/podman-container-tools/skopeo/archive/refs/tags/v1.24.0.tar.gz"
-  sha256 "1eea9364e344c4d6cb398fac53bc387663d19690552003b43c75819e8bb55527"
+  url "https://github.com/podman-container-tools/skopeo/archive/refs/tags/v1.24.1.tar.gz"
+  sha256 "6a3f342a327ec7684198eb0e15114940b6101506db48d9a96590f5942cb3b335"
   license "Apache-2.0"
 
   bottle do
