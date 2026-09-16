e50677aef7a161892ee814186626de0b89606b1e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-16 02:55:12 +0000

    ty 0.0.81

--- Formula/t/ty.rb
+++ Formula/t/ty.rb
@@ -1,8 +1,8 @@
 class Ty < Formula
   desc "Extremely fast Python type checker, written in Rust"
   homepage "https://docs.astral.sh/ty/"
-  url "https://files.pythonhosted.org/packages/54/b0/6d1b10e0d422736a3c439e487c950ae785f401d71ff879d5be68bccb6d90/ty-0.0.80.tar.gz"
-  sha256 "fe86bc91327e45ff5e3593b7e306e7f57a44bc0608f38d647b8d97bd99c96013"
+  url "https://files.pythonhosted.org/packages/61/b7/c9d736f48585f5a711ea47bb97a353d3771834f89481d747ea9687b74fa9/ty-0.0.81.tar.gz"
+  sha256 "ef721aa649bf41d665ba86e1ea726fd3feab6800e2c4887a062a704baf304ca8"
   license "MIT"
   head "https://github.com/astral-sh/ty.git", branch: "main"
 
