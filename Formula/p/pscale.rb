ccf17ac9641f89268babb98643c8c92fb2155014
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-16 20:39:42 +0000

    pscale 0.335.0

--- Formula/p/pscale.rb
+++ Formula/p/pscale.rb
@@ -1,8 +1,8 @@
 class Pscale < Formula
   desc "CLI for PlanetScale Database"
   homepage "https://www.planetscale.com/"
-  url "https://github.com/planetscale/cli/archive/refs/tags/v0.334.0.tar.gz"
-  sha256 "cad86698394fdff78672e4078fb57a31dfdb11ff599afa6baf844ebb2fccce53"
+  url "https://github.com/planetscale/cli/archive/refs/tags/v0.335.0.tar.gz"
+  sha256 "d93e4a1ff4c0953a4b23c042de3e69f8ff9bf8ad74885fc2950c19b39c53ef73"
   license "Apache-2.0"
   head "https://github.com/planetscale/cli.git", branch: "main"
 
