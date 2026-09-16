37c4c06702c3de6644b9e9dfd6d58d0344decd3c
Michael Cho <michael@michaelcho.dev>
2026-09-16 11:13:26 -0400

    neovide: use `llvm@22` clang when running bindgen

--- Formula/n/neovide.rb
+++ Formula/n/neovide.rb
@@ -24,6 +24,8 @@ class Neovide < Formula
 
   on_macos do
     depends_on "cargo-bundle" => :build
+    # work around https://github.com/rust-skia/rust-skia/issues/1331
+    depends_on "llvm@22" => :build if DevelopmentTools.clang_build_version >= 2100
   end
 
   on_linux do
@@ -59,6 +61,9 @@ class Neovide < Formula
       # GN doesn't use CFLAGS so pass extra paths using superenv
       ENV.append_path "HOMEBREW_INCLUDE_PATHS", formula_opt_include("freetype")/"freetype2"
       ENV.append_path "HOMEBREW_INCLUDE_PATHS", formula_opt_include("harfbuzz")/"harfbuzz"
+    elsif DevelopmentTools.clang_build_version >= 2100
+      # Work around https://github.com/rust-skia/rust-skia/issues/1331
+      ENV["CLANG_PATH"] = formula_opt_bin("llvm@22")/"clang"
     end
 
     system "cargo", "install", *std_cargo_args
@@ -82,6 +87,9 @@ class Neovide < Formula
   end
 
   test do
+    assert_match version.to_s, shell_output("#{bin}/neovide --version")
+    return if OS.mac? # unable to start neovide within macOS sandbox
+
     socket = testpath/"nvim.sock"
     nvim_cmd = ["nvim", "--headless", "-i", "NONE", "-u", "NONE", "--listen", socket]
     ohai nvim_cmd.join(" ")
@@ -89,15 +97,26 @@ class Neovide < Formula
 
     sleep 1 until socket.exist? && socket.socket?
 
-    neovide_cmd = [bin/"neovide", "--no-fork", "--server=#{socket}"]
-    neovide_cmd.unshift(formula_opt_bin("xorg-server")/"xvfb-run") if OS.linux? && ENV.exclude?("DISPLAY")
-    ohai neovide_cmd.join(" ")
-    neovide_pid = spawn(*neovide_cmd)
+    IO.pipe do |read_io, write_io|
+      xvfb = formula_opt_bin("xorg-server")/"Xvfb"
+      xvfb_pid = spawn(xvfb, "-displayfd", write_io.fileno.to_s, write_io => write_io)
+      write_io.close
+      ENV["DISPLAY"] = ":#{read_io.read.strip}"
 
-    sleep 1 until nvim_ui_count(socket).positive?
-    system "nvim", "--server", socket, "--remote-send", ":q<CR>"
+      neovide_cmd = [bin/"neovide", "--no-fork", "--server=#{socket}"]
+      ohai neovide_cmd.join(" ")
+      neovide_pid = spawn(*neovide_cmd)
 
-    Process.wait nvim_pid
-    Process.wait neovide_pid
+      sleep 1 until nvim_ui_count(socket).positive?
+      system "nvim", "--server", socket, "--remote-send", ":q<CR>"
+
+      Process.wait nvim_pid
+      Process.wait neovide_pid
+    ensure
+      if xvfb_pid
+        Process.kill "TERM", xvfb_pid
+        Process.wait xvfb_pid
+      end
+    end
   end
 end
