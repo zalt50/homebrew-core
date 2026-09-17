2a8536ae666058d7b09079fc0698f2d395fde6fe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-17 08:23:55 +0000

    prettier 3.9.7

--- Formula/p/prettier.rb
+++ Formula/p/prettier.rb
@@ -1,8 +1,8 @@
 class Prettier < Formula
   desc "Code formatter for JavaScript, CSS, JSON, GraphQL, Markdown, YAML"
   homepage "https://prettier.io/"
-  url "https://registry.npmjs.org/prettier/-/prettier-3.9.6.tgz"
-  sha256 "997da95cf2ae81053cafc79ef122a6e8dc12e3f2c619d57eb1f2e19525fb212f"
+  url "https://registry.npmjs.org/prettier/-/prettier-3.9.7.tgz"
+  sha256 "57a7b2b4c760cb8b365584826713f44f8efa51398a0c597513b6947ea6c9add3"
   license "MIT"
   head "https://github.com/prettier/prettier.git", branch: "main"
 
