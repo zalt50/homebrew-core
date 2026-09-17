11db8ce83cab87d24874a030cd556984c3b0210b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-17 22:53:08 +0000

    bashka 0.11.0

--- Formula/b/bashka.rb
+++ Formula/b/bashka.rb
@@ -1,8 +1,8 @@
 class Bashka < Formula
   desc "Static verification of installation bash scripts"
   homepage "https://github.com/dmtrKovalenko/bashka"
-  url "https://github.com/dmtrKovalenko/bashka/archive/refs/tags/v0.9.0.tar.gz"
-  sha256 "dd3150e9029164be08b55d22f828d1bf989b5b4ced25cd665478f89558ed8849"
+  url "https://github.com/dmtrKovalenko/bashka/archive/refs/tags/v0.11.0.tar.gz"
+  sha256 "d25f80f00132287cd2c32d880848fffa14c648c838f3bd5bbd4b47e06f471200"
   license "MIT"
   head "https://github.com/dmtrKovalenko/bashka.git", branch: "main"
 
