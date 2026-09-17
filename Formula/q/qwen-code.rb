fee43a8d0881ff70b7b7eb4b261bb02f0f1b1b57
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-17 16:16:31 +0000

    qwen-code: update 0.24.0 bottle.

--- Formula/q/qwen-code.rb
+++ Formula/q/qwen-code.rb
@@ -6,11 +6,11 @@ class QwenCode < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any, arm64_golden_gate: "de984bee0d2858510b3f81cf27188492d903c72a38792f179a061245daafa4ce"
-    sha256 cellar: :any, arm64_tahoe:       "de984bee0d2858510b3f81cf27188492d903c72a38792f179a061245daafa4ce"
-    sha256 cellar: :any, arm64_sequoia:     "de984bee0d2858510b3f81cf27188492d903c72a38792f179a061245daafa4ce"
-    sha256 cellar: :any, arm64_linux:       "9d60e284c3b768bfef1c93cc4755547f10ee27195fd746eb19a5c6a40dfa2c28"
-    sha256 cellar: :any, x86_64_linux:      "23986831f0ef57da66b0fa236cafe6e852d2293cb209d051f9382837960380a5"
+    sha256 cellar: :any, arm64_golden_gate: "1697f03796861c845f63f981cce7552114ddd608bde840bfac287ea97405d50c"
+    sha256 cellar: :any, arm64_tahoe:       "1697f03796861c845f63f981cce7552114ddd608bde840bfac287ea97405d50c"
+    sha256 cellar: :any, arm64_sequoia:     "1697f03796861c845f63f981cce7552114ddd608bde840bfac287ea97405d50c"
+    sha256 cellar: :any, arm64_linux:       "e9b1246c4374bc2093926e49fd5be40aef08010ee0a1e5d8a4690b3dd92edb2c"
+    sha256 cellar: :any, x86_64_linux:      "788cfd35b919ad2775487ac652d1a475e738e690087a0ec8d518be39194a4a16"
   end
 
   depends_on "node"
