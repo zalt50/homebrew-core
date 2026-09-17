d672f2cb7d869767eda017818d7c8dc182ae8869
botantony <antonsm21@gmail.com>
2026-09-17 18:16:41 +0200

    openexr: revision bump (openjph 0.32.0)

--- Formula/o/openexr.rb
+++ Formula/o/openexr.rb
@@ -4,6 +4,7 @@ class Openexr < Formula
   url "https://github.com/AcademySoftwareFoundation/openexr/archive/refs/tags/v3.4.15.tar.gz"
   sha256 "445ed5b0ea4d9cf98be3a4f219e419628b123b61dec65ccb743ab9b07fbebdaa"
   license "BSD-3-Clause"
+  revision 1
   compatibility_version 1
 
   bottle do
