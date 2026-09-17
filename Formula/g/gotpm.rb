a48b2b85235ee180372ebfeb68845663334a6ccb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-17 02:18:02 +0000

    gotpm 0.4.10

--- Formula/g/gotpm.rb
+++ Formula/g/gotpm.rb
@@ -1,8 +1,8 @@
 class Gotpm < Formula
   desc "CLI for using TPM 2.0"
   homepage "https://github.com/google/go-tpm-tools"
-  url "https://github.com/google/go-tpm-tools/archive/refs/tags/v0.4.9.tar.gz"
-  sha256 "a2eb3739afa65b60c351550c6c0541f17c5af6a22837da3690fcd6e44ef354bf"
+  url "https://github.com/google/go-tpm-tools/archive/refs/tags/v0.4.10.tar.gz"
+  sha256 "a40ecfff3222c9303c4fd0fb7aae9aa60f74bb7a7c649df661daaf5db47c6f80"
   license "Apache-2.0"
   head "https://github.com/google/go-tpm-tools.git", branch: "main"
 
