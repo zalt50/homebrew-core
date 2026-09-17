37ff4e579509097e09a09c4c45130fb4a7b4d405
Michael Cho <michael@michaelcho.dev>
2026-09-17 16:09:56 -0400

    lz4: install cmake config

--- Formula/l/lz4.rb
+++ Formula/l/lz4.rb
@@ -23,12 +23,26 @@ class Lz4 < Formula
     sha256 cellar: :any, x86_64_linux:      "cbfa3337697c4585b2f0cbe679af7802046c36cd9162f519af89b33c3d52b4e0"
   end
 
+  depends_on "cmake" => :build
+
   deny_network_access!
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
     # Prevent dependents from hardcoding Cellar paths.
     inreplace lib/"pkgconfig/liblz4.pc", prefix, opt_prefix
+
+    # We use CMake for package configuration files. These are currently needed to build `tiledb`.
+    # The Makefile is used for everything else as official build system and installs multi-threaded CLI.
+    ENV["DESTDIR"] = buildpath
+    system "cmake", "-S", "build/cmake", "-B", "build",
+                    "-DBUILD_SHARED_LIBS=ON",
+                    "-DBUILD_STATIC_LIBS=ON", # for LZ4::lz4_static target
+                    "-DLZ4_BUILD_CLI=OFF",
+                    *std_cmake_args
+    system "cmake", "--build", "build" # needed to run `--install` which rewrites build path in .cmake file
+    system "cmake", "--install", "build"
+    lib.install File.join(buildpath, lib, "cmake")
   end
 
   test do
@@ -37,5 +51,8 @@ class Lz4 < Formula
     refute_empty compressed
     decompressed = pipe_output("#{bin}/lz4 -d", compressed)
     assert_equal decompressed, input
+
+    # Make sure lz4 executable is built multi-threaded
+    assert_match "multithread", shell_output("#{bin}/lz4 -V")
   end
 end
