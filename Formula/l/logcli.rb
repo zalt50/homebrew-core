b3dd966c9247b7b1e44007450b86351b7d7b74b3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-17 18:01:19 +0000

    logcli: update 3.7.8 bottle.

--- Formula/l/logcli.rb
+++ Formula/l/logcli.rb
@@ -11,12 +11,11 @@ class Logcli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "742d68ae76b41e2924268532bd1d54860dad77392f7f5a2d35b7b81ff56da230"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "4b684ea41575dfcd24408d5e31def521be4aef604ceae8120aba7f482c7c2d60"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "dbe3979cb8530c6c914c7d892a2530faaeab8523d8d60f0faf2deae3796a2e04"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:      "3450cba9e453a6978d86fe44dc4b973f427384477f7a1b284b1fd014223b55b8"
-    sha256 cellar: :any_skip_relocation, arm64_linux:       "162ee00d38cd7b3cc161488b66cea64e53dd8845d422a25799824d3ca316eece"
-    sha256 cellar: :any,                 x86_64_linux:      "1d55b28c1f9207d3cac3afbb8282891612c4c538e07db16b184dd294e01c6203"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "72c99f82bafcfe71ebdc95f75c2bed6ad32e894d0134e33dd7f5681f4986c8f9"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "2771a20e92d6ea7f59d4bde4c9fad1a2ed50ece8b75f8b36c81e964388065fe3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "6de3cc2803bfef2a910bd9df805f7b09c11601531acbf52732e0a55cb02bcccd"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "1a799da2a42fbe65ae43abbe56bb3d90fc2b1d50c86d1e5db6b5a86776cddf96"
+    sha256 cellar: :any,                 x86_64_linux:      "e81918580815560b083b39551b430d195d01057e52b7472c5cee7a004e12671f"
   end
 
   depends_on "go" => :build
