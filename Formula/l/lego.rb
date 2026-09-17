a234230bb4cee6678329dab59b61f455d70d2f5f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-17 23:17:30 +0000

    lego 5.5.1

--- Formula/l/lego.rb
+++ Formula/l/lego.rb
@@ -1,8 +1,8 @@
 class Lego < Formula
   desc "Let's Encrypt client and ACME library"
   homepage "https://go-acme.github.io/lego/"
-  url "https://github.com/go-acme/lego/archive/refs/tags/v5.5.0.tar.gz"
-  sha256 "f35b85add7f25d8219993eda320e9f5947a6b266e464c92acc993aef985591ad"
+  url "https://github.com/go-acme/lego/archive/refs/tags/v5.5.1.tar.gz"
+  sha256 "9735d946aa00522764d08fb56c1483f2da42f0ebd0bc1eebffc35e4b89c289ee"
   license "MIT"
   head "https://github.com/go-acme/lego.git", branch: "main"
 
