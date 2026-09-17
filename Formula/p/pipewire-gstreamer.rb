4fec3ccecff7d1105404cf62915921ba41370f3e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-17 11:32:30 +0000

    pipewire-gstreamer 1.6.9

--- Formula/p/pipewire-gstreamer.rb
+++ Formula/p/pipewire-gstreamer.rb
@@ -1,8 +1,8 @@
 class PipewireGstreamer < Formula
   desc "GStreamer Plugin for PipeWire"
   homepage "https://pipewire.org"
-  url "https://gitlab.freedesktop.org/pipewire/pipewire/-/archive/1.6.8/pipewire-1.6.8.tar.gz"
-  sha256 "8181172a1d95131f6af8bbc0b98f90b2a33349b042b84c3ce57dd5d11348cc58"
+  url "https://gitlab.freedesktop.org/pipewire/pipewire/-/archive/1.6.9/pipewire-1.6.9.tar.gz"
+  sha256 "dbc3624cf71215c26a22fc98f6f71f7b8599f5f6b415eab4a5c90ca96c170c7c"
   license "MIT"
   head "https://gitlab.freedesktop.org/pipewire/pipewire.git", branch: "master"
 
