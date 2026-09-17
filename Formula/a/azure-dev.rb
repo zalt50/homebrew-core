c2a39647c9af486748df68c9fe3476f53447330f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-17 02:19:12 +0000

    azure-dev: update 1.34.1 bottle.

--- Formula/a/azure-dev.rb
+++ Formula/a/azure-dev.rb
@@ -7,12 +7,11 @@ class AzureDev < Formula
   head "https://github.com/Azure/azure-dev.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "1a5164224530a481caa91700eb841911a744f3bb7c3633a45805a9f909c8de7d"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "1a5164224530a481caa91700eb841911a744f3bb7c3633a45805a9f909c8de7d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "1a5164224530a481caa91700eb841911a744f3bb7c3633a45805a9f909c8de7d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:      "1a5164224530a481caa91700eb841911a744f3bb7c3633a45805a9f909c8de7d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:       "b37876ddcde7a65b5927a7857d477a16bee322a4b1d50580adeda3b5a63f6c1e"
-    sha256 cellar: :any,                 x86_64_linux:      "a48f5516f033caa1f27a4451399cfe944dc61355ab565a57304b60588fe4e772"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "ae14779a8d8992a6a77900de30cebe9b4ed6d7b6186cfef0bde111b4b97d2a01"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "ae14779a8d8992a6a77900de30cebe9b4ed6d7b6186cfef0bde111b4b97d2a01"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "ae14779a8d8992a6a77900de30cebe9b4ed6d7b6186cfef0bde111b4b97d2a01"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "7c30540937e2d11a84b68f77c230e145d4ffb7d58406290dd9e63792a092e39d"
+    sha256 cellar: :any,                 x86_64_linux:      "041c81e6683abf751f9ae695c1711909f198d99e3e31d52c569c80b965f22256"
   end
 
   depends_on "go" => :build
