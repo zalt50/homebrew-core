a9da581ff9ec8c1090957376ac9b9ac6bec8e771
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-17 17:39:17 +0000

    protobuf 36.2

--- Formula/p/protobuf.rb
+++ Formula/p/protobuf.rb
@@ -1,10 +1,10 @@
 class Protobuf < Formula
   desc "Protocol buffers (Google's data interchange format)"
   homepage "https://protobuf.dev/"
-  url "https://github.com/protocolbuffers/protobuf/releases/download/v36.1/protobuf-36.1.tar.gz"
-  sha256 "dc74fa582f559cbd31614ddfefb4868f43c919d7184bde514bb47f90c6025eb8"
+  url "https://github.com/protocolbuffers/protobuf/releases/download/v36.2/protobuf-36.2.tar.gz"
+  sha256 "3d9642a662d10e68ebae5e53f14dcce5105684212d5078f8e0d47d1ab3ae6b64"
   license "BSD-3-Clause"
-  compatibility_version 6
+  compatibility_version 7
 
   livecheck do
     url :stable
