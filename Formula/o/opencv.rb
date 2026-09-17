039ece8598af6b4d15b8a6f3ba852fc7c04d1144
Rui Chen <rui@chenrui.dev>
2026-09-17 05:30:00 -0400

    opencv: bump revision (openvino 2026.4.0)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/o/opencv.rb
+++ Formula/o/opencv.rb
@@ -2,7 +2,7 @@ class Opencv < Formula
   desc "Open source computer vision library"
   homepage "https://opencv.org/"
   license "Apache-2.0"
-  revision 7
+  revision 8
   compatibility_version 2
 
   stable do
