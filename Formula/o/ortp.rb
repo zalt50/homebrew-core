de6ceb0ba272badb20258861e417ea5733173c35
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-17 17:36:04 +0000

    ortp 5.5.23

--- Formula/o/ortp.rb
+++ Formula/o/ortp.rb
@@ -1,8 +1,8 @@
 class Ortp < Formula
   desc "Real-time transport protocol (RTP, RFC3550) library"
   homepage "https://linphone.org/"
-  url "https://gitlab.linphone.org/BC/public/linphone-sdk/-/archive/5.5.22/linphone-sdk-5.5.22.tar.bz2"
-  sha256 "328488a97293567e6a37e3863febc1d95aa00be61a42172fe408d422aa1adf7a"
+  url "https://gitlab.linphone.org/BC/public/linphone-sdk/-/archive/5.5.23/linphone-sdk-5.5.23.tar.bz2"
+  sha256 "8c7f255e50e0b9f4a09bb2c4796d0e8ff5abdbdd13fe4c10a94cca9c2278c8fa"
   license all_of: ["AGPL-3.0-or-later", "GPL-3.0-or-later"]
   head "https://gitlab.linphone.org/BC/public/linphone-sdk.git", branch: "master"
 
