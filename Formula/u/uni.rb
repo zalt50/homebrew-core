f2431945fa716eb99307a4faf2d3dc26e239c973
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-17 12:00:17 +0000

    uni 2.10.0

--- Formula/u/uni.rb
+++ Formula/u/uni.rb
@@ -1,8 +1,8 @@
 class Uni < Formula
   desc "Unicode database query tool for the command-line"
   homepage "https://github.com/arp242/uni"
-  url "https://github.com/arp242/uni/archive/refs/tags/v2.9.0.tar.gz"
-  sha256 "dc595807a0ab875111dafd55be9f3de116cbea652216f9d0082d03dddb3d83be"
+  url "https://github.com/arp242/uni/archive/refs/tags/v2.10.0.tar.gz"
+  sha256 "e9208bc0028d239f9cfbb701d98b14e93eddd138ac6433c6f2f5718244ffa5bf"
   license "MIT"
 
   bottle do
