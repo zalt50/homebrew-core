7af783b59e62053d1eba7b1cbd197f4a16fbf360
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-18 10:00:09 +0000

    cloudsplaining: update 0.9.1_1 bottle.

--- Formula/c/cloudsplaining.rb
+++ Formula/c/cloudsplaining.rb
@@ -10,13 +10,11 @@ class Cloudsplaining < Formula
   head "https://github.com/salesforce/cloudsplaining.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, arm64_golden_gate: "d70dd32b2c223d75a05afefc6c1b43651806474aa7c18c8c2307c8f928e1c9f3"
-    sha256 cellar: :any, arm64_tahoe:       "ebf93c9d04bec4843a52545c316738c34cacd1e3867bdd68960d934064629f91"
-    sha256 cellar: :any, arm64_sequoia:     "8639cfd208c131fbf1d3be215e2e3f281607de774cfbdbe75440f49d4072eb03"
-    sha256 cellar: :any, arm64_sonoma:      "c4d811819a9e23bdf0e4954cba452592a821ce3bbffc2c13f3045a406b62042f"
-    sha256 cellar: :any, sonoma:            "293de1c38afc9b5c36aa0b02a5b1617667d9a349086120e5f813e52bdace0f49"
-    sha256 cellar: :any, arm64_linux:       "a6370bc12ee831cddd6e69a3bdc3040efc6f81b04a0b8734358d5ef921723342"
-    sha256 cellar: :any, x86_64_linux:      "20c4631c24b10ad64e746d6f83f672696e6ba16e30e088b9072a84b3cafc3197"
+    sha256 cellar: :any, arm64_golden_gate: "093eaf299cc8593de4210d2aaf80a9b25bb23fed57f44ecdd4b54463c372f268"
+    sha256 cellar: :any, arm64_tahoe:       "5b354c402a9910ee91822730dcd2653ce85d9e7ada9e21005d3e3219b5d574b8"
+    sha256 cellar: :any, arm64_sequoia:     "2f6c6fcc434fe5904cfe25347bbee6ae4d63cdd4acd222bce68e2179e4c067da"
+    sha256 cellar: :any, arm64_linux:       "06944e23ca224bc8b97809a93b7e2e47638ad536ce39bdddd35095a00f91ff61"
+    sha256 cellar: :any, x86_64_linux:      "51a47670df27454168d9bc1027c18c4d34d2ef493917eae04c27ac9ee83e6108"
   end
 
   depends_on "rust" => :build # for orjson
