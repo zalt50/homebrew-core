3619d57844d27bd1d8274a2979b219a747f407e5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-18 21:03:12 +0000

    ty: update 0.0.82 bottle.

--- Formula/t/ty.rb
+++ Formula/t/ty.rb
@@ -7,11 +7,11 @@ class Ty < Formula
   head "https://github.com/astral-sh/ty.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "c2c2e6948588b690ef7cdd5ab27d4a459cb3ccfa467060b6d1219830f29416a5"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "e72137653c27328360dcce6cf3c050bf56d529c6305010811310e76d098a553c"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "37901efecaf90ec2d80ba3a70259479deb05fcf37530c03282e9e01e0ce5fab9"
-    sha256 cellar: :any,                 arm64_linux:       "237c945d845400345e96520a5404f37063c6d16372c09e8c0c44d9d5f26afef5"
-    sha256 cellar: :any,                 x86_64_linux:      "ad05e0d726e8276768948067134b28992017dd880e10396d798eaa701e822a1c"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "091b904ab53457c6275e41b72a8bb45e963cc4568940d0c7aead4f12c2fa9ebb"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "0852f0af878c7dcbc0df0a7a88c8b67c9ec3654e5ae7eae612b508f3d5d05892"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "db9be1151fa2acd7d6c8a7ed0caf0b8fa3b9e24b031c4302d10747b24fe6ef8b"
+    sha256 cellar: :any,                 arm64_linux:       "2f8a867fd06cab726f326ca593bb0802d94340af096e32ec36180ebfc7400fd9"
+    sha256 cellar: :any,                 x86_64_linux:      "73fa8fec665df6d765bc3ca836b7898e4066a9f1e78ed1ef6bdccc5aeb1b965b"
   end
 
   depends_on "rust" => :build
