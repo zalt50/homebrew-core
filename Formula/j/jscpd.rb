3f2e6ae105928b1fcaee4c4d780d155193d4aa3f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-18 14:08:07 +0000

    jscpd 5.3.0

--- Formula/j/jscpd.rb
+++ Formula/j/jscpd.rb
@@ -1,8 +1,8 @@
 class Jscpd < Formula
   desc "Copy/paste detector for programming source code"
   homepage "https://jscpd.dev/"
-  url "https://github.com/kucherenko/jscpd/archive/refs/tags/v5.2.1.tar.gz"
-  sha256 "902f07f7ebc3ff7c546b8533424a05176d29bab4539dbcc447c4815fc47c8136"
+  url "https://github.com/kucherenko/jscpd/archive/refs/tags/v5.3.0.tar.gz"
+  sha256 "a0287018300ebee9e406793b54f2df106be1eee5cdab94c6172cdd5869b49d39"
   license "MIT"
 
   bottle do
