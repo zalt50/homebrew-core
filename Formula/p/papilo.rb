0e3e3c8d9cbaea920bffdf24dc31a91844155750
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-18 12:38:16 +0000

    papilo: update 3.0.2 bottle.

--- Formula/p/papilo.rb
+++ Formula/p/papilo.rb
@@ -7,13 +7,11 @@ class Papilo < Formula
   version_scheme 1
 
   bottle do
-    sha256 cellar: :any, arm64_golden_gate: "8f4a1fc0338357a18a88c9b35a41476e012134838242dcff7b72c53c9a02ae13"
-    sha256 cellar: :any, arm64_tahoe:       "4c7789a51574302947874e9dcc46e14c68d47f3978e17e050e31bd940d35635d"
-    sha256 cellar: :any, arm64_sequoia:     "09d3220af1a1bb2d8daf1cd6b4c490aac33bf3f29883ade5121d2045f26e4b0d"
-    sha256 cellar: :any, arm64_sonoma:      "056cfa53026ccb19a3e6949259cefcfec4ada37314d7f75ffab1094a28285e8d"
-    sha256 cellar: :any, sonoma:            "d9c9e94dadf1259a3ddb37cdfa6fa5fc9e681a60ad19b382546c4d1aceec1037"
-    sha256 cellar: :any, arm64_linux:       "4a1db47d03b0910bb5f4f77ea452883ae5465258480d20a5b8fd96ef279fee37"
-    sha256 cellar: :any, x86_64_linux:      "d315d22ae574c5c4fe7e8487f8bf04ae61191008dc23bbdd60a091c1e05ca422"
+    sha256 cellar: :any, arm64_golden_gate: "a3e22125aa7ff189326a169b6ac70a82130b4b8aa3de46383e26d0227edcaa36"
+    sha256 cellar: :any, arm64_tahoe:       "2798c40754021e04cb150aca412baec0ec1f0624e6e8cf5974bbc032b085a74e"
+    sha256 cellar: :any, arm64_sequoia:     "eacc1cf1dcf01558291455e6c546015d3253db1d90adfe466e3347e4fa495039"
+    sha256 cellar: :any, arm64_linux:       "bfa51c5035c86ac267a8a6e3dc39a9af1bc9401103fa8bba29b95f569d2b4127"
+    sha256 cellar: :any, x86_64_linux:      "4910a46ab4a09132dff9da5d0d73a6a1435de77f3451fb43b8d16efd123ee678"
   end
 
   depends_on "cmake" => :build
