c4cdf64b14e1225c27f8039d952427756462436b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-18 02:37:25 +0000

    openspec 1.13.1

--- Formula/o/openspec.rb
+++ Formula/o/openspec.rb
@@ -1,8 +1,8 @@
 class Openspec < Formula
   desc "Spec-driven development (SDD) for AI coding assistants"
   homepage "https://openspec.dev/"
-  url "https://registry.npmjs.org/@fission-ai/openspec/-/openspec-1.13.0.tgz"
-  sha256 "f3c129f3f1e3aece105a4c1798301c7b6faeacec8a30eec8def63787e4d59ace"
+  url "https://registry.npmjs.org/@fission-ai/openspec/-/openspec-1.13.1.tgz"
+  sha256 "66bea1499c367cd0669fb1cedb59b1ea88b8c3c7496702e8af5726c0b2f3c5e1"
   license "MIT"
 
   bottle do
