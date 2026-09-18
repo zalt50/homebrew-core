6a8be825a40cd838e690389af6d8a348aadc01df
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-18 22:58:46 +0000

    defuddle 0.19.4

--- Formula/d/defuddle.rb
+++ Formula/d/defuddle.rb
@@ -1,8 +1,8 @@
 class Defuddle < Formula
   desc "Extract article content and metadata from web pages"
   homepage "https://defuddle.md"
-  url "https://registry.npmjs.org/defuddle/-/defuddle-0.19.3.tgz"
-  sha256 "5ee0e894b27f8342975f7acbbb96dd31b79baa0e2f1bba47d0d25f16cc49d153"
+  url "https://registry.npmjs.org/defuddle/-/defuddle-0.19.4.tgz"
+  sha256 "affb9cbb19ae0f072c833a0dba45ef71ecb512118b9258a49bacd1d8b124fd30"
   license "MIT"
 
   bottle do
