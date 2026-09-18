350f3ea2098f262f483e848bb1ed4e70c1490b17
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-18 08:30:56 +0000

    release-it 21.1.0

--- Formula/r/release-it.rb
+++ Formula/r/release-it.rb
@@ -1,8 +1,8 @@
 class ReleaseIt < Formula
   desc "Generic CLI tool to automate versioning and package publishing related tasks"
   homepage "https://github.com/release-it/release-it"
-  url "https://registry.npmjs.org/release-it/-/release-it-21.0.3.tgz"
-  sha256 "dd630049c5fb2771895ce7439a1446cca0b376498301c12595515de9bf553150"
+  url "https://registry.npmjs.org/release-it/-/release-it-21.1.0.tgz"
+  sha256 "9db50403ddffb79fd7d99913b6fd3ba29d91afc173dc1de32971b941d4dfd0b5"
   license "MIT"
 
   livecheck do
