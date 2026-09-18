9e0f75dfa37859ff340aa9d3e82bd11bee9a916b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-18 14:40:48 +0000

    jscpd: update 5.3.0 bottle.

--- Formula/j/jscpd.rb
+++ Formula/j/jscpd.rb
@@ -6,11 +6,11 @@ class Jscpd < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "fa0266fbe61763ee27f5c3662cc9ab8b5df3b23b0d12a424975304b35fc41b0b"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "3efa2d9af0f640a1a3a143044bb274e3740b9dbd8156cfaaee2cfea2cced5451"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "bf571b464e3bf25ebb8fd7c68535068f7f6e1c5d9f7cd86997d73af7869040e0"
-    sha256 cellar: :any,                 arm64_linux:       "bc859f30aaf87994862ee247720dc77aa3dc11cf6f998164d3e8265829262e40"
-    sha256 cellar: :any,                 x86_64_linux:      "76eb7f5aa901d12d9bcf6b265714d60c3a7ff9987bfcddfcf92c06c7cedd2b63"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "9a4e418733c58915e2af1fef52ecca3eac2c7073e204b4eb076ed36f429e186a"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "c0698ca50ed6feb4b0c55bd8272e025042580ff80fd8fd21582f262a6bc515ff"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "83137e60fd49674d84d9e9cf1cedd38e6aedd88f5df74a9ad52056f3026912c0"
+    sha256 cellar: :any,                 arm64_linux:       "bfa8c7989e320e984db789cfaa795afa8158654b71e2f7b10dc30b9ca9958109"
+    sha256 cellar: :any,                 x86_64_linux:      "10ade997af4bdb669dd684617e1b3ad182a4d3d07e2ccf755d68fcae17891a43"
   end
 
   depends_on "rust" => :build
