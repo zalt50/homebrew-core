5440b6f5f3f38f366efd9e45322d5d2f1e8172d9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-18 09:44:58 +0000

    livebook-cli: update 0.19.10 bottle.

--- Formula/l/livebook-cli.rb
+++ Formula/l/livebook-cli.rb
@@ -7,13 +7,11 @@ class LivebookCli < Formula
   head "https://github.com/livebook-dev/livebook.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "37cc7c8627aec2073df91f05ae1cbf68912a9d48c3647ec98ecb37f83acd32f9"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "6df9eb7509af8c104f8104e4ce871de73964ccf5a09f107822334723ab1908e2"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "935fa9188488db9b0a9ec0920a5ff9d5bee9b6003be3661dbcc42be3d1188865"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:      "05c42ef0348ef2cc4db161ac2a94784c8d029adda71936c7dd4b0187f3231715"
-    sha256 cellar: :any_skip_relocation, sonoma:            "b50994c84f55eea9b82f25791c632c91fae2121b858b7ee8a29ccc581c5314bc"
-    sha256 cellar: :any_skip_relocation, arm64_linux:       "19f06c33207b0122ea0732f23d4c7422799697449952bfa7e96dbd40749689cf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:      "63804d1240546b246fb99334b720d3325ff1a5f087f9e195141c7394945ecd76"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "5a13fff8b5dde469b9b8d10dbdb242807a4fc5b4911c05b99de9470a5a16322c"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "4dbcc26b243423dd80f505b23a783f7528b361d7f404ba3ac7b13033f3e927a0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "67fc352c30e977326071b2d45e813bd4b5aca625766a1232a10b6978d35b8da6"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "97a38b960b65a5c21005ab4676f17d3ce1f93cf22e3b40601586dc6a84593787"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:      "b1306b5af117cede744913ae2bfde5397a6aa304eb62bc949f3e4eafd0f70787"
   end
 
   depends_on "elixir" => :build
