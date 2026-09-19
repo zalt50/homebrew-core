1028631151d982cac021fd773794a0567a70f460
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-19 16:54:45 +0000

    conftest 0.70.1

--- Formula/c/conftest.rb
+++ Formula/c/conftest.rb
@@ -1,8 +1,8 @@
 class Conftest < Formula
   desc "Test your configuration files using Open Policy Agent"
   homepage "https://www.conftest.dev/"
-  url "https://github.com/open-policy-agent/conftest/archive/refs/tags/v0.70.0.tar.gz"
-  sha256 "cf491b8e398895a8e0dbec76486defab65d4ad9723b9a19af159c89f63676da5"
+  url "https://github.com/open-policy-agent/conftest/archive/refs/tags/v0.70.1.tar.gz"
+  sha256 "f3bef9d3794c4f63b4b1add52f6310e5fb2699190a4e181e7a110864b5d29308"
   license "Apache-2.0"
   head "https://github.com/open-policy-agent/conftest.git", branch: "master"
 
