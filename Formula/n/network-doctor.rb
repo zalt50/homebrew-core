edaaa8180c112db4797add36e6ffba0a8f555728
botantony <antonsm21@gmail.com>
2026-09-19 23:11:21 +0200

    network-doctor: add `allow_network_access!`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Formula/n/network-doctor.rb
+++ Formula/n/network-doctor.rb
@@ -15,6 +15,12 @@ class NetworkDoctor < Formula
 
   depends_on "go" => :build
 
+  allow_network_access! :test
+
+  def fetch
+    system "go", "mod", "download"
+  end
+
   def install
     system "go", "build", *std_go_args(ldflags: "-X main.version=#{version}", output: bin/"netdoc")
   end
