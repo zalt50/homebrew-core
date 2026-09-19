cd1177ccd9fda8404d95fbc36e3682aef01b0661
Rui Chen <rui@chenrui.dev>
2026-09-19 06:40:26 +0000

    dotnet@8: update 8.0.131 bottle.

--- Formula/d/dotnet@8.rb
+++ Formula/d/dotnet@8.rb
@@ -13,11 +13,12 @@ class DotnetAT8 < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "3f07359848f9883e27d9242a063b21b1b159e90209f0b7ed8ca2bc26a7a6e448"
-    sha256 cellar: :any, arm64_sequoia: "2166e2f4856aa1e6066dff9e4079eb892b74b226cc96a2ba25b593e9a1587c46"
-    sha256 cellar: :any, arm64_sonoma:  "9a4c379ec1728f843bb74b725c9a8ae465d9ea865fce21d8faf65983e228c4c8"
-    sha256 cellar: :any, arm64_linux:   "dde17475ab661cec62698a886bcd336fa16cfa619377ce80dce5d3bc2922d118"
-    sha256 cellar: :any, x86_64_linux:  "e70864dc08b6369bf730383dc425c3e2436b179e400f8371b525b327f005a5ce"
+    rebuild 1
+    sha256 cellar: :any, arm64_golden_gate: "1541927a6d3509acafc72ae3735e8d449e2c67835e84296da542a43b1784ecea"
+    sha256 cellar: :any, arm64_tahoe:       "c78fadd8d5546c7cfa7cef7127ae8dc942b41263eed9e9714029dac3601b9315"
+    sha256 cellar: :any, arm64_sequoia:     "c0c2fa17e79aa727bfe40d9ae3b284b9cce10c2f3407457a5587ffcbc978bc0f"
+    sha256 cellar: :any, arm64_linux:       "2dbcb5921af87f3aa8de94c6ef9d798a08efd2396207d393c30be4706c160038"
+    sha256 cellar: :any, x86_64_linux:      "f39876995501a0350b3b6c4e9940167f037267da1b30796f189c2eb345cc20f3"
   end
 
   keg_only :versioned_formula
