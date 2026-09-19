b8356178514a463cc9d00a3000f162912508eca2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-19 11:03:02 +0000

    fluid-synth 2.6.1

--- Formula/f/fluid-synth.rb
+++ Formula/f/fluid-synth.rb
@@ -1,8 +1,8 @@
 class FluidSynth < Formula
   desc "Real-time software synthesizer based on the SoundFont 2 specs"
   homepage "https://www.fluidsynth.org"
-  url "https://github.com/FluidSynth/fluidsynth/archive/refs/tags/v2.6.0.tar.gz"
-  sha256 "6d17570ea2086dd6fbcac995465773a6f316e689089b7fb7ba0b34c2ec6f680d"
+  url "https://github.com/FluidSynth/fluidsynth/archive/refs/tags/v2.6.1.tar.gz"
+  sha256 "3d258a3bf97cc20c59eeebfe62c2432fae88adda74d3ad098681c76e0ebf446b"
   license "LGPL-2.1-or-later"
   compatibility_version 1
   head "https://github.com/FluidSynth/fluidsynth.git", branch: "master"
