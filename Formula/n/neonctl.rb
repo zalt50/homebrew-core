f39a7ded7c737bed79408de98556373a2cb96d27
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-19 03:14:58 +0000

    neonctl: update 5.0.0 bottle.

--- Formula/n/neonctl.rb
+++ Formula/n/neonctl.rb
@@ -6,11 +6,11 @@ class Neonctl < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_golden_gate: "5ed5ce020aaefd8c4e4526ebbd7c8600887a0fc587dfc354f85e8d851b4921fd"
-    sha256 cellar: :any,                 arm64_tahoe:       "5ed5ce020aaefd8c4e4526ebbd7c8600887a0fc587dfc354f85e8d851b4921fd"
-    sha256 cellar: :any,                 arm64_sequoia:     "5ed5ce020aaefd8c4e4526ebbd7c8600887a0fc587dfc354f85e8d851b4921fd"
-    sha256 cellar: :any_skip_relocation, arm64_linux:       "cb7f1ebca6f7fcaa5569aa85c7ba87a2392c70188368f536612a79eff394d0ea"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:      "c7f740b3012403e30d99d85d09ef6da0ad690933eb5d2d8fcdf8fa14c9e5b6a4"
+    sha256 cellar: :any,                 arm64_golden_gate: "ad83b8f86b66a0ed6114e9a48d050c16143d9b7435da57ef8274c1fb2972fc34"
+    sha256 cellar: :any,                 arm64_tahoe:       "ad83b8f86b66a0ed6114e9a48d050c16143d9b7435da57ef8274c1fb2972fc34"
+    sha256 cellar: :any,                 arm64_sequoia:     "ad83b8f86b66a0ed6114e9a48d050c16143d9b7435da57ef8274c1fb2972fc34"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "72d7dbf032ea8ef3d37cbddd386c1bec31c3e6ac174b9ed2da06037dbd75dd02"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:      "b52e0894c1ad3b87608d8aa090f52d01bf860b749d0d46cf0a74159a7c12c824"
   end
 
   depends_on "esbuild" # replaces the bundled copy
