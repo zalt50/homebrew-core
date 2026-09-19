471806931731c19fe4d65c5b57f0cf05f2da2b33
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-19 03:17:59 +0000

    sentry-native: update 0.16.7 bottle.

--- Formula/s/sentry-native.rb
+++ Formula/s/sentry-native.rb
@@ -6,13 +6,11 @@ class SentryNative < Formula
   license "MIT"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any, arm64_golden_gate: "0560ea1c6d5f459b856aea7ca16f9b5635a4e1d57fa6735d63d31f086b9fd52f"
-    sha256 cellar: :any, arm64_tahoe:       "b0eac9f6121c860f33900ca68fc7adb22666a92619a1c347263f4c3e3a4d4732"
-    sha256 cellar: :any, arm64_sequoia:     "57785f6c3cb1b53dc09a1162a97dec578c66411368476fdea3f54f039d0a1c8e"
-    sha256 cellar: :any, arm64_sonoma:      "9b48fdba80db1bc8ee59fd6570cc808238e2324407234f8a23c34eee9e43b96d"
-    sha256 cellar: :any, arm64_linux:       "9cd394e56f4752a3055e935198bfdd150f1d18ed0c01ab366060b88147b01364"
-    sha256 cellar: :any, x86_64_linux:      "e9891cc27f8255dbca46121bb178af0114d72534a011e12791941d2de4157abf"
+    sha256 cellar: :any, arm64_golden_gate: "4f1d57b0a4acf9673a16d1757c6cba6506145816f8ccff74da532e0710083958"
+    sha256 cellar: :any, arm64_tahoe:       "ac68343ad6f163a57445f4e667c5dcf1c34fb35127bc0898eb7839461953b12d"
+    sha256 cellar: :any, arm64_sequoia:     "b4be126221785be7ad8cb9f2aa11ead6d6d27a4530bc1075eda78ff9dd256925"
+    sha256 cellar: :any, arm64_linux:       "cae985547d082adc93a4dfaf6efed664519e5f67797fb1d823d3e90eac4bfbb4"
+    sha256 cellar: :any, x86_64_linux:      "bb2ade616d9d57de0429821d9b7fcb976706877e50407116509af362cddf74a5"
   end
 
   depends_on "cmake" => :build
