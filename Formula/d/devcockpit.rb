7417bf615ee86266246ec1d543f9069a7118e279
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-19 19:44:11 +0000

    devcockpit: update 3.0.0 bottle.

--- Formula/d/devcockpit.rb
+++ Formula/d/devcockpit.rb
@@ -7,12 +7,11 @@ class Devcockpit < Formula
   head "https://github.com/caioricciuti/dev-cockpit.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "fa1866707e6e402a79caa167586e35b427a17a786cdf43c4633c1c759acfc40c"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "c219068bdf25afb8b35eec889ca9904a71bab68eb877d74037a1bd53f0a7f443"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "ba82bc26988600c8291347d23446a3aa3ac854f12a12059d47a5dd1b47676686"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:      "30b27277a81e21bb38aa937dada422adbad94ed6ea91eab3e34cb7981d619111"
-    sha256 cellar: :any_skip_relocation, arm64_linux:       "fa8f5a921bb8f92fe91226b0fba5b679ab9ba28e8832140bfd70794ead6f8bd3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:      "5d4501f7e77eababad97ccc88b68baaf3f93aae146bbbd5409669c48bc201512"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "c12a38fb9fe8b0cd21abae41ff99523b00e4028a54d92a9bcf938b48e9b43fbc"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "9d289d6a1f9a8e61492837edcbcf4276eb7d88d0f5ba5ab6321eab4f4741a0fe"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "503d130e526e7dc94e1e45d9978ffbb838ca040eda24dd5b106869a74ed92008"
+    sha256 cellar: :any,                 arm64_linux:       "4b7d4ddb69b7df02f9748e5fae09f1604c49a81a6b44af82a44d82afbe211d85"
+    sha256 cellar: :any,                 x86_64_linux:      "e52d378982ede33463cfbc438c21138c5f058e1b5fdff2650fb3ef3a3a6a954d"
   end
 
   depends_on "go" => :build
