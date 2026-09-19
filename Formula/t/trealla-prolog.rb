f45fb394cc3ffdf476bd7b49c5d19e2331616e12
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-19 02:39:46 +0000

    trealla-prolog 3.10.20

--- Formula/t/trealla-prolog.rb
+++ Formula/t/trealla-prolog.rb
@@ -1,8 +1,8 @@
 class TreallaProlog < Formula
   desc "Compact and efficient ISO Prolog interpreter written in plain old C"
   homepage "https://github.com/trealla-prolog/trealla-prolog"
-  url "https://github.com/trealla-prolog/trealla-prolog/archive/refs/tags/v3.10.15.tar.gz"
-  sha256 "7d9e047faa42e5bfa10b2c59656fad412508ee93bab147c8aaab590a35243b83"
+  url "https://github.com/trealla-prolog/trealla-prolog/archive/refs/tags/v3.10.20.tar.gz"
+  sha256 "d1dca13f24f4ba445fd6d2fbfc1a867539c92ebe085d8f51469b3aeb18737045"
   license "MIT"
   head "https://github.com/trealla-prolog/trealla-prolog.git", branch: "main"
 
