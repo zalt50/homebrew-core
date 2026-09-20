dab6e94c1dee81558219e84aff5eb313dcfe18a0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-20 13:52:28 +0000

    bend 2.0.21

--- Formula/b/bend.rb
+++ Formula/b/bend.rb
@@ -1,8 +1,8 @@
 class Bend < Formula
   desc "Language that blocks AI mistakes via proof"
   homepage "https://bend-lang.com"
-  url "https://github.com/bendlang/bend/archive/refs/tags/v2.0.20.tar.gz"
-  sha256 "aed470331b14a2978a4d96bc10fff41b5299adf7df9b7d7497d989cf35b885cb"
+  url "https://github.com/bendlang/bend/archive/refs/tags/v2.0.21.tar.gz"
+  sha256 "dca645e7b192247bfed990af471b5eb90c7601ffc7477c509b520914cc9523b2"
   license "Apache-2.0"
   head "https://github.com/bendlang/bend.git", branch: "main"
 
