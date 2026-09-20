f000a385d05d67c2de74813bd8d0d429951ca17e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-20 20:16:05 +0000

    libgsf 1.14.59

--- Formula/lib/libgsf.rb
+++ Formula/lib/libgsf.rb
@@ -1,8 +1,8 @@
 class Libgsf < Formula
   desc "I/O abstraction library for dealing with structured file formats"
   homepage "https://gitlab.gnome.org/GNOME/libgsf"
-  url "https://download.gnome.org/sources/libgsf/1.14/libgsf-1.14.58.tar.xz"
-  sha256 "06e07ea12b7a52b9e316faddfecb640b1717a4875c59f0efb3b0cec1e2ccf35a"
+  url "https://download.gnome.org/sources/libgsf/1.14/libgsf-1.14.59.tar.xz"
+  sha256 "0d03cb6fadfe735caa13498a024ccd8fdb6cab77df6d9d283a64410c96f2fa49"
   license "LGPL-2.1-only"
   compatibility_version 1
 
