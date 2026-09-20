74046e9ca0bbcceee0cab2c409111fd6aebe00e1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-20 11:03:37 +0000

    gzip 1.15

--- Formula/g/gzip.rb
+++ Formula/g/gzip.rb
@@ -1,9 +1,9 @@
 class Gzip < Formula
   desc "Popular GNU data compression program"
   homepage "https://www.gnu.org/software/gzip/"
-  url "https://ftpmirror.gnu.org/gzip/gzip-1.14.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/gzip/gzip-1.14.tar.gz"
-  sha256 "613d6ea44f1248d7370c7ccdeee0dd0017a09e6c39de894b3c6f03f981191c6b"
+  url "https://ftpmirror.gnu.org/gzip/gzip-1.15.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/gzip/gzip-1.15.tar.gz"
+  sha256 "545886cf57fa88a65e967fbf705903d7fcb2567c82c7342493e82e8d7b1a210b"
   license "GPL-3.0-or-later"
 
   bottle do
@@ -16,6 +16,15 @@ class Gzip < Formula
     sha256 cellar: :any_skip_relocation, ventura:           "39d86283bbdd91c6347ce5c7869e5a75db4d0bc6e961c8763fb7e81802cdeb55"
     sha256 cellar: :any_skip_relocation, arm64_linux:       "bf3ee62bd32f32b5288069e02bd54a9b1df35b2ec45dfc1a403aa95c010e0f6b"
     sha256 cellar: :any_skip_relocation, x86_64_linux:      "6cdbf878eda84da0ba1cac4bae09d3bf3c91ba1d5666806f9fa1ed19acc1f142"
+  end
+
+  # Fix compile error on aarch64 Linux
+  # gzip.h:120:21: error: expected ')' before '+' token
+  patch do
+    url "https://raw.githubusercontent.com/OpenMandrivaAssociation/gzip/5a3c8e5316bac3ac837f7aa8dc7e3a4b0ba74321/gzip-1.15-aarch64-head-macro.patch"
+    sha256 "82ef5b24041eeb86511ce67cb4e60edf8fbfcce01f7a1ab28fd1e024a3050df0"
+    type :unofficial
+    resolves "https://lists.gnu.org/archive/html/bug-gzip/2026-09/msg00031.html"
   end
 
   def install
