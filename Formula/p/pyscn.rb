8c422f1b15abd30738f0a50acaa22f1dba7b0e6c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-20 11:22:31 +0000

    pyscn 1.32.1

--- Formula/p/pyscn.rb
+++ Formula/p/pyscn.rb
@@ -1,8 +1,8 @@
 class Pyscn < Formula
   desc "Intelligent Python Code Quality Analyzer"
   homepage "https://ludo-technologies.github.io/pyscn/"
-  url "https://github.com/ludo-technologies/pyscn/archive/refs/tags/v1.32.0.tar.gz"
-  sha256 "aaaf0c0a5e08bb7b5d4f94af8f39a0717b7394daba2151a0e05f282498ed0f6e"
+  url "https://github.com/ludo-technologies/pyscn/archive/refs/tags/v1.32.1.tar.gz"
+  sha256 "e63a1ed5c42d953ec2c40da341e897f2dee2eda44e70ab7a295d499523cc933a"
   license "MIT"
   head "https://github.com/ludo-technologies/pyscn.git", branch: "main"
 
