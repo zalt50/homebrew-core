73767cf2712b33810b6f201f1f6171416570b3ca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-20 02:28:46 +0000

    libnfs 8.0.0
    libnfs: bump compatibility_version
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/lib/libnfs.rb
+++ Formula/lib/libnfs.rb
@@ -1,10 +1,10 @@
 class Libnfs < Formula
   desc "C client library for NFS"
   homepage "https://github.com/sahlberg/libnfs"
-  url "https://github.com/sahlberg/libnfs/archive/refs/tags/libnfs-7.0.2.tar.gz"
-  sha256 "188a8a5de2ad2099a2e97e42f34721bfc4c16c8d33808de0f7484f99d714a3df"
+  url "https://github.com/sahlberg/libnfs/archive/refs/tags/libnfs-8.0.0.tar.gz"
+  sha256 "bc91216e927a85142b5de611c7f711558e02119a7daad67620ea631634038350"
   license "LGPL-2.1-or-later"
-  compatibility_version 4
+  compatibility_version 5
 
   bottle do
     rebuild 1
