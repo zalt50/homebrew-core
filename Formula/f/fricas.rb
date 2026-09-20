cfc237069895e9080bb2adb3e347c80f98468591
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2026-09-20 03:05:42 +0000

    fricas: update 1.3.13_7 bottle.

--- Formula/f/fricas.rb
+++ Formula/f/fricas.rb
@@ -7,11 +7,12 @@ class Fricas < Formula
   revision 7
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "4757c100ed2611239c2c89853953cfcf391fd8b0f48809191161188d36c1e29c"
-    sha256 cellar: :any, arm64_sequoia: "391057131522b1f068d42bd873a1f3c576e3eb0a60ab621cb50571ba42cd28ba"
-    sha256 cellar: :any, arm64_sonoma:  "5bdae2eec1d1453b6c76bada3e786ae317edc1b9ce15c62b46fc174650930be5"
-    sha256 cellar: :any, arm64_linux:   "0e874a862d5680153400acd1bf1588dfac8dc4a93e07e827c07fb5c1656d9126"
-    sha256 cellar: :any, x86_64_linux:  "32efb07bb5e6c56cffcb8b3fbcb7b42493e2c2784efb3110ac6ca897205bf9d6"
+    sha256 cellar: :any, arm64_golden_gate: "a5bf832a22e111827203875402746117838a7e8f18de9b92da4e6ebf94030494"
+    sha256 cellar: :any, arm64_tahoe:       "4757c100ed2611239c2c89853953cfcf391fd8b0f48809191161188d36c1e29c"
+    sha256 cellar: :any, arm64_sequoia:     "391057131522b1f068d42bd873a1f3c576e3eb0a60ab621cb50571ba42cd28ba"
+    sha256 cellar: :any, arm64_sonoma:      "5bdae2eec1d1453b6c76bada3e786ae317edc1b9ce15c62b46fc174650930be5"
+    sha256 cellar: :any, arm64_linux:       "0e874a862d5680153400acd1bf1588dfac8dc4a93e07e827c07fb5c1656d9126"
+    sha256 cellar: :any, x86_64_linux:      "32efb07bb5e6c56cffcb8b3fbcb7b42493e2c2784efb3110ac6ca897205bf9d6"
   end
 
   depends_on "gmp"
