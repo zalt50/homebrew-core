b411c05cc9f37cceee35ef0cf993cc6b3ce86b42
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-20 14:34:11 +0000

    utf8cpp 4.2.1

--- Formula/u/utf8cpp.rb
+++ Formula/u/utf8cpp.rb
@@ -1,8 +1,8 @@
 class Utf8cpp < Formula
   desc "UTF-8 with C++ in a Portable Way"
   homepage "https://github.com/nemtrif/utfcpp"
-  url "https://github.com/nemtrif/utfcpp/archive/refs/tags/v4.2.0.tar.gz"
-  sha256 "54a8e96ea835a7359e8e53d03e30e9833d51350cc4615ff53f8449ef19ee46ab"
+  url "https://github.com/nemtrif/utfcpp/archive/refs/tags/v4.2.1.tar.gz"
+  sha256 "6d6a5493a111884cc085ee31babfe6d9960c8fb08fc80a64852eaeea8323dbc1"
   license "BSL-1.0"
   version_scheme 1
   compatibility_version 1
