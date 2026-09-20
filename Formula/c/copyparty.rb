4584a42212a5ff95ded0c560769ba07b325b3a73
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-20 23:47:33 +0000

    copyparty: update 1.20.24 bottle.

--- Formula/c/copyparty.rb
+++ Formula/c/copyparty.rb
@@ -8,12 +8,11 @@ class Copyparty < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_golden_gate: "5b148e8b91ee202ae3546e5e57182cc120ee310b7e85aa4a6cadf4fd88d8ecff"
-    sha256 cellar: :any, arm64_tahoe:       "20ab36794e3b034f05d655dc78c9bf72546bdef305bb7d13a5bd6623feeec204"
-    sha256 cellar: :any, arm64_sequoia:     "6b42e51504ec6a9af94033dbbfc3d03e383296f5d9904b5f7421d8a9484e4867"
-    sha256 cellar: :any, arm64_sonoma:      "e45e5d4d946bd5ccc5840dbd02d6df9f113c9bd128bcdfe6fdb7aeb026e955ae"
-    sha256 cellar: :any, arm64_linux:       "1d06ddae0f1d97a8e282227db0d8e87a96b21cbea705eae1610ab941243a997b"
-    sha256 cellar: :any, x86_64_linux:      "aae9acaeca822f32ff8f629aee345629b5c5eaeaac556952a974df1a529c5047"
+    sha256 cellar: :any, arm64_golden_gate: "df9b7c8f3fa9bb61b287bc17a7b149e7bbb32432422b88ebf0c79c033fb72be4"
+    sha256 cellar: :any, arm64_tahoe:       "dbf673e4b9708a75ee3cbd100c994c5322236f257ad36a03d0f3e6f7e553a998"
+    sha256 cellar: :any, arm64_sequoia:     "90acfabfae7de2ce74c14620405eca9ee961305ff2dd77e2d9687ac60f28e51f"
+    sha256 cellar: :any, arm64_linux:       "26455d6880c3e63365bfd7fc3ed086e26c0f28f3c6fc3eebb7081b1256cfca15"
+    sha256 cellar: :any, x86_64_linux:      "baa314448e7310d36894559e91c757329492820967cf8e91dc43c12633eebe5c"
   end
 
   # `pkgconf` and `rust` are for bcrypt
