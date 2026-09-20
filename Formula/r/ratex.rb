a98a45bd21dec6c70a2fdd6fd3a604e4f642288a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-20 08:27:55 +0000

    ratex 0.4.0

--- Formula/r/ratex.rb
+++ Formula/r/ratex.rb
@@ -1,8 +1,8 @@
 class Ratex < Formula
   desc "Fast TeX engine written in Rust"
   homepage "https://github.com/leoliu0/ratex"
-  url "https://github.com/leoliu0/ratex/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "bfbcf3fdc34fc9269f6f2f4a523f36e954fe96a1ce3d2f1388e0adb1607b3a6c"
+  url "https://github.com/leoliu0/ratex/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "9a9ac0bc31a4004271772fbe44aed298a96f5b486b47df2579ea6dd4c8da4dab"
   license any_of: ["MIT", "Apache-2.0"]
   head "https://github.com/leoliu0/ratex.git", branch: "main"
 
