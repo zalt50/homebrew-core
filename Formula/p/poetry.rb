6db4ee1bed8720cfbb8d697d237394bca1bb1bf0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-21 08:47:16 +0000

    poetry 2.5.1

--- Formula/p/poetry.rb
+++ Formula/p/poetry.rb
@@ -3,8 +3,8 @@ class Poetry < Formula
 
   desc "Python package management tool"
   homepage "https://python-poetry.org/"
-  url "https://files.pythonhosted.org/packages/e4/48/9656abd10e8c6c78d23c3e9ebbaed7ebd2fbdb792ff33fcb9afe37990177/poetry-2.5.0.tar.gz"
-  sha256 "e73b873890526c03e4d8e20d934292175f55a5823b3bdee2f154f63dae81336f"
+  url "https://files.pythonhosted.org/packages/43/86/204ef622b09005c0290b4cb23cc8178b0fdd52d8d6b36ad6d78dab447035/poetry-2.5.1.tar.gz"
+  sha256 "19fbd566efced512be58989a756aa1333cdc9b6aabb4fc84fd1ec9d29ee74874"
   license "MIT"
   head "https://github.com/python-poetry/poetry.git", branch: "main"
 
@@ -229,8 +229,8 @@ class Poetry < Formula
   end
 
   resource "virtualenv" do
-    url "https://files.pythonhosted.org/packages/af/76/6067b553c6cfda44ed049ef821bdeda1b40b4597c934663ecd6cdfcdfc16/virtualenv-21.8.1.tar.gz"
-    sha256 "529a4c883b4efa067b98cfb61b64cdc0e453d01800ef5b5a29d0a9c514499a4f"
+    url "https://files.pythonhosted.org/packages/2f/5d/42254e91a9203a13d95ab4f5211f4cdda52b82fc3546340f7fb33a642173/virtualenv-21.9.0.tar.gz"
+    sha256 "fa0f2a26fcb6f32b376fc8b2c705b6057366763d5b9e5242d1ab10ecc3279fd3"
   end
 
   resource "xattr" do
