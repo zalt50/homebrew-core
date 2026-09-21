189dc273e34b64530284625a94d83dc6b917d42e
Rui Chen <rui@chenrui.dev>
2026-09-21 12:45:41 -0400

    libheif-plugins: add deny_network_access!
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/lib/libheif-plugins.rb
+++ Formula/lib/libheif-plugins.rb
@@ -19,6 +19,7 @@ class LibheifPlugins < Formula
   end
 
   depends_on "cmake" => :build
+
   depends_on "pkgconf" => :build
 
   depends_on "dav1d"
@@ -31,6 +32,7 @@ class LibheifPlugins < Formula
   depends_on "svt-av1"
   depends_on "x264"
 
+  deny_network_access!
   def install
     # Enabling plugins for "popular" formulae
     plugins = %w[
