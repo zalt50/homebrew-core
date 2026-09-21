69aad589dccae40d2fbda76fa4b0557ac86cad63
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-21 02:36:36 +0000

    llgo 1.0.4

--- Formula/l/llgo.rb
+++ Formula/l/llgo.rb
@@ -1,8 +1,8 @@
 class Llgo < Formula
   desc "Go compiler based on LLVM integrate with the C ecosystem and Python"
   homepage "https://github.com/xgo-dev/llgo"
-  url "https://github.com/xgo-dev/llgo/archive/refs/tags/v1.0.3.tar.gz"
-  sha256 "f4e37dd1771d854771a559399ea50d894ae5b4c85b6106a1993655dfbfd74dde"
+  url "https://github.com/xgo-dev/llgo/archive/refs/tags/v1.0.4.tar.gz"
+  sha256 "9479a9baa51d40c062ea77dd256f007b98e1b773c26ef224af5f7a393af845bd"
   license "Apache-2.0"
   head "https://github.com/xgo-dev/llgo.git", branch: "main"
 
