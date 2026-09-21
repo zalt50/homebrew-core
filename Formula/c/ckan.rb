c6831ce420d8c3332ad6568f65bf5f0a6364af39
Anton Melnikov <antonsm21@gmail.com>
2026-09-21 17:46:43 +0000

    ckan: add 1.36.4 bottle.

--- Formula/c/ckan.rb
+++ Formula/c/ckan.rb
@@ -10,6 +10,15 @@ class Ckan < Formula
     strategy :github_latest
   end
 
+  bottle do
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "8d58c9014f39a3e847cc0feea02a216b13c9aa5ef77036686f80d131ec2389f4"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "dffad48ff1aaadcb51aa576f692df0b79476e6a914b7eec5234afceb2614198a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "49a0e9dfbc380cafacd0fd2b5bf714f4f4dd94f93a7f4800e376913525018a9e"
+    sha256 cellar: :any,                 arm64_linux:       "ec6e13e5494f3fc369897206f024ebe87575c4797b285a6189d138bf0feec742"
+    sha256 cellar: :any,                 x86_64_linux:      "f997195e4880fb61ce3f592c41ec09f83e417381ebc0e2d0a0e967b065c81904"
+  end
+
   depends_on "dotnet"
 
   def install
