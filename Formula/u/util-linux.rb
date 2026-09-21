9d1b13c258a661e1b110a1e4833e0bb21af471ed
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-21 11:51:08 +0000

    util-linux 2.42.4

--- Formula/u/util-linux.rb
+++ Formula/u/util-linux.rb
@@ -1,8 +1,8 @@
 class UtilLinux < Formula
   desc "Collection of Linux utilities"
   homepage "https://github.com/util-linux/util-linux"
-  url "https://mirrors.edge.kernel.org/pub/linux/utils/util-linux/v2.42/util-linux-2.42.3.tar.xz"
-  sha256 "66ac7c0e725278eb2b039e3104f2c91119341d941b41bac7a285c695f940bd57"
+  url "https://mirrors.edge.kernel.org/pub/linux/utils/util-linux/v2.42/util-linux-2.42.4.tar.xz"
+  sha256 "fbd62a100ab7bb8746ba0661255c3c48185b1e9021507c624da01fbc696330ec"
   license all_of: [
     "BSD-3-Clause",
     "BSD-4-Clause-UC",
@@ -55,13 +55,7 @@ class UtilLinux < Formula
     conflicts_with "rename", because: "both install `rename` binaries"
   end
 
-  # libmount/src/hook_idmap.c:335:33: error: ‘RESOLVE_NO_SYMLINKS’ undeclared (first use in this function)
-  patch do
-    url "https://github.com/util-linux/util-linux/commit/a323dddbcd1ed05a10e7e870b3e1a48b4ed44a43.patch?full_index=1"
-    sha256 "7c3cd540618ff46cfb1e22a5273d198012711c0b1169b70ddd731d8e581e6f64"
-    type :backport
-    resolves "https://github.com/util-linux/util-linux/issues/4597"
-  end
+  deny_network_access!
 
   def install
     args = %W[--disable-silent-rules --disable-asciidoc --with-bashcompletiondir=#{bash_completion}]
