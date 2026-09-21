ee6babab189c4a9999633c8a2b031f42da1765a2
Rui Chen <rui@chenrui.dev>
2026-09-21 15:23:24 -0400

    sqlitecpp: add deny_network_access!
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/s/sqlitecpp.rb
+++ Formula/s/sqlitecpp.rb
@@ -20,6 +20,8 @@ class Sqlitecpp < Formula
   depends_on "cmake" => :build
   depends_on "sqlite" # needs sqlite3_load_extension
 
+  deny_network_access!
+
   def install
     system "cmake", "-S", ".", "-B", "build",
                     "-DBUILD_SHARED_LIBS=ON",
