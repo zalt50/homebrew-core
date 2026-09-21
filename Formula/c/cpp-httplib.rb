fd511ff5436885e0589b64b0f77800f5d15ff289
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-21 02:26:20 +0000

    cpp-httplib: update 0.57.0 bottle.

--- Formula/c/cpp-httplib.rb
+++ Formula/c/cpp-httplib.rb
@@ -6,7 +6,7 @@ class CppHttplib < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "89af345efce61b55c59554172ade20733a2661a2ad86da23be9d309f28f4813e"
+    sha256 cellar: :any_skip_relocation, all: "10b265c519adf9001407fac7cacea41786a74af9fd57d8f0fbd4eb6cc2ec9b98"
   end
 
   depends_on "cmake" => :build
