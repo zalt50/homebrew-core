eb6b6b35798b381e769891179ce1146be4dae05a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-22 05:17:52 +0000

    mavsdk 4.0.0

--- Formula/m/mavsdk.rb
+++ Formula/m/mavsdk.rb
@@ -2,10 +2,9 @@ class Mavsdk < Formula
   desc "API and library for MAVLink compatible systems written in C++17"
   homepage "https://mavsdk.mavlink.io/main/en/index.html"
   url "https://github.com/mavlink/MAVSDK.git",
-      tag:      "v3.17.4",
-      revision: "2cc85e4c70668adda4a97eb52ec0b38c39ebc934"
+      tag:      "v4.0.0",
+      revision: "2794207ac27b8cfbbe33e0cfa3b6b6ddef765c3a"
   license "BSD-3-Clause"
-  revision 3
 
   livecheck do
     url :stable
@@ -24,15 +23,22 @@ class Mavsdk < Formula
   depends_on "python@3.14" => :build
   depends_on "rust" => :build
   depends_on "abseil"
+  depends_on "asio"
   depends_on "c-ares"
   depends_on "curl"
+  depends_on "fmt"
   depends_on "grpc"
-  depends_on "jsoncpp"
+  depends_on "nlohmann-json"
   depends_on "openssl@3"
   depends_on "protobuf"
   depends_on "re2"
   depends_on "tinyxml2"
   depends_on "xz"
+
+  on_macos do
+    # `cpp/tools/generate_from_protos.sh` uses `mapfile`, which needs Bash 4
+    depends_on "bash" => :build
+  end
 
   on_linux do
     depends_on "zlib-ng-compat"
@@ -45,24 +51,24 @@ class Mavsdk < Formula
     version "d6a7eeaf43319ce6da19a1973ca40180a4210643"
 
     livecheck do
-      url "https://raw.githubusercontent.com/mavlink/MAVSDK/refs/tags/v#{LATEST_VERSION}/third_party/CMakeLists.txt"
+      url "https://raw.githubusercontent.com/mavlink/MAVSDK/refs/tags/v#{LATEST_VERSION}/cpp/third_party/CMakeLists.txt"
       regex(/MAVLINK_HASH.*(\h{40})/i)
     end
   end
 
   def install
     # Fix version being reported as `v#{version}-dirty`
-    inreplace "CMakeLists.txt", "OUTPUT_VARIABLE VERSION_STR", "OUTPUT_VARIABLE VERSION_STR_IGNORED"
+    inreplace "cpp/CMakeLists.txt", "OUTPUT_VARIABLE VERSION_STR", "OUTPUT_VARIABLE VERSION_STR_IGNORED"
 
     # Regenerate files to support newer protobuf
-    system "tools/generate_from_protos.sh"
+    system "tools/generate_from_protos.bash"
 
     # `mavlink` repo and hash info moved to `third_party/CMakeLists.txt` only for SUPERBUILD,
     # so we have to manage the hash manually, but then it is better to keep it as a resource.
-    (buildpath/"third_party/mavlink").install resource("mavlink")
+    (buildpath/"cpp/third_party/mavlink").install resource("mavlink")
 
     %w[mavlink picosha2 libevents libmavlike].each do |dep|
-      system "cmake", "-S", "third_party/#{dep}", "-B", "build_#{dep}",
+      system "cmake", "-S", "cpp/third_party/#{dep}", "-B", "build_#{dep}",
                       "-DMAVLINK_DIALECT=ardupilotmega",
                       *std_cmake_args(install_prefix: libexec)
       system "cmake", "--build", "build_#{dep}"
@@ -71,7 +77,7 @@ class Mavsdk < Formula
 
     # Install MAVLink message definitions manually
     messages_files = "{minimal,standard,common,ardupilotmega}.xml"
-    messages_dir = Dir["#{buildpath}/third_party/mavlink/message_definitions/v1.0/#{messages_files}"]
+    messages_dir = Dir["#{buildpath}/cpp/third_party/mavlink/message_definitions/v1.0/#{messages_files}"]
     (libexec/"include/mavlink/message_definitions/v1.0").install messages_dir
 
     # Source build adapted from
@@ -80,14 +86,14 @@ class Mavsdk < Formula
       -DSUPERBUILD=OFF
       -DBUILD_SHARED_LIBS=ON
       -DBUILD_MAVSDK_SERVER=ON
-      -DBUILD_TESTS=OFF
+      -DBUILD_TESTING=OFF
       -DVERSION_STR=v#{version}-#{tap.user}
       -DCMAKE_PREFIX_PATH=#{libexec}
       -DCMAKE_INSTALL_RPATH=#{rpath}
       -DDEPS_INSTALL_PATH=#{libexec}
     ]
 
-    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "-S", "cpp", "-B", "build", *args, *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
@@ -95,7 +101,7 @@ class Mavsdk < Formula
   test do
     (testpath/"test.cpp").write <<~CPP
       #include <iostream>
-      #include <mavsdk/mavsdk.h>
+      #include <mavsdk/mavsdk.hpp>
       using namespace mavsdk;
       int main() {
           Mavsdk mavsdk{Mavsdk::Configuration{ComponentType::GroundStation}};
