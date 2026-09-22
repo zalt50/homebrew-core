821696929ac5b25230ee0ef2facfe9f280736fed
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-22 03:59:34 +0000

    ccmux: add 1.4.1 bottle.

--- Formula/c/ccmux.rb
+++ Formula/c/ccmux.rb
@@ -5,6 +5,14 @@ class Ccmux < Formula
   sha256 "f19afae151a688b707a81f4d11d6a1b29e1320868a7fc0dd9672136518e9b92a"
   license "MIT"
 
+  bottle do
+    sha256 arm64_golden_gate: "caee880be4e57938e0b7a059d4a059606892cc3811bee7696e7d7d6883b4cd27"
+    sha256 arm64_tahoe:       "281f0f624d0cc5ef3f4d1dcbed9ecb617f1239ee5845a9e5a41759482b52daab"
+    sha256 arm64_sequoia:     "f1b0ab051e1919fa5214a8a191a71a316b1e93566d66b31e88e8ad635064917e"
+    sha256 arm64_linux:       "e214af63d3c6187931972919e46ea553d8ca90bec8d02ffb4313fe6a8c372a74"
+    sha256 x86_64_linux:      "e08eb10ff4c1109f703b5cecc3816ebd5b2350caee1970cb19b7bec6b7d00178"
+  end
+
   depends_on "bun" => :build
   depends_on "tmux"
 
