52373991f93da84c62faf7775d7aa46fad2e608a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-22 22:57:42 +0000

    diffoscope 330

--- Formula/d/diffoscope.rb
+++ Formula/d/diffoscope.rb
@@ -3,8 +3,8 @@ class Diffoscope < Formula
 
   desc "In-depth comparison of files, archives, and directories"
   homepage "https://diffoscope.org"
-  url "https://files.pythonhosted.org/packages/f5/e1/c8e6656893ca111fba107e03195930195e0105fa67b99919f0529b9fc520/diffoscope-329.tar.gz"
-  sha256 "940ee30d8d98231f4d4855e36caf96e9eec9ec3d3cb359b744687b369b2b54d9"
+  url "https://files.pythonhosted.org/packages/65/33/6b150dd23df29dadf21735cdd684fd0883d6c582be4263ebb825a53bfb3a/diffoscope-330.tar.gz"
+  sha256 "92fb70b930077282ceaf104b4eb02c6368c860b0c89d6aab98f0a55682ee0361"
   license "GPL-3.0-or-later"
 
   bottle do
