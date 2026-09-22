8e4d32d112f758ff4e6277166bb28bcb9f79f95a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-22 15:32:00 +0000

    libklvanc: add 1.7.0 bottle.

--- Formula/lib/libklvanc.rb
+++ Formula/lib/libklvanc.rb
@@ -6,6 +6,14 @@ class Libklvanc < Formula
   license "LGPL-2.1-only"
   head "https://github.com/stoth68000/libklvanc.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any, arm64_golden_gate: "2cc30e79117f1f1c8f8ef35a9f2cb265a1b9db783b8bd51f013f4b97620775da"
+    sha256 cellar: :any, arm64_tahoe:       "3cee6392d6e38a0d0235669ff6f4ebd668931dc2b8180f3ef47e01faee183969"
+    sha256 cellar: :any, arm64_sequoia:     "aa6f71bdb7e4204904e28a87c398b9fedae4f9beea9f507ad7539d61c15e06af"
+    sha256 cellar: :any, arm64_linux:       "ca68e9866cf1afcc5c54bf595873032074560e85fe969b12fe76be343d7a3882"
+    sha256 cellar: :any, x86_64_linux:      "150b312a114140a1ea310a0613aeaa3e22a1523075d500dde4d8f38194059b47"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
