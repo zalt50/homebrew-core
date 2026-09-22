61c87a68b39514e83e66aa9b1a8f71afa78390c3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-22 00:06:27 +0000

    vtcode: update 0.165.0 bottle.

--- Formula/v/vtcode.rb
+++ Formula/v/vtcode.rb
@@ -11,11 +11,11 @@ class Vtcode < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "f74764716be9b5af6389be280a9c1f5377e7572deec2eb8ca91b0b88f47f1f54"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "1920d69dfeb3a5e488d3a8efd4b2e88ec2bc19af93da89b8a7422d083c604c84"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "5947ccad09fd2af09c77c0f559e4eea70017519af7afc76ae4dd4297d04838df"
-    sha256 cellar: :any,                 arm64_linux:       "4e4053a60d60ad0d72f43f7864f0694003f8ed672f2130d9524802b0967321cb"
-    sha256 cellar: :any,                 x86_64_linux:      "18c49f124ccebb1a6c20035a47472e2775f3e310035e096afa5541c6178bb169"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "cb6a08fa25e761fae438cc50c1a3113340f90cd858593ee7e7844ef0e06924fe"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "bd19117afd5ed745b02d86ca483e1524eb7bccf62de4a76c9cbef640535fd590"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "8f6a975752043596522e5e20340c6d22ce28b0c24d96e6e4b9d4f3538a044b30"
+    sha256 cellar: :any,                 arm64_linux:       "6e9a5fc9eb3dc8a82c48a9fcebb412513a3e31c1dc550ed46443df2930cc0b39"
+    sha256 cellar: :any,                 x86_64_linux:      "b700e760bc8d5439617d7ebec6f9e6ed54ddfb3a04a51fdfb5662f707f8e64f7"
   end
 
   depends_on "pkgconf" => :build
