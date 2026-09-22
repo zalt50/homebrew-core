b5651e6eac0beaff7cc28ac522f35bcecf92565a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-22 14:41:53 +0000

    panache 3.12.0

--- Formula/p/panache.rb
+++ Formula/p/panache.rb
@@ -1,8 +1,8 @@
 class Panache < Formula
   desc "Language server, formatter, and linter for Markdown, Quarto, and R Markdown"
   homepage "https://panache.bz"
-  url "https://github.com/jolars/panache/archive/refs/tags/v3.11.0.tar.gz"
-  sha256 "30e69acfd3c859d992dbc9926b84c615056c2155986877a723203505cbeac162"
+  url "https://github.com/jolars/panache/archive/refs/tags/v3.12.0.tar.gz"
+  sha256 "d525de4b609c1b8c0aee60fab31eeda09c10e95d414a56c178b82e5efce3719f"
   license "MIT"
   head "https://github.com/jolars/panache.git", branch: "main"
 
