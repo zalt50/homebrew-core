0378df980b1b7863ac0185dad2942c8cfffc074e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-22 09:21:04 +0000

    libclc: update 23.1.2 bottle.

--- Formula/lib/libclc.rb
+++ Formula/lib/libclc.rb
@@ -12,12 +12,11 @@ class Libclc < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "8f34be9ae8fc5f50383834ba18334baf7d9249208e2e0326509b3421da1604c9"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "9b7f7dbc7f8277430626bfadd3a8de0fc8696702f1072988bc20687848c44ecd"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "afdda900c48f3e9893a3293683be8f461a7c1a49f9c14fe14b48963e348a3164"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:      "55b5343848a365defcd52e7967e0849707be4d79a87859a174c39664719550b3"
-    sha256 cellar: :any_skip_relocation, arm64_linux:       "aa8e36615fd41e17459b17f690a445baee4133e3743922bb80dc8d9da14dd945"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:      "63e2fb4a19219a6e9b8b491427763a8e80289a3c314c3fa8696deb669738e905"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "6e53b3c7a34ee6b8137ce09ec70dcd3f83933c5b0f8f4c25a98e580e1e32c59c"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "6e53b3c7a34ee6b8137ce09ec70dcd3f83933c5b0f8f4c25a98e580e1e32c59c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "3eda4992160ce90a93cd314a8e4f34eadfef5777637699decc84acb14882ad3a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "0e57776d153453e753a3b16c63a962e5b94540f6a9a3d981c0f81508b481c94e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:      "aab4482f91bff89dc065ca970d32f7f4265a85ea8615d9a9057b340ce5dca5c7"
   end
 
   depends_on "cmake" => :build
