1b76dbce5846b52516c50424b903ea2fe2c6308d
Michael Cho <michael@michaelcho.dev>
2026-09-21 23:41:23 -0400

    wget: migrate to `openssl@4`

--- Formula/w/wget.rb
+++ Formula/w/wget.rb
@@ -1,11 +1,22 @@
 class Wget < Formula
   desc "Internet file retriever"
   homepage "https://www.gnu.org/software/wget/"
-  url "https://ftpmirror.gnu.org/wget/wget-1.25.0.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/wget/wget-1.25.0.tar.gz"
-  sha256 "766e48423e79359ea31e41db9e5c289675947a7fcf2efdcedb726ac9d0da3784"
   license "GPL-3.0-or-later"
+  revision 2
   compatibility_version 1
+
+  stable do
+    url "https://ftpmirror.gnu.org/wget/wget-1.25.0.tar.gz"
+    mirror "https://ftp.gnu.org/gnu/wget/wget-1.25.0.tar.gz"
+    sha256 "766e48423e79359ea31e41db9e5c289675947a7fcf2efdcedb726ac9d0da3784"
+
+    # Backport support for OpenSSL 4
+    patch do
+      url "https://gitlab.com/gnuwget/wget/-/commit/aaec8d52a06c46eeab570b8ef3e10760ebc66b7a.diff"
+      sha256 "96aa2923ce9a12dbc2c82969705da4f72792816b087dbee19c04cd2cb2769ea8"
+      type :backport
+    end
+  end
 
   bottle do
     rebuild 2
@@ -29,7 +40,7 @@ class Wget < Formula
   depends_on "pkgconf" => :build
   depends_on "libidn2"
   depends_on "libpsl"
-  depends_on "openssl@3"
+  depends_on "openssl@4"
 
   on_macos do
     depends_on "gettext"
@@ -48,7 +59,7 @@ class Wget < Formula
     system "./bootstrap", "--skip-po" if build.head?
     system "./configure", "--sysconfdir=#{etc}",
                           "--with-ssl=openssl",
-                          "--with-libssl-prefix=#{formula_opt_prefix("openssl@3")}",
+                          "--with-libssl-prefix=#{formula_opt_prefix("openssl@4")}",
                           "--disable-pcre",
                           "--disable-pcre2",
                           "--with-libpsl",
