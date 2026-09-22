acb8c478faede3e5a55d5dc6f5c5bbeffb9919b5
Michael Cho <michael@michaelcho.dev>
2026-09-22 18:22:25 -0400

    superseedr: remove openssl dependency

--- Formula/s/superseedr.rb
+++ Formula/s/superseedr.rb
@@ -17,8 +17,10 @@ class Superseedr < Formula
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
 
-  on_linux do
-    depends_on "openssl@3"
+  deny_network_access!
+
+  def fetch
+    system "cargo", "fetch", *std_cargo_fetch_args
   end
 
   def install
