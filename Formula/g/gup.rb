0f6429790e55760f1712fa920fc11286c1b7e49c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-22 02:31:54 +0000

    gup: update 1.10.0 bottle.

--- Formula/g/gup.rb
+++ Formula/g/gup.rb
@@ -7,11 +7,11 @@ class Gup < Formula
   head "https://github.com/nao1215/gup.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "3e303ac65ef1cc81f777d9c141f9a237d22ababb23114a27bbd8c89b7eb7af86"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "3e303ac65ef1cc81f777d9c141f9a237d22ababb23114a27bbd8c89b7eb7af86"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "3e303ac65ef1cc81f777d9c141f9a237d22ababb23114a27bbd8c89b7eb7af86"
-    sha256 cellar: :any_skip_relocation, arm64_linux:       "a5f92ed83ca2fd0a03887f8cdd922c70e8ce48950fceec17053d07e5f20ea7a7"
-    sha256 cellar: :any,                 x86_64_linux:      "01018b943ac1a1cd6625f0d3bd914730e9393d6bea05c10e72a4b7303e8e5f24"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "663cffb68a124ef83c1123497d369f9961e5a6fd695b4665963c2306894c95f3"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "663cffb68a124ef83c1123497d369f9961e5a6fd695b4665963c2306894c95f3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "663cffb68a124ef83c1123497d369f9961e5a6fd695b4665963c2306894c95f3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "00974a2d868653acade4e7bfba7ca0965f146b71a8bf57c3eabc61791f966e25"
+    sha256 cellar: :any,                 x86_64_linux:      "8ea4a74fe40ed2a6b58449d7e3c588bf396ef76da14f131241edd86231af637b"
   end
 
   depends_on "go"
