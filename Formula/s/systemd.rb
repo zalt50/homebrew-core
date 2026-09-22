bcba3530b6ee051b418841f473d40bddb209bd2c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-22 21:47:28 +0000

    systemd: update 262 bottle.

--- Formula/s/systemd.rb
+++ Formula/s/systemd.rb
@@ -31,8 +31,8 @@ class Systemd < Formula
   head "https://github.com/systemd/systemd.git", branch: "main"
 
   bottle do
-    sha256 arm64_linux:  "643e4e10ae816664b625f69d85706721b8a72cd326cd34354cc9108a11c8418e"
-    sha256 x86_64_linux: "4671122c97f9e9f207876b1eefc8c569d25a3dd3d0eb8676affca716c6bc5957"
+    sha256 arm64_linux:  "b8781f5f5dd6f7294408065331d564fa7e1d089fa8598e0e02431252801f2a4c"
+    sha256 x86_64_linux: "8992355dbcecf65e0fc4ba2f8929cc1204c46050dd359b7506a59524604c8f1c"
   end
 
   keg_only "it will shadow system systemd if linked"
