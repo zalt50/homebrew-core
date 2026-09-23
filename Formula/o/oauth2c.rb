2636a9c7c52015dd4cd36d89a006bc6dc1cf4073
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-23 19:24:38 +0000

    oauth2c: update 1.21.1 bottle.

--- Formula/o/oauth2c.rb
+++ Formula/o/oauth2c.rb
@@ -7,11 +7,11 @@ class Oauth2c < Formula
   head "https://github.com/SecureAuthCorp/oauth2c.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "0760551fcfbe5673c854be3edf0cbbb32eef47e9dadac0e3eff9b38ce77b0d4b"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "0760551fcfbe5673c854be3edf0cbbb32eef47e9dadac0e3eff9b38ce77b0d4b"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "0760551fcfbe5673c854be3edf0cbbb32eef47e9dadac0e3eff9b38ce77b0d4b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:       "e665041a1098d7f70cfe829f7431984c8e77a42269de4657ef8dec3506e674c0"
-    sha256 cellar: :any,                 x86_64_linux:      "0aa3076d02dab34bf8c4ed4f79bc77949ca26300200b7ef8d914fbfef8b8f039"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "d75d44a4993f6232df595f26415b5391523d2bf5f81c0f8ec355a8ec731b0be2"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "d75d44a4993f6232df595f26415b5391523d2bf5f81c0f8ec355a8ec731b0be2"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "d75d44a4993f6232df595f26415b5391523d2bf5f81c0f8ec355a8ec731b0be2"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "9c99ff220b799c8fe151c963564b8a657f1d5081d52c2a6e9ca0380e13784999"
+    sha256 cellar: :any,                 x86_64_linux:      "490575b781e17bb1df816f758a177f6d2f9cccf7019524b2dadd0c1e9910f181"
   end
 
   depends_on "go" => :build
