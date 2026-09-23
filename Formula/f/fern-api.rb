212ac87b0e4e63296862f135efd6414fe1ddcfac
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-23 17:03:51 +0000

    fern-api 5.132.0

--- Formula/f/fern-api.rb
+++ Formula/f/fern-api.rb
@@ -1,8 +1,8 @@
 class FernApi < Formula
   desc "Stripe-level SDKs and Docs for your API"
   homepage "https://buildwithfern.com/"
-  url "https://registry.npmjs.org/fern-api/-/fern-api-5.130.0.tgz"
-  sha256 "f03231df0a0c92c369e07b3302c069f89724dc0548106ac7fc4fa2da57946075"
+  url "https://registry.npmjs.org/fern-api/-/fern-api-5.132.0.tgz"
+  sha256 "48bea3fd8e1f0166b24c453b51f1cf5c538022f50e7a1cc29a39f37e27f8b1e4"
   license "Apache-2.0"
 
   livecheck do
