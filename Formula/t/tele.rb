0a38a72143d75728a24738159bd0ad49b972245e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-23 12:15:24 +0000

    tele: update 1.11.8 bottle.

--- Formula/t/tele.rb
+++ Formula/t/tele.rb
@@ -6,11 +6,11 @@ class Tele < Formula
   license "GPL-3.0-only"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "3b87d6ee2d15b8f7673258ea17523f22ba9e9c75e76cb4c08d17df0d47994975"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "6c90769aa794f57ab3d84788dff5e1e5c337b073c75af532f771a6e8922f1f2c"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "7fdfad5eab1256c1a63c523411f80e2564890239f078449edc8ef7b513f5e111"
-    sha256 cellar: :any_skip_relocation, arm64_linux:       "40ffdbfa4b551dc35b2623132bf28eafd656ce9e1782b1bb2e0417106d92e6ca"
-    sha256 cellar: :any,                 x86_64_linux:      "11d573d9125e65d45f8bcb2c0bb9fe3a3f948e97dea38515ba61635f72c89ce3"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "f1838ba5250b6689d9522895b119417e4436bc477df58a7027defcf77bffa8d3"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "d6a18bfd00ff2544be4847148d4ff244059ec6bbf35a1a0847364b96c6de2351"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "4df49378fef58e7a66be95d8531b81d4c45ea1a77b64c61649355c4660098e4f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "8800983c197d207d7e13698351b85b0789979e1bac90efc439bee2520bc1a53e"
+    sha256 cellar: :any,                 x86_64_linux:      "24b464499af1862b54fad2e4bd1deeb38584b0d0896481cf5e71d3ab7ca2b69f"
   end
 
   depends_on "go" => :build
