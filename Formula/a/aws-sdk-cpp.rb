353db8c95b33e1609616b26c95257a7e15d97e1e
botantony <antonsm21@gmail.com>
2026-09-23 09:14:39 +0200

    aws-sdk-cpp: revision bump (aws-c-s3 1.2.0)

--- Formula/a/aws-sdk-cpp.rb
+++ Formula/a/aws-sdk-cpp.rb
@@ -4,7 +4,7 @@ class AwsSdkCpp < Formula
   url "https://github.com/aws/aws-sdk-cpp/archive/refs/tags/1.11.885.tar.gz"
   sha256 "b9fb6d2accb9b27bbe62b168556a8666f3e49deedbfa2d924601b77ecc70b563"
   license "Apache-2.0"
-  revision 1
+  revision 2
   compatibility_version 3
   head "https://github.com/aws/aws-sdk-cpp.git", branch: "main"
 
