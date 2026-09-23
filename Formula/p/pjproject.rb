2aba792ed1f13d3aa2907095a61bacccab456141
Michael Cho <michael@michaelcho.dev>
2026-09-22 20:54:42 -0400

    pjproject: migrate to `openssl@4`

--- Formula/p/pjproject.rb
+++ Formula/p/pjproject.rb
@@ -1,10 +1,21 @@
 class Pjproject < Formula
   desc "C library for multimedia protocols such as SIP, SDP, RTP and more"
   homepage "https://www.pjsip.org/"
-  url "https://github.com/pjsip/pjproject/archive/refs/tags/2.17.tar.gz"
-  sha256 "065fe06c06788d97c35f563796d59f00ce52fe9558a52d7b490a042a966facce"
   license "GPL-2.0-or-later"
   head "https://github.com/pjsip/pjproject.git", branch: "master"
+
+  stable do
+    url "https://github.com/pjsip/pjproject/archive/refs/tags/2.17.tar.gz"
+    sha256 "065fe06c06788d97c35f563796d59f00ce52fe9558a52d7b490a042a966facce"
+
+    # Backport support for OpenSSL 4.0
+    patch do
+      url "https://github.com/pjsip/pjproject/commit/3923fad2e4f6f3403c3d6f1176b113c1c1b91066.patch?full_index=1"
+      sha256 "6958040fc0b0502381aa1514cacf1d947ea1a6104b2da388e1c99870f67bc998"
+      type :backport
+      resolves "https://github.com/pjsip/pjproject/pull/5036"
+    end
+  end
 
   livecheck do
     url :stable
@@ -21,7 +32,7 @@ class Pjproject < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:      "75b740db8f890d9877cbae6441375d9c960f971252ca2b2331396b412fbad3d2"
   end
 
-  depends_on "openssl@3"
+  depends_on "openssl@4"
 
   def install
     system "./configure", *std_configure_args
