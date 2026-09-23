9bda3ad6557e8a3b460cb5748ad29a5c9b512003
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-23 23:00:41 +0000

    headscale-cli: update 0.29.4 bottle.

--- Formula/h/headscale-cli.rb
+++ Formula/h/headscale-cli.rb
@@ -11,13 +11,11 @@ class HeadscaleCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "11ae4780a047a61234f6100f25429fbaeccc90cc987ea663975197e71fe7345f"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "6114ea7d64e423dd3d0c6e473b3eb7c9b2a56ad88285550ef68c368074362d12"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "a2636b371992ad4f49f4d3bd76d264a0be9fb24776d5fa20fb07c86cc000bbaf"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:      "38d32dc36e758fac95495bac999cf6d2bcd00ca3ac2f4aa1e22722c88373ca58"
-    sha256 cellar: :any_skip_relocation, sonoma:            "bba4570479819538375c813a51144011d37ec62d4b4b675692cf8b2b8cb8efc3"
-    sha256 cellar: :any_skip_relocation, arm64_linux:       "9956734bd49b726d9e6d451b93054b811b7aa8a0c6a645dd43dc4119c42aeb8f"
-    sha256 cellar: :any,                 x86_64_linux:      "c246afb358ee76fca8b6a7b115f71087fe42cc78b6ee32ce733e191d77e3cad2"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "f606d2a20c953551e3dacf61ab4acc1c0e947956a47b5372df29ad55bab932cf"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "7520ef980a345dd08830bdb9bdaeb01477b65f83065e0b527ec8489d7f252de9"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "431f3183c2f83e513cc0d558e0b5c81026d3ffea4608e625361077ddc8392149"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "a4c01e0b08eab87608a3e30a296ce6d7c7d08a86dce2bdf5db516511b1555697"
+    sha256 cellar: :any,                 x86_64_linux:      "afde48459ea0913b575b25e12cfc90d9d2c0f739ca7b04b230ba8d7c3bd95a92"
   end
 
   depends_on "go" => :build
