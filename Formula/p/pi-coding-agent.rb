fd43c4b24bf1307526c16806d436d3491372f978
Vinicios <42501522+viniciosrab@users.noreply.github.com>
2026-09-22 23:31:59 -0300

    pi-coding-agent 0.87.1

--- Formula/p/pi-coding-agent.rb
+++ Formula/p/pi-coding-agent.rb
@@ -1,8 +1,8 @@
 class PiCodingAgent < Formula
   desc "AI agent toolkit"
   homepage "https://pi.dev/"
-  url "https://registry.npmjs.org/@earendil-works/pi-coding-agent/-/pi-coding-agent-0.86.1.tgz"
-  sha256 "8dff93e6fa03e0d498e72a78d2c7bb5f094f5e06ee268e6abd000ba2984a0b6a"
+  url "https://registry.npmjs.org/@earendil-works/pi-coding-agent/-/pi-coding-agent-0.87.1.tgz"
+  sha256 "1423ee3c61e7c96464e1cbf3c8dc24d3056cb3410995c3671a98c3ecc527540f"
   license "MIT"
 
   bottle do
