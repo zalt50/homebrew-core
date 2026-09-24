86d6955f9b69822f3e0d745fc08f2fe696e7ee2d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-24 20:28:28 +0000

    biber: update 2.22 bottle.

--- Formula/b/biber.rb
+++ Formula/b/biber.rb
@@ -6,12 +6,12 @@ class Biber < Formula
   license "Artistic-2.0"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any, arm64_golden_gate: "3ac812c44d3c8b0a8a1a38347b5728a329dd3fb0bd35e95447b94cecac03161c"
-    sha256 cellar: :any, arm64_tahoe:       "ff71789356a3285f6854b6cb5dd75beb85fff1997dab2282517d0701ee06b7e0"
-    sha256 cellar: :any, arm64_sequoia:     "607af65f5f2a5f697a9f2d1a92150ade4630aaf0139f7b152732d366275de8f2"
-    sha256 cellar: :any, arm64_linux:       "0468c4ebd19b8bf432b94bc104093fae6610b88b6a3740a16ef74b7fec4c84e4"
-    sha256 cellar: :any, x86_64_linux:      "c25d46fc70146117cb8bee0030103076cd34079b509f4db211f292a5750f156d"
+    rebuild 2
+    sha256 cellar: :any, arm64_golden_gate: "8b7449bedeb39cfb25cc533b9448a473283f306e6325c8ae1ce6253ea63dd6ec"
+    sha256 cellar: :any, arm64_tahoe:       "de9f5e453d737f1d25bae91b5fc87c16af5e8174a4f3c378e5de0a4949e285b1"
+    sha256 cellar: :any, arm64_sequoia:     "0d8056305d36256d3346916977baf4c32fa6dfeaa502bbd1e12e22e7f01bd68a"
+    sha256 cellar: :any, arm64_linux:       "a9417721640ff25ce4713575a5e3885222e42fc94621991506d082f6ebf4d6fa"
+    sha256 cellar: :any, x86_64_linux:      "edd833e7e014a511884d29368463478c2e984e1c94246c80af440bb9bb1489be"
   end
 
   depends_on "pkgconf" => :build
