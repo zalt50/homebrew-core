e45426521c7b5cbc1c0c74a79b6cfa93a5f87031
Michael Cho <michael@michaelcho.dev>
2026-09-24 19:11:33 -0400

    cassandra-cpp-driver: migrate to `openssl@4`

--- Formula/c/cassandra-cpp-driver.rb
+++ Formula/c/cassandra-cpp-driver.rb
@@ -1,10 +1,21 @@
 class CassandraCppDriver < Formula
   desc "DataStax C/C++ Driver for Apache Cassandra"
   homepage "https://docs.datastax.com/en/developer/cpp-driver/latest"
-  url "https://github.com/apache/cassandra-cpp-driver/archive/refs/tags/2.17.1.tar.gz"
-  sha256 "e6ab5f5c60a916dd6c0dd9a19a883a4a1ab3d6b4e95cab925a186fecff08344e"
   license "Apache-2.0"
   head "https://github.com/apache/cassandra-cpp-driver.git", branch: "trunk"
+
+  stable do
+    url "https://github.com/apache/cassandra-cpp-driver/archive/refs/tags/2.17.1.tar.gz"
+    sha256 "e6ab5f5c60a916dd6c0dd9a19a883a4a1ab3d6b4e95cab925a186fecff08344e"
+
+    # Backport support for OpenSSL 4
+    patch do
+      url "https://github.com/apache/cassandra-cpp-driver/commit/b01f0dcfbec419ffeaee31f9a61f4bc86f790dec.patch?full_index=1"
+      sha256 "b7d1aa90d4ad45cdfa119cfdc944ed1d7a0f743404dd242fed323be49a02417c"
+      type :backport
+      resolves "https://github.com/apache/cassandra-cpp-driver/pull/590"
+    end
+  end
 
   livecheck do
     url :stable
@@ -24,7 +35,7 @@ class CassandraCppDriver < Formula
 
   depends_on "cmake" => :build
   depends_on "libuv"
-  depends_on "openssl@3"
+  depends_on "openssl@4"
 
   on_linux do
     depends_on "pkgconf" => :build
