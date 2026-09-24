c2500ec8fd163833d1739408ab8307cf52955768
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-24 20:39:15 +0000

    ruff 0.16.9

--- Formula/r/ruff.rb
+++ Formula/r/ruff.rb
@@ -1,8 +1,8 @@
 class Ruff < Formula
   desc "Extremely fast Python linter, written in Rust"
   homepage "https://docs.astral.sh/ruff/"
-  url "https://github.com/astral-sh/ruff/archive/refs/tags/0.16.8.tar.gz"
-  sha256 "42f2b5af083151646886e7eddcf10ebfdfa89d4ba1a8146b82422497688921d3"
+  url "https://github.com/astral-sh/ruff/archive/refs/tags/0.16.9.tar.gz"
+  sha256 "3b75e09dd9cf0fafbb2ae1fe13bcfdb4d43d3c07cd8578e2c48bee86b2d328e3"
   license "MIT"
   head "https://github.com/astral-sh/ruff.git", branch: "main"
 
