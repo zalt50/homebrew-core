d36dc25ca9f3ee0768c4292421b9e05dd38ef235
Michael Cho <michael@michaelcho.dev>
2026-09-23 22:12:45 -0400

    libcoap: migrate to `openssl@4`

--- Formula/lib/libcoap.rb
+++ Formula/lib/libcoap.rb
@@ -20,7 +20,20 @@ class Libcoap < Formula
   depends_on "automake" => :build
   depends_on "libtool" => :build
   depends_on "pkgconf" => :build
-  depends_on "openssl@3"
+  depends_on "openssl@4"
+
+  # Backport support for OpenSSL 4
+  patch do
+    url "https://github.com/obgm/libcoap/commit/83ceb9687cfcc3c4683e8b1423ac00ba01c5be3c.patch?full_index=1"
+    sha256 "4d22e9267fe8fe8d23424df1ec1fac1ccfffd50d07010d2f459402feb71abcdf"
+    type :backport
+  end
+  patch do
+    url "https://github.com/obgm/libcoap/commit/5ea8b7d5e956d1574d78115da007bf23d118f1ae.patch?full_index=1"
+    sha256 "58f7d528e62c7a887201f7c47ae91dbe8b09b0f5cdc5a26fb3c685bb4815ea69"
+    type :backport
+    resolves "https://github.com/obgm/libcoap/pull/2048"
+  end
 
   allow_network_access! :test
 
