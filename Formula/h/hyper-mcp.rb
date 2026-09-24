da77a2310d97abf3cd496c6b3412a550f2f68339
Michael Cho <michael@michaelcho.dev>
2026-09-24 12:47:07 -0400

    hyper-mcp: migrate to `openssl@4`

--- Formula/h/hyper-mcp.rb
+++ Formula/h/hyper-mcp.rb
@@ -25,7 +25,13 @@ class HyperMcp < Formula
 
   on_linux do
     depends_on "pkgconf" => :build
-    depends_on "openssl@3"
+    depends_on "openssl@4"
+  end
+
+  deny_network_access!
+
+  def fetch
+    system "cargo", "fetch", *std_cargo_fetch_args
   end
 
   def install
