c4a319e4fd05bdc55fcd12313d4730399ad6a154
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-24 11:55:35 +0000

    meta-package-manager: update 8.0.1 bottle.

--- Formula/m/meta-package-manager.rb
+++ Formula/m/meta-package-manager.rb
@@ -10,11 +10,11 @@ class MetaPackageManager < Formula
   head "https://github.com/kdeldycke/meta-package-manager.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "d4677225f4ccb89fbdb7a38644447be80a494f8f5a72abadf4abc5b73524d4d0"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "d4677225f4ccb89fbdb7a38644447be80a494f8f5a72abadf4abc5b73524d4d0"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "d4677225f4ccb89fbdb7a38644447be80a494f8f5a72abadf4abc5b73524d4d0"
-    sha256 cellar: :any_skip_relocation, arm64_linux:       "cf95cd6709766da7be01d20dc32490194302bebd0f73d5e07cd3db939ff2b222"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:      "a663fd3b5d487cba8cc80b939d5c04b20982fcc1396745643f56d000fbc73bb1"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "4af1849c93d5d9a5acda66f6ee79617f3fa1eb754eaf5da053a4828d53753756"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "4af1849c93d5d9a5acda66f6ee79617f3fa1eb754eaf5da053a4828d53753756"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "4af1849c93d5d9a5acda66f6ee79617f3fa1eb754eaf5da053a4828d53753756"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "5acd44b8655d0651acd8ae53f2559a3e01003b2fefeb431fa9e6892a4a148b5b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:      "5acd44b8655d0651acd8ae53f2559a3e01003b2fefeb431fa9e6892a4a148b5b"
   end
 
   depends_on "rust" => :build
