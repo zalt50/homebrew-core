e9e13d170870b47007197dd2d69f51c1a2607cab
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-24 11:40:34 +0000

    cronboard: update 1.2.0 bottle.

--- Formula/c/cronboard.rb
+++ Formula/c/cronboard.rb
@@ -8,13 +8,11 @@ class Cronboard < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any, arm64_golden_gate: "f4ba0b5af0a837cc235665d112260d18dbf4bffd36e0da57d8bf148c714f324d"
-    sha256 cellar: :any, arm64_tahoe:       "995c0c0f9556509f6d943e5dd9482fec2101319807258b6918b9ce5b8624a4fe"
-    sha256 cellar: :any, arm64_sequoia:     "3cf0cc5d139fd00016b7979d30b24d79418fc5b73bdf773405329fb75d5e4f0c"
-    sha256 cellar: :any, arm64_sonoma:      "083bed9131a6b2e899fe6600ba10bd31b8d76ff9ceb98f47d27e1181a3550a42"
-    sha256 cellar: :any, sonoma:            "700f4b9341d2377e75e7d915e8306e6e50628d3bd52e6ac17639479c35ec1e1c"
-    sha256 cellar: :any, arm64_linux:       "1bf4835f9eb53423a936303c1687aea66052673df682b55f1bcd7477e358dee8"
-    sha256 cellar: :any, x86_64_linux:      "af37e0631fb5725a497635d8b0bd43bd7646fda817c1d94afaf1f9c88dc5a88b"
+    sha256 cellar: :any, arm64_golden_gate: "463d46098c6d3061f2a3abc09d2fc5c809b7f1ec8f64805993382d498a7a9fe0"
+    sha256 cellar: :any, arm64_tahoe:       "1c256bdfb8753d4dc77770911a960af73e7fc2716d54e4dd0a31fdbe1d8e529c"
+    sha256 cellar: :any, arm64_sequoia:     "0ba754e6c7cf84e8b8f0d92b5400ed0fd2498f1a7c37f544ec22adfa0be7b3b6"
+    sha256 cellar: :any, arm64_linux:       "966b7103c98c7463fbe44ecc84218021cc973297376f6b66f7d6c6ca2b18c3e9"
+    sha256 cellar: :any, x86_64_linux:      "9126b76e41db77353763a5a08fb17f241b6dfcf19619009744417c111f9667cf"
   end
 
   # `pkgconf` and `rust` are for bcrypt
