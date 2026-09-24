fbba1f0e0cef69fa359c00b3634a93798164b0b7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-24 12:35:16 +0000

    synergy-core: update 1.21.3 bottle.

--- Formula/s/synergy-core.rb
+++ Formula/s/synergy-core.rb
@@ -17,11 +17,11 @@ class SynergyCore < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_golden_gate: "d71795492e91748665c7a84489e191e8202bc03ea53b89b4cf562e2a7a64dad5"
-    sha256 cellar: :any, arm64_tahoe:       "943b18663b65ffb1818fbf0fa72cd59c5037c8ca81a17ca90ba2180dfca3623a"
-    sha256 cellar: :any, arm64_sequoia:     "dfe44cc8bbb0aaa82c397562cb275867b4d7d0c06228a60b08bb4d9d60580bb2"
-    sha256 cellar: :any, arm64_linux:       "000a5cda373ba98d0399d4fb7d9b7dfa1389ba273bdf31efc1669287af4d5a3a"
-    sha256 cellar: :any, x86_64_linux:      "fb8d58163dc366947ad5c37a9e078ba939f908f594bb1db3799fc9e8f3876135"
+    sha256 cellar: :any, arm64_golden_gate: "4cda7e29e117c1f87d5f4356c494d4290ac908f4b69c0469c7b8042a6f3e6af7"
+    sha256 cellar: :any, arm64_tahoe:       "00b43438d4da5f6711074f3c3a769b1ccd44c8f07375319b71033d810d3e39c2"
+    sha256 cellar: :any, arm64_sequoia:     "19dfde03d384f3d8ac947d97e03b5806a5cd2d5345f09e8ee19ba33995bed270"
+    sha256 cellar: :any, arm64_linux:       "bc6243721e490e0f8eddd7d51f2a4f1422d8089e49f3a24fcb299ef403cc4033"
+    sha256 cellar: :any, x86_64_linux:      "c5f0d6c144e21f1e70f265ac98984dd2fd4e8a5f060958541ff18dc39eb52340"
   end
 
   depends_on "cmake" => :build
