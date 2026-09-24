d5db6b63ff712a99b6c8262bc378fd402f993e37
Michael Cho <michael@michaelcho.dev>
2026-09-23 20:10:13 -0400

    mosh: migrate to `openssl@4`

--- Formula/m/mosh.rb
+++ Formula/m/mosh.rb
@@ -31,7 +31,7 @@ class Mosh < Formula
   end
 
   on_linux do
-    depends_on "openssl@3" # Uses CommonCrypto on macOS
+    depends_on "openssl@4" # Uses CommonCrypto on macOS
     depends_on "zlib-ng-compat"
   end
 
