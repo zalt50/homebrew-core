1a981ddf0ac3af80c06eb3ac4ba7a071773b675e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-24 14:24:50 +0000

    php@8.2 8.2.34

--- Formula/p/php@8.2.rb
+++ Formula/p/php@8.2.rb
@@ -2,9 +2,9 @@ class PhpAT82 < Formula
   desc "General-purpose scripting language"
   homepage "https://www.php.net/"
   # Should only be updated if the new version is announced on the homepage, https://www.php.net/
-  url "https://www.php.net/distributions/php-8.2.33.tar.xz"
-  mirror "https://ftp.osuosl.org/pub/php/php-8.2.33.tar.xz"
-  sha256 "fbdeace9b38220436a4c8fd79b900df92878151db145e641750743a283b514c1"
+  url "https://www.php.net/distributions/php-8.2.34.tar.xz"
+  mirror "https://ftp.osuosl.org/pub/php/php-8.2.34.tar.xz"
+  sha256 "5351330c54de240f54f7527c26ef292e239749e6fe11db0330acb5317e02bb39"
   license all_of: [
     "PHP-3.01",
 
