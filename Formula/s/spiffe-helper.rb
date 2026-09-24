b6f4b642cf0ddbfd237f5189840ec36ef26a2633
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-24 09:35:40 +0000

    spiffe-helper: update 0.12.1 bottle.

--- Formula/s/spiffe-helper.rb
+++ Formula/s/spiffe-helper.rb
@@ -7,11 +7,11 @@ class SpiffeHelper < Formula
   head "https://github.com/spiffe/spiffe-helper.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "18c7eb0639a9c9f0eb597eefbc5310c9ed7ed66ebc98babc4fc084583063473b"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "18c7eb0639a9c9f0eb597eefbc5310c9ed7ed66ebc98babc4fc084583063473b"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "18c7eb0639a9c9f0eb597eefbc5310c9ed7ed66ebc98babc4fc084583063473b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:       "ba482378cc2b8b0efcdfc2d42cf7bf04c3e128e4f7e4d8e68992694fe7d77ba8"
-    sha256 cellar: :any,                 x86_64_linux:      "09e314b30ebfcac674e8022a5fd55d8ea648ae5ab0e92b9bc78110db67e58328"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "29ec768ad9f6ea8cda6c031ed59a9f50d7732759068e3e06dca1b4520535e428"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "29ec768ad9f6ea8cda6c031ed59a9f50d7732759068e3e06dca1b4520535e428"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "29ec768ad9f6ea8cda6c031ed59a9f50d7732759068e3e06dca1b4520535e428"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "2286b5e6badbe348f2b6e9264cfb30d12301c9bbe83c372259f070022cc06194"
+    sha256 cellar: :any,                 x86_64_linux:      "9e74a289fe8179c655b2fa30b16722b62aae983a4e544fd432c74912214a2f6f"
   end
 
   depends_on "go" => :build
