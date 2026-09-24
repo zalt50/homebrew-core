10e276f489a21067e5a4778863427588882a22ad
Michael Cho <michael@michaelcho.dev>
2026-09-23 21:15:11 -0400

    libtpms: migrate to `openssl@4`

--- Formula/lib/libtpms.rb
+++ Formula/lib/libtpms.rb
@@ -4,6 +4,7 @@ class Libtpms < Formula
   url "https://github.com/stefanberger/libtpms/archive/refs/tags/v0.10.2.tar.gz"
   sha256 "edac03680f8a4a1c5c1d609a10e3f41e1a129e38ff5158f0c8deaedc719fb127"
   license "BSD-2-Clause"
+  revision 1
 
   bottle do
     sha256 cellar: :any,                 arm64_golden_gate: "5c1d8448048e3d0f0fac6a74c135e1a3a435196f3d484d4966a165a8eb106f94"
@@ -19,7 +20,7 @@ class Libtpms < Formula
   depends_on "automake" => :build
   depends_on "libtool" => :build
   depends_on "pkgconf" => :build
-  depends_on "openssl@3"
+  depends_on "openssl@4"
 
   def install
     system "./autogen.sh", "--with-openssl", "--with-tpm2", *std_configure_args
