a569256c1711173ea54ec7d7e313d0dc8ef9d0cd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-24 17:30:08 +0000

    cloudflare-wrangler: update 4.137.0 bottle.

--- Formula/c/cloudflare-wrangler.rb
+++ Formula/c/cloudflare-wrangler.rb
@@ -6,11 +6,11 @@ class CloudflareWrangler < Formula
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
-    sha256 cellar: :any, arm64_golden_gate: "402c43556a0519978b69375a455ac577e4fbb91fa8dc7e89683561b5005519bb"
-    sha256 cellar: :any, arm64_tahoe:       "402c43556a0519978b69375a455ac577e4fbb91fa8dc7e89683561b5005519bb"
-    sha256 cellar: :any, arm64_sequoia:     "402c43556a0519978b69375a455ac577e4fbb91fa8dc7e89683561b5005519bb"
-    sha256 cellar: :any, arm64_linux:       "14994bf847a041a0ec68277ebf25795aee9317c42859d61ebae2ccaae12245b8"
-    sha256 cellar: :any, x86_64_linux:      "931e4db9ed3525621ec42432890c9314f9ff067a39934c8f04972aa84434e9f3"
+    sha256 cellar: :any, arm64_golden_gate: "6a0f0e3467c37863f66e297475b3aebe68c2da30faf6f0dd46939a22cfac6bee"
+    sha256 cellar: :any, arm64_tahoe:       "6a0f0e3467c37863f66e297475b3aebe68c2da30faf6f0dd46939a22cfac6bee"
+    sha256 cellar: :any, arm64_sequoia:     "6a0f0e3467c37863f66e297475b3aebe68c2da30faf6f0dd46939a22cfac6bee"
+    sha256 cellar: :any, arm64_linux:       "9ca9b64f2bb10d4e5e2ba7807c44a0bef5506f250a6085b952995b1861652f74"
+    sha256 cellar: :any, x86_64_linux:      "7a55f4e350e6aa7417ffef8ea090f88ae7d5e8fe4341f335638c3c102b7897df"
   end
 
   depends_on "node"
