db748c4f8e0af13db99a43705f5f2bbe52d68ba3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-25 08:58:24 +0000

    kubectl-radar: add 1.14.1 bottle.

--- Formula/k/kubectl-radar.rb
+++ Formula/k/kubectl-radar.rb
@@ -5,6 +5,14 @@ class KubectlRadar < Formula
   sha256 "7e02a2ad29f3653dfea485bbab8934fcbf2b82045f57e7ce84871605f4b6fded"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "f9d3f3523592f1390ab1a9f1c4f62af98182eb9895571ab3dd2dc3e2214bde3b"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "03971193f773741a244a2412b84433d972f77c01cd99321dabc515abe609dfa7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "f46e9f96da88d9f981899b70e199c505d1f8fb955fc612beec00fe7d70afcc20"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "52126efcf02cb7f917ea3f02308683a3ffd9413d007bea3fc955d2515ce5de2a"
+    sha256 cellar: :any,                 x86_64_linux:      "d44ff54988f92031bfe3103f6e70ddf2dd55cbb5079bd8c3341e3476949ea5a8"
+  end
+
   depends_on "go" => :build
   depends_on "node" => :build
 
