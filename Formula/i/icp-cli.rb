6556f40748069c3ca10b39ffe50f56689e06176c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-25 05:58:22 +0000

    icp-cli: update 1.6.0 bottle.

--- Formula/i/icp-cli.rb
+++ Formula/i/icp-cli.rb
@@ -6,12 +6,11 @@ class IcpCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any, arm64_golden_gate: "903d119fc57276078cfff33ef3daeb007795b6c3d304c29c0174d2708c0e44dc"
-    sha256 cellar: :any, arm64_tahoe:       "a915e90f32895374774ea34a08c99d5e2bca064109190185b52f7c978ec6e916"
-    sha256 cellar: :any, arm64_sequoia:     "daf897a6d8d223ff0df6d3d07eb03a9996f5ef8971058c0aaa8710eaf7ea1b2d"
-    sha256 cellar: :any, arm64_sonoma:      "56fb19618eaf0bf22e7ea27d6e60a46819123cb2042caa88c92da71a9010a149"
-    sha256 cellar: :any, arm64_linux:       "f96d106a7e305146ce23c6c2db69e6ae5955553823a054ab2b437ae9fe89da87"
-    sha256 cellar: :any, x86_64_linux:      "166bea812c67463e9d29c2174385698f03734e11c810b2707d8b884afc561291"
+    sha256 cellar: :any, arm64_golden_gate: "2f1cf2897c761e6b7ceb23f701e989a12a383f3f428aa6f345ba7b61b463616c"
+    sha256 cellar: :any, arm64_tahoe:       "9c5301855f0e6d57726b9c84642290c1d849c10af0d5e7041c64744ed8091339"
+    sha256 cellar: :any, arm64_sequoia:     "ebe097dbc3ee5cc82d987738103fbfaaf2ae3c78324daa1e14f0b5c599db4666"
+    sha256 cellar: :any, arm64_linux:       "15564ce13051d357f33eb434b48093c00b4d2fc0f21e748d7bca1fa8a0e4c6b4"
+    sha256 cellar: :any, x86_64_linux:      "ff8c8460f898f79ba35687969a1e3d5abc21817998629b09899ed2b4063f8f07"
   end
 
   depends_on "lld" => :build # for `wasm-ld`
