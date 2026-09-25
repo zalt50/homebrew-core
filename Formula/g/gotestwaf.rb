e4d0187388e8cc7029bb375e09f9b0f033355cb5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-25 05:17:46 +0000

    gotestwaf 0.5.9

--- Formula/g/gotestwaf.rb
+++ Formula/g/gotestwaf.rb
@@ -1,8 +1,8 @@
 class Gotestwaf < Formula
   desc "Tool for API and OWASP attack simulation"
   homepage "https://lab.wallarm.com/test-your-waf-before-hackers/"
-  url "https://github.com/wallarm/gotestwaf/archive/refs/tags/v0.5.8.tar.gz"
-  sha256 "46e6cf3f07957aa58683447572cb9bd68c51aa684b3a5f84439f0f1cbd83621c"
+  url "https://github.com/wallarm/gotestwaf/archive/refs/tags/v0.5.9.tar.gz"
+  sha256 "95a504b2e456f102a9973a973c8a831ff5e6d6be735ac0b8442a77f0a4f6a4d7"
   license "MIT"
   head "https://github.com/wallarm/gotestwaf.git", branch: "master"
 
