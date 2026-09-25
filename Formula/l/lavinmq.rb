b14ff1afd88b000ee46f1250c3f6c22f16dbeb5d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-25 14:29:43 +0000

    lavinmq 2.10.0

--- Formula/l/lavinmq.rb
+++ Formula/l/lavinmq.rb
@@ -1,8 +1,8 @@
 class Lavinmq < Formula
   desc "Message broker implementing the AMQP 0-9-1 and MQTT protocols"
   homepage "https://lavinmq.com"
-  url "https://github.com/cloudamqp/lavinmq/archive/refs/tags/v2.9.3.tar.gz"
-  sha256 "a6f14b3a6b4d80a4e58b4a46124522d79390588025c97ab5c290a0fe6cf0262e"
+  url "https://github.com/cloudamqp/lavinmq/archive/refs/tags/v2.10.0.tar.gz"
+  sha256 "f7e2ddd3110be9ece9821dd3ad9ebe9f82d142c8431105ea3ad0043b4b25619d"
   license "Apache-2.0"
   head "https://github.com/cloudamqp/lavinmq.git", branch: "main"
 
@@ -32,6 +32,13 @@ class Lavinmq < Formula
     depends_on "zlib-ng-compat"
   end
 
+  deny_network_access!
+
+  def fetch
+    # Fetch the shards and the web UI's JavaScript libraries
+    system "make", "lib", "js"
+  end
+
   def install
     ENV.prepend_path "PATH", formula_opt_libexec("coreutils")/"gnubin" if OS.mac?
 
@@ -55,7 +62,9 @@ class Lavinmq < Formula
 
   test do
     ENV["LAVINMQCTL_CONTROL_UNIX_PATH"] = control_unix_path = testpath/"lavinmqctl.sock"
-    pid = spawn bin/"lavinmq", "--data-dir", testpath/"data", "--control-unix-path", control_unix_path
+    # Disable the TCP listeners, which the network sandbox doesn't allow
+    tcp_ports = %w[amqp http mqtt mqtts metrics-http].map { |listener| "--#{listener}-port=-1" }
+    pid = spawn bin/"lavinmq", "--data-dir", testpath/"data", "--control-unix-path", control_unix_path, *tcp_ports
     30.times do
       break if control_unix_path.exist?
 
