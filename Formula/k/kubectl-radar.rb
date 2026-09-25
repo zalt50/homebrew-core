442c8cacf9b711a7ca7e9f500a4a21bd8ea30c49
botantony <antonsm21@gmail.com>
2026-09-25 11:43:17 +0200

    kubectl-radar: add livecheck block
    Needed as some old tags don't use semver:
    
    ```
    Formula:          kubectl-radar
    livecheck block?: No
    
    URL:              https://github.com/skyhook-io/radar/archive/refs/tags/v1.14.1.tar.gz
    URL (processed):  https://github.com/skyhook-io/radar.git
    Strategy:         Git
    
    Matched Versions:
    1109, 2-1105, 8s-ui-v1.0.0, 8s-ui-v1.1.0, 8s-ui-v1.1.1, 8s-ui-v1.10.0, 8s-ui-v1.10.1, 8s-ui-v1.10.2, 8s-ui-v1.10.3, 8s-ui-v1.10.4, 8s-ui-v1.10.5, 8s-ui-v1.11.0, 8s-ui-v1.12.0, 8s-ui-v1.13.0, 8s-ui-v1.13.1, 8s-ui-v1.13.2, 8s-ui-v1.13.3, 8s-ui-v1.13.4, 8s-ui-v1.14.0, 8s-ui-v1.14.1, 8s-ui-v1.14.10, 8s-ui-v1.14.2, 8s-ui-v1.14.3, 8s-ui-v1.14.4, 8s-ui-v1.14.5, 8s-ui-v1.14.6, 8s-ui-v1.14.7, 8s-ui-v1.14.8, 8s-ui-v1.14.9, 8s-ui-v1.2.0, 8s-ui-v1.2.1, 8s-ui-v1.2.2, 8s-ui-v1.2.3, 8s-ui-v1.2.4, 8s-ui-v1.3.0, 8s-ui-v1.3.1, 8s-ui-v1.3.2, 8s-ui-v1.4.0, 8s-ui-v1.4.1, 8s-ui-v1.4.2, 8s-ui-v1.4.3, 8s-ui-v1.5.0, 8s-ui-v1.5.1, 8s-ui-v1.5.10, 8s-ui-v1.5.11, 8s-ui-v1.5.12, 8s-ui-v1.5.13, 8s-ui-v1.5.2, 8s-ui-v1.5.3, 8s-ui-v1.5.4, 8s-ui-v1.5.5, 8s-ui-v1.5.6, 8s-ui-v1.5.7, 8s-ui-v1.5.8, 8s-ui-v1.5.9, 8s-ui-v1.6.0, 8s-ui-v1.6.1, 8s-ui-v1.6.2, 8s-ui-v1.7.0, 8s-ui-v1.7.1, 8s-ui-v1.7.10, 8s-ui-v1.7.11, 8s-ui-v1.7.12, 8s-ui-v1.7.13, 8s-ui-v1.7.14, 8s-ui-v1.7.15, 8s-ui-v1.7.2, 8s-ui-v1.7.3, 8s-ui-v1.7.4, 8s-ui-v1.7.5, 8s-ui-v1.7.6, 8s-ui-v1.7.7, 8s-ui-v1.7.8, 8s-ui-v1.7.9, 8s-ui-v1.8.0, 8s-ui-v1.8.1, 8s-ui-v1.8.10, 8s-ui-v1.8.11, 8s-ui-v1.8.12, 8s-ui-v1.8.13, 8s-ui-v1.8.14, 8s-ui-v1.8.15, 8s-ui-v1.8.2, 8s-ui-v1.8.3, 8s-ui-v1.8.4, 8s-ui-v1.8.5, 8s-ui-v1.8.6, 8s-ui-v1.8.7, 8s-ui-v1.8.8, 8s-ui-v1.8.9, 8s-ui-v1.9.0, 8s-ui-v1.9.1, 1.0.6, 1.0.7, 1.1.0, 1.1.1, 1.1.2, 1.1.3, 1.10.0, 1.10.1, 1.10.2, 1.11.0, 1.12.0, 1.12.1, 1.12.2, 1.12.3, 1.13.0, 1.13.1, 1.14.0, 1.14.1, 1.14.2, 1.14.3, 1.2.0, 1.2.1, 1.3.0, 1.3.1, 1.4.0, 1.4.1, 1.4.2, 1.4.3, 1.4.4, 1.5.0, 1.5.1, 1.5.2, 1.5.3, 1.5.4, 1.5.5, 1.5.6, 1.5.7, 1.5.8, 1.6.0, 1.7.0, 1.7.1, 1.7.2, 1.7.3, 1.7.4, 1.7.5, 1.7.6, 1.7.7, 1.7.8, 1.7.9, 1.8.0, 1.8.1, 1.8.2, 1.8.3, 1.8.4, 1.8.5, 1.8.6, 1.8.7, 1.8.8, 1.8.9, 1.9.0, 0.1.0, 0.1.1, 0.1.2, 0.1.3, 0.1.5, 0.1.6, 0.1.8, 0.1.9, 0.2.0, 0.2.1, 0.2.2, 1.0.0, 1.0.1, 1.0.2, 1.13.2, 1.13.3, 1.13.4, 1.13.5, 1.14.4, 1.14.5, 1.2.2, 1.2.3, 1.3.2, 1.3.3, 1.3.4, 1.3.5, 1.6.1, 1.6.2, 1.8.10, 1.8.11, 1.8.12, 1.8.13, 1.9.1, 1.9.2, 1.9.3, 1.9.4, 1.9.5, 1.9.6, 1.9.7, 0.3.0, 0.4.0, 0.4.1, 0.4.2, 0.5.0, 0.6.0, 0.6.1, 0.6.10, 0.6.11, 0.6.2, 0.6.3, 0.6.4, 0.6.5, 0.6.6, 0.6.7, 0.6.8, 0.6.9, 0.7.0, 0.7.1, 0.7.2, 0.7.3, 0.7.4, 0.7.5, 0.7.6, 0.7.7, 0.8.0, 0.8.1, 0.8.2, 0.9.0, 0.9.1, 0.9.10, 0.9.11, 0.9.12, 0.9.13, 0.9.14, 0.9.15, 0.9.16, 0.9.17, 0.9.2, 0.9.3, 0.9.4, 0.9.5, 0.9.6, 0.9.7, 0.9.8, 0.9.9, 1.0.3, 1.0.5, 1.1.4, 1.2.4, 1.2.5, 1.4.5, 1.4.6, 1.4.7, 1.4.8, 1.5.10, 1.5.11, 1.5.12, 1.5.13, 1.5.14, 1.5.9
    
    kubectl-radar: 1.14.1 ==> 1109
    ```
    
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Formula/k/kubectl-radar.rb
+++ Formula/k/kubectl-radar.rb
@@ -5,6 +5,11 @@ class KubectlRadar < Formula
   sha256 "7e02a2ad29f3653dfea485bbab8934fcbf2b82045f57e7ce84871605f4b6fded"
   license "Apache-2.0"
 
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_golden_gate: "f9d3f3523592f1390ab1a9f1c4f62af98182eb9895571ab3dd2dc3e2214bde3b"
     sha256 cellar: :any_skip_relocation, arm64_tahoe:       "03971193f773741a244a2412b84433d972f77c01cd99321dabc515abe609dfa7"
