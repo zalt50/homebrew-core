10a4ff0cf0c0985007cea36696b7dbb2c3bf65c2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-25 11:28:45 +0000

    pmd 7.28.0

--- Formula/p/pmd.rb
+++ Formula/p/pmd.rb
@@ -1,8 +1,8 @@
 class Pmd < Formula
   desc "Source code analyzer for Java, JavaScript, and more"
   homepage "https://pmd.github.io"
-  url "https://github.com/pmd/pmd/releases/download/pmd_releases%2F7.27.0/pmd-dist-7.27.0-bin.zip"
-  sha256 "4ae396ffaf2b0d3ef0b73a10b2925e77066f73d57a4ce9078c60e7302bcddec9"
+  url "https://github.com/pmd/pmd/releases/download/pmd_releases%2F7.28.0/pmd-dist-7.28.0-bin.zip"
+  sha256 "f974ba571f7bc01c73fffe11bade25fbc1f438698f9e9c00e416e8d65e9fbac2"
   license "BSD-4-Clause"
 
   livecheck do
@@ -16,6 +16,8 @@ class Pmd < Formula
 
   depends_on "openjdk"
 
+  deny_network_access!
+
   def install
     rm Dir["bin/*.bat"]
     libexec.install Dir["*"]
@@ -23,7 +25,7 @@ class Pmd < Formula
   end
 
   test do
-    (testpath/"java/testClass.java").write <<~JAVA
+    (testpath/"java/BrewTestClass.java").write <<~JAVA
       public class BrewTestClass {
         // dummy constant
         public String SOME_CONST = "foo";
