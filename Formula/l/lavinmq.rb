b2a45d3c25707368a6625eb402ac0b94c3bfc588
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-25 15:14:30 +0000

    lavinmq: update 2.10.0 bottle.

--- Formula/l/lavinmq.rb
+++ Formula/l/lavinmq.rb
@@ -7,12 +7,11 @@ class Lavinmq < Formula
   head "https://github.com/cloudamqp/lavinmq.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any, arm64_golden_gate: "0df9309468faf135855f8277ab5965c61b7e65e3698c3c23063b77b43f7e9c52"
-    sha256 cellar: :any, arm64_tahoe:       "8f0a48331fe09e37a123fe76d60919adbd187c8cdd45ab0a1abae89f1b947bca"
-    sha256 cellar: :any, arm64_sequoia:     "2e8c020b0d2d9830bf69b6ee6e748b4fc296c7713f395c5ed9d55ee502c38405"
-    sha256 cellar: :any, arm64_sonoma:      "fa2661113c90ed56235daf7968d6cb7dd485c30dc3f0ae74997fcf3a4e58a095"
-    sha256 cellar: :any, arm64_linux:       "0ff998fddaf9b9439aec2b53c86e4fcbb7681947c8cb986590b9c0d87810e45c"
-    sha256 cellar: :any, x86_64_linux:      "fd70bbab0e1c0372b4d29192bf4f9412e1a71f48bbf7e5c3581e614f9bbcaf4e"
+    sha256 cellar: :any, arm64_golden_gate: "4eea07750eeab197a5a7b39b20f35fe862b121b43ad9285732622ed8d2041a7f"
+    sha256 cellar: :any, arm64_tahoe:       "00bceb8cb4b1c56090b3b670b38c6f44db7595ecfb0cf5509f9524da7ec88c71"
+    sha256 cellar: :any, arm64_sequoia:     "1b375442ef985c291d83fb4081d93aaf996f41df742b5f50176321c2a92d17cf"
+    sha256 cellar: :any, arm64_linux:       "c125db50c7f0bb0bd03cf0001ed3fad9262a417531e817e93df89291b3aa6d72"
+    sha256 cellar: :any, x86_64_linux:      "219d1ce430ac5bd95e6ca44828ddde975e16d997282d2015722f2871ce0eab57"
   end
 
   depends_on "crystal" => :build
