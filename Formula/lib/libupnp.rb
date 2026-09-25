3e05706ab3396432c37f63d1783e4373b82a2797
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-25 20:28:45 +0000

    libupnp 22.1.5

--- Formula/lib/libupnp.rb
+++ Formula/lib/libupnp.rb
@@ -1,8 +1,8 @@
 class Libupnp < Formula
   desc "Portable UPnP development kit"
   homepage "https://pupnp.sourceforge.io/"
-  url "https://github.com/pupnp/pupnp/releases/download/release-22.1.4/libupnp-22.1.4.tar.bz2"
-  sha256 "ff26dbf59e786cedbffd37192c44042207349fc04680b67ef48a4c2505426944"
+  url "https://github.com/pupnp/pupnp/releases/download/release-22.1.5/libupnp-22.1.5.tar.bz2"
+  sha256 "6fca8477d885ec5b7bbc81cf3d21cd36ff5a24da925aa8e86a44e8a20f2da67d"
   license "BSD-3-Clause"
 
   livecheck do
