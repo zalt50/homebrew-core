7a6dd576ec7166885d6b10de60149245c4cb3bd3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-25 02:27:57 +0000

    letta-code 0.33.0

--- Formula/l/letta-code.rb
+++ Formula/l/letta-code.rb
@@ -1,8 +1,8 @@
 class LettaCode < Formula
   desc "Memory-first coding agent"
   homepage "https://docs.letta.com/letta-code"
-  url "https://registry.npmjs.org/@letta-ai/letta-code/-/letta-code-0.32.19.tgz"
-  sha256 "772ed30cd105b8cb788ba36b750e8ee60bb00ef5df35b9abe1196d9468d58183"
+  url "https://registry.npmjs.org/@letta-ai/letta-code/-/letta-code-0.33.0.tgz"
+  sha256 "073b2ad10f2206ec90383abe8c480cad6cf1756f3c34d44ab1e0bbda40de32f1"
   license "Apache-2.0"
 
   bottle do
