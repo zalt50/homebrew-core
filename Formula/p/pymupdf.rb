8b378bf8bcd50102226ec3b7de77d99185fd27a4
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2026-09-26 01:07:30 +0900

    pymupdf: revision bump (mupdf 1.28.5)

--- Formula/p/pymupdf.rb
+++ Formula/p/pymupdf.rb
@@ -4,7 +4,7 @@ class Pymupdf < Formula
   url "https://files.pythonhosted.org/packages/a3/fb/b6761fa2d5266f2cdb24c3b91f4023070ab7848381417678e7a289a1d52a/pymupdf-1.28.2.tar.gz"
   sha256 "5e0be7908a715aa20333caddd73f1d6f01e4cd0c26e869fa2dd0b7f344da2249"
   license "AGPL-3.0-only"
-  revision 2
+  revision 3
 
   bottle do
     sha256 cellar: :any, arm64_golden_gate: "7191121625872a01bee8395ff74e1d283d572498bcd88859d7d33f0a296b5a1a"
