04bcce7f8b79a87441faf564f0be4dc4a5c6a1a2
Michael Cho <michael@michaelcho.dev>
2026-09-24 21:22:34 -0400

    berkeley-db: migrate to `openssl@4`

--- Formula/b/berkeley-db.rb
+++ Formula/b/berkeley-db.rb
@@ -5,7 +5,7 @@ class BerkeleyDb < Formula
   mirror "https://fossies.org/linux/misc/db-18.1.40.tar.gz"
   sha256 "0cecb2ef0c67b166de93732769abdeba0555086d51de1090df325e18ee8da9c8"
   license "AGPL-3.0-only"
-  revision 2
+  revision 4
 
   livecheck do
     url "https://www.oracle.com/database/technologies/related/berkeleydb-downloads.html"
@@ -33,7 +33,7 @@ class BerkeleyDb < Formula
 
   keg_only :provided_by_macos
 
-  depends_on "openssl@3"
+  depends_on "openssl@4"
 
   # Fix -flat_namespace being used on Big Sur and later.
   patch do
