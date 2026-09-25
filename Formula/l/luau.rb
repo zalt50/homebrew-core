2d8af111b00cd8a0ed655b4208d3942fcdfa31dc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-25 20:30:33 +0000

    luau 0.740

--- Formula/l/luau.rb
+++ Formula/l/luau.rb
@@ -1,8 +1,8 @@
 class Luau < Formula
   desc "Fast, safe, gradually typed embeddable scripting language derived from Lua"
   homepage "https://luau.org"
-  url "https://github.com/luau-lang/luau/archive/refs/tags/0.739.tar.gz"
-  sha256 "7eca9d2e4362588e9ce95f2fa976e46252231564b29a88fe77f0806a4eae9b40"
+  url "https://github.com/luau-lang/luau/archive/refs/tags/0.740.tar.gz"
+  sha256 "419f96e4ecf5a0dd415a2625ea7ea9dc8a407e86d64b25e00936302b25a7afbd"
   license "MIT"
   version_scheme 1
   head "https://github.com/luau-lang/luau.git", branch: "master"
