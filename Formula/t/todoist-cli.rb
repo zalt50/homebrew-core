7169186139f09feb8c2a20e481434eae930fcde3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-25 12:09:36 +0000

    todoist-cli: update 5.4.2 bottle.

--- Formula/t/todoist-cli.rb
+++ Formula/t/todoist-cli.rb
@@ -6,11 +6,11 @@ class TodoistCli < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_golden_gate: "f7c4667f367bd01cd0e252e598122057fa362944a55637122bc417ee3f158238"
-    sha256 cellar: :any,                 arm64_tahoe:       "4e2ff675d36e339ad1f647f0b40295b58d5b4b1eafa58d893cc76749f5de6a73"
-    sha256 cellar: :any,                 arm64_sequoia:     "62052db50615cd82437c1275c560e4b2ff688324b84dbcc70b4add5d905e16cd"
-    sha256 cellar: :any_skip_relocation, arm64_linux:       "3acfd2a76c834618ac39d10e8e767ae1a0b17e1b67b647eee2c9215c679caf9b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:      "a6d6accf0a39933539395527976a5aafec4f62bec83243aae818b311d1528381"
+    sha256 cellar: :any,                 arm64_golden_gate: "d497602e78d171b8be2d28d4b232388fa5231216a24bdd4bfe57235bb67fa883"
+    sha256 cellar: :any,                 arm64_tahoe:       "799f41e435bcc6b491401ea968cd9d3c64f651d356a20123f15cd8e69ddc9eb1"
+    sha256 cellar: :any,                 arm64_sequoia:     "aa745298f5115e69bcdd83ebae90c85cfe44a36c189c9e996f6eea16b6e68bfc"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "7b7c9ec94a6a7739545291ec5d59a5ca205ca37785591f48419b743e39046098"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:      "cb2c4b6a5d1c8ba1b4553fe6723de85a7b2f35866f2aa82291ab7d5075738681"
   end
 
   depends_on "rust" => :build
