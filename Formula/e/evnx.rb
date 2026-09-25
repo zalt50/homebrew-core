27ffdfcbec3d4f5975d2a0851f425a3888514eec
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-25 14:51:09 +0000

    evnx: update 0.5.2 bottle.

--- Formula/e/evnx.rb
+++ Formula/e/evnx.rb
@@ -6,11 +6,11 @@ class Evnx < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "20644c4d1aff1a03e919f499dcd86b3a1d1f26a488e6648f1261c15fc6ccc2fa"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "b2d18988887c9186c7de9fb05e997b69205d7e35adb92ba45997abb73c003847"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "28665ee718d23f2be3a0346ccfeebc005aad779dcf268edb948e278e3df379db"
-    sha256 cellar: :any,                 arm64_linux:       "e209b7c51c029895fc7a6f864f0363f505ee8892f615ca8922c8dc967440d0a4"
-    sha256 cellar: :any,                 x86_64_linux:      "3668b00c02d6e51b10d8a59728da98295c1b46486933c7df0c9eb36aaa587e40"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "81aaccc0118a046e32f410d43865776b76d7f58c9add5f3334da9a52ce20003b"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "a4f4659e61dcab6f29e3b5a561a1d1c5e2296f47eee942acad602c6b8a48963f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "aa00a66dcb182b49b0a18ffbd8ece080912ec466f1ba813802add230f8d3a0fd"
+    sha256 cellar: :any,                 arm64_linux:       "a7cac0816e25981f0183927d17cad4063b8edf93a051c802141985d8a24329b0"
+    sha256 cellar: :any,                 x86_64_linux:      "a9749cc11e4888e8917f6c7f01b3a7140daeadfb16e24258aa317f344fe0ccec"
   end
 
   depends_on "rust" => :build
