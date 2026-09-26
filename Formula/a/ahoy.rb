143a2de780fceb078bf97f22656c4f438ea6288d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-26 03:10:04 +0000

    ahoy: update 3.0.2 bottle.

--- Formula/a/ahoy.rb
+++ Formula/a/ahoy.rb
@@ -7,12 +7,11 @@ class Ahoy < Formula
   head "https://github.com/ahoy-cli/ahoy.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "cfec4320a87043cb68e06c916110ae8d5da264259cf6c9f950c8afb0e1790644"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "cfec4320a87043cb68e06c916110ae8d5da264259cf6c9f950c8afb0e1790644"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "cfec4320a87043cb68e06c916110ae8d5da264259cf6c9f950c8afb0e1790644"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:      "cfec4320a87043cb68e06c916110ae8d5da264259cf6c9f950c8afb0e1790644"
-    sha256 cellar: :any_skip_relocation, arm64_linux:       "74e509a00183d99a75a3495a7d18497bf6d8c8781d8e15c599bc33f6ec91e512"
-    sha256 cellar: :any,                 x86_64_linux:      "2be13b1292133e38d8d21ab8f29484399bb1ebcf61145ea23d17b9f5e3cad862"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "b8b6fca45fdd1b82131871ecfa6e04945d56e199cf1ac9ae9731fed551105b81"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "b8b6fca45fdd1b82131871ecfa6e04945d56e199cf1ac9ae9731fed551105b81"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "b8b6fca45fdd1b82131871ecfa6e04945d56e199cf1ac9ae9731fed551105b81"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "a1069158e83e29bcec1e8aa2d08c891bd1d3b338e25cf83e5bd9a107bcc4739a"
+    sha256 cellar: :any,                 x86_64_linux:      "8f4e4a978352ee3a30e277a08b1dcd3a57cad76f7dab7e61c0b67dcefa926782"
   end
 
   depends_on "go" => :build
