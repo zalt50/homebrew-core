e1e4b4e52467c0496d2bc9e80914eb9c4440d5a8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-26 14:24:40 +0000

    keploy: update 3.6.77 bottle.

--- Formula/k/keploy.rb
+++ Formula/k/keploy.rb
@@ -7,11 +7,11 @@ class Keploy < Formula
   head "https://github.com/keploy/keploy.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "f435e97c36473dafd5ec02cd4019fb405bc8e476b2de92efbe0e13a60bf02d38"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "f435e97c36473dafd5ec02cd4019fb405bc8e476b2de92efbe0e13a60bf02d38"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "f435e97c36473dafd5ec02cd4019fb405bc8e476b2de92efbe0e13a60bf02d38"
-    sha256 cellar: :any_skip_relocation, arm64_linux:       "add94fefa4d9efc4853cc7ae5dfc3f3eaabc5ad5345d3bd00af262bf3e808729"
-    sha256 cellar: :any,                 x86_64_linux:      "ef617310af7e0270a8af14b5f4fedb31933efc82fdb8991c7fc3e3bf6bdc0e38"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "aa4ea8bb23bbd0062210bac4d058009dac6ab668d819cfbfd1c2861ce22186b3"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "aa4ea8bb23bbd0062210bac4d058009dac6ab668d819cfbfd1c2861ce22186b3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "aa4ea8bb23bbd0062210bac4d058009dac6ab668d819cfbfd1c2861ce22186b3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "f3a521add116a43bddf5cf8ec68c58d3bb18868a241405af968c2c7b5de1ede9"
+    sha256 cellar: :any,                 x86_64_linux:      "37e0a885ad921babbe9e0bd59937d4da00274aa10b395d76feb8d88ee0400c0e"
   end
 
   depends_on "go" => :build
