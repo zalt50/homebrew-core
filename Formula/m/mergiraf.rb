a124bc4f1cdc04e1943767f47778d533d168055b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-26 12:02:01 +0000

    mergiraf: update 0.20.0 bottle.

--- Formula/m/mergiraf.rb
+++ Formula/m/mergiraf.rb
@@ -7,12 +7,11 @@ class Mergiraf < Formula
   head "https://codeberg.org/mergiraf/mergiraf.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "6e24ba2ff071e80fcc71c2cfa32ad531f5c9babbda870ff10bcdf9308d8c381e"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "accfe0cbe2ca8bf10090bf83b9232a408ff35a9bb8e0f9008f3eb24827973daf"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "ca8ddbe0d66b1d66ab6a4f20bbfb5da27b18adaa4d59e59d2d9efe933dd71817"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:      "b48ec04e1ad7436573c642df2b7ccec3fa936365b662e0ee4242207d7dbf7811"
-    sha256 cellar: :any,                 arm64_linux:       "5c0784a53cb4e153fb9a0b9e98f0f9c42ae7635d85a33cd5f341c853fabf78d3"
-    sha256 cellar: :any,                 x86_64_linux:      "1644b1746e94668f297e6d7301cdb18d8d2e9112d78811319f12539c5c824ac8"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "6cfa5f2da73d1de20a2e5ac468087be030deef6a0bceab0fe8851d5adce0d57b"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "f0e38cdc14a1c56d2f6f66f04f9d7441a9d7fa27fbfbbd3388710734c10ecdde"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "6efc7bf4a38b82faf5b9cfc9d18a9bd6a648bd7e808e4267d2bdcf23ba686b2e"
+    sha256 cellar: :any,                 arm64_linux:       "c34d8d842b6f4d8d1f99b3281a0b563994cd807cf146e69bd32c247071faaac0"
+    sha256 cellar: :any,                 x86_64_linux:      "11105ab497a9b70e746240ac7ebaae55e75fabb91e16f7a84dbde841a0d49858"
   end
 
   depends_on "rust" => :build
