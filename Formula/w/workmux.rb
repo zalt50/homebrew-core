443816905100ef68cd5ea1df39e640e7a6e8ecc4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-26 12:01:44 +0000

    workmux: update 0.1.268 bottle.

--- Formula/w/workmux.rb
+++ Formula/w/workmux.rb
@@ -6,11 +6,11 @@ class Workmux < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "7c709b1f90c25abcb85509fd45abe2ea9dfe59c4096b81e51afb3e3f69094465"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "2129735e65b10e0b90f320545f4c545d1d939d492b6e797e3484882a61b683de"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "b6a85807b940d681c67a28b85e9ab09120f324ce73b88e994ee1612bee55def1"
-    sha256 cellar: :any,                 arm64_linux:       "77f6454e0d56642420b4ff582aae0ec92abb4f198088b6e1c2709ab18156c15a"
-    sha256 cellar: :any,                 x86_64_linux:      "d7e84a6e8d90d743afa7a567774e4a7b0a713486be3ce7d1636ea4c34cdd4fd5"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "e4f7c8733de7d9d935234f08e93db4bf1cf55f3c09beb8ceaad8eaf2c2505e7d"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "34d1940ae0ea83b0781c75b18fa27ebf552c4e11390abd44e68be39a75ed445e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "c68f3251396d96729214ed99bab73840aa6b981053c22c1563f7588958162ba0"
+    sha256 cellar: :any,                 arm64_linux:       "07cee987efd903e2a05918fb1bfdc3d2549ea716e626317e16c6caf45d72248e"
+    sha256 cellar: :any,                 x86_64_linux:      "884913d89a23c10539be9c880f6ec80454c51be843ae63b46dbf003df4228465"
   end
 
   depends_on "rust" => :build
