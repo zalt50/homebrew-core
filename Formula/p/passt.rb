034ac580730ee050e27f71b25a70346b362d5297
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-26 08:21:38 +0000

    passt 2026_09_25.df90211

--- Formula/p/passt.rb
+++ Formula/p/passt.rb
@@ -1,9 +1,9 @@
 class Passt < Formula
   desc "User-mode networking daemons for virtual machines and namespaces"
   homepage "https://passt.top/passt/about/"
-  url "https://passt.top/passt/snapshot/passt-2026_07_28.f8df3f1.tar.xz"
-  version "2026_07_28.f8df3f1"
-  sha256 "fcfeb5fbdf775bcc48edc1d5eac8a6d57bc333f8e67b714149376d36061416f0"
+  url "https://passt.top/passt/snapshot/passt-2026_09_25.df90211.tar.xz"
+  version "2026_09_25.df90211"
+  sha256 "cdf655b5677ce219108cf3df93a55e0ae98a376887be110fe35303ea900d5d68"
   license all_of: ["GPL-2.0-or-later", "BSD-3-Clause"]
   head "git://passt.top/passt", branch: "master"
 
@@ -31,7 +31,7 @@ class Passt < Formula
     begin
       # Just check failure as unable to use pasta or passt on unprivileged Docker
       output = shell_output("#{bin}/pasta --pid #{pidfile} 2>&1", 1)
-      assert_match "Couldn't create user namespace", output
+      assert_match "Unable to create user namespace", output
     ensure
       if pidfile.exist? && (pid = pidfile.read.to_i).positive?
         Process.kill("TERM", pid)
